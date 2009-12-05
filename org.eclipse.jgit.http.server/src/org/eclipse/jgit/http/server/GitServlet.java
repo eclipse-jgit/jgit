a318f5e0620ef9daa0fd73554bf69e202d8f3398
Shawn O. Pearce <spearce@spearce.org>
2009-12-04 20:37:02 -0800

    Simple dumb HTTP server for Git
    This is a simple HTTP server that provides the minimum server side
    support required for dumb (non-git aware) transport clients.
    
    We produce the info/refs and objects/info/packs file on the fly
    from the local repository state, but otherwise serve data as raw
    files from the on-disk structure.
    
    In the future we could better optimize the FileSender class and the
    servlets that use it to take advantage of direct file to network
    APIs in more advanced servlet containers like Jetty.
    
    Our glue package borrows the idea of a micro embedded DSL from
    Google Guice and uses it to configure a collection of Filters
    and HttpServlets, all of which are matched against requests using
    regular expressions.  If a subgroup exists in the pattern, it is
    extracted and used for the path info component of the request.
    
    Change-Id: Ia0f1a425d07d035e344ae54faf8aeb04763e7487
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/GitServlet.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2009, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server;
+
+import java.io.File;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.eclipse.jgit.http.server.glue.MetaServlet;
+import org.eclipse.jgit.http.server.glue.RegexGroupFilter;
+import org.eclipse.jgit.http.server.glue.ServletBinder;
+import org.eclipse.jgit.http.server.resolver.FileResolver;
+import org.eclipse.jgit.http.server.resolver.GetAnyFile;
+import org.eclipse.jgit.http.server.resolver.RepositoryResolver;
+import org.eclipse.jgit.lib.Constants;
+
+/**
+ * Handles Git repository access over HTTP.
+ * <p>
+ * Applications embedding this servlet should map a directory path within the
+ * application to this servlet, for example:
+ *
+ * <pre>
+ *   &lt;servlet&gt;
+ *     &lt;servlet-name&gt;GitServlet&lt;/servlet-name&gt;
+ *     &lt;servlet-class&gt;org.eclipse.jgit.http.server.GitServlet&lt;/servlet-class&gt;
+ *     &lt;init-param&gt;
+ *       &lt;param-name&gt;base-path&lt;/param-name&gt;
+ *       &lt;param-value&gt;/var/srv/git&lt;/param-value&gt;
+ *     &lt;/init-param&gt;
+ *   &lt;/servlet&gt;
+ *   &lt;servlet-mapping&gt;
+ *     &lt;servlet-name&gt;GitServlet&lt;/servlet-name&gt;
+ *     &lt;url-pattern&gt;/git/*&lt;/url-pattern&gt;
+ *   &lt;/servlet-mapping&gt;
+ * </pre>
+ *
+ * <p>
+ * Applications may wish to add additional repository action URLs to this
+ * servlet by taking advantage of its extension from {@link MetaServlet}.
+ * Callers may register their own URL suffix translations through
+ * {@link #serve(String)}, or their regex translations through
+ * {@link #serveRegex(String)}. Each translation should contain a complete
+ * filter pipeline which ends with the HttpServlet that should handle the
+ * requested action.
+ */
+public class GitServlet extends MetaServlet {
+	private static final long serialVersionUID = 1L;
+
+	private volatile boolean initialized;
+
+	private RepositoryResolver resolver;
+
+	private GetAnyFile getAnyFile = new GetAnyFile();
+
+	/**
+	 * New servlet that will load its base directory from {@code web.xml}.
+	 * <p>
+	 * The required parameter {@code base-path} must be configured to point to
+	 * the local filesystem directory where all served Git repositories reside.
+	 */
+	public GitServlet() {
+		// Initialized above by field declarations.
+	}
+
+	/**
+	 * New servlet configured with a specific resolver.
+	 *
+	 * @param resolver
+	 *            the resolver to use when matching URL to Git repository. If
+	 *            null the {@code base-path} parameter will be looked for in the
+	 *            parameter table during init, which usually comes from the
+	 *            {@code web.xml} file of the web application.
+	 */
+	public void setRepositoryResolver(RepositoryResolver resolver) {
+		assertNotInitialized();
+		this.resolver = resolver;
+	}
+
+	/**
+	 * @param f
+	 *            the filter to validate direct access to repository files
+	 *            through a dumb client. If {@code null} then dumb client
+	 *            support is completely disabled.
+	 */
+	public void setGetAnyFile(GetAnyFile f) {
+		assertNotInitialized();
+		this.getAnyFile = f != null ? f : GetAnyFile.DISABLED;
+	}
+
+	private void assertNotInitialized() {
+		if (initialized)
+			throw new IllegalStateException("Already initialized by container");
+	}
+
+	@Override
+	public void init(final ServletConfig config) throws ServletException {
+		super.init(config);
+
+		if (resolver == null) {
+			final String basePath = config.getInitParameter("base-path");
+			if (basePath == null || "".equals(basePath))
+				throw new ServletException("Filter parameter base-path not set");
+			setRepositoryResolver(new FileResolver(new File(basePath)));
+		}
+
+		initialized = true;
+
+		if (getAnyFile != GetAnyFile.DISABLED) {
+			final IsLocalFilter mustBeLocal = new IsLocalFilter();
+			final GetAnyFileFilter enabled = new GetAnyFileFilter(getAnyFile);
+
+			serve("*/" + Constants.INFO_REFS)//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.with(new InfoRefsServlet());
+
+			serve("*/" + Constants.HEAD)//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.with(new TextFileServlet(Constants.HEAD));
+
+			final String info_alternates = "objects/info/alternates";
+			serve("*/" + info_alternates)//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.with(new TextFileServlet(info_alternates));
+
+			final String http_alternates = "objects/info/http-alternates";
+			serve("*/" + http_alternates)//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.with(new TextFileServlet(http_alternates));
+
+			serve("*/objects/info/packs")//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.with(new InfoPacksServlet());
+
+			serveRegex("^/(.*)/objects/([0-9a-f]{2}/[0-9a-f]{38})$")//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.through(new RegexGroupFilter(2))//
+					.with(new ObjectFileServlet.Loose());
+
+			serveRegex("^/(.*)/objects/(pack/pack-[0-9a-f]{40}\\.pack)$")//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.through(new RegexGroupFilter(2))//
+					.with(new ObjectFileServlet.Pack());
+
+			serveRegex("^/(.*)/objects/(pack/pack-[0-9a-f]{40}\\.idx)$")//
+					.through(mustBeLocal)//
+					.through(enabled)//
+					.through(new RegexGroupFilter(2))//
+					.with(new ObjectFileServlet.PackIdx());
+		}
+	}
+
+	@Override
+	protected ServletBinder register(final ServletBinder b) {
+		if (resolver == null)
+			throw new IllegalStateException("No resolver available");
+		return b.through(new RepositoryFilter(resolver));
+	}
+}
---
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/glue/RegexGroupFilter.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2009, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server.glue;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Switch servlet path and path info to use another regex match group.
+ * <p>
+ * This filter is meant to be installed in the middle of a pipeline created by
+ * {@link MetaServlet#serveRegex(String)}. The passed request's servlet path is
+ * updated to be all text up to the start of the designated capture group, and
+ * the path info is changed to the contents of the capture group.
+ **/
+public class RegexGroupFilter implements Filter {
+	private final int groupIdx;
+
+	/**
+	 * @param groupIdx
+	 *            capture group number, 1 through the number of groups.
+	 */
+	public RegexGroupFilter(final int groupIdx) {
+		if (groupIdx < 1)
+			throw new IllegalArgumentException("Invalid index: " + groupIdx);
+		this.groupIdx = groupIdx - 1;
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		// Do nothing.
+	}
+
+	public void destroy() {
+		// Do nothing.
+	}
+
+	public void doFilter(final ServletRequest request,
+			final ServletResponse rsp, final FilterChain chain)
+			throws IOException, ServletException {
+		final WrappedRequest[] g = groupsFor(request);
+		if (groupIdx < g.length)
+			chain.doFilter(g[groupIdx], rsp);
+		else
+			throw new ServletException("Invalid regex group " + (groupIdx + 1));
+	}
+
+	private static WrappedRequest[] groupsFor(final ServletRequest r) {
+		return (WrappedRequest[]) r.getAttribute(MetaServlet.REGEX_GROUPS);
+	}
+}
