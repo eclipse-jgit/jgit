f252b000fce1b3c0ed94701a998543b48385b95e
Kristoff Kiefer <kristoff.kiefer@googlemail.com>
2009-12-21 16:42:54 -0800

    Clarify the definition of AnyObjectId.RAW_LEN and make assumptions explicit
    This value should always be 20, and should always match with
    Constants.OBJECT_ID_LENGTH.  Hardcode the constant to 20 to
    support inlining, but check at class load time that the value
    in Constants still matches our assumptions.
    
    Also make the check for it an explicit assert. This will make the programm
    fail at runtime when the assumption is violated.
    Also it will eliminate the "dead code" warning encountered before.
    The added comment will warn off anybody trying to change the assert.
    
    Change-Id: I03f52131e64edcd0aa74bbbf36e7d42faaf4a698

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/AnyObjectId.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/AnyObjectId.java
@@ -58,16 +58,19 @@ import org.eclipse.jgit.util.NB;
  * represent a different object name.
  */
 public abstract class AnyObjectId implements Comparable {
-	static final int RAW_LEN = Constants.OBJECT_ID_LENGTH;
-
-	static final int STR_LEN = RAW_LEN * 2;
+	static final int RAW_LEN = 20;
 
 	static {
-		if (RAW_LEN != 20)
-			throw new LinkageError("ObjectId expects"
-					+ " Constants.OBJECT_ID_LENGTH = 20; it is " + RAW_LEN
-					+ ".");
+		/*
+		 * Throughout the code of JGIT it is assumed that RAW_LEN is 20.
+		 * If someone were to change the hashing algorithm this would not necessary be the case
+		 * anymore. We'd rather fail here then somewhere deeper down.
+		 */
+		assert RAW_LEN == 20;
 	}
+
+
+	static final int STR_LEN = RAW_LEN * 2;
 
 	/**
 	 * Compare to object identifier byte sequences for equality.
