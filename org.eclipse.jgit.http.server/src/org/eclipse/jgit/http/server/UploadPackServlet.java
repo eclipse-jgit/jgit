feea3a8cde57d6ec8a6187062aa8330cd9ede52f
Shawn O. Pearce <spearce@spearce.org>
2010-01-06 11:13:25 -0800

    server side: smart fetch over HTTP
    Clients can request smart fetch support by examining the info/refs URL
    with the service parameter set to the magic git-upload-pack string:
    
      GET /$GIT_DIR/info/refs?service=git-upload-pack HTTP/1.1
    
    The response is formatted with the upload pack capabilities, using
    the standard packet line formatter.  A special header line is put
    in front of the standard upload-pack advertisement to let clients
    know the service was recognized and is supported.
    
    If the requested service is disabled an authorization status code is
    returned, allowing the user agent to retry once they have obtained
    credentials from a human, in case authentication is required by
    the configured UploadPackFactory implementation.
    
    Change-Id: Ib0f1a458c88b4b5509b0f882f55f83f5752bc57a
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/UploadPackServlet.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2009-2010, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server;
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE;
+import static org.eclipse.jgit.http.server.ServletUtils.getInputStream;
+import static org.eclipse.jgit.http.server.ServletUtils.getRepository;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jgit.http.server.resolver.ServiceNotAuthorizedException;
+import org.eclipse.jgit.http.server.resolver.ServiceNotEnabledException;
+import org.eclipse.jgit.http.server.resolver.UploadPackFactory;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.UploadPack;
+import org.eclipse.jgit.transport.RefAdvertiser.PacketLineOutRefAdvertiser;
+
+/** Server side implementation of smart fetch over HTTP. */
+class UploadPackServlet extends HttpServlet {
+	private static final String REQ_TYPE = "application/x-git-upload-pack-request";
+
+	private static final String RSP_TYPE = "application/x-git-upload-pack-result";
+
+	private static final long serialVersionUID = 1L;
+
+	static class InfoRefs extends SmartServiceInfoRefs {
+		private final UploadPackFactory uploadPackFactory;
+
+		InfoRefs(final UploadPackFactory uploadPackFactory) {
+			super("git-upload-pack");
+			this.uploadPackFactory = uploadPackFactory;
+		}
+
+		@Override
+		protected void advertise(HttpServletRequest req, Repository db,
+				PacketLineOutRefAdvertiser pck) throws IOException,
+				ServiceNotEnabledException, ServiceNotAuthorizedException {
+			uploadPackFactory.create(req, db).sendAdvertisedRefs(pck);
+		}
+	}
+
+	private final UploadPackFactory uploadPackFactory;
+
+	UploadPackServlet(final UploadPackFactory uploadPackFactory) {
+		this.uploadPackFactory = uploadPackFactory;
+	}
+
+	@Override
+	public void doPost(final HttpServletRequest req,
+			final HttpServletResponse rsp) throws IOException {
+		if (!REQ_TYPE.equals(req.getContentType())) {
+			rsp.sendError(SC_UNSUPPORTED_MEDIA_TYPE);
+			return;
+		}
+
+		final Repository db = getRepository(req);
+		try {
+			final UploadPack up = uploadPackFactory.create(req, db);
+			up.setBiDirectionalPipe(false);
+			rsp.setContentType(RSP_TYPE);
+			up.upload(getInputStream(req), rsp.getOutputStream(), null);
+
+		} catch (ServiceNotAuthorizedException e) {
+			rsp.reset();
+			rsp.sendError(SC_UNAUTHORIZED);
+			return;
+
+		} catch (ServiceNotEnabledException e) {
+			rsp.reset();
+			rsp.sendError(SC_FORBIDDEN);
+			return;
+
+		} catch (IOException e) {
+			getServletContext().log("Internal error during upload-pack", e);
+			rsp.reset();
+			rsp.sendError(SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+	}
+}
