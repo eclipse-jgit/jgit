a3048839a352f65c3bbbdeee80b46130eafb8933
Shawn O. Pearce <spearce@spearce.org>
2010-02-10 11:49:27 -0800

    Capture non-progress side band #2 messages and put in result
    Any messages received on side band #2 that aren't scraped as a
    progress message into our ProgressMonitor are now forwarded to a
    buffer which is later included into the OperationResult object.
    Application callers can use this buffer to present the additional
    messages from the remote peer after the push or fetch operation
    has concluded.
    
    The smart push connections using the native send-pack/receive-pack
    protocol now request side-band-64k capability if it is available
    and forward any messages received through that channel onto this
    message buffer.  This makes hook messages available over smart HTTP,
    or even over SSH.
    
    The SSH transport was modified to redirect the remote command's
    stderr stream into the message buffer, interleaved with any data
    received over side band #2.  Due to buffering between these two
    different channels in the SSH channel mux itself the order of any
    writes between the two cannot be ensured, but it tries to stay close.
    
    The local fork transport was also modified to redirect the local
    receive-pack's stderr into the message buffer, rather than going to
    the invoking JVM's System.err.  This gives applications a chance
    to log the local error messages, rather than needing to redirect
    their JVM's stderr before startup.
    
    To keep things simple, the application has to wait for the entire
    operation to complete before it can see the messages.  This may
    be a downside if the user is trying to debug a remote hook that is
    blocking indefinitely, the user would need to abort the connection
    before they can inspect the message buffer in any sort of UI built
    on top of JGit.
    
    Change-Id: Ibc215f4569e63071da5b7e5c6674ce924ae39e11
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackPushConnection.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackPushConnection.java
@@ -94,6 +94,8 @@ class BasePackPushConnection extends BasePackConnection implements
 
 	private boolean capableReport;
 
+	private boolean capableSideBand;
+
 	private boolean capableOfsDelta;
 
 	private boolean sentCommand;
@@ -145,8 +147,21 @@ class BasePackPushConnection extends BasePackConnection implements
 			writeCommands(refUpdates.values(), monitor);
 			if (writePack)
 				writePack(refUpdates, monitor);
-			if (sentCommand && capableReport)
-				readStatusReport(refUpdates);
+			if (sentCommand) {
+				if (capableReport)
+					readStatusReport(refUpdates);
+				if (capableSideBand) {
+					// Ensure the data channel is at EOF, so we know we have
+					// read all side-band data from all channels and have a
+					// complete copy of the messages (if any) buffered from
+					// the other data channels.
+					//
+					int b = in.read();
+					if (0 <= b)
+						throw new TransportException(uri, "expected EOF;"
+								+ " received '" + (char) b + "' instead");
+				}
+			}
 		} catch (TransportException e) {
 			throw e;
 		} catch (Exception e) {
@@ -158,7 +173,7 @@ class BasePackPushConnection extends BasePackConnection implements
 
 	private void writeCommands(final Collection<RemoteRefUpdate> refUpdates,
 			final ProgressMonitor monitor) throws IOException {
-		final String capabilities = enableCapabilities();
+		final String capabilities = enableCapabilities(monitor);
 		for (final RemoteRefUpdate rru : refUpdates) {
 			if (!capableDeleteRefs && rru.isDelete()) {
 				rru.setStatus(Status.REJECTED_NODELETE);
@@ -191,11 +206,18 @@ class BasePackPushConnection extends BasePackConnection implements
 		outNeedsEnd = false;
 	}
 
-	private String enableCapabilities() {
+	private String enableCapabilities(final ProgressMonitor monitor) {
 		final StringBuilder line = new StringBuilder();
 		capableReport = wantCapability(line, CAPABILITY_REPORT_STATUS);
 		capableDeleteRefs = wantCapability(line, CAPABILITY_DELETE_REFS);
 		capableOfsDelta = wantCapability(line, CAPABILITY_OFS_DELTA);
+
+		capableSideBand = wantCapability(line, CAPABILITY_SIDE_BAND_64K);
+		if (capableSideBand) {
+			in = new SideBandInputStream(in, monitor, getMessageWriter());
+			pckIn = new PacketLineIn(in);
+		}
+
 		if (line.length() > 0)
 			line.setCharAt(0, '\0');
 		return line.toString();
