20d27f161c925cd4e7d95246cd427ea83f9cc0c8
Sasa Zivkov <sasa.zivkov@sap.com>
2010-02-24 17:25:10 +0100

    Provide NLS support in JGIT.
    The support for NLS relies on java.util API to load a standard
    ResourceBundle and then uses java reflection API to inject localized
    strings into public String fields of the corresponding instance
    of TranslationBundle.
    
    Locale setting is supported per thread to enable concurrent threads
    to use different locales. This is useful when JGit runs in a server
    context where (error) messages might need to differ per-request to
    suit the user's preference.
    
    Change-Id: Ie0e63a0d7bb74eaad495dbe8248595d8a3a76883
    Signed-off-by: Sasa Zivkov <sasa.zivkov@sap.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/nls/NLS.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.nls;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * The purpose of this class is to provide NLS (National Language Support)
+ * configurable per thread.
+ *
+ * <p>
+ * The {@link #setLocale(Locale)} method is used to configure locale for the
+ * calling thread. The locale setting is thread inheritable. This means that a
+ * child thread will have the same locale setting as its creator thread until it
+ * changes it explicitly.
+ *
+ * <p>
+ * Example of usage:
+ *
+ * <pre>
+ * NLS.setLocale(Locale.GERMAN);
+ * TransportText t = NLS.getBundleFor(TransportText.class);
+ * </pre>
+ */
+public class NLS {
+
+	private static final InheritableThreadLocal<NLS> local = new InheritableThreadLocal<NLS>();
+
+	/**
+	 * Sets the locale for the calling thread.
+	 * <p>
+	 * The {@link #getBundleFor(Class)} method will honor this setting if if it
+	 * is supported by the provided resource bundle property files. Otherwise,
+	 * it will use a fallback locale as described in the
+	 * {@link TranslationBundle}
+	 *
+	 * @param locale
+	 *            the preferred locale
+	 */
+	public static void setLocale(Locale locale) {
+		local.set(new NLS(locale));
+	}
+
+	/**
+	 * Returns an instance of the translation bundle of the required type. All
+	 * public String fields of the bundle instance will get their values
+	 * injected as described in the {@link TranslationBundle}.
+	 *
+	 * @param <T>
+	 *            required bundle type
+	 * @param type
+	 *            required bundle type
+	 * @return an instance of the required bundle type
+	 */
+	public static <T extends TranslationBundle> T getBundleFor(Class<T> type) {
+		NLS self = local.get();
+		if (self == null) {
+			self = new NLS(Locale.getDefault());
+		}
+		return self.get(type);
+	}
+
+	private NLS(Locale locale) {
+		this.locale = locale;
+	}
+
+	private Locale locale;
+	private Map<Class, TranslationBundle> map = new HashMap<Class, TranslationBundle>();
+
+	private synchronized <T extends TranslationBundle> T get(Class<T> type) {
+		try {
+			TranslationBundle bundle = map.get(type);
+			if (bundle == null) {
+				bundle = type.newInstance();
+				bundle.load(locale);
+				map.put(type, bundle);
+			}
+			return (T) bundle;
+		} catch (InstantiationException e) {
+			throw new Error(e);
+		} catch (IllegalAccessException e) {
+			throw new Error(e);
+		}
+	}
+}
