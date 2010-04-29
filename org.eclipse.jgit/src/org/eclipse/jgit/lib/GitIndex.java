62aac949e2ba87ad3ad68b22d7b1564b5045dea2
Marc Strapetz <marc.strapetz@syntevo.com>
2010-04-29 20:35:39 +0200

    Getter for mtime field of a GitIndex entry added.

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/GitIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/GitIndex.java
@@ -740,6 +740,14 @@ public class GitIndex {
 		public int getModeBits() {
 			return mode;
 		}
+
+		/**
+		 * Return the index modification time of this entry.
+		 * @return index modification time
+		 */
+		public long getMtime() {
+			return mtime;
+		}
 	}
 
 	static class Header {
