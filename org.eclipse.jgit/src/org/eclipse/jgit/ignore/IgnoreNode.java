b270281c068ee5d3cdac29a7970c61c3b9020c4e
Charley Wang <chwang@redhat.com>
2010-05-11 10:04:14 -0400

    Add compatibility with gitignore specifications
    I tried to meet the requirements. Let me know if there's
    anything that needs changing :)
    
    I will attach the test cases shortly. Please see my talk page
    for some documentation of the covered cases (documentation is
    not comprehensive right now)
    
    http://wiki.eclipse.org/User_talk:Charley.wang.gmail.com
    
    Todo:
    - Implement more intelligent node parsing.
    - Better cache than just a HashMap. Suggestion was to move
      this into RepositoryMapping in EGit.
    - The rule for matching e.g. "/src/ne?" to "/src/new/file.c"
      is slow.
    
    Bug-id: 303925
    Change-Id: I5c40e237275f2d9031b2b56591ba98c5ce166008

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/ignore/IgnoreNode.java
@@ -0,0 +1,213 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jgit.ignore;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+/**
+ * Represents a bundle of ignore nodes.
+ *
+ *
+ * @author Charley Wang
+ *
+ */
+public class IgnoreNode {
+	//The base directory will be used to find the .gitignore file
+	private File baseDir;
+	//Only used for root node.
+	private File secondaryFile;
+	private ArrayList<IgnoreRule> rules;
+	private boolean matched;
+
+
+	/**
+	 * Create a new ignore node based on the given directory. The node's
+	 * ignore file will be the .gitignore file in the directory (if any)
+	 * Rules contained within this node will only be applied to files
+	 * which are descendants of this directory.
+	 *
+	 * @param baseDir -- base directory of this ignore node
+	 */
+	public IgnoreNode(File baseDir) {
+		this.baseDir = baseDir;
+		rules = new ArrayList<IgnoreRule>();
+		secondaryFile = null;
+	}
+
+
+	/**
+	 * Parse files according to gitignore standards.
+	 *
+	 * @throws IOException
+	 */
+	private void parse() throws IOException {
+		if (secondaryFile != null && secondaryFile.exists())
+			parse(secondaryFile);
+
+		parse(new File(baseDir.getAbsolutePath() + "/.gitignore"));
+	}
+
+
+	private void parse(File targetFile) throws IOException {
+		if (!targetFile.exists()) {
+			return;
+		}
+
+		BufferedReader br = new BufferedReader(new FileReader(targetFile));
+		String txt;
+		while ((txt = br.readLine()) != null) {
+			txt = txt.trim();
+			if (txt.length() > 0 && !txt.startsWith("#")) {
+				rules.add(new IgnoreRule(txt));
+			}
+		}
+
+	}
+
+
+	/**
+	 * @return base directory for the .gitignore
+	 */
+	public String getBaseDir() {
+		return baseDir.getAbsolutePath();
+	}
+
+
+	/**
+	 *
+	 * @return Ignore rules as an array list.
+	 */
+	public ArrayList<IgnoreRule> getRules() {
+		return rules;
+	}
+
+
+	/**
+	 *
+	 * Returns whether or not a target is matched as being ignored by
+	 * any patterns in this directory.
+	 * <br>
+	 * Will return false if the file is not a descendant of this directory.
+	 * <br>
+	 *
+	 * @param targetFile
+	 * @return whether or not the target is ignored
+	 * @throws IOException
+	 */
+	public boolean isIgnored(File targetFile) throws IOException {
+		String target = targetFile.getAbsolutePath();
+		if (!target.startsWith(baseDir.getAbsolutePath())) {
+			return false;
+		}
+		if (rules.isEmpty()) {
+			try {
+				parse();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rules == null || rules.size() == 0)
+			return false;
+
+
+		/*
+		 * Boolean matched is necessary because we may have encountered
+		 * a negation ("!/test.c").
+		 */
+		matched = false;
+		String tar = target.replaceFirst(baseDir.getAbsolutePath(), "");
+		int i;
+		//Parse rules in the reverse order that they were read
+		for (i = rules.size() -1; i > -1; i--) {
+			matched = rules.get(i).isMatch(tar, targetFile.isDirectory());
+			if (matched) {
+				break;
+			}
+		}
+
+
+		if (i > -1 && rules.get(i) != null) {
+			return rules.get(i).getResult();
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * @return True if the previous call to isIgnored resulted in a match.
+	 * False otherwise.
+	 */
+	public boolean wasMatched() {
+		return matched;
+	}
+
+
+	/**
+	 * Adds another file as a source of ignore rules for this file. The
+	 * secondary file will have a lower priority than the first file, and
+	 * the parent directory of this node will be regarded as firstFile.getParent()
+	 *
+	 * @param f
+	 * @throws IOException
+	 */
+	public void addSecondarySource(File f) throws IOException {
+		secondaryFile = f;
+	}
+
+
+	/**
+	 * Clear all rules in this node.
+	 */
+	public void empty() {
+		rules.clear();
+	}
+
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/ignore/SimpleIgnoreCache.java
@@ -0,0 +1,303 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jgit.ignore;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * A simple ignore cache. Stores ignore information on .gitignore and exclude files.
+ *
+ * @author Charley Wang
+ * Inspiration from: Ferry Huberts
+ *
+ */
+public class SimpleIgnoreCache {
+
+	//Indexed by the base directory
+	private HashMap<String, IgnoreNode> ignoreMap;
+
+	//The base directory of the repository we are caching for.
+	private String basePath;
+
+	/**
+	 * Creates a base implementation of an ignore cache. This default implementation
+	 * will search for all .gitignore files in all children of the base directory,
+	 * and grab the exclude file from baseDir/.git/info/exclude.
+	 * <br>
+	 * To fully initialize, call {@link #fullyInitialize()}. Otherwise call
+	 * {@link #partiallyInitialize(File)} to fetch only the ignore information relevant
+	 * to the target file.
+	 *
+	 *
+	 * @param baseDir
+	 */
+	public SimpleIgnoreCache(File baseDir) {
+		ignoreMap = new HashMap<String, IgnoreNode>();
+		this.basePath = baseDir.getAbsolutePath();
+	}
+
+
+	/**
+	 * Load the ignore nodes, starting from the specified root directory.
+	 * <br>
+	 * This will replace all nodes in the ignore map.
+	 *
+	 * @return True if successful, false otherwise. If false, the cache
+	 * will not be cleared
+	 *
+	 * @throws IOException
+	 */
+	public boolean fullyInitialize() throws IOException {
+		File baseDir = new File(basePath);
+		if (!baseDir.exists() || !baseDir.isDirectory())
+			return false;
+		ignoreMap.clear();
+
+		//Collect rules for base (.gitignore and exclude)
+		readRulesAtBase();
+
+		//Collect .gitignore files for all children of baseDir
+		for (String member : baseDir.list()) {
+			File file = new File(member);
+			if (file.isDirectory()) {
+				readIgnoreRules (file);
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Initializes the ignore map for the target file and all parents.
+	 * This will delete existing ignore information for all folders
+	 * on the partial initialization path.
+	 * <br>
+	 * Note that this does not intialize the ignore rules. Ignore rules will
+	 * automatically be parsed during a call to {@link #isIgnored(File)}
+	 *
+	 * @param targetFile
+	 * @throws IOException
+	 *
+	 */
+	public void partiallyInitialize(File targetFile) throws IOException {
+		//TODO: Optimize to only parse files that have changed, or use listeners
+		File temp = targetFile.getParentFile();
+
+		while (temp != null && !temp.getAbsolutePath().equals(basePath)) {
+			if (temp.isDirectory()) {
+				addIgnoreNode(temp.getAbsolutePath());
+			}
+			temp = temp.getParentFile();
+		}
+
+		if (temp != null && temp.getAbsolutePath().equals(basePath)) {
+			readRulesAtBase();
+		}
+
+	}
+
+
+	/**
+	 * Creates rules for .git/info/exclude and .gitignore to the base node.
+	 * It will overwrite the existing base ignore node. There will always
+	 * be a base ignore node, even if there is no .gitignore file
+	 *
+	 * @return True if successful, false otherwise
+	 * @throws IOException
+	 *
+	 */
+	private boolean readRulesAtBase() throws IOException {
+		//Add .gitignore rules
+		String path = basePath + "/.gitignore";
+		File f = new File(path);
+		IgnoreNode n = new IgnoreNode(f.getParentFile());
+
+
+		//Add exclude rules
+		path = basePath + "/.git/info/exclude";
+		f = new File(path);
+		if (f.canRead()) {
+			n.addSecondarySource(f);
+		}
+
+		ignoreMap.put(basePath, n);
+		return true;
+	}
+
+
+	/**
+	 * Reads ignore rules from .gitignore file in the specified directory
+ 	 * Not recommended to use this on the base directory.
+	 * Please see {@link #readRulesAtBase()}
+	 *
+	 * @param container Should be a directory.
+	 */
+	private void readIgnoreRules(File container) {
+		addIgnoreNode(container.getAbsolutePath());
+		for (String member : container.list()) {
+			File file = new File(member);
+			if (file.isDirectory()) {
+				readIgnoreRules (file);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Maps the directory to an IgnoreNode, but does not initialize
+	 * the IgnoreNode. If a node already exists it will be emptied.
+	 * <br>
+	 * To initialize the IgnoreNode, see {@link #isIgnored(File)}
+	 *
+	 *
+	 * @param directory -- directory to load rules from
+	 * @return true if set successfully, false if directory does not exist
+	 * or if directory does not contain a .gitignore file.
+	 */
+	protected boolean addIgnoreNode(String directory) {
+		File dir = new File(directory);
+		if (!dir.exists()) {
+			return false;
+		}
+
+
+		File ignoreFile = new File(directory + "/.gitignore");
+		if (!ignoreFile.exists()) {
+			return false;
+		}
+
+		if (ignoreMap.get(directory) != null) {
+			ignoreMap.get(directory).empty();
+			return true;
+		}
+
+		IgnoreNode n = new IgnoreNode(dir);
+		ignoreMap.put(directory, n);
+		return true;
+	}
+
+	/**
+	 * Returns the ignored status of the file based on the current state
+	 * of the ignore nodes. Empty ignore nodes will be filled, but otherwise
+	 * ignore nodes will not be updated and new ignore nodes will not be
+	 * created.
+	 * <br>
+	 * Traverses from highest to lowest priority and quits as soon as a match
+	 * is made.
+	 * <br>
+	 * If no information is available, the file is assumed to be not ignored.
+	 *
+	 * @param file -- the file to check
+	 * @return true if the file is ignored
+	 */
+	public boolean isIgnored(File file) {
+		String currentPriority = file.getAbsolutePath();
+
+		//File is not in repository
+		if (!currentPriority.startsWith(basePath))
+			return false;
+
+		boolean ignored = false;
+		while (currentPriority.length() > 1 && !currentPriority.equals(basePath)) {
+			currentPriority = getParent(currentPriority);
+			IgnoreNode n = ignoreMap.get(currentPriority);
+			if (n != null) {
+				try {
+					ignored = n.isIgnored(file);
+				} catch (IOException e) {
+					//TODO: Suggested error action?
+					e.printStackTrace();
+				}
+				if (n.wasMatched())
+					return ignored;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * String manipulation to get the parent directory of the given path.
+	 * It may be more efficient to make a file and call <code>File.getParent()</code>
+	 *
+	 * @param filePath
+	 * @return Parent of the filePath.
+	 */
+	private String getParent(String filePath) {
+		int lastSlash = filePath.lastIndexOf("/");
+		if (filePath.length() > 0 && lastSlash != -1)
+			return filePath.substring(0, lastSlash);
+		return "";
+	}
+
+
+	/**
+	 * @param base
+	 * @return Ignore rules for given base directory
+	 */
+	public IgnoreNode getRules(String base) {
+		return ignoreMap.get(base);
+	}
+
+
+	/**
+	 * @return True if there are no Ignore Rules in this cache
+	 */
+	public boolean isEmpty() {
+		return ignoreMap.isEmpty();
+	}
+
+	/**
+	 * Clears the cache
+	 */
+	public void clear() {
+		ignoreMap.clear();
+		basePath = "";
+	}
+}
