046d1a2ef6ddea2a9521ac49ae339e05c1cc0acd
Alex Blewitt <alex.blewitt@gmail.com>
2010-06-04 00:38:50 +0100

    Provide a public entry method to determine whether a URI protocol is supported

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/Transport.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/Transport.java
@@ -319,6 +319,38 @@ public abstract class Transport {
 	}
 
 	/**
+	 * Determines whether the transport can handle the given URIish.
+	 *
+	 * @param remote
+	 *            location of the remote repository.
+	 * @return true if the protocol is supported.
+	 */
+	public static boolean canHandleProtocol(final URIish remote) {
+		if (TransportGitSsh.canHandle(remote))
+			return true;
+
+		else if (TransportHttp.canHandle(remote))
+			return true;
+
+		else if (TransportSftp.canHandle(remote))
+			return true;
+
+		else if (TransportGitAnon.canHandle(remote))
+			return true;
+
+		else if (TransportAmazonS3.canHandle(remote))
+			return true;
+
+		else if (TransportBundleFile.canHandle(remote))
+			return true;
+
+		else if (TransportLocal.canHandle(remote))
+			return true;
+
+		return false;
+	}
+
+	/**
 	 * Open a new transport instance to connect two repositories.
 	 *
 	 * @param local
