f0a37452b0b999ad3417ae7beaa36c76a0d81c3e
Shawn O. Pearce <spearce@spearce.org>
2010-06-14 15:48:53 -0700

    Refactor object writing responsiblities to ObjectDatabase
    The ObjectInserter API permits ObjectDatabase implementations to
    control their own object insertion behavior, rather than forcing
    it to always be a new loose file created in the local filesystem.
    Inserted objects can also be queued and written asynchronously to
    the main application, such as by appending into a pack file that
    is later closed and added to the repository.
    
    This change also starts to open the door to non-file based object
    storage, such as an in-memory HashMap for unit testing, or a more
    complex system built on top of a distributed hash table.
    
    To help existing application code port to the newer interface we
    are keeping ObjectWriter as a delegation wrapper to the new API.
    Each ObjectWriter instances holds a reference to an ObjectInserter
    for the Repository's top-level ObjectDatabase, and it flushes and
    releases that instance on each object processed.
    
    Change-Id: I413224fb95563e7330c82748deb0aada4e0d6ace
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -277,12 +277,17 @@ public class Repository {
 		}
 
 		refs = new RefDirectory(this);
-		if (objectDir != null)
-			objectDatabase = new ObjectDirectory(fs.resolve(objectDir, ""),
-					alternateObjectDir, fs);
-		else
-			objectDatabase = new ObjectDirectory(fs.resolve(gitDir, "objects"),
-					alternateObjectDir, fs);
+		if (objectDir != null) {
+			objectDatabase = new ObjectDirectory(config, //
+					fs.resolve(objectDir, ""), //
+					alternateObjectDir, //
+					fs);
+		} else {
+			objectDatabase = new ObjectDirectory(config, //
+					fs.resolve(gitDir, "objects"), //
+					alternateObjectDir, //
+					fs);
+		}
 
 		if (indexFile != null)
 			this.indexFile = indexFile;
@@ -394,6 +399,11 @@ public class Repository {
 	 */
 	public ObjectDatabase getObjectDatabase() {
 		return objectDatabase;
+	}
+
+	/** @return a new inserter to create objects in {@link #getObjectDatabase()} */
+	public ObjectInserter newObjectInserter() {
+		return getObjectDatabase().newInserter();
 	}
 
 	/** @return the reference database which stores the reference namespace. */
