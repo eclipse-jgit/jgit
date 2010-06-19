315e8d3e4d456b7e45f0739087dbe1f597aca9d8
Shawn O. Pearce <spearce@spearce.org>
2010-06-18 20:23:13 -0700

    Refactor alternate object databases below ObjectDirectory
    Not every object storage system will have the concept of alternate
    object databases to search, and even if they do, they may not have the
    notion of fast-access / slow-access split like we do within the
    ObjectDirectory code for pack files and loose objects.
    
    Push all of that down below the generic API so that it is a hidden
    detail of the ObjectDirectory and its related supporting classes.
    
    Change-Id: I54bc1ca5ff2ac94dfffad1f9a9dad7af202b9523
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/FileRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/FileRepository.java
@@ -61,6 +61,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.FileObjectDatabase.AlternateHandle;
+import org.eclipse.jgit.lib.FileObjectDatabase.AlternateRepository;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.RawParseUtils;
@@ -432,11 +434,11 @@ public class FileRepository extends Repository {
 	@Override
 	public Set<ObjectId> getAdditionalHaves() {
 		HashSet<ObjectId> r = new HashSet<ObjectId>();
-		for (ObjectDatabase d : objectDatabase.getAlternates()) {
-			if (d instanceof AlternateRepositoryDatabase) {
+		for (AlternateHandle d : objectDatabase. myAlternates()) {
+			if (d instanceof AlternateRepository) {
 				Repository repo;
 
-				repo = ((AlternateRepositoryDatabase) d).getRepository();
+				repo = ((AlternateRepository) d).repository;
 				for (Ref ref : repo.getAllRefs().values())
 					r.add(ref.getObjectId());
 				r.addAll(repo.getAdditionalHaves());
