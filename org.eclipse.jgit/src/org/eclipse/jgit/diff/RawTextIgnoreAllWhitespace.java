ae3a3590cc96b6b931f06a3222851b6f4d4ec98e
Jeff Schumacher <jeffschu@google.com>
2010-06-24 13:39:50 -0700

    Added support for whitespace ignoring
    JGit did not have support for skipping whitespace when comparing
    lines in RawText objects. I added a subclass of RawText that skips
    whitespace in its equals and hashCode methods. I used a subclass
    rather than adding functionality into RawText so that performance
    would not be impacted by extra logic.
    
    This class only supports ignoring all whitespace. Others will follow
    that allow other forms of whitespace ignoring.
    
    Change-Id: Ic2f79e85215e48d3fd53ec1b4ad13373dd183a4a

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/RawTextIgnoreAllWhitespace.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2009-2010, Google Inc.
+ * Copyright (C) 2008-2009, Johannes E. Schindelin <johannes.schindelin@gmx.de>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.diff;
+
+import static org.eclipse.jgit.util.RawCharUtil.isWhitespace;
+
+/**
+ * A version of {@link RawText} that ignores all whitespace.
+ */
+public class RawTextIgnoreAllWhitespace extends RawText {
+
+	/**
+	 * Create a new sequence from an existing content byte array.
+	 * <p>
+	 * The entire array (indexes 0 through length-1) is used as the content.
+	 *
+	 * @param input
+	 *            the content array. The array is never modified, so passing
+	 *            through cached arrays is safe.
+	 */
+	public RawTextIgnoreAllWhitespace(byte[] input) {
+		super(input);
+	}
+
+	@Override
+	public boolean equals(final int i, final Sequence other, final int j) {
+		return equals(this, i + 1, (RawText) other, j + 1);
+	}
+
+	private static boolean equals(final RawText a, final int ai,
+			final RawText b, final int bi) {
+		if (a.hashes.get(ai) != b.hashes.get(bi))
+			return false;
+
+		int as = a.lines.get(ai);
+		int bs = b.lines.get(bi);
+		int ae = a.lines.get(ai + 1);
+		int be = b.lines.get(bi + 1);
+
+		as = trimLeadingWhitespace(a.content, as, ae);
+		bs = trimLeadingWhitespace(b.content, bs, be);
+		ae = trimTrailingWhitespace(a.content, as, ae);
+		be = trimTrailingWhitespace(b.content, bs, be);
+
+		while (as < ae && bs < be) {
+			byte ac = a.content[as];
+			byte bc = b.content[bs];
+
+			while (as < ae - 1 && isWhitespace(ac)) {
+				as++;
+				ac = a.content[as];
+			}
+
+			while (bs < be - 1 && isWhitespace(bc)) {
+				bs++;
+				bc = b.content[bs];
+			}
+
+			if (ac != bc)
+				return false;
+
+			as++;
+			bs++;
+		}
+
+		return true;
+	}
+
+	private static int trimTrailingWhitespace(byte[] raw, int start, int end) {
+		while (end > start && isWhitespace(raw[end - 1]))
+			end--;
+
+		return end;
+	}
+
+	private static int trimLeadingWhitespace(byte[] raw, int start, int end) {
+		while (start < end && isWhitespace(raw[start]))
+			start++;
+
+		return start;
+	}
+
+	@Override
+	protected int hashLine(final byte[] raw, int ptr, final int end) {
+		int hash = 5381;
+		for (; ptr < end; ptr++) {
+			byte c = raw[ptr];
+			if (!isWhitespace(c))
+				hash = (hash << 5) ^ (c & 0xff);
+		}
+		return hash;
+	}
+}
