ee25e70ac0dc16582f122700d3d68f1923cfe6a1
Jeff Schumacher <jeffschu@google.com>
2010-06-24 13:39:50 -0700

    Added support for whitespace ignoring
    JGit did not have support for skipping whitespace when comparing
    lines in RawText objects. I added a subclass of RawText that skips
    whitespace in its equals and hashCode methods. I used a subclass
    rather than adding functionality into RawText so that performance
    would not be impacted by extra logic.
    
    This class only supports ignoring all whitespace. Others will follow
    that allow other forms of whitespace ignoring.

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/RawTextIgnoreAllWhitespaceTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2009-2010, Google Inc.
+ * Copyright (C) 2009, Johannes E. Schindelin <johannes.schindelin@gmx.de>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.diff;
+
+import org.eclipse.jgit.lib.Constants;
+
+import junit.framework.TestCase;
+
+public class RawTextIgnoreAllWhitespaceTest extends TestCase {
+	public void testEqualsWithoutWhitespace() {
+		final RawText a = new RawTextIgnoreAllWhitespace(Constants
+				.encodeASCII("foo-a\nfoo-b\n"));
+		final RawText b = new RawTextIgnoreAllWhitespace(Constants
+				.encodeASCII("foo-b\nfoo-c\n"));
+
+		assertEquals(2, a.size());
+		assertEquals(2, b.size());
+
+		// foo-a != foo-b
+		assertFalse(a.equals(0, b, 0));
+		assertFalse(b.equals(0, a, 0));
+
+		// foo-b == foo-b
+		assertTrue(a.equals(1, b, 0));
+		assertTrue(b.equals(0, a, 1));
+	}
+
+	public void testEqualsWithWhitespace() {
+		final RawText a = new RawTextIgnoreAllWhitespace(Constants
+				.encodeASCII("foo-a\n         \n a b c\na      "));
+		final RawText b = new RawTextIgnoreAllWhitespace(Constants
+				.encodeASCII("foo-a        b\n\nab  c\na"));
+
+		// "foo-a" != "foo-a        b"
+		assertFalse(a.equals(0, b, 0));
+		assertFalse(b.equals(0, a, 0));
+
+		// "         " == ""
+		assertTrue(a.equals(1, b, 1));
+		assertTrue(b.equals(1, a, 1));
+
+		// " a b c" == "ab  c"
+		assertTrue(a.equals(2, b, 2));
+		assertTrue(b.equals(2, a, 2));
+
+		// "a      " == "a"
+		assertTrue(a.equals(3, b, 3));
+		assertTrue(b.equals(3, a, 3));
+	}
+}
