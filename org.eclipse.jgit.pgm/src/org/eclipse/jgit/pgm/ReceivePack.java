2370ad9514587a354e3a8767d7b10e83587ab9cf
Shawn O. Pearce <spearce@spearce.org>
2010-06-24 11:39:20 -0700

    Use FileKey to resolve Git repository arguments
    upload-pack and receive-pack take a git repository as an argument,
    but its a lenient path format.  Locate the repository and open it.
    
    Change-Id: I4b377e57b28ba3b1717c13d9ab51a602de1ad257
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/ReceivePack.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/ReceivePack.java
@@ -47,9 +47,10 @@ package org.eclipse.jgit.pgm;
 import java.io.File;
 import java.text.MessageFormat;
 
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.RepositoryCache.FileKey;
+import org.eclipse.jgit.util.FS;
 import org.kohsuke.args4j.Argument;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
 
 @Command(common = false, usage = "usage_ServerSideBackendForJgitPush")
 class ReceivePack extends TextBuiltin {
@@ -65,11 +66,14 @@ class ReceivePack extends TextBuiltin {
 	protected void run() throws Exception {
 		final org.eclipse.jgit.transport.ReceivePack rp;
 
-		if (new File(dstGitdir, Constants.DOT_GIT).isDirectory())
-			dstGitdir = new File(dstGitdir, Constants.DOT_GIT);
-		db = new Repository(dstGitdir);
-		if (!db.getObjectsDirectory().isDirectory())
-			throw die(MessageFormat.format(CLIText.get().notAGitRepository, dstGitdir.getPath()));
+		try {
+			FileKey key = FileKey.lenient(dstGitdir, FS.DETECTED);
+			db = key.open(true /* must exist */);
+		} catch (RepositoryNotFoundException notFound) {
+			throw die(MessageFormat.format(CLIText.get().notAGitRepository,
+					dstGitdir.getPath()));
+		}
+
 		rp = new org.eclipse.jgit.transport.ReceivePack(db);
 		rp.receive(System.in, System.out, System.err);
 	}
