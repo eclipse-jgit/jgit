0e49dc80f95c71e4841610b5464e271a3f29fffd
Jeff Schumacher <jeffschu@google.com>
2010-07-02 17:29:09 -0700

    Added content based rename detection
    I had added SHA1 based rename detection in a prior commit. This class
    builds on that work by adding content based rename detection, along
    the lines of what C Git does. This content based detection works by
    hashing portions of each file (every 60 bytes or until a newline)
    and populating a hash table of content hash -> number of occurences.
    It then walks through the hash tables it created for the files to
    determine commonality. Finally, it determines the files most likely
    to be renames based on their commonality.
    
    Change-Id: I11dff969e8a2e4cf252636d857d2113053bdd9dc

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/RenameDetectorTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/RenameDetectorTest.java
@@ -50,6 +50,7 @@ import org.eclipse.jgit.junit.TestRepository;
 import org.eclipse.jgit.lib.AbbreviatedObjectId;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryTestCase;
 
 public class RenameDetectorTest extends RepositoryTestCase {
@@ -58,11 +59,27 @@ public class RenameDetectorTest extends RepositoryTestCase {
 
 	TestRepository testDb;
 
+	private static class RenameDetectorForTesting extends RenameDetector {
+		public RenameDetectorForTesting(Repository repo) {
+			super(repo);
+		}
+
+		@Override
+		int hashLine(byte[] raw, int ptr, int end) {
+			if (ptr == end)
+				return 0;
+			if (end - ptr == 1)
+				return raw[ptr];
+
+			return (raw[ptr] + raw[ptr + 1] * HASH_TABLE_SIZE) * 2;
+		}
+	}
+
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 		testDb = new TestRepository(db);
-		rd = new RenameDetector();
+		rd = new RenameDetector(db);
 	}
 
 	public void testGetEntriesAddDelete() throws Exception {
@@ -201,4 +218,231 @@ public class RenameDetectorTest extends RepositoryTestCase {
 		assertEquals("some/other_file.c", somefile.oldName);
 	}
 
+	public void testGetEntriesHeuristicRenames() throws Exception {
+		ObjectId aId = testDb.blob("foo\nbar\nbaz\n").copy();
+		ObjectId bId = testDb.blob("foo\nbar\nblah\n").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		rd.addDiffEntry(a);
+		rd.addDiffEntry(b);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(1, entries.size());
+
+		DiffEntry somefile = entries.get(0);
+		assertNotNull(somefile);
+		assertTrue(aId.equals(somefile.newId.toObjectId()));
+		assertTrue(bId.equals(somefile.oldId.toObjectId()));
+		assertEquals(FileMode.REGULAR_FILE, somefile.newMode);
+		assertEquals(FileMode.REGULAR_FILE, somefile.oldMode);
+		assertEquals(ChangeType.RENAME, somefile.changeType);
+		assertEquals("some/file.c", somefile.newName);
+		assertEquals("some/other_file.c", somefile.oldName);
+		assertEquals(67, somefile.score); // changed 1/3 of the file
+	}
+
+	public void testGetEntriesMultipleHeuristicRenames() throws Exception {
+		ObjectId aId = testDb.blob("foo\nbar\nbaz\n").copy();
+		ObjectId bId = testDb.blob("foo\nbar\nblah\n").copy();
+		ObjectId cId = testDb.blob("some\nsort\nof\ntext\n").copy();
+		ObjectId dId = testDb.blob("completely\nunrelated\ntext\n").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		DiffEntry c = new DiffEntry();
+		c.newId = AbbreviatedObjectId.fromObjectId(cId);
+		c.newMode = FileMode.REGULAR_FILE;
+		c.newName = "c";
+		c.changeType = ChangeType.ADD;
+
+		DiffEntry d = new DiffEntry();
+		d.oldId = AbbreviatedObjectId.fromObjectId(dId);
+		d.oldMode = FileMode.REGULAR_FILE;
+		d.oldName = "d";
+		d.changeType = ChangeType.DELETE;
+
+		rd.addDiffEntry(a);
+		rd.addDiffEntry(b);
+		rd.addDiffEntry(c);
+		rd.addDiffEntry(d);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(3, entries.size());
+
+		assertEquals(c, entries.get(0));
+
+		assertEquals(d, entries.get(1));
+
+		DiffEntry somefile = entries.get(2);
+		assertNotNull(somefile);
+		assertTrue(aId.equals(somefile.newId.toObjectId()));
+		assertTrue(bId.equals(somefile.oldId.toObjectId()));
+		assertEquals(FileMode.REGULAR_FILE, somefile.newMode);
+		assertEquals(FileMode.REGULAR_FILE, somefile.oldMode);
+		assertEquals(ChangeType.RENAME, somefile.changeType);
+		assertEquals("some/file.c", somefile.newName);
+		assertEquals("some/other_file.c", somefile.oldName);
+		assertEquals(67, somefile.score); // changed 1/3 of the file
+	}
+
+	public void testGetEntriesEmptyFile() throws Exception {
+		ObjectId aId = testDb.blob("").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		rd.addDiffEntry(a);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(1, entries.size());
+
+		assertEquals(a, entries.get(0));
+	}
+
+	public void testGetEntriesEmptyFile2() throws Exception {
+		ObjectId aId = testDb.blob("").copy();
+		ObjectId bId = testDb.blob("blah").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		rd.addDiffEntry(a);
+		rd.addDiffEntry(b);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(2, entries.size());
+
+		assertEquals(a, entries.get(0));
+		assertEquals(b, entries.get(1));
+	}
+
+	public void testGetEntriesHeuristicRenamesLastByteDifferent()
+			throws Exception {
+		ObjectId aId = testDb.blob("foo\nbar\na").copy();
+		ObjectId bId = testDb.blob("foo\nbar\nb").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		rd.addDiffEntry(a);
+		rd.addDiffEntry(b);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(1, entries.size());
+
+		DiffEntry somefile = entries.get(0);
+		assertNotNull(somefile);
+		assertTrue(aId.equals(somefile.newId.toObjectId()));
+		assertTrue(bId.equals(somefile.oldId.toObjectId()));
+		assertEquals(FileMode.REGULAR_FILE, somefile.newMode);
+		assertEquals(FileMode.REGULAR_FILE, somefile.oldMode);
+		assertEquals(ChangeType.RENAME, somefile.changeType);
+		assertEquals("some/file.c", somefile.newName);
+		assertEquals("some/other_file.c", somefile.oldName);
+		assertEquals(67, somefile.score); // changed 1/3 of the file
+	}
+
+	public void testGetEntriesSingleByteFiles() throws Exception {
+		ObjectId aId = testDb.blob("a").copy();
+		ObjectId bId = testDb.blob("b").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		rd.addDiffEntry(a);
+		rd.addDiffEntry(b);
+
+		List<DiffEntry> entries = rd.getEntries();
+		assertEquals(2, entries.size());
+
+		assertEquals(a, entries.get(0));
+		assertEquals(b, entries.get(1));
+	}
+
+	public void testGetEntriesSameBucket() throws Exception {
+		RenameDetectorForTesting testRd = new RenameDetectorForTesting(db);
+
+		ObjectId aId = testDb.blob("ab\nab\nab\nac\nad\nae\n").copy();
+		ObjectId bId = testDb.blob("ac\nab\nab\nab\naa\na0\na1\n").copy();
+
+		DiffEntry a = new DiffEntry();
+		a.newId = AbbreviatedObjectId.fromObjectId(aId);
+		a.newMode = FileMode.REGULAR_FILE;
+		a.newName = "some/file.c";
+		a.changeType = ChangeType.ADD;
+
+		DiffEntry b = new DiffEntry();
+		b.oldId = AbbreviatedObjectId.fromObjectId(bId);
+		b.oldMode = FileMode.REGULAR_FILE;
+		b.oldName = "some/other_file.c";
+		b.changeType = ChangeType.DELETE;
+
+		testRd.addDiffEntry(a);
+		testRd.addDiffEntry(b);
+
+		List<DiffEntry> entries = testRd.getEntries();
+		assertEquals(1, entries.size());
+
+		DiffEntry somefile = entries.get(0);
+		assertNotNull(somefile);
+		assertTrue(aId.equals(somefile.newId.toObjectId()));
+		assertTrue(bId.equals(somefile.oldId.toObjectId()));
+		assertEquals(FileMode.REGULAR_FILE, somefile.newMode);
+		assertEquals(FileMode.REGULAR_FILE, somefile.oldMode);
+		assertEquals(ChangeType.RENAME, somefile.changeType);
+		assertEquals("some/file.c", somefile.newName);
+		assertEquals("some/other_file.c", somefile.oldName);
+		assertEquals(62, somefile.score);
+	}
 }
--- org.eclipse.jgit/src/org/eclipse/jgit/diff/RenameDetector.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/RenameDetector.java
@@ -59,16 +59,23 @@ import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.AbbreviatedObjectId;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.MutableObjectId;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.TreeWalk;
 
 /** Detect and resolve object renames. */
 public class RenameDetector {
 
+	/**
+	 * The size of the hash table to build in the heuristic rename detection
+	 */
+	static final int HASH_TABLE_SIZE = 65537; // 2^16+1, which is prime
+
 	private static final int EXACT_RENAME_SCORE = 100;
 
 	private static final Comparator<DiffEntry> DIFF_COMPARATOR = new Comparator<DiffEntry>() {
 		public int compare(DiffEntry o1, DiffEntry o2) {
-			return o1.newName.compareTo(o2.newName);
+			return o1.newName.compareTo((o2.newName == null ? o2.oldName
+					: o2.newName));
 		}
 	};
 
@@ -79,6 +86,32 @@ public class RenameDetector {
 	private List<DiffEntry> added = new ArrayList<DiffEntry>();
 
 	private boolean done = false;
+
+	private final Repository repo;
+
+	private static final class HashCount {
+		int hash;
+
+		int count;
+
+		HashCount next;
+
+		HashCount(int hash, int count, HashCount next) {
+			this.hash = hash;
+			this.count = count;
+			this.next = next;
+		}
+	}
+
+	/**
+	 * Create a new rename detector for the given repository
+	 *
+	 * @param repo
+	 *            the repository to use for rename detection
+	 */
+	public RenameDetector(Repository repo) {
+		this.repo = repo;
+	}
 
 	/**
 	 * Walk through a given tree walk with exactly two trees and add all
@@ -178,6 +211,7 @@ public class RenameDetector {
 		if (!done) {
 			done = true;
 			findExactRenames();
+			findContentRenames();
 			entries.addAll(added);
 			entries.addAll(deleted);
 			added = null;
@@ -185,6 +219,187 @@ public class RenameDetector {
 			Collections.sort(entries, DIFF_COMPARATOR);
 		}
 		return Collections.unmodifiableList(entries);
+	}
+
+	private void findContentRenames() throws IOException {
+
+		if (added.isEmpty() || deleted.isEmpty())
+			return;
+
+		boolean[] paired = new boolean[deleted.size()];
+		int[][] scores = calculateScores();
+
+		class MaxLocation {
+			int max = 0;
+			int location;
+		}
+
+		MaxLocation[] rowMax = new MaxLocation[added.size()];
+		MaxLocation[] colMax = new MaxLocation[deleted.size()];
+
+		for (int r = 0; r < added.size(); r++) {
+			MaxLocation rml = new MaxLocation();
+			rowMax[r] = rml;
+			for (int c = 0; c < deleted.size(); c++) {
+				MaxLocation cml = colMax[c];
+				if (cml == null) {
+					cml = new MaxLocation();
+					colMax[c] = cml;
+				}
+				if (scores[r][c] > rml.max) {
+					rml.max = scores[r][c];
+					rml.location = c;
+				}
+				if (scores[r][c] > colMax[c].max) {
+					colMax[c].max = scores[r][c];
+					colMax[c].location = r;
+				}
+			}
+		}
+
+		ArrayList<DiffEntry> tempAdded = new ArrayList<DiffEntry>(added.size());
+		for (int r = 0; r < added.size(); r++) {
+			int c = rowMax[r].location;
+			// Check if we have a column max and a row max in the same cell
+			if (colMax[c].location == r && scores[r][c] > 50) {
+				entries.add(resolveRename(added.get(r), deleted.get(c),
+						scores[r][c]));
+				paired[c] = true;
+			} else {
+				tempAdded.add(added.get(r));
+			}
+		}
+		added = tempAdded;
+
+		ArrayList<DiffEntry> tempDeleted = new ArrayList<DiffEntry>(deleted
+				.size());
+		for (int i = 0; i < deleted.size(); i++)
+			if (!paired[i])
+				tempDeleted.add(deleted.get(i));
+
+		deleted = tempDeleted;
+	}
+
+	private int[][] calculateScores() throws IOException {
+		int[][] scores = new int[added.size()][deleted.size()];
+		for (int r = 0; r < added.size(); r++) {
+			for (int c = 0; c < deleted.size(); c++) {
+				scores[r][c] = score(added.get(r).newId, deleted.get(c).oldId);
+			}
+		}
+		return scores;
+	}
+
+	private int score(AbbreviatedObjectId a, AbbreviatedObjectId b)
+			throws IOException {
+		byte[] ac = repo.openBlob(a.toObjectId()).getBytes();
+		byte[] bc = repo.openBlob(b.toObjectId()).getBytes();
+
+		HashCount[] aCounts = calcHashCounts(ac);
+		HashCount[] bCounts = calcHashCounts(bc);
+
+		int aDiff = 0; // blocks that a has that b does not
+		int bDiff = 0; // blocks that b has that a does not
+		int same = 0; // block that are common
+
+		for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+			HashCount aptr = aCounts[i];
+			HashCount bptr = bCounts[i];
+
+			while (aptr != null && bptr != null) {
+				if (aptr.hash < bptr.hash) {
+					// a has something b does not
+					aDiff += aptr.count;
+					aptr = aptr.next;
+				} else if (aptr.hash > bptr.hash) {
+					// b has something a does not
+					bDiff += bptr.count;
+					bptr = bptr.next;
+				} else {
+					// a and b both have the same thing, but possibly a
+					// different number of them
+					if (aptr.count > bptr.count) {
+						aDiff += aptr.count - bptr.count;
+					} else {
+						bDiff += bptr.count - aptr.count;
+					}
+
+					// add the common blocks to same, which is the minimum of
+					// both counts
+					same += (aptr.count < bptr.count ? aptr.count : bptr.count);
+					aptr = aptr.next;
+					bptr = bptr.next;
+				}
+			}
+
+			// at this point, either a or b might have more things left
+			while (aptr != null) {
+				aDiff += aptr.count;
+				aptr = aptr.next;
+			}
+
+			while (bptr != null) {
+				bDiff += bptr.count;
+				bptr = bptr.next;
+			}
+		}
+
+		return Math
+				.round((same * 2) / ((float) same * 2 + aDiff + bDiff) * 100);
+	}
+
+	private HashCount[] calcHashCounts(byte[] bytes) {
+		int s = 0;
+		int e = 0;
+		HashCount[] table = new HashCount[HASH_TABLE_SIZE];
+
+		while (e < bytes.length) {
+			if (bytes[e] == '\n' || e - s == 60 || e == bytes.length - 1) {
+				int hash = hashLine(bytes, s, e + 1);
+				int tableIndex = (hash >>> 1) % HASH_TABLE_SIZE;
+
+				HashCount tmp = table[tableIndex];
+
+				if (tmp == null) {
+					HashCount hc = new HashCount(hash, 1, null);
+					table[tableIndex] = hc;
+				} else if (hash < tmp.hash) {
+					HashCount hc = new HashCount(hash, 1, tmp);
+					table[tableIndex] = hc;
+				} else if (hash > tmp.hash) {
+					while (tmp.hash != hash && tmp.next != null
+							&& hash >= tmp.next.hash) {
+						tmp = tmp.next;
+					}
+					if (tmp.hash == hash) {
+						tmp.count++;
+					} else if (tmp.next == null) {
+						HashCount hc = new HashCount(hash, 1, null);
+						tmp.next = hc;
+					} else {
+						// tmp.hash < hash < tmp.next.hash
+						// Must insert between tmp and tmp.next
+						HashCount hc = new HashCount(hash, 1, tmp.next);
+						tmp.next = hc;
+					}
+				} else {
+					tmp.count++;
+				}
+
+				s = e + 1;
+			}
+
+			e++;
+		}
+
+		return table;
+	}
+
+	int hashLine(final byte[] raw, int ptr, final int end) {
+		int hash = 5381;
+		for (; ptr < end; ptr++)
+			hash = (hash << 5) ^ (raw[ptr] & 0xff);
+		return hash;
 	}
 
 	@SuppressWarnings("unchecked")
