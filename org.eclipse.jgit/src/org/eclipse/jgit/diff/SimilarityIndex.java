6b102c09f2adde080249921899fd07cd61f4b8d4
Jeff Schumacher <jeffschu@google.com>
2010-07-09 12:53:57 -0700

    Fixed potential div by zero bug
    The scoring logic in SimilarityIndex was dividing by the max file
    size. If both files are empty, this would cause a div by zero
    error. This case cannot currently happen, since two empty files
    would have the same SHA1, and would therefore be caught in the
    earlier SHA1 based detection pass. Still, if this logic eventually
    gets separated from that pass, a div by zero error would occur.
    
    I changed the logic to instead consider two empty files to have a
    similarity score of 100.
    
    Change-Id: Ic08e18a066b8fef25bb5e7c62418106a8cee762a

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/SimilarityIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/SimilarityIndex.java
@@ -144,6 +144,8 @@ class SimilarityIndex {
 
 	int score(SimilarityIndex dst) {
 		long max = Math.max(fileSize, dst.fileSize);
+		if (max == 0)
+			return 100;
 		return (int) ((common(dst) * 100L) / max);
 	}
 
