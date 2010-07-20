e55162704dbb64c1303390b3f60a7c6a54c421ed
Stefan Lay <stefan.lay@sap.com>
2010-07-20 15:35:06 +0200

    Add "all" parameter to the commit Command
    When the add parameter is set all modified and deleted files
    are staged prior to commit. 
    
    Change-Id: Id23bc25730fcdd151386cd495a7cdc0935cbc00b
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/AddCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/AddCommand.java
@@ -111,6 +111,9 @@ public class AddCommand extends GitCommand<DirCache> {
 			throw new NoFilepatternException(JGitText.get().atLeastOnePatternIsRequired);
 		checkCallable();
 		DirCache dc = null;
+		boolean addAll = false;
+		if (filepatterns.contains("."))
+			addAll = true;
 
 		try {
 			dc = DirCache.lock(repo);
@@ -124,7 +127,8 @@ public class AddCommand extends GitCommand<DirCache> {
 			FileTreeIterator fileTreeIterator = new FileTreeIterator(repo);
 			tw.addTree(fileTreeIterator);
 			tw.setRecursive(true);
-			tw.setFilter(PathFilterGroup.createFromStrings(filepatterns));
+			if (!addAll)
+				tw.setFilter(PathFilterGroup.createFromStrings(filepatterns));
 
 			String lastAddedFile = null;
 
