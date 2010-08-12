d5eba2cf8a751c8952e3caccfa885416040f1ab0
Chris Aniszczyk <caniszczyk@gmail.com>
2010-08-12 11:25:31 -0500

    Add TagCommand
    A tag command is added to the Git porcelain API. Tests were
    also added to stress test the tag command.
    
    Change-Id: Iab282a918eb51b0e9c55f628a3396ff01c9eb9eb
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
@@ -203,6 +203,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String exceptionCaughtDuringExecutionOfAddCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfCommitCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfMergeCommand;
+	/***/ public String exceptionCaughtDuringExecutionOfTagCommand;
 	/***/ public String exceptionOccuredDuringAddingOfOptionToALogCommand;
 	/***/ public String exceptionOccuredDuringReadingOfGIT_DIR;
 	/***/ public String expectedACKNAKFoundEOF;
@@ -386,6 +387,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String shortReadOfBlock;
 	/***/ public String shortReadOfOptionalDIRCExtensionExpectedAnotherBytes;
 	/***/ public String shortSkipOfBlock;
+	/***/ public String signingNotSupportedOnTag;
 	/***/ public String similarityScoreMustBeWithinBounds;
 	/***/ public String sizeExceeds2GB;
 	/***/ public String smartHTTPPushDisabled;
@@ -398,6 +400,8 @@ public class JGitText extends TranslationBundle {
 	/***/ public String statelessRPCRequiresOptionToBeEnabled;
 	/***/ public String submodulesNotSupported;
 	/***/ public String symlinkCannotBeWrittenAsTheLinkTarget;
+	/***/ public String tagNameInvalid;
+	/***/ public String tagOnRepoWithoutHEADCurrentlyNotSupported;
 	/***/ public String tSizeMustBeGreaterOrEqual1;
 	/***/ public String theFactoryMustNotBeNull;
 	/***/ public String timerAlreadyTerminated;
