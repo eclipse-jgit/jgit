73cbf7ba5d1ed06f6720daecb1e0a58c2aa1a2a4
Mathias Kinzler <mathias.kinzler@sap.com>
2010-08-17 13:57:02 +0200

    Fix missing Configuration Change eventing
    Currently, configuration change events are not triggered at all.
    
    Change-Id: Ida94a59f5a2b7fa8ae0126e33c13343275483ee5
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/events/ConfigChangeEventTest.java
@@ -0,0 +1,33 @@
+package org.eclipse.jgit.events;
+
+import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+
+public class ConfigChangeEventTest extends RepositoryTestCase {
+	public void testChangeEventsOnlyOnSave() throws Exception {
+		final ConfigChangedEvent[] events = new ConfigChangedEvent[1];
+		db.getListenerList().addConfigChangedListener(
+				new ConfigChangedListener() {
+					public void onConfigChanged(ConfigChangedEvent event) {
+						events[0] = event;
+					}
+				});
+		FileBasedConfig config = db.getConfig();
+		assertNull(events[0]);
+		config.setString("test", "section", "event", "value");
+		// no changes until we save
+		assertNull(events[0]);
+		config.save();
+		assertNotNull(events[0]);
+		// correct repository?
+		assertEquals(events[0].getRepository(), db);
+		events[0] = null;
+		config.unset("test", "section", "event");
+		// no changes until we save
+		assertNull(events[0]);
+		config.save();
+		assertNotNull(events[0]);
+		// correct repository?
+		assertEquals(events[0].getRepository(), db);
+	}
+}
