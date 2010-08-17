1127a15a60aa5de29c7fdbd8d2bf814d6193a729
Mathias Kinzler <mathias.kinzler@sap.com>
2010-08-17 10:24:01 +0200

    Fix missing Configuration Change eventing
    Currently, configuration change events are not triggered at all.
    
    Change-Id: Ida94a59f5a2b7fa8ae0126e33c13343275483ee5
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
@@ -54,6 +54,9 @@ import java.util.Set;
 
 import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.events.ConfigChangedEvent;
+import org.eclipse.jgit.events.ConfigChangedListener;
+import org.eclipse.jgit.events.ListenerHandle;
 import org.eclipse.jgit.lib.BaseRepositoryBuilder;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
@@ -100,6 +103,8 @@ public class FileRepository extends Repository {
 
 	private final ObjectDirectory objectDatabase;
 
+	private final ListenerHandle configChangeListenerHandle;
+
 	/**
 	 * Construct a representation of a Git repository.
 	 * <p>
@@ -143,6 +148,15 @@ public class FileRepository extends Repository {
 		loadUserConfig();
 		loadRepoConfig();
 
+		ConfigChangedListener configChangeListener = new ConfigChangedListener() {
+			public void onConfigChanged(ConfigChangedEvent event) {
+				fireEvent(event);
+
+			}
+		};
+
+		configChangeListenerHandle = getConfig().addChangeListener(configChangeListener);
+
 		refs = new RefDirectory(this);
 		objectDatabase = new ObjectDirectory(repoConfig, //
 				options.getObjectDirectory(), //
@@ -160,6 +174,12 @@ public class FileRepository extends Repository {
 						repositoryFormatVersion));
 			}
 		}
+	}
+
+	@Override
+	protected void doClose() {
+		configChangeListenerHandle.remove();
+		super.doClose();
 	}
 
 	private void loadUserConfig() throws IOException {
