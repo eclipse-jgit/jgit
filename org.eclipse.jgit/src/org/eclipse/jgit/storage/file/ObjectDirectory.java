e29cd27961856376184edc74df700629003ad2f0
Shawn O. Pearce <spearce@spearce.org>
2010-08-31 13:13:28 -0700

    Move ObjectDirectory streaming limit to WindowCacheConfig
    IDEs like Eclipse offer up the settings in WindowCacheConfig to the
    user as a global set of options that are configured for the entire
    JVM process, not per-repository, as the cache is shared across the
    entire JVM.  The limit on how much we are willing to allocate for
    an object buffer is similar to the limit on how much we can use for
    data caches, allocating that much space impacts the entire JVM and
    not just a single repository, so it should be a global limit.
    
    Change-Id: I22eafb3e223bf8dea57ece82cd5df8bfe5badebc
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
@@ -63,13 +63,10 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.errors.PackMismatchException;
-import org.eclipse.jgit.events.ConfigChangedEvent;
-import org.eclipse.jgit.events.ConfigChangedListener;
 import org.eclipse.jgit.lib.AbbreviatedObjectId;
 import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.CoreConfig;
 import org.eclipse.jgit.lib.ObjectDatabase;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
@@ -98,8 +95,7 @@ import org.eclipse.jgit.util.FS;
  * searched (recursively through all alternates) before the slow half is
  * considered.
  */
-public class ObjectDirectory extends FileObjectDatabase implements
-		ConfigChangedListener {
+public class ObjectDirectory extends FileObjectDatabase {
 	private static final PackList NO_PACKS = new PackList(-1, -1, new PackFile[0]);
 
 	/** Maximum number of candidates offered as resolutions of abbreviation. */
@@ -120,8 +116,6 @@ public class ObjectDirectory extends FileObjectDatabase implements
 	private final FS fs;
 
 	private final AtomicReference<AlternateHandle[]> alternates;
-
-	private int streamFileThreshold;
 
 	/**
 	 * Initialize a reference to an on-disk object directory.
@@ -157,13 +151,6 @@ public class ObjectDirectory extends FileObjectDatabase implements
 				alt[i] = openAlternate(alternatePaths[i]);
 			alternates.set(alt);
 		}
-
-		onConfigChanged(new ConfigChangedEvent());
-	}
-
-	public void onConfigChanged(ConfigChangedEvent event) {
-		CoreConfig core = config.get(CoreConfig.KEY);
-		streamFileThreshold = core.getStreamFileThreshold();
 	}
 
 	/**
@@ -743,10 +730,5 @@ public class ObjectDirectory extends FileObjectDatabase implements
 
 	FileObjectDatabase newCachedFileObjectDatabase() {
 		return new CachedObjectDirectory(this);
-	}
-
-	@Override
-	int getStreamFileThreshold() {
-		return streamFileThreshold;
 	}
 }
