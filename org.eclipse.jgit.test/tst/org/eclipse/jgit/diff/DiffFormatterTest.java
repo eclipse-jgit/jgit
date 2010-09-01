df8adefe86934cbd25982527780b5eed9a1feae1
Shawn O. Pearce <spearce@spearce.org>
2010-08-31 19:28:15 -0700

    Correct diff header formatting
    When adding or deleting a file, we shouldn't ever prefix /dev/null
    with the a/ or b/ prefixes.  Doing so is a mistake and confuses a
    patch parser which handles /dev/null magically, while a/dev/null is
    a file called null in the dev directory of the project.
    
    Also when adding or deleting the "diff --git" line has the "real"
    path on both sides, so we should see the following when adding the
    file called foo:
    
      diff --git a/foo b/foo
      --- /dev/null
      +++ b/foo
    
    The --- and +++ lines do not appear in a pure rename or copy delta,
    C Git diff seems to omit these, so we now omit them as well.  We also
    omit the index line when the ObjectIds are exactly equal.
    
    Change-Id: Ic46892dea935ee8bdee29088aab96307d7ec6d3d
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/DiffFormatterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/DiffFormatterTest.java
@@ -77,6 +77,76 @@ public class DiffFormatterTest extends RepositoryTestCase {
 		df.setAbbreviationLength(8);
 	}
 
+	public void testCreateFileHeader_Add() throws Exception {
+		ObjectId adId = blob("a\nd\n");
+		DiffEntry ent = DiffEntry.add("FOO", adId);
+		FileHeader fh = df.createFileHeader(ent);
+
+		String diffHeader = "diff --git a/FOO b/FOO\n" //
+				+ "new file mode " + REGULAR_FILE + "\n"
+				+ "index "
+				+ ObjectId.zeroId().abbreviate(8).name()
+				+ ".."
+				+ adId.abbreviate(8).name() + "\n" //
+				+ "--- /dev/null\n"//
+				+ "+++ b/FOO\n";
+		assertEquals(diffHeader, RawParseUtils.decode(fh.getBuffer()));
+
+		assertEquals(0, fh.getStartOffset());
+		assertEquals(fh.getBuffer().length, fh.getEndOffset());
+		assertEquals(FileHeader.PatchType.UNIFIED, fh.getPatchType());
+
+		assertEquals(1, fh.getHunks().size());
+
+		HunkHeader hh = fh.getHunks().get(0);
+		assertEquals(1, hh.toEditList().size());
+
+		EditList el = hh.toEditList();
+		assertEquals(1, el.size());
+
+		Edit e = el.get(0);
+		assertEquals(0, e.getBeginA());
+		assertEquals(0, e.getEndA());
+		assertEquals(0, e.getBeginB());
+		assertEquals(2, e.getEndB());
+		assertEquals(Edit.Type.INSERT, e.getType());
+	}
+
+	public void testCreateFileHeader_Delete() throws Exception {
+		ObjectId adId = blob("a\nd\n");
+		DiffEntry ent = DiffEntry.delete("FOO", adId);
+		FileHeader fh = df.createFileHeader(ent);
+
+		String diffHeader = "diff --git a/FOO b/FOO\n" //
+				+ "deleted file mode " + REGULAR_FILE + "\n"
+				+ "index "
+				+ adId.abbreviate(8).name()
+				+ ".."
+				+ ObjectId.zeroId().abbreviate(8).name() + "\n" //
+				+ "--- a/FOO\n"//
+				+ "+++ /dev/null\n";
+		assertEquals(diffHeader, RawParseUtils.decode(fh.getBuffer()));
+
+		assertEquals(0, fh.getStartOffset());
+		assertEquals(fh.getBuffer().length, fh.getEndOffset());
+		assertEquals(FileHeader.PatchType.UNIFIED, fh.getPatchType());
+
+		assertEquals(1, fh.getHunks().size());
+
+		HunkHeader hh = fh.getHunks().get(0);
+		assertEquals(1, hh.toEditList().size());
+
+		EditList el = hh.toEditList();
+		assertEquals(1, el.size());
+
+		Edit e = el.get(0);
+		assertEquals(0, e.getBeginA());
+		assertEquals(2, e.getEndA());
+		assertEquals(0, e.getBeginB());
+		assertEquals(0, e.getEndB());
+		assertEquals(Edit.Type.DELETE, e.getType());
+	}
+
 	public void testCreateFileHeader_Modify() throws Exception {
 		ObjectId adId = blob("a\nd\n");
 		ObjectId abcdId = blob("a\nb\nc\nd\n");
