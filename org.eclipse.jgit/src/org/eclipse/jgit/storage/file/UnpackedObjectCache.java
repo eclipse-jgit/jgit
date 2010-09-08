a71524766807dd914bab10d3d645d99b74ba6420
Shawn O. Pearce <spearce@spearce.org>
2010-09-07 19:16:39 -0700

    Remember loose objects and fast-track their lookup
    Recently created objects are usually what branches point to, and
    are usually written out as loose objects.  But due to the high cost
    of asking the operating system if a file exists, these are the last
    thing that ObjectDirectory examines when looking for an object by
    its ObjectId.
    
    Caching recently seen loose objects permits the opening code to
    jump directly to the loose object, accelerating lookup for branch
    heads that are accessed often.
    
    To avoid exploding the cache its limited to approximately 2048
    entries.  When more ids are added, the table is simply cleared
    and reset in size.
    
    Change-Id: I18f483217412b102f754ffd496c87061d592e535
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/UnpackedObjectCache.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2010, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.storage.file;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+
+/** Remembers objects that are currently unpacked. */
+class UnpackedObjectCache {
+	private static final int PUT_LIMIT = 2048;
+
+	private final ConcurrentHashMap<ObjectId, ObjectId> ids;
+
+	UnpackedObjectCache() {
+		ids = new ConcurrentHashMap<ObjectId, ObjectId>(8, 0.75f, 1);
+	}
+
+	boolean isUnpacked(AnyObjectId objectId) {
+		return ids.containsKey(objectId);
+	}
+
+	void add(AnyObjectId objectId) {
+		ObjectId id = objectId.copy();
+		if (ids.putIfAbsent(id, id) != null) // already existed
+			return;
+
+		if (ids.size() < PUT_LIMIT)
+			return;
+
+		// Rather than do proper LRU, dump the table and restart once
+		// the put limit has been reached.
+		//
+		ids.clear();
+		ids.putIfAbsent(id, id);
+	}
+
+	void remove(AnyObjectId objectId) {
+		if (isUnpacked(objectId))
+			clear();
+	}
+
+	void clear() {
+		ids.clear();
+	}
+}
