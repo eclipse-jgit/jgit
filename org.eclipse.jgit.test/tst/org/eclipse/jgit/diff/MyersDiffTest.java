b60eefb247191b37dea3d51e367ed89a2df575a5
Shawn O. Pearce <spearce@spearce.org>
2010-09-21 09:24:12 -0700

    Define an abstract DiffAlgorithm test framework
    For certain tiny input sequences, every DiffAlgorithm should produce
    exactly the same results, as there should be no ambiguity.  Package
    these up in an abstract TestCase that algorithms can extend from in
    order to perform basic validation of their implementation.
    
    Since these tests are more complete than what we used to have for
    the MyersDiff algorithm, throw away Johannes' tests and only use
    this new package.
    
    Change-Id: I9a044330887c849ad4c78aa5c7aa04c783c10252
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/MyersDiffTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/MyersDiffTest.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2009, Johannes E. Schindelin
- * Copyright (C) 2009, Johannes Schindelin <johannes.schindelin@gmx.de>
+ * Copyright (C) 2010, Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -44,74 +43,9 @@
 
 package org.eclipse.jgit.diff;
 
-import junit.framework.TestCase;
-
-public class MyersDiffTest extends TestCase {
-	public void testAtEnd() {
-		assertDiff("HELLO", "HELL", " -4,1 +4,0");
-	}
-
-	public void testAtStart() {
-		assertDiff("Git", "JGit", " -0,0 +0,1");
-	}
-
-	public void testSimple() {
-		assertDiff("HELLO WORLD", "LOW",
-			" -0,3 +0,0 -5,1 +2,0 -7,4 +3,0");
-		// is ambiguous, could be this, too:
-		// " -0,2 +0,0 -3,1 +1,0 -5,1 +2,0 -7,4 +3,0"
-	}
-
-	public void assertDiff(String a, String b, String edits) {
-		EditList editList = MyersDiff.INSTANCE.diff(new CharCmp(),
-				toCharArray(a), toCharArray(b));
-		assertEquals(edits, toString(editList));
-	}
-
-	private static String toString(EditList list) {
-		StringBuilder builder = new StringBuilder();
-		for (Edit e : list)
-			builder.append(" -" + e.beginA
-					+ "," + (e.endA - e.beginA)
-				+ " +" + e.beginB + "," + (e.endB - e.beginB));
-		return builder.toString();
-	}
-
-	private static CharArray toCharArray(String s) {
-		return new CharArray(s);
-	}
-
-	protected static String toString(CharArray a, int begin, int end) {
-		return new String(a.array, begin, end - begin);
-	}
-
-	protected static String toString(CharArray a, CharArray b,
-			int x, int k) {
-		return "(" + x + "," + (k + x)
-			+ (x < 0 ? '<' :
-					(x >= a.array.length ?
-					 '>' : a.array[x]))
-			+ (k + x < 0 ? '<' :
-					(k + x >= b.array.length ?
-					 '>' : b.array[k + x]))
-			+ ")";
-	}
-
-	private static class CharArray extends Sequence {
-		char[] array;
-		public CharArray(String s) { array = s.toCharArray(); }
-		public int size() { return array.length; }
-	}
-
-	private static class CharCmp extends SequenceComparator<CharArray> {
-		@Override
-		public boolean equals(CharArray a, int ai, CharArray b, int bi) {
-			return a.array[ai] == b.array[bi];
-		}
-
-		@Override
-		public int hash(CharArray seq, int ptr) {
-			return seq.array[ptr];
-		}
+public class MyersDiffTest extends AbstractDiffTestCase {
+	@Override
+	protected DiffAlgorithm algorithm() {
+		return MyersDiff.INSTANCE;
 	}
 }
--- org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/PatienceDiffTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/PatienceDiffTest.java
@@ -43,102 +43,12 @@
 
 package org.eclipse.jgit.diff;
 
-import java.io.UnsupportedEncodingException;
-
-import junit.framework.TestCase;
-
-public class PatienceDiffTest extends TestCase {
-	public void testEmptyInputs() {
-		EditList r = diff(t(""), t(""));
-		assertTrue("is empty", r.isEmpty());
-	}
-
-	public void testCreateFile() {
-		EditList r = diff(t(""), t("AB"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(0, 0, 0, 2), r.get(0));
-	}
-
-	public void testDeleteFile() {
-		EditList r = diff(t("AB"), t(""));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(0, 2, 0, 0), r.get(0));
-	}
-
-	public void testDegenerate_InsertMiddle() {
-		EditList r = diff(t("ac"), t("aBc"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(1, 1, 1, 2), r.get(0));
-	}
-
-	public void testDegenerate_DeleteMiddle() {
-		EditList r = diff(t("aBc"), t("ac"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(1, 2, 1, 1), r.get(0));
-	}
-
-	public void testDegenerate_ReplaceMiddle() {
-		EditList r = diff(t("bCd"), t("bEd"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(1, 2, 1, 2), r.get(0));
-	}
-
-	public void testDegenerate_InsertsIntoMidPosition() {
-		EditList r = diff(t("aaaa"), t("aaXaa"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(2, 2, 2, 3), r.get(0));
-	}
-
-	public void testDegenerate_InsertStart() {
-		EditList r = diff(t("bc"), t("Abc"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(0, 0, 0, 1), r.get(0));
-	}
-
-	public void testDegenerate_DeleteStart() {
-		EditList r = diff(t("Abc"), t("bc"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(0, 1, 0, 0), r.get(0));
-	}
-
-	public void testDegenerate_InsertEnd() {
-		EditList r = diff(t("bc"), t("bcD"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(2, 2, 2, 3), r.get(0));
-	}
-
-	public void testDegenerate_DeleteEnd() {
-		EditList r = diff(t("bcD"), t("bc"));
-		assertEquals(1, r.size());
-		assertEquals(new Edit(2, 3, 2, 2), r.get(0));
-	}
-
-	public void testEdit_ReplaceCommonDelete() {
-		EditList r = diff(t("RbC"), t("Sb"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(0, 1, 0, 1), r.get(0));
-		assertEquals(new Edit(2, 3, 2, 2), r.get(1));
-	}
-
-	public void testEdit_CommonReplaceCommonDeleteCommon() {
-		EditList r = diff(t("aRbCd"), t("aSbd"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(1, 2, 1, 2), r.get(0));
-		assertEquals(new Edit(3, 4, 3, 3), r.get(1));
-	}
-
-	public void testEdit_MoveBlock() {
-		EditList r = diff(t("aYYbcdz"), t("abcdYYz"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(1, 3, 1, 1), r.get(0));
-		assertEquals(new Edit(6, 6, 4, 6), r.get(1));
-	}
-
-	public void testEdit_InvertBlocks() {
-		EditList r = diff(t("aYYbcdXXz"), t("aXXbcdYYz"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(1, 3, 1, 3), r.get(0));
-		assertEquals(new Edit(6, 8, 6, 8), r.get(1));
+public class PatienceDiffTest extends AbstractDiffTestCase {
+	@Override
+	protected DiffAlgorithm algorithm() {
+		PatienceDiff pd = new PatienceDiff();
+		pd.setFallbackAlgorithm(null);
+		return pd;
 	}
 
 	public void testEdit_NoUniqueMiddleSideA() {
@@ -153,55 +63,13 @@ public class PatienceDiffTest extends TestCase {
 		assertEquals(new Edit(1, 3, 1, 5), r.get(0));
 	}
 
-	public void testEdit_UniqueCommonLargerThanMatchPoint() {
-		// We are testing 3 unique common matches, but two of
-		// them are consumed as part of the 1st's LCS region.
-		EditList r = diff(t("AbdeZ"), t("PbdeQR"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(0, 1, 0, 1), r.get(0));
-		assertEquals(new Edit(4, 5, 4, 6), r.get(1));
-	}
-
-	public void testEdit_CommonGrowsPrefixAndSuffix() {
-		// Here there is only one common unique point, but we can grow it
-		// in both directions to find the LCS in the middle.
-		EditList r = diff(t("AaabccZ"), t("PaabccR"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(0, 1, 0, 1), r.get(0));
-		assertEquals(new Edit(6, 7, 6, 7), r.get(1));
-	}
-
-	public void testEdit_DuplicateAButCommonUniqueInB() {
-		EditList r = diff(t("AbbcR"), t("CbcS"));
-		assertEquals(2, r.size());
-		assertEquals(new Edit(0, 2, 0, 1), r.get(0));
-		assertEquals(new Edit(4, 5, 3, 4), r.get(1));
-	}
-
 	public void testPerformanceTestDeltaLength() {
 		String a = DiffTestDataGenerator.generateSequence(40000, 971, 3);
 		String b = DiffTestDataGenerator.generateSequence(40000, 1621, 5);
 		CharArray ac = new CharArray(a);
 		CharArray bc = new CharArray(b);
-		EditList r = new PatienceDiff().diff(new CharCmp(), ac, bc);
+		EditList r = algorithm().diff(new CharCmp(), ac, bc);
 		assertEquals(25, r.size());
-	}
-
-	private static EditList diff(RawText a, RawText b) {
-		return new PatienceDiff().diff(RawTextComparator.DEFAULT, a, b);
-	}
-
-	private static RawText t(String text) {
-		StringBuilder r = new StringBuilder();
-		for (int i = 0; i < text.length(); i++) {
-			r.append(text.charAt(i));
-			r.append('\n');
-		}
-		try {
-			return new RawText(r.toString().getBytes("UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	private static class CharArray extends Sequence {
