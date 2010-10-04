8c706ab4640a740d85326686c4421e537ba1e0f7
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-10-04 07:48:12 +0200

    Use capital L for long constants
    Change-Id: Ib7b8c5f982dc72c68cf3d81e45a536c464837f7d
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/PackIndexV2Test.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/PackIndexV2Test.java
@@ -74,21 +74,21 @@ public class PackIndexV2Test extends PackIndexTestCase {
 	public void testCRC32() throws MissingObjectException,
 			UnsupportedOperationException {
 		assertTrue(smallIdx.hasCRC32Support());
-		assertEquals(0x00000000C2B64258l, smallIdx.findCRC32(ObjectId
+		assertEquals(0x00000000C2B64258L, smallIdx.findCRC32(ObjectId
 				.fromString("4b825dc642cb6eb9a060e54bf8d69288fbee4904")));
-		assertEquals(0x0000000072AD57C2l, smallIdx.findCRC32(ObjectId
+		assertEquals(0x0000000072AD57C2L, smallIdx.findCRC32(ObjectId
 				.fromString("540a36d136cf413e4b064c2b0e0a4db60f77feab")));
-		assertEquals(0x00000000FF10A479l, smallIdx.findCRC32(ObjectId
+		assertEquals(0x00000000FF10A479L, smallIdx.findCRC32(ObjectId
 				.fromString("5b6e7c66c276e7610d4a73c70ec1a1f7c1003259")));
-		assertEquals(0x0000000034B27DDCl, smallIdx.findCRC32(ObjectId
+		assertEquals(0x0000000034B27DDCL, smallIdx.findCRC32(ObjectId
 				.fromString("6ff87c4664981e4397625791c8ea3bbb5f2279a3")));
-		assertEquals(0x000000004743F1E4l, smallIdx.findCRC32(ObjectId
+		assertEquals(0x000000004743F1E4L, smallIdx.findCRC32(ObjectId
 				.fromString("82c6b885ff600be425b4ea96dee75dca255b69e7")));
-		assertEquals(0x00000000640B358Bl, smallIdx.findCRC32(ObjectId
+		assertEquals(0x00000000640B358BL, smallIdx.findCRC32(ObjectId
 				.fromString("902d5476fa249b7abc9d84c611577a81381f0327")));
-		assertEquals(0x000000002A17CB5El, smallIdx.findCRC32(ObjectId
+		assertEquals(0x000000002A17CB5EL, smallIdx.findCRC32(ObjectId
 				.fromString("aabf2ffaec9b497f0950352b3e582d73035c2035")));
-		assertEquals(0x000000000B3B5BA6l, smallIdx.findCRC32(ObjectId
+		assertEquals(0x000000000B3B5BA6L, smallIdx.findCRC32(ObjectId
 				.fromString("c59759f143fb1fe21c197981df75a7ee00290799")));
 	}
 }
