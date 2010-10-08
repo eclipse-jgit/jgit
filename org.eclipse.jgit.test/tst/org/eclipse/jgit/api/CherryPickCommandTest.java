7159de976a44cfdf6de82d2cd4fd1278cab59305
Christian Halstrick <christian.halstrick@sap.com>
2010-10-08 15:39:04 +0200

    Add Cherry-Pick command
    Implemented the initial version of a cherry-pick command.
    A correct error handling is missing (what happens if the
    checkout fails, the cherry-pick leads to conflicts etc).
    But straightforward cherry-picks works.
    
    Change-Id: I235c0eb3a7a2d5bdfe40400f1deed06f29d746e1
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CherryPickCommandTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2010, Christian Halstrick <christian.halstrick@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.eclipse.jgit.api.errors.*;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/**
+ * Test cherry-pick command
+ */
+public class CherryPickCommandTest extends RepositoryTestCase {
+	public void testCherryPick() throws IOException, JGitInternalException,
+			GitAPIException {
+		Git git = new Git(db);
+
+		writeTrashFile("a", "first line\nsec. line\nthird line\n");
+		git.add().addFilepattern("a").call();
+		RevCommit firstCommit = git.commit().setMessage("create a").call();
+
+		writeTrashFile("b", "content\n");
+		git.add().addFilepattern("b").call();
+		git.commit().setMessage("create b").call();
+
+		writeTrashFile("a", "first line\nsec. line\nthird line\nfourth line\n");
+		git.add().addFilepattern("a").call();
+		git.commit().setMessage("enlarged a").call();
+
+		writeTrashFile("a",
+				"first line\nsecond line\nthird line\nfourth line\n");
+		git.add().addFilepattern("a").call();
+		RevCommit fixingA = git.commit().setMessage("fixed a").call();
+
+		git.branchCreate().setName("side").setStartPoint(firstCommit).call();
+		checkoutBranch("refs/heads/side");
+
+		writeTrashFile("a", "first line\nsec. line\nthird line\nfeature++\n");
+		git.add().addFilepattern("a").call();
+		git.commit().setMessage("enhanced a").call();
+
+		git.cherryPick().include(fixingA).call();
+
+		assertFalse(new File(db.getWorkTree(), "b").exists());
+		checkFile(new File(db.getWorkTree(), "a"),
+				"first line\nsecond line\nthird line\nfeature++\n");
+		Iterator<RevCommit> history = git.log().call().iterator();
+		assertEquals("fixed a", history.next().getFullMessage());
+		assertEquals("enhanced a", history.next().getFullMessage());
+		assertEquals("create a", history.next().getFullMessage());
+		assertFalse(history.hasNext());
+	}
+
+	private void checkoutBranch(String branchName)
+			throws IllegalStateException, IOException {
+		RevWalk walk = new RevWalk(db);
+		RevCommit head = walk.parseCommit(db.resolve(Constants.HEAD));
+		RevCommit branch = walk.parseCommit(db.resolve(branchName));
+		DirCacheCheckout dco = new DirCacheCheckout(db, head.getTree().getId(),
+				db.lockDirCache(), branch.getTree().getId());
+		dco.setFailOnConflict(true);
+		dco.checkout();
+		walk.release();
+		// update the HEAD
+		RefUpdate refUpdate = db.updateRef(Constants.HEAD);
+		refUpdate.link(branchName);
+	}
+
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CherryPickCommand.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2010, Christian Halstrick <christian.halstrick@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jgit.JGitText;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidCherryPickCommit;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectIdRef;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Ref.Storage;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.merge.ResolveMerger;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
+
+/**
+ * A class used to execute a {@code cherry-pick} command. It has setters for all
+ * supported options and arguments of this command and a {@link #call()} method
+ * to finally execute the command. Each instance of this class should only be
+ * used for one invocation of the command (means: one call to {@link #call()})
+ *
+ * @see <a
+ *      href="http://www.kernel.org/pub/software/scm/git/docs/git-cherry-pick.html"
+ *      >Git documentation about cherry-pick</a>
+ */
+public class CherryPickCommand extends GitCommand<RevCommit> {
+	private List<Ref> commits = new LinkedList<Ref>();
+
+	private List<Ref> cherryPickedRefs = new LinkedList<Ref>();
+
+	/**
+	 * @return the list of successfully cherry-picked {@link Ref}'s. Never
+	 *         <code>null</code> but maybe an empty list if no commit was
+	 *         successfully cherry-picked
+	 */
+	public List<Ref> getCherryPickedRefs() {
+		return cherryPickedRefs;
+	}
+
+	/**
+	 * @param repo
+	 */
+	protected CherryPickCommand(Repository repo) {
+		super(repo);
+	}
+
+	/**
+	 * Executes the {@code Cherry-Pick} command with all the options and
+	 * parameters collected by the setter methods (e.g. {@link #include(Ref)} of
+	 * this class. Each instance of this class should only be used for one
+	 * invocation of the command. Don't call this method twice on an instance.
+	 *
+	 * @return on success the {@link RevCommit} pointed to by the new HEAD is
+	 *         returned. If a failure occurred during cherry-pick
+	 *         <code>null</code> is returned. The list of successfully
+	 *         cherry-picked {@link Ref}'s can be obtained by calling
+	 *         {@link #getCherryPickedRefs()}
+	 */
+	public RevCommit call() throws GitAPIException {
+		RevCommit newHead = null;
+		checkCallable();
+
+		RevWalk revWalk = new RevWalk(repo);
+		try {
+
+			// get the head commit
+			Ref headRef = repo.getRef(Constants.HEAD);
+			if (headRef == null)
+				throw new NoHeadException(
+						JGitText.get().commitOnRepoWithoutHEADCurrentlyNotSupported);
+			RevCommit headCommit = revWalk.lookupCommit(headRef.getObjectId());
+
+			// loop through all refs to be cherry-picked
+			for (Ref src : commits) {
+				// get the commit to be cherry-picked
+				// handle annotated tags
+				ObjectId srcObjectId = src.getPeeledObjectId();
+				if (srcObjectId == null)
+					srcObjectId = src.getObjectId();
+				RevCommit srcCommit = revWalk.parseCommit(srcObjectId);
+
+				// get the parent of the commit to cherry-pick
+				if (srcCommit.getParentCount() != 1) {
+					throw new InvalidCherryPickCommit(
+							JGitText.get().canOnlyCherryPickCommitsWithOneParent);
+				}
+				RevCommit srcParent = revWalk.parseCommit(srcCommit
+						.getParent(0));
+
+				ResolveMerger merger = (ResolveMerger) MergeStrategy.RESOLVE
+						.newMerger(repo);
+				merger.setWorkingTreeIterator(new FileTreeIterator(repo));
+				merger.setBase(srcParent.getTree());
+
+				if (merger.merge(headCommit, srcCommit)) {
+					DirCacheCheckout dco = new DirCacheCheckout(repo,
+							headCommit.getTree(), repo.lockDirCache(),
+							merger.getResultTreeId());
+					dco.setFailOnConflict(true);
+					dco.checkout();
+					newHead = new Git(getRepository()).commit()
+							.setMessage(srcCommit.getFullMessage())
+							.setAuthor(srcCommit.getAuthorIdent()).call();
+					cherryPickedRefs.add(src);
+				} else {
+					return null;
+				}
+			}
+		} catch (IOException e) {
+			throw new JGitInternalException(
+					MessageFormat.format(
+							JGitText.get().exceptionCaughtDuringExecutionOfCherryPickCommand,
+							e), e);
+		} finally {
+			revWalk.release();
+		}
+		return newHead;
+	}
+
+	/**
+	 * @param commit
+	 *            a reference to a commit which is cherry-picked to the current
+	 *            head
+	 * @return {@code this}
+	 */
+	public CherryPickCommand include(Ref commit) {
+		checkCallable();
+		commits.add(commit);
+		return this;
+	}
+
+	/**
+	 * @param commit
+	 *            the Id of a commit which is cherry-picked to the current head
+	 * @return {@code this}
+	 */
+	public CherryPickCommand include(AnyObjectId commit) {
+		return include(commit.getName(), commit);
+	}
+
+	/**
+	 * @param name
+	 *            a name given to the commit
+	 * @param commit
+	 *            the Id of a commit which is cherry-picked to the current head
+	 * @return {@code this}
+	 */
+	public CherryPickCommand include(String name, AnyObjectId commit) {
+		return include(new ObjectIdRef.Unpeeled(Storage.LOOSE, name,
+				commit.copy()));
+	}
+
+}
