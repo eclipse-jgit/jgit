76872aa1e65b726da7bda886d058682ccb57a90b
Shawn O. Pearce <spearce@spearce.org>
2010-10-14 13:06:54 -0700

    Call ProgressMonitor.update() from main thread
    Don't permit transient worker threads to access the underlying output
    stream of a ProgressMonitor, as they might get marked as the stream's
    writer thread.  Instead proxy update events from the workers back onto
    the application's real work thread.  This ensures that the stream only
    sees a single thread, and its the thread that will remain alive for
    the entire life cycle of the operation.
    
    This fixes IOException("Write end dead") during local repository fetch
    when threaded delta search is enabled.  One of the transient delta
    search threads became the designated writer for the pipe, and when it
    terminated the reader end thought the writer was dead, even though the
    main writer thread was still executing in PackWriter.
    
    Bug: 326557
    Change-Id: I01d1b20a3d7be1c0b480c7fb5c9773c161fe5c15
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ThreadSafeProgressMonitorTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2010, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.lib;
+
+import junit.framework.TestCase;
+
+public class ThreadSafeProgressMonitorTest extends TestCase {
+	public void testFailsMethodsOnBackgroundThread()
+			throws InterruptedException {
+		final MockProgressMonitor mock = new MockProgressMonitor();
+		final ThreadSafeProgressMonitor pm = new ThreadSafeProgressMonitor(mock);
+
+		runOnThread(new Runnable() {
+			public void run() {
+				try {
+					pm.start(1);
+					fail("start did not fail on background thread");
+				} catch (IllegalStateException notMainThread) {
+					// Expected result
+				}
+
+				try {
+					pm.beginTask("title", 1);
+					fail("beginTask did not fail on background thread");
+				} catch (IllegalStateException notMainThread) {
+					// Expected result
+				}
+
+				try {
+					pm.endTask();
+					fail("endTask did not fail on background thread");
+				} catch (IllegalStateException notMainThread) {
+					// Expected result
+				}
+			}
+		});
+
+		// Ensure we didn't alter the mock above when checking threads.
+		assertNull(mock.taskTitle);
+		assertEquals(0, mock.value);
+	}
+
+	public void testMethodsOkOnMainThread() {
+		final MockProgressMonitor mock = new MockProgressMonitor();
+		final ThreadSafeProgressMonitor pm = new ThreadSafeProgressMonitor(mock);
+
+		pm.start(1);
+		assertEquals(1, mock.value);
+
+		pm.beginTask("title", 42);
+		assertEquals("title", mock.taskTitle);
+		assertEquals(42, mock.value);
+
+		pm.update(1);
+		assertEquals(43, mock.value);
+
+		pm.update(2);
+		assertEquals(45, mock.value);
+
+		pm.endTask();
+		assertNull(mock.taskTitle);
+		assertEquals(0, mock.value);
+	}
+
+	public void testUpdateOnBackgroundThreads() throws InterruptedException {
+		final MockProgressMonitor mock = new MockProgressMonitor();
+		final ThreadSafeProgressMonitor pm = new ThreadSafeProgressMonitor(mock);
+
+		pm.startWorker();
+		runOnThread(new Runnable() {
+			public void run() {
+				assertFalse(pm.isCancelled());
+				pm.update(2);
+				pm.endWorker();
+			}
+		});
+		pm.waitForCompletion();
+		assertEquals(2, mock.value);
+	}
+
+	private static void runOnThread(Runnable task) throws InterruptedException {
+		Thread t = new Thread(task);
+		t.start();
+		t.join(1000);
+		assertFalse("thread has stopped", t.isAlive());
+	}
+
+	private static class MockProgressMonitor implements ProgressMonitor {
+		String taskTitle;
+
+		int value;
+
+		public void update(int completed) {
+			value += completed;
+		}
+
+		public void start(int totalTasks) {
+			value = totalTasks;
+		}
+
+		public void beginTask(String title, int totalWork) {
+			taskTitle = title;
+			value = totalWork;
+		}
+
+		public void endTask() {
+			taskTitle = null;
+			value = 0;
+		}
+
+		public boolean isCancelled() {
+			return false;
+		}
+	}
+}
