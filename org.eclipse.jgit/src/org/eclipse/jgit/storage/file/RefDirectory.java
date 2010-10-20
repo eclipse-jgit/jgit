a4f7992dfb7ac1931e89af9035b33604717cb2c9
Christian Halstrick <christian.halstrick@sap.com>
2010-10-20 17:39:19 +0200

    Add support for special symref FETCH_HEAD and MERGE_HEAD
    The RefDirectory class was not returning FETCH_HEAD and
    MERGE_HEAD when trying to get all refs via getRefs(RefDatabase.ALL).
    This fix adds constants for FETCH_HEAD and ORIG_HEAD and adds a
    new getter getAdditionalRefs() to get these additional refs.
    To be compatible with c git the getRefs(ALL) method will not return
    FETCH_HEAD, MERGE_HEAD and ORIG_HEAD.
    
    Change-Id: Ie114ca92e9d5e7d61d892f4413ade65acdc08c32
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/RefDirectory.java
@@ -69,6 +69,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -122,6 +124,10 @@ public class RefDirectory extends RefDatabase {
 
 	/** If in the header, denotes the file has peeled data. */
 	public static final String PACKED_REFS_PEELED = " peeled"; //$NON-NLS-1$
+
+	/** The names of the additional refs supported by this class */
+	private static final String[] additionalRefsNames = new String[] {
+			Constants.MERGE_HEAD, Constants.FETCH_HEAD, Constants.ORIG_HEAD };
 
 	private final FileRepository parent;
 
@@ -295,6 +301,17 @@ public class RefDirectory extends RefDatabase {
 		}
 
 		return new RefMap(prefix, packed, upcast(loose), symbolic.toRefList());
+	}
+
+	@Override
+	public List<Ref> getAdditionalRefs() throws IOException {
+		List<Ref> ret = new LinkedList<Ref>();
+		for (String name : additionalRefsNames) {
+			Ref r = getRef(name);
+			if (r != null)
+				ret.add(r);
+		}
+		return ret;
 	}
 
 	@SuppressWarnings("unchecked")
