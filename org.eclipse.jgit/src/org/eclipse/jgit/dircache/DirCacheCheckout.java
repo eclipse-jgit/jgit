9e025a53facd42410796eae821baaeff684a25c5
Lluis Sanchez <slluis.devel@gmail.com>
2010-10-22 13:10:12 +0200

    Optimize DirCacheCheckout by only updating files that have changed
    When checking out a tree, files that are identical to the file in
    the current index and working directory don't need to be updated.

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
@@ -273,8 +273,11 @@ public class DirCacheCheckout {
 	void processEntry(CanonicalTreeParser m, DirCacheBuildIterator i,
 			WorkingTreeIterator f) {
 		if (m != null) {
-			update(m.getEntryPathString(), m.getEntryObjectId(),
-					m.getEntryFileMode());
+			if (f == null || !m.idEqual(f) || i == null || !m.idEqual(i)) {
+				update(m.getEntryPathString(), m.getEntryObjectId(),
+						m.getEntryFileMode());
+			} else
+				keep(i.getDirCacheEntry());
 		} else {
 			if (f != null) {
 				if (walk.isDirectoryFileConflict()) {
