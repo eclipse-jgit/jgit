d36c80fd048fc55d7ac31ae62eb67fbac9b12e31
Robin Stocker <robin@nibor.org>
2010-10-29 15:12:48 +0200

    Remove unnecessary null check
    The field monitor is never null, it's a NullProgressMonitor when not
    explicitly set.
    
    Change-Id: I8ce703a32c28ce5c3455efeb7ed5f5c9a443cbef

--- org.eclipse.jgit/src/org/eclipse/jgit/api/PullCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/PullCommand.java
@@ -202,8 +202,7 @@ public class PullCommand extends GitCommand<PullResult> {
 
 			FetchCommand fetch = new FetchCommand(repo);
 			fetch.setRemote(remote);
-			if (monitor != null)
-				fetch.setProgressMonitor(monitor);
+			fetch.setProgressMonitor(monitor);
 			fetch.setTimeout(this.timeout);
 
 			fetchRes = fetch.call();
