2dc2dd8b1b4c98b255b3f2dec655a3f0b197ce07
Jens Baumgart <jens.baumgart@sap.com>
2010-11-08 16:18:57 +0100

    IndexDiff: support state [removed, untracked]
    IndexDiff was extended to detect files which are both removed from the
    index and untracked.  Before this change these files were only added
    to the removed collection.
    
    Change-Id: I971d8261d2e8932039fce462b59c12e143f79f90
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/IndexDiffTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/IndexDiffTest.java
@@ -48,6 +48,9 @@ package org.eclipse.jgit.lib;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheEditor;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
 
 public class IndexDiffTest extends RepositoryTestCase {
@@ -209,5 +212,33 @@ public class IndexDiffTest extends RepositoryTestCase {
 		} finally {
 			oi.release();
 		}
+	}
+
+	/**
+	 * A file is removed from the index but stays in the working directory. It
+	 * is checked if IndexDiff detects this file as removed and untracked.
+	 *
+	 * @throws Exception
+	 */
+	public void testRemovedUntracked() throws Exception{
+		Git git = new Git(db);
+		String path = "file";
+		writeTrashFile(path, "content");
+		git.add().addFilepattern(path).call();
+		git.commit().setMessage("commit").call();
+		removeFromIndex(path);
+		FileTreeIterator iterator = new FileTreeIterator(db);
+		IndexDiff diff = new IndexDiff(db, Constants.HEAD, iterator);
+		diff.diff();
+		assertTrue(diff.getRemoved().contains(path));
+		assertTrue(diff.getUntracked().contains(path));
+	}
+
+	private void removeFromIndex(String path) throws IOException {
+		final DirCache dirc = db.lockDirCache();
+		final DirCacheEditor edit = dirc.editor();
+		edit.add(new DirCacheEditor.DeletePath(path));
+		if (!edit.commit())
+			throw new IOException("could not commit");
 	}
 }
