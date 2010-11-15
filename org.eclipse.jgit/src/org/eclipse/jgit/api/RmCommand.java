7168a96ca7ede1951141793507de048d13b43cc4
Chris Aniszczyk <caniszczyk@gmail.com>
2010-11-15 13:57:04 -0600

    Add RmCommand to Git API
    Bug: 330827
    Change-Id: I0b74bb92254d0ee988139d25022d06d16ed89d58
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RmCommand.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2010, Chris Aniszczyk <caniszczyk@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.eclipse.jgit.JGitText;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheBuildIterator;
+import org.eclipse.jgit.dircache.DirCacheBuilder;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
+
+/**
+ * A class used to execute a {@code Rm} command. It has setters for all
+ * supported options and arguments of this command and a {@link #call()} method
+ * to finally execute the command. Each instance of this class should only be
+ * used for one invocation of the command (means: one call to {@link #call()})
+ *
+ * @see <a href="http://www.kernel.org/pub/software/scm/git/docs/git-rm.html"
+ *      >Git documentation about Rm</a>
+ */
+public class RmCommand extends GitCommand<DirCache> {
+
+	private Collection<String> filepatterns;
+
+	/**
+	 *
+	 * @param repo
+	 */
+	public RmCommand(Repository repo) {
+		super(repo);
+		filepatterns = new LinkedList<String>();
+	}
+
+	/**
+	 * @param filepattern
+	 *            File to remove.
+	 * @return {@code this}
+	 */
+	public RmCommand addFilepattern(String filepattern) {
+		checkCallable();
+		filepatterns.add(filepattern);
+		return this;
+	}
+
+	/**
+	 * Executes the {@code Rm} command. Each instance of this class should only
+	 * be used for one invocation of the command. Don't call this method twice
+	 * on an instance.
+	 *
+	 * @return the DirCache after Rm
+	 */
+	public DirCache call() throws NoFilepatternException {
+
+		if (filepatterns.isEmpty())
+			throw new NoFilepatternException(JGitText.get().atLeastOnePatternIsRequired);
+		checkCallable();
+		DirCache dc = null;
+
+		try {
+			dc = repo.lockDirCache();
+			DirCacheBuilder builder = dc.builder();
+			final TreeWalk tw = new TreeWalk(repo);
+			tw.reset(); // drop the first empty tree, which we do not need here
+			tw.setRecursive(true);
+			tw.setFilter(PathFilterGroup.createFromStrings(filepatterns));
+			tw.addTree(new DirCacheBuildIterator(builder));
+
+			while (tw.next()) {
+				final File path = new File(repo.getWorkTree(),
+						tw.getPathString());
+				final FileMode mode = tw.getFileMode(0);
+				if (mode.getObjectType() == Constants.OBJ_BLOB) {
+					// Deleting a blob is simply a matter of removing
+					// the file or symlink named by the tree entry.
+					delete(path);
+				}
+			}
+			builder.commit();
+			setCallable(false);
+		} catch (IOException e) {
+			throw new JGitInternalException(
+					JGitText.get().exceptionCaughtDuringExecutionOfRmCommand, e);
+		} finally {
+			if (dc != null)
+				dc.unlock();
+		}
+
+		return dc;
+	}
+
+	private void delete(File p) {
+		while (p != null && !p.equals(repo.getWorkTree()) && p.delete())
+			p = p.getParentFile();
+	}
+
+}
