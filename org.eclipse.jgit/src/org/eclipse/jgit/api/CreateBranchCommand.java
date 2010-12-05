88a19131e1b2097e916e29880479211ae4df6a9b
Manuel Doninger <manuel.doninger@googlemail.com>
2010-12-05 18:15:31 +0100

    Don't prepend refs/heads/ to branch name, if it already starts with it
    If the proposed name for a new branch already starts with refs/heads/,
    don't prepend that string.
    
    Change-Id: Ie2770eed66445dbdd210b5151e5fe5b9d18d0a78
    Signed-off-by: Manuel Doninger <manuel.doninger@googlemail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CreateBranchCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CreateBranchCommand.java
@@ -173,7 +173,14 @@ public class CreateBranchCommand extends GitCommand<Ref> {
 							+ startPointFullName;
 			}
 
-			RefUpdate updateRef = repo.updateRef(Constants.R_HEADS + name);
+			RefUpdate updateRef;
+
+			if (name.startsWith(Constants.R_HEADS)) {
+				updateRef = repo.updateRef(name);
+			} else {
+				updateRef = repo.updateRef(Constants.R_HEADS + name);
+			}
+
 			updateRef.setNewObjectId(startAt);
 			updateRef.setRefLogMessage(refLogMessage, false);
 			Result updateResult;
