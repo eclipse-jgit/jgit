6b71a4cbe9107bce2a10cfa2410bde65ae440070
Jens Baumgart <jens.baumgart@sap.com>
2010-12-08 16:15:00 +0100

    Introduce http test bundle
    Introduce a http test bundle to make this functionality available for
    EGit tests. A simple http server class is provided. The jetty version
    was updated to a version that is also available via p2 (needed in EGit
    UI tests).
    
    Change-Id: I13bfc4c6c47e27d8f97d3e9752347d6d23e553d4
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.junit.http/src/org/eclipse/jgit/junit/http/SimpleHttpServer.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2009-2010, Google Inc.
+ * Copyright (C) 2010, Jens Baumgart <jens.baumgart@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.junit.http;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.http.server.GitServlet;
+import org.eclipse.jgit.http.server.resolver.RepositoryResolver;
+import org.eclipse.jgit.http.server.resolver.ServiceNotEnabledException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepository;
+import org.eclipse.jgit.transport.URIish;
+
+/**
+ * Simple http server for testing http access to Git repositories.
+ * Authentication with hardcoded credentials user:agitter password:letmein.
+ */
+public class SimpleHttpServer {
+
+	AppServer server;
+
+	private final FileRepository db;
+
+	private URIish uri;
+
+	public SimpleHttpServer(FileRepository repository) {
+		this.db = repository;
+		server = new AppServer();
+	}
+
+	public void start() throws Exception {
+		ServletContextHandler sBasic = server.authBasic(smart("/sbasic"));
+		server.setUp();
+		final String srcName = db.getDirectory().getName();
+		uri = toURIish(sBasic, srcName);
+	}
+
+	public void stop() throws Exception {
+		server.tearDown();
+	}
+
+	public URIish getUri() {
+		return uri;
+	}
+
+	private ServletContextHandler smart(final String path) {
+		GitServlet gs = new GitServlet();
+		gs.setRepositoryResolver(new RepositoryResolver() {
+			public Repository open(HttpServletRequest req, String name)
+					throws RepositoryNotFoundException,
+					ServiceNotEnabledException {
+				if (!name.equals(nameOf(db)))
+					throw new RepositoryNotFoundException(name);
+
+				db.incrementOpen();
+				return db;
+			}
+		});
+
+		ServletContextHandler ctx = server.addContext(path);
+		ctx.addServlet(new ServletHolder(gs), "/*");
+		return ctx;
+	}
+
+	private static String nameOf(final FileRepository db) {
+		return db.getDirectory().getName();
+	}
+
+	private URIish toURIish(String path) throws URISyntaxException {
+		URI u = server.getURI().resolve(path);
+		return new URIish(u.toString());
+	}
+
+	private URIish toURIish(ServletContextHandler app, String name)
+			throws URISyntaxException {
+		String p = app.getContextPath();
+		if (!p.endsWith("/") && !name.startsWith("/"))
+			p += "/";
+		p += name;
+		return toURIish(p);
+	}
+}
