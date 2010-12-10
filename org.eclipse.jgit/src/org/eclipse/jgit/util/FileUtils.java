6c2a69555a496d252f954ed5951efd83bef9f27b
Matthias Sohn <matthias.sohn@sap.com>
2010-12-10 02:15:46 +0100

    File utilities for creating directories
    The java.io.File methods for creating directories report failure by
    returning false. To ease proper checking of return values provide
    utility methods wrapping mkdir() and mkdirs() which throw IOException
    on failure.
    
    Also fix the tests to store test data under a trash folder and cleanup
    after test.
    
    Change-Id: I09c7f9909caf7e25feabda9d31e21ce154e7fcd5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FileUtils.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FileUtils.java
@@ -135,4 +135,40 @@ public class FileUtils {
 		}
 	}
 
+	/**
+	 * Creates the directory named by this abstract pathname.
+	 *
+	 * @param d
+	 *            directory to be created
+	 * @throws IOException
+	 *             if creation of {@code d} fails. This may occur if {@code d}
+	 *             did exist when the method was called. This can therefore
+	 *             cause IOExceptions during race conditions when multiple
+	 *             concurrent threads all try to create the same directory.
+	 */
+	public static void mkdir(final File d) throws IOException {
+		if (!d.mkdir())
+			throw new IOException(MessageFormat.format(
+					JGitText.get().mkDirFailed, d.getAbsolutePath()));
+	}
+
+	/**
+	 * Creates the directory named by this abstract pathname, including any
+	 * necessary but nonexistent parent directories. Note that if this operation
+	 * fails it may have succeeded in creating some of the necessary parent
+	 * directories.
+	 *
+	 * @param d
+	 *            directory to be created
+	 * @throws IOException
+	 *             if creation of {@code d} fails. This may occur if {@code d}
+	 *             did exist when the method was called. This can therefore
+	 *             cause IOExceptions during race conditions when multiple
+	 *             concurrent threads all try to create the same directory.
+	 */
+	public static void mkdirs(final File d) throws IOException {
+		if (!d.mkdirs())
+			throw new IOException(MessageFormat.format(
+					JGitText.get().mkDirsFailed, d.getAbsolutePath()));
+	}
 }
