e7049886004f0def4291811a863c658061411014
Shawn O. Pearce <spearce@spearce.org>
2010-12-13 14:19:02 -0800

    FileBasedConfig: Use FileSnapshot for isOutdated()
    Relying only on the last modified time for a file can be tricky.
    The "racy git" problem may cause some modifications to be missed.
    
    Use the new FileSnapshot code to track when a configuration file
    has been modified, and needs to be reloaded in memory.
    
    Change-Id: Ib6312fdd3b2403eee5af3f8ae711294b0e5f9035
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/LockFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/LockFile.java
@@ -89,11 +89,11 @@ public class LockFile {
 
 	private FileOutputStream os;
 
-	private boolean needStatInformation;
+	private boolean needSnapshot;
 
 	private boolean fsync;
 
-	private long commitLastModified;
+	private FileSnapshot commitSnapshot;
 
 	private final FS fs;
 
@@ -334,12 +334,24 @@ public class LockFile {
 
 	/**
 	 * Request that {@link #commit()} remember modification time.
+	 * <p>
+	 * This is an alias for {@code setNeedSnapshot(true)}.
 	 *
 	 * @param on
 	 *            true if the commit method must remember the modification time.
 	 */
 	public void setNeedStatInformation(final boolean on) {
-		needStatInformation = on;
+		setNeedSnapshot(on);
+	}
+
+	/**
+	 * Request that {@link #commit()} remember the {@link FileSnapshot}.
+	 *
+	 * @param on
+	 *            true if the commit method must remember the FileSnapshot.
+	 */
+	public void setNeedSnapshot(final boolean on) {
+		needSnapshot = on;
 	}
 
 	/**
@@ -442,8 +454,8 @@ public class LockFile {
 	}
 
 	private void saveStatInformation() {
-		if (needStatInformation)
-			commitLastModified = lck.lastModified();
+		if (needSnapshot)
+			commitSnapshot = FileSnapshot.save(lck);
 	}
 
 	/**
@@ -452,7 +464,12 @@ public class LockFile {
 	 * @return modification time of the lock file right before we committed it.
 	 */
 	public long getCommitLastModified() {
-		return commitLastModified;
+		return commitSnapshot.lastModified();
+	}
+
+	/** @return get the {@link FileSnapshot} just before commit. */
+	public FileSnapshot getCommitSnapshot() {
+		return commitSnapshot;
 	}
 
 	/**
