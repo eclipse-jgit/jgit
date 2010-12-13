65a27aef8a1a5d159543b6c255d2ae4d801ee4ce
Shawn O. Pearce <spearce@spearce.org>
2010-12-13 12:23:07 -0800

    Extract pack directory last modified check code
    Pulling the last modified checking logic out of ObjectDirectory
    makes it possible to reuse this code for other files, such as
    the $GIT_DIR/config or $GIT_DIR/packed-refs files.
    
    Change-Id: If2f27a89fc3b7adde7e65ff40bbca5d55b98b772
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
@@ -96,7 +96,8 @@ import org.eclipse.jgit.util.FileUtils;
  * considered.
  */
 public class ObjectDirectory extends FileObjectDatabase {
-	private static final PackList NO_PACKS = new PackList(-1, -1, new PackFile[0]);
+	private static final PackList NO_PACKS = new PackList(
+			FileSnapshot.DIRTY, new PackFile[0]);
 
 	/** Maximum number of candidates offered as resolutions of abbreviation. */
 	private static final int RESOLVE_ABBREV_LIMIT = 256;
@@ -509,7 +510,7 @@ public class ObjectDirectory extends FileObjectDatabase {
 
 	boolean tryAgain1() {
 		final PackList old = packList.get();
-		if (old.tryAgain(packDirectory.lastModified()))
+		if (old.snapshot.isModified(packDirectory))
 			return old != scanPacks(old);
 		return false;
 	}
@@ -539,7 +540,7 @@ public class ObjectDirectory extends FileObjectDatabase {
 			final PackFile[] newList = new PackFile[1 + oldList.length];
 			newList[0] = pf;
 			System.arraycopy(oldList, 0, newList, 1, oldList.length);
-			n = new PackList(o.lastRead, o.lastModified, newList);
+			n = new PackList(o.snapshot, newList);
 		} while (!packList.compareAndSet(o, n));
 	}
 
@@ -556,7 +557,7 @@ public class ObjectDirectory extends FileObjectDatabase {
 			final PackFile[] newList = new PackFile[oldList.length - 1];
 			System.arraycopy(oldList, 0, newList, 0, j);
 			System.arraycopy(oldList, j + 1, newList, j, newList.length - j);
-			n = new PackList(o.lastRead, o.lastModified, newList);
+			n = new PackList(o.snapshot, newList);
 		} while (!packList.compareAndSet(o, n));
 		deadPack.close();
 	}
@@ -590,8 +591,7 @@ public class ObjectDirectory extends FileObjectDatabase {
 
 	private PackList scanPacksImpl(final PackList old) {
 		final Map<String, PackFile> forReuse = reuseMap(old);
-		final long lastRead = System.currentTimeMillis();
-		final long lastModified = packDirectory.lastModified();
+		final FileSnapshot snapshot = FileSnapshot.save(packDirectory);
 		final Set<String> names = listPackDirectory();
 		final List<PackFile> list = new ArrayList<PackFile>(names.size() >> 2);
 		boolean foundNew = false;
@@ -628,19 +628,21 @@ public class ObjectDirectory extends FileObjectDatabase {
 		// the same as the set we were given. Instead of building a new object
 		// return the same collection.
 		//
-		if (!foundNew && lastModified == old.lastModified && forReuse.isEmpty())
-			return old.updateLastRead(lastRead);
+		if (!foundNew && forReuse.isEmpty() && snapshot.equals(old.snapshot)) {
+			old.snapshot.setClean(snapshot);
+			return old;
+		}
 
 		for (final PackFile p : forReuse.values()) {
 			p.close();
 		}
 
 		if (list.isEmpty())
-			return new PackList(lastRead, lastModified, NO_PACKS.packs);
+			return new PackList(snapshot, NO_PACKS.packs);
 
 		final PackFile[] r = list.toArray(new PackFile[list.size()]);
 		Arrays.sort(r, PackFile.SORT);
-		return new PackList(lastRead, lastModified, r);
+		return new PackList(snapshot, r);
 	}
 
 	private static Map<String, PackFile> reuseMap(final PackList old) {
@@ -737,62 +739,15 @@ public class ObjectDirectory extends FileObjectDatabase {
 	}
 
 	private static final class PackList {
-		/** Last wall-clock time the directory was read. */
-		volatile long lastRead;
-
-		/** Last modification time of {@link ObjectDirectory#packDirectory}. */
-		final long lastModified;
+		/** State just before reading the pack directory. */
+		final FileSnapshot snapshot;
 
 		/** All known packs, sorted by {@link PackFile#SORT}. */
 		final PackFile[] packs;
 
-		private boolean cannotBeRacilyClean;
-
-		PackList(final long lastRead, final long lastModified,
-				final PackFile[] packs) {
-			this.lastRead = lastRead;
-			this.lastModified = lastModified;
+		PackList(final FileSnapshot monitor, final PackFile[] packs) {
+			this.snapshot = monitor;
 			this.packs = packs;
-			this.cannotBeRacilyClean = notRacyClean(lastRead);
-		}
-
-		private boolean notRacyClean(final long read) {
-			return read - lastModified > 2 * 60 * 1000L;
-		}
-
-		PackList updateLastRead(final long now) {
-			if (notRacyClean(now))
-				cannotBeRacilyClean = true;
-			lastRead = now;
-			return this;
-		}
-
-		boolean tryAgain(final long currLastModified) {
-			// Any difference indicates the directory was modified.
-			//
-			if (lastModified != currLastModified)
-				return true;
-
-			// We have already determined the last read was far enough
-			// after the last modification that any new modifications
-			// are certain to change the last modified time.
-			//
-			if (cannotBeRacilyClean)
-				return false;
-
-			if (notRacyClean(lastRead)) {
-				// Our last read should have marked cannotBeRacilyClean,
-				// but this thread may not have seen the change. The read
-				// of the volatile field lastRead should have fixed that.
-				//
-				return false;
-			}
-
-			// We last read this directory too close to its last observed
-			// modification time. We may have missed a modification. Scan
-			// the directory again, to ensure we still see the same state.
-			//
-			return true;
 		}
 	}
 
