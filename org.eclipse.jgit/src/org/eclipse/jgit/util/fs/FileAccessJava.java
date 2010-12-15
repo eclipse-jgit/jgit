90cd1cd104044b1a134e5056f476ca9eb55be497
Shawn O. Pearce <spearce@spearce.org>
2010-12-14 19:49:35 -0800

    Provide native readdir() via JNI
    Some platforms (notably Linux and FreeBSD) can supply not only the
    name of a file in a directory, but also what sort of file it is.
    This can be useful when traversing the directory structure as it
    may save lstat() calls on directories or symbolic links.
    
    Bug: 331071
    Change-Id: I40e753283fbd40a4ae9dd42f9bc0c7f2cd28c0c4
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/fs/FileAccessJava.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/fs/FileAccessJava.java
@@ -141,4 +141,16 @@ public class FileAccessJava extends FileAccess {
 			throw new NativeException(err.getMessage(), err);
 		}
 	}
+
+	@Override
+	public DirEnt[] list(File dir) throws AccessDeniedException {
+		String[] names = dir.list();
+		if (names == null)
+			throw new AccessDeniedException(dir.getPath());
+
+		DirEnt[] r = new DirEnt[names.length];
+		for (int i = 0; i < names.length; i++)
+			r[i] = new DirEnt(names[i]);
+		return r;
+	}
 }
