c4c8d80fd3a32cace67e7e42650aae7c46dcc622
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-01-25 23:26:38 +0100

    Fix adding files in a network share
    We cannot always rename read-only files on network shares,
    so rename the temp file for a new loose object first, and
    then set it as read-only.
    
    Bug: 335388
    Change-Id: Ie0350e032a97e0d09626d6143c5c692873a5f6a2
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ObjectDirectory.java
@@ -472,8 +472,6 @@ public class ObjectDirectory extends FileObjectDatabase {
 			return InsertLooseObjectResult.EXISTS_PACKED;
 		}
 
-		tmp.setReadOnly();
-
 		final File dst = fileFor(id);
 		if (dst.exists()) {
 			// We want to be extra careful and avoid replacing an object
@@ -484,6 +482,7 @@ public class ObjectDirectory extends FileObjectDatabase {
 			return InsertLooseObjectResult.EXISTS_LOOSE;
 		}
 		if (tmp.renameTo(dst)) {
+			tmp.setReadOnly();
 			unpackedObjectCache.add(id);
 			return InsertLooseObjectResult.INSERTED;
 		}
