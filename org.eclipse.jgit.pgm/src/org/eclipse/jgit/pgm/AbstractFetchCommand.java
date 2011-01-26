7173eb9a897042b36c2d64df3b9c08a1dd91c4e4
Sasa Zivkov <sasa.zivkov@sap.com>
2011-01-26 18:20:13 +0100

    Fixed NLS in JGit command line interface.
    There was one place where the parameter substitution wasn't done which caused
    text fragments like "{0}" to appear in JGits output.
    
    Bug: 325025
    Change-Id: I89b881a8b5ef39f609437546310463ed4f6e1fb5
    Signed-off-by: Sasa Zivkov <sasa.zivkov@sap.com>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/AbstractFetchCommand.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/AbstractFetchCommand.java
@@ -77,8 +77,8 @@ abstract class AbstractFetchCommand extends TextBuiltin {
 				final String dst = abbreviateRef(u.getLocalName(), true);
 
 				if (!shownURI) {
-					out.format(CLIText.get().fromURI, r.getURI());
-					out.println();
+					out.println(MessageFormat.format(CLIText.get().fromURI,
+							r.getURI()));
 					shownURI = true;
 				}
 
