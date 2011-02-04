e702d05582093db5ae26a41c0633e3dd41922f3a
Shawn O. Pearce <spearce@spearce.org>
2011-02-04 05:51:27 -0800

    Fix JGit --upload-pack, --receive-pack options
    JGit did not use sh -c to run the receive-pack or upload-pack programs
    locally, which caused errors if these strings contained spaces and
    needed the local shell to evaluate them.
    
    Win32 support using cmd.exe /c is completely untested, but seems like
    it should work based on the limited information I could get through
    Google search results.
    
    Bug: 336301
    Change-Id: I22e5e3492fdebbae092d1ce6b47ad411e57cc1ba
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS_Win32_Cygwin.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS_Win32_Cygwin.java
@@ -46,6 +46,9 @@ package org.eclipse.jgit.util;
 import java.io.File;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class FS_Win32_Cygwin extends FS_Win32 {
 	private static String cygpath;
@@ -85,5 +88,18 @@ class FS_Win32_Cygwin extends FS_Win32 {
 		if (home == null || home.length() == 0)
 			return super.userHomeImpl();
 		return resolve(new File("."), home);
+	}
+
+	@Override
+	public ProcessBuilder runInShell(String cmd, String[] args) {
+		List<String> argv = new ArrayList<String>(4 + args.length);
+		argv.add("sh.exe");
+		argv.add("-c");
+		argv.add(cmd + " \"$@\"");
+		argv.add(cmd);
+		argv.addAll(Arrays.asList(args));
+		ProcessBuilder proc = new ProcessBuilder();
+		proc.command(argv);
+		return proc;
 	}
 }
