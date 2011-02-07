522e27ca2567523d3bdbae76cfc20510e004aa80
Shawn O. Pearce <spearce@spearce.org>
2011-02-06 17:42:28 -0800

    pgm: Make --git-dir a string
    DHT based repository types don't use a java.io.File to name the
    repository.  Moving the type to a string starts to open up more types
    of repository names, making the standard pgm package easier to reuse
    on other storage systems.
    
    Change-Id: I262ccc8c01cd6db88f832ef317b0e1e5db2d016a
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/TextHashFunctions.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/TextHashFunctions.java
@@ -266,7 +266,7 @@ class TextHashFunctions extends TextBuiltin {
 	protected void run() throws Exception {
 		if (gitDirs.isEmpty()) {
 			RepositoryBuilder rb = new RepositoryBuilder() //
-					.setGitDir(gitdir) //
+					.setGitDir(new File(gitdir)) //
 					.readEnvironment() //
 					.findGitDir();
 			if (rb.getGitDir() == null)
