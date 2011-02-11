e2a35242b05676a9b0cbad9c7ade5e6d5c6e6420
Matthias Sohn <matthias.sohn@sap.com>
2011-02-11 23:03:40 +0100

    Fix version.sh
    Change-Id: Ia010c9cecefbfb90ae54786adc7c8d838525d2f3
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- tools/version.sh
+++ tools/version.sh
@@ -116,6 +116,12 @@ perl -pi~ -e '
 	' $(git ls-files | grep META-INF/MANIFEST.MF)
 
 perl -pi~ -e '
+	s/^(Bundle-Version:\s*).*$/${1}'"$OSGI_V"'/;
+	s/(org.eclipse.jgit.*;version=")[^"[(]*(")/${1}'"$API_V"'${2}/;
+	s/(org.eclipse.jgit.*;version="\[)[^"]*(\)")/${1}'"$API_V,$API_N"'${2}/;
+	' $(git ls-files | grep META-INF/SOURCE-MANIFEST.MF)
+
+perl -pi~ -e '
 	if ($ARGV ne $old_argv) {
 		$seen_version = 0;
 		$old_argv = $ARGV;
@@ -135,11 +141,22 @@ perl -pi~ -e '
 		$seen_version = 0;
 		$old_argv = $ARGV;
 	}
-	if ($seen_version < 4) {
+	if ($seen_version < 6) {
 		$seen_version++ if
 		s{<(version)>.*</\1>}{<${1}>'"$POM_V"'</${1}>};
 	}
 	' org.eclipse.jgit.packaging/org.eclipse.jgit.updatesite/pom.xml
+
+perl -pi~ -e '
+	if ($ARGV ne $old_argv) {
+		$seen_version = 0;
+		$old_argv = $ARGV;
+	}
+	if ($seen_version < 2) {
+		$seen_version++ if
+		s{<(version)>.*</\1>}{<${1}>'"$POM_V"'</${1}>};
+	}
+	' org.eclipse.jgit.packaging/pom.xml
 
 perl -pi~ -e '
 	if ($ARGV ne $old_argv) {
