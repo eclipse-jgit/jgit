109c81439ad2f92a23884557d1f72c6417794edb
Shawn O. Pearce <spearce@spearce.org>
2011-03-02 15:23:30 -0800

    RFC Store Git on any DHT
    jgit.storage.dht is a storage provider implementation for JGit that
    permits storing the Git repository in a distributed hashtable, NoSQL
    system, or other database.  The actual underlying storage system is
    undefined, and can be plugged in by implementing 7 small interfaces:
    
      *  Database
      *  RepositoryIndexTable
      *  RepositoryTable
      *  RefTable
      *  ChunkTable
      *  ObjectIndexTable
      *  WriteBuffer
    
    The storage provider interface tries to assume very little about the
    underlying storage system, and requires only three key features:
    
      *  key -> value lookup (a hashtable is suitable)
      *  atomic updates on single rows
      *  asynchronous operations (Java's ExecutorService is easy to use)
    
    Most NoSQL database products offer all 3 of these features in their
    clients, and so does any decent network based cache system like the
    open source memcache product.  Relying only on key equality for data
    retrevial makes it simple for the storage engine to distribute across
    multiple machines.  Traditional SQL systems could also be used with a
    (yet to be written) JDBC based spi implementation.
    
    Thus far I have implemented five storage systems for the spi layer:
    Apache HBase[1], Apache Cassandra[2], an in-memory implmenetation for
    unit testing, a JDBC implementation for SQL, and a generic cache
    provider that can ride on top of memcache.  All five systems weigh in
    with an spi layer around 1000 lines of code to implement the above 7
    interfaces.  This is a huge reduction in size compared to prior
    attempts to implement a new JGit storage layer.  As this package
    shows, a complete JGit storage implementation is closer to 17,000
    lines of fairly complex code.
    
    A simple network cache is also provided in storage.dht.spi.cache,
    implementers can use CacheDatabase to wrap any other type of Database
    and perform fast reads against a network based cache service, such as
    the open source memcached.
    
    [1] https://github.com/spearce/jgit_hbase
    [2] https://github.com/spearce/jgit_cassandra
    
    Change-Id: I0aa4072781f5ccc019ca421c036adff2c40c4295
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit.storage.dht/src/org/eclipse/jgit/storage/dht/spi/memory/MemObjectIndexTable.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2011, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.storage.dht.spi.memory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jgit.storage.dht.AsyncCallback;
+import org.eclipse.jgit.storage.dht.ChunkKey;
+import org.eclipse.jgit.storage.dht.DhtException;
+import org.eclipse.jgit.storage.dht.ObjectIndexKey;
+import org.eclipse.jgit.storage.dht.ObjectInfo;
+import org.eclipse.jgit.storage.dht.spi.Context;
+import org.eclipse.jgit.storage.dht.spi.ObjectIndexTable;
+import org.eclipse.jgit.storage.dht.spi.WriteBuffer;
+import org.eclipse.jgit.storage.dht.spi.util.ColumnMatcher;
+
+final class MemObjectIndexTable implements ObjectIndexTable {
+	private final MemTable table = new MemTable();
+
+	private final ColumnMatcher colInfo = new ColumnMatcher("info:");
+
+	public void get(Context options, Set<ObjectIndexKey> objects,
+			AsyncCallback<Map<ObjectIndexKey, Collection<ObjectInfo>>> callback) {
+		Map<ObjectIndexKey, Collection<ObjectInfo>> out = new HashMap<ObjectIndexKey, Collection<ObjectInfo>>();
+
+		for (ObjectIndexKey objId : objects) {
+			for (MemTable.Cell cell : table.scanFamily(objId.asBytes(), colInfo)) {
+				Collection<ObjectInfo> info = out.get(objId);
+				if (info == null) {
+					info = new ArrayList<ObjectInfo>(4);
+					out.put(objId, info);
+				}
+
+				ChunkKey chunk = ChunkKey.fromBytes(
+						colInfo.suffix(cell.getName()));
+				byte[] value = cell.getValue();
+				long time = cell.getTimestamp();
+				info.add(ObjectInfo.fromBytes(chunk, value, time));
+			}
+		}
+
+		callback.onSuccess(out);
+	}
+
+	public void add(ObjectIndexKey objId, ObjectInfo info, WriteBuffer buffer)
+			throws DhtException {
+		ChunkKey chunk = info.getChunkKey();
+		table.put(objId.asBytes(), colInfo.append(chunk.asBytes()),
+				info.asBytes());
+	}
+
+	public void remove(ObjectIndexKey objId, ChunkKey chunk, WriteBuffer buffer)
+			throws DhtException {
+		table.delete(objId.asBytes(), colInfo.append(chunk.asBytes()));
+	}
+}
