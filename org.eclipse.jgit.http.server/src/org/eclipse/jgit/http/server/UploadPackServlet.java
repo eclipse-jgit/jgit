a306b38a7eb1298ca8fe257a4a79a32aecfafc3a
Shawn O. Pearce <spearce@spearce.org>
2011-03-16 19:19:15 -0700

    smart HTTP: Return errors inside payload
    When the client is clearly making a smart HTTP request to our smart
    HTTP server, return any errors like RepositoryNotFoundException or
    ServiceNotEnabledException inside of the payload as a Git level ERR
    message, rather than an HTTP error code.
    
    This prevents the C Git command line client from retrying a failed
    "$URL/info/refs?service=git-upload-pack" request without the smart
    service URL, only to fail again with "403 Forbidden" when the dumb
    as-is service has been disabled by the server configuration, or is
    unavailable because the repository is not on the local filesystem.
    
    Change-Id: I57e8756d5026e885e0ca615979bfcd729703be6c
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/UploadPackServlet.java
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/UploadPackServlet.java
@@ -77,7 +77,7 @@ import org.eclipse.jgit.transport.resolver.UploadPackFactory;
 class UploadPackServlet extends HttpServlet {
 	private static final String REQ_TYPE = "application/x-git-upload-pack-request";
 
-	private static final String RSP_TYPE = "application/x-git-upload-pack-result";
+	static final String RSP_TYPE = "application/x-git-upload-pack-result";
 
 	private static final long serialVersionUID = 1L;
 
@@ -130,7 +130,7 @@ class UploadPackServlet extends HttpServlet {
 				return;
 
 			} catch (ServiceNotEnabledException e) {
-				rsp.sendError(SC_FORBIDDEN);
+				RepositoryFilter.sendError(SC_FORBIDDEN, req, rsp);
 				return;
 			}
 
