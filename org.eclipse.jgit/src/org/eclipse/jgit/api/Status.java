87c79e708c4282aacc6ce208c530fb7d5b4c9016
Christian Halstrick <christian.halstrick@sap.com>
2011-03-23 00:06:07 +0100

    Add status command to GIT api
    Allow users of the GIT api to get to know the state of their
    workingtree and index by adding a status command. The implementation
    is mainly a wrapper around IndexDiff class. Better support for multiple
    stages in the index (conflict situations) is still missing. An
    appropriate change to IndexDiff and StatusCommand will come in a
    subsequent commit.
    
    Change-Id: Idb390375a68611853c1c903299ec678c89b081dc
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/Status.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2011, Christian Halstrick <christian.halstrick@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.IndexDiff;
+
+/**
+ * A class telling where the working-tree, the index and the current HEAD differ
+ * from each other. Collections are exposed containing the paths of the modified
+ * files. E.g. to find out which files are dirty in the working tree (modified
+ * but not added) you would inspect the collection returned by
+ * {@link #getModified()}.
+ * <p>
+ * The same path can be returned by multiple getters. E.g. if a modification has
+ * been added to the index and afterwards the corresponding working tree file is
+ * again modified this path will be returned by {@link #getModified()} and
+ * {@link #getChanged()}
+ */
+public class Status {
+	private IndexDiff diff;
+
+	/**
+	 * @param diff
+	 */
+	public Status(IndexDiff diff) {
+		super();
+		this.diff = diff;
+	}
+
+	/**
+	 * @return list of files added to the index, not in HEAD (e.g. what you get
+	 *         if you call 'git add ...' on a newly created file)
+	 */
+	public Set<String> getAdded() {
+		return Collections.unmodifiableSet(diff.getAdded());
+	}
+
+	/**
+	 * @return list of files changed from HEAD to index (e.g. what you get if
+	 *         you modify an existing file and call 'git add ...' on it)
+	 */
+	public Set<String> getChanged() {
+		return Collections.unmodifiableSet(diff.getChanged());
+	}
+
+	/**
+	 * @return list of files removed from index, but in HEAD (e.g. what you get
+	 *         if you call 'git rm ...' on a existing file)
+	 */
+	public Set<String> getRemoved() {
+		return Collections.unmodifiableSet(diff.getRemoved());
+	}
+
+	/**
+	 * @return list of files in index, but not filesystem (e.g. what you get if
+	 *         you call 'rm ...' on a existing file)
+	 */
+	public Set<String> getMissing() {
+		return Collections.unmodifiableSet(diff.getMissing());
+	}
+
+	/**
+	 * @return list of files modified on disk relative to the index (e.g. what
+	 *         you get if you modify an existing file without adding it to the
+	 *         index)
+	 */
+	public Set<String> getModified() {
+		return Collections.unmodifiableSet(diff.getModified());
+	}
+
+	/**
+	 * @return list of files that are not ignored, and not in the index. (e.g.
+	 *         what you get if you create a new file without adding it to the
+	 *         index)
+	 */
+	public Set<String> getUntracked() {
+		return Collections.unmodifiableSet(diff.getUntracked());
+	}
+}
