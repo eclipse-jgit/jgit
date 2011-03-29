0f9bf6bdd46789120867e1cac29e72e82d3c464d
Chris Aniszczyk <caniszczyk@gmail.com>
2011-03-29 16:56:58 -0500

    Add NotesCommand to the JGit API
    Bug: 334765
    Change-Id: Ie0fb79671e7a741eb85651f507b8ade930bfcc20
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/ListNotesCommand.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2011, Chris Aniszczyk <caniszczyk@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.notes.Note;
+import org.eclipse.jgit.notes.NoteMap;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/**
+ * List object notes.
+ *
+ * @see <a href="http://www.kernel.org/pub/software/scm/git/docs/git-notes.html"
+ *      >Git documentation about Notes</a>
+ */
+public class ListNotesCommand extends GitCommand<List<Note>> {
+
+	private String notesRef = Constants.R_NOTES_COMMITS;
+
+	/**
+	 * @param repo
+	 */
+	protected ListNotesCommand(Repository repo) {
+		super(repo);
+	}
+
+	/**
+	 * @throws JGitInternalException
+	 *             upon internal failure
+	 */
+	public List<Note> call() throws JGitInternalException {
+		checkCallable();
+		List<Note> notes = new ArrayList<Note>();
+		RevWalk walk = new RevWalk(repo);
+		NoteMap map = NoteMap.newEmptyMap();
+		try {
+			Ref ref = repo.getRef(notesRef);
+			// if we have a notes ref, use it
+			if (ref != null) {
+				RevCommit notesCommit = walk.parseCommit(ref.getObjectId());
+				map = NoteMap.read(walk.getObjectReader(), notesCommit);
+			}
+
+			Iterator<Note> i = map.iterator();
+			while (i.hasNext())
+				notes.add(i.next());
+		} catch (IOException e) {
+			throw new JGitInternalException(e.getMessage(), e);
+		} finally {
+			walk.release();
+		}
+
+		return notes;
+	}
+
+	/**
+	 * @param notesRef
+	 *            the ref to read notes from. Note, the default value of
+	 *            {@link Constants#R_NOTES_COMMITS} will be used if nothing is
+	 *            set
+	 * @return {@code this}
+	 *
+	 * @see Constants#R_NOTES_COMMITS
+	 */
+	public ListNotesCommand setNotesRef(String notesRef) {
+		checkCallable();
+		this.notesRef = notesRef;
+		return this;
+	}
+
+}
