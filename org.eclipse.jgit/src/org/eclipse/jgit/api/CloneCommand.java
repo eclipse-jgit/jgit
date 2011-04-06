b7a4512f06b8dea3abb403b865cfc89ce71fd8e1
Stefan Lay <stefan.lay@sap.com>
2011-04-06 14:08:25 +0200

    Add parameters for timeout and branches to clone
    The timeout is also used in the FetchCommand called by the
    CloneCommand. 
    
    The possibility to provide a list of branches to fetch initially is a
    feature offered by EGit. To implement it here is a prerequisite for
    EGit to be able to use the CloneCommand.
    
    Change-Id: I21453de22e9ca61919a7c3386fcc526024742f5f
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
@@ -45,6 +45,9 @@ package org.eclipse.jgit.api;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
@@ -90,6 +93,12 @@ public class CloneCommand implements Callable<Git> {
 	private ProgressMonitor monitor = NullProgressMonitor.INSTANCE;
 
 	private CredentialsProvider credentialsProvider;
+
+	private int timeout;
+
+	private boolean cloneAllBranches;
+
+	private Collection<String> branchesToClone;
 
 	/**
 	 * Executes the {@code Clone} command.
@@ -146,9 +155,30 @@ public class CloneCommand implements Callable<Git> {
 		command.setRemote(remote);
 		command.setProgressMonitor(monitor);
 		command.setTagOpt(TagOpt.FETCH_TAGS);
+		command.setTimeout(timeout);
 		if (credentialsProvider != null)
 			command.setCredentialsProvider(credentialsProvider);
+
+		List<RefSpec> specs = calculateRefSpecs(dst);
+		command.setRefSpecs(specs);
+
 		return command.call();
+	}
+
+	private List<RefSpec> calculateRefSpecs(final String dst) {
+		RefSpec wcrs = new RefSpec();
+		wcrs = wcrs.setForceUpdate(true);
+		wcrs = wcrs.setSourceDestination(Constants.R_HEADS + "*", dst + "/*"); //$NON-NLS-1$ //$NON-NLS-2$
+		List<RefSpec> specs = new ArrayList<RefSpec>();
+		if (cloneAllBranches)
+			specs.add(wcrs);
+		else if (branchesToClone != null
+				&& branchesToClone.size() > 0) {
+			for (final String selectedRef : branchesToClone)
+				if (wcrs.matchSource(selectedRef))
+					specs.add(wcrs.expandFromSource(selectedRef));
+		}
+		return specs;
 	}
 
 	private void checkout(Repository repo, FetchResult result)
@@ -303,6 +333,38 @@ public class CloneCommand implements Callable<Git> {
 	public CloneCommand setCredentialsProvider(
 			CredentialsProvider credentialsProvider) {
 		this.credentialsProvider = credentialsProvider;
+		return this;
+	}
+
+	/**
+	 * @param timeout
+	 *            the timeout used for the fetch step
+	 * @return {@code this}
+	 */
+	public CloneCommand setTimeout(int timeout) {
+		this.timeout = timeout;
+		return this;
+	}
+
+	/**
+	 * @param cloneAllBranches
+	 *            true when all branches have to be fetched (indicates wildcard
+	 *            in created fetch refspec), false otherwise.
+	 * @return {@code this}
+	 */
+	public CloneCommand setCloneAllBranches(boolean cloneAllBranches) {
+		this.cloneAllBranches = cloneAllBranches;
+		return this;
+	}
+
+	/**
+	 * @param branchesToClone
+	 *            collection of branches to clone. Ignored when allSelected is
+	 *            true.
+	 * @return {@code this}
+	 */
+	public CloneCommand setBranchesToClone(Collection<String> branchesToClone) {
+		this.branchesToClone = branchesToClone;
 		return this;
 	}
 
