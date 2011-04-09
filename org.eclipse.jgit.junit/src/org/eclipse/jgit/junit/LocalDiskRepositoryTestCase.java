c9e6097d687b21fc9af6790ce8d918efdcac7197
Matthias Sohn <matthias.sohn@sap.com>
2011-04-10 01:00:31 +0200

    Create all test data in trash folder
    This ensures that all test data is separated from project sources and cleaned up after the test. Previously the cloned bare test repository
    was created in org.eclipse.jgit.test/ and not deleted after the test
    run.
    
    Change-Id: I55110442e365fc8fe610f1c372f72a71ee6e1412
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
@@ -293,15 +293,29 @@ public abstract class LocalDiskRepositoryTestCase {
 	 *             the repository could not be created in the temporary area
 	 */
 	private FileRepository createRepository(boolean bare) throws IOException {
-		String uniqueId = System.currentTimeMillis() + "_" + (testCount++);
-		String gitdirName = "test" + uniqueId + (bare ? "" : "/") + Constants.DOT_GIT;
-		File gitdir = new File(trash, gitdirName).getCanonicalFile();
+		File gitdir = createUniqueTestGitDir(bare);
 		FileRepository db = new FileRepository(gitdir);
-
 		assertFalse(gitdir.exists());
 		db.create();
 		toClose.add(db);
 		return db;
+	}
+
+	/**
+	 * Creates a new unique directory for a test repository
+	 *
+	 * @param bare
+	 *            true for a bare repository; false for a repository with a
+	 *            working directory
+	 * @return a unique directory for a test repository
+	 * @throws IOException
+	 */
+	protected File createUniqueTestGitDir(boolean bare) throws IOException {
+		String uniqueId = System.currentTimeMillis() + "_" + (testCount++);
+		String gitdirName = "test" + uniqueId + (bare ? "" : "/")
+				+ Constants.DOT_GIT;
+		File gitdir = new File(trash, gitdirName).getCanonicalFile();
+		return gitdir;
 	}
 
 	/**
