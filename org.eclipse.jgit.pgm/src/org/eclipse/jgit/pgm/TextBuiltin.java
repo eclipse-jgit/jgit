811cc926b2497a5a736c626c7ea6134561bc6671
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2011-05-06 16:40:33 +0200

    org.eclipse.jgit.pgm.Diff gives strange result
    Change-Id: I1f0dd9f8062f2148a7c1341c9122202e082ad19d
    bug: 344975

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/TextBuiltin.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/TextBuiltin.java
@@ -83,7 +83,7 @@ public abstract class TextBuiltin {
 	protected PrintWriter out;
 
 	/** Git repository the command was invoked within. */
-	protected Repository db;
+	public Repository db;
 
 	/** Directory supplied via --git-dir command line option. */
 	protected String gitdir;
