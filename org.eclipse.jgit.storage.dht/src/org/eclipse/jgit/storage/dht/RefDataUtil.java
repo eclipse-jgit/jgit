b8c508e54d64628660aa0d907b37a9648923c1f5
Shawn O. Pearce <spearce@spearce.org>
2011-05-13 09:16:58 -0700

    DHT: Add sequence RefData
    RefData now uses a sequence number as part of the field, ensuring
    that updates always increase the sequence number by one whenever
    a reference is modified.
    
    Attaching a sequence number to RefData will help with storing
    reference log entries during updates. As the sequence number should
    be unique within the reference name space, log entries can be keyed
    by the sequence number and remain unique.  Making this work over
    reference delete-create cycles will require an additional RefTable
    API to return the oldest sequence number previously used in the
    reference log to seed the recreated reference.
    
    Change-Id: I11cfff2a96ef962e57f29925a3eef41bdbf9f9bb
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.jgit.storage.dht/src/org/eclipse/jgit/storage/dht/RefDataUtil.java
+++ org.eclipse.jgit.storage.dht/src/org/eclipse/jgit/storage/dht/RefDataUtil.java
@@ -46,69 +46,13 @@ package org.eclipse.jgit.storage.dht;
 import org.eclipse.jgit.generated.storage.dht.proto.GitStore.RefData;
 import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
 
 /** Tools to work with {@link RefData}. */
 public class RefDataUtil {
 	/** Magic constant meaning does not exist. */
-	public static final RefData NONE = RefData.newBuilder().build();
-
-	static RefData symbolic(String target) {
-		RefData.Builder b = RefData.newBuilder();
-		b.setSymref(target);
-		return b.build();
-	}
-
-	static RefData id(AnyObjectId id) {
-		RefData.Builder b = RefData.newBuilder();
-		b.setTarget(toRefData(id));
-		return b.build();
-	}
-
-	static RefData fromRef(Ref ref) {
-		if (ref.isSymbolic())
-			return symbolic(ref.getTarget().getName());
-
-		if (ref.getObjectId() == null)
-			return NONE;
-
-		RefData.Builder b = RefData.newBuilder();
-		b.setTarget(toRefData(ref.getObjectId()));
-		if (ref.isPeeled()) {
-			b.setIsPeeled(true);
-			if (ref.getPeeledObjectId() != null)
-				b.setPeeled(toRefData(ref.getPeeledObjectId()));
-		}
-		return b.build();
-	}
-
-	static RefData peeled(ObjectId targetId, ObjectId peeledId) {
-		RefData.Builder b = RefData.newBuilder();
-		b.setTarget(toRefData(targetId));
-		b.setIsPeeled(true);
-		if (peeledId != null)
-			b.setPeeled(toRefData(peeledId));
-		return b.build();
-	}
-
-	private static RefData.Id toRefData(AnyObjectId id) {
-		RefData.Id.Builder r = RefData.Id.newBuilder();
-		r.setObjectName(id.name());
-		if (id instanceof IdWithChunk)
-			r.setChunkKey(((IdWithChunk) id).getChunkKey().asString());
-		return r.build();
-	}
+	public static final RefData NONE = RefData.newBuilder().buildPartial();
 
 	static class IdWithChunk extends ObjectId {
-		static ObjectId create(RefData.Id src) {
-			if (src.hasChunkKey()) {
-				return new IdWithChunk(
-						ObjectId.fromString(src.getObjectName()),
-						ChunkKey.fromString(src.getChunkKey()));
-			}
-			return ObjectId.fromString(src.getObjectName());
-		}
-
 		private final ChunkKey chunkKey;
 
 		IdWithChunk(AnyObjectId id, ChunkKey key) {
