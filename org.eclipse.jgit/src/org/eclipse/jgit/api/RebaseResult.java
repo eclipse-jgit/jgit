84bbbfc4a7090af2cf82de648b4bb6bc65fb15bb
Bernard Leach <leachbj@bouncycastle.org>
2011-05-14 21:36:10 +1000

    Implement rebase fast forward for upstream branches with merge commits
    Change Ib9898fe0f982fa08e41f1dca9452c43de715fdb6 added support for
    the 'cherry-pick' fast forward case where the upstream commit history
    does not include any merge commits.  This change adds support for the
    case where merge commits exist and the local branch has no changes.
    
    Bug: 344779
    Change-Id: If203ce5aa1b4e5d4d7982deb621b710e71f4ee10

--- org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
@@ -82,8 +82,15 @@ public class RebaseResult {
 		FAST_FORWARD;
 	}
 
+	static final RebaseResult OK_RESULT = new RebaseResult(Status.OK);
+
+	static final RebaseResult ABORTED_RESULT = new RebaseResult(Status.ABORTED);
+
 	static final RebaseResult UP_TO_DATE_RESULT = new RebaseResult(
 			Status.UP_TO_DATE);
+
+	static final RebaseResult FAST_FORWARD_RESULT = new RebaseResult(
+			Status.FAST_FORWARD);
 
 	static final RebaseResult FAST_FORWARD_RESULT = new RebaseResult(
 			Status.FAST_FORWARD);
@@ -94,7 +101,7 @@ public class RebaseResult {
 
 	private Map<String, MergeFailureReason> failingPaths;
 
-	RebaseResult(Status status) {
+	private RebaseResult(Status status) {
 		this.mySatus = status;
 		currentCommit = null;
 	}
