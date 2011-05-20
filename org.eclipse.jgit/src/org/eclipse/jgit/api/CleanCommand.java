83239ea654ee72f4d77b3b07462c99611808aa16
Abhishek Bhatnagar <abhatnag@redhat.com>
2011-05-20 10:01:15 -0400

    First submission of CleanCommand

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CleanCommand.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package org.eclipse.jgit.api;
+
+import org.eclipse.jgit.api.GitCommand;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * @author Abhishek Bhatnagar
+ *
+ */
+public class CleanCommand extends GitCommand {
+
+	/**
+	 * @param repo
+	 */
+	protected CleanCommand(Repository repo) {
+		super(repo);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
