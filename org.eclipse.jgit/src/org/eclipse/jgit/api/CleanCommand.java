08f0e1ffed11012e5069945eb730d44f0adbcd68
Abhishek Bhatnagar <abhatnag@redhat.com>
2011-05-20 12:01:37 -0400

    Add CleanCommand to JGit API
    Bug: 334767
    Change-Id: I2650c77bc4af2d8f401bbcce5384b44ff2a64ba2
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CleanCommand.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2011, Chris Aniszczyk <zx@redhat.com>
+ * Copyright (C) 2011, Abhishek Bhatnagar <abhatnag@redhat.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.util.FileUtils;
+
+/**
+ * Remove untracked files from the working tree
+ *
+ * @see <a
+ *      href="http://www.kernel.org/pub/software/scm/git/docs/git-clean.html"
+ *      >Git documentation about Clean</a>
+ */
+public class CleanCommand extends GitCommand<Set<String>> {
+
+	private Set<String> paths = Collections.emptySet();
+
+	/**
+	 * @param repo
+	 */
+	protected CleanCommand(Repository repo) {
+		super(repo);
+	}
+
+	/**
+	 * Executes the {@code clean} command with all the options and parameters
+	 * collected by the setter methods of this class. Each instance of this
+	 * class should only be used for one invocation of the command (means: one
+	 * call to {@link #call()})
+	 *
+	 * @return a set of strings representing each file cleaned.
+	 */
+	public Set<String> call() {
+		Set<String> files = Collections.emptySet();
+		try {
+			StatusCommand command = new StatusCommand(repo);
+			Status status = command.call();
+			files = status.getUntracked();
+
+			for (String file : files) {
+				if (paths.isEmpty() || paths.contains(file))
+					FileUtils.delete(new File(repo.getWorkTree(), file));
+			}
+		} catch (IOException e) {
+			throw new JGitInternalException(e.getMessage(), e);
+		}
+		return files;
+	}
+
+	/**
+	 * If paths are set, only these paths are affected by the cleaning.
+	 *
+	 * @param paths
+	 *            the paths to set
+	 * @return {@code this}
+	 */
+	public CleanCommand setPaths(Set<String> paths) {
+		this.paths = paths;
+		return this;
+	}
+
+}
