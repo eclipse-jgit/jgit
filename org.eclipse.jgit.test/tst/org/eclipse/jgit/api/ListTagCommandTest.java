2b8d90c71e050f8efc6cd0698a2a76c63363829b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2011-05-23 12:06:52 +0530

    Add a command to list all tags (aka. git-tag)
    Change-Id: I11e019f3c19b4340ac7160ac8fcbadd52499d322

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/ListTagCommandTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2011, Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListTagCommandTest extends RepositoryTestCase {
+
+	private Git git;
+
+	@Before
+	public void before() {
+		git = new Git(db);
+	}
+
+	@Test
+	public void shouldNotBlowUpIfThereAreNoTagsInRepository() throws Exception {
+		git.add().addFilepattern("*").call();
+		git.commit().setMessage("initial commit").call();
+		List<Ref> list = git.tagList().call();
+		assertEquals(0, list.size());
+	}
+
+	@Test
+	public void shouldNotBlowUpIfThereAreNoCommitsInRepository()
+			throws Exception {
+		List<Ref> list = git.tagList().call();
+		assertEquals(0, list.size());
+	}
+
+	@Test
+	public void shouldListAllTagsInRepositoryInOrder() throws Exception {
+		git.add().addFilepattern("*").call();
+		git.commit().setMessage("initial commit").call();
+
+		git.tag().setName("v3").call();
+		git.tag().setName("v2").call();
+		git.tag().setName("v10").call();
+
+		List<Ref> list = git.tagList().call();
+
+		assertEquals(3, list.size());
+		assertEquals(
+				Arrays.asList(db.getRef("refs/tags/v10"),
+						db.getRef("refs/tags/v2"), db.getRef("refs/tags/v3")),
+				list);
+	}
+}
