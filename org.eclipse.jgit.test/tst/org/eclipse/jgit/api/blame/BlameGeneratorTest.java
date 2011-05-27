ea6c87d212704da80f9103a4b8ed08c645e006be
Shawn O. Pearce <spearce@spearce.org>
2011-05-27 15:27:17 -0700

    blame: Compute the origin of lines in a result file
    BlameGenerator digs through history and discovers the origin of each
    line of some result file.  BlameResult consumes the stream of regions
    created by the generator and lays them out in a table for applications
    to display alongside of source lines.
    
    Applications may optionally push in the working tree copy of a file
    using the push(String, byte[]) method, allowing the application to
    receive accurate line annotations for the working tree version.  Lines
    that are uncommitted (difference between HEAD and working tree) will
    show up with the description given by the application as the author,
    or "Not Committed Yet" as a default string.
    
    Applications may also run the BlameGenerator in reverse mode using the
    reverse(AnyObjectId, AnyObjectId) method instead of push(). When
    running in the reverse mode the generator annotates lines by the
    commit they are removed in, rather than the commit they were added in.
    This allows a user to discover where a line disappeared from when they
    are looking at an older revision in the repository.
    
    This is a basic implementation.  Quite notably it is missing support
    for the smart block copy/move detection that the C implementation of
    `git blame` is well known for.  Despite being incremental, the
    BlameGenerator can only be run once.  After the generator runs it
    cannot be reused.  A better implementation would support applications
    browsing through history efficiently.
    
    CQ: 5110  [sp: note very little survives]
    Bug: 306161
    Change-Id: I84b8ea4838bb7d25f4fcdd540547884704661b8f
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/blame/BlameGeneratorTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2011, GitHub Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api.blame;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.blame.BlameGenerator;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+
+/** Unit tests of {@link BlameGenerator}. */
+public class BlameGeneratorTest extends RepositoryTestCase {
+	@Test
+	public void testBoundLineDelete() throws Exception {
+		Git git = new Git(db);
+
+		String[] content1 = new String[] { "first", "second" };
+		writeTrashFile("file.txt", join(content1));
+		git.add().addFilepattern("file.txt").call();
+		RevCommit c1 = git.commit().setMessage("create file").call();
+
+		String[] content2 = new String[] { "third", "first", "second" };
+		writeTrashFile("file.txt", join(content2));
+		git.add().addFilepattern("file.txt").call();
+		RevCommit c2 = git.commit().setMessage("create file").call();
+
+		BlameGenerator generator = new BlameGenerator(db, "file.txt");
+		try {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(3, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c2, generator.getSourceCommit());
+			assertEquals(1, generator.getRegionLength());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(1, generator.getResultEnd());
+			assertEquals(0, generator.getSourceStart());
+			assertEquals(1, generator.getSourceEnd());
+			assertEquals("file.txt", generator.getSourcePath());
+
+			assertTrue(generator.next());
+			assertEquals(c1, generator.getSourceCommit());
+			assertEquals(2, generator.getRegionLength());
+			assertEquals(1, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+			assertEquals(0, generator.getSourceStart());
+			assertEquals(2, generator.getSourceEnd());
+			assertEquals("file.txt", generator.getSourcePath());
+
+			assertFalse(generator.next());
+		} finally {
+			generator.release();
+		}
+	}
+
+	@Test
+	public void testLinesAllDeletedShortenedWalk() throws Exception {
+		Git git = new Git(db);
+
+		String[] content1 = new String[] { "first", "second", "third" };
+
+		writeTrashFile("file.txt", join(content1));
+		git.add().addFilepattern("file.txt").call();
+		git.commit().setMessage("create file").call();
+
+		String[] content2 = new String[] { "" };
+
+		writeTrashFile("file.txt", join(content2));
+		git.add().addFilepattern("file.txt").call();
+		git.commit().setMessage("create file").call();
+
+		writeTrashFile("file.txt", join(content1));
+		git.add().addFilepattern("file.txt").call();
+		RevCommit c3 = git.commit().setMessage("create file").call();
+
+		BlameGenerator generator = new BlameGenerator(db, "file.txt");
+		try {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(3, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c3, generator.getSourceCommit());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+
+			assertFalse(generator.next());
+		} finally {
+			generator.release();
+		}
+	}
+
+	private static String join(String... lines) {
+		StringBuilder joined = new StringBuilder();
+		for (String line : lines)
+			joined.append(line).append('\n');
+		return joined.toString();
+	}
+}
