2cebb7dbc712898197029881a8c1f41628e51804
Chris Aniszczyk <caniszczyk@gmail.com>
2011-06-19 09:17:21 -0500

    Add ReflogCommand
    Adds a git-reflog command and associated tests.
    
    Bug: 347859
    Change-Id: Iba146ac842cc9ca0be43d3381b4082c9e92bf56f
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/ReflogCommand.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2011, Chris Aniszczyk <caniszczyk@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Collection;
+
+import org.eclipse.jgit.JGitText;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.ReflogEntry;
+import org.eclipse.jgit.storage.file.ReflogReader;
+
+/**
+ * The reflog command
+ *
+ * @see <a
+ *      href="http://www.kernel.org/pub/software/scm/git/docs/git-reflog.html"
+ *      >Git documentation about reflog</a>
+ */
+public class ReflogCommand extends GitCommand<Collection<ReflogEntry>> {
+
+	private String ref = Constants.R_HEADS + Constants.MASTER;
+
+	/**
+	 * @param repo
+	 */
+	public ReflogCommand(Repository repo) {
+		super(repo);
+	}
+
+	/**
+	 * The ref used for the reflog operation. If no ref is set, the default
+	 * value of refs/heads/master will be used.
+	 *
+	 * @param ref
+	 * @return {@code this}
+	 */
+	public ReflogCommand setRef(String ref) {
+		checkCallable();
+		this.ref = ref;
+		return this;
+	}
+
+	public Collection<ReflogEntry> call() throws Exception {
+		checkCallable();
+
+		try {
+			ReflogReader reader = new ReflogReader(repo, ref);
+			return reader.getReverseEntries();
+		} catch (IOException e) {
+			throw new InvalidRemoteException(MessageFormat.format(
+					JGitText.get().cannotRead, ref));
+		}
+	}
+
+}
