078d58adbfb32dcd6bf4f76c6ea7bf396a130a6b
Shawn O. Pearce <spearce@spearce.org>
2011-07-01 18:31:53 -0700

    DFS: A storage layer for JGit
    In practice the DHT storage layer has not been performing as well as
    large scale server environments want to see from a Git server.
    
    The performance of the DHT schema degrades rapidly as small changes
    are pushed into the repository due to the chunk size being less than
    1/3 of the pushed pack size.  Small chunks cause poor prefetch
    performance during reading, and require significantly longer prefetch
    lists inside of the chunk meta field to work around the small size.
    
    The DHT code is very complex (>17,000 lines of code) and is very
    sensitive to the underlying database round-trip time, as well as the
    way objects were written into the pack stream that was chunked and
    stored on the database.  A poor pack layout (from any version of C Git
    prior to Junio reworking it) can cause the DHT code to be unable to
    enumerate the objects of the linux-2.6 repository in a completable
    time scale.
    
    Performing a clone from a DHT stored repository of 2 million objects
    takes 2 million row lookups in the DHT to locate the OBJECT_INDEX row
    for each object being cloned. This is very difficult for some DHTs to
    scale, even at 5000 rows/second the lookup stage alone takes 6 minutes
    (on local filesystem, this is almost too fast to bother measuring).
    Some servers like Apache Cassandra just fall over and cannot complete
    the 2 million lookups in rapid fire.
    
    On a ~400 MiB repository, the DHT schema has an extra 25 MiB of
    redundant data that gets downloaded to the JGit process, and that is
    before you consider the cost of the OBJECT_INDEX table also being
    fully loaded, which is at least 223 MiB of data for the linux kernel
    repository.  In the DHT schema answering a `git clone` of the ~400 MiB
    linux kernel needs to load 248 MiB of "index" data from the DHT, in
    addition to the ~400 MiB of pack data that gets sent to the client.
    This is 193 MiB more data to be accessed than the native filesystem
    format, but it needs to come over a much smaller pipe (local Ethernet
    typically) than the local SATA disk drive.
    
    I also never got around to writing the "repack" support for the DHT
    schema, as it turns out to be fairly complex to safely repack data in
    the repository while also trying to minimize the amount of changes
    made to the database, due to very common limitations on database
    mutation rates..
    
    This new DFS storage layer fixes a lot of those issues by taking the
    simple approach for storing relatively standard Git pack and index
    files on an abstract filesystem. Packs are accessed by an in-process
    buffer cache, similar to the WindowCache used by the local filesystem
    storage layer. Unlike the local file IO, there are some assumptions
    that the storage system has relatively high latency and no concept of
    "file handles". Instead it looks at the file more like HTTP byte range
    requests, where a read channel is a simply a thunk to trigger a read
    request over the network.
    
    The DFS code in this change is still abstract, it does not store on
    any particular filesystem, but is fairly well suited to the Amazon S3
    or Apache Hadoop HDFS. Storing packs directly on HDFS rather than
    HBase removes a layer of abstraction, as most HBase row reads turn
    into an HDFS read.
    
    Most of the DFS code in this change was blatently copied from the
    local filesystem code. Most parts should be refactored to be shared
    between the two storage systems, but right now I am hesistent to do
    this due to how well tuned the local filesystem code currently is.
    
    Change-Id: Iec524abdf172e9ec5485d6c88ca6512cd8a6eafb

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/dfs/DfsGarbageCollector.java
@@ -0,0 +1,347 @@
+/*
+ * Copyright (C) 2011, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.storage.dfs;
+
+import static org.eclipse.jgit.storage.dfs.DfsObjDatabase.PackSource.GC;
+import static org.eclipse.jgit.storage.dfs.DfsObjDatabase.PackSource.UNREACHABLE_GARBAGE;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ProgressMonitor;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.dfs.DfsObjDatabase.PackSource;
+import org.eclipse.jgit.storage.file.PackIndex;
+import org.eclipse.jgit.storage.pack.PackConfig;
+import org.eclipse.jgit.storage.pack.PackWriter;
+import org.eclipse.jgit.util.io.CountingOutputStream;
+
+/** Repack and garbage collect a repository. */
+public class DfsGarbageCollector {
+	private final DfsRepository repo;
+
+	private final DfsRefDatabase refdb;
+
+	private final DfsObjDatabase objdb;
+
+	private final List<DfsPackDescription> newPackDesc;
+
+	private final List<PackWriter.Statistics> newPackStats;
+
+	private final List<DfsPackFile> newPackList;
+
+	private DfsReader ctx;
+
+	private PackConfig packConfig;
+
+	private Map<String, Ref> refsBefore;
+
+	private List<DfsPackFile> packsBefore;
+
+	private Set<ObjectId> allHeads;
+
+	private Set<ObjectId> nonHeads;
+
+	/** Sum of object counts in {@link #packsBefore}. */
+	private long objectsBefore;
+
+	/** Sum of object counts iN {@link #newPackDesc}. */
+	private long objectsPacked;
+
+	private Set<ObjectId> tagTargets;
+
+	/**
+	 * Initialize a garbage collector.
+	 *
+	 * @param repository
+	 *            repository objects to be packed will be read from.
+	 */
+	public DfsGarbageCollector(DfsRepository repository) {
+		repo = repository;
+		refdb = repo.getRefDatabase();
+		objdb = repo.getObjectDatabase();
+		newPackDesc = new ArrayList<DfsPackDescription>(4);
+		newPackStats = new ArrayList<PackWriter.Statistics>(4);
+		newPackList = new ArrayList<DfsPackFile>(4);
+
+		packConfig = new PackConfig(repo);
+		packConfig.setIndexVersion(2);
+	}
+
+	/** @return configuration used to generate the new pack file. */
+	public PackConfig getPackConfig() {
+		return packConfig;
+	}
+
+	/**
+	 * @param newConfig
+	 *            the new configuration to use when creating the pack file.
+	 * @return {@code this}
+	 */
+	public DfsGarbageCollector setPackConfig(PackConfig newConfig) {
+		packConfig = newConfig;
+		return this;
+	}
+
+	/**
+	 * Create a single new pack file containing all of the live objects.
+	 * <p>
+	 * This method safely decides which packs can be expired after the new pack
+	 * is created by validating the references have not been modified in an
+	 * incompatible way.
+	 *
+	 * @param pm
+	 *            progress monitor to receive updates on as packing may take a
+	 *            while, depending on the size of the repository.
+	 * @return true if the repack was successful without race conditions. False
+	 *         if a race condition was detected and the repack should be run
+	 *         again later.
+	 * @throws IOException
+	 *             a new pack cannot be created.
+	 */
+	public boolean pack(ProgressMonitor pm) throws IOException {
+		if (pm == null)
+			pm = NullProgressMonitor.INSTANCE;
+		if (packConfig.getIndexVersion() != 2)
+			throw new IllegalStateException("Only index version 2");
+
+		ctx = (DfsReader) objdb.newReader();
+		try {
+			refdb.clearCache();
+			objdb.clearCache();
+
+			refsBefore = repo.getAllRefs();
+			packsBefore = Arrays.asList(objdb.getPacks());
+			if (packsBefore.isEmpty())
+				return true;
+
+			allHeads = new HashSet<ObjectId>();
+			nonHeads = new HashSet<ObjectId>();
+			tagTargets = new HashSet<ObjectId>();
+			for (Ref ref : refsBefore.values()) {
+				if (ref.isSymbolic() || ref.getObjectId() == null)
+					continue;
+				if (isHead(ref))
+					allHeads.add(ref.getObjectId());
+				else
+					nonHeads.add(ref.getObjectId());
+				if (ref.getPeeledObjectId() != null)
+					tagTargets.add(ref.getPeeledObjectId());
+			}
+			tagTargets.addAll(allHeads);
+
+			boolean rollback = true;
+			try {
+				packHeads(pm);
+				packRest(pm);
+				packGarbage(pm);
+				objdb.commitPack(newPackDesc, toPrune());
+				rollback = false;
+				return true;
+			} finally {
+				if (rollback)
+					objdb.rollbackPack(newPackDesc);
+			}
+		} finally {
+			ctx.release();
+		}
+	}
+
+	/** @return all of the source packs that fed into this compaction. */
+	public List<DfsPackDescription> getSourcePacks() {
+		return toPrune();
+	}
+
+	/** @return new packs created by this compaction. */
+	public List<DfsPackDescription> getNewPacks() {
+		return newPackDesc;
+	}
+
+	/** @return statistics corresponding to the {@link #getNewPacks()}. */
+	public List<PackWriter.Statistics> getNewPackStatistics() {
+		return newPackStats;
+	}
+
+	private List<DfsPackDescription> toPrune() {
+		int cnt = packsBefore.size();
+		List<DfsPackDescription> all = new ArrayList<DfsPackDescription>(cnt);
+		for (DfsPackFile pack : packsBefore)
+			all.add(pack.getPackDescription());
+		return all;
+	}
+
+	private void packHeads(ProgressMonitor pm) throws IOException {
+		if (allHeads.isEmpty())
+			return;
+
+		PackWriter pw = newPackWriter();
+		try {
+			pw.preparePack(pm, allHeads, Collections.<ObjectId> emptySet());
+			if (0 < pw.getObjectCount())
+				writePack(GC, pw, pm).setTips(allHeads);
+		} finally {
+			pw.release();
+		}
+	}
+
+	private void packRest(ProgressMonitor pm) throws IOException {
+		if (nonHeads.isEmpty() || objectsPacked == getObjectsBefore())
+			return;
+
+		PackWriter pw = newPackWriter();
+		try {
+			for (DfsPackFile pack : newPackList)
+				pw.excludeObjects(pack.getPackIndex(ctx));
+			pw.preparePack(pm, nonHeads, allHeads);
+			if (0 < pw.getObjectCount())
+				writePack(GC, pw, pm);
+		} finally {
+			pw.release();
+		}
+	}
+
+	private void packGarbage(ProgressMonitor pm) throws IOException {
+		if (objectsPacked == getObjectsBefore())
+			return;
+
+		// TODO(sop) This is ugly. The garbage pack needs to be deleted.
+		List<PackIndex> newIdx = new ArrayList<PackIndex>(newPackList.size());
+		for (DfsPackFile pack : newPackList)
+			newIdx.add(pack.getPackIndex(ctx));
+
+		PackWriter pw = newPackWriter();
+		try {
+			RevWalk pool = new RevWalk(ctx);
+			for (DfsPackFile oldPack : packsBefore) {
+				PackIndex oldIdx = oldPack.getPackIndex(ctx);
+				pm.beginTask("Finding garbage", (int) oldIdx.getObjectCount());
+				for (PackIndex.MutableEntry ent : oldIdx) {
+					pm.update(1);
+					ObjectId id = ent.toObjectId();
+					if (pool.lookupOrNull(id) != null || anyIndexHas(newIdx, id))
+						continue;
+
+					int type = oldPack.getObjectType(ctx, ent.getOffset());
+					pw.addObject(pool.lookupAny(id, type));
+				}
+				pm.endTask();
+			}
+			if (0 < pw.getObjectCount())
+				writePack(UNREACHABLE_GARBAGE, pw, pm);
+		} finally {
+			pw.release();
+		}
+	}
+
+	private static boolean anyIndexHas(List<PackIndex> list, AnyObjectId id) {
+		for (PackIndex idx : list)
+			if (idx.hasObject(id))
+				return true;
+		return false;
+	}
+
+	private static boolean isHead(Ref ref) {
+		return ref.getName().startsWith(Constants.R_HEADS);
+	}
+
+	private long getObjectsBefore() {
+		if (objectsBefore == 0) {
+			for (DfsPackFile p : packsBefore)
+				objectsBefore += p.getPackDescription().getObjectCount();
+		}
+		return objectsBefore;
+	}
+
+	private PackWriter newPackWriter() {
+		PackWriter pw = new PackWriter(packConfig, ctx);
+		pw.setDeltaBaseAsOffset(true);
+		pw.setReuseDeltaCommits(false);
+		pw.setTagTargets(tagTargets);
+		return pw;
+	}
+
+	private DfsPackDescription writePack(PackSource source, PackWriter pw,
+			ProgressMonitor pm) throws IOException {
+		DfsOutputStream out;
+		DfsPackDescription pack = repo.getObjectDatabase().newPack(source);
+		newPackDesc.add(pack);
+
+		out = objdb.writePackFile(pack);
+		try {
+			pw.writePack(pm, pm, out);
+		} finally {
+			out.close();
+		}
+
+		out = objdb.writePackIndex(pack);
+		try {
+			CountingOutputStream cnt = new CountingOutputStream(out);
+			pw.writeIndex(cnt);
+			pack.setIndexSize(cnt.getCount());
+		} finally {
+			out.close();
+		}
+
+		PackWriter.Statistics stats = pw.getStatistics();
+		pack.setPackStats(stats);
+		pack.setPackSize(stats.getTotalBytes());
+		pack.setObjectCount(stats.getTotalObjects());
+		pack.setDeltaCount(stats.getTotalDeltas());
+		objectsPacked += stats.getTotalObjects();
+		newPackStats.add(stats);
+		newPackList.add(DfsBlockCache.getInstance().getOrCreate(pack, null));
+		return pack;
+	}
+}
