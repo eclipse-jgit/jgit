8c87f903d7bfcbd2575f36d00cd792e06c86769a
Adrian Goerler <adrian.goerler@sap.com>
2011-07-04 18:17:10 +0200

    Cloning should fail when destination directory exists and is not empty
    In msysgit, when I try to clone into a folder that already
    contains a cloned repository it will fail with a message "fatal:
    destination path 'folder' already exists and is not an empty
    directory.". I would expect JGit to throw an exception with a similar
    message in that case.
    
    Bug: 347852
    Change-Id: If9e9919a5f92d13cf038dc470c21ee5967322dac
    Signed-off-by: Adrian Goerler <adrian.goerler@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/InitCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/InitCommandTest.java
@@ -78,6 +78,24 @@ public class InitCommandTest extends RepositoryTestCase {
 	}
 
 	@Test
+	public void testInitNonEmptyRepository() {
+		try {
+			File directory = createTempDirectory("testInitRepository2");
+			File someFile = new File(directory, "someFile");
+			someFile.createNewFile();
+			assertTrue(someFile.exists());
+			assertTrue(directory.listFiles().length > 0);
+			InitCommand command = new InitCommand();
+			command.setDirectory(directory);
+			Repository repository = command.call().getRepository();
+			addRepoToClose(repository);
+			assertNotNull(repository);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
 	public void testInitBareRepository() {
 		try {
 			File directory = createTempDirectory("testInitBareRepository");
