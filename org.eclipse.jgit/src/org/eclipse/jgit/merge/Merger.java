081ef1656ee70e8da1dc9da7b2b8afaadb450db8
Denys Digtiar <duemir@gmail.com>
2011-07-11 09:03:20 +0300

    ClassCastException in MergeCommand was fixed.
    Test were added which reproduce the ClassCastException when ours or
    theirs merge strategy is set to MergeCommand. Merger and MergeCommand
    were updated in order to avoid exception.
    
    Signed-off-by: Denys Digtiar <duemir@gmail.com>
    Change-Id: I4c1284b4e80d82638d0677a05e5d38182526d196

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
@@ -114,6 +114,31 @@ public abstract class Merger {
 			inserter = getRepository().newObjectInserter();
 		return inserter;
 	}
+	
+	/**
+	 * Merge together two tree-ish objects.
+	 * <p>
+	 * Any tree-ish may be supplied as inputs. Commits and/or tags pointing at
+	 * trees or commits may be passed as input objects.
+	 * 
+	 * @param a
+	 *            source tree to be combined together.
+	 * @param b
+	 *            source tree to be combined together.
+	 * @return true if the merge was completed without conflicts; false if the
+	 *         merge strategy cannot handle this merge or there were conflicts
+	 *         preventing it from automatically resolving all paths.
+	 * @throws IncorrectObjectTypeException
+	 *             one of the input objects is not a commit, but the strategy
+	 *             requires it to be a commit.
+	 * @throws IOException
+	 *             one or more sources could not be read, or outputs could not
+	 *             be written to the Repository.
+	 */
+	public boolean merge(final AnyObjectId a, final AnyObjectId b)
+			throws IOException {
+		return merge(new AnyObjectId[] { a, b });
+	}
 
 	/**
 	 * Merge together two or more tree-ish objects.
