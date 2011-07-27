8da617267a73d0c37ec43b3cf158b8b446f4292a
Shawn O. Pearce <spearce@spearce.org>
2011-07-27 07:55:52 -0700

    Cluster packed blobs by delta chains
    When writing out the blobs in a pack, sort first by the ObjectWalk
    enumeration order (aka recency and tree path order), but cluster all
    children of a delta behind their delta base. This matches the ordering
    used by IndexPack when it is resolving deltas, speeding up IndexPack.
    
    It also produces better locality when accessing a blob. Most blob
    reads access a delta (as the vast majority of blobs are stored in
    delta format), which then needs to inflate the base and apply the
    patch. Placing all deltas near their base improves the odds that
    a the base will be in memory, if not in the JGit buffer cache,
    at least the OS buffer cache.
    
    This change makes JGit PackWriter more closely match C Git behavior
    changed in 1b4bb16b9ec3 ('pack-objects: optimize "recency order"').
    JGit was already quite close to the new C Git behavior, and it is one
    of the things that prompted C Git to update the ordering, but JGit was
    missing the blob delta family clustering.
    
    Change-Id: Id1c9d75a5af091ecd1032dd2ea0c8eb54a94b6f6
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/ObjectToPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/ObjectToPack.java
@@ -81,6 +81,12 @@ public class ObjectToPack extends PackedObjectInfo {
 	/** Other object being packed that this will delta against. */
 	private ObjectId deltaBase;
 
+	/** If this is a delta base, first child using it. */
+	ObjectToPack deltaChild;
+
+	/** If this is a delta, next child in {@link #deltaBase}'s child list. */
+	ObjectToPack deltaNext;
+
 	/**
 	 * Bit field, from bit 0 to bit 31:
 	 * <ul>
@@ -163,6 +169,15 @@ public class ObjectToPack extends PackedObjectInfo {
 		this.deltaBase = deltaBase;
 	}
 
+	void setDeltaBase(ObjectToPack base) {
+		if (deltaBase instanceof ObjectToPack)
+			clearDeltaBase();
+
+		deltaBase = base;
+		deltaNext = base.deltaChild;
+		base.deltaChild = this;
+	}
+
 	void setCachedDelta(DeltaCache.Ref data){
 		cachedDelta = data;
 	}
@@ -175,6 +190,25 @@ public class ObjectToPack extends PackedObjectInfo {
 	}
 
 	void clearDeltaBase() {
+		if (deltaBase instanceof ObjectToPack) {
+			ObjectToPack base = (ObjectToPack) deltaBase;
+			ObjectToPack child = base.deltaChild;
+			if (child == this) {
+				base.deltaChild = deltaNext;
+			} else {
+				ObjectToPack n = child.deltaNext;
+				while (n != null) {
+					if (n == this) {
+						child.deltaNext = deltaNext;
+						break;
+					}
+					child = n;
+					n = n.deltaNext;
+				}
+			}
+			deltaNext = null;
+		}
+
 		this.deltaBase = null;
 
 		if (cachedDelta != null) {
