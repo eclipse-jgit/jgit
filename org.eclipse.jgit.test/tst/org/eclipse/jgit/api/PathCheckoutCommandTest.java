6a83030de9ff21e97bdde7f8dd1cbdb06f3a8793
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2011-08-04 13:02:52 +0200

    File checkout fails when mixed line endings are used and autocrlf=true
    Bug: 353867
    Change-Id: Ia692894034eccb3ee32ca1bdb8550660c7ee7578

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/PathCheckoutCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/PathCheckoutCommandTest.java
@@ -46,7 +46,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Before;
 import org.junit.Test;
@@ -148,4 +150,35 @@ public class PathCheckoutCommandTest extends RepositoryTestCase {
 		assertEquals("c", read(new File(db.getWorkTree(), FILE2)));
 	}
 
+	@Test
+	public void testCheckoutMixedNewlines() throws Exception {
+		// "git config core.autocrlf true"
+		StoredConfig config = git.getRepository().getConfig();
+		config.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_KEY_AUTOCRLF, true);
+		config.save();
+		// edit <FILE1>
+		File written = writeTrashFile(FILE1, "4\r\n4");
+		assertEquals("4\r\n4", read(written));
+		// "git add <FILE1>"
+		git.add().addFilepattern(FILE1).call();
+		// "git commit -m 'CRLF'"
+		git.commit().setMessage("CRLF").call();
+		// edit <FILE1>
+		written = writeTrashFile(FILE1, "4\n4");
+		assertEquals("4\n4", read(written));
+		// "git add <FILE1>"
+		git.add().addFilepattern(FILE1).call();
+		// "git checkout -- <FILE1>
+		git.checkout().addPath(FILE1).call();
+		// "git status" => clean
+		Status status = git.status().call();
+		assertEquals(0, status.getAdded().size());
+		assertEquals(0, status.getChanged().size());
+		assertEquals(0, status.getConflicting().size());
+		assertEquals(0, status.getMissing().size());
+		assertEquals(0, status.getModified().size());
+		assertEquals(0, status.getRemoved().size());
+		assertEquals(0, status.getUntracked().size());
+	}
 }
