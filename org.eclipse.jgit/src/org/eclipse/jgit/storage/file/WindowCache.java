c849790bc65213867280258ce299c927fa7de232
Carl Myers <cmyers@palantir.com>
2011-08-23 14:45:19 -0700

    Modified WindowCache to allow defaults to be set via System Properties
    * The following system properties are now available:
    org.eclipse.jgit.config.deltaBaseCacheLimit (in bytes)
    org.eclipse.jgit.config.packedGitLimit (in bytes)
    org.eclipse.jgit.config.gitOpenFiles (number of files)
    org.eclipse.jgit.config.gitWindowSize (in bytes)
    org.eclipse.jgit.config.streamFileThreshold (in bytes)
    
    In particular, streamFileThreshold should be set to be larger than the
    largest object in the repository (but small enough to fit in heap
    memory), otherwise checkouts will be very slow due to switching to the
    streaming implementation.  Clients of this library should either set
    these system properties, or call WindowCache.reconfigure with
    appropriate settings.
    
    Change-Id: I921c88c350d3849db9bdab25512f78e4030902d5

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
@@ -136,7 +136,72 @@ public class WindowCache {
 	private static volatile int streamFileThreshold;
 
 	static {
-		reconfigure(new WindowCacheConfig());
+		reconfigureWithSystemProperties(new WindowCacheConfig());
+	}
+
+	/**
+	 * Modify the configuration of the window cache using first system
+	 * properties, then falling back to defaults.
+	 * 
+	 * Uses org.eclipse.jgit.config.* properties to override certain defaults
+	 * 
+	 * Properties supported: deltaBaseCacheLimit (in bytes) packedGitLimit (in
+	 * bytes) gitOpenFiles (number of files) gitWindowSize (in bytes)
+	 * streamFileThreshold (in bytes)
+	 *
+	 * @param c
+	 *            WindowCacheConfig object to use as default settings
+	 */
+	public static void reconfigureWithSystemProperties(WindowCacheConfig c) {
+		c.setDeltaBaseCacheLimit(getSystemPropertyValueOrDefaultInteger(
+				"org.eclipse.jgit.config.deltaBaseCacheLimit",
+				c.getDeltaBaseCacheLimit()));
+
+		c.setPackedGitLimit(getSystemPropertyValueOrDefaultLong(
+				"org.eclipse.jgit.config.packagedGitLimit",
+				c.getPackedGitLimit()));
+
+		c.setPackedGitOpenFiles(getSystemPropertyValueOrDefaultInteger(
+				"org.eclipse.jgit.config.packedGitOpenFiles",
+				c.getPackedGitOpenFiles()));
+
+		c.setPackedGitWindowSize(getSystemPropertyValueOrDefaultInteger(
+				"org.eclipse.jgit.config.packedGitWindowSize",
+				c.getPackedGitWindowSize()));
+
+		c.setStreamFileThreshold(getSystemPropertyValueOrDefaultInteger(
+				"org.eclipse.jgit.config.streamFileThreshold",
+				c.getStreamFileThreshold()));
+
+		reconfigure(c);
+	}
+
+	private static int getSystemPropertyValueOrDefaultInteger(String key,
+			int defaultValue) {
+		final String propertyString = System.getProperty(key);
+		int propertyValue = defaultValue;
+		if (propertyString != null) {
+			try {
+				propertyValue = Integer.valueOf(propertyString).intValue();
+			} catch (NumberFormatException e) {
+				// return default
+			}
+		}
+		return propertyValue;
+	}
+
+	private static long getSystemPropertyValueOrDefaultLong(String key,
+			long defaultValue) {
+		final String propertyString = System.getProperty(key);
+		long propertyValue = defaultValue;
+		if (propertyString != null) {
+			try {
+				propertyValue = Long.valueOf(propertyString).longValue();
+			} catch (NumberFormatException e) {
+				// return default
+			}
+		}
+		return propertyValue;
 	}
 
 	/**
