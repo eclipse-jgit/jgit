18d8a69bfee7ff62744b19b58581e4ff71a2e2ff
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-09-04 16:34:15 +0200

    Allow commit when submodule changes are present
    We do not yet check or validate submodules, but can accept that
    someone staged a change in a submodule with other tools.
    
    Change-Id: I642ede382314bfbd1892dd509a2222885cc5350a
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
@@ -66,6 +66,7 @@ import org.eclipse.jgit.dircache.DirCacheIterator;
 import org.eclipse.jgit.errors.UnmergedPathException;
 import org.eclipse.jgit.lib.CommitBuilder;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -341,17 +342,20 @@ public class CommitCommand extends GitCommand<RevCommit> {
 					if (objectExists) {
 						dcEntry.setObjectId(fTree.getEntryObjectId());
 					} else {
-						// insert object
-						if (inserter == null)
-							inserter = repo.newObjectInserter();
+						// Do not check the content of submodule entries
+						if (!FileMode.GITLINK.equals(dcEntry.getFileMode())) {
+							// insert object
+							if (inserter == null)
+								inserter = repo.newObjectInserter();
 
-						InputStream inputStream = fTree.openEntryStream();
-						try {
-							dcEntry.setObjectId(inserter.insert(
-									Constants.OBJ_BLOB, entryLength,
-									inputStream));
-						} finally {
-							inputStream.close();
+							InputStream inputStream = fTree.openEntryStream();
+							try {
+								dcEntry.setObjectId(inserter.insert(
+										Constants.OBJ_BLOB, entryLength,
+										inputStream));
+							} finally {
+								inputStream.close();
+							}
 						}
 					}
 
