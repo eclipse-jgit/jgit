f12d639d3148b7efd9379e340d2cffd12526db20
Kevin Sawicki <kevin@github.com>
2011-09-29 12:06:10 -0700

    [RFC] Add comand support for submodules
    Adds the following commands:
        - Add
        - Init
        - Status
        - Sync
        - Update
    
    This also updates AddCommand so that file patterns added that
    are submodules can be staged in the index.
    
    Change-Id: I17c1c67b79b44abd4ea00733d6137bfe48151777
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/SubmoduleUpdateCommand.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2011, GitHub Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ProgressMonitor;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.submodule.SubmoduleWalk;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
+
+/**
+ * A class used to execute a submodule update command.
+ *
+ * @see <a
+ *      href="http://www.kernel.org/pub/software/scm/git/docs/git-submodule.html"
+ *      >Git documentation about submodules</a>
+ */
+public class SubmoduleUpdateCommand extends GitCommand<Collection<String>> {
+
+	private ProgressMonitor monitor;
+
+	private CredentialsProvider credentialsProvider;
+
+	private final Collection<String> paths;
+
+	/**
+	 * @param repo
+	 */
+	public SubmoduleUpdateCommand(final Repository repo) {
+		super(repo);
+		paths = new ArrayList<String>();
+	}
+
+	/**
+	 * The progress monitor associated with the clone operation. By default,
+	 * this is set to <code>NullProgressMonitor</code>
+	 *
+	 * @see NullProgressMonitor
+	 * @param monitor
+	 * @return this command
+	 */
+	public SubmoduleUpdateCommand setProgressMonitor(
+			final ProgressMonitor monitor) {
+		this.monitor = monitor;
+		return this;
+	}
+
+	/**
+	 * @param credentialsProvider
+	 *            the {@link CredentialsProvider} to use
+	 * @return this command
+	 */
+	public SubmoduleUpdateCommand setCredentialsProvider(
+			final CredentialsProvider credentialsProvider) {
+		this.credentialsProvider = credentialsProvider;
+		return this;
+	}
+
+	/**
+	 * Add repository-relative submodule path to initialize
+	 *
+	 * @param path
+	 * @return this command
+	 */
+	public SubmoduleUpdateCommand addPath(final String path) {
+		paths.add(path);
+		return this;
+	}
+
+	public Collection<String> call() throws JGitInternalException {
+		checkCallable();
+
+		try {
+			SubmoduleWalk generator = SubmoduleWalk.forIndex(repo);
+			if (!paths.isEmpty())
+				generator.setFilter(PathFilterGroup.createFromStrings(paths));
+			List<String> updated = new ArrayList<String>();
+			while (generator.next()) {
+				// Skip submodules not registered in .gitmodules file
+				if (generator.getModulesPath() == null)
+					continue;
+				// Skip submodules not registered in parent repository's config
+				String url = generator.getConfigUrl();
+				if (url == null)
+					continue;
+
+				Repository submoduleRepo = generator.getRepository();
+				// Clone repository is not present
+				if (submoduleRepo == null) {
+					CloneCommand clone = Git.cloneRepository();
+					clone.setURI(url);
+					clone.setDirectory(generator.getDirectory());
+					if (monitor != null)
+						clone.setProgressMonitor(monitor);
+					if (credentialsProvider != null)
+						clone.setCredentialsProvider(credentialsProvider);
+					submoduleRepo = clone.call().getRepository();
+				}
+
+				RevWalk walk = new RevWalk(submoduleRepo);
+				RevCommit commit = walk.parseCommit(generator.getObjectId());
+
+				String update = generator.getConfigUpdate();
+				if (ConfigConstants.CONFIG_KEY_MERGE.equals(update)) {
+					MergeCommand merge = new MergeCommand(submoduleRepo);
+					merge.include(commit);
+					merge.call();
+				} else if (ConfigConstants.CONFIG_KEY_REBASE.equals(update)) {
+					RebaseCommand rebase = new RebaseCommand(submoduleRepo);
+					rebase.setUpstream(commit);
+					rebase.call();
+				} else {
+					// Checkout commit referenced in parent repository's index
+					// as a detached HEAD
+					DirCacheCheckout co = new DirCacheCheckout(submoduleRepo,
+							submoduleRepo.lockDirCache(), commit.getTree());
+					co.setFailOnConflict(true);
+					co.checkout();
+					RefUpdate refUpdate = submoduleRepo.updateRef(
+							Constants.HEAD, true);
+					refUpdate.setNewObjectId(commit);
+					refUpdate.forceUpdate();
+				}
+				updated.add(generator.getPath());
+			}
+			return updated;
+		} catch (IOException e) {
+			throw new JGitInternalException(e.getMessage(), e);
+		} catch (ConfigInvalidException e) {
+			throw new JGitInternalException(e.getMessage(), e);
+		} catch (GitAPIException e) {
+			throw new JGitInternalException(e.getMessage(), e);
+		}
+	}
+}
