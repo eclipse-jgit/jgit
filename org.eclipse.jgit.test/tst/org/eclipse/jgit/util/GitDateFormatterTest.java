a1e3d307567db540e054b35b2d7c4fb58364b1ef
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-10-09 21:56:35 +0200

    Define a utility class for handling Git date formats
    Besides the formats known by git-log(1) we also add "locale" and "localelocal"
    that formats dates according to the user's locale. "locale" does not translate
    into local timezone, while localelocale does.
    
    Change-Id: I1c088dcec992c107e43f6c17be4ac9ed6eb428bf

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/util/GitDateFormatterTest.java
@@ -0,0 +1,83 @@
+package org.eclipse.jgit.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.jgit.junit.MockSystemReader;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.util.GitDateFormatter.Format;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GitDateFormatterTest {
+
+	private MockSystemReader mockSystemReader;
+
+	private PersonIdent ident;
+
+	@Before
+	public void setUp() {
+		mockSystemReader = new MockSystemReader() {
+			@Override
+			public long getCurrentTime() {
+				return 1318125997291L;
+			}
+		};
+		SystemReader.setInstance(mockSystemReader);
+		ident = RawParseUtils
+				.parsePersonIdent("A U Thor <author@example.com> 1316560165 -0400");
+	}
+
+	@Test
+	public void DEFAULT() {
+		assertEquals("Tue Sep 20 19:09:25 2011 -0400", new GitDateFormatter(
+				Format.DEFAULT).formatDate(ident));
+	}
+
+	@Test
+	public void RELATIVE() {
+		assertEquals("3 weeks ago",
+				new GitDateFormatter(Format.RELATIVE).formatDate(ident));
+	}
+
+	@Test
+	public void LOCAL() {
+		assertEquals("Tue Sep 20 19:39:25 2011", new GitDateFormatter(
+				Format.LOCAL).formatDate(ident));
+	}
+
+	@Test
+	public void ISO() {
+		assertEquals("2011-09-20 19:09:25 -0400", new GitDateFormatter(
+				Format.ISO).formatDate(ident));
+	}
+
+	@Test
+	public void RFC() {
+		assertEquals("Tue, 20 Sep 2011 19:09:25 -0400", new GitDateFormatter(
+				Format.RFC).formatDate(ident));
+	}
+
+	@Test
+	public void SHORT() {
+		assertEquals("2011-09-20",
+				new GitDateFormatter(Format.SHORT).formatDate(ident));
+	}
+
+	@Test
+	public void RAW() {
+		assertEquals("1316560165 -0400",
+				new GitDateFormatter(Format.RAW).formatDate(ident));
+	}
+
+	@Test
+	public void LOCALE() {
+		assertEquals("Sep 20, 2011 7:09:25 PM -0400", new GitDateFormatter(
+				Format.LOCALE).formatDate(ident));
+	}
+
+	@Test
+	public void LOCALELOCAL() {
+		assertEquals("Sep 20, 2011 7:39:25 PM", new GitDateFormatter(
+				Format.LOCALELOCAL).formatDate(ident));
+	}
+}
