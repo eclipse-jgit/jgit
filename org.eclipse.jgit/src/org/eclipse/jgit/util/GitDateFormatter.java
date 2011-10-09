c5c25becc3e5de94acc88ac37778b41a060bfb57
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-10-09 21:56:35 +0200

    Define a utility class for handling Git date formats
    Besides the formats known by git-log(1) we also add "locale" and "localelocal"
    that formats dates according to the user's locale. "locale" does not translate
    into local timezone, while localelocale does.
    
    Change-Id: I1c088dcec992c107e43f6c17be4ac9ed6eb428bf

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/GitDateFormatter.java
@@ -0,0 +1,155 @@
+package org.eclipse.jgit.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.eclipse.jgit.lib.PersonIdent;
+
+/**
+ * A utility for formatting dates according to the Git log.date formats plus
+ * extensions. <p< The enum {@link Format} defines the available types
+ */
+public class GitDateFormatter {
+
+	private DateFormat dateTimeInstance;
+
+	private DateFormat dateTimeInstance2;
+
+	private final Format format;
+
+	/**
+	 * Git and JGit formats
+	 */
+	static public enum Format {
+
+		/**
+		 * Git format: Time and original time zone
+		 */
+		DEFAULT,
+
+		/**
+		 * Git format: Relative time stamp
+		 */
+		RELATIVE,
+
+		/**
+		 * Git format: Date and time in locale time zone
+		 */
+		LOCAL,
+
+		/**
+		 * Git format: ISO 8601 plus time zone
+		 */
+		ISO,
+
+		/**
+		 * Git formt: RFC 2822 plus time zone
+		 */
+		RFC,
+
+		/**
+		 * Git format: YYYY-MM-DD
+		 */
+		SHORT,
+
+		/**
+		 * Git format: Seconds size 1970 in UTC plus time zone
+		 */
+		RAW,
+
+		/**
+		 * Locale dependent formatting with original time zone
+		 */
+		LOCALE,
+
+		/**
+		 * Locale dependent formatting in local time zone
+		 */
+		LOCALELOCAL
+	}
+
+	/**
+	 * Create a new Git oriented date formatter
+	 *
+	 * @param format
+	 */
+	public GitDateFormatter(Format format) {
+		this.format = format;
+		switch (format) {
+		default:
+			break;
+		case DEFAULT: // Not default:
+			dateTimeInstance = new SimpleDateFormat(
+					"EEE MMM dd HH:mm:ss yyyy Z", Locale.US);
+			break;
+		case ISO:
+			dateTimeInstance = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z",
+					Locale.US);
+			break;
+		case LOCAL:
+			dateTimeInstance = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy",
+					Locale.US);
+			break;
+		case RFC:
+			dateTimeInstance = new SimpleDateFormat(
+					"EEE, dd MMM yyyy HH:mm:ss Z", Locale.US);
+			break;
+		case SHORT:
+			dateTimeInstance = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+			break;
+		case LOCALE:
+		case LOCALELOCAL:
+			dateTimeInstance = DateFormat.getDateTimeInstance();
+			dateTimeInstance2 = new SimpleDateFormat("Z");
+			break;
+		}
+	}
+
+	/**
+	 * Format committer, author or tagger ident according to this formatter's
+	 * specification.
+	 *
+	 * @param ident
+	 * @return formatted version of date, time and time zone
+	 */
+	@SuppressWarnings("boxing")
+	public String formatDate(PersonIdent ident) {
+		switch (format) {
+		case RAW:
+			int offset = ident.getTimeZoneOffset();
+			String sign = offset < 0 ? "-" : "+";
+			int offset2;
+			if (offset < 0)
+				offset2 = -offset;
+			else
+				offset2 = offset;
+			int hours = offset2 / 60;
+			int minutes = offset2 % 60;
+			return String.format("%d %s%02d%02d",
+					ident.getWhen().getTime() / 1000, sign, hours, minutes);
+		case RELATIVE:
+			return RelativeDateFormatter.format(ident.getWhen());
+		case LOCALELOCAL:
+		case LOCAL:
+			dateTimeInstance.setTimeZone(SystemReader.getInstance()
+					.getTimeZone());
+			return dateTimeInstance.format(ident.getWhen());
+		case LOCALE:
+			TimeZone tz = ident.getTimeZone();
+			if (tz == null)
+				tz = SystemReader.getInstance().getTimeZone();
+			dateTimeInstance.setTimeZone(tz);
+			dateTimeInstance2.setTimeZone(tz);
+			return dateTimeInstance.format(ident.getWhen()) + " "
+					+ dateTimeInstance2.format(ident.getWhen());
+		default:
+			tz = ident.getTimeZone();
+			if (tz == null)
+				tz = SystemReader.getInstance().getTimeZone();
+			dateTimeInstance.setTimeZone(ident.getTimeZone());
+			return dateTimeInstance.format(ident.getWhen());
+		}
+	}
+}
