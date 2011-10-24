7ecd8e4ececf6b61766c26e2a9cb7ca11a8cffe9
Matthias Sohn <matthias.sohn@sap.com>
2011-10-24 22:05:55 +0200

    Implement Config.Entry.toString() to help debugging
    Change-Id: I86f6359d955d39ab033848b87ed39d20378d3c1f
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Config.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Config.java
@@ -1414,6 +1414,7 @@ public class Config {
 	 * The configuration file entry
 	 */
 	private static class Entry {
+
 		/**
 		 * The text content before entry
 		 */
@@ -1481,6 +1482,21 @@ public class Config {
 			if (a == null || b == null)
 				return false;
 			return a.equals(b);
+		}
+
+		@Override
+		public String toString() {
+			if (section == null)
+				return "<emtpy>";
+			StringBuilder b = new StringBuilder("[").append(section);
+			if (subsection != null)
+				b.append(".").append(subsection);
+			if (name != null)
+				b.append(".").append(name);
+			b.append("]");
+			if (value != null)
+				b.append("=").append(value);
+			return b.toString();
 		}
 	}
 
