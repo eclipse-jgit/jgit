ebd0a3af54abfd628c90dccf20d0cfdd9fba8b03
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-10-30 21:51:22 +0100

    Clean up tab usage in Directory/File conflict table
    Change-Id: I394fc1ef714c8465cbd5af9c73338b9a324ad9c4
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/DirCacheCheckoutTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/DirCacheCheckoutTest.java
@@ -467,15 +467,15 @@ public class DirCacheCheckoutTest extends RepositoryTestCase {
 	 *8    F        D       F       N         Y       N       N           Conflict
 	 *9    F        D       F       Y         N       N       N           Update
 	 *10   F        D       D                 N       N       Y           Keep
-	 *11   F		D		D				  N		  N		  N			  Conflict
-	 *12   F		F		D		Y		  N		  Y		  N			  Update
-	 *13   F		F		D		N		  N		  Y		  N			  Conflict
-	 *14   F		F		D				  N		  N		  N			  Conflict
-	 *15   0		F		D				  N		  N		  N			  Conflict
-	 *16   0		D		F		Y		  N		  N		  N			  Update
-	 *17   0		D		F		 		  N		  N		  N			  Conflict
-	 *18   F        0       D    										  Update
-	 *19   D	    0       F											  Update
+	 *11   F        D       D                 N       N       N           Conflict
+	 *12   F        F       D       Y         N       Y       N           Update
+	 *13   F        F       D       N         N       Y       N           Conflict
+	 *14   F        F       D                 N       N       N           Conflict
+	 *15   0        F       D                 N       N       N           Conflict
+	 *16   0        D       F       Y         N       N       N           Update
+	 *17   0        D       F                 N       N       N           Conflict
+	 *18   F        0       D                                             Update
+	 *19   D        0       F                                             Update
 	 */
 	@Test
 	public void testDirectoryFileSimple() throws IOException {
