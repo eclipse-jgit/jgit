4b84186b641ddef39399dfff58bc762090da8fdd
Shawn O. Pearce <spearce@spearce.org>
2011-11-16 14:52:31 -0800

    Refactor DfsReader selection of cached packs
    Make the code more clear with a simple refactoring of the boolean
    logic into a method that describes the condition we are looking
    for on each pack file. A cached pack is possible if there exists
    a tips collection, and the collection is non-empty.
    
    Change-Id: I4ac42b0622b39d159a0f4f223e291c35c71f672c

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/dfs/DfsReader.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/dfs/DfsReader.java
@@ -554,11 +554,14 @@ final class DfsReader extends ObjectReader implements ObjectReuseAsIs {
 		List<CachedPack> cached = new ArrayList<CachedPack>(packList.length);
 		for (DfsPackFile pack : packList) {
 			DfsPackDescription desc = pack.getPackDescription();
-			if (desc.getTips() == null || desc.getTips().isEmpty())
-				continue;
-			cached.add(new DfsCachedPack(pack));
+			if (canBeCachedPack(desc))
+				cached.add(new DfsCachedPack(pack));
 		}
 		return cached;
+	}
+
+	private static boolean canBeCachedPack(DfsPackDescription desc) {
+		return desc.getTips() != null && !desc.getTips().isEmpty();
 	}
 
 	public void copyPackAsIs(PackOutputStream out, CachedPack pack,
