7ed1ef953cec6835e254df0abdd8b70c5dc545c2
Kevin Sawicki <kevin@github.com>
2011-11-28 16:14:21 -0800

    Implement Serializable interface in ReflogEntry
    Change-Id: Idf798dd3981bef3dc9e17c13c12809f89089e96f

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ReflogEntry.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/ReflogEntry.java
@@ -44,6 +44,8 @@
 
 package org.eclipse.jgit.storage.file;
 
+import java.io.Serializable;
+
 import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
@@ -53,7 +55,9 @@ import org.eclipse.jgit.util.RawParseUtils;
 /**
  * Parsed reflog entry
  */
-public class ReflogEntry {
+public class ReflogEntry implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private ObjectId oldId;
 
 	private ObjectId newId;
