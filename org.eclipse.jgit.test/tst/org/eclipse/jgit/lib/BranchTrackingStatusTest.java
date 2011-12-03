1e6c4751c51eedd030c7dbdd72f054d77dc3e190
Robin Stocker <robin@nibor.org>
2011-12-03 18:28:09 +0100

    Add BranchTrackingStatus for getting remote tracking status
    This is used by EGit change I1e1caca561d1b0a0c194bfc42e64b698f42c6e6a to
    show branch status in decoration.
    
    It can also be used for providing the same output as C Git in "git
    status".
    
    Change-Id: I8d2b108c89905c3f0496f3d517879596740787c0
    Signed-off-by: Robin Stocker <robin@nibor.org>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/BranchTrackingStatusTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2011, Robin Stocker <robin@nibor.org>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.lib;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.junit.Test;
+
+public class BranchTrackingStatusTest extends RepositoryTestCase {
+	private TestRepository<Repository> util;
+
+	protected RevWalk rw;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		util = new TestRepository<Repository>(db);
+		StoredConfig config = util.getRepository().getConfig();
+		config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, "master",
+				ConfigConstants.CONFIG_KEY_REMOTE, "origin");
+		config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, "master",
+				ConfigConstants.CONFIG_KEY_MERGE, "refs/heads/master");
+		config.setString(ConfigConstants.CONFIG_REMOTE_SECTION, "origin",
+				"fetch", "+refs/heads/*:refs/remotes/origin/*");
+	}
+
+	@Test
+	public void shouldWorkInNormalCase() throws Exception {
+		RevCommit remoteTracking = util.branch("refs/remotes/origin/master")
+				.commit().create();
+		util.branch("master").commit().parent(remoteTracking).create();
+		util.branch("master").commit().create();
+
+		BranchTrackingStatus status = BranchTrackingStatus.of(
+				util.getRepository(), "master");
+		assertEquals(2, status.getAheadCount());
+		assertEquals(0, status.getBehindCount());
+		assertEquals("refs/remotes/origin/master",
+				status.getRemoteTrackingBranch());
+	}
+
+	@Test
+	public void shouldWorkWithoutMergeBase() throws Exception {
+		util.branch("refs/remotes/origin/master").commit().create();
+		util.branch("master").commit().create();
+
+		BranchTrackingStatus status = BranchTrackingStatus.of(util.getRepository(), "master");
+		assertEquals(1, status.getAheadCount());
+		assertEquals(1, status.getBehindCount());
+	}
+
+	@Test
+	public void shouldReturnNullWhenBranchDoesntExist() throws Exception {
+		BranchTrackingStatus status = BranchTrackingStatus.of(
+				util.getRepository(), "doesntexist");
+
+		assertNull(status);
+	}
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/BranchTrackingStatus.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2011, Robin Stocker <robin@nibor.org>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.lib;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.RevWalkUtils;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+
+/**
+ * Status of a branch's relation to its remote-tracking branch.
+ */
+public class BranchTrackingStatus {
+
+	/**
+	 * Compute the tracking status for the <code>branchName</code> in
+	 * <code>repository</code>.
+	 *
+	 * @param repository
+	 *            the git repository to compute the status from
+	 * @param branchName
+	 *            the local branch
+	 * @return the tracking status, or null if it is not known
+	 * @throws IOException
+	 */
+	public static BranchTrackingStatus of(Repository repository, String branchName)
+			throws IOException {
+
+		BranchConfig branchConfig = new BranchConfig(repository.getConfig(),
+				branchName);
+
+		String remoteTrackingBranch = branchConfig.getRemoteTrackingBranch();
+		if (remoteTrackingBranch == null)
+			return null;
+
+		Ref tracking = repository.getRef(remoteTrackingBranch);
+		if (tracking == null)
+			return null;
+
+		Ref local = repository.getRef(branchName);
+		if (local == null)
+			return null;
+
+		RevWalk walk = new RevWalk(repository);
+
+		RevCommit localCommit = walk.parseCommit(local.getObjectId());
+		RevCommit trackingCommit = walk.parseCommit(tracking.getObjectId());
+
+		walk.setRevFilter(RevFilter.MERGE_BASE);
+		walk.markStart(localCommit);
+		walk.markStart(trackingCommit);
+		RevCommit mergeBase = walk.next();
+
+		walk.reset();
+		walk.setRevFilter(RevFilter.ALL);
+		int aheadCount = RevWalkUtils.count(walk, localCommit, mergeBase);
+		int behindCount = RevWalkUtils.count(walk, trackingCommit, mergeBase);
+
+		return new BranchTrackingStatus(remoteTrackingBranch, aheadCount, behindCount);
+	}
+
+	private final String remoteTrackingBranch;
+
+	private final int aheadCount;
+
+	private final int behindCount;
+
+	private BranchTrackingStatus(String remoteTrackingBranch, int aheadCount,
+			int behindCount) {
+		this.remoteTrackingBranch = remoteTrackingBranch;
+		this.aheadCount = aheadCount;
+		this.behindCount = behindCount;
+	}
+
+	/**
+	 * @return full remote-tracking branch name
+	 */
+	public String getRemoteTrackingBranch() {
+		return remoteTrackingBranch;
+	}
+
+	/**
+	 * @return number of commits that the local branch is ahead of the
+	 *         remote-tracking branch
+	 */
+	public int getAheadCount() {
+		return aheadCount;
+	}
+
+	/**
+	 * @return number of commits that the local branch is behind of the
+	 *         remote-tracking branch
+	 */
+	public int getBehindCount() {
+		return behindCount;
+	}
+}
