7e56841113ae7d3d056985d4527765d33f475c9c
Christian Halstrick <christian.halstrick@gmail.com>
2011-12-06 01:37:55 +0100

    Fix history rendering not to occupy to many lanes
    There was a bug in history rendering which caused jgit to use too
    many lanes in case lanes get repositioned. Looking at commit
    90c11cbaeb83ee9b02238cbd2c0e5bcf68068772 in JGit was one example. Vadim
    Dmitriev found the problem and the solution.
    
    Bug: 365460
    Change-Id: I6024265b7a593dcfd4fc612d0baf6652a0092ff4
    Signed-off-by: Christian Halstrick <christian.halstrick@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revplot/PlotCommitList.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revplot/PlotCommitList.java
@@ -232,7 +232,9 @@ public class PlotCommitList<L extends PlotLane> extends
 			if (newPos == -1)
 				newPos = positionsAllocated++;
 			freePositions.add(commit.lane.getPosition());
+			activeLanes.remove(commit.lane);
 			commit.lane.position = newPos;
+			activeLanes.add(commit.lane);
 		}
 	}
 
