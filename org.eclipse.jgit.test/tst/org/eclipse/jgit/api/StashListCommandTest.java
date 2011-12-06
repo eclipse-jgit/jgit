68e137a29816271b521ba93be7564c44bf2adbc8
Kevin Sawicki <kevin@github.com>
2011-12-06 11:33:24 -0800

    Add command support for listing stashed commits
    Change-Id: I34a8c251b89abcdb67565ca49bee02e5e2113593

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/StashListCommandTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2011, GitHub Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.RefUpdate.Result;
+import org.eclipse.jgit.lib.RepositoryTestCase;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link StashListCommand}
+ */
+public class StashListCommandTest extends RepositoryTestCase {
+
+	@Test
+	public void noStashRef() throws Exception {
+		StashListCommand command = Git.wrap(db).stashList();
+		Collection<RevCommit> stashed = command.call();
+		assertNotNull(stashed);
+		assertTrue(stashed.isEmpty());
+	}
+
+	@Test
+	public void emptyStashReflog() throws Exception {
+		Git git = Git.wrap(db);
+		writeTrashFile("file.txt", "content");
+		git.add().addFilepattern("file.txt").call();
+		RevCommit commit = git.commit().setMessage("create file").call();
+
+		RefUpdate update = db.updateRef(Constants.R_STASH);
+		update.setNewObjectId(commit);
+		update.disableRefLog();
+		assertEquals(Result.NEW, update.update());
+
+		StashListCommand command = Git.wrap(db).stashList();
+		Collection<RevCommit> stashed = command.call();
+		assertNotNull(stashed);
+		assertTrue(stashed.isEmpty());
+	}
+
+	@Test
+	public void singleStashedCommit() throws Exception {
+		Git git = Git.wrap(db);
+		writeTrashFile("file.txt", "content");
+		git.add().addFilepattern("file.txt").call();
+		RevCommit commit = git.commit().setMessage("create file").call();
+
+		RefUpdate update = db.updateRef(Constants.R_STASH);
+		update.setNewObjectId(commit);
+		assertEquals(Result.NEW, update.update());
+
+		StashListCommand command = git.stashList();
+		Collection<RevCommit> stashed = command.call();
+		assertNotNull(stashed);
+		assertEquals(1, stashed.size());
+		assertEquals(commit, stashed.iterator().next());
+	}
+
+	@Test
+	public void multipleStashedCommits() throws Exception {
+		Git git = Git.wrap(db);
+
+		writeTrashFile("file.txt", "content");
+		git.add().addFilepattern("file.txt").call();
+		RevCommit commit1 = git.commit().setMessage("create file").call();
+
+		writeTrashFile("file.txt", "content2");
+		git.add().addFilepattern("file.txt").call();
+		RevCommit commit2 = git.commit().setMessage("edit file").call();
+
+		RefUpdate create = db.updateRef(Constants.R_STASH);
+		create.setNewObjectId(commit1);
+		assertEquals(Result.NEW, create.update());
+
+		RefUpdate update = db.updateRef(Constants.R_STASH);
+		update.setNewObjectId(commit2);
+		assertEquals(Result.FAST_FORWARD, update.update());
+
+		StashListCommand command = git.stashList();
+		Collection<RevCommit> stashed = command.call();
+		assertNotNull(stashed);
+		assertEquals(2, stashed.size());
+		Iterator<RevCommit> iter = stashed.iterator();
+		assertEquals(commit2, iter.next());
+		assertEquals(commit1, iter.next());
+	}
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/StashListCommand.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2011, GitHub Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.api;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.jgit.JGitText;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.ReflogEntry;
+
+/**
+ * Command class to list the stashed commits in a repository.
+ *
+ * @see <a href="http://www.kernel.org/pub/software/scm/git/docs/git-stash.html"
+ *      >Git documentation about Stash</a>
+ */
+public class StashListCommand extends GitCommand<Collection<RevCommit>> {
+
+	/**
+	 * Create a new stash list command
+	 *
+	 * @param repo
+	 */
+	public StashListCommand(final Repository repo) {
+		super(repo);
+	}
+
+	public Collection<RevCommit> call() throws Exception {
+		checkCallable();
+
+		try {
+			if (repo.getRef(Constants.R_STASH) == null)
+				return Collections.emptyList();
+		} catch (IOException e) {
+			throw new InvalidRefNameException(MessageFormat.format(
+					JGitText.get().cannotRead, Constants.R_STASH), e);
+		}
+
+		final ReflogCommand refLog = new ReflogCommand(repo);
+		refLog.setRef(Constants.R_STASH);
+		final Collection<ReflogEntry> stashEntries = refLog.call();
+		if (stashEntries.isEmpty())
+			return Collections.emptyList();
+
+		final List<RevCommit> stashCommits = new ArrayList<RevCommit>(
+				stashEntries.size());
+		final RevWalk walk = new RevWalk(repo);
+		walk.setRetainBody(true);
+		try {
+			for (ReflogEntry entry : stashEntries)
+				try {
+					stashCommits.add(walk.parseCommit(entry.getNewId()));
+				} catch (IOException e) {
+					throw new JGitInternalException(MessageFormat.format(
+							JGitText.get().cannotReadCommit, entry.getNewId()),
+							e);
+				}
+		} finally {
+			walk.dispose();
+		}
+		return stashCommits;
+	}
+}
