84c80be1dc45885969e2d0fa0421f709b8706453
Dave Borowitz <dborowitz@google.com>
2011-12-13 11:00:19 -0800

    Fire DfsPacksChangedEvents when committing packs.
    Once a pack has been committed with commitPack(), we know that the pack
    list has changed but we don't re-scan the underlying storage.
    
    Change-Id: Ia7b35df4442a5f5dfe7e817edcc77b44b5410d08

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/dfs/InMemoryRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/dfs/InMemoryRepository.java
@@ -79,7 +79,7 @@ public class InMemoryRepository extends DfsRepository {
 		}
 
 		@Override
-		protected synchronized void commitPack(
+		protected synchronized void commitPackImpl(
 				Collection<DfsPackDescription> desc,
 				Collection<DfsPackDescription> replace) {
 			List<DfsPackDescription> n;
