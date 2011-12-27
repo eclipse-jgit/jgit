3ae064c56824e02829f873590794ec200b07994d
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2011-12-27 22:18:56 +0100

    Skip a number commits before starting to show the commit output
    Change-Id: Id2666d897d29b6371f7a6cf241cfda02964b4971

--- org.eclipse.jgit/src/org/eclipse/jgit/api/LogCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/LogCommand.java
@@ -60,6 +60,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.filter.MaxCountRevFilter;
+import org.eclipse.jgit.revwalk.filter.SkipRevFilter;
 import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
@@ -88,6 +89,8 @@ public class LogCommand extends GitCommand<Iterable<RevCommit>> {
 
 	private int maxCount = -1;
 
+	private int skip = -1;
+
 	/**
 	 * @param repo
 	 */
@@ -111,6 +114,8 @@ public class LogCommand extends GitCommand<Iterable<RevCommit>> {
 		if (pathFilters.size() > 0)
 			walk.setTreeFilter(AndTreeFilter.create(
 					PathFilterGroup.create(pathFilters), TreeFilter.ANY_DIFF));
+		if (skip > -1)
+			walk.setRevFilter(SkipRevFilter.create(skip));
 		if (maxCount > -1)
 			walk.setRevFilter(MaxCountRevFilter.create(maxCount));
 		if (!startSpecified) {
@@ -253,7 +258,20 @@ public class LogCommand extends GitCommand<Iterable<RevCommit>> {
 	}
 
 	/**
-	 * Limit the number of commits to output
+	 * Skip the number of commits before starting to show the commit output.
+	 *
+	 * @param skip
+	 *            the number of commits to skip
+	 * @return {@code this}
+	 */
+	public LogCommand setSkip(int skip) {
+		checkCallable();
+		this.skip = skip;
+		return this;
+	}
+
+	/**
+	 * Limit the number of commits to output.
 	 *
 	 * @param maxCount
 	 *            the limit
