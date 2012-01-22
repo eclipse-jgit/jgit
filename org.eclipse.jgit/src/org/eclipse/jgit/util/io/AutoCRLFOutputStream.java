96acc4e5cfb84a181874007074b956763db347bb
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-01-23 00:57:39 +0100

    Really close output stream when core.autocrlf is true
    Change-Id: I8a0d91e908145168c32589600bd1598826511ae3

--- org.eclipse.jgit/src/org/eclipse/jgit/util/io/AutoCRLFOutputStream.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/io/AutoCRLFOutputStream.java
@@ -177,11 +177,12 @@ public class AutoCRLFOutputStream extends OutputStream {
 		if (binbufcnt < binbuf.length)
 			decideMode();
 		buf = -1;
+		out.flush();
 	}
 
 	@Override
 	public void close() throws IOException {
 		flush();
-		super.close();
+		out.close();
 	}
 }
