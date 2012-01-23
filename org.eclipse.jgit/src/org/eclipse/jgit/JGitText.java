9766ffe13de703856008ee99702e63fb029a398a
Kevin Sawicki <kevin@github.com>
2012-01-23 10:40:50 -0800

    Support submodule '.git' repository references
    A '.git' file in a submodule's working tree root is now parsed
    as a ref to a folder located elsewhere.  This supports submodules
    having their repository location outside of the parent repository's
    working directory such as in the parent repository's '.git/modules'
    directory.
    
    Change-Id: I73c538f6d845bdbc0c4e2bce5a77f900cf36e1a9

--- org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
@@ -483,6 +483,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String startingReadStageWithoutWrittenRequestDataPendingIsNotSupported;
 	/***/ public String statelessRPCRequiresOptionToBeEnabled;
 	/***/ public String submoduleExists;
+	/***/ public String submoduleInvalidGitdirRef;
 	/***/ public String submodulesNotSupported;
 	/***/ public String submoduleParentRemoteUrlInvalid;
 	/***/ public String symlinkCannotBeWrittenAsTheLinkTarget;
