29357ee1b6a11ce06cf19005b9e0dfae6a3c3c7f
Kevin Sawicki <kevin@github.com>
2012-01-30 18:39:23 -0800

    Support gitdir references in working tree .git file
    A '.git' file in a repository's working tree root is now parsed
    as a ref to a folder located elsewhere.  This supports submodules
    having their repository location outside of the parent repository's
    working directory such as in the parent repository's '.git/modules'
    directory.
    
    This adds support to BaseRepositoryBuilder for repositories created
    with the '--separate-git-dir' option specified to 'git init'.
    
    Change-Id: I73c538f6d845bdbc0c4e2bce5a77f900cf36e1a9

--- org.eclipse.jgit/src/org/eclipse/jgit/submodule/SubmoduleWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/submodule/SubmoduleWalk.java
@@ -155,27 +155,32 @@ public class SubmoduleWalk {
 	 */
 	public static Repository getSubmoduleRepository(final Repository parent,
 			final String path) throws IOException {
-		File directory = getSubmoduleGitDirectory(parent, path);
-		if (!directory.isDirectory())
-			return null;
-		try {
-			return new RepositoryBuilder().setMustExist(true)
-					.setFS(FS.DETECTED).setGitDir(directory).build();
-		} catch (RepositoryNotFoundException e) {
-			return null;
-		}
+		return getSubmoduleRepository(parent.getWorkTree(), path);
 	}
 
 	/**
-	 * Get the .git directory for a repository submodule path
+	 * Get submodule repository at path
 	 *
 	 * @param parent
 	 * @param path
-	 * @return .git for submodule repository
+	 * @return repository or null if repository doesn't exist
+	 * @throws IOException
 	 */
-	public static File getSubmoduleGitDirectory(final Repository parent,
-			final String path) {
-		return new File(getSubmoduleDirectory(parent, path), Constants.DOT_GIT);
+	public static Repository getSubmoduleRepository(final File parent,
+			final String path) throws IOException {
+		File subWorkTree = new File(parent, path);
+		if (!subWorkTree.isDirectory())
+			return null;
+		File workTree = new File(parent, path);
+		try {
+			return new RepositoryBuilder() //
+					.setMustExist(true) //
+					.setFS(FS.DETECTED) //
+					.setWorkTree(workTree) //
+					.build();
+		} catch (RepositoryNotFoundException e) {
+			return null;
+		}
 	}
 
 	/**
@@ -349,15 +354,6 @@ public class SubmoduleWalk {
 	}
 
 	/**
-	 * Get the .git directory for the current submodule entry
-	 *
-	 * @return .git for submodule repository
-	 */
-	public File getGitDirectory() {
-		return getSubmoduleGitDirectory(repository, path);
-	}
-
-	/**
 	 * Advance to next submodule in the index tree.
 	 *
 	 * The object id and path of the next entry can be obtained by calling
@@ -467,19 +463,8 @@ public class SubmoduleWalk {
 	}
 
 	/**
-	 * Does the current submodule entry have a .git directory in the parent
-	 * repository's working tree?
-	 *
-	 * @return true if .git directory exists, false otherwise
-	 */
-	public boolean hasGitDirectory() {
-		return getGitDirectory().isDirectory();
-	}
-
-	/**
 	 * Get repository for current submodule entry
 	 *
-	 * @see #hasGitDirectory()
 	 * @return repository or null if non-existent
 	 * @throws IOException
 	 */
