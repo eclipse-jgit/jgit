924dee7c28420f97a057011be84981ad1d4217ed
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-02-06 12:21:04 +0100

    Allow to get end-of-line chars for a RawText
    Bug: 370320
    Change-Id: I18adc63596f4657516ccc6d704a561924c79d445

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/RawText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/RawText.java
@@ -276,4 +276,19 @@ public class RawText extends Sequence {
 
 		return false;
 	}
+
+	/**
+	 * Get the EOL chars for the first line.
+	 *
+	 * @return the EOL chars or <code>null</code>
+	 */
+	public String getEOL() {
+		int e = getEnd(0);
+		if (content.length > 1 && content[e - 2] == '\r'
+				&& content[e - 1] == '\n')
+			return "\r\n";
+		if (content.length > 0 && content[e - 1] == '\n')
+			return "\n";
+		return null;
+	}
 }
