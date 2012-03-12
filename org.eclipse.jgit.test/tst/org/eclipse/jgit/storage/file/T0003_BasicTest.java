7aeea3b27c86e638dae2328929769703db5f1a89
Kevin Sawicki <kevin@github.com>
2012-03-12 10:00:52 -0700

    Compare repository format version as parsed long
    This allows repositoryies with a missing repositoryformatversion
    config value to be successfully opened but still throws exceptions
    when the value is a non-long or greater than zero.
    
    git-core attempts to parse this config value as a long as well
    and defaults to 0 if the value is missing.
    
    Bug: 368697
    Change-Id: I4a93117afca37e591e8e0ab4d2f2eef4273f0cc9
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/T0003_BasicTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/T0003_BasicTest.java
@@ -370,9 +370,8 @@ public class T0003_BasicTest extends SampleDataRepositoryTestCase {
 		try {
 			new FileRepository(db.getDirectory());
 			fail("incorrectly opened a bad repository");
-		} catch (IOException ioe) {
-			assertTrue(ioe.getMessage().indexOf("format") > 0);
-			assertTrue(ioe.getMessage().indexOf(badvers) > 0);
+		} catch (IllegalArgumentException ioe) {
+			assertNotNull(ioe.getMessage());
 		}
 	}
 
