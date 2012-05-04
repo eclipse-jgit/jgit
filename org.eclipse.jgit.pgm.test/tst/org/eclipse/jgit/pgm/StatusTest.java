4c6e4790c81cc599775462ce59633fb9c4da4f23
François Rey <egit@francois.rey.name>
2012-05-05 00:14:22 +0200

    Added new Status CLI command 'jgit status'
    This is a first basic implementation that displays current branch and
    list of files of various status, but isn't as refined as its native
    counterpart (e.g. does not say if we're ahead or behind the remote).
    It's been helpful in the diagnostic of bug #347885.
    
    Note that changes to CLIText.properties are not mingled with other
    lines, as to preserve alphabetical order, but are kept together because
    some padding alignment needs to be done across some of them. If later
    on one needs to change a label, other labels will need to be changed
    accordingly so that padding is respected.
    
    Changes to CLIText.java are also kept together. It's less important to
    keep them together in that file, but did so just because I kept them
    together in the twin properties file.
    
    Bug: 348318
    Change-Id: Ifc35da608fbba652524c1b5b522e3c0d5369ad5e
    Signed-off-by: François Rey <eclipse.org@francois.rey.name>

---
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/StatusTest.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2012, François Rey <eclipse.org_@_francois_._rey_._name>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.pgm;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.CLIRepositoryTestCase;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+
+public class StatusTest extends CLIRepositoryTestCase {
+	@Test
+	public void testStatus() throws Exception {
+		Git git = new Git(db);
+		// Write all files
+		writeTrashFile("tracked", "tracked");
+		writeTrashFile("stagedNew", "stagedNew");
+		writeTrashFile("stagedModified", "stagedModified");
+		writeTrashFile("stagedDeleted", "stagedDeleted");
+		writeTrashFile("trackedModified", "trackedModified");
+		writeTrashFile("trackedDeleted", "trackedDeleted");
+		writeTrashFile("untracked", "untracked");
+		// Test untracked count
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 7 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Test option -u (untracked file listing)
+		assertArrayEquals(new String[] { // git status -u output
+						"# On branch master", //
+						"# Untracked files:", //
+						"# \t            stagedDeleted", //
+						"# \t            stagedModified", //
+						"# \t            stagedNew", //
+						"# \t            tracked", //
+						"# \t            trackedDeleted", //
+						"# \t            trackedModified", //
+						"# \t            untracked", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 7 untracked.", //
+						"" //
+				}, execute("git status -u")); //
+		// Add to index
+		git.add().addFilepattern("tracked").call();
+		git.add().addFilepattern("stagedModified").call();
+		git.add().addFilepattern("stagedDeleted").call();
+		git.add().addFilepattern("trackedModified").call();
+		git.add().addFilepattern("trackedDeleted").call();
+		// Test staged count
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"# Changes to be committed:", //
+						"# \tnew file:   stagedDeleted", //
+						"# \tnew file:   stagedModified", //
+						"# \tnew file:   tracked", //
+						"# \tnew file:   trackedDeleted", //
+						"# \tnew file:   trackedModified", //
+						"Summary: 5 to commit, 0 not staged, 0 unmerged, 2 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Test option -u (untracked file listing)
+		assertArrayEquals(new String[] { // git status -u output
+						"# On branch master", //
+						"# Changes to be committed:", //
+						"# \tnew file:   stagedDeleted", //
+						"# \tnew file:   stagedModified", //
+						"# \tnew file:   tracked", //
+						"# \tnew file:   trackedDeleted", //
+						"# \tnew file:   trackedModified", //
+						"# Untracked files:", //
+						"# \t            stagedNew", //
+						"# \t            untracked", //
+						"Summary: 5 to commit, 0 not staged, 0 unmerged, 2 untracked.", //
+						"" //
+				}, execute("git status -u")); //
+		// Commit
+		git.commit().setMessage("initial commit")
+				.call();
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 2 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Make some changes and stage them
+		writeTrashFile("stagedModified", "stagedModified, really!");
+		deleteTrashFile("stagedDeleted");
+		writeTrashFile("trackedModified", "trackedModified, really!");
+		deleteTrashFile("trackedDeleted");
+		git.add().addFilepattern("stagedModified").call();
+		git.rm().addFilepattern("stagedDeleted").call();
+		git.add().addFilepattern("stagedNew").call();
+		// Test staged/not-staged status
+		assertArrayEquals(new String[] { // git status -u output
+						"# On branch master", //
+						"# Changes to be committed:", //
+						"# \tnew file:   stagedNew", //
+						"# \tmodified:   stagedModified", //
+						"# \tdeleted:    stagedDeleted", //
+						"# Changes not staged for commit:", //
+						"# \tmodified:   trackedModified", //
+						"# \tdeleted:    trackedDeleted", //
+						"# Untracked files:", //
+						"# \t            untracked", //
+						"Summary: 3 to commit, 2 not staged, 0 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status -u")); //
+		// Create unmerged file
+		writeTrashFile("unmerged", "unmerged");
+		git.add().addFilepattern("unmerged").call();
+		// Commit pending changes
+		git.add().addFilepattern("trackedModified").call();
+		git.rm().addFilepattern("trackedDeleted").call();
+		git.commit().setMessage("commit before branching").call();
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Checkout new branch
+		git.checkout().setCreateBranch(true).setName("test").call();
+		// Test branch status
+		assertArrayEquals(new String[] { // git status output
+						"# On branch test", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Commit change and checkout master again
+		writeTrashFile("unmerged", "changed in test branch");
+		git.add().addFilepattern("unmerged").call();
+		RevCommit testBranch = git.commit()
+				.setMessage("changed unmerged in test branch").call();
+		assertArrayEquals(new String[] { // git status output
+						"# On branch test", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+		git.checkout().setName("master").call();
+		// Change the same file and commit
+		writeTrashFile("unmerged", "changed in master branch");
+		git.add().addFilepattern("unmerged").call();
+		git.commit().setMessage("changed unmerged in master branch").call();
+
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"Summary: 0 to commit, 0 not staged, 0 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Merge test branch into master
+		git.merge().include(testBranch.getId()).call();
+		// Test unmerged status
+		assertArrayEquals(new String[] { // git status output
+						"# On branch master", //
+						"# Unmerged paths:", //
+						"# \t            unmerged", //
+						"Summary: 0 to commit, 0 not staged, 1 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+		// Test detached head
+		String commitId = db.getRef(Constants.MASTER).getObjectId().name();
+		git.checkout().setName(commitId).call();
+		assertArrayEquals(new String[] { // git status output
+						"# Not currently on any branch.", //
+						"# Unmerged paths:", //
+						"# \t            unmerged", //
+						"Summary: 0 to commit, 0 not staged, 1 unmerged, 1 untracked.", //
+						"" //
+				}, execute("git status")); //
+	}
+}
