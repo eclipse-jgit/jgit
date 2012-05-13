61f5f510db4e2d2342e60a9d23a0712917e4289f
François Rey <egit@francois.rey.name>
2012-05-13 09:13:56 +0200

    Added new Status CLI command 'jgit status'
    This is a first basic implementation that displays current branch and
    list of files of various status, but isn't as refined as its native
    counterpart (e.g. does not say if we're ahead or behind the remote).
    It's been helpful in the diagnostic of bug #347885.
    
    Bug: 348318
    Change-Id: Ifc35da608fbba652524c1b5b522e3c0d5369ad5e
    Signed-off-by: François Rey <eclipse.org@francois.rey.name>

--- org.eclipse.jgit.pgm.test/src/org/eclipse/jgit/lib/CLIRepositoryTestCase.java
+++ org.eclipse.jgit.pgm.test/src/org/eclipse/jgit/lib/CLIRepositoryTestCase.java
@@ -79,4 +79,8 @@ public class CLIRepositoryTestCase extends LocalDiskRepositoryTestCase {
 			throws IOException {
 		return JGitTestUtil.writeTrashFile(db, name, data);
 	}
+
+	protected void deleteTrashFile(final String name) throws IOException {
+		JGitTestUtil.deleteTrashFile(db, name);
+	}
 }
