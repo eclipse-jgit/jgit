cd7ddbb125dc3fb00c4e607a717d97418bbcfa2f
Mikael Karlsson <mmmicke@gmail.com>
2012-05-16 23:05:20 +0200

    Fix test case setup not doing what it should
    Change-Id: Ie0fe025a5ef861a3c1b4a995c68a3577ad041f00

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/BranchCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/BranchCommandTest.java
@@ -108,10 +108,10 @@ public class BranchCommandTest extends RepositoryTestCase {
 		Repository remoteRepository = createWorkRepository();
 		Git remoteGit = new Git(remoteRepository);
 		// commit something
-		writeTrashFile("Test.txt", "Hello world");
+		JGitTestUtil.writeTrashFile(remoteRepository, "Test.txt", "Hello world");
 		remoteGit.add().addFilepattern("Test.txt").call();
 		initialCommit = remoteGit.commit().setMessage("Initial commit").call();
-		writeTrashFile("Test.txt", "Some change");
+		JGitTestUtil.writeTrashFile(remoteRepository, "Test.txt", "Some change");
 		remoteGit.add().addFilepattern("Test.txt").call();
 		secondCommit = remoteGit.commit().setMessage("Second commit").call();
 		// create a master branch
