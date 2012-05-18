4479ad724ac8770aa1cb2c4e72b8495116e4ffd1
Ian Wetherbee <wetherbeei@google.com>
2012-05-17 18:30:03 -0700

    Commands for publish-subscribe config editing
    A daemon process will consume the config file and subscribe to all
    repositories and remote sections listed under each publisher section.
    Each subscriber line is built using the remote name and the local
    repository metadata directory. The "fetch" spec in each remote config
    section will be used to subscribe to remote updates.
    
    Example:
    
    [publisher "https://android.googlesource.com/"]
    	subscriber = origin /home/.../android/.git
    	subscriber = origin /home/.../android-copy/.git
    
    Change-Id: Ifd84aa8a6538277062395629f6c20be38d13ee06

---
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/SubscribeDaemon.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2012, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.pgm;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.transport.PubSubConfig;
+import org.eclipse.jgit.util.FS;
+
+@Command(common = false, usage = "usage_RunSubscribeDaemon")
+class SubscribeDaemon extends TextBuiltin {
+	/** Name of the pubsub config file */
+	protected static String GLOBAL_PUBSUB_FILE = ".gitpubsub";
+
+	/** @return the pubsub config file */
+	public static File getConfigFile() {
+		return new File(FS.detect().userHome(), GLOBAL_PUBSUB_FILE);
+	}
+
+	/**
+	 * @return the config file for this user
+	 * @throws ConfigInvalidException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static PubSubConfig getConfig()
+			throws IOException, ConfigInvalidException, URISyntaxException {
+		File cfg = getConfigFile();
+		FileBasedConfig c = new FileBasedConfig(cfg, FS.detect());
+		c.load();
+		return new PubSubConfig(c);
+	}
+
+	/**
+	 * @param pubsubConfig
+	 * @throws IOException
+	 */
+	public static void updateConfig(PubSubConfig pubsubConfig)
+			throws IOException {
+		File cfg = getConfigFile();
+		FileBasedConfig c = new FileBasedConfig(cfg, FS.detect());
+		pubsubConfig.update(c);
+		c.save();
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO(wetherbeei): fill in daemon launch
+	}
+}
