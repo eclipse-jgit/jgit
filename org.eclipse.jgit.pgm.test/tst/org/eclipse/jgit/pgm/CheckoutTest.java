ca16def92bd44ebbcaafb760c30c15495a68928d
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-05-20 12:49:54 +0200

    Output result of switching branch -- o.e.jgit.pgm.Checkout
    Change-Id: I9829950b686ce3b8c70b8f7a1774d5e2b55cd00a

--- org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/CheckoutTest.java
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/CheckoutTest.java
@@ -42,30 +42,60 @@
  */
 package org.eclipse.jgit.pgm;
 
-import static org.junit.Assert.assertEquals;
-
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.CLIRepositoryTestCase;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class BranchTest extends CLIRepositoryTestCase {
-	@Override
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
+public class CheckoutTest extends CLIRepositoryTestCase {
+
+	@Test
+	public void testCheckoutSelf() throws Exception {
 		new Git(db).commit().setMessage("initial commit").call();
+
+		assertEquals("Already on 'master'", execute("git checkout master"));
 	}
 
 	@Test
-	public void testList() throws Exception {
-		assertEquals("* master 6fd41be initial commit",
-				execute("git branch -v")[0]);
+	public void testCheckoutBranch() throws Exception {
+		new Git(db).commit().setMessage("initial commit").call();
+		new Git(db).branchCreate().setName("side").call();
+
+		assertEquals("Switched to branch 'side'", execute("git checkout side"));
 	}
 
 	@Test
-	public void testExistingBranch() throws Exception {
-		assertEquals("A branch named 'master' already exists.",
-				execute("git branch master")[0]);
+	public void testCheckoutNewBranch() throws Exception {
+		new Git(db).commit().setMessage("initial commit").call();
+
+		assertEquals("Switched to a new branch 'side'",
+				execute("git checkout -b side"));
+	}
+
+	@Test
+	public void testCheckoutNonExistingBranch() throws Exception {
+		assertEquals(
+				"error: pathspec 'side' did not match any file(s) known to git.",
+				execute("git checkout side"));
+	}
+
+	@Test
+	public void testCheckoutNewBranchThatAlreadyExists() throws Exception {
+		new Git(db).commit().setMessage("initial commit").call();
+
+		assertEquals("git checkout: branch master already exists",
+				execute("git checkout -b master"));
+	}
+
+	@Test
+	public void testCheckoutNewBranchOnBranchToBeBorn() throws Exception {
+		assertEquals("You are on a branch yet to be born",
+				execute("git checkout -b side"));
+	}
+
+	static private void assertEquals(String expected, String[] actual) {
+		Assert.assertEquals(actual[actual.length - 1].equals("") ? 2 : 1,
+				actual.length); // ignore last empty line
+		Assert.assertEquals(expected, actual[0]);
 	}
 }
