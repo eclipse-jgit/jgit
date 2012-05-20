d7a6b750554d897f0ef179eb55d9efd04824d5ef
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-05-20 12:00:46 +0200

    Typo in CLIText#unknownMergeStratey, should be #unknownMergeStrategy
    Change-Id: I4d4aa59723d6422ff755165e45f368da76d309ab

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
@@ -182,7 +182,7 @@ public class CLIText extends TranslationBundle {
 	/***/ public String skippingObject;
 	/***/ public String timeInMilliSeconds;
 	/***/ public String tooManyRefsGiven;
-	/***/ public String unknownMergeStratey;
+	/***/ public String unknownMergeStrategy;
 	/***/ public String unsupportedOperation;
 	/***/ public String warningNoCommitGivenOnCommandLine;
 }
