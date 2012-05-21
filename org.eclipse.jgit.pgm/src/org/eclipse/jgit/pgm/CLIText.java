73dad2f48c3aff85650ecda421d699baf169e93b
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-05-21 11:01:53 +0200

    Output result of switching branch -- o.e.jgit.pgm.Checkout
    Change-Id: I9829950b686ce3b8c70b8f7a1774d5e2b55cd00a

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
+ * Copyright (C) 2010, 2012 Sasa Zivkov <sasa.zivkov@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -59,6 +59,7 @@ public class CLIText extends TranslationBundle {
 	}
 
 	/***/ public String IPZillaPasswordPrompt;
+	/***/ public String alreadyOnBranch;
 	/***/ public String authorInfo;
 	/***/ public String averageMSPerRead;
 	/***/ public String branchAlreadyExists;
@@ -169,7 +170,9 @@ public class CLIText extends TranslationBundle {
 	/***/ public String notAnObject;
 	/***/ public String notFound;
 	/***/ public String noteObjectTooLargeToPrint;
+	/***/ public String onBranchToBeBorn;
 	/***/ public String onlyOneMetaVarExpectedIn;
+	/***/ public String pathspecDidNotMatch;
 	/***/ public String pushTo;
 	/***/ public String pathsRequired;
 	/***/ public String refDoesNotExistOrNoCommit;
@@ -179,6 +182,8 @@ public class CLIText extends TranslationBundle {
 	/***/ public String repaint;
 	/***/ public String serviceNotSupported;
 	/***/ public String skippingObject;
+	/***/ public String switchedToNewBranch;
+	/***/ public String switchedToBranch;
 	/***/ public String timeInMilliSeconds;
 	/***/ public String tooManyRefsGiven;
 	/***/ public String unknownMergeStratey;
