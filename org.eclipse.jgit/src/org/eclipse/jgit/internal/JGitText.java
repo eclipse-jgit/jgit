04bb1cfd8ea89f79fb6559c3d53ccef88cb150dc
Ian Wetherbee <wetherbeei@google.com>
2012-06-13 15:31:48 -0700

    Add manager to control multiple subscribers
    Change-Id: I98436066a0b5e23fe0f1299b81f60af10f83044b

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -503,6 +503,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String submoduleExists;
 	/***/ public String submodulesNotSupported;
 	/***/ public String submoduleParentRemoteUrlInvalid;
+	/***/ public String subscribeError;
 	/***/ public String subscribeNewUpdate;
 	/***/ public String subscribeNotSupported;
 	/***/ public String subscribeStart;
