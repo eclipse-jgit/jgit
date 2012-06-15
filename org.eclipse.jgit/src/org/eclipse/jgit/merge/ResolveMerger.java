7a1bc1887699b654cf8833d3483841146514c33f
Matthias Sohn <matthias.sohn@sap.com>
2012-06-15 08:49:58 +0200

    Remove unnecessary import statement
    This import became superfluous with commit b3dbf198 and Eclipse is
    raising an error on that.
    
    Change-Id: Ie95d0f59f0438d79f4ad4212e376cdd3185cbab5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
@@ -78,7 +78,6 @@ import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
