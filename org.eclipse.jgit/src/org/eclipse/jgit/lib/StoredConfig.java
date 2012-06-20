b0349227e4adb7aed9c23b8d2fb3686ea6f3a1ce
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-06-21 01:07:36 +0200

    Make clear method on StoredConfig public
    EGit needs this to be able to revert unsaved changes.
    
    Change-Id: I50cc8056aaff47fef6080970866962e3eb634e29
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/StoredConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/StoredConfig.java
@@ -87,4 +87,9 @@ public abstract class StoredConfig extends Config {
 	 *             the configuration could not be written.
 	 */
 	public abstract void save() throws IOException;
+
+	@Override
+	public void clear() {
+		super.clear();
+	}
 }
