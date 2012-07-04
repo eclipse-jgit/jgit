6bc48cdc62287934ce1b7003280b19a5994e7668
Christian Halstrick <christian.halstrick@sap.com>
2012-07-04 15:08:13 +0200

    CommitBuilder should check for duplicate parents
    When setting the parents of a commit with setParentIds() or
    addParentId() it should be checked that we don't have duplicate parents.
    An IllegalArgumentException should be thrown in this case.
    
    Change-Id: I9fa9f31149b7732071b304bca232f037146de454
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/CommitBuilder.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CommitBuilder.java
@@ -50,7 +50,10 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.text.MessageFormat;
 import java.util.List;
+
+import org.eclipse.jgit.internal.JGitText;
 
 /**
  * Mutable builder to construct a commit recording the state of a project.
@@ -166,7 +169,11 @@ public class CommitBuilder {
 	 *            branch being merged into the current branch.
 	 */
 	public void setParentIds(AnyObjectId parent1, AnyObjectId parent2) {
-		parentIds = new ObjectId[] { parent1.copy(), parent2.copy() };
+		if (!parent1.equals(parent2))
+			parentIds = new ObjectId[] { parent1.copy(), parent2.copy() };
+		else
+			throw new IllegalArgumentException(MessageFormat.format(
+					JGitText.get().duplicateParents, parent1.getName()));
 	}
 
 	/**
@@ -176,9 +183,18 @@ public class CommitBuilder {
 	 *            the entire list of parents for this commit.
 	 */
 	public void setParentIds(ObjectId... newParents) {
-		parentIds = new ObjectId[newParents.length];
-		for (int i = 0; i < newParents.length; i++)
-			parentIds[i] = newParents[i].copy();
+		ObjectId[] tmpIds = new ObjectId[newParents.length];
+
+		int newParentCount = 0;
+		for (int i = 0; i < newParents.length; i++) {
+			for (int j = 0; j < newParentCount; j++)
+				if (tmpIds[j].equals(newParents[i]))
+					throw new IllegalArgumentException(MessageFormat.format(
+							JGitText.get().duplicateParents,
+							tmpIds[j].getName()));
+			tmpIds[newParentCount++] = newParents[i].copy();
+		}
+		parentIds = tmpIds;
 	}
 
 	/**
@@ -188,9 +204,18 @@ public class CommitBuilder {
 	 *            the entire list of parents for this commit.
 	 */
 	public void setParentIds(List<? extends AnyObjectId> newParents) {
-		parentIds = new ObjectId[newParents.size()];
-		for (int i = 0; i < newParents.size(); i++)
-			parentIds[i] = newParents.get(i).copy();
+		ObjectId[] tmpIds = new ObjectId[newParents.size()];
+
+		int newParentCount = 0;
+		for (AnyObjectId newId : newParents) {
+			for (int j = 0; j < newParentCount; j++)
+				if (tmpIds[j].equals(newId))
+					throw new IllegalArgumentException(MessageFormat.format(
+							JGitText.get().duplicateParents,
+							tmpIds[j].getName()));
+			tmpIds[newParentCount++] = newId.copy();
+		}
+		parentIds = tmpIds;
 	}
 
 	/**
@@ -203,6 +228,11 @@ public class CommitBuilder {
 		if (parentIds.length == 0) {
 			setParentId(additionalParent);
 		} else {
+			for (int i = 0; i < parentIds.length; i++)
+				if (parentIds[i].equals(additionalParent))
+					throw new IllegalArgumentException(MessageFormat.format(
+							JGitText.get().duplicateParents,
+							parentIds[i].getName()));
 			ObjectId[] newParents = new ObjectId[parentIds.length + 1];
 			System.arraycopy(parentIds, 0, newParents, 0, parentIds.length);
 			newParents[parentIds.length] = additionalParent.copy();
