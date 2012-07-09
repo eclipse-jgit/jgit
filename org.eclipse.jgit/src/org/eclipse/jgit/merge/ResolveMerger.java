756584058ed6a848a8562438727899ed4bed4f0f
Markus Duft <markus.duft@salomon.at>
2012-07-09 08:22:59 +0200

    Teach ResolveMerger to create more correct DirCacheEntry's
    Currently, after a merge/cherry-pick/rebase, all index entries are
    smudged as the ResolveMerger never sets entry lengths and/or
    modification times. This change teaches it to re-set them at least for
    things it did not touch. The other entries are then repaired when the
    index is persisted, or entries are checked out.
    
    Change-Id: I0944f2017483d32043d0d09409b13055b5609a4b

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
@@ -383,13 +383,14 @@ public class ResolveMerger extends ThreeWayMerger {
 			return false;
 
 		if (nonTree(modeO) && nonTree(modeT) && tw.idEqual(T_OURS, T_THEIRS)) {
+			DirCacheEntry entry;
 			// OURS and THEIRS have equal content. Check the file mode
 			if (modeO == modeT) {
 				// content and mode of OURS and THEIRS are equal: it doesn't
 				// matter which one we choose. OURS is chosen.
-				add(tw.getRawPath(), ours, DirCacheEntry.STAGE_0);
+				entry = add(tw.getRawPath(), ours,
+						DirCacheEntry.STAGE_0);
 				// no checkout needed!
-				return true;
 			} else {
 				// same content but different mode on OURS and THEIRS.
 				// Try to merge the mode and report an error if this is
@@ -398,17 +399,18 @@ public class ResolveMerger extends ThreeWayMerger {
 				if (newMode != FileMode.MISSING.getBits()) {
 					if (newMode == modeO)
 						// ours version is preferred
-						add(tw.getRawPath(), ours, DirCacheEntry.STAGE_0);
+						entry = add(tw.getRawPath(), ours,
+								DirCacheEntry.STAGE_0);
 					else {
 						// the preferred version THEIRS has a different mode
 						// than ours. Check it out!
 						if (isWorktreeDirty(work))
 							return false;
-						DirCacheEntry e = add(tw.getRawPath(), theirs,
+						entry = add(tw.getRawPath(), theirs,
 								DirCacheEntry.STAGE_0);
-						toBeCheckedOut.put(tw.getPathString(), e);
+						toBeCheckedOut.put(tw.getPathString(), entry);
+						return true;
 					}
-					return true;
 				} else {
 					// FileModes are not mergeable. We found a conflict on modes
 					add(tw.getRawPath(), base, DirCacheEntry.STAGE_1);
@@ -419,15 +421,28 @@ public class ResolveMerger extends ThreeWayMerger {
 							tw.getPathString(),
 							new MergeResult<RawText>(Collections
 									.<RawText> emptyList()));
+					return true;
 				}
-				return true;
 			}
+
+			if (workingTreeIterator != null) {
+				entry.setLength(workingTreeIterator.getEntryLength());
+				entry.setLastModified(workingTreeIterator
+						.getEntryLastModified());
+			}
+			return true;
 		}
 
 		if (nonTree(modeO) && modeB == modeT && tw.idEqual(T_BASE, T_THEIRS)) {
 			// THEIRS was not changed compared to BASE. All changes must be in
 			// OURS. OURS is chosen.
-			add(tw.getRawPath(), ours, DirCacheEntry.STAGE_0);
+			DirCacheEntry entry = add(tw.getRawPath(), ours,
+					DirCacheEntry.STAGE_0);
+			if (workingTreeIterator != null) {
+				entry.setLength(workingTreeIterator.getEntryLength());
+				entry.setLastModified(workingTreeIterator
+						.getEntryLastModified());
+			}
 			// no checkout needed!
 			return true;
 		}
