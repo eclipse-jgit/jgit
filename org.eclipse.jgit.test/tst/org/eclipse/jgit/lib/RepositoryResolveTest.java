ee1aca2d24d78dad1e3537886b51b9b7dc53a75b
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-07-12 08:04:35 +0200

    Support parsing previous checkout as a revision expresion.
    Repository.resolve can only return an ObjectId and will
    continue to do so, but another method, simplify(), will
    be able to return a branch name for some cases.
    
    Previous checkouts can be specified as @{-n}, where n is an
    integer speifying the n:th previous branch. The result
    is the branch name, unless the checkout was a detached head,
    in which case the object id is returned. Since the result
    is a branch it may be followed by a references to the reflog,
    such as @{-1}@{1} if necessary.
    
    A simple expression like "master" is resolved to master in
    simplify, but anything starting with refs gets resolved to
    its object id, even if it is a branch.
    
    A symbolic ref is resolved to its leaf ref, e.g. "HEAD" might
    be resolved to "master".
    
    Change-Id: Ifb815a1247ba2a3e2d9c46249c09be9d47f2b693

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/RepositoryResolveTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/RepositoryResolveTest.java
@@ -53,7 +53,9 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Test;
 
 public class RepositoryResolveTest extends SampleDataRepositoryTestCase {
@@ -229,6 +231,17 @@ public class RepositoryResolveTest extends SampleDataRepositoryTestCase {
 		assertNull("no b/FOO", db.resolve("b:b/FOO"));
 		assertNull("no b/FOO", db.resolve(":b/FOO"));
 		assertNull("no not-a-branch:", db.resolve("not-a-branch:"));
+	}
+
+	@Test
+	public void resolveExprSimple() throws Exception {
+		Git git = new Git(db);
+		writeTrashFile("file.txt", "content");
+		git.add().addFilepattern("file.txt").call();
+		RevCommit c1 = git.commit().setMessage("create file").call();
+		assertEquals("master", db.simplify("master"));
+		assertEquals(c1.getName(), db.simplify("refs/heads/master"));
+		assertEquals("master", db.simplify("HEAD"));
 	}
 
 	private static ObjectId id(String name) {
