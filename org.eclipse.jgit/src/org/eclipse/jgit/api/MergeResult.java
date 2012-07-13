6b7ce4373301bcaca704c74a8a24b85832d3a66c
Markus Duft <markus.duft@salomon.at>
2012-07-13 08:25:25 +0200

    Improve handling of checkout conflicts
    This converts a checkout conflict exception into a RebaseResult /
    MergeResult containing the conflicting paths, which enables EGit (or
    others) to handle the situation in a user-friendly way
    
    Change-Id: I48d9bdcc1e98095576513a54a225a42409f301f3

--- org.eclipse.jgit/src/org/eclipse/jgit/api/MergeResult.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/MergeResult.java
@@ -45,6 +45,7 @@ package org.eclipse.jgit.api;
 
 import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jgit.internal.JGitText;
@@ -135,6 +136,21 @@ public class MergeResult {
 			public boolean isSuccessful() {
 				return false;
 			}
+		},
+		/**
+		 * Status representing a checkout conflict, meaning that nothing could
+		 * be merged, as the pre-scan for the trees already failed for certain
+		 * files (i.e. local modifications prevent checkout of files).
+		 */
+		CHECKOUT_CONFLICT {
+			public String toString() {
+				return "Checkout Conflict";
+			}
+
+			@Override
+			public boolean isSuccessful() {
+				return false;
+			}
 		};
 
 		/**
@@ -158,6 +174,8 @@ public class MergeResult {
 	private MergeStrategy mergeStrategy;
 
 	private Map<String, MergeFailureReason> failingPaths;
+
+	private List<String> checkoutConflicts;
 
 	/**
 	 * @param newHead
@@ -250,6 +268,18 @@ public class MergeResult {
 			for (Map.Entry<String, org.eclipse.jgit.merge.MergeResult<?>> result : lowLevelResults
 					.entrySet())
 				addConflict(result.getKey(), result.getValue());
+	}
+
+	/**
+	 * Creates a new result that represents a checkout conflict before the
+	 * operation even started for real.
+	 *
+	 * @param checkoutConflicts
+	 *            the conflicting files
+	 */
+	public MergeResult(List<String> checkoutConflicts) {
+		this.checkoutConflicts = checkoutConflicts;
+		this.mergeStatus = MergeStatus.CHECKOUT_CONFLICT;
 	}
 
 	/**
@@ -406,5 +436,15 @@ public class MergeResult {
 	 */
 	public Map<String, MergeFailureReason> getFailingPaths() {
 		return failingPaths;
+	}
+
+	/**
+	 * Returns a list of paths that cause a checkout conflict. These paths
+	 * prevent the operation from even starting.
+	 *
+	 * @return the list of files that caused the checkout conflict.
+	 */
+	public List<String> getCheckoutConflicts() {
+		return checkoutConflicts;
 	}
 }
