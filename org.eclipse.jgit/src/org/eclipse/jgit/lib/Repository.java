46817eb69a24ef6727f69ecbe19577832adab50b
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-07-19 16:05:28 +0200

    Support @{upstream} syntax
    Change-Id: I0f0e094ca1f8bf8a4c8949f3b58cb72f6255e226

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -583,7 +583,22 @@ public abstract class Repository {
 					}
 				}
 				if (time != null) {
-					if (time.matches("^-\\d+$")) {
+					if (time.equals("upstream")) {
+						if (ref == null) {
+							String refName = new String(revChars, done, i);
+							ref = getRef(refName);
+							if (ref == null)
+								throw new RevisionSyntaxException(revstr);
+						}
+						String remoteBranchName = getConfig()
+								.getString(
+										ConfigConstants.CONFIG_BRANCH_SECTION,
+										ref.getName(),
+										ConfigConstants.CONFIG_KEY_MERGE);
+						ref = getRef(remoteBranchName);
+						if (ref == null)
+							throw new RevisionSyntaxException(revstr);
+					} else if (time.matches("^-\\d+$")) {
 						if (ref != null)
 							throw new RevisionSyntaxException(revstr);
 						else {
