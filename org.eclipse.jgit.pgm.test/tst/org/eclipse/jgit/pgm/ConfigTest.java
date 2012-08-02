de439b363a18a2bf942c3be83cadb654e2ad2cb2
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-08-03 00:56:46 +0200

    Set core.precomposeunicode to true when creating repository on Mac
    Java has no option but to use precomposed Unicode, so we should
    state that when creating a new repository. Not that Java will use
    precomposed unicode regardless of this setting, but this reduces
    the risk of incompatibility with C Git.
    
    Change-Id: I3779b75f76d2e2061c836cbc9b4b7c2ae0cf18f4

--- org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ConfigTest.java
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ConfigTest.java
@@ -44,6 +44,9 @@ package org.eclipse.jgit.pgm;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.CLIRepositoryTestCase;
 import org.eclipse.jgit.util.SystemReader;
@@ -62,12 +65,18 @@ public class ConfigTest extends CLIRepositoryTestCase {
 	public void testListConfig() throws Exception {
 		boolean isWindows = SystemReader.getInstance().getProperty("os.name")
 				.startsWith("Windows");
+		boolean isMac = SystemReader.getInstance().getProperty("os.name")
+				.equals("Mac OS X");
 
 		String[] output = execute("git config --list");
-		assertArrayEquals("expected default configuration", //
-				new String[] { "core.filemode=" + !isWindows, //
-						"core.logallrefupdates=true", //
-						"core.repositoryformatversion=0", //
-						"" /* ends with LF (last line empty) */}, output);
+		List<String> expect = new ArrayList<String>();
+		expect.add("core.filemode=" + !isWindows);
+		expect.add("core.logallrefupdates=true");
+		if (isMac)
+			expect.add("core.precomposeunicode=true");
+		expect.add("core.repositoryformatversion=0");
+		expect.add(""); // ends with LF (last line empty)
+		assertArrayEquals("expected default configuration", expect.toArray(),
+				output);
 	}
 }
