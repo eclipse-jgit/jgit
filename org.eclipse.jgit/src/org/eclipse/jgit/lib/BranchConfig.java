6ea7c945d0d61125b8b8d85ba790f365492b12d4
Matthias Sohn <matthias.sohn@sap.com>
2012-08-11 01:12:17 +0200

    Teach BranchTrackingStatus to handle tracking of local branches
    EGit wasn't able to decorate local branches tracking another local
    branch with number of commits the checked out local branch differs from
    the other local branch it's tracking.
    
    Bug: 376970 
    Change-Id: I74e932d5eacd74dbf6b0dffcfc65ba3222a8250e
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/BranchConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/BranchConfig.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011, Robin Stocker <robin@nibor.org>
+ * Copyright (C) 2012, Matthias Sohn <matthias.sohn@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -71,15 +72,32 @@ public class BranchConfig {
 	}
 
 	/**
-	 * @return the full remote-tracking branch name or <code>null</code> if it
-	 *         could not be determined
+	 * @return the full tracking branch name or <code>null</code> if it could
+	 *         not be determined
 	 */
-	public String getRemoteTrackingBranch() {
+	public String getTrackingBranch() {
 		String remote = getRemote();
 		String mergeRef = getMergeBranch();
 		if (remote == null || mergeRef == null)
 			return null;
 
+		if (remote.equals("."))
+			return mergeRef;
+
+		return findRemoteTrackingBranch(remote, mergeRef);
+	}
+
+	/**
+	 * Finds the tracked remote tracking branch
+	 *
+	 * @param remote
+	 *            Remote name
+	 * @param mergeRef
+	 *            merge Ref of the local branch tracking the remote tracking
+	 *            branch
+	 * @return full remote tracking branch name or null
+	 */
+	private String findRemoteTrackingBranch(String remote, String mergeRef) {
 		RemoteConfig remoteConfig;
 		try {
 			remoteConfig = new RemoteConfig(config, remote);
@@ -95,6 +113,18 @@ public class BranchConfig {
 		return null;
 	}
 
+	/**
+	 * @return the full remote-tracking branch name or <code>null</code> if it
+	 *         could not be determined
+	 */
+	public String getRemoteTrackingBranch() {
+		String remote = getRemote();
+		String mergeRef = getMergeBranch();
+		if (remote == null || mergeRef == null)
+			return null;
+
+		return findRemoteTrackingBranch(remote, mergeRef);
+	}
 	private String getRemote() {
 		String remoteName = config.getString(
 				ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
