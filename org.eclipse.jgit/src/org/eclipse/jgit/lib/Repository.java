a0222cefb805cce140fb48fac82391f6448b4778
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-09-04 22:51:21 +0200

    Allow @ in branch names and tighten syntax checking
    Valid refs are defined by git-check-ref-format(1). In addition we
    will not try to perform a lookup of an invalid ref name in 
    Repository.resolve().
    
    Change-Id: I0b098eec9ecb98a9ce16b1cfb476729aaf2fb190

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -571,6 +571,8 @@ public abstract class Repository {
 			case '@':
 				if (rev != null)
 					throw new RevisionSyntaxException(revstr);
+				if (i + 1 < revChars.length && revChars[i + 1] != '{')
+					continue;
 				int m;
 				String time = null;
 				for (m = i + 2; m < revChars.length; ++m) {
@@ -587,6 +589,8 @@ public abstract class Repository {
 							// Currently checked out branch, HEAD if
 							// detached
 							name = Constants.HEAD;
+						if (!Repository.isValidRefName("x/" + name))
+							throw new RevisionSyntaxException(revstr);
 						Ref ref = getRef(name);
 						if (ref == null)
 							return null;
@@ -634,6 +638,8 @@ public abstract class Repository {
 							name = new String(revChars, done, i);
 						if (name.equals(""))
 							name = Constants.HEAD;
+						if (!Repository.isValidRefName("x/" + name))
+							throw new RevisionSyntaxException(revstr);
 						Ref ref = getRef(name);
 						if (ref == null)
 							return null;
@@ -678,6 +684,8 @@ public abstract class Repository {
 		if (name != null)
 			return name;
 		name = revstr.substring(done);
+		if (!Repository.isValidRefName("x/" + name))
+			throw new RevisionSyntaxException(revstr);
 		if (getRef(name) != null)
 			return name;
 		return resolveSimple(name);
@@ -706,9 +714,11 @@ public abstract class Repository {
 		if (ObjectId.isId(revstr))
 			return ObjectId.fromString(revstr);
 
-		Ref r = getRefDatabase().getRef(revstr);
-		if (r != null)
-			return r.getObjectId();
+		if (Repository.isValidRefName("x/" + revstr)) {
+			Ref r = getRefDatabase().getRef(revstr);
+			if (r != null)
+				return r.getObjectId();
+		}
 
 		if (AbbreviatedObjectId.isId(revstr))
 			return resolveAbbreviation(revstr);
@@ -1129,6 +1139,8 @@ public abstract class Repository {
 			case '/':
 				if (i == 0 || i == len - 1)
 					return false;
+				if (p == '/')
+					return false;
 				components++;
 				break;
 			case '{':
@@ -1138,6 +1150,7 @@ public abstract class Repository {
 			case '~': case '^': case ':':
 			case '?': case '[': case '*':
 			case '\\':
+			case '\u007F':
 				return false;
 			}
 			p = c;
