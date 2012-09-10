79d3dd839198c1ef10791fcc91973729d7755b31
Jason Pyeron <jpyeron@pdinc.us>
2012-09-10 23:50:10 +0200

    Ignore attempts to set the timeout to -1
    The value of -1 is the default value used by the underlying http
    transports provided by the jre. On some versions an attempt to
    set the timeout explicitly to -1 triggers a check condition,
    disallowing negative numbers.
    
    Bug: 389003
    Change-Id: I74a22f8edc6c8e15843ad07c96a137739d9dcad1
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/TransportHttp.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/TransportHttp.java
@@ -514,8 +514,12 @@ public class TransportHttp extends HttpTransport implements WalkTransport,
 		conn.setRequestProperty(HDR_ACCEPT_ENCODING, ENCODING_GZIP);
 		conn.setRequestProperty(HDR_PRAGMA, "no-cache"); //$NON-NLS-1$
 		conn.setRequestProperty(HDR_USER_AGENT, userAgent);
-		conn.setConnectTimeout(getTimeout() * 1000);
-		conn.setReadTimeout(getTimeout() * 1000);
+		int timeOut = getTimeout();
+		if (timeOut != -1) {
+			int effTimeOut = timeOut * 1000;
+			conn.setConnectTimeout(effTimeOut);
+			conn.setReadTimeout(effTimeOut);
+		}
 		authMethod.configureRequest(conn);
 		return conn;
 	}
