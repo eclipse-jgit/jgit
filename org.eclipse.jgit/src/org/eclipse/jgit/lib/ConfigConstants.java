27b791c591a8216a792ad72818e729d841fa53b3
Christian Halstrick <christian.halstrick@sap.com>
2012-09-17 21:21:00 +0200

    Support config param "gc.pruneexpire"
    Make GC honor the config parameter gc.pruneexpire. If the parameter is
    not set then the default is "2.weeks.ago"
    
    Change-Id: I0ae0ca85993cafb4bc75ba80504da18544894ec3

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -75,6 +75,9 @@ public class ConfigConstants {
 	/** The "submodule" section */
 	public static final String CONFIG_SUBMODULE_SECTION = "submodule";
 
+	/** The "gc" section */
+	public static final String CONFIG_GC_SECTION = "gc";
+
 	/** The "pack" section */
 	public static final String CONFIG_PACK_SECTION = "pack";
 
@@ -181,4 +184,7 @@ public class ConfigConstants {
 
 	/** The "precomposeunicode" key */
 	public static final String CONFIG_KEY_PRECOMPOSEUNICODE = "precomposeunicode";
+
+	/** The "pruneexpire" key */
+	public static final String CONFIG_KEY_PRUNEEXPIRE = "pruneexpire";
 }
