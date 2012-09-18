42a52febb57c41a7e3a8636386b229171efb4225
Christian Halstrick <christian.halstrick@sap.com>
2012-09-18 08:14:13 +0200

    Introduce "never" as parseable date
    For configuration parameter like "gc.pruneexpire" we need to understand
    the value "never". This should cause the parser to return null. All
    users of the parser have to be adapted to handle null return values
    correctly
    
    Change-Id: I7744eaee9bf5026da517151c212c88325c348d6c

--- org.eclipse.jgit/src/org/eclipse/jgit/util/GitDateParser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/GitDateParser.java
@@ -62,6 +62,11 @@ import org.eclipse.jgit.internal.JGitText;
  * understands.
  */
 public class GitDateParser {
+	/**
+	 * The Date representing never
+	 * */
+	public static final Date NEVER = new Date(Long.MAX_VALUE);
+
 	// Since SimpleDateFormat instances are expensive to instantiate they should
 	// be cached. Since they are also not threadsafe they are cached using
 	// ThreadLocal.
@@ -113,6 +118,7 @@ public class GitDateParser {
 	 * relative formats (e.g. "yesterday") the caller can specify the reference
 	 * date. These types of strings can be parsed:
 	 * <ul>
+	 * <li>"never"</li>
 	 * <li>"now"</li>
 	 * <li>"yesterday"</li>
 	 * <li>"(x) years|months|weeks|days|hours|minutes|seconds ago"<br>
@@ -146,6 +152,9 @@ public class GitDateParser {
 			throws ParseException {
 		dateStr = dateStr.trim();
 		Date ret;
+
+		if ("never".equalsIgnoreCase(dateStr))
+			return NEVER;
 		ret = parse_relative(dateStr, now);
 		if (ret != null)
 			return ret;
