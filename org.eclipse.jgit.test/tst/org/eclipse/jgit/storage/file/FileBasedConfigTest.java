c422794b92fa07727062d6c2da1fd472a6eede64
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-09-26 21:21:59 +0200

    cleanup in FileBasedConfig
    I1f5dc07182dbf6bba2a9f4807fdd25b475da4ead introduced a dependency
    on Java 6 and an unneeded import.
    
    Change-Id: Ic0906b5182436547f4b35f4d6d875da71c95b3e7

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/FileBasedConfigTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/storage/file/FileBasedConfigTest.java
@@ -54,7 +54,6 @@ import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.util.IO;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FileBasedConfigTest {
