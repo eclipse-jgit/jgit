29b2ff7c92ef1d08e3dd4b0497c2b040762a10a6
Tomasz Zarna <tomasz.zarna@tasktop.com>
2012-10-29 16:18:11 +0100

    [WIP]Add reflog command to JGit CLI
    Change-Id: Ib8bc1d9fd789d22fe5f10e03068a11cfdd3e46eb

---
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Reflog.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2012, Tomasz Zarna <tomasz.zarna@tasktop.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.pgm;
+
+import java.util.Collection;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ReflogCommand;
+import org.eclipse.jgit.lib.AbbreviatedObjectId;
+import org.eclipse.jgit.storage.file.ReflogEntry;
+
+@Command(common = true, usage = "usage_manageReflogInformation")
+class Reflog extends TextBuiltin {
+
+	@Override
+	protected void run() throws Exception {
+		ReflogCommand cmd = new Git(db).reflog();
+		Collection<ReflogEntry> entries = cmd.call();
+		int i = 0;
+		for (ReflogEntry entry : entries) {
+			outw.println(toString(entry, i++));
+		}
+	}
+
+	private String toString(ReflogEntry entry, int i) {
+		final StringBuilder s = new StringBuilder();
+		s.append(AbbreviatedObjectId.fromObjectId(entry.getNewId()).name()
+				.subSequence(0, 7));
+		s.append(" ");
+		s.append("HEAD@{" + i + "}:");
+		s.append(" ");
+		s.append(entry.getComment());
+		return s.toString();
+	}
+}
