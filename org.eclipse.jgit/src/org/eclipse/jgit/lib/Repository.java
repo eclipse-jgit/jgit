a917eaafe6f8908e0a358dac0fcfb8d4c84c24f0
Dani Megert <Daniel_Megert@ch.ibm.com>
2012-10-30 15:55:14 +0100

    Declare all thrown exceptions
    Bug 393155
    Change-Id: If702f2e54b17e82890f016126ee7bde4bff4af1d
    Signed-off-by: Dani Megert <Daniel_Megert@ch.ibm.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -3,6 +3,7 @@
  * Copyright (C) 2008-2010, Google Inc.
  * Copyright (C) 2006-2010, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2006-2012, Shawn O. Pearce <spearce@spearce.org>
+ * Copyright (C) 2012, Daniel Megert <daniel_megert@ch.ibm.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -377,7 +378,8 @@ public abstract class Repository {
 	 *             on serious errors
 	 */
 	public ObjectId resolve(final String revstr)
-			throws AmbiguousObjectException, IOException {
+			throws AmbiguousObjectException, IncorrectObjectTypeException,
+			RevisionSyntaxException, IOException {
 		RevWalk rw = new RevWalk(this);
 		try {
 			Object resolved = resolve(rw, revstr);
