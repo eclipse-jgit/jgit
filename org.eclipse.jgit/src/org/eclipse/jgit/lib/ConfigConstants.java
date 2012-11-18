20c3b6b8a0ba4f3d9f5666dd48b132839ab7e5bf
Tomasz Zarna <tomasz.zarna@tasktop.com>
2012-11-18 22:35:53 +0100

    Add config constant for "mergeoptions"
    Bug: 336933
    Change-Id: Idcc0fbc8a8143dd665e80e4d6f4a4adaeae287db
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -187,4 +187,7 @@ public class ConfigConstants {
 
 	/** The "pruneexpire" key */
 	public static final String CONFIG_KEY_PRUNEEXPIRE = "pruneexpire";
+
+	/** The "mergeoptions" key */
+	public static final String CONFIG_KEY_MERGEOPTIONS = "mergeoptions";
 }
