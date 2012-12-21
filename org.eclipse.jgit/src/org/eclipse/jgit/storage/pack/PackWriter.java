75255742fcb7a5c2b664d1cb1a996e6eb776d9e6
Colby Ranger <cranger@google.com>
2012-12-21 12:54:42 -0800

    Set the delta depth before delta search in PackWriter.
    Currently, the PackWriter does not carry forward the delta depth of
    reused objects during delta compression.  The delta search only carries
    forward the delta depth of new deltas.  In practice, this can cause
    delta chain lengths to have no limit. Update the PackWriter to
    calculate the delta depth of reused objects, based on the chain
    that is actually being written.
    
    Change-Id: I4fd402854b2b3d699ebd900e7f232ce33efde9de

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackWriter.java
@@ -1075,6 +1075,9 @@ public class PackWriter {
 
 				else
 					otp.setWeight((int) sz);
+
+				if (otp.isDeltaRepresentation() && !otp.isEdge())
+					setDeltaDepth(otp);
 				monitor.update(1);
 			}
 		} finally {
@@ -1135,6 +1138,26 @@ public class PackWriter {
 		for (int i = 0; i < cnt; i++)
 			if (!list[i].isEdge() && list[i].isDeltaRepresentation())
 				stats.deltasFound++;
+	}
+
+	private void setDeltaDepth(ObjectToPack otp) {
+		LinkedList<ObjectToPack> deltaChain = new LinkedList<ObjectToPack>();
+		int deltaDepth = 0;
+		for (ObjectToPack cur = otp;;) {
+			if (cur == null || !cur.isDeltaRepresentation() || cur.isEdge())
+				break;
+			int curDeltaDepth = cur.getDeltaDepth();
+			if (curDeltaDepth > 0) {
+				deltaDepth = curDeltaDepth;
+				break;
+			}
+
+			deltaChain.push(cur);
+			cur = objectsMap.get(cur.getDeltaBaseId());
+		}
+
+		while (!deltaChain.isEmpty())
+			deltaChain.pop().setDeltaDepth(++deltaDepth);
 	}
 
 	private int findObjectsNeedingDelta(ObjectToPack[] list, int cnt, int type) {
