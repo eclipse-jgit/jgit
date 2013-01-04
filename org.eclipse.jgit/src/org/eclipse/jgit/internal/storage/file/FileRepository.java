d3e6b948d605cd9e5d5379a8d57f2840b3f2a6b7
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-05 00:34:03 +0100

    Add symlink support to JGit
    The change includes comparing symbolic links between disk and index,
    adding symbolic links to the index, creating/modifying links on
    checkout. The behavior is controlled by the core.symlinks setting, just
    as C Git does. When a new repository is created core.symlinks will be
    set depending on the capabilities of the operating system and Java
    runtime.
    
    If core.symlinks is set to true, the assumption is that symlinks are
    supported, which may result in runtime errors if this turs out not to
    be the case.
    
    Measuring the cost of jgit status on a repository with ~70000 files,
    of which ~30000 are tracked reveals a penalty of about 10% for using
    the Java7 (really NIO2) support module.
    
    Bug: 354367
    Change-Id: I12f0fdd9d26212324a586896ef7eb1f6ff89c39c

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/FileRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/FileRepository.java
@@ -64,6 +64,7 @@ import org.eclipse.jgit.internal.storage.file.FileObjectDatabase.AlternateReposi
 import org.eclipse.jgit.lib.BaseRepositoryBuilder;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.CoreConfig.SymLinks;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
@@ -293,6 +294,21 @@ public class FileRepository extends Repository {
 			fileMode = false;
 		}
 
+		SymLinks symLinks = SymLinks.FALSE;
+		if (getFS().supportsSymlinks()) {
+			File tmp = new File(getDirectory(), "tmplink"); //$NON-NLS-1$
+			try {
+				getFS().createSymLink(tmp, "target"); //$NON-NLS-1$
+				symLinks = null;
+				FileUtils.delete(tmp);
+			} catch (IOException e) {
+				// Normally a java.nio.file.FileSystemException
+			}
+		}
+		if (symLinks != null)
+			cfg.setString(ConfigConstants.CONFIG_CORE_SECTION, null,
+					ConfigConstants.CONFIG_KEY_SYMLINKS, symLinks.name()
+							.toLowerCase());
 		cfg.setInt(ConfigConstants.CONFIG_CORE_SECTION, null,
 				ConfigConstants.CONFIG_KEY_REPO_FORMAT_VERSION, 0);
 		cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
