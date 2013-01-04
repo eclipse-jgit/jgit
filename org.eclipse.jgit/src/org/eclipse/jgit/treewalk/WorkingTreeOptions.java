47402862afb57cf09f63bcb0224b67413500c14f
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-05 00:34:03 +0100

    Add symlink support to JGit
    The change includes comparing symbolic links between disk and index,
    adding symbolic links to the index, creating/modifying links on
    checkout. The behavior is controlled by the core.symlinks setting, just
    as C Git does. When a new repository is created core.symlinks will be
    set depending on the capabilities of the operating system and Java
    runtime.
    
    If core.symlinks is set to true, the assumption is that symlinks are
    supported, which may result in runtime errors if this turs out not to
    be the case.
    
    Measuring the cost of jgit status on a repository with ~70000 files,
    of which ~30000 are tracked reveals a penalty of about 10% for using
    the Java7 (really NIO2) support module.
    
    Bug: 354367
    Change-Id: I12f0fdd9d26212324a586896ef7eb1f6ff89c39c

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeOptions.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeOptions.java
@@ -48,6 +48,7 @@ import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Config.SectionParser;
 import org.eclipse.jgit.lib.CoreConfig.AutoCRLF;
 import org.eclipse.jgit.lib.CoreConfig.CheckStat;
+import org.eclipse.jgit.lib.CoreConfig.SymLinks;
 
 /** Options used by the {@link WorkingTreeIterator}. */
 public class WorkingTreeOptions {
@@ -64,6 +65,8 @@ public class WorkingTreeOptions {
 
 	private final CheckStat checkStat;
 
+	private final SymLinks symlinks;
+
 	private WorkingTreeOptions(final Config rc) {
 		fileMode = rc.getBoolean(ConfigConstants.CONFIG_CORE_SECTION,
 				ConfigConstants.CONFIG_KEY_FILEMODE, true);
@@ -71,6 +74,8 @@ public class WorkingTreeOptions {
 				ConfigConstants.CONFIG_KEY_AUTOCRLF, AutoCRLF.FALSE);
 		checkStat = rc.getEnum(ConfigConstants.CONFIG_CORE_SECTION, null,
 				ConfigConstants.CONFIG_KEY_CHECKSTAT, CheckStat.DEFAULT);
+		symlinks = rc.getEnum(ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_KEY_SYMLINKS, SymLinks.TRUE);
 	}
 
 	/** @return true if the execute bit on working files should be trusted. */
@@ -89,5 +94,13 @@ public class WorkingTreeOptions {
 	 */
 	public CheckStat getCheckStat() {
 		return checkStat;
+	}
+
+	/**
+	 * @return how we handle symbolic links
+	 * @since 3.3
+	 */
+	public SymLinks getSymLinks() {
+		return symlinks;
 	}
 }
