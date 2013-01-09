4726138c9e526c7dfc91fb16d709452b075d5e97
Stefan Lay <stefan.lay@sap.com>
2013-01-09 14:02:51 +0100

    Add conflicts message before footer
    In case of a conflict during cherry-pick or revert the commit message
    was amended after the footer. This made the footer invalid. Many users
    do not understand that they have to edit the commit message in order to
    make it valid again.
    
    Change-Id: I7e7fae125129e2a0d8950510550acda766531835
    Bug: 367416

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/merge/MergeMessageFormatterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/merge/MergeMessageFormatterTest.java
@@ -185,4 +185,46 @@ public class MergeMessageFormatterTest extends SampleDataRepositoryTestCase {
 		String message = formatter.format(Arrays.asList(a), head);
 		assertEquals("Merge branch 'a'", message);
 	}
+
+	@Test
+	public void testFormatWithConflictsNoFooter() {
+		String originalMessage = "Header Line\n\nCommit body\n";
+		String message = formatter.formatWithConflicts(originalMessage,
+				Arrays.asList(new String[] { "path1" }));
+		assertEquals("Header Line\n\nCommit body\n\nConflicts:\n\tpath1\n",
+				message);
+	}
+
+	@Test
+	public void testFormatWithConflictsNoFooterNoLineBreak() {
+		String originalMessage = "Header Line\n\nCommit body";
+		String message = formatter.formatWithConflicts(originalMessage,
+				Arrays.asList(new String[] { "path1" }));
+		assertEquals("Header Line\n\nCommit body\n\nConflicts:\n\tpath1\n",
+				message);
+	}
+
+	@Test
+	public void testFormatWithConflictsWithFooters() {
+		String originalMessage = "Header Line\n\nCommit body\n\nChangeId:"
+				+ " I123456789123456789123456789123456789\nBug:1234567\n";
+		String message = formatter.formatWithConflicts(originalMessage,
+				Arrays.asList(new String[] { "path1" }));
+		assertEquals(
+				"Header Line\n\nCommit body\n\nConflicts:\n\tpath1\n\n"
+						+ "ChangeId: I123456789123456789123456789123456789\nBug:1234567\n",
+				message);
+	}
+
+	@Test
+	public void testFormatWithConflictsWithFooterlikeLineInBody() {
+		String originalMessage = "Header Line\n\nCommit body\nBug:1234567\nMore Body\n\nChangeId:"
+				+ " I123456789123456789123456789123456789\nBug:1234567\n";
+		String message = formatter.formatWithConflicts(originalMessage,
+				Arrays.asList(new String[] { "path1" }));
+		assertEquals(
+				"Header Line\n\nCommit body\nBug:1234567\nMore Body\n\nConflicts:\n\tpath1\n\n"
+						+ "ChangeId: I123456789123456789123456789123456789\nBug:1234567\n",
+				message);
+	}
 }
