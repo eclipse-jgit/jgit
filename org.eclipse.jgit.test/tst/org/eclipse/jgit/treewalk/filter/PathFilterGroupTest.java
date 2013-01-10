29032b7f2f70525c13bbd229032f2ccafc798331
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-10 23:46:27 +0100

    Add testcase for PathFilterGroup
    This is to prepare for optimization.
    
    Change-Id: If38507ef8cc5a161353cc07bf7cd1922f54d1f8a

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/treewalk/filter/PathFilterGroupTest.java
@@ -0,0 +1,117 @@
+package org.eclipse.jgit.treewalk.filter;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheEditor;
+import org.eclipse.jgit.dircache.DirCacheEntry;
+import org.eclipse.jgit.dircache.DirCacheIterator;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.StopWalkException;
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PathFilterGroupTest {
+
+	private TreeFilter filter;
+
+	@Before
+	public void setup() {
+		// @formatter:off
+		String[] paths = new String[] {
+				"a",
+				"b/c",
+				"c/d/e",
+				"c/d/f",
+				"d/e/f/g"
+				};
+		filter = PathFilterGroup.createFromStrings(paths);
+	}
+
+	@Test
+	public void testExact() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		assertTrue(filter.include(fakeWalk("a")));
+		assertTrue(filter.include(fakeWalk("b/c")));
+		assertTrue(filter.include(fakeWalk("c/d/e")));
+		assertTrue(filter.include(fakeWalk("c/d/f")));
+		assertTrue(filter.include(fakeWalk("d/e/f/g")));
+	}
+
+	@Test
+	public void testNoMatchButClose() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		assertFalse(filter.include(fakeWalk("a+")));
+		assertFalse(filter.include(fakeWalk("b+/c")));
+		assertFalse(filter.include(fakeWalk("c+/d/e")));
+		assertFalse(filter.include(fakeWalk("c+/d/f")));
+		assertFalse(filter.include(fakeWalk("d+/e/f/g")));
+	}
+
+	@Test
+	public void testKeyIsPrefixOfFilter() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		assertTrue(filter.include(fakeWalk("b")));
+		assertTrue(filter.include(fakeWalk("c/d")));
+		assertTrue(filter.include(fakeWalk("c/d")));
+		assertTrue(filter.include(fakeWalk("c")));
+		assertTrue(filter.include(fakeWalk("d/e/f")));
+		assertTrue(filter.include(fakeWalk("d/e")));
+		assertTrue(filter.include(fakeWalk("d")));
+	}
+
+	@Test
+	public void testFilterIsPrefixOfKey() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		assertTrue(filter.include(fakeWalk("a/b")));
+		assertTrue(filter.include(fakeWalk("b/c/d")));
+		assertTrue(filter.include(fakeWalk("c/d/e/f")));
+		assertTrue(filter.include(fakeWalk("c/d/f/g")));
+		assertTrue(filter.include(fakeWalk("d/e/f/g/h")));
+	}
+
+	@Test
+	public void testStopWalk() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		filter.include(fakeWalk("d/e/f/f"));
+		try {
+			filter.include(fakeWalk("de"));
+			fail("StopWalkException expected");
+		} catch (StopWalkException e) {
+			// good
+		}
+		try {
+			filter.include(fakeWalk("\u00C0"));
+			fail("StopWalkException expected");
+		} catch (StopWalkException e) {
+			// good
+		}
+	}
+
+	TreeWalk fakeWalk(final String path) throws IOException {
+		TreeWalk ret = new TreeWalk((ObjectReader) null);
+		ret.reset();
+		ret.setRecursive(true);
+		DirCache dc = DirCache.newInCore();
+		DirCacheEditor dce = dc.editor();
+		dce.add(new DirCacheEditor.PathEdit(path) {
+
+			public void apply(DirCacheEntry ent) {
+				ent.setFileMode(FileMode.REGULAR_FILE);
+			}
+		});
+		dce.finish();
+		ret.addTree(new DirCacheIterator(dc));
+		ret.next();
+		return ret;
+	}
+
+}
