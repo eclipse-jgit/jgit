4a75c383cc5ca1f54418779b150ab1d761bdb4e2
George C. Young <geyoung@rim.com>
2013-01-13 23:46:55 +0100

    Implement recursive merge strategy
    Extend ResolveMerger with RecursiveMerger to merge two tips
    that have up to 200 bases.
    
    Bug: 380314
    CQ: 6854
    Change-Id: I6292bb7bda55c0242a448a94956f2d6a94fddbaa
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>
    Also-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
+ * Copyright (c) 2012, Research In Motion Limited
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -348,6 +349,8 @@ public class JGitText extends TranslationBundle {
 	/***/ public String mergeStrategyAlreadyExistsAsDefault;
 	/***/ public String mergeStrategyDoesNotSupportHeads;
 	/***/ public String mergeUsingStrategyResultedInDescription;
+	/***/ public String mergeRecursiveReturnedNoCommit;
+	/***/ public String mergeRecursiveTooManyMergeBasesFor;
 	/***/ public String minutesAgo;
 	/***/ public String missingAccesskey;
 	/***/ public String missingConfigurationForKey;
@@ -373,6 +376,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String noClosingBracket;
 	/***/ public String noHEADExistsAndNoExplicitStartingRevisionWasSpecified;
 	/***/ public String noHMACsupport;
+	/***/ public String noMergeBase;
 	/***/ public String noMergeHeadSpecified;
 	/***/ public String noSuchRef;
 	/***/ public String notABoolean;
