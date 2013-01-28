5893182eb1814290f5fafb3036e8754a36a5d565
Colby Ranger <cranger@google.com>
2013-01-28 11:49:01 -0800

    Include supported extensions in PackFile constructor.
    Previously a PackFile class was assumed to only support a .pack and .idx
    file. Update the constructor to enumerate the supported extensions for
    the pack file. This will allow the bitmap code to only be executed if
    the bitmap extension file is known to exist.
    
    Change-Id: Ie59041dffec5f60d7ea2771026ffd945106bd4bf

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
@@ -97,6 +97,8 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 
 	private final File packFile;
 
+	private final int extensions;
+
 	private File keepFile;
 
 	private volatile String packName;
@@ -138,10 +140,16 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 	 *
 	 * @param packFile
 	 *            path of the <code>.pack</code> file holding the data.
+	 * @param extensions
+	 *            additional pack file extensions with the same base as the pack
 	 */
-	public PackFile(final File packFile) {
+	public PackFile(final File packFile, Iterable<PackExt> extensions) {
 		this.packFile = packFile;
 		this.packLastModified = (int) (packFile.lastModified() >> 10);
+		int bits = 0;
+		for (PackExt ext : extensions)
+			bits |= 1 << ext.getPosition();
+		this.extensions = bits;
 
 		// Multiply by 31 here so we can more directly combine with another
 		// value in WindowCache.hash(), without doing the multiply there.
