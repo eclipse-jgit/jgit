3fea197b22341561cfaaf252af36d922abfa3402
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-30 21:50:22 +0100

    Implement core.checkstat = minimal
    There is a huge performance issue when using both JGit (EGit) and Git
    because JGit does not fille all dircache stat fields with the values Git
    would expect. As a result thereof Git would typically revalidate a large
    number of tracked files. This can take several minutes for large
    repositories with many large files.
    
    Since 1.8.2 Git will restrict stat checking to the size and whole second
    part of the modification time stamp, if core.statinfo is set to
    "minimal".
    
    As JGit checks only size and modification time this is close to what
    JGit already does. To make the match perfect ignore the sub-second part
    of the modification time stamp if core.statinfo = minimal.
    
    Change-Id: I8eaff1858a891571075a86db043f9d80da3d7503

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
@@ -74,6 +74,25 @@ public class CoreConfig {
 		INPUT;
 	}
 
+	/**
+	 * Permissible values for {@code core.checkstat}
+	 *
+	 * @since 2.3
+	 */
+	public static enum CheckStat {
+		/**
+		 * Only check the size and whole second part of time stamp when
+		 * comparing the stat info in the dircache with actual file stat info.
+		 */
+		MINIMAL,
+
+		/**
+		 * Check as much of the dircache stat info as possible. Implementation
+		 * limits may apply.
+		 */
+		DEFAULT
+	}
+
 	private final int compression;
 
 	private final int packIndexVersion;
