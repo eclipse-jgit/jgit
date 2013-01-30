636f261bbfe15a9956b56dec5cf5fb2dd827a7bf
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-30 21:50:22 +0100

    Implement core.checkstat = minimal
    There is a huge performance issue when using both JGit (EGit) and Git
    because JGit does not fille all dircache stat fields with the values Git
    would expect. As a result thereof Git would typically revalidate a large
    number of tracked files. This can take several minutes for large
    repositories with many large files.
    
    Since 1.8.2 Git will restrict stat checking to the size and whole second
    part of the modification time stamp, if core.statinfo is set to
    "minimal".
    
    As JGit checks only size and modification time this is close to what
    JGit already does. To make the match perfect ignore the sub-second part
    of the modification time stamp if core.statinfo = minimal.
    
    Change-Id: I8eaff1858a891571075a86db043f9d80da3d7503

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
@@ -2,6 +2,7 @@
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2010, Christian Halstrick <christian.halstrick@sap.com>
  * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2012-2013, Robin Rosenberg
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -75,6 +76,7 @@ import org.eclipse.jgit.lib.CoreConfig;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.CoreConfig.CheckStat;
 import org.eclipse.jgit.submodule.SubmoduleWalk;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.IO;
@@ -754,14 +756,18 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 
 		// Git under windows only stores seconds so we round the timestamp
 		// Java gives us if it looks like the timestamp in index is seconds
-		// only. Otherwise we compare the timestamp at millisecond precision.
+		// only. Otherwise we compare the timestamp at millisecond precision,
+		// unless core.checkstat is set to "minimal", in which case we only
+		// compare the whole second part.
 		long cacheLastModified = entry.getLastModified();
 		long fileLastModified = getEntryLastModified();
-		if (cacheLastModified % 1000 == 0)
+		if (cacheLastModified % 1000 == 0
+				|| getOptions().getCheckStat() == CheckStat.MINIMAL)
 			fileLastModified = fileLastModified - fileLastModified % 1000;
 		// Some Java version on Linux return whole seconds only even when
 		// the file systems supports more precision.
-		else if (fileLastModified % 1000 == 0)
+		else if (fileLastModified % 1000 == 0
+				|| getOptions().getCheckStat() == CheckStat.MINIMAL)
 			cacheLastModified = cacheLastModified - cacheLastModified % 1000;
 		if (fileLastModified != cacheLastModified)
 			return MetadataDiff.DIFFER_BY_TIMESTAMP;
