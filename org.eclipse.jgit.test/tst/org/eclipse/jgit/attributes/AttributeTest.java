1d49ddb708847d736f5934c8796bf0ba7e5b8766
Arthur Daussy <arthur.daussy@obeo.fr>
2013-01-31 20:27:10 +0100

    Add basic support for attributes
    Core classes to parse and process .gitattributes files including
    support for reading attributes in WorkingTreeIterator and the
    dirCacheIterator.
    
    The implementation follows the git ignore implementation. It supports
    lazy reading attributes while walking the working tree.
    
    Bug: 342372
    CQ: 9078
    Change-Id: I05f3ce1861fbf9896b1bcb7816ba78af35f3ad3d
    Also-by: Marc Strapetz <marc.strapetz@syntevo.com>
    Also-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Also-by: Arthur Daussy <arthur.daussy@obeo.fr>
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributeTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributeTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2014, Andrey Loskutov <loskutov@gmx.de>
+ * Copyright (C) 2010, Marc Strapetz <marc.strapetz@syntevo.com>
+ * Copyright (C) 2013, Gunnar Wagenknecht
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,55 +41,37 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.eclipse.jgit.ignore.internal;
+package org.eclipse.jgit.attributes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.eclipse.jgit.attributes.Attribute.State;
+import org.junit.Test;
 
 /**
- * Generic string matcher
- *
- * @since 3.6
+ * Tests {@link Attribute}
  */
-public interface IMatcher {
+public class AttributeTest {
 
-	/**
-	 * Matcher that does not match any pattern.
-	 */
-	public static final IMatcher NO_MATCH = new IMatcher() {
-		public boolean matches(String path, boolean assumeDirectory) {
-			return false;
-		}
+	@Test
+	public void testBasic() {
+		Attribute a = new Attribute("delta", State.SET);
+		assertEquals(a.getKey(), "delta");
+		assertEquals(a.getState(), State.SET);
+		assertNull(a.getValue());
+		assertEquals(a.toString(), "delta");
 
-		public boolean matches(String segment, int startIncl, int endExcl,
-				boolean assumeDirectory) {
-			return false;
-		}
-	};
+		a = new Attribute("delta", State.UNSET);
+		assertEquals(a.getKey(), "delta");
+		assertEquals(a.getState(), State.UNSET);
+		assertNull(a.getValue());
+		assertEquals(a.toString(), "-delta");
 
-	/**
-	 * Matches entire given string
-	 *
-	 * @param path
-	 *            string which is not null, but might be empty
-	 * @param assumeDirectory
-	 *            true to assume this path as directory (even if it doesn't end
-	 *            with a slash)
-	 * @return true if this matcher pattern matches given string
-	 */
-	boolean matches(String path, boolean assumeDirectory);
-
-	/**
-	 * Matches only part of given string
-	 *
-	 * @param segment
-	 *            string which is not null, but might be empty
-	 * @param startIncl
-	 *            start index, inclusive
-	 * @param endExcl
-	 *            end index, exclusive
-	 * @param assumeDirectory
-	 *            true to assume this path as directory (even if it doesn't end
-	 *            with a slash)
-	 * @return true if this matcher pattern matches given string
-	 */
-	boolean matches(String segment, int startIncl, int endExcl,
-			boolean assumeDirectory);
+		a = new Attribute("delta", "value");
+		assertEquals(a.getKey(), "delta");
+		assertEquals(a.getState(), State.CUSTOM);
+		assertEquals(a.getValue(), "value");
+		assertEquals(a.toString(), "delta=value");
+	}
 }
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/attributes/AttributesNode.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2010, Red Hat Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.attributes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import org.eclipse.jgit.lib.Constants;
+
+/**
+ * Represents a bundle of attributes inherited from a base directory.
+ *
+ * This class is not thread safe, it maintains state about the last match.
+ *
+ * @since 3.7
+ */
+public class AttributesNode {
+	/** The rules that have been parsed into this node. */
+	private final List<AttributesRule> rules;
+
+	/** Create an empty ignore node with no rules. */
+	public AttributesNode() {
+		rules = new ArrayList<AttributesRule>();
+	}
+
+	/**
+	 * Create an ignore node with given rules.
+	 *
+	 * @param rules
+	 *            list of rules.
+	 **/
+	public AttributesNode(List<AttributesRule> rules) {
+		this.rules = rules;
+	}
+
+	/**
+	 * Parse files according to gitattribute standards.
+	 *
+	 * @param in
+	 *            input stream holding the standard ignore format. The caller is
+	 *            responsible for closing the stream.
+	 * @throws IOException
+	 *             Error thrown when reading an ignore file.
+	 */
+	public void parse(InputStream in) throws IOException {
+		BufferedReader br = asReader(in);
+		String txt;
+		while ((txt = br.readLine()) != null) {
+			txt = txt.trim();
+			if (txt.length() > 0 && !txt.startsWith("#") /* Comments *///$NON-NLS-1$
+					&& !txt.startsWith("!") /* Negative pattern forbidden for attributes */) { //$NON-NLS-1$
+				int patternEndSpace = txt.indexOf(' ');
+				int patternEndTab = txt.indexOf('\t');
+
+				final int patternEnd;
+				if (patternEndSpace == -1)
+					patternEnd = patternEndTab;
+				else if (patternEndTab == -1)
+					patternEnd = patternEndSpace;
+				else
+					patternEnd = Math.min(patternEndSpace, patternEndTab);
+
+				if (patternEnd > -1)
+					rules.add(new AttributesRule(txt.substring(0, patternEnd),
+							txt.substring(patternEnd + 1).trim()));
+			}
+		}
+	}
+
+	private static BufferedReader asReader(InputStream in) {
+		return new BufferedReader(new InputStreamReader(in, Constants.CHARSET));
+	}
+
+	/** @return list of all ignore rules held by this node. */
+	public List<AttributesRule> getRules() {
+		return Collections.unmodifiableList(rules);
+	}
+
+	/**
+	 * Returns the matching attributes for an entry path.
+	 *
+	 * @param entryPath
+	 *            the path to test. The path must be relative to this attribute
+	 *            node's own repository path, and in repository path format
+	 *            (uses '/' and not '\').
+	 * @param isDirectory
+	 *            true if the target item is a directory.
+	 * @param attributes
+	 *            Map that will hold the attributes matching this entry path. If
+	 *            it is not empty, this method will NOT override any
+	 *            existing entry.
+	 */
+	public void getAttributes(String entryPath, boolean isDirectory,
+			Map<String, Attribute> attributes) {
+		// Parse rules in the reverse order that they were read since the last
+		// entry should be used
+		ListIterator<AttributesRule> ruleIterator = rules.listIterator(rules
+				.size());
+		while (ruleIterator.hasPrevious()) {
+			AttributesRule rule = ruleIterator.previous();
+			if (rule.isMatch(entryPath, isDirectory)) {
+				ListIterator<Attribute> attributeIte = rule.getAttributes()
+						.listIterator(rule.getAttributes().size());
+				// Parses the attributes in the reverse order that they were
+				// read since the last entry should be used
+				while (attributeIte.hasPrevious()) {
+					Attribute attr = attributeIte.previous();
+					if (!attributes.containsKey(attr.getKey()))
+						attributes.put(attr.getKey(), attr);
+				}
+			}
+		}
+	}
+}
