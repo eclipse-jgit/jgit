4647a0bc37bf9e76599755310ebb524e6ecb349c
Arthur Daussy <arthur.daussy@obeo.fr>
2013-01-31 20:27:10 +0100

    Add basic support for attributes.
    Core classes to parse and process .gitattributes files including
    support for reading attributes in WorkingTreeIterator and the
    dirCacheIterator.
    
    The implementation follows the git ignore implementation. It supports
    lazy reading attributes while walking the working tree.
    
    Bug: 342372
    Change-Id: I05f3ce1861fbf9896b1bcb7816ba78af35f3ad3d
    Also-by: Marc Strapetz <marc.strapetz@syntevo.com>
    Also-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Also-by: Arthur Daussy <arthur.daussy@obeo.fr>
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributeNodeTest.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2014, Obeo.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.attributes;
+
+import static org.eclipse.jgit.attributes.Attribute.State.SET;
+import static org.eclipse.jgit.attributes.Attribute.State.UNSET;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Test;
+
+/**
+ * Test {@link AttributesNode}
+ */
+public class AttributeNodeTest {
+
+	private static final Attribute A_SET_ATTR = new Attribute("A", SET);
+
+	private static final Attribute A_UNSET_ATTR = new Attribute("A", UNSET);
+
+	private static final Attribute B_SET_ATTR = new Attribute("B", SET);
+
+	private static final Attribute B_UNSET_ATTR = new Attribute("B", UNSET);
+
+	private static final Attribute C_VALUE_ATTR = new Attribute("C", "value");
+
+	private static final Attribute C_VALUE2_ATTR = new Attribute("C", "value2");
+
+	private InputStream is;
+
+	@After
+	public void after() throws IOException {
+		if (is != null)
+			is.close();
+	}
+
+	@Test
+	public void testBasic() throws IOException {
+		String attributeFileContent = "*.type1 A -B C=value\n"
+				+ "*.type2 -A B C=value2";
+
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node,
+				asSet(A_SET_ATTR, B_UNSET_ATTR, C_VALUE_ATTR));
+		assertAttribute("file.type2", node,
+				asSet(A_UNSET_ATTR, B_SET_ATTR, C_VALUE2_ATTR));
+	}
+
+	@Test
+	public void testNegativePattern() throws IOException {
+		String attributeFileContent = "!*.type1 A -B C=value\n"
+				+ "!*.type2 -A B C=value2";
+
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node, Collections.<Attribute> emptySet());
+		assertAttribute("file.type2", node, Collections.<Attribute> emptySet());
+	}
+
+	@Test
+	public void testEmptyNegativeAttributeKey() throws IOException {
+		String attributeFileContent = "*.type1 - \n" //
+				+ "*.type2 -   -A";
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node, Collections.<Attribute> emptySet());
+		assertAttribute("file.type2", node, asSet(A_UNSET_ATTR));
+	}
+
+	@Test
+	public void testEmptyValueKey() throws IOException {
+		String attributeFileContent = "*.type1 = \n" //
+				+ "*.type2 =value\n"//
+				+ "*.type3 attr=\n";
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node, Collections.<Attribute> emptySet());
+		assertAttribute("file.type2", node, Collections.<Attribute> emptySet());
+		assertAttribute("file.type3", node, asSet(new Attribute("attr", "")));
+	}
+
+	@Test
+	public void testEmptyLine() throws IOException {
+		String attributeFileContent = "*.type1 A -B C=value\n" //
+				+ "\n" //
+				+ "    \n" //
+				+ "*.type2 -A B C=value2";
+
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node,
+				asSet(A_SET_ATTR, B_UNSET_ATTR, C_VALUE_ATTR));
+		assertAttribute("file.type2", node,
+				asSet(A_UNSET_ATTR, B_SET_ATTR, C_VALUE2_ATTR));
+	}
+
+	@Test
+	public void testTabSeparator() throws IOException {
+		String attributeFileContent = "*.type1 \tA -B\tC=value\n"
+				+ "*.type2\t -A\tB C=value2\n" //
+				+ "*.type3  \t\t   B\n" //
+				+ "*.type3\t-A";//
+
+		is = new ByteArrayInputStream(attributeFileContent.getBytes());
+		AttributesNode node = new AttributesNode();
+		node.parse(is);
+		assertAttribute("file.type1", node,
+				asSet(A_SET_ATTR, B_UNSET_ATTR, C_VALUE_ATTR));
+		assertAttribute("file.type2", node,
+				asSet(A_UNSET_ATTR, B_SET_ATTR, C_VALUE2_ATTR));
+		assertAttribute("file.type3", node, asSet(A_UNSET_ATTR, B_SET_ATTR));
+	}
+
+	private void assertAttribute(String path, AttributesNode node,
+			Set<Attribute> attrs) {
+		HashMap<String, Attribute> attributes = new HashMap<String, Attribute>();
+		node.getAttributes(path, false, attributes);
+		assertEquals(attrs, new HashSet<Attribute>(attributes.values()));
+	}
+
+	static Set<Attribute> asSet(Attribute... attrs) {
+		Set<Attribute> result = new HashSet<Attribute>();
+		for (Attribute attr : attrs)
+			result.add(attr);
+		return result;
+	}
+
+}
