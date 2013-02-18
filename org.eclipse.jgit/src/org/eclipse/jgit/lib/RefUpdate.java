096d43ff4c76bd0596c7d094f42763edb2694a60
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-02-18 20:25:00 +0100

    Speed up clone/fetch with large number of refs
    Instead of re-reading all refs after each update, only
    read all refs once and check all before update.
    
    Change-Id: I17d0b3ccc27f868c8497607d8e57bf7082e65ba3

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RefUpdate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RefUpdate.java
@@ -178,6 +178,8 @@ public abstract class RefUpdate {
 	 */
 	private boolean detachingSymbolicRef;
 
+	private boolean checkConflicting = true;
+
 	/**
 	 * Construct a new update operation for the reference.
 	 * <p>
@@ -564,7 +566,7 @@ public abstract class RefUpdate {
 	public Result link(String target) throws IOException {
 		if (!target.startsWith(Constants.R_REFS))
 			throw new IllegalArgumentException(MessageFormat.format(JGitText.get().illegalArgumentNotA, Constants.R_REFS));
-		if (getRefDatabase().isNameConflicting(getName()))
+		if (checkConflicting && getRefDatabase().isNameConflicting(getName()))
 			return Result.LOCK_FAILURE;
 		try {
 			if (!tryLock(false))
@@ -598,7 +600,7 @@ public abstract class RefUpdate {
 		RevObject newObj;
 		RevObject oldObj;
 
-		if (getRefDatabase().isNameConflicting(getName()))
+		if (checkConflicting && getRefDatabase().isNameConflicting(getName()))
 			return Result.LOCK_FAILURE;
 		try {
 			if (!tryLock(true))
@@ -628,6 +630,16 @@ public abstract class RefUpdate {
 		} finally {
 			unlock();
 		}
+	}
+
+	/**
+	 * Enable/disable the check for conflicting ref names. By default conflicts
+	 * are checked explicitly.
+	 *
+	 * @param check
+	 */
+	public void setCheckConflicting(boolean check) {
+		checkConflicting = check;
 	}
 
 	private static RevObject safeParse(final RevWalk rw, final AnyObjectId id)
