8f088390f3a6a8d504d8f04806d101f20d8116c4
Dave Borowitz <dborowitz@google.com>
2013-03-07 09:41:05 -0800

    Add a NameRevCommand for describing IDs in terms of refnames
    The walk logic does not use RevWalk because it needs to walk all paths
    to each of the requested commits, keeping track of each path along which
    the commit was found in the RevCommit subclass. From these paths, a
    single "best" path is chosen based on the total path length, with a
    penalty applied for paths that traverse merges.
    
    This functionality parallels "git name-rev".
    
    Change-Id: I92bfb47dd16c898313d2ee525395609c3bf72ebe

--- org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
@@ -633,6 +633,16 @@ public class Git {
 	}
 
 	/**
+	 * Returns a command object to find human-readable names of revisions.
+	 *
+	 * @return a {@link NameRevCommand}.
+	 * @since 2.3
+	 */
+	public NameRevCommand nameRev() {
+		return new NameRevCommand(repo);
+	}
+
+	/**
 	 * @return the git repository this class is interacting with
 	 */
 	public Repository getRepository() {
