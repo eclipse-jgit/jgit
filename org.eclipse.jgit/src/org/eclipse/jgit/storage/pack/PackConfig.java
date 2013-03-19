e02708a8b37b7d58976cd66bc017c91377053a76
Edwin Kempin <edwin.kempin@sap.com>
2013-03-19 06:59:52 +0100

    Fix formatting of PackConfig.toString() & GC.RepoStatistics.toString()
    Change-Id: I7e0c74ecfd0e0615d10fb582b2897d33be23440a
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackConfig.java
@@ -689,18 +689,18 @@ public class PackConfig {
 	public String toString() {
 		final StringBuilder b = new StringBuilder();
 		b.append("maxDeltaDepth=").append(getMaxDeltaDepth()); //$NON-NLS-1$
-		b.append("deltaSearchWindowSize=").append(getDeltaSearchWindowSize()); //$NON-NLS-1$
-		b.append("deltaSearchMemoryLimit=").append(getDeltaSearchMemoryLimit()); //$NON-NLS-1$
-		b.append("deltaCacheSize=").append(getDeltaCacheSize()); //$NON-NLS-1$
-		b.append("deltaCacheLimit=").append(getDeltaCacheLimit()); //$NON-NLS-1$
-		b.append("compressionLevel=").append(getCompressionLevel()); //$NON-NLS-1$
-		b.append("indexVersion=").append(getIndexVersion()); //$NON-NLS-1$
-		b.append("bigFileThreshold=").append(getBigFileThreshold()); //$NON-NLS-1$
-		b.append("threads=").append(getThreads()); //$NON-NLS-1$
-		b.append("reuseDeltas=").append(isReuseDeltas()); //$NON-NLS-1$
-		b.append("reuseObjects=").append(isReuseObjects()); //$NON-NLS-1$
-		b.append("deltaCompress=").append(isDeltaCompress()); //$NON-NLS-1$
-		b.append("buildBitmaps=").append(isBuildBitmaps()); //$NON-NLS-1$
+		b.append(", deltaSearchWindowSize=").append(getDeltaSearchWindowSize()); //$NON-NLS-1$
+		b.append(", deltaSearchMemoryLimit=").append(getDeltaSearchMemoryLimit()); //$NON-NLS-1$
+		b.append(", deltaCacheSize=").append(getDeltaCacheSize()); //$NON-NLS-1$
+		b.append(", deltaCacheLimit=").append(getDeltaCacheLimit()); //$NON-NLS-1$
+		b.append(", compressionLevel=").append(getCompressionLevel()); //$NON-NLS-1$
+		b.append(", indexVersion=").append(getIndexVersion()); //$NON-NLS-1$
+		b.append(", bigFileThreshold=").append(getBigFileThreshold()); //$NON-NLS-1$
+		b.append(", threads=").append(getThreads()); //$NON-NLS-1$
+		b.append(", reuseDeltas=").append(isReuseDeltas()); //$NON-NLS-1$
+		b.append(", reuseObjects=").append(isReuseObjects()); //$NON-NLS-1$
+		b.append(", deltaCompress=").append(isDeltaCompress()); //$NON-NLS-1$
+		b.append(", buildBitmaps=").append(isBuildBitmaps()); //$NON-NLS-1$
 		return b.toString();
 	}
 }
