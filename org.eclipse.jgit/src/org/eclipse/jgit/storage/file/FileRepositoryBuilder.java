0c0ea6dc05cd40e3432708cd70287038385a71e1
Matthias Sohn <matthias.sohn@sap.com>
2013-03-19 23:26:17 +0100

    Add convenience factory method for most used builder pattern
    This will simplify to adapt EGit to the removal of FileRepository from
    jgit's public API in change I2ab1327c202ef2003565e1b0770a583970e432e9.
    
    Change-Id: If98b0b97e8f13a94d4ea7ba1be0f90d82b0fba4b
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepositoryBuilder.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepositoryBuilder.java
@@ -50,7 +50,6 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.BaseRepositoryBuilder;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.util.SystemReader;
 
 /**
  * Constructs a {@link FileRepository}.
@@ -93,5 +92,20 @@ public class FileRepositoryBuilder extends
 		if (isMustExist() && !repo.getObjectDatabase().exists())
 			throw new RepositoryNotFoundException(getGitDir());
 		return repo;
+	}
+
+	/**
+	 * Convenience factory method to construct a {@link FileRepository}.
+	 *
+	 * @param gitDir
+	 *            {@code GIT_DIR}, the repository meta directory.
+	 * @return a repository matching this configuration.
+	 * @throws IOException
+	 *             the repository could not be accessed to configure the rest of
+	 *             the builder's parameters.
+	 */
+	public static Repository create(File gitDir) throws IOException {
+		return new FileRepositoryBuilder().setGitDir(gitDir).readEnvironment()
+				.build();
 	}
 }
