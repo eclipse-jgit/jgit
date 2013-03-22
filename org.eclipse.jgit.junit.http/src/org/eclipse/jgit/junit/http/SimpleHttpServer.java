9b63f324413fb093313c3f33e52627b1c97cd6f6
Matthias Sohn <matthias.sohn@sap.com>
2013-03-22 21:36:10 +0100

    SimpleHttpServer API shouldn't expose internals
    Change-Id: I5963ae720f33cb148de08b4c64d02c81d6791139
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.junit.http/src/org/eclipse/jgit/junit/http/SimpleHttpServer.java
+++ org.eclipse.jgit.junit.http/src/org/eclipse/jgit/junit/http/SimpleHttpServer.java
@@ -52,7 +52,6 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.http.server.GitServlet;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.resolver.RepositoryResolver;
@@ -66,11 +65,11 @@ public class SimpleHttpServer {
 
 	AppServer server;
 
-	private final FileRepository db;
+	private final Repository db;
 
 	private URIish uri;
 
-	public SimpleHttpServer(FileRepository repository) {
+	public SimpleHttpServer(Repository repository) {
 		this.db = repository;
 		server = new AppServer();
 	}
@@ -109,7 +108,7 @@ public class SimpleHttpServer {
 		return ctx;
 	}
 
-	private static String nameOf(final FileRepository db) {
+	private static String nameOf(final Repository db) {
 		return db.getDirectory().getName();
 	}
 
