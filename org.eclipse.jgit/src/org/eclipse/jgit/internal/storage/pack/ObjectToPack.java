876a2ffb21372091f6ac321bc6d70726009131d4
Shawn Pearce <spearce@spearce.org>
2013-04-04 11:03:30 -0700

    Remove unused method isDeltaAttempted()
    This flag is never checked on its own. It is only checked as part
    of a pair through the doNotAttemptDelta() method. Delete the method
    so there is less confusion about the flag being used on its own.
    
    Change-Id: Id7088caa649599f4f11d633412c2a2af0fd45dd8

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/ObjectToPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/ObjectToPack.java
@@ -258,10 +258,6 @@ public class ObjectToPack extends PackedObjectInfo {
 		return (flags & ATTEMPT_DELTA_MASK) == ATTEMPT_DELTA_MASK;
 	}
 
-	boolean isDeltaAttempted() {
-		return (flags & DELTA_ATTEMPTED) != 0;
-	}
-
 	void setDeltaAttempted(boolean deltaAttempted) {
 		if (deltaAttempted)
 			flags |= DELTA_ATTEMPTED;
