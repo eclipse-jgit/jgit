ec56e836aaa2afdadfac5c1e88440641d194c636
Robin Stocker <robin@nibor.org>
2013-04-18 10:55:23 +0200

    Fix examples with refs/heads/ in RefSpec Javadoc
    Change-Id: I06c1c7242a1b4c8f499c27a598cca714803799b7

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/RefSpec.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/RefSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
+ * Copyright (C) 2008, 2013 Shawn O. Pearce <spearce@spearce.org>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -106,13 +106,13 @@ public class RefSpec implements Serializable {
 	 * <p>
 	 * Specifications are typically one of the following forms:
 	 * <ul>
-	 * <li><code>refs/head/master</code></li>
-	 * <li><code>refs/head/master:refs/remotes/origin/master</code></li>
-	 * <li><code>refs/head/*:refs/remotes/origin/*</code></li>
-	 * <li><code>+refs/head/master</code></li>
-	 * <li><code>+refs/head/master:refs/remotes/origin/master</code></li>
-	 * <li><code>+refs/head/*:refs/remotes/origin/*</code></li>
-	 * <li><code>:refs/head/master</code></li>
+	 * <li><code>refs/heads/master</code></li>
+	 * <li><code>refs/heads/master:refs/remotes/origin/master</code></li>
+	 * <li><code>refs/heads/*:refs/remotes/origin/*</code></li>
+	 * <li><code>+refs/heads/master</code></li>
+	 * <li><code>+refs/heads/master:refs/remotes/origin/master</code></li>
+	 * <li><code>+refs/heads/*:refs/remotes/origin/*</code></li>
+	 * <li><code>:refs/heads/master</code></li>
 	 * </ul>
 	 *
 	 * @param spec
