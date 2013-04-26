bea5cc26af066c8cc9d203cf1927f7e0efeb2a24
Jonathan Nieder <jrn@google.com>
2013-04-26 13:43:23 -0700

    Add internal porcelain-style API for ArchiveCommand
    One step closer to exposing archive creation functionality in a
    org.eclipse.jgit.archive bundle.
    
    Change-Id: If0ebb2417a941d9d3fc0d3f444316d0d1c494ff3

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Archive.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Archive.java
@@ -43,148 +43,33 @@
 
 package org.eclipse.jgit.pgm;
 
-import java.lang.String;
-import java.lang.System;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.EnumMap;
-import java.util.Map;
-import java.text.MessageFormat;
-
-import org.apache.commons.compress.archivers.ArchiveOutputStream;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
-import org.apache.commons.compress.archivers.tar.TarConstants;
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
-import org.eclipse.jgit.lib.FileMode;
-import org.eclipse.jgit.lib.MutableObjectId;
-import org.eclipse.jgit.lib.ObjectLoader;
-import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.pgm.CLIText;
 import org.eclipse.jgit.pgm.TextBuiltin;
-import org.eclipse.jgit.treewalk.AbstractTreeIterator;
-import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.pgm.archive.ArchiveCommand;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 @Command(common = true, usage = "usage_archive")
 class Archive extends TextBuiltin {
 	@Argument(index = 0, metaVar = "metaVar_treeish")
-	private AbstractTreeIterator tree;
+	private ObjectId tree;
 
 	@Option(name = "--format", metaVar = "metaVar_archiveFormat", usage = "usage_archiveFormat")
-	private Format format = Format.ZIP;
+	private ArchiveCommand.Format format = ArchiveCommand.Format.ZIP;
 
 	@Override
 	protected void run() throws Exception {
-		final MutableObjectId idBuf = new MutableObjectId();
-		final Archiver fmt = formats.get(format);
-
 		if (tree == null)
 			throw die(CLIText.get().treeIsRequired);
 
-		final ArchiveOutputStream outa = fmt.createArchiveOutputStream(outs);
-		final TreeWalk walk = new TreeWalk(db);
-		final ObjectReader reader = walk.getObjectReader();
-
+		final ArchiveCommand cmd = new ArchiveCommand(db);
 		try {
-			walk.reset();
-			walk.addTree(tree);
-			walk.setRecursive(true);
-			while (walk.next()) {
-				final String name = walk.getPathString();
-				final FileMode mode = walk.getFileMode(0);
-
-				if (mode == FileMode.TREE)
-					// ZIP entries for directories are optional.
-					// Leave them out, mimicking "git archive".
-					continue;
-
-				walk.getObjectId(idBuf, 0);
-				fmt.putEntry(name, mode, reader.open(idBuf), outa);
-			}
+			cmd.setTree(tree)
+					.setFormat(format)
+					.setOutputStream(outs).call();
 		} finally {
-			reader.release();
-			outa.close();
+			cmd.release();
 		}
-	}
-
-	static private void warnArchiveEntryModeIgnored(String name) {
-		System.err.println(MessageFormat.format( //
-				CLIText.get().archiveEntryModeIgnored, //
-				name));
-	}
-
-	public enum Format {
-		ZIP,
-		TAR
-	}
-
-	private static interface Archiver {
-		ArchiveOutputStream createArchiveOutputStream(OutputStream s);
-		void putEntry(String path, FileMode mode, //
-				ObjectLoader loader, ArchiveOutputStream out) //
-				throws IOException;
-	}
-
-	private static final Map<Format, Archiver> formats;
-	static {
-		Map<Format, Archiver> fmts = new EnumMap<Format, Archiver>(Format.class);
-		fmts.put(Format.ZIP, new Archiver() {
-			public ArchiveOutputStream createArchiveOutputStream(OutputStream s) {
-				return new ZipArchiveOutputStream(s);
-			}
-
-			public void putEntry(String path, FileMode mode, //
-					ObjectLoader loader, ArchiveOutputStream out) //
-					throws IOException {
-				final ZipArchiveEntry entry = new ZipArchiveEntry(path);
-
-				if (mode == FileMode.REGULAR_FILE) {
-					// ok
-				} else if (mode == FileMode.EXECUTABLE_FILE
-						|| mode == FileMode.SYMLINK) {
-					entry.setUnixMode(mode.getBits());
-				} else {
-					warnArchiveEntryModeIgnored(path);
-				}
-				entry.setSize(loader.getSize());
-				out.putArchiveEntry(entry);
-				loader.copyTo(out);
-				out.closeArchiveEntry();
-			}
-		});
-		fmts.put(Format.TAR, new Archiver() {
-			public ArchiveOutputStream createArchiveOutputStream(OutputStream s) {
-				return new TarArchiveOutputStream(s);
-			}
-
-			public void putEntry(String path, FileMode mode, //
-					ObjectLoader loader, ArchiveOutputStream out) //
-					throws IOException {
-				if (mode == FileMode.SYMLINK) {
-					final TarArchiveEntry entry = new TarArchiveEntry( //
-							path, TarConstants.LF_SYMLINK);
-					entry.setLinkName(new String( //
-							loader.getCachedBytes(100), "UTF-8")); //$NON-NLS-1$
-					out.putArchiveEntry(entry);
-					out.closeArchiveEntry();
-					return;
-				}
-
-				final TarArchiveEntry entry = new TarArchiveEntry(path);
-				if (mode == FileMode.REGULAR_FILE ||
-				    mode == FileMode.EXECUTABLE_FILE)
-					entry.setMode(mode.getBits());
-				else
-					warnArchiveEntryModeIgnored(path);
-				entry.setSize(loader.getSize());
-				out.putArchiveEntry(entry);
-				loader.copyTo(out);
-				out.closeArchiveEntry();
-			}
-		});
-		formats = fmts;
 	}
 }
