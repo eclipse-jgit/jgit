83aefbf949828e0c4678e1b2467e0d9047791086
Dave Borowitz <dborowitz@google.com>
2013-05-01 10:20:31 -0700

    Add a method to ObjectInserter to read back inserted objects
    In the DFS implementation, flushing an inserter writes a new pack to
    the storage system and is potentially very slow, but was the only way
    to ensure previously-inserted objects were available. For some tasks,
    like performing a series of three-way merges, the total size of all
    inserted objects may be small enough to avoid flushing the in-memory
    buffered data. DfsOutputStream already provides a read method to read
    back from the not-yet-flushed data, so use this to provide an
    ObjectReader in the DFS case.
    
    In the file-backed case, objects are written out loosely on the fly,
    so the implementation can just return the existing WindowCursor.
    
    Change-Id: I454fdfb88f4d215e31b7da2b2a069853b197b3dd

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectInserter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectInserter.java
@@ -81,6 +81,11 @@ public abstract class ObjectInserter {
 		}
 
 		@Override
+		public ObjectReader newReader() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
 		public void flush() throws IOException {
 			// Do nothing.
 		}
@@ -134,6 +139,10 @@ public abstract class ObjectInserter {
 
 		public PackParser newPackParser(InputStream in) throws IOException {
 			return delegate().newPackParser(in);
+		}
+
+		public ObjectReader newReader() {
+			return delegate().newReader();
 		}
 
 		public void flush() throws IOException {
@@ -379,6 +388,15 @@ public abstract class ObjectInserter {
 	 *             parse objects into the ObjectDatabase.
 	 */
 	public abstract PackParser newPackParser(InputStream in) throws IOException;
+
+	/**
+	 * @return a reader for objects that may have been written by this inserter,
+	 *         without having to flush first. Should only be used from the same
+	 *         thread as this inserter; objects written by this inserter may not
+	 *         be visible to {@code this.newReader().newReader()}.
+	 * @since 3.0
+	 */
+	public abstract ObjectReader newReader();
 
 	/**
 	 * Make all inserted objects visible.
