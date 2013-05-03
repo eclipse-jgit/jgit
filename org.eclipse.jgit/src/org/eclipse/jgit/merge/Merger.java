8839f4bb85cd991676973e6bf717145f8bbfce13
Dave Borowitz <dborowitz@google.com>
2013-05-03 11:45:36 -0700

    Add a Merger.merge method that makes flushing optional
    This allows callers performing multiple separate merges to reuse a
    single ObjectInserter without flushing the inserter on each iteration
    (which can be slow in the DFS case).
    
    Change-Id: Icaff7d2bc2c20c873ce5a7d9af5002da84ae1c2b

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
@@ -160,6 +160,34 @@ public abstract class Merger {
 	 *             be written to the Repository.
 	 */
 	public boolean merge(final AnyObjectId... tips) throws IOException {
+		return merge(true, tips);
+	}
+
+	/**
+	 * Merge together two or more tree-ish objects.
+	 * <p>
+	 * Any tree-ish may be supplied as inputs. Commits and/or tags pointing at
+	 * trees or commits may be passed as input objects.
+	 *
+	 * @param flush
+	 *            whether to flush the underlying object inserter when finished to
+	 *            store any content-merged blobs and virtual merged bases; if
+	 *            false, callers are responsible for flushing.
+	 * @param tips
+	 *            source trees to be combined together. The merge base is not
+	 *            included in this set.
+	 * @return true if the merge was completed without conflicts; false if the
+	 *         merge strategy cannot handle this merge or there were conflicts
+	 *         preventing it from automatically resolving all paths.
+	 * @throws IncorrectObjectTypeException
+	 *             one of the input objects is not a commit, but the strategy
+	 *             requires it to be a commit.
+	 * @throws IOException
+	 *             one or more sources could not be read, or outputs could not
+	 *             be written to the Repository.
+	 */
+	public boolean merge(final boolean flush, final AnyObjectId... tips)
+			throws IOException {
 		sourceObjects = new RevObject[tips.length];
 		for (int i = 0; i < tips.length; i++)
 			sourceObjects[i] = walk.parseAny(tips[i]);
@@ -179,11 +207,11 @@ public abstract class Merger {
 
 		try {
 			boolean ok = mergeImpl();
-			if (ok && inserter != null)
+			if (ok && inserter != null && flush)
 				inserter.flush();
 			return ok;
 		} finally {
-			if (inserter != null)
+			if (inserter != null && flush)
 				inserter.release();
 			reader.release();
 		}
