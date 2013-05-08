cbc1907b846c3b1bcb36066e4252e1a696064043
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-05-08 23:52:52 +0200

    Fix the parameters to an exception
    A parenthesis was in the wrong place passing arguments to the wrong
    format call.
    
    Change-Id: I4cb9642f08b58c39033c3a81dab4bd56bebf4fd2

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
@@ -626,13 +626,12 @@ public class GC {
 			        continue;
 			      default:
 					throw new IOException(MessageFormat.format(
-							JGitText.get().corruptObjectInvalidMode3, String
-									.format("%o", Integer.valueOf(treeWalk //$NON-NLS-1$
-											.getRawMode(0)),
-											(objectId == null) ? "null" //$NON-NLS-1$
-													: objectId.name(), treeWalk
-											.getPathString(), repo
-											.getIndexFile())));
+							JGitText.get().corruptObjectInvalidMode3, //
+							String.format("%o", //$NON-NLS-1$
+									Integer.valueOf(treeWalk.getRawMode(0))),
+							(objectId == null) ? "null" : objectId.name(), //$NON-NLS-1$
+							treeWalk.getPathString(), //
+							repo.getIndexFile()));
 			    }
 			  }
 			return ret;
@@ -703,7 +702,6 @@ public class GC {
 			}
 
 			// write the packindex
-			@SuppressWarnings("resource")
 			FileChannel idxChannel = new FileOutputStream(tmpIdx).getChannel();
 			OutputStream idxStream = Channels.newOutputStream(idxChannel);
 			try {
