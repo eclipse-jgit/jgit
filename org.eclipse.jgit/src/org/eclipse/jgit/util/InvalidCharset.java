81e3ea5a963ca9429b897117789d70ce4fcbef68
Marc Strapetz <marc.strapetz@syntevo.com>
2013-05-27 10:32:47 +0200

    RawParseUtils: be tolerant in case of invalid 'charset', as CGit is
    Change-Id: I482e9e230b0a801799ac16d61a700bd01c3b940f
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>‌

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/InvalidCharset.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2014, Marc Strapetz <marc.strapetz@syntevo.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.util;
+
+import java.nio.*;
+import java.nio.charset.*;
+
+public class InvalidCharset extends Charset {
+
+	public static final InvalidCharset INSTANCE = new InvalidCharset();
+
+	private InvalidCharset() {
+		super("Invalid", new String[0]);
+	}
+
+	@Override
+	public boolean contains(Charset cs) {
+		return false;
+	}
+
+	@Override
+	public CharsetDecoder newDecoder() {
+		return new CharsetDecoder(this, 1, 1) {
+			@Override
+			protected CoderResult decodeLoop(ByteBuffer in, CharBuffer out) {
+				if (in.remaining() > 0) {
+					return CoderResult.unmappableForLength(1);
+				}
+
+				return CoderResult.UNDERFLOW;
+			}
+		};
+	}
+
+	@Override
+	public CharsetEncoder newEncoder() {
+		return new CharsetEncoder(this, 4, 4) {
+			@Override
+			protected CoderResult encodeLoop(CharBuffer in, ByteBuffer out) {
+				if (in.remaining() > 0) {
+					return CoderResult.unmappableForLength(1);
+				}
+
+				return CoderResult.UNDERFLOW;
+			}
+		};
+	}
+}
