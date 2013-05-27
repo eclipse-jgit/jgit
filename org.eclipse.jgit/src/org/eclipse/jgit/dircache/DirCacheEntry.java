49d82e7cf2aa1ddc987ec0ec19e67c27f2f559eb
Marc Strapetz <marc.strapetz@syntevo.com>
2013-05-27 10:32:47 +0200

    DirCacheEntry: access to raw path
    Change-Id: I5d1f6c4b5b3d7a971367cdd0cc9fa4022dff5b85
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>‌

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
@@ -635,6 +635,15 @@ public class DirCacheEntry {
 	}
 
 	/**
+	 * Get a copy of the entry's raw path bytes.
+	 *
+	 * @return raw path bytes.
+	 */
+	public byte[] getRawPath() {
+		return path.clone();
+	}
+
+	/**
 	 * Use for debugging only !
 	 */
 	@SuppressWarnings("nls")
