cd86ace275a0f15590c4b45f932cb1fddbdf7eef
Dave Borowitz <dborowitz@google.com>
2013-06-18 15:31:17 -0700

    UploadPack: configure RequestPolicy with TransportConfig
    C git 1.8.2 supports setting the equivalent of RequestPolicy.TIP with
    uploadpack.allowtipsha1. Parse this into TransportConfig and use it
    from UploadPack. An explicitly set RequestPolicy overrides the config,
    and the policy may still be upgraded on a unidirectional connection to
    avoid races.
    
    Defer figuring out the effective RequestPolicy to later in the
    process. This is a minor semantic change to fix a bug: previously,
    calling setRequestPolicy(ADVERTISED) _after_ calling
    setBiDirectionalPipe(true) would have reintroduced the race condition
    otherwise fixed by 01888db892aa9590862d886c01f3b293140db153.
    
    Change-Id: I264e028a76574434cecb34904d9f5944b290df78

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -176,6 +176,9 @@ public class UploadPack {
 	/** Configuration to pass into the PackWriter. */
 	private PackConfig packConfig;
 
+	/** Configuration for various transfer options. */
+	private TransferConfig transferConfig;
+
 	/** Timeout in seconds to wait for client interaction. */
 	private int timeout;
 
@@ -263,7 +266,7 @@ public class UploadPack {
 
 	private final RevFlagSet SAVE;
 
-	private RequestPolicy requestPolicy = RequestPolicy.ADVERTISED;
+	private RequestPolicy requestPolicy;
 
 	private MultiAck multiAck = MultiAck.OFF;
 
@@ -295,6 +298,8 @@ public class UploadPack {
 		SAVE.add(PEER_HAS);
 		SAVE.add(COMMON);
 		SAVE.add(SATISFIED);
+
+		transferConfig = new TransferConfig(db);
 	}
 
 	/** @return the repository this upload is reading from. */
@@ -373,10 +378,6 @@ public class UploadPack {
 	 */
 	public void setBiDirectionalPipe(final boolean twoWay) {
 		biDirectionalPipe = twoWay;
-		if (!biDirectionalPipe &&
-				(requestPolicy == RequestPolicy.ADVERTISED
-				|| requestPolicy == RequestPolicy.TIP))
-			requestPolicy = RequestPolicy.REACHABLE_COMMIT;
 	}
 
 	/** @return policy used by the service to validate client requests. */
@@ -392,9 +393,10 @@ public class UploadPack {
 	 *            object that a reference directly points to. This may be relaxed
 	 *            to {@link RequestPolicy#REACHABLE_COMMIT} when callers
 	 *            have {@link #setBiDirectionalPipe(boolean)} set to false.
+	 *            Overrides any policy specified in a {@link TransferConfig}.
 	 */
 	public void setRequestPolicy(RequestPolicy policy) {
-		requestPolicy = policy != null ? policy : RequestPolicy.ADVERTISED;
+		requestPolicy = policy;
 	}
 
 	/** @return the hook used while advertising the refs to the client */
@@ -462,6 +464,15 @@ public class UploadPack {
 	 */
 	public void setPackConfig(PackConfig pc) {
 		this.packConfig = pc;
+	}
+
+	/**
+	 * @param tc
+	 *            configuration controlling transfer options. If null the source
+	 *            repository's settings will be used.
+	 */
+	public void setTransferConfig(TransferConfig tc) {
+		this.transferConfig = tc != null ? tc : new TransferConfig(db);
 	}
 
 	/** @return the configured logger. */
@@ -567,7 +578,24 @@ public class UploadPack {
 		return refs;
 	}
 
+	private RequestPolicy getEffectiveRequestPolicy() {
+		RequestPolicy rp;
+		if (requestPolicy != null)
+			rp = requestPolicy;
+		else if (transferConfig.isAllowTipSha1InWant())
+			rp = RequestPolicy.TIP;
+		else
+			rp = RequestPolicy.ADVERTISED;
+
+		if (!biDirectionalPipe
+				&& (rp == RequestPolicy.ADVERTISED || rp == RequestPolicy.TIP))
+			rp = RequestPolicy.REACHABLE_COMMIT;
+		return rp;
+	}
+
 	private void service() throws IOException {
+		requestPolicy = getEffectiveRequestPolicy();
+
 		if (biDirectionalPipe)
 			sendAdvertisedRefs(new PacketLineOutRefAdvertiser(pckOut));
 		else if (requestPolicy == RequestPolicy.ANY)
