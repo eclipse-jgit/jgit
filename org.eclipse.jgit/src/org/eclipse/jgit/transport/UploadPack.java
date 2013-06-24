0e3845e6442b8914da20108b9dc6217889922c3a
Dave Borowitz <dborowitz@google.com>
2013-06-23 17:02:29 -0700

    UploadPack: refactor want validation
    Associate each RequestPolicy with an implementation of a
    RequestValidator interface that contains the validation logic. The
    checkWants method is only called if there are wants that were not
    advertised, since clients may always request any advertised want
    according to the git protocol. Calling the method only once at the
    end of parsing the want list also means policy implementations need
    not be stateful.
    
    For the special handling of unidirectional pipes, pass a
    biDirectionalPipe flag into checkWants, which implementations can use
    to delegate to other implementations if necessary.
    
    Change-Id: I52a174999ac3a5aca46d3469cb0b81edd1710580

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -141,6 +141,32 @@ public class UploadPack {
 		ANY;
 	}
 
+	/** Validator for client requests. */
+	private interface RequestValidator {
+		/**
+		 * Check that a list of client wants against the request policy.
+		 *
+		 * @param db
+		 *            the repository.
+		 * @param walk
+		 *            the {@link UploadPack}'s walker instance.
+		 * @param advertised
+		 *            refs that were previously advertised.
+		 * @param wants
+		 *            objects the client requested that were not advertised.
+		 * @param biDirectionalPipe
+		 *            whether the connection is on a bi-directional pipe.
+		 *
+		 * @throws PackProtocolException
+		 *            if one or more wants is not valid.
+		 * @throws IOException
+		 *            if a low-level exception occurred.
+		 */
+		void checkWants(Repository db, RevWalk walk, Set<ObjectId> advertised,
+				List<RevObject> wants, boolean biDirectionalPipe)
+				throws PackProtocolException, IOException;
+	}
+
 	/** Data in the first line of a request, the line itself plus options. */
 	public static class FirstLine {
 		private final String line;
@@ -595,28 +621,7 @@ public class UploadPack {
 		return refs;
 	}
 
-	private RequestPolicy getEffectiveRequestPolicy() {
-		RequestPolicy rp;
-		if (requestPolicy != null)
-			rp = requestPolicy;
-		else if (transferConfig.isAllowTipSha1InWant())
-			rp = RequestPolicy.TIP;
-		else
-			rp = RequestPolicy.ADVERTISED;
-
-
-		if (!biDirectionalPipe) {
-			if (rp == RequestPolicy.ADVERTISED)
-				rp = RequestPolicy.REACHABLE_COMMIT;
-			else if (rp == RequestPolicy.TIP)
-				rp = RequestPolicy.REACHABLE_COMMIT_TIP;
-		}
-		return rp;
-	}
-
 	private void service() throws IOException {
-		requestPolicy = getEffectiveRequestPolicy();
-
 		if (biDirectionalPipe)
 			sendAdvertisedRefs(new PacketLineOutRefAdvertiser(pckOut));
 		else if (requestPolicy == RequestPolicy.ANY)
@@ -989,49 +994,13 @@ public class UploadPack {
 	private void parseWants() throws IOException {
 		AsyncRevObjectQueue q = walk.parseAny(wantIds, true);
 		try {
-			List<RevCommit> checkReachable = null;
-			Set<ObjectId> reachableFrom = null;
+			List<RevObject> notAdvertisedWants = null;
 			RevObject obj;
-			Set<ObjectId> tips = null;
 			while ((obj = q.next()) != null) {
 				if (!advertised.contains(obj)) {
-					switch (requestPolicy) {
-					case ADVERTISED:
-					default:
-						throw new PackProtocolException(MessageFormat.format(
-								JGitText.get().wantNotValid, obj));
-					case REACHABLE_COMMIT:
-						if (!(obj instanceof RevCommit)) {
-							throw new PackProtocolException(MessageFormat.format(
-								JGitText.get().wantNotValid, obj));
-						}
-						if (checkReachable == null) {
-							checkReachable = new ArrayList<RevCommit>();
-							reachableFrom = advertised;
-						}
-						checkReachable.add((RevCommit) obj);
-						break;
-					case TIP:
-						if (tips == null)
-							tips = refIdSet(db.getAllRefs().values());
-						if (!tips.contains(obj))
-							throw new PackProtocolException(MessageFormat.format(
-									JGitText.get().wantNotValid, obj));
-						break;
-					case REACHABLE_COMMIT_TIP:
-						if (!(obj instanceof RevCommit)) {
-							throw new PackProtocolException(MessageFormat.format(
-								JGitText.get().wantNotValid, obj));
-						}
-						if (checkReachable == null) {
-							checkReachable = new ArrayList<RevCommit>();
-							reachableFrom = refIdSet(db.getAllRefs().values());
-						}
-						checkReachable.add((RevCommit) obj);
-						break;
-					case ANY:
-						break;
-					}
+					if (notAdvertisedWants == null)
+						notAdvertisedWants = new ArrayList<RevObject>();
+					notAdvertisedWants.add(obj);
 				}
 				want(obj);
 
@@ -1043,8 +1012,9 @@ public class UploadPack {
 						want(obj);
 				}
 			}
-			if (checkReachable != null)
-				checkNotAdvertisedWants(checkReachable, reachableFrom);
+			if (notAdvertisedWants != null)
+				getRequestValidator().checkWants(db, walk, advertised,
+						notAdvertisedWants, biDirectionalPipe);
 			wantIds.clear();
 		} catch (MissingObjectException notFound) {
 			ObjectId id = notFound.getObjectId();
@@ -1062,8 +1032,83 @@ public class UploadPack {
 		}
 	}
 
-	private void checkNotAdvertisedWants(List<RevCommit> notAdvertisedWants,
-			Set<ObjectId> reachableFrom)
+	private RequestValidator getRequestValidator() {
+		switch (requestPolicy) {
+			case ADVERTISED:
+			default:
+				return new AdvertisedRequestValidator();
+			case REACHABLE_COMMIT:
+				return new ReachableCommitRequestValidator();
+			case TIP:
+				return new TipRequestValidator();
+			case REACHABLE_COMMIT_TIP:
+				return new ReachableCommitTipRequestValidator();
+			case ANY:
+				return new AnyRequestValidator();
+		}
+	}
+
+	private static class AdvertisedRequestValidator implements RequestValidator {
+		public void checkWants(Repository db, RevWalk walk,
+				Set<ObjectId> advertised, List<RevObject> wants,
+				boolean biDirectionalPipe) throws PackProtocolException, IOException {
+			if (biDirectionalPipe)
+				new ReachableCommitRequestValidator().checkWants(
+						db, walk, advertised, wants, biDirectionalPipe);
+			else if (!wants.isEmpty())
+				throw new PackProtocolException(MessageFormat.format(
+						JGitText.get().wantNotValid, wants.iterator().next().name()));
+		}
+	}
+
+	private static class ReachableCommitRequestValidator
+			implements RequestValidator {
+		public void checkWants(Repository db, RevWalk walk,
+				Set<ObjectId> advertised, List<RevObject> wants,
+				boolean biDirectionalPipe) throws PackProtocolException, IOException {
+			checkNotAdvertisedWants(walk, wants, advertised);
+		}
+	}
+
+	private static class TipRequestValidator implements RequestValidator {
+		public void checkWants(Repository db, RevWalk walk,
+				Set<ObjectId> advertised, List<RevObject> wants,
+				boolean biDirectionalPipe) throws PackProtocolException, IOException {
+			if (biDirectionalPipe) {
+				new ReachableCommitTipRequestValidator().checkWants(
+						db, walk, advertised, wants, biDirectionalPipe);
+				return;
+			}
+			if (!wants.isEmpty()) {
+				Set<ObjectId> refIds = refIdSet(db.getAllRefs().values());
+				for (RevObject obj : wants) {
+					if (!refIds.contains(obj))
+						throw new PackProtocolException(MessageFormat.format(
+								JGitText.get().wantNotValid, obj.name()));
+				}
+			}
+		}
+	}
+
+	private static class ReachableCommitTipRequestValidator
+			implements RequestValidator {
+		public void checkWants(Repository db, RevWalk walk,
+				Set<ObjectId> advertised, List<RevObject> wants,
+				boolean biDirectionalPipe) throws PackProtocolException, IOException {
+			checkNotAdvertisedWants(walk, wants, refIdSet(db.getAllRefs().values()));
+		}
+	}
+
+	private static class AnyRequestValidator implements RequestValidator {
+		public void checkWants(Repository db, RevWalk walk,
+				Set<ObjectId> advertised, List<RevObject> wants,
+				boolean biDirectionalPipe) throws PackProtocolException, IOException {
+			// All requests are valid.
+		}
+	}
+
+	private static void checkNotAdvertisedWants(RevWalk walk,
+			List<RevObject> notAdvertisedWants, Set<ObjectId> reachableFrom)
 			throws MissingObjectException, IncorrectObjectTypeException, IOException {
 		// Walk the requested commits back to the provided set of commits. If any
 		// commit exists, a branch was deleted or rewound and the repository owner
@@ -1071,8 +1116,12 @@ public class UploadPack {
 		// into an advertised branch it will be marked UNINTERESTING and no commits
 		// return.
 
-		for (RevCommit c : notAdvertisedWants)
-			walk.markStart(c);
+		for (RevObject obj : notAdvertisedWants) {
+			if (!(obj instanceof RevCommit))
+				throw new PackProtocolException(MessageFormat.format(
+					JGitText.get().wantNotValid, obj.name()));
+			walk.markStart((RevCommit) obj);
+		}
 		for (ObjectId id : reachableFrom) {
 			try {
 				walk.markUninteresting(walk.parseCommit(id));
