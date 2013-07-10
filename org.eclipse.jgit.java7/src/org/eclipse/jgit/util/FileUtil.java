feb211e4d5ff9a47c0d1b63a63487443b18651a1
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-07-10 17:33:50 +0200

    Length of symbolic link is the number of bytes, not characters
    Change-Id: I6b615f0d5da4339f1f23a29bcaeb80f0346f5764

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
@@ -56,6 +56,7 @@ import java.nio.file.attribute.PosixFilePermission;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
 
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.util.FS.Attributes;
 
 class FileUtil {
@@ -147,7 +148,8 @@ class FileUtil {
 	public static long getLength(File path) throws IOException {
 		Path nioPath = path.toPath();
 		if (Files.isSymbolicLink(nioPath))
-			return Files.readSymbolicLink(nioPath).toString().length();
+			return Files.readSymbolicLink(nioPath).toString()
+					.getBytes(Constants.CHARSET).length;
 		return Files.size(nioPath);
 	}
 
