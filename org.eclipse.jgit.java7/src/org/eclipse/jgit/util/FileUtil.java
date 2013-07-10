f1b918f939f2ebedaa9b6f1e8be37d69841b722c
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-07-11 01:11:12 +0200

    Normalize paths on OS X with Java 7
    Java 6 and Java8 ea as or this writing normalizes file names read
    from the file system to NFC, but under Java7 this is not done. I'm
    sure this is a mistake, and I even think preview versions of Java 7
    on Macs did normalize to NFC.
    
    Change-Id: I08649ac58c9b3cb8bf12794703e4137b1b4e94d5

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
@@ -190,4 +190,16 @@ class FileUtil {
 		}
 	}
 
+	public static File normalize(File file) {
+		if (SystemReader.getInstance().isMacOS()) {
+			if (System.getProperties().getProperty("java.version")
+					.startsWith("1.7")) {
+				String normalized = Normalizer.normalize(file.getPath(),
+						Normalizer.Form.NFC);
+				return new File(normalized);
+			}
+		}
+		return file;
+	}
+
 }
