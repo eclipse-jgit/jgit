78986d03a363fb646636b1dfea1bd441449e7a5c
Tobias Pfeifer <to.pfeifer@web.de>
2013-07-11 11:09:30 +0200

    Enable to prepare interactive rebase and then start it explicitly
    Add Operation.PROCESS_STEPS to RebaseCommand to enable starting
    interactive rebase explicitly after rebase steps have been configured.
    
    Change-Id: I2d6f0de82010ea6523fbce6fb4501e847bdcdddc
    Signed-off-by: Tobias Pfeifer <to.pfeifer@web.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
+ * Copyright (C) 2010, 2013, Mathias Kinzler <mathias.kinzler@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -131,6 +131,16 @@ public class RebaseResult {
 			public boolean isSuccessful() {
 				return false;
 			}
+		},
+
+		/**
+		 * Interactive rebase has been prepared
+		 */
+		INTERACTIVE_PREPARED {
+			@Override
+			public boolean isSuccessful() {
+				return false;
+			}
 		};
 
 		/**
@@ -151,6 +161,9 @@ public class RebaseResult {
 
 	static final RebaseResult NOTHING_TO_COMMIT_RESULT = new RebaseResult(
 			Status.NOTHING_TO_COMMIT);
+
+	static final RebaseResult INTERACTIVE_PREPARED_RESULT =  new RebaseResult(
+			Status.INTERACTIVE_PREPARED);
 
 	private final Status status;
 
