94d398e80b334a5f3c1f13cede143dabeabffef0
Lars Vogel <Lars.Vogel@gmail.com>
2013-07-25 23:28:05 +0200

    Missing @since tags
    Trivial change, Eclipse showed errors for missing @since tags, I used
    Ctrl+1 to fix them
    
    Change-Id: I9754e2124c0fe6ad2dbde5597c3ed10f1c3efef5
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/StatusCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/StatusCommand.java
@@ -91,6 +91,7 @@ public class StatusCommand extends GitCommand<Status> {
 	 * @param path
 	 *            a path is relative to the top level of the repository
 	 * @return {@code this}
+	 * @since 3.1
 	 */
 	public StatusCommand addPath(String path) {
 		if (paths == null)
@@ -104,6 +105,7 @@ public class StatusCommand extends GitCommand<Status> {
 	 *
 	 * @return the paths for which the status is shown or <code>null</code> if
 	 *         the complete status for the whole repo is shown.
+	 * @since 3.1
 	 */
 	public List<String> getPaths() {
 		return paths;
