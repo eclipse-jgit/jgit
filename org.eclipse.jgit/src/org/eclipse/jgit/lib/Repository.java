75680a9ac2f7ac7e9926903940127cade5a3c987
Christian Halstrick <christian.halstrick@sap.com>
2013-07-30 14:00:18 +0200

    Enhance reading of git-rebase-todo formatted files
    Reading and writing files formatted like the git-rebase-todo files was
    hidden in the RebaseCommand. Certain constructs (like leading
    tabs+spaces) have not been handled as native git. Also the upcoming
    rebase interactive feature in EGit needs reading/writing these files
    independent from a RebaseCommand. Therefore reading and writing those
    files has been refactored to the Repository class. RebaseCommand get's
    smaller because of that and doesn't have to deal with reading/writing
    files.
    Additional tests for empty todo-list files, or files containing comments
    have been added.
    
    Change-Id: I323f3619952fecdf28ddf50139a88e0bea34f5ba
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -48,10 +48,12 @@
 package org.eclipse.jgit.lib;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.Collection;
@@ -1563,4 +1565,117 @@ public abstract class Repository {
 		}
 	}
 
+	/**
+	 * Read a file formatted like the git-rebase-todo file. The "done" file is
+	 * also formatted like the git-rebase-todo file. These files can be found in
+	 * .git/rebase-merge/ or .git/rebase-append/ folders.
+	 *
+	 * @param f
+	 * @param includeComments
+	 *            <code>true</code> if also comments should be reported
+	 * @return the list of steps
+	 * @throws IOException
+	 */
+	public List<RebaseTodoLine> readRebaseTodo(File f, boolean includeComments)
+			throws IOException {
+		byte[] buf = IO.readFully(f);
+		int ptr = 0;
+		int tokenBegin = 0;
+		List<RebaseTodoLine> r = new LinkedList<RebaseTodoLine>();
+		while (ptr < buf.length) {
+			RebaseTodoLine.Action action = null;
+			AbbreviatedObjectId commit = null;
+			tokenBegin = ptr;
+			ptr = RawParseUtils.nextLF(buf, ptr);
+			int lineStart = tokenBegin;
+			int lineEnd = ptr - 2;
+			if (lineEnd >= 0 && buf[lineEnd] == '\r')
+				lineEnd--;
+			int tokenCount = 0;
+			// Handle comments
+			if (buf[tokenBegin] == '#') {
+				if (includeComments)
+					r.add(new RebaseTodoLine(buf, tokenBegin, 1 + lineEnd
+							- tokenBegin));
+				continue;
+			}
+			// skip leading spaces+tabs+cr
+			while (tokenBegin <= lineEnd
+					&& (buf[tokenBegin] == ' ' || buf[tokenBegin] == '\t' || buf[tokenBegin] == '\r'))
+				tokenBegin++;
+			// Handle empty lines (maybe empty after skipping leading whitespaces)
+			if (tokenBegin > lineEnd) {
+				if (includeComments)
+					r.add(new RebaseTodoLine(buf, lineStart, 1 + lineEnd
+							- lineStart));
+				continue;
+			}
+			int nextSpace = RawParseUtils.next(buf, tokenBegin, ' ');
+			while (tokenCount < 3 && nextSpace < ptr) {
+				switch (tokenCount) {
+				case 0:
+					String actionToken = new String(buf, tokenBegin, nextSpace
+							- tokenBegin - 1);
+					tokenBegin = nextSpace;
+					action = RebaseTodoLine.Action.parse(actionToken);
+					break;
+				case 1:
+					if (action == null)
+						break;
+					nextSpace = RawParseUtils.next(buf, tokenBegin, ' ');
+					String commitToken = new String(buf, tokenBegin, nextSpace
+							- tokenBegin - 1);
+					tokenBegin = nextSpace;
+					commit = AbbreviatedObjectId.fromString(commitToken);
+					break;
+				case 2:
+					if (action == null)
+						break;
+					r.add(new RebaseTodoLine(action, commit, buf, tokenBegin, 1
+							+ lineEnd - tokenBegin));
+					break;
+				}
+				tokenCount++;
+			}
+		}
+		return r;
+	}
+
+	/**
+	 * Write a file formatted like a git-rebase-todo file.
+	 *
+	 * @param f
+	 *            the file to write
+	 * @param steps
+	 *            the steps to be written
+	 * @param append
+	 *            whether to append to an existing file or to write a new file
+	 * @throws IOException
+	 */
+	public void writeRebaseTodoFile(File f, List<RebaseTodoLine> steps,
+			boolean append)
+			throws IOException {
+		BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(f, append), Constants.CHARACTER_ENCODING));
+		try {
+			StringBuilder sb = new StringBuilder();
+			for (RebaseTodoLine step : steps) {
+				sb.setLength(0);
+				if (RebaseTodoLine.Action.COMMENT.equals(step.action))
+					sb.append(RawParseUtils.decode(step.getComment()));
+				else {
+					sb.append(step.getAction().toToken());
+					sb.append(" "); //$NON-NLS-1$
+					sb.append(step.getCommit().name());
+					sb.append(" "); //$NON-NLS-1$
+					sb.append(RawParseUtils.decode(step.getShortMessage())
+							.trim());
+				}
+				fw.write(sb.toString());
+				fw.newLine();
+			}
+		} finally {
+			fw.close();
+		}
+	}
 }
