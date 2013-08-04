2290516ddb5a92ccc296df500aa5e9e30cbc240e
Christian Halstrick <christian.halstrick@sap.com>
2013-08-04 23:37:50 +0200

    Add an implementation for HttpConnection using Apache HttpClient
    This change implements the http connection abstraction with the help of
    org.apache.http.client.HttpClient. The default implementation used by
    JGit is still the JDK HttpURLConnection. But now JGit users have the
    possibility to switch completely to org.apache.httpclient. The reason
    for this is that in certain (e.g. cloud) environments you are forced to
    use the org.apache classes.
    
    Change-Id: I0b357f23243ed13a014c79ba179fa327dfe318b2
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/http/HttpConnection.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/http/HttpConnection.java
@@ -258,6 +258,7 @@ public interface HttpConnection {
 	 * @param random
 	 *            the source of randomness for this generator or null. See
 	 *            {@link SSLContext#init(KeyManager[], TrustManager[], SecureRandom)}
+	 *
 	 * @throws NoSuchAlgorithmException
 	 * @throws KeyManagementException
 	 */
