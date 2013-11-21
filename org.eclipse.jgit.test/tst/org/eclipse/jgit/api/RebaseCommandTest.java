4feace2b9ecb90fe591c4317403f6d8e6309287e
Stefan Lay <stefan.lay@sap.com>
2013-11-21 14:46:12 +0100

    Fix parsing Rebase todo lines when commit message is missing
    Bug: 422253
    Change-Id: I9739b16c91d2df31a481360a712d3479a4eeee2e
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RebaseCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RebaseCommandTest.java
@@ -1642,6 +1642,19 @@ public class RebaseCommandTest extends RepositoryTestCase {
 	}
 
 	@Test
+	public void testRebaseShouldBeAbleToHandleLinesWithoutCommitMessageInRebaseTodoFile()
+			throws IOException {
+		String todo = "pick 1111111 \n" + "pick 2222222 Commit 2\n"
+				+ "# Comment line at end\n";
+		write(getTodoFile(), todo);
+
+		List<RebaseTodoLine> steps = db.readRebaseTodo(GIT_REBASE_TODO, false);
+		assertEquals(2, steps.size());
+		assertEquals("1111111", steps.get(0).getCommit().name());
+		assertEquals("2222222", steps.get(1).getCommit().name());
+	}
+
+	@Test
 	public void testRebaseShouldNotFailIfUserAddCommentLinesInPrepareSteps()
 			throws Exception {
 		commitFile(FILE1, FILE1, "master");
