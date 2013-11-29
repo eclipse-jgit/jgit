e35f59a76c081aedbffcf7cbca8217fb584af094
Axel Richard <axel.richard@obeo.fr>
2013-11-29 11:06:11 +0100

    Manage CheckoutConflictException in pgm
    Change-Id: I49f92bf7cafc80404f0bd07d62ff4b25e4db6e7c
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Checkout.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Checkout.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010, 2012 Chris Aniszczyk <caniszczyk@gmail.com>
+ * Copyright (C) 2013, Obeo
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -47,6 +48,7 @@ import java.text.MessageFormat;
 
 import org.eclipse.jgit.api.CheckoutCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.lib.Constants;
@@ -106,6 +108,9 @@ class Checkout extends TextBuiltin {
 					name));
 		} catch (RefAlreadyExistsException e) {
 			throw die(MessageFormat.format(CLIText.get().branchAlreadyExists,
+					name));
+		} catch (CheckoutConflictException e) {
+			outw.println(MessageFormat.format(CLIText.get().checkoutConflict,
 					name));
 		}
 	}
