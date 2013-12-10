ea04d2329d1283a3155ff26cfda7c46253f68854
Christian Halstrick <christian.halstrick@sap.com>
2013-12-10 11:06:10 +0100

    Don't use API exception in RebaseTodoLine
    This came up while testing the proposed buck build for jgit. With buck
    we can introduce smaller modules to allow for more concurrency during
    build and to better control inner structure of jgit. Trying to put the
    porcelain API into a different module than lower level implementation
    classes failed since RebaseTodoLine used a porcelain API exception
    causing a dependency cycle on the proposed modules. Using an exception
    defined on the same abstraction level fixes this problem.
    
    Change-Id: I26a5353e1a8fc23e67d8ce61309bd964f7665bcb
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RebaseTodoLine.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RebaseTodoLine.java
@@ -45,7 +45,7 @@ package org.eclipse.jgit.lib;
 
 import java.text.MessageFormat;
 
-import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.errors.IllegalTodoFileModification;
 import org.eclipse.jgit.internal.JGitText;
 
 /**
@@ -111,7 +111,7 @@ public class RebaseTodoLine {
 						|| action.shortToken.equals(token))
 					return action;
 			}
-			throw new JGitInternalException(MessageFormat.format(
+			throw new IllegalArgumentException(MessageFormat.format(
 					JGitText.get().unknownOrUnsupportedCommand, token,
 					Action.values()));
 		}
@@ -167,8 +167,11 @@ public class RebaseTodoLine {
 	 * non-comment.
 	 *
 	 * @param newAction
+	 * @throws IllegalTodoFileModification
+	 *             on attempt to set a non-comment action on a line which was a
+	 *             comment line before.
 	 */
-	public void setAction(Action newAction) {
+	public void setAction(Action newAction) throws IllegalTodoFileModification {
 		if (!Action.COMMENT.equals(action) && Action.COMMENT.equals(newAction)) {
 			// transforming from non-comment to comment
 			if (comment == null)
@@ -180,7 +183,7 @@ public class RebaseTodoLine {
 		} else if (Action.COMMENT.equals(action) && !Action.COMMENT.equals(newAction)) {
 			// transforming from comment to non-comment
 			if (commit == null)
-				throw new JGitInternalException(MessageFormat.format(
+				throw new IllegalTodoFileModification(MessageFormat.format(
 						JGitText.get().cannotChangeActionOnComment, action,
 						newAction));
 		}
@@ -219,12 +222,11 @@ public class RebaseTodoLine {
 		throw createInvalidCommentException(newComment);
 	}
 
-	private static JGitInternalException createInvalidCommentException(
+	private static IllegalArgumentException createInvalidCommentException(
 			String newComment) {
-		IllegalArgumentException iae = new IllegalArgumentException(
+		return new IllegalArgumentException(
 				MessageFormat.format(
 				JGitText.get().argumentIsNotAValidCommentString, newComment));
-		return new JGitInternalException(iae.getMessage(), iae);
 	}
 
 	/**
