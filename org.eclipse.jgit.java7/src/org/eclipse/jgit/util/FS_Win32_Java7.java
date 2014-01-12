530fee33b3adcc5775cb120419f5ad5acb64ce32
Robin Rosenberg <robin.rosenberg@dewire.com>
2014-01-12 01:34:58 +0100

    Dynamically detect if Windows supports symbolic links
    To get symlink suport you typically need to run as administrator.
    
    Change-Id: I394ea75bc2f250c62f860e537a0af9e6380b3b38

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7.java
@@ -51,6 +51,8 @@ import java.io.IOException;
  */
 public class FS_Win32_Java7 extends FS_Win32 {
 
+	private volatile Boolean dectedSupportSymlinks;
+
 	FS_Win32_Java7(FS src) {
 		super(src);
 	}
@@ -65,7 +67,29 @@ public class FS_Win32_Java7 extends FS_Win32 {
 
 	@Override
 	public boolean supportsSymlinks() {
-		return true;
+		if (dectedSupportSymlinks == null)
+			dectedSymlinkSupport();
+		return Boolean.TRUE.equals(dectedSupportSymlinks);
+	}
+
+	private void dectedSymlinkSupport() {
+		File tempFile = null;
+		try {
+			tempFile = File.createTempFile("tempsymlinktarget", ""); //$NON-NLS-1$ //$NON-NLS-2$
+			File linkName = new File(tempFile.getParentFile(), "tempsymlink"); //$NON-NLS-1$
+			FileUtil.createSymLink(linkName, tempFile.getPath());
+			dectedSupportSymlinks = Boolean.TRUE;
+			linkName.delete();
+		} catch (IOException e) {
+			dectedSupportSymlinks = Boolean.FALSE;
+		} finally {
+			if (tempFile != null)
+				try {
+					FileUtils.delete(tempFile);
+				} catch (IOException e) {
+					throw new RuntimeException(e); // panic
+				}
+		}
 	}
 
 	@Override
