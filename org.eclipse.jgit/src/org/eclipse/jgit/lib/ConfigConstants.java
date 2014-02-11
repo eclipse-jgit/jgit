0ce987d486add16c33676b3a8d0aa77efa08ad6b
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-02-11 23:01:04 +0100

    Use fetch.prune and remote.<name>.prune to set prune mode when fetching
    When no explicit value is set via FetchCommand.setRemoveDeletedRefs()
    checks if pruning is enabled in the configuration.
    
    The following commit introduced the prune config to C Git:
    https://github.com/git/git/commit/737c5a9cde708d6995c765b7c2e95033edd0a896
    
    Change-Id: Ida79d335218e1c9f5c6e2ce03386ac8a1c0b212e
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -90,6 +90,13 @@ public class ConfigConstants {
 	/** The "pack" section */
 	public static final String CONFIG_PACK_SECTION = "pack";
 
+	/**
+	 * The "fetch" section
+	 *
+	 * @since 3.3
+	 */
+	public static final String CONFIG_FETCH_SECTION = "fetch";
+
 	/** The "algorithm" key */
 	public static final String CONFIG_KEY_ALGORITHM = "algorithm";
 
@@ -247,4 +254,11 @@ public class ConfigConstants {
          * @since 3.0
          */
 	public static final String CONFIG_KEY_RENAMES = "renames";
+
+	/**
+	 * The "prune" key
+	 *
+	 * @since 3.3
+	 */
+	public static final String CONFIG_KEY_PRUNE = "prune";
 }
