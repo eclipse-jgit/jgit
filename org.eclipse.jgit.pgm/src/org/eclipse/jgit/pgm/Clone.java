44812c02e844c91869c3c43f5187fe23b5426275
Kaloyan Raev <kaloyan.r@zend.com>
2014-02-24 16:03:51 +0200

    CLI clone command should support --no-checkout
    doCheckout() is called only if --no-checkout option is not set.
    
    Bug: 428917
    Change-Id: I350bef446dd7a37613b9506aae99679569bd36e1
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Clone.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Clone.java
@@ -79,6 +79,9 @@ class Clone extends AbstractFetchCommand {
 	@Option(name = "--branch", aliases = { "-b" }, metaVar = "metaVar_branchName", usage = "usage_checkoutBranchAfterClone")
 	private String branch;
 
+	@Option(name = "--no-checkout", aliases = { "-n" }, usage = "usage_noCheckoutAfterClone")
+	private boolean noCheckout;
+
 	@Argument(index = 0, required = true, metaVar = "metaVar_uriish")
 	private String sourceUri;
 
@@ -122,16 +125,19 @@ class Clone extends AbstractFetchCommand {
 
 		saveRemote(uri);
 		final FetchResult r = runFetch();
-		final Ref checkoutRef;
-		if (branch == null)
-			checkoutRef = guessHEAD(r);
-		else {
-			checkoutRef = r.getAdvertisedRef(Constants.R_HEADS + branch);
-			if (checkoutRef == null)
-				throw die(MessageFormat.format(CLIText.get().noSuchRemoteRef,
-						branch));
+
+		if (!noCheckout) {
+			final Ref checkoutRef;
+			if (branch == null)
+				checkoutRef = guessHEAD(r);
+			else {
+				checkoutRef = r.getAdvertisedRef(Constants.R_HEADS + branch);
+				if (checkoutRef == null)
+					throw die(MessageFormat.format(
+							CLIText.get().noSuchRemoteRef, branch));
+			}
+			doCheckout(checkoutRef);
 		}
-		doCheckout(checkoutRef);
 	}
 
 	private void saveRemote(final URIish uri) throws URISyntaxException,
