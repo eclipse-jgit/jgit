c41b1b96f9b148c6282df95e5c17f4d4fd3da2ea
Shawn Pearce <spearce@spearce.org>
2014-04-17 15:26:04 -0700

    blame: Reduce running time ~4.5% by skipping common subtrees
    With this commit running blame on render_view_impl.cc[1] saves
    about 644 ms over prior versions, reducing the time about 4.5%.
    
    Large projects often contain strands of commits where no changes
    are made to a particular subtree. Blame used to dive recursively
    into these subtrees to look for the blob and check if its SHA-1
    was changed. In chromium/src[1] only 20% of the commits modify
    the content/renderer subtree relevant for the file.
    
    The recursivePath is necessary to check for '/' and remember
    if common subtree elimination should be attempted. When a file
    lives within a subtree the extra cost to check for unmodified
    subtrees saves time. However for files in the root tree the
    extra work incurred by TreeWalk is not worthwhile and would
    significantly increase overall running time.
    
    Now typical running times from an otherwise idle desktop:
    
      real	0m13.387s  0m13.341s  0m13.443s
      user	0m15.410s  0m15.220s  0m15.350s
    
    previously:
    
      real	0m14.085s  0m14.049s  0m13.968s
      user	0m15.730s  0m15.820s  0m15.770s
    
    [1] https://chromium.googlesource.com/chromium/src/+show/34d6e5c5b4248b1b199405af7ad00f961921f347/content/renderer/render_view_impl.cc
    
    Change-Id: Ib16d684df7ffa034ee28def3fb22c797998d5b7b

--- org.eclipse.jgit/src/org/eclipse/jgit/blame/Candidate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/blame/Candidate.java
@@ -80,6 +80,7 @@ class Candidate {
 
 	/** Path of the candidate file in {@link #sourceCommit}. */
 	PathFilter sourcePath;
+	boolean recursivePath;
 
 	/** Unique name of the candidate blob in {@link #sourceCommit}. */
 	ObjectId sourceBlob;
@@ -110,6 +111,7 @@ class Candidate {
 	Candidate(RevCommit commit, PathFilter path) {
 		sourceCommit = commit;
 		sourcePath = path;
+		recursivePath = path.shouldBeRecursive();
 	}
 
 	int getParentCount() {
@@ -142,6 +144,11 @@ class Candidate {
 
 	PersonIdent getAuthor() {
 		return sourceCommit.getAuthorIdent();
+	}
+
+	void setSourcePath(PathFilter path) {
+		sourcePath = path;
+		recursivePath = path.shouldBeRecursive();
 	}
 
 	Candidate create(RevCommit commit, PathFilter path) {
