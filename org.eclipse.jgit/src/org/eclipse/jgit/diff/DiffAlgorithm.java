073aa54df51971d049cf2cd2e603206a4e863dcb
Shawn Pearce <spearce@spearce.org>
2014-04-18 09:05:05 -0700

    diff: Optimize single line edits
    If the header and trailer are identical up to a single line on either
    side, return that REPLACE edit as the only result.  No algorithm can
    break down a REPLACE with height of 1 element on either side.
    
    Change-Id: I483c40e8790cc3e8b322ef6dfce2299491fd0ac7

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffAlgorithm.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffAlgorithm.java
@@ -114,6 +114,9 @@ public abstract class DiffAlgorithm {
 			return EditList.singleton(region);
 
 		case REPLACE: {
+			if (region.getLengthA() == 1 || region.getLengthB() == 1)
+				return EditList.singleton(region);
+
 			SubsequenceComparator<S> cs = new SubsequenceComparator<S>(cmp);
 			Subsequence<S> as = Subsequence.a(a, region);
 			Subsequence<S> bs = Subsequence.b(b, region);
