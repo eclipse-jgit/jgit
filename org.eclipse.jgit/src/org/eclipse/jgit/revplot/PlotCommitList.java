c49c3af9544cde01cd60e84c0c2f5afd1e8d567b
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-04-27 20:35:14 +0200

    PlotLane: Use identity hashCode and equals
    Using the lane position and other data for equals/hashCode is not
    useful.
    
    Change-Id: I7af151d8a84544a77a486474c8ac71dd80090c66
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/revplot/PlotCommitList.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revplot/PlotCommitList.java
@@ -236,9 +236,7 @@ public class PlotCommitList<L extends PlotLane> extends
 			if (newPos == -1)
 				newPos = positionsAllocated++;
 			freePositions.add(Integer.valueOf(commit.lane.getPosition()));
-			activeLanes.remove(commit.lane);
 			commit.lane.position = newPos;
-			activeLanes.add(commit.lane);
 		}
 	}
 
