d451c09a3d32da058077756d6e28c750a15cc203
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-05-19 21:46:09 +0200

    Cherry-Pick: Support --no-commit
    Repository.getRepositoryState() reports the result as CHERRY_PICKING if
    there were conflicts and SAFE otherwise.
    
    Change-Id: Ic4c313a8971a5aa15745518c49e2587a1a0fe8d4
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CherryPickCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CherryPickCommandTest.java
@@ -76,6 +76,18 @@ public class CherryPickCommandTest extends RepositoryTestCase {
 	@Test
 	public void testCherryPick() throws IOException, JGitInternalException,
 			GitAPIException {
+		doTestCherryPick(false);
+	}
+
+	@Test
+	public void testCherryPickNoCommit() throws IOException,
+			JGitInternalException, GitAPIException {
+		doTestCherryPick(true);
+	}
+
+	private void doTestCherryPick(boolean noCommit) throws IOException,
+			JGitInternalException,
+			GitAPIException {
 		Git git = new Git(db);
 
 		writeTrashFile("a", "first line\nsec. line\nthird line\n");
@@ -102,13 +114,16 @@ public class CherryPickCommandTest extends RepositoryTestCase {
 		git.add().addFilepattern("a").call();
 		git.commit().setMessage("enhanced a").call();
 
-		git.cherryPick().include(fixingA).call();
+		CherryPickResult pickResult = git.cherryPick().include(fixingA)
+				.setNoCommit(noCommit).call();
 
+		assertEquals(CherryPickStatus.OK, pickResult.getStatus());
 		assertFalse(new File(db.getWorkTree(), "b").exists());
 		checkFile(new File(db.getWorkTree(), "a"),
 				"first line\nsecond line\nthird line\nfeature++\n");
 		Iterator<RevCommit> history = git.log().call().iterator();
-		assertEquals("fixed a", history.next().getFullMessage());
+		if (!noCommit)
+			assertEquals("fixed a", history.next().getFullMessage());
 		assertEquals("enhanced a", history.next().getFullMessage());
 		assertEquals("create a", history.next().getFullMessage());
 		assertFalse(history.hasNext());
@@ -199,6 +214,37 @@ public class CherryPickCommandTest extends RepositoryTestCase {
 
 		assertEquals(RepositoryState.CHERRY_PICKING_RESOLVED,
 				db.getRepositoryState());
+
+		git.commit().setOnly("a").setMessage("resolve").call();
+
+		assertEquals(RepositoryState.SAFE, db.getRepositoryState());
+	}
+
+	@Test
+	public void testCherryPickConflictResolutionNoCOmmit() throws Exception {
+		Git git = new Git(db);
+		RevCommit sideCommit = prepareCherryPick(git);
+
+		CherryPickResult result = git.cherryPick().include(sideCommit.getId())
+				.setNoCommit(true).call();
+
+		assertEquals(CherryPickStatus.CONFLICTING, result.getStatus());
+		assertTrue(db.readDirCache().hasUnmergedPaths());
+		String expected = "<<<<<<< master\na(master)\n=======\na(side)\n>>>>>>> 527460a side\n";
+		assertEquals(expected, read("a"));
+		assertTrue(new File(db.getDirectory(), Constants.MERGE_MSG).exists());
+		assertEquals("side\n\nConflicts:\n\ta\n", db.readMergeCommitMsg());
+		assertFalse(new File(db.getDirectory(), Constants.CHERRY_PICK_HEAD)
+				.exists());
+		assertEquals(RepositoryState.CHERRY_PICKING, db.getRepositoryState());
+
+		// Resolve
+		writeTrashFile("a", "a");
+		git.add().addFilepattern("a").call();
+
+		// Note: This is not CHERRY_PICKING_RESOLVED, because this can't be
+		// determined from the repository, as there is no CHERRY_PICK_GEAD
+		assertEquals(RepositoryState.SAFE, db.getRepositoryState());
 
 		git.commit().setOnly("a").setMessage("resolve").call();
 
