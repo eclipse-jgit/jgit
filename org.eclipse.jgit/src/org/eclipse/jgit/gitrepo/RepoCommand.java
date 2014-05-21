eb98ebcc279ce26633c4fa6542d13968a06624dd
Yuxuan 'fishy' Wang <fishywang@google.com>
2014-05-21 11:16:36 -0700

    Unit test for the updating behavior in RepoCommand API.
    When update the manifest against a bare repository, RepoCommand will replace
    every existing content from the repository with contents populated from the
    manifest. Added note for that and a unit test to make sure this behavior.
    
    Change-Id: I1d5960e84bca5aa2a4e86f424d2ddd4197894cdc
    Signed-off-by: Yuxuan 'fishy' Wang <fishywang@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
@@ -101,6 +101,9 @@ import org.xml.sax.helpers.XMLReaderFactory;
  * This will parse a repo XML manifest, convert it into .gitmodules file and the
  * repository config file.
  *
+ * If called against a bare repository, it will replace all the existing content
+ * of the repository with the contents populated from the manifest.
+ *
  * @see <a href="https://code.google.com/p/git-repo/">git-repo project page</a>
  * @since 3.4
  */
@@ -358,8 +361,8 @@ public class RepoCommand extends GitCommand<RevCommit> {
 				if (inGroups(proj)) {
 					command.addSubmodule(remoteUrl + proj.name,
 							proj.path,
-							proj.revision == null ?
-									defaultRevision : proj.revision,
+							proj.revision == null
+									? defaultRevision : proj.revision,
 							proj.copyfiles);
 				}
 			}
@@ -468,7 +471,8 @@ public class RepoCommand extends GitCommand<RevCommit> {
 	/**
 	 * Set the author/committer for the bare repository commit.
 	 *
-	 * For non-bare repositories, the current user will be used and this will be ignored.
+	 * For non-bare repositories, the current user will be used and this will be
+	 * ignored.
 	 *
 	 * @param author
 	 * @return this command
@@ -520,7 +524,6 @@ public class RepoCommand extends GitCommand<RevCommit> {
 			DirCacheBuilder builder = index.builder();
 			ObjectInserter inserter = repo.newObjectInserter();
 			RevWalk rw = new RevWalk(repo);
-
 			try {
 				Config cfg = new Config();
 				for (Project proj : bareProjects) {
