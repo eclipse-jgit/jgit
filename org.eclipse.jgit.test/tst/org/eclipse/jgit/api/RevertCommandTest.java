c14a2a25e93fc6ceb098b4d8e37dd64169610f78
Maik Schreiber <blizzy@blizzy.de>
2014-06-19 12:54:14 +0200

    Fix RevertCommand to correctly revert multiple commits at once.
    The fix is to move the new head commit to the newly-created revert
    commit, so that additional revert commits will use the correct head.
    
    Change-Id: I5de3a9a2a4c276e60af732e9c507cbbdfd1a4652
    Signed-off-by: Maik Schreiber <blizzy@blizzy.de>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RevertCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RevertCommandTest.java
@@ -124,6 +124,51 @@ public class RevertCommandTest extends RepositoryTestCase {
 	}
 
 	@Test
+	public void testRevertMultiple() throws IOException, JGitInternalException,
+			GitAPIException {
+		Git git = new Git(db);
+
+		writeTrashFile("a", "first\n");
+		git.add().addFilepattern("a").call();
+		git.commit().setMessage("add first").call();
+
+		writeTrashFile("a", "first\nsecond\n");
+		git.add().addFilepattern("a").call();
+		RevCommit secondCommit = git.commit().setMessage("add second").call();
+
+		writeTrashFile("a", "first\nsecond\nthird\n");
+		git.add().addFilepattern("a").call();
+		RevCommit thirdCommit = git.commit().setMessage("add third").call();
+
+		git.revert().include(thirdCommit).include(secondCommit).call();
+
+		checkFile(new File(db.getWorkTree(), "a"), "first\n");
+		Iterator<RevCommit> history = git.log().call().iterator();
+		RevCommit revertCommit = history.next();
+		String expectedMessage = "Revert \"add second\"\n\n"
+				+ "This reverts commit "
+				+ secondCommit.getId().getName() + ".\n";
+		assertEquals(expectedMessage, revertCommit.getFullMessage());
+		revertCommit = history.next();
+		expectedMessage = "Revert \"add third\"\n\n"
+				+ "This reverts commit " + thirdCommit.getId().getName()
+				+ ".\n";
+		assertEquals(expectedMessage, revertCommit.getFullMessage());
+		assertEquals("add third", history.next().getFullMessage());
+		assertEquals("add second", history.next().getFullMessage());
+		assertEquals("add first", history.next().getFullMessage());
+		assertFalse(history.hasNext());
+
+		ReflogReader reader = db.getReflogReader(Constants.HEAD);
+		assertTrue(reader.getLastEntry().getComment()
+				.startsWith("revert: Revert \""));
+		reader = db.getReflogReader(db.getBranch());
+		assertTrue(reader.getLastEntry().getComment()
+				.startsWith("revert: Revert \""));
+
+	}
+
+	@Test
 	public void testRevertDirtyIndex() throws Exception {
 		Git git = new Git(db);
 		RevCommit sideCommit = prepareRevert(git);
