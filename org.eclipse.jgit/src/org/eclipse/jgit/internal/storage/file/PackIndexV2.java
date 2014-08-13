35f6e7ab1d00455c98a1e8943e87e76577bc5af2
Marc Strapetz <marc.strapetz@syntevo.com>
2014-08-13 10:38:13 +0200

    PackIndexV2 should check for possible corruption
    Change-Id: I1803ec6d8141f07dd4085778da6461abe81c30a9
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackIndexV2.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackIndexV2.java
@@ -113,10 +113,11 @@ class PackIndexV2 extends PackIndex {
 				offset32[k] = NO_BYTES;
 				crc32[k] = NO_BYTES;
 				continue;
-			}
+			} else if (bucketCnt < 0)
+				throw new IOException(JGitText.get().indexFileCorrupted);
 
 			final long nameLen = bucketCnt * Constants.OBJECT_ID_LENGTH;
-			if (nameLen > Integer.MAX_VALUE)
+			if (nameLen > Integer.MAX_VALUE - 8) // see http://stackoverflow.com/a/8381338
 				throw new IOException(JGitText.get().indexFileIsTooLargeForJgit);
 
 			final int intNameLen = (int) nameLen;
