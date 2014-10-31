319ed11bede392ceaa3f0326053318e483ecc8d3
Arthur Daussy <arthur.daussy@obeo.fr>
2014-10-31 17:46:36 +0100

    Adds the git attributes computation on the treewalk
    Adds the getAttributes feature to the tree walk. The computation of
    attributes needs to be done by the TreeWalk since it needs both a
    WorkingTreeIterator and a DirCacheIterator.
    
    Bug: 342372
    CQ: 9120
    Change-Id: I5e33257fd8c9895869a128bad3fd1e720409d361
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>
    Signed-off-by: Christian Halstrick <christian.halstrick@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/IndexDiff.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/IndexDiff.java
@@ -74,6 +74,7 @@ import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
+import org.eclipse.jgit.treewalk.TreeWalk.OperationType;
 import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
 import org.eclipse.jgit.treewalk.filter.IndexDiffFilter;
 import org.eclipse.jgit.treewalk.filter.SkipWorkTreeFilter;
@@ -403,6 +404,7 @@ public class IndexDiff {
 		dirCache = repository.readDirCache();
 
 		try (TreeWalk treeWalk = new TreeWalk(repository)) {
+			treeWalk.setOperationType(OperationType.CHECKIN_OP);
 			treeWalk.setRecursive(true);
 			// add the trees (tree, dirchache, workdir)
 			if (tree != null)
