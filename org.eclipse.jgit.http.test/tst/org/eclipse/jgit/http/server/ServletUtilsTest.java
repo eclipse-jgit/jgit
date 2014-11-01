6d068e853154d8caab3089be0f1c7bf89fc25e45
David Pletcher <dpletcher@google.com>
2014-11-01 14:34:18 -0700

    Extract path info from requests without decoding
    Gitiles malfunctions in conjunction with jgit and guice
    because of a recent Guice bug fix. Work around the problem
    by parsing the URI directly, bypassing the unescaping
    performed by the getPathInfo method.
    
    This rest of this message is copied from
    https://gerrit-review.googlesource.com/#/c/60820/ :
    
    The fix for Guice issue #745[1] causes getPathInfo() within the
    GuiceFilter to return decoded values, eliminating the difference
    between "foo/bar" and "foo%2Fbar". This is in spec with the servlet
    standard, whose javadoc for getPathInfo[2] states that the return
    value be "decoded by the web container".
    
    Work around this by extracting the path part directly from the request
    URI, which is unmodified by the container. This is copying the Guice
    behavior prior to the bugfix.
    
    [1] https://github.com/google/guice/issues/745
    [2] http://docs.oracle.com/javaee/7/api/javax/servlet/http/HttpServletRequest.html#getPathInfo()
    
    Change-Id: I7fdb291bda377dab6160599ee537962d5f60f1e8
    Signed-off-by: David Pletcher <dpletcher@google.com>

--- org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ServletUtilsTest.java
+++ org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ServletUtilsTest.java
@@ -48,7 +48,50 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ServletUtilsTest {
+	@Test
+	public void emptyContextPath() {
+		assertEquals("/foo/bar",
+				ServletUtils.getEncodedPathInfo("", "/s", "/s/foo/bar"));
+		assertEquals("/foo%2Fbar",
+				ServletUtils.getEncodedPathInfo("", "/s", "/s/foo%2Fbar"));
+	}
+
+	public void slashContextPath() {
+		assertEquals("/foo/bar",
+				ServletUtils.getEncodedPathInfo("/", "/s", "/s/foo/bar"));
+		assertEquals("/foo%2Fbar",
+				ServletUtils.getEncodedPathInfo("/", "/s", "/s/foo%2Fbar"));
+	}
+
+	@Test
+	public void emptyServletPath() {
+		assertEquals("/foo/bar",
+				ServletUtils.getEncodedPathInfo("/c", "", "/c/foo/bar"));
+		assertEquals("/foo%2Fbar",
+				ServletUtils.getEncodedPathInfo("/c", "", "/c/foo%2Fbar"));
+	}
+
+	@Test
+	public void trailingSlashes() {
+		assertEquals("/foo/bar/",
+				ServletUtils.getEncodedPathInfo("/c", "/s", "/c/s/foo/bar/"));
+		assertEquals("/foo/bar/",
+				ServletUtils.getEncodedPathInfo("/c", "/s", "/c/s/foo/bar///"));
+		assertEquals("/foo%2Fbar/",
+				ServletUtils.getEncodedPathInfo("/c", "/s", "/c/s/foo%2Fbar/"));
+		assertEquals("/foo%2Fbar/", ServletUtils.getEncodedPathInfo("/c", "/s",
+				"/c/s/foo%2Fbar///"));
+	}
+
+	@Test
+	public void servletPathMatchesRequestPath() {
+		assertEquals((String) null,
+				ServletUtils.getEncodedPathInfo("/c", "/s", "/c/s"));
+	}
+
 	@Test
 	public void testAcceptGzip() {
 		assertFalse(ServletUtils.acceptsGzipEncoding((String) null));
