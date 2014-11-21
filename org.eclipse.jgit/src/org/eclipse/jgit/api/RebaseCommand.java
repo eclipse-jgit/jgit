08ef4dad291697f4089add9c41b618693d2d663e
Arthur Daussy <arthur.daussy@obeo.fr>
2014-11-21 11:28:19 +0100

    [DRAFT][452623] Makes RepositoryState.REBASING_MERGE reachable again
    If a non interactive rebase is launched, stopping after a conflict
    should set the repository state to RepositoryState.REBASING_MERGE
    instead of RepositoryState.REBASING_INTERACTIVE.
    
    bug: 452623
    Change-Id: Ie885aab6d71dabd158a718af0d14fff643c9b850
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseCommand.java
@@ -1089,7 +1089,8 @@ public class RebaseCommand extends GitCommand<RebaseResult> {
 		rebaseState.createFile(HEAD_NAME, headName);
 		rebaseState.createFile(ONTO, upstreamCommit.name());
 		rebaseState.createFile(ONTO_NAME, upstreamCommitName);
-		rebaseState.createFile(INTERACTIVE, ""); //$NON-NLS-1$
+		if (isInteractive()) 
+			rebaseState.createFile(INTERACTIVE, ""); //$NON-NLS-1$
 		rebaseState.createFile(QUIET, ""); //$NON-NLS-1$
 
 		ArrayList<RebaseTodoLine> toDoSteps = new ArrayList<RebaseTodoLine>();
