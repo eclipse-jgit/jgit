8d37688d6ce8d1f4e506e0f2d5faf0b986dc4c71
Christian Halstrick <christian.halstrick@sap.com>
2014-12-10 17:42:42 +0100

    Allow explicit configuration of git directory in InitCommand
    Native git's "init" command allows to specify the location of the .git
    folder with the option "--separate-git-dir". This allows for example to
    setup repositories with a non-standard layout. E.g. .git folder under
    /repos/a.git and the worktree under /home/git/a. Both directories
    contain pointers to the other side: /repos/a.git/config contains
    core.worktree=/home/git/a . And /home/git/a/.git is a file containing
    "gitdir: /repos/a.git". This commit adds that option to InitCommand.
    This feature is needed to support the new submodule layout where the
    .git folder of the submodules is under .git/modules/<submodule>.
    
    Change-Id: I0208f643808bf8f28e2c979d6e33662607775f1f

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -308,6 +308,8 @@ public class JGitText extends TranslationBundle {
 	/***/ public String indexFileIsTooLargeForJgit;
 	/***/ public String indexSignatureIsInvalid;
 	/***/ public String indexWriteException;
+	/***/ public String initFailedBareRepoDifferentDirs;
+	/***/ public String initFailedNonBareRepoSameDirs;
 	/***/ public String inMemoryBufferLimitExceeded;
 	/***/ public String inputStreamMustSupportMark;
 	/***/ public String integerValueOutOfRange;
