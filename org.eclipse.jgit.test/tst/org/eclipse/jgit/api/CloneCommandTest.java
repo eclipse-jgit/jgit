53ff0529988ae8ec6f75a1f3d1b7b75de7dc304f
Christian Halstrick <christian.halstrick@sap.com>
2014-12-10 17:54:28 +0100

    Support the new repository layout for submodules
    When updating a submodule (e.g. during recursive clone) the repository
    for the submodule should be located at <gitdir>/modules/<submodule-path>
    whereas the working tree of the submodule should be located at
    <working-tree>/<submodule-path> (<gitdir> and <working-tree> are
    associated to the containing repository). Since CloneCommand has learned
    about specifying a separate gitdir this is easy to implement in
    SubmoduleUpdateCommand.
    
    Change-Id: I9b56a3dfa50f97f6975c2bb7c97b36296f331b64

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
@@ -410,6 +410,18 @@ public class CloneCommandTest extends RepositoryTestCase {
 		assertEquals(SubmoduleStatusType.INITIALIZED, pathStatus.getType());
 		assertEquals(commit, pathStatus.getHeadId());
 		assertEquals(commit, pathStatus.getIndexId());
+
+		SubmoduleWalk walk = SubmoduleWalk.forIndex(git2.getRepository());
+		assertTrue(walk.next());
+		Repository clonedSub1 = walk.getRepository();
+		addRepoToClose(clonedSub1);
+		assertNotNull(clonedSub1);
+		assertEquals(
+				new File(git2.getRepository().getWorkTree(), walk.getPath()),
+				clonedSub1.getWorkTree());
+		assertEquals(new File(new File(git2.getRepository().getDirectory(),
+				"modules"), walk.getPath()), clonedSub1.getDirectory());
+		walk.release();
 	}
 
 	@Test
@@ -492,6 +504,12 @@ public class CloneCommandTest extends RepositoryTestCase {
 		assertTrue(walk.next());
 		Repository clonedSub1 = walk.getRepository();
 		assertNotNull(clonedSub1);
+		assertEquals(
+				new File(git2.getRepository().getWorkTree(), walk.getPath()),
+				clonedSub1.getWorkTree());
+		assertEquals(new File(new File(git2.getRepository().getDirectory(),
+				"modules"), walk.getPath()),
+				clonedSub1.getDirectory());
 		status = new SubmoduleStatusCommand(clonedSub1);
 		statuses = status.call();
 		clonedSub1.close();
