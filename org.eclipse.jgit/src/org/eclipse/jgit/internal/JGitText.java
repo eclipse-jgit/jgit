8d508bd66a418e55bf232790658a4c74d8143a54
Matthias Sohn <matthias.sohn@sap.com>
2015-01-15 17:30:11 +0100

    Don't remove pack from pack list for problems which could be transient
    If we hit a corrupt object or invalid pack remove the pack from the pack
    list. Other IOException could be transient hence we should not remove
    the pack from the list to avoid the problem reported on the Gerrit list
    [1]. It looks like in the reported case the pack was removed from the
    pack list causing MissingObjectExceptions which disappear when the
    server is restarted.
    
    [1] https://groups.google.com/forum/#!topic/repo-discuss/Qdmbl-YZ4NU
    
    Change-Id: I331626110d54b190e46cddc2c40f29ddeb9613cd
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -190,6 +190,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String corruptObjectNotree;
 	/***/ public String corruptObjectNoType;
 	/***/ public String corruptObjectPackfileChecksumIncorrect;
+	/***/ public String corruptPack;
 	/***/ public String couldNotCheckOutBecauseOfConflicts;
 	/***/ public String couldNotDeleteLockFileShouldNotHappen;
 	/***/ public String couldNotDeleteTemporaryIndexFileShouldNotHappen;
