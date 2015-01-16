82556b1896222ea83c6f10b1c9db00e91ebaab6a
Stefan Beller <sbeller@google.com>
2015-01-16 15:48:22 -0800

    WIP: Port push certificates
    push certs have been part of git-core for a few months now. Let's try
    to support that feature.
    
    This is not complete and is lacking the actual function to validate
    the signature for now
    
    Change-Id: I249869cadb2d55aef016371b9311b8583591b9cf
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/ReceivePack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/ReceivePack.java
@@ -185,6 +185,7 @@ public class ReceivePack extends BaseReceivePack {
 		recvCommands();
 		if (hasCommands()) {
 			enableCapabilities();
+			preparePushCertificate();
 
 			Throwable unpackError = null;
 			if (needPack()) {
