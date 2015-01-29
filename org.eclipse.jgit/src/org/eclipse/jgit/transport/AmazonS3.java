9a787a179d8db32b54b2c94f9a61b5237840361e
Matthew Spurrier <matthew@spurrier.com.au>
2015-01-29 17:56:03 +0800

    Make s3 domain dynamic to support different s3 regions
    Change-Id: I24ebf3bd747f57b42eada8c89b95738100d57920
    Signed-off-by: Matthew Spurrier <matthew@spurrier.com.au>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/AmazonS3.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/AmazonS3.java
@@ -115,7 +115,7 @@ public class AmazonS3 {
 
 	private static final String HMAC = "HmacSHA1"; //$NON-NLS-1$
 
-	private static final String DOMAIN = "s3.amazonaws.com"; //$NON-NLS-1$
+	private static final String DOMAIN; //$NON-NLS-1$
 
 	private static final String X_AMZ_ACL = "x-amz-acl"; //$NON-NLS-1$
 
@@ -201,6 +201,10 @@ public class AmazonS3 {
 	 * # PRIVATE, PUBLIC_READ (defaults to PRIVATE).
 	 * acl: PRIVATE
 	 *
+	 * # S3 Domain
+	 * # AWS S3 Region Domain (defaults to s3.amazonaws.com)
+	 * domain: s3.amazonaws.com
+	 *
 	 * # Number of times to retry after internal error from S3.
 	 * httpclient.retry-max: 3
 	 *
@@ -214,6 +218,7 @@ public class AmazonS3 {
 	 *
 	 */
 	public AmazonS3(final Properties props) {
+		DOMAIN = props.getProperty("domain", "s3.amazonaws.com");
 		publicKey = props.getProperty("accesskey"); //$NON-NLS-1$
 		if (publicKey == null)
 			throw new IllegalArgumentException(JGitText.get().missingAccesskey);
