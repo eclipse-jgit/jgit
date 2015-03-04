5073286a7b70109f8f32afe141045402044e0771
Christian Halstrick <christian.halstrick@sap.com>
2015-03-04 12:43:01 +0100

    Add "--long" option to JGit describe
    Native git supports "git describe --long". This will enforce returning a
    long description of a commit even if a tag is directly pointing to the
    commit (in contrast to just returning the tag name as it is now). This
    commit teaches JGits DescribeCommand and the describe command in the pgm
    package to support "--long".
    
    Bug: 460991
    Change-Id: I65e179b79e89049c6deced3c71cb3ebb08ed0a8f

--- org.eclipse.jgit/src/org/eclipse/jgit/api/DescribeCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/DescribeCommand.java
@@ -89,6 +89,11 @@ public class DescribeCommand extends GitCommand<String> {
 	private int maxCandidates = 10;
 
 	/**
+	 * Whether to always use long output format or not.
+	 */
+	private boolean longDesc = false;
+
+	/**
 	 *
 	 * @param repo
 	 */
@@ -136,6 +141,24 @@ public class DescribeCommand extends GitCommand<String> {
 		if (id == null)
 			throw new RefNotFoundException(MessageFormat.format(JGitText.get().refNotResolved, rev));
 		return setTarget(id);
+	}
+
+	/**
+	 * Determine whether always to use the long format or not. When set to
+	 * <code>true</code> the long format is used even the commit matches a tag.
+	 *
+	 * @param longDesc
+	 *            <code>true</code> if always the long format should be used.
+	 * @return {@code this}
+	 *
+	 * @see <a
+	 *      href="https://www.kernel.org/pub/software/scm/git/docs/git-describe.html"
+	 *      >Git documentation about describe</a>
+	 * @since 4.0
+	 */
+	public DescribeCommand setLong(boolean longDesc) {
+		this.longDesc = longDesc;
+		return this;
 	}
 
 	/**
@@ -213,8 +236,20 @@ public class DescribeCommand extends GitCommand<String> {
 
 			// is the target already pointing to a tag? if so, we are done!
 			Ref lucky = tags.get(target);
-			if (lucky != null)
-				return lucky.getName().substring(R_TAGS.length());
+			if (lucky != null) {
+				if (!longDesc) {
+					return lucky.getName().substring(R_TAGS.length());
+				} else {
+					ObjectId objectId = lucky.getPeeledObjectId();
+					if (objectId == null) {
+						objectId = lucky.getObjectId();
+					}
+					return String.format("%s-0-g%s",
+							lucky.getName().substring(R_TAGS.length()), w
+									.getObjectReader().abbreviate(objectId)
+									.name());
+				}
+			}
 
 			w.markStart(target);
 
