d45c4c5e88112eea7f4729dad2fad2c4d1a3d182
Hugo Arès <hugo.ares@ericsson.com>
2015-04-07 11:17:06 -0400

    Remove pack from list when file handle is stale
    This error happens on nfs file system when you try to read a file that
    was deleted or replaced.
    
    When the error happens because the file was deleted, removing it from
    the list is the proper way to handle the error, same use case as
    FileNotFoundException. When the error happens because the file was
    replaced, removing the file from the list will cause the file to be
    re-read so it will get the latest version of the file.
    
    Bug: 462868
    Change-Id: I368af61a6cf73706601a3e4df4ef24f0aa0465c5
    Signed-off-by: Hugo Arès <hugo.ares@ericsson.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -453,6 +453,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String packFileInvalid;
 	/***/ public String packfileIsTruncated;
 	/***/ public String packfileIsTruncatedNoParam;
+	/***/ public String packHandleIsStale;
 	/***/ public String packHasUnresolvedDeltas;
 	/***/ public String packingCancelledDuringObjectsWriting;
 	/***/ public String packObjectCountMismatch;
