988aac4429431b8dc0a0107f07d3ac9ffbc2f7cd
Shawn Pearce <spearce@spearce.org>
2015-05-08 22:26:28 -0700

    ObjectReader: remove the walkAdvice API
    This was added a very long time ago to support the failed
    DHT storage implementation. Since then no storage system
    was able to make use of this API, but it pollutes internals
    of the walkers.
    
    Kill the API on ObjectReader and drop the invocations from
    the walker code.
    
    Change-Id: I36608afdac13a6c3084d7c7e0af5e0cb22900332

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/ObjectWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/ObjectWalk.java
@@ -99,10 +99,6 @@ public class ObjectWalk extends RevWalk {
 
 	private BlockObjQueue pendingObjects;
 
-	private RevCommit firstCommit;
-
-	private RevCommit lastCommit;
-
 	private TreeVisit freeVisit;
 
 	private TreeVisit currVisit;
@@ -260,8 +256,6 @@ public class ObjectWalk extends RevWalk {
 		for (;;) {
 			final RevCommit r = super.next();
 			if (r == null) {
-				if (firstCommit != null)
-					reader.walkAdviceBeginTrees(this, firstCommit, lastCommit);
 				return null;
 			}
 			if ((r.flags & UNINTERESTING) != 0) {
@@ -270,9 +264,6 @@ public class ObjectWalk extends RevWalk {
 					return r;
 				continue;
 			}
-			if (firstCommit == null)
-				firstCommit = r;
-			lastCommit = r;
 			pendingObjects.add(r.getTree());
 			return r;
 		}
@@ -366,7 +357,6 @@ public class ObjectWalk extends RevWalk {
 		for (;;) {
 			RevObject o = pendingObjects.next();
 			if (o == null) {
-				reader.walkAdviceEnd();
 				return null;
 			}
 			int flags = o.flags;
@@ -634,8 +624,6 @@ public class ObjectWalk extends RevWalk {
 	public void dispose() {
 		super.dispose();
 		pendingObjects = new BlockObjQueue();
-		firstCommit = null;
-		lastCommit = null;
 		currVisit = null;
 		freeVisit = null;
 	}
@@ -649,8 +637,6 @@ public class ObjectWalk extends RevWalk {
 
 		rootObjects = new ArrayList<RevObject>();
 		pendingObjects = new BlockObjQueue();
-		firstCommit = null;
-		lastCommit = null;
 		currVisit = null;
 		freeVisit = null;
 	}
