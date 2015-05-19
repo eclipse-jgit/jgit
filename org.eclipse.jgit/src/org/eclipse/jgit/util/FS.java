bbfcd856df0220ca8410ce34d79365cc4993973b
Sebastian Schuberth <sschuberth@gmail.com>
2015-05-19 11:43:30 +0200

    FS: Remove the gitprefix logic
    The only purpose of the gitprefix logic was to determine the path to the
    system-wide config file. This is now done by discoverGitSystemConfig()
    independent of the gitprefix, so get rid of this unused code.
    
    Change-Id: Iaa88df9bd066dc1ed4067c18618af809e49876b3
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
@@ -157,8 +157,6 @@ public abstract class FS {
 
 	private volatile Holder<File> userHome;
 
-	private volatile Holder<File> gitPrefix;
-
 	/**
 	 * Constructs a file system abstraction.
 	 */
@@ -174,7 +172,6 @@ public abstract class FS {
 	 */
 	protected FS(FS src) {
 		userHome = src.userHome;
-		gitPrefix = src.gitPrefix;
 	}
 
 	/** @return a new instance of the same type of FS. */
@@ -514,21 +511,6 @@ public abstract class FS {
 		return null;
 	}
 
-	/** @return the $prefix directory C Git would use. */
-	public File gitPrefix() {
-		Holder<File> p = gitPrefix;
-		if (p == null) {
-			String overrideGitPrefix = SystemReader.getInstance().getProperty(
-					"jgit.gitprefix"); //$NON-NLS-1$
-			if (overrideGitPrefix != null)
-				p = new Holder<File>(new File(overrideGitPrefix));
-			else
-				p = new Holder<File>(discoverGitPrefix());
-			gitPrefix = p;
-		}
-		return p.value;
-	}
-
 	/** @return the path to the Git executable. */
 	protected abstract File discoverGitExe();
 
@@ -552,11 +534,6 @@ public abstract class FS {
 		return new File(w);
 	}
 
-	/** @return the $prefix directory C Git would use. */
-	protected File discoverGitPrefix() {
-		return resolveGrandparentFile(discoverGitExe());
-	}
-
 	protected static File resolveGrandparentFile(File grandchild) {
 		if (grandchild != null) {
 			File parent = grandchild.getParentFile();
@@ -564,18 +541,6 @@ public abstract class FS {
 				return parent.getParentFile();
 		}
 		return null;
-	}
-
-	/**
-	 * Set the $prefix directory C Git uses.
-	 *
-	 * @param path
-	 *            the directory. Null if C Git is not installed.
-	 * @return {@code this}
-	 */
-	public FS setGitPrefix(File path) {
-		gitPrefix = new Holder<File>(path);
-		return this;
 	}
 
 	/**
