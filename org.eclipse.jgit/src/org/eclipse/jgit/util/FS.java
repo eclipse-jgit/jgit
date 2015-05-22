877578210c0f252d115c91d9b054c3c3ea307fb0
Sebastian Schuberth <sschuberth@gmail.com>
2015-05-22 17:21:27 +0200

    FS: Allow to manually set the path to the Git system config file
    Now that d7a4473 removed the gitprefix property, we did not have a way to
    specify the path to the Git system config file in case
    discoverGitSystemConfig() fails. Fix that by introducing a member variable
    that caches the result of discoverGitSystemConfig() as well as a setter
    method to overwrite the content of that variable.
    
    Change-Id: Icd965bffbe2f11b18c9505ee2ddd2afad5b64d70
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
@@ -157,11 +157,13 @@ public abstract class FS {
 
 	private volatile Holder<File> userHome;
 
+	private volatile Holder<File> gitSystemConfig;
+
 	/**
 	 * Constructs a file system abstraction.
 	 */
 	protected FS() {
-		// Do nothing by default.
+		gitSystemConfig = new Holder<File>(discoverGitSystemConfig());
 	}
 
 	/**
@@ -172,6 +174,7 @@ public abstract class FS {
 	 */
 	protected FS(FS src) {
 		userHome = src.userHome;
+		gitSystemConfig = src.gitSystemConfig;
 	}
 
 	/** @return a new instance of the same type of FS. */
@@ -543,6 +546,27 @@ public abstract class FS {
 		}
 
 		return new File(w);
+	}
+
+	/**
+	 * @return the currently used path to the system-wide Git configuration file.
+	 * @since 4.0
+	 */
+	public File getGitSystemConfig() {
+		return gitSystemConfig.value;
+	}
+
+	/**
+	 * Set the path to the system-wide Git configuration file to use.
+	 *
+	 * @param configFile
+	 *            the path to the config file.
+	 * @return {@code this}
+	 * @since 4.0
+	 */
+	public FS setGitSystemConfig(File configFile) {
+		gitSystemConfig = new Holder<File>(configFile);
+		return this;
 	}
 
 	/**
