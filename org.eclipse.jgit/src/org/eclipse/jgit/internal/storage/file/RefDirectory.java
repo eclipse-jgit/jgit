b9c99a0709e52842661e0abbf33bc2c1e6426976
Jonathan Nieder <jrn@google.com>
2015-06-05 14:10:45 -0700

    Do not use SEARCH_PATH to find MERGE_HEAD and co
    Even if there is a branch named refs/heads/MERGE_HEAD and no
    MERGE_HEAD directly under .git, getAdditionalRefs() should not include
    refs under refs/heads/ in its result.
    
    NEEDSWORK: tests
    
    Change-Id: Iab8ac47008822fa78fc0691e239e518c34d7a98e
    Signed-off-by: Jonathan Nieder <jrn@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
@@ -341,7 +341,7 @@ public class RefDirectory extends RefDatabase {
 	public List<Ref> getAdditionalRefs() throws IOException {
 		List<Ref> ret = new LinkedList<Ref>();
 		for (String name : additionalRefsNames) {
-			Ref r = getRef(name);
+			Ref r = exactRef(name);
 			if (r != null)
 				ret.add(r);
 		}
