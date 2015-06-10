eb5ab723e41186d35077817b44482a2c08712022
Dave Borowitz <dborowitz@google.com>
2015-06-09 17:23:03 -0700

    Rewrite push certificate parsing
    - Consistently return structured data, such as actual ReceiveCommands,
      which is more useful for callers that are doing things other than
      verifying the signature, e.g. recording the set of commands.
    - Store the certificate version field, as this is required to be part
      of the signed payload.
    - Add a toText() method to recreate the actual payload for signature
      verification. This requires keeping track of the un-chomped command
      strings from the original protocol stream.
    - Separate the parser from the certificate itself, so the actual
      PushCertificate object can be immutable. Make a fair attempt at deep
      immutability, but this is not possible with the current mutable
      ReceiveCommand structure.
    - Use more detailed error messages that don't involve NON-NLS strings.
    - Document null return values more thoroughly. Instead of having the
      undocumented behavior of throwing NPE from certain methods if they
      are not first guarded by enabled(), eliminate enabled() and return
      null from those methods.
    - Add tests for parsing a push cert from a section of pkt-line stream
      using a real live stream captured with Wireshark (which, it should
      be noted, uncovered several simply incorrect statements in C git's
      Documentation/technical/pack-protocol.txt).
    
    Since these classes are public APIs in 4.0, we need to leave the
    existing inheritance structure and methods. For PushCertificate, the
    only way to create an instance with non-null field values was to use a
    subclass, so it is sufficient to add a public no-arg constructor that
    sets all fields to null. For the parser, we mark the getters as
    @Deprecated and refer to the results of build().
    
    Change-Id: I5382193347a8eb1811032d9b32af9651871372d0

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/BaseReceivePack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/BaseReceivePack.java
@@ -252,13 +252,19 @@ public abstract class BaseReceivePack {
 	/** The size of the received pack, including index size */
 	private Long packSize;
 
-	PushCertificateParser pushCertificateParser;
+	private PushCertificateParser pushCertificateParser;
 
 	/**
-	 * @return the push certificate used to verify the pushers identity.
+	 * Get the push certificate used to verify the pusher's identity.
+	 * <p>
+	 * Only valid after commands are read from the wire.
+	 *
+	 * @return the parsed certificate, or null if push certificates are disabled.
+	 * @throws IOException if the certificate was present but invalid.
+	 * @since 4.1
 	 */
-	PushCertificate getPushCertificate() {
-		return pushCertificateParser;
+	public PushCertificate getPushCertificate() throws IOException {
+		return pushCertificateParser.build();
 	}
 
 	/**
@@ -1014,9 +1020,10 @@ public abstract class BaseReceivePack {
 		adv.advertiseCapability(CAPABILITY_REPORT_STATUS);
 		if (allowQuiet)
 			adv.advertiseCapability(CAPABILITY_QUIET);
-		if (pushCertificateParser.enabled())
-			adv.advertiseCapability(
-				pushCertificateParser.getAdvertiseNonce());
+		String nonce = pushCertificateParser.getAdvertiseNonce();
+		if (nonce != null) {
+			adv.advertiseCapability(nonce);
+		}
 		if (db.getRefDatabase().performsAtomicTransactions())
 			adv.advertiseCapability(CAPABILITY_ATOMIC);
 		if (allowOfsDelta)
@@ -1065,13 +1072,8 @@ public abstract class BaseReceivePack {
 							!isBiDirectionalPipe());
 			}
 
-			if (line.equals("-----BEGIN PGP SIGNATURE-----\n")) //$NON-NLS-1$
+			if (line.equals(PushCertificateParser.BEGIN_SIGNATURE)) {
 				pushCertificateParser.receiveSignature(pckIn);
-
-			if (pushCertificateParser.enabled()) {
-				// Must use raw line with optional newline so signed payload can be
-				// reconstructed.
-				pushCertificateParser.addCommand(rawLine);
 			}
 
 			if (line.length() < 83) {
@@ -1087,6 +1089,11 @@ public abstract class BaseReceivePack {
 				cmd.setRef(refs.get(cmd.getRefName()));
 			}
 			commands.add(cmd);
+			if (pushCertificateParser.enabled()) {
+				// Must use raw line with optional newline so signed payload can be
+				// reconstructed.
+				pushCertificateParser.addCommand(cmd, rawLine);
+			}
 		}
 	}
 
