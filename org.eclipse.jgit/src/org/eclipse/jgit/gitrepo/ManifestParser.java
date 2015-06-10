46fc3e818a7ece9daba604a7ef732434944a45bf
Jonathan Nieder <jrn@google.com>
2015-06-10 16:35:43 -0700

    Require all projects to have a name
    A project's name attribute is required according to repo's
    doc/manifest-format.txt:
    
        <!ELEMENT project (annotation*,
                           project*)>
        <!ATTLIST project name        CDATA #REQUIRED>
    
    Enforcing this in code makes reading easier (in particular making it
    clearer that getName() and getPath() can never return null).
    
    Change-Id: I8c7014dd6042183d7fecb2202af5acdc384aa8e4
    Signed-off-by: Jonathan Nieder <jrn@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/ManifestParser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/ManifestParser.java
@@ -183,6 +183,9 @@ public class ManifestParser extends DefaultHandler {
 			String qName,
 			Attributes attributes) throws SAXException {
 		if ("project".equals(qName)) { //$NON-NLS-1$
+			if (attributes.getValue("name") == null) {
+				throw new SAXException(RepoText.get().invalidManifest);
+			}
 			currentProject = new RepoProject(
 					attributes.getValue("name"), //$NON-NLS-1$
 					attributes.getValue("path"), //$NON-NLS-1$
