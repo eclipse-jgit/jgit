450a764d98e94aa93958f50c128e9a6a4c7baa09
Dave Borowitz <dborowitz@google.com>
2015-06-29 17:26:57 -0700

    PushCertificateParser: Add method for parsing from a stream
    We intend to store received push certificates somewhere, like a
    particular ref in the repository in question. For reading data back
    out, it will be useful to read push certificates (without pkt-line
    framing) in a streaming fashion.
    
    Change-Id: I70de313b1ae463407b69505caee63e8f4e057ed4

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PushCertificate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PushCertificate.java
@@ -106,11 +106,6 @@ public class PushCertificate {
 			throw new IllegalArgumentException(MessageFormat.format(
 					JGitText.get().pushCertificateInvalidField, NONCE));
 		}
-		if (nonceStatus == null) {
-			throw new IllegalArgumentException(MessageFormat.format(
-					JGitText.get().pushCertificateInvalidField,
-					"nonce status")); //$NON-NLS-1$
-		}
 		if (commands == null || commands.isEmpty()) {
 			throw new IllegalArgumentException(MessageFormat.format(
 					JGitText.get().pushCertificateInvalidField,
