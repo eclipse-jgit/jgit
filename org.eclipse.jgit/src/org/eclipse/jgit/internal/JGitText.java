8cb2dd9adafc83523e9d6e475a9df38b72b82dcc
Dave Borowitz <dborowitz@google.com>
2015-06-29 18:05:11 -0700

    Store push certificates in refs/meta/push-certs
    Inspired by a proposal from gitolite[1], where we store a file in
    a tree for each ref name, and the contents of the file is the latest
    push cert to affect that ref.
    
    The main modification from that proposal (other than lacking the
    out-of-git batching) is to append "@{cert}" to filenames, which allows
    storing certificates for both refs/foo and refs/foo/bar. Those
    refnames cannot coexist at the same time in a repository, but we do
    not want to discard the push certificate responsible for deleting the
    ref, which we would have to do if refs/foo in the push cert tree
    changed from a tree to a blob.
    
    The "@{cert}" syntax is at least somewhat consistent with
    gitrevisions(7) wherein @{...} describe operators on ref names.
    
    As we cannot (currently) atomically update the push cert ref with the
    refs that were updated, this operation is inherently racy. Kick the can
    down the road by pushing this burden on callers.
    
    [1] https://github.com/sitaramc/gitolite/blob/cf062b8bb6b21a52f7c5002d33fbc950762c1aa7/contrib/hooks/repo-specific/save-push-signatures
    
    Change-Id: Id3eb32416f969fba4b5e4d9c4b47053c564b0ccd

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -623,6 +623,9 @@ public class JGitText extends TranslationBundle {
 	/***/ public String stashFailed;
 	/***/ public String stashResolveFailed;
 	/***/ public String statelessRPCRequiresOptionToBeEnabled;
+	/***/ public String storePushCertMultipleRefs;
+	/***/ public String storePushCertOneRef;
+	/***/ public String storePushCertReflog;
 	/***/ public String submoduleExists;
 	/***/ public String submodulesNotSupported;
 	/***/ public String submoduleParentRemoteUrlInvalid;
