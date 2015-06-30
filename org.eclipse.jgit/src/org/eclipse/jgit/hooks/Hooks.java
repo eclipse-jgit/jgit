dfc954340f18304daf06cfe7604d040f73caa4da
Christian Halstrick <christian.halstrick@sap.com>
2015-06-30 16:43:20 +0200

    Add support for pre-push hooks
    When the file <git-dir>/hooks/pre-push exists make sure that is is
    executing during a push. The pre-push hook runs during git push, after
    the remote refs have been updated but before any objects have been
    transferred.
    
    Change-Id: Ibbb58ee3227742d1a2f913134ce11e7a135c7f4c

--- org.eclipse.jgit/src/org/eclipse/jgit/hooks/Hooks.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/hooks/Hooks.java
@@ -74,4 +74,15 @@ public class Hooks {
 			PrintStream outputStream) {
 		return new CommitMsgHook(repo, outputStream);
 	}
+
+	/**
+	 * @param repo
+	 * @param outputStream
+	 *            The output stream, or {@code null} to use {@code System.out}
+	 * @return The pre-push hook for the given repository.
+	 * @since 4.2
+	 */
+	public static PrePushHook prePush(Repository repo, PrintStream outputStream) {
+		return new PrePushHook(repo, outputStream);
+	}
 }
