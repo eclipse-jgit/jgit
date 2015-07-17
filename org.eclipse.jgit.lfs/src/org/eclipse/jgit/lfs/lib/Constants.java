312f736d861c4d33befe857d0a6011c9b139b6b8
Matthias Sohn <matthias.sohn@sap.com>
2015-07-17 16:22:56 +0200

    Implement SHA-256 abstraction
    The Large File Storage extension specified by GitHub [1] uses SHA-256 to
    compute the ID of large files stored by the extension. Hence implement a
    SHA-256 abstraction similar to the SHA-1 abstraction used by JGit.
    
    [1] https://git-lfs.github.com/
    
    Bug: 470333
    Change-Id: I3a95954543c8570d73929e55f4a884b55dbf1b7a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/lib/Constants.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015, Matthias Sohn <matthias.sohn@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.lfs.lib;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.MessageFormat;
+
+import org.eclipse.jgit.lfs.internal.LfsText;
+
+/**
+ * Misc. constants used throughout JGit LFS extension.
+ *
+ * @since 4.1
+ **/
+@SuppressWarnings("nls")
+public final class Constants {
+	/** Hash function used natively by Git LFS extension for large objects. */
+	private static final String LONG_HASH_FUNCTION = "SHA-256";
+
+	/**
+	 * A Git LFS large object hash is 256 bits, i.e. 32 bytes.
+	 * <p>
+	 * Changing this assumption is not going to be as easy as changing this
+	 * declaration.
+	 */
+	public static final int LONG_OBJECT_ID_LENGTH = 32;
+
+	/**
+	 * A Git LFS large object can be expressed as a 64 character string of
+	 * hexadecimal digits.
+	 *
+	 * @see #LONG_OBJECT_ID_LENGTH
+	 */
+	public static final int LONG_OBJECT_ID_STRING_LENGTH = LONG_OBJECT_ID_LENGTH
+			* 2;
+
+	/**
+	 * Create a new digest function for objects.
+	 *
+	 * @return a new digest object.
+	 * @throws RuntimeException
+	 *             this Java virtual machine does not support the required hash
+	 *             function. Very unlikely given that JGit uses a hash function
+	 *             that is in the Java reference specification.
+	 */
+	public static MessageDigest newMessageDigest() {
+		try {
+			return MessageDigest.getInstance(LONG_HASH_FUNCTION);
+		} catch (NoSuchAlgorithmException nsae) {
+			throw new RuntimeException(MessageFormat.format(
+					LfsText.get().requiredHashFunctionNotAvailable,
+					LONG_HASH_FUNCTION), nsae);
+		}
+	}
+
+	static {
+		if (LONG_OBJECT_ID_LENGTH != newMessageDigest().getDigestLength())
+			throw new LinkageError(
+					LfsText.get().incorrectLONG_OBJECT_ID_LENGTH);
+	}
+}
