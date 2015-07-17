37873b02abcdd17161de9de3bbdad3367d2b021b
Matthias Sohn <matthias.sohn@sap.com>
2015-07-17 16:22:56 +0200

    Implement SHA-256 abstraction
    The Large File Storage extension specified by GitHub [1] uses SHA-256 to
    compute the ID of large files stored by the extension. Hence implement a
    SHA-256 abstraction similar to the SHA-1 abstraction used by JGit.
    
    [1] https://git-lfs.github.com/
    
    Bug: 470333
    Change-Id: I3a95954543c8570d73929e55f4a884b55dbf1b7a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.lfs.test/src/org/eclipse/jgit/lfs/test/LongObjectIdTestUtils.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2015, Matthias Sohn <matthias.sohn@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.lfs.test;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+
+import org.eclipse.jgit.lfs.lib.Constants;
+import org.eclipse.jgit.lfs.lib.LongObjectId;
+
+public class LongObjectIdTestUtils {
+
+	/**
+	 * Create id as hash of the given string.
+	 *
+	 * @param s
+	 *            the string to hash
+	 * @return id calculated by hashing string
+	 */
+	public static LongObjectId hash(String s) {
+		MessageDigest md = Constants.newMessageDigest();
+		md.update(s.getBytes(StandardCharsets.UTF_8));
+		return LongObjectId.fromRaw(md.digest());
+	}
+
+	/**
+	 * Create id as hash of a file content
+	 *
+	 * @param file
+	 *            the file to hash
+	 * @return id calculated by hashing file content
+	 * @throws FileNotFoundException
+	 *             if file doesn't exist
+	 * @throws IOException
+	 */
+	public static LongObjectId hash(Path file)
+			throws FileNotFoundException, IOException {
+		MessageDigest md = Constants.newMessageDigest();
+		try (InputStream is = new BufferedInputStream(
+				Files.newInputStream(file))) {
+			final byte[] buffer = new byte[4096];
+			for (int read = 0; (read = is.read(buffer)) != -1;) {
+				md.update(buffer, 0, read);
+			}
+		}
+		return LongObjectId.fromRaw(md.digest());
+	}
+}
