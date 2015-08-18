8b38c7db4266741b3098fabc86e4c2907035517a
Andrey Loskutov <loskutov@gmx.de>
2015-08-18 20:01:15 +0200

    Restored obsoleted createSymLink/readSymLink in FileUtil
    Bug: 475070
    Change-Id: I425ad842dc26b55f747f192348398a3912c0ca6b
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FileUtil.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FileUtil.java
@@ -66,6 +66,31 @@ public class FileUtil {
 
 	/**
 	 * @param path
+	 * @return target path of the symlink
+	 * @throws IOException
+	 * @deprecated use {@link FileUtils#readSymLink(File)} instead
+	 */
+	@Deprecated
+	public static String readSymlink(File path) throws IOException {
+		return FileUtils.readSymLink(path);
+	}
+
+	/**
+	 * @param path
+	 *            path of the symlink to be created
+	 * @param target
+	 *            target of the symlink to be created
+	 * @throws IOException
+	 * @deprecated use {@link FileUtils#createSymLink(File, String)} instead
+	 */
+	@Deprecated
+	public static void createSymLink(File path, String target)
+			throws IOException {
+		FileUtils.createSymLink(path, target);
+	}
+
+	/**
+	 * @param path
 	 * @return {@code true} if the passed path is a symlink
 	 */
 	public static boolean isSymlink(File path) {
