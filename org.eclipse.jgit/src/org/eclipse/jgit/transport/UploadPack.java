b46c4463959f45b32e471e4c10b5ddc71232949e
Shawn Pearce <spearce@spearce.org>
2015-09-14 12:18:20 -0700

    UploadPack: Verify clients send only commits for shallow lines
    If a client mistakenly tries to send a tag object as a shallow line
    JGit blindly assumes this is a commit and tries to parse the tag
    buffer using the commit parser. This can cause an obtuse error like:
    
      InvalidObjectIdException: Invalid id: t c0ff331234...
    
    The "t" comes from the "object c0ff331234..." line of the tag tring
    to be parsed as though it where the "tree" line of a commit.
    
    Run any client supplied shallow lines through the RevWalk to lookup
    the object types. Fail fast with a protocol exception if any of them
    are non-commit.
    
    Skip objects not known to this repository. This matches behavior
    with git-core's upload-pack, which sliently skips over any shallow
    line object named by the client but not known by the server.
    
    Change-Id: Ic6c57a90a42813164ce65c2244705fc42e84d700

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -735,6 +735,8 @@ public class UploadPack {
 			else
 				multiAck = MultiAck.OFF;
 
+			if (!clientShallowCommits.isEmpty())
+				verifyClientShallow();
 			if (depth != 0)
 				processShallow();
 			if (!clientShallowCommits.isEmpty())
@@ -818,6 +820,35 @@ public class UploadPack {
 			}
 		}
 		pckOut.end();
+	}
+
+	private void verifyClientShallow()
+			throws IOException, PackProtocolException {
+		AsyncRevObjectQueue q = walk.parseAny(clientShallowCommits, true);
+		try {
+			for (;;) {
+				try {
+					// Shallow objects named by the client must be commits.
+					RevObject o = q.next();
+					if (o == null) {
+						break;
+					}
+					if (!(o instanceof RevCommit)) {
+						throw new PackProtocolException(
+							MessageFormat.format(
+								JGitText.get().invalidShallowObject,
+								o.name()));
+					}
+				} catch (MissingObjectException notCommit) {
+					// shallow objects not known at the server are ignored
+					// by git-core upload-pack, match that behavior.
+					clientShallowCommits.remove(notCommit.getObjectId());
+					continue;
+				}
+			}
+		} finally {
+			q.release();
+		}
 	}
 
 	/**
