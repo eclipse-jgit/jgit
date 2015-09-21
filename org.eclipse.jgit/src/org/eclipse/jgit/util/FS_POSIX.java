6bb3f79adb93421f301d2e18c049230b25ed1c0a
Matthias Sohn <matthias.sohn@sap.com>
2015-09-22 01:17:18 +0200

    Use java.nio.file consistently in FS
    Since 4.0 we require Java 7 so there is no longer a need to override the
    following methods in FS_POSIX and FS_Win32
    - lastModified()
    - setLastModified()
    - length()
    - isSymlink()
    - exists()
    - isDirectory()
    - isFile()
    - isHidden()
    hence implement these methods in FS and remove overrides in subclasses.
    
    Change-Id: I5dbde6ec806c66c86ac542978918361461021294
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS_POSIX.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS_POSIX.java
@@ -246,46 +246,6 @@ public class FS_POSIX extends FS {
 	}
 
 	@Override
-	public boolean isSymLink(File path) throws IOException {
-		return FileUtils.isSymlink(path);
-	}
-
-	@Override
-	public long lastModified(File path) throws IOException {
-		return FileUtils.lastModified(path);
-	}
-
-	@Override
-	public void setLastModified(File path, long time) throws IOException {
-		FileUtils.setLastModified(path, time);
-	}
-
-	@Override
-	public long length(File f) throws IOException {
-		return FileUtils.getLength(f);
-	}
-
-	@Override
-	public boolean exists(File path) {
-		return FileUtils.exists(path);
-	}
-
-	@Override
-	public boolean isDirectory(File path) {
-		return FileUtils.isDirectory(path);
-	}
-
-	@Override
-	public boolean isFile(File path) {
-		return FileUtils.isFile(path);
-	}
-
-	@Override
-	public boolean isHidden(File path) throws IOException {
-		return FileUtils.isHidden(path);
-	}
-
-	@Override
 	public void setHidden(File path, boolean hidden) throws IOException {
 		// no action on POSIX
 	}
