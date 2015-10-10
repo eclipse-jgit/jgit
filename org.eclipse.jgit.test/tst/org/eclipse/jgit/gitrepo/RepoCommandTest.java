424b3ac45ea405be8abf92a38d38b3562c76b87e
Andrey Loskutov <loskutov@gmx.de>
2015-10-10 22:02:24 +0300

    Fixed jgit test failures on Windows
    RepoCommandTest was failing because of open file handle left.
    IgnoreNodeTest was failing because of problems with creation of files
    with trailing spaces on Windows.
    RepoCommandTest was failing because of wrong line delimiter.
    
    Change-Id: I34f074ac447eb4c3ada8b250309bb568b426189d
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
@@ -722,18 +722,23 @@ public class RepoCommandTest extends RepositoryTestCase {
 				.call();
 			// Clone it
 			File directory = createTempDirectory("testBareRepo");
-			Repository localDb = Git.cloneRepository().setDirectory(directory)
+			try (Repository localDb = Git.cloneRepository()
+					.setDirectory(directory)
 					.setURI(remoteDb.getDirectory().toURI().toString()).call()
-					.getRepository();
-			// The .gitmodules file should exist
-			File gitmodules = new File(localDb.getWorkTree(), ".gitmodules");
-			assertTrue("The .gitmodules file should exist", gitmodules.exists());
-			FileBasedConfig c = new FileBasedConfig(gitmodules, FS.DETECTED);
-			c.load();
-			assertEquals("standard branches work", "master",
-				c.getString("submodule", "with-branch", "branch"));
-			assertEquals("long branches work", "refs/heads/master",
-				c.getString("submodule", "with-long-branch", "branch"));
+					.getRepository();) {
+				// The .gitmodules file should exist
+				File gitmodules = new File(localDb.getWorkTree(),
+						".gitmodules");
+				assertTrue("The .gitmodules file should exist",
+						gitmodules.exists());
+				FileBasedConfig c = new FileBasedConfig(gitmodules,
+						FS.DETECTED);
+				c.load();
+				assertEquals("standard branches work", "master",
+						c.getString("submodule", "with-branch", "branch"));
+				assertEquals("long branches work", "refs/heads/master",
+						c.getString("submodule", "with-long-branch", "branch"));
+			}
 		}
 	}
 
