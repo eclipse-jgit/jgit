3a7548d2ceefe92e7bf98edc5cf1a33562d03efa
Christian Halstrick <christian.halstrick@sap.com>
2015-10-29 14:15:08 +0100

    WIP: Add support for smudge filters
    If defined in .gitattributes call smudge filter during checkout.
    
    To support checkout where current HEAD,index do not contain attributes
    we need to also consider attributes from the tree we checkout. Therefore
    CanonicalTreeParser has to learn how to provide attributes.
    
    TODO: write a test which clones a repo with smudge filters
    TODO: TreeWalk.getAttributes should be inspected. Decisions on whether
    to search for attributes are done based on iterators which look at the
    current entry. But relevant attributes could reside in the other
    iterators.
    
    Change-Id: I168fdb81a8e1a9f991587b3e95a36550ea845f0a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
@@ -141,9 +141,6 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 	/** If there is a .gitignore file present, the parsed rules from it. */
 	private IgnoreNode ignoreNode;
 
-	/** If there is a .gitattributes file present, the parsed rules from it. */
-	private AttributesNode attributesNode;
-
 	private String cleanFilterCommand;
 
 	/** Repository that is the root level being iterated over */
