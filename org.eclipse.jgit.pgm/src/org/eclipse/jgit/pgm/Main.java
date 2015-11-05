527ddccbf0d734cab822bcebb945e4a2c557d71a
Christian Halstrick <christian.halstrick@sap.com>
2015-11-06 00:56:20 +0100

    WIP: Remove installation of Authenticators from pgm.Main
    When pushing with JGit commandline to e.g. github exceptions occur
    without this fix. The problem is that during a push two request are sent
    to the server, first a GET and then a POST (containing the pack data).
    The first GET request sent anonymously fails with 401 (unauthorized).
    When Authenticators are installed the java.net classes will use the
    Authenticators to ask the user for credentials and retry the request.
    But this happens under the hood and jgit level code doesn't see that
    this happens.
    The next request is the POST but since JGit thinks the first GET request
    went through anonymously it doesn't add authentication headers to the
    POST request. This POST of course also fails with 401 but since this
    request contains a lot of body-data streamed from JGit (the pack file!)
    the java.net classes can't simply retry the request with authorization
    headers. The whole process fails.
    
    If no Authenticators are installed then the situation is better. First
    anonymous GET request fails with 401 and this bubbles up to JGit. Then
    JGit asks the user for credentials (with the help of the installed
    credentialsproviders) and retrys with basic authentication headers. This
    succeeds and when the POST request should be sent JGit knows about the
    succesfull authorization and adds the correct header directly to the
    POST. The request succeeds.
    
    In this proposal I simply remove the authenticators from JGit pgm.
    Anybody knows what will break know?  
    
    Change-Id: Ic50a0685cdf5b1aef77426c115f19ef3f4fab156

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Main.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Main.java
@@ -54,7 +54,6 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jgit.awtui.AwtAuthenticator;
 import org.eclipse.jgit.awtui.AwtCredentialsProvider;
 import org.eclipse.jgit.errors.TransportException;
 import org.eclipse.jgit.lib.Repository;
@@ -117,7 +116,6 @@ public class Main {
 	protected void run(final String[] argv) {
 		try {
 			if (!installConsole()) {
-				AwtAuthenticator.install();
 				AwtCredentialsProvider.install();
 			}
 			configureHttpProxy();
@@ -251,7 +249,6 @@ public class Main {
 
 	private static boolean installConsole() {
 		try {
-			install("org.eclipse.jgit.console.ConsoleAuthenticator"); //$NON-NLS-1$
 			install("org.eclipse.jgit.console.ConsoleCredentialsProvider"); //$NON-NLS-1$
 			return true;
 		} catch (ClassNotFoundException e) {
