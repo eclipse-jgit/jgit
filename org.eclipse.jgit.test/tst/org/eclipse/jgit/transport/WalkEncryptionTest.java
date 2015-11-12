5a1d9de43f86511ab842c42fda783e4ea3204fda
Matthias Sohn <matthias.sohn@sap.com>
2015-11-12 11:28:01 +0100

    Avoid UnknownHostException in WalkEncryptionTest
    Prevent that WalkEncryptionTest fails when it can't determine the public
    IP address using http://checkip.amazonws.com
    
    Change-Id: I1d2fe09f99df2a5f75f8077811a72fb2271cdddb
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/WalkEncryptionTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/WalkEncryptionTest.java
@@ -60,6 +60,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.security.GeneralSecurityException;
@@ -401,14 +402,19 @@ public class WalkEncryptionTest {
 		 * @throws Exception
 		 */
 		static String publicAddress() throws Exception {
-			String service = "http://checkip.amazonaws.com";
-			URL url = new URL(service);
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(url.openStream()));
 			try {
-				return reader.readLine();
-			} finally {
-				reader.close();
+				String service = "http://checkip.amazonaws.com";
+				URL url = new URL(service);
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(url.openStream()));
+				try {
+					return reader.readLine();
+				} finally {
+					reader.close();
+				}
+			} catch (UnknownHostException e) {
+				return "Can't reach http://checkip.amazonaws.com to"
+						+ " determine public address";
 			}
 		}
 
