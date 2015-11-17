59c17254472f5945129a987d2601752793da4481
Ivan Motsch <ivan.motsch@bsiag.com>
2015-11-17 16:04:01 +0100

    Add Attribute Macro Expansion
    Attributes MacroExpander implements macros in git attributes. This is
    implemented inside the TreeWalk using the MacroExpanderImpl class. Basic
    attribute tests use a NullMacroExpander.
    
    Change-Id: I2e69c9fc84e9d7fb8df0a05817d688fc456d8f00
    Signed-off-by: Ivan Motsch <ivan.motsch@bsiag.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/AbstractTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/AbstractTreeIterator.java
@@ -90,6 +90,13 @@ public abstract class AbstractTreeIterator {
 	/** Iterator for the parent tree; null if we are the root iterator. */
 	final AbstractTreeIterator parent;
 
+	/**
+	 * Fast access to the root of the iterator. Used for attributes macro
+	 * expander in order to detect if the current root has changed and
+	 * therefore the macro cache is invalid.
+	 */
+	final AbstractTreeIterator root;
+
 	/** The iterator this current entry is path equal to. */
 	AbstractTreeIterator matches;
 
@@ -149,6 +156,7 @@ public abstract class AbstractTreeIterator {
 	/** Create a new iterator with no parent. */
 	protected AbstractTreeIterator() {
 		parent = null;
+		root = this;
 		path = new byte[DEFAULT_PATH_SIZE];
 		pathOffset = 0;
 	}
@@ -170,6 +178,7 @@ public abstract class AbstractTreeIterator {
 	 */
 	protected AbstractTreeIterator(final String prefix) {
 		parent = null;
+		root = this;
 
 		if (prefix != null && prefix.length() > 0) {
 			final ByteBuffer b;
@@ -204,6 +213,7 @@ public abstract class AbstractTreeIterator {
 	 */
 	protected AbstractTreeIterator(final byte[] prefix) {
 		parent = null;
+		root = this;
 
 		if (prefix != null && prefix.length > 0) {
 			pathLen = prefix.length;
@@ -226,6 +236,7 @@ public abstract class AbstractTreeIterator {
 	 */
 	protected AbstractTreeIterator(final AbstractTreeIterator p) {
 		parent = p;
+		root = p.root;
 		path = p.path;
 		pathOffset = p.pathLen + 1;
 
@@ -256,6 +267,7 @@ public abstract class AbstractTreeIterator {
 	protected AbstractTreeIterator(final AbstractTreeIterator p,
 			final byte[] childPath, final int childPathOffset) {
 		parent = p;
+		root = p.root;
 		path = childPath;
 		pathOffset = childPathOffset;
 	}
