4b114d3c6bc1ed80ba55a5a42241ce1ead6d51cc
Christian Halstrick <christian.halstrick@sap.com>
2015-11-19 17:21:46 +0100

    Throw IndexReadException if existing index can't be read
    If the index file exists but can't be read for example because of wrong
    filesystem permissions we should throw a specific exception. This allows
    EGit to handle this error situation.
    
    Bug: 482607
    Change-Id: I50bfcb719c45caac3cb5550a8b16307c2ea9def4
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCache.java
@@ -63,6 +63,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IndexReadException;
 import org.eclipse.jgit.errors.LockFailedException;
 import org.eclipse.jgit.errors.UnmergedPathException;
 import org.eclipse.jgit.events.IndexChangedEvent;
@@ -418,6 +419,12 @@ public class DirCache {
 					}
 				}
 			} catch (FileNotFoundException fnfe) {
+				if (liveFile.exists()) {
+					// Panic: the index file exists but we can't read it
+					throw new IndexReadException(
+							MessageFormat.format(JGitText.get().cannotReadIndex,
+									liveFile.getAbsolutePath(), fnfe));
+				}
 				// Someone must have deleted it between our exists test
 				// and actually opening the path. That's fine, its empty.
 				//
