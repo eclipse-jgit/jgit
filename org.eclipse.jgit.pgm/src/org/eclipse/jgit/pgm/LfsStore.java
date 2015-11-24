6d347a95a3726afdbd3124f108265cb9a90a7f48
Saša Živkov <sasa.zivkov@sap.com>
2015-11-24 16:55:28 +0100

    EDIT: Running LargeObjectServlet using AppServer on random port
    Change-Id: I1ae6de5f0a599a7f12ff702d402ad09fc25f5677

---
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LfsStore.java
@@ -0,0 +1,31 @@
+package org.eclipse.jgit.pgm;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jgit.junit.http.AppServer;
+import org.eclipse.jgit.lfs.lib.PlainFSRepository;
+import org.eclipse.jgit.lfs.server.LargeObjectServlet;
+import org.kohsuke.args4j.Argument;
+
+@Command(common = true, usage = "usage_runLfsStore")
+class LfsStore extends TextBuiltin {
+
+	@Argument(required = true, metaVar = "metaVar_directory", usage = "usage_LFSDirectory")
+	String directory;
+
+	protected void run() throws Exception {
+		outw.println("Starting LFS Store in: " + directory);
+		AppServer server = new AppServer();
+		ServletContextHandler app = server.addContext("/lfs");
+		Path dir = Paths.get(directory);
+		PlainFSRepository repository = new PlainFSRepository(dir);
+		LargeObjectServlet servlet = new LargeObjectServlet(repository, 30000);
+		app.addServlet(new ServletHolder(servlet), "/objects/*");
+		server.setUp();
+		outw.println("Running on http://localhost:" + server.getPort()
+				+ "/lfs/objects/*");
+	}
+}
