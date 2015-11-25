f78085b467a6f145299b8d7606e6bfdcbe044f73
Saša Živkov <sasa.zivkov@sap.com>
2015-11-25 15:28:20 +0100

    EDIT: add --port option to the lfs-store command
    Change-Id: I85bdb07ec0e1875b8ccf636f9d50623f0707ad23

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LfsStore.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LfsStore.java
@@ -9,16 +9,20 @@ import org.eclipse.jgit.junit.http.AppServer;
 import org.eclipse.jgit.lfs.lib.PlainFSRepository;
 import org.eclipse.jgit.lfs.server.LargeObjectServlet;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 
 @Command(common = true, usage = "usage_runLfsStore")
 class LfsStore extends TextBuiltin {
+
+	@Option(name = "--port", aliases = { "-p" }, usage = "usage_LFSPort")
+	int port;
 
 	@Argument(required = true, metaVar = "metaVar_directory", usage = "usage_LFSDirectory")
 	String directory;
 
 	protected void run() throws Exception {
 		outw.println("Starting LFS Store in: " + directory);
-		AppServer server = new AppServer();
+		AppServer server = new AppServer(port);
 		ServletContextHandler app = server.addContext("/lfs");
 		Path dir = Paths.get(directory);
 		PlainFSRepository repository = new PlainFSRepository(dir);
