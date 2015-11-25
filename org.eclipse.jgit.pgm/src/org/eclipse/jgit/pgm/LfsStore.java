2e8dfa63b4817499f23777c6e6e94ef54d6e7e8a
Saša Živkov <sasa.zivkov@sap.com>
2015-11-25 17:56:14 +0100

    Cleanup of lfs-store startup
    Rename the --storeUrl to --store-url because this is a more common
    style.
    
    Add a new option --no-run-store to skip starting the store servlet.
    This is useful if we want to run only the protocol servlet and use an
    external object store (like amazon S3).
    
    Change-Id: I4956bd788179e996a2827d5f209c15a53069cdba

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LfsStore.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LfsStore.java
@@ -15,31 +15,65 @@ import org.kohsuke.args4j.Option;
 @Command(common = true, usage = "usage_runLfsStore")
 class LfsStore extends TextBuiltin {
 
+	private static final String OBJECTS = "objects/";
+
+	private static final String STORE_PATH = "/" + OBJECTS + "*";
+
+	private static final String PROTOCOL_PATH = "/info/lfs";
+
 	@Option(name = "--port", aliases = { "-p" }, usage = "usage_LFSPort")
 	int port;
 
-	@Option(name = "--storeUrl", aliases = { "-u" }, usage = "usage_LFSStoreUrl")
+	@Option(name = "--store-url", aliases = { "-u" }, usage = "usage_LFSStoreUrl")
 	String storeUrl;
+
+	@Option(name = "--no-run-store", usage = "usage_LFSRunStore")
+	boolean noRunStore;
 
 	@Argument(required = true, metaVar = "metaVar_directory", usage = "usage_LFSDirectory")
 	String directory;
 
+	String protocolUrl;
+
 	protected void run() throws Exception {
-		outw.println("Starting LFS Store in: " + directory);
 		AppServer server = new AppServer(port);
 		ServletContextHandler app = server.addContext("/");
 		Path dir = Paths.get(directory);
 		PlainFSRepository repository = new PlainFSRepository(dir);
-		LargeObjectServlet content = new LargeObjectServlet(repository, 30000);
-		if (storeUrl == null) {
-			// TODO: get real host name from the OS
-			storeUrl = "http://localhost:" + port + "/";
+
+		if (!noRunStore) {
+			LargeObjectServlet content = new LargeObjectServlet(repository, 30000);
+			app.addServlet(new ServletHolder(content), STORE_PATH);
 		}
-		LfsProtocolServlet protocol = new LfsProtocolServlet(storeUrl);
-		app.addServlet(new ServletHolder(content), "/objects/*");
-		app.addServlet(new ServletHolder(protocol), "/info/lfs");
+
+		LfsProtocolServlet protocol = new LfsProtocolServlet(getStoreUrl());
+		app.addServlet(new ServletHolder(protocol), PROTOCOL_PATH);
+
 		server.setUp();
-		outw.println("Running on http://localhost:" + server.getPort()
-				+ "/lfs/objects/*");
+
+		if (!noRunStore) {
+		  outw.println("LFS objects located in: " + directory);
+		}
+		outw.println("LFS store URL: " + getStoreUrl());
+		outw.println("LFS protocol URL: " + getProtocolUrl());
+	}
+
+	private String getStoreUrl() {
+		if (storeUrl == null) {
+			if (noRunStore) {
+				die("Local store not running and no --store-url specified");
+			} else {
+				// TODO: get real host name from the OS
+				storeUrl = "http://localhost:" + port + "/" + OBJECTS;
+			}
+		}
+		return storeUrl;
+	}
+
+	private String getProtocolUrl() {
+		if (protocolUrl == null) {
+			protocolUrl = "http://localhost:" + port + PROTOCOL_PATH;
+		}
+		return protocolUrl;
 	}
 }
