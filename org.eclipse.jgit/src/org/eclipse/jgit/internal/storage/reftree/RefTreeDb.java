9d063ca7b34c76409b6be4f4379a9cd328aa2f0e
Shawn Pearce <spearce@spearce.org>
2015-11-27 23:21:43 -0800

    RefTreeDb: Ref database using refs/txn/committed
    Instead of storing references in the local filesystem rely on the
    RefTree rooted at refs/txn/committed.  This avoids needing to store
    references in the packed-refs file by keeping all data rooted under
    a single refs/txn/committed ref.
    
    Performance to scan all references from a well packed RefTree is very
    close to reading the packed-refs file from local disk.
    
    Storing a packed RefTree is smaller due to pack file compression,
    about 49.39 bytes/ref (on average) compared to packed-refs using
    ~65.49 bytes/ref.
    
    Change-Id: I75caa631162dc127a780095066195cbacc746d49

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftree/RefTreeDb.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright (C) 2015, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.internal.storage.reftree;
+
+import static org.eclipse.jgit.lib.Ref.Storage.LOOSE;
+import static org.eclipse.jgit.lib.Ref.Storage.PACKED;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.lib.BatchRefUpdate;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectIdRef;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Ref.Storage;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.SymbolicRef;
+import org.eclipse.jgit.revwalk.RevObject;
+import org.eclipse.jgit.revwalk.RevTag;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.util.RefList;
+import org.eclipse.jgit.util.RefMap;
+
+/**
+ * Reference database backed by a {@link RefTree}.
+ *
+ * @since 4.2
+ */
+public class RefTreeDb extends RefDatabase {
+	static final String R_TXN = "refs/txn/"; //$NON-NLS-1$
+	static final String R_TXN_COMMITTED = "refs/txn/committed"; //$NON-NLS-1$
+	static final int MAX_SYMREF_DEPTH = MAX_SYMBOLIC_REF_DEPTH;
+
+	private final Repository repo;
+	private final RefDatabase bootstrap;
+	private volatile Scanner.Result refs;
+
+	/**
+	 * Create a RefTreeDb for a repository.
+	 *
+	 * @param repo
+	 * @param bootstrap
+	 */
+	public RefTreeDb(Repository repo, RefDatabase bootstrap) {
+		this.repo = repo;
+		this.bootstrap = bootstrap;
+	}
+
+	Repository getRepository() {
+		return repo;
+	}
+
+	RefDatabase getBootstrap() {
+		return bootstrap;
+	}
+
+	@Override
+	public void create() throws IOException {
+		bootstrap.create();
+	}
+
+	@Override
+	public void close() {
+		refs = null;
+		bootstrap.close();
+	}
+
+	@Override
+	public Ref getRef(String name) throws IOException {
+		return findRef(getRefs(ALL), name);
+	}
+
+	@Override
+	public Ref exactRef(String name) throws IOException {
+		if (name.startsWith(R_TXN)) {
+			return bootstrap.exactRef(name);
+		}
+
+		boolean partial = false;
+		Ref src = bootstrap.exactRef(R_TXN_COMMITTED);
+		Scanner.Result c = refs;
+		if (c == null || !c.id.equals(idOf(src))) {
+			c = Scanner.scanRefTree(repo, src, prefixOf(name), false);
+			partial = true;
+		}
+
+		Ref r = c.all.get(name);
+		if (r != null && r.isSymbolic()) {
+			r = c.sym.get(name);
+			if (partial && r.getObjectId() == null) {
+				// Attempting exactRef("HEAD") with partial scan will leave
+				// an unresolved symref as its target e.g. refs/heads/master
+				// was not read by the partial scan. Scan everything instead.
+				return getRefs(ALL).get(name);
+			}
+		}
+		return r;
+	}
+
+	private static String prefixOf(String name) {
+		int s = name.lastIndexOf('/');
+		if (s >= 0) {
+			return name.substring(0, s);
+		}
+		return ""; //$NON-NLS-1$
+	}
+
+	@Override
+	public Map<String, Ref> getRefs(String prefix) throws IOException {
+		if (prefix.startsWith(R_TXN)) {
+			return bootstrap.getRefs(prefix);
+		}
+
+		RefList<Ref> txn;
+		Ref src;
+		if (prefix.isEmpty()) {
+			txn = listRefsTxn();
+			src = txn.get(R_TXN_COMMITTED);
+		} else {
+			txn = RefList.emptyList();
+			src = bootstrap.exactRef(R_TXN_COMMITTED);
+		}
+
+		Scanner.Result c = refs;
+		if (c == null || !c.id.equals(idOf(src))) {
+			c = Scanner.scanRefTree(repo, src, prefix, true);
+			if (prefix.isEmpty()) {
+				refs = c;
+			}
+		}
+		return new RefMap(prefix, txn, c.all, c.sym);
+	}
+
+	private static ObjectId idOf(@Nullable Ref src) {
+		return src != null && src.getObjectId() != null
+				? src.getObjectId()
+				: ObjectId.zeroId();
+	}
+
+	private RefList<Ref> listRefsTxn() throws IOException {
+		RefList.Builder<Ref> txn = new RefList.Builder<>();
+		for (Ref r : bootstrap.getRefs(R_TXN).values()) {
+			txn.add(r);
+		}
+		txn.sort();
+		return txn.toRefList();
+	}
+
+	@Override
+	public List<Ref> getAdditionalRefs() throws IOException {
+		return bootstrap.getAdditionalRefs();
+	}
+
+	@Override
+	public Ref peel(Ref ref) throws IOException {
+		Ref i = ref.getLeaf();
+		ObjectId id = i.getObjectId();
+		if (i.isPeeled() || id == null) {
+			return ref;
+		}
+		try (RevWalk rw = new RevWalk(repo)) {
+			RevObject obj = rw.parseAny(id);
+			if (obj instanceof RevTag) {
+				ObjectId p = rw.peel(obj).copy();
+				i = new ObjectIdRef.PeeledTag(PACKED, i.getName(), id, p);
+			} else {
+				i = new ObjectIdRef.PeeledNonTag(PACKED, i.getName(), id);
+			}
+		}
+		return recreate(ref, i);
+	}
+
+	private static Ref recreate(Ref old, Ref leaf) {
+		if (old.isSymbolic()) {
+			Ref dst = recreate(old.getTarget(), leaf);
+			return new SymbolicRef(old.getName(), dst);
+		}
+		return leaf;
+	}
+
+	@Override
+	public void refresh() {
+		bootstrap.refresh();
+	}
+
+	@Override
+	public boolean isNameConflicting(String name) throws IOException {
+		if (name.startsWith(R_TXN)) {
+			return bootstrap.isNameConflicting(name);
+		}
+		return !getConflictingNames(name).isEmpty();
+	}
+
+	@Override
+	public boolean performsAtomicTransactions() {
+		return true;
+	}
+
+	@Override
+	public BatchRefUpdate newBatchUpdate() {
+		return new Batch(this);
+	}
+
+	@Override
+	public RefUpdate newUpdate(String name, boolean detach) throws IOException {
+		if (name.startsWith(R_TXN)) {
+			return bootstrap.newUpdate(name, detach);
+		}
+
+		Ref r = exactRef(name);
+		if (r == null) {
+			r = new ObjectIdRef.Unpeeled(Storage.NEW, name, null);
+		}
+
+		boolean detaching = detach && r.isSymbolic();
+		if (detaching) {
+			r = new ObjectIdRef.Unpeeled(LOOSE, name, r.getObjectId());
+		}
+
+		Update u = new Update(this, r);
+		if (detaching) {
+			u.setDetachingSymbolicRef();
+		}
+		return u;
+	}
+
+	@Override
+	public RefRename newRename(String fromName, String toName)
+			throws IOException {
+		if (fromName.startsWith(R_TXN) && toName.startsWith(R_TXN)) {
+			return bootstrap.newRename(fromName, toName);
+		}
+
+		RefUpdate from = newUpdate(fromName, true);
+		RefUpdate to = newUpdate(toName, true);
+		return new Rename(this, from, to);
+	}
+}
