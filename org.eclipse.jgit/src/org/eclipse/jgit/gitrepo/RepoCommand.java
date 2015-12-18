c4947d21bb14a8a925de70eeba2a5e3515e0892d
Yuxuan 'fishy' Wang <fishywang@google.com>
2015-12-17 18:27:56 -0800

    Skip nested copyfiles in RepoCommand.
    Similar to nested directories, nested copyfiles won't work with git submodule
    either.
    
    Change-Id: Idbe965ec20a682fca0432802858162f8238f05de
    Signed-off-by: Yuxuan 'fishy' Wang <fishywang@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
@@ -51,6 +51,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
