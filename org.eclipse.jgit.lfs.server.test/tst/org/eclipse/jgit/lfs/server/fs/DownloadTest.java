eeeedd3eccc304b2a2a930ef6714a6ac73b388e5
Matthias Sohn <matthias.sohn@sap.com>
2015-12-22 16:11:43 +0100

    Support LFS protocol and a file system based LFS storage
    Implement LfsProtocolServlet handling the "Git LFS v1 Batch API"
    protocol [1]. Add a simple file system based LFS content store and the
    debug-lfs-store command to simplify testing.
    
    Introduce a LargeFileRepository interface to enable additional storage
    implementation while reusing the same protocol implementation.
    
    At the client side we have to configure the lfs.url, specify that
    we use the batch API and we don't use authentication:
    
      [lfs]
    	  url = http://host:port/lfs
    	  batch = true
      [lfs "http://host:port/lfs"]
    	  access = none
    
    the git-lfs client appends the "objects/batch" to the lfs.url.
    
    Hard code an Authorization header in the FileLfsRepository.getAction
    because then git-lfs client will skip asking for credentials. It will
    just forward the Authorization header from the response to the
    download/upload request.
    
    The FileLfsServlet supports file content storage for "Large File
    Storage" (LFS) server as defined by the Github LFS API [2].
    
    - upload and download of large files is probably network bound hence use
      an asynchronous servlet for good scalability
    - simple object storage in file system with 2 level fan-out
    - use LockFile to protect writing large objects against multiple
      concurrent uploads of the same object
    - to prevent corrupt uploads the uploaded file is rejected if its hash
      doesn't match id given in URL
    
    The debug-lfs-store command is used to run the LfsProtocolServlet and,
    optionally, the FileLfsServlet which makes it easier to setup a
    local test server.
    
    [1]
    https://github.com/github/git-lfs/blob/master/docs/api/http-v1-batch.md
    [2] https://github.com/github/git-lfs/tree/master/docs/api
    
    Bug: 472961
    Change-Id: I7378da5575159d2195138d799704880c5c82d5f3
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Sasa Zivkov <sasa.zivkov@sap.com>

---
+++ org.eclipse.jgit.lfs.server.test/tst/org/eclipse/jgit/lfs/server/fs/DownloadTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2015, Matthias Sohn <matthias.sohnk@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.lfs.server.fs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+
+import org.apache.http.client.ClientProtocolException;
+import org.eclipse.jgit.lfs.lib.AnyLongObjectId;
+import org.eclipse.jgit.lfs.test.LongObjectIdTestUtils;
+import org.eclipse.jgit.util.FileUtils;
+import org.junit.Test;
+
+public class DownloadTest extends LfsServerTest {
+
+	@Test
+	public void testDownload() throws Exception {
+		String TEXT = "test";
+		AnyLongObjectId id = putContent(TEXT);
+		Path f = Paths.get(getTempDirectory().toString(), "download");
+		long len = getContent(id, f);
+		assertEquals(TEXT.length(), len);
+		FileUtils.delete(f.toFile(), FileUtils.RETRY);
+	}
+
+	@Test
+	public void testDownloadInvalidPathInfo()
+			throws ClientProtocolException, IOException {
+		String TEXT = "test";
+		AnyLongObjectId id = putContent(TEXT);
+		Path f = Paths.get(getTempDirectory().toString(), "download");
+		try {
+			getContent(id.name().substring(0, 60), f);
+			fail("expected RuntimeException");
+		} catch (RuntimeException e) {
+			assertEquals("Status: 400 Bad Request",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testDownloadInvalidId()
+			throws ClientProtocolException, IOException {
+		String TEXT = "test";
+		AnyLongObjectId id = putContent(TEXT);
+		Path f = Paths.get(getTempDirectory().toString(), "download");
+		try {
+			getContent(id.name().replace('f', 'z'), f);
+			fail("expected RuntimeException");
+		} catch (RuntimeException e) {
+			assertEquals("Status: 400 Bad Request",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testDownloadNotFound()
+			throws ClientProtocolException, IOException {
+		String TEXT = "test";
+		AnyLongObjectId id = LongObjectIdTestUtils.hash(TEXT);
+		Path f = Paths.get(getTempDirectory().toString(), "download");
+		try {
+			getContent(id, f);
+			fail("expected RuntimeException");
+		} catch (RuntimeException e) {
+			assertEquals("Status: 404 Not Found",
+					e.getMessage());
+		}
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public void testLargeFileDownload() throws Exception {
+		Path f = Paths.get(getTempDirectory().toString(), "largeRandomFile");
+		long expectedLen = createPseudoRandomContentFile(f, 5 * MiB);
+		AnyLongObjectId id = putContent(f);
+		Path f2 = Paths.get(getTempDirectory().toString(), "download");
+		long start = System.nanoTime();
+		long len = getContent(id, f2);
+		System.out.println(
+				MessageFormat.format("dowloaded 10 MiB random data in {0}ms",
+						(System.nanoTime() - start) / 1e6));
+		assertEquals(expectedLen, len);
+		FileUtils.delete(f.toFile(), FileUtils.RETRY);
+
+	}
+}
