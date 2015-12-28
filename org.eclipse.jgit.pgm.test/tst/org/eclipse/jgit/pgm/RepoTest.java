fadf465a37c2f5da6c75e396f3eaa5a085fadbd3
Andrey Loskutov <loskutov@gmx.de>
2015-12-28 18:14:05 +0100

    repo command: properly name the required 'path' argument
    Fixes point 4 in bug 484951, where "jgit repo" or "jgit repo -h" dumps a
    stack trace.
    
    Bug: 484951
    Change-Id: Ic8b362e07a40ad923dc9acde0c0983a1e7932a02
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/RepoTest.java
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/RepoTest.java
@@ -42,6 +42,7 @@
  */
 package org.eclipse.jgit.pgm;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -95,6 +96,12 @@ public class RepoTest extends CLIRepositoryTestCase {
 		git.commit().setMessage("Initial commit").call();
 
 		resolveRelativeUris();
+	}
+
+	@Test
+	public void testMissingPath() throws Exception {
+		assertEquals("fatal: Argument \"path\" is required",
+				execute("git repo")[0]);
 	}
 
 	@Test
