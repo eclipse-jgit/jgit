aabbc58341c2381a2c5907b82c50691a296ae6c9
Andrey Loskutov <loskutov@gmx.de>
2015-12-29 14:27:37 +0100

    Sort "eager" path-like options to the end of the help
    The "--" path option (and all other similar options consuming all
    remaining arguments) should be placed at the end of the command line
    help.
    
    Currently jgit reset -h shows this:
    
    jgit reset [commit-ish] [path ... ...] [-- path ... ...] [--hard]
    [--help (-h)] [--mixed] [--soft]
    
    After the patch the help shows this:
    
    jgit reset [commit-ish] [path ... ...] [--hard] [--help (-h)] [--mixed]
    [--soft] [-- path ... ...]
    
    Bug: 484951
    Change-Id: I3db332bf293ca8d6bfaab0d546cd35af689bd46e
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ResetTest.java
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ResetTest.java
@@ -63,6 +63,13 @@ public class ResetTest extends CLIRepositoryTestCase {
 	}
 
 	@Test
+	public void testPathOptionHelp() throws Exception {
+		String[] result = execute("git reset -h");
+		assertTrue("Unexpected argument: " + result[1],
+				result[1].endsWith("[-- path ... ...]"));
+	}
+
+	@Test
 	public void testZombieArgument_Bug484951() throws Exception {
 		String[] result = execute("git reset -h");
 		assertFalse("Unexpected argument: " + result[0],
