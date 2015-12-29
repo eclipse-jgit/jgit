159990ee281608bad750d1721069540f5fa26051
Andrey Loskutov <loskutov@gmx.de>
2015-12-29 13:58:58 +0100

    commit command: allow to specify path(s) argument(s)
    This fixes the command below:
    
    jgit commit a -m "added file a"
    
    which currently fails with:
    
    org.eclipse.jgit.api.errors.JGitInternalException: The combination of
    arguments --all and --only is not allowed
    
    Bug: 484973
    Change-Id: I37a4ccd68101a66520ef99110f7aa0cbdcc8beba
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
@@ -681,7 +681,7 @@ public class CommitCommand extends GitCommand<RevCommit> {
 	 */
 	public CommitCommand setAll(boolean all) {
 		checkCallable();
-		if (!only.isEmpty())
+		if (all && !only.isEmpty())
 			throw new JGitInternalException(MessageFormat.format(
 					JGitText.get().illegalCombinationOfArguments, "--all", //$NON-NLS-1$
 					"--only")); //$NON-NLS-1$
