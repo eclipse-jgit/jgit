4128b59d4a8cbe97d7c1323dfe106b0e5d915486
Andrey Loskutov <loskutov@gmx.de>
2016-01-03 16:15:34 +0100

    branch command: print help if requested, even if arguments are wrong
    git branch -d -h reports an error (because of missing -d option value)
    but does not print the help as expected.
    
    To fix this, CmdLineParser must catch, print but do not propagate
    exceptions if help is requested.
    
    Bug: 484951
    Change-Id: I51265ebe295f22da540792c6a1980b8bdb295a02
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/opt/CmdLineParser.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/opt/CmdLineParser.java
@@ -43,6 +43,7 @@
 
 package org.eclipse.jgit.pgm.opt;
 
+import java.io.IOException;
 import java.io.Writer;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -53,6 +54,7 @@ import java.util.ResourceBundle;
 
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.pgm.Die;
 import org.eclipse.jgit.pgm.TextBuiltin;
 import org.eclipse.jgit.pgm.internal.CLIText;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -95,6 +97,8 @@ public class CmdLineParser extends org.kohsuke.args4j.CmdLineParser {
 
 	private boolean seenHelp;
 
+	private TextBuiltin cmd;
+
 	/**
 	 * Creates a new command line owner that parses arguments/options and set
 	 * them into the given object.
@@ -126,8 +130,12 @@ public class CmdLineParser extends org.kohsuke.args4j.CmdLineParser {
 	 */
 	public CmdLineParser(final Object bean, Repository repo) {
 		super(bean);
-		if (repo == null && bean instanceof TextBuiltin)
-			repo = ((TextBuiltin) bean).getRepository();
+		if (bean instanceof TextBuiltin) {
+			cmd = (TextBuiltin) bean;
+		}
+		if (repo == null && cmd != null) {
+			repo = cmd.getRepository();
+		}
 		this.db = repo;
 	}
 
@@ -167,12 +175,29 @@ public class CmdLineParser extends org.kohsuke.args4j.CmdLineParser {
 
 		try {
 			super.parseArgument(tmp.toArray(new String[tmp.size()]));
+		} catch (Die e) {
+			if (!seenHelp) {
+				throw e;
+			}
+			printToErrorWriter(CLIText.fatalError(e.getMessage()));
 		} finally {
 			// reset "required" options to defaults for correct command printout
 			if (backup != null && !backup.isEmpty()) {
 				restoreRequiredOptions(backup);
 			}
 			seenHelp = false;
+		}
+	}
+
+	private void printToErrorWriter(String error) {
+		if (cmd == null) {
+			System.err.println(error);
+		} else {
+			try {
+				cmd.getErrorWriter().println(error);
+			} catch (IOException e1) {
+				System.err.println(error);
+			}
 		}
 	}
 
