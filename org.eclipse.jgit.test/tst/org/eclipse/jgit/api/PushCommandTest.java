dcc858fdf0035d4aa7f911a600f320a395beb30c
Matt Coleman <matt.coleman01234@gmail.com>
2016-01-11 16:44:44 -0800

    Bug 485572: Allow tags to end with @
    But do not allow '@' to be the only character.
    According to the CGit man page for git-check-ref-format
    (https://www.kernel.org/pub/software/scm/git/docs/git-check-ref-format.html),
    references cannot be the single character '@'.
    
    Change-Id: I5eafe9b9dba00a2d06c9f18904b9b609ed40342f
    Signed-off-by: Matt Coleman <matt.coleman01234@gmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/PushCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/PushCommandTest.java
@@ -382,4 +382,42 @@ public class PushCommandTest extends RepositoryTestCase {
 		assertEquals(commit3.getId(),
 				db2.resolve(commit3.getId().getName() + "^{commit}"));
 	}
+
+	@Test
+	public void testPushTagEndingWithAtSymbol() throws JGitInternalException, IOException,
+			GitAPIException, URISyntaxException {
+
+		// create other repository
+		Repository db2 = createWorkRepository();
+
+		// setup the first repository
+		final StoredConfig config = db.getConfig();
+		RemoteConfig remoteConfig = new RemoteConfig(config, "test");
+		URIish uri = new URIish(db2.getDirectory().toURI().toURL());
+		remoteConfig.addURI(uri);
+		remoteConfig.update(config);
+		config.save();
+
+		Git git1 = new Git(db);
+		// create some refs via commits and a tag ending in an '@' symbol
+		RevCommit commit = git1.commit().setMessage("initial commit").call();
+		Ref tagRef = git1.tag().setName("tag@").call();
+
+		try {
+			db2.resolve(commit.getId().getName() + "^{commit}");
+			fail("id shouldn't exist yet");
+		} catch (MissingObjectException e) {
+			// we should get here
+		}
+
+		RefSpec spec = new RefSpec("refs/heads/master:refs/heads/x");
+		git1.push().setRemote("test").setRefSpecs(spec)
+				.setPushTags()
+				.call();
+
+		assertEquals(commit.getId(),
+				db2.resolve(commit.getId().getName() + "^{commit}"));
+		assertEquals(tagRef.getObjectId(),
+				db2.resolve(tagRef.getObjectId().getName()));
+	}
 }
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -626,6 +626,8 @@ public abstract class Repository implements AutoCloseable {
 			case '@':
 				if (rev != null)
 					throw new RevisionSyntaxException(revstr);
+				if (i == revChars.length - 1 && i != 0)
+					continue;
 				if (i + 1 < revChars.length && revChars[i + 1] != '{')
 					continue;
 				int m;
