3c4e7c08a70ce72cf67bec5af689feadba3c3c61
David Pursehouse <david.pursehouse@sonymobile.com>
2016-02-05 17:26:03 +0900

    RevCommitListTest: Open Git and RevWalk in try-with-resource
    Change-Id: I4ba273a364a12f82a3b3b06ba050bc633411daf6
    Signed-off-by: David Pursehouse <david.pursehouse@sonymobile.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevCommitListTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevCommitListTest.java
@@ -57,14 +57,15 @@ public class RevCommitListTest extends RepositoryTestCase {
 	private RevCommitList<RevCommit> list;
 
 	public void setup(int count) throws Exception {
-		Git git = new Git(db);
-		for (int i = 0; i < count; i++)
-			git.commit().setCommitter(committer).setAuthor(author)
-					.setMessage("commit " + i).call();
-		list = new RevCommitList<RevCommit>();
-		RevWalk w = new RevWalk(db);
-		w.markStart(w.lookupCommit(db.resolve(Constants.HEAD)));
-		list.source(w);
+		try (Git git = new Git(db);
+				RevWalk w = new RevWalk(db);) {
+			for (int i = 0; i < count; i++)
+				git.commit().setCommitter(committer).setAuthor(author)
+						.setMessage("commit " + i).call();
+			list = new RevCommitList<RevCommit>();
+			w.markStart(w.lookupCommit(db.resolve(Constants.HEAD)));
+			list.source(w);
+		}
 	}
 
 	@Test
@@ -107,17 +108,18 @@ public class RevCommitListTest extends RepositoryTestCase {
 	public void testFillToCommit() throws Exception {
 		setup(3);
 
-		RevWalk w = new RevWalk(db);
-		w.markStart(w.lookupCommit(db.resolve(Constants.HEAD)));
+		try (RevWalk w = new RevWalk(db)) {
+			w.markStart(w.lookupCommit(db.resolve(Constants.HEAD)));
 
-		w.next();
-		RevCommit c = w.next();
-		assertNotNull("should have found 2. commit", c);
+			w.next();
+			RevCommit c = w.next();
+			assertNotNull("should have found 2. commit", c);
 
-		list.fillTo(c, 5);
-		assertEquals(2, list.size());
-		assertEquals("commit 1", list.get(1).getFullMessage());
-		assertNull(list.get(3));
+			list.fillTo(c, 5);
+			assertEquals(2, list.size());
+			assertEquals("commit 1", list.get(1).getFullMessage());
+			assertNull(list.get(3));
+		}
 	}
 
 	@Test
