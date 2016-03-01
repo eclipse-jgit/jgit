54418bd60d25c586c4b148b21fd10d12ecdec64e
Preben Ingvaldsen <preben@puppetlabs.com>
2016-03-01 11:37:54 -0800

    WIP: add initial gitlink tests
    This is an initial implementation of some tests surrounding
    gitlinks that will inform the implementation of the
    DIR_NO_GITLINKS setting
    
    Change-Id: Icd0a16e21aa2242fa21f4fea32babcb673bb9f93
    Signed-off-by: Preben Ingvaldsen <preben@puppetlabs.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeOptions.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeOptions.java
@@ -70,6 +70,8 @@ public class WorkingTreeOptions {
 
 	private final HideDotFiles hideDotFiles;
 
+    private final boolean dirNoGitLinks;
+
 	private WorkingTreeOptions(final Config rc) {
 		fileMode = rc.getBoolean(ConfigConstants.CONFIG_CORE_SECTION,
 				ConfigConstants.CONFIG_KEY_FILEMODE, true);
@@ -82,6 +84,9 @@ public class WorkingTreeOptions {
 		hideDotFiles = rc.getEnum(ConfigConstants.CONFIG_CORE_SECTION, null,
 				ConfigConstants.CONFIG_KEY_HIDEDOTFILES,
 				HideDotFiles.DOTGITONLY);
+        dirNoGitLinks = rc.getBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+                ConfigConstants.CONFIG_KEY_DIRNOGITLINKS,
+                false);
 	}
 
 	/** @return true if the execute bit on working files should be trusted. */
@@ -117,4 +122,6 @@ public class WorkingTreeOptions {
 	public HideDotFiles getHideDotFiles() {
 		return hideDotFiles;
 	}
+
+    public boolean isDirNoGitLinks() { return dirNoGitLinks; }
 }
