011e5da3e0727d9a2cdfb3261ef78e5067380eab
Philipp Marx <smigfu@googlemail.com>
2016-03-07 22:55:46 +0100

    Change visibility of DirCacheTree#getObjectId to public
    Allow access to the ObjectId of a DirCacheTree if known for low level
    integration code.
    
    Change-Id: I6f05b10c9ac781f5e8b38af4a19e653313c91fa8
    Signed-off-by: Philipp Marx <smigfu@googlemail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheTree.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheTree.java
@@ -249,7 +249,15 @@ public class DirCacheTree {
 		return children[i];
 	}
 
-	ObjectId getObjectId() {
+	/**
+	 * Get the tree's ObjectId.
+	 * <p>
+	 * If {@link #isValid()} returns false this method will return null.
+	 *
+	 * @return ObjectId of this tree or null.
+	 * @since 4.3
+	 */
+	public ObjectId getObjectId() {
 		return id;
 	}
 
