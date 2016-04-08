36a53d1a3cdd66c40b6db241a75a28293f22f5e1
Christian Halstrick <christian.halstrick@sap.com>
2016-04-08 16:04:17 +0200

    Fix CommitCommand to be able to skip writing to RefLog
    CommitCommand already provided a method to set the comment which should
    be written into the reflog. The underlying RefUpdate class supported to
    skip writing a reflog entry. But through the CommitCommand API it was
    not possible to prevent writing a reflog entry. Fix this and allow
    creating commits which don't occur in the reflog.
    
    Change-Id: I193c53de71fb5958ea749c4bfa8360a51acc9b58

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CommitCommand.java
@@ -124,6 +124,8 @@ public class CommitCommand extends GitCommand<RevCommit> {
 
 	private String reflogComment;
 
+	private boolean useDefaultReflogMessage = true;
+
 	/**
 	 * Setting this option bypasses the pre-commit and commit-msg hooks.
 	 */
@@ -258,7 +260,7 @@ public class CommitCommand extends GitCommand<RevCommit> {
 				RevCommit revCommit = rw.parseCommit(commitId);
 				RefUpdate ru = repo.updateRef(Constants.HEAD);
 				ru.setNewObjectId(commitId);
-				if (reflogComment != null) {
+				if (!useDefaultReflogMessage) {
 					ru.setRefLogMessage(reflogComment, false);
 				} else {
 					String prefix = amend ? "commit (amend): " //$NON-NLS-1$
@@ -790,10 +792,13 @@ public class CommitCommand extends GitCommand<RevCommit> {
 	 * Override the message written to the reflog
 	 *
 	 * @param reflogComment
+	 *            the comment to be written into the reflog or <code>null</code>
+	 *            to specify that no reflog should be written
 	 * @return {@code this}
 	 */
 	public CommitCommand setReflogComment(String reflogComment) {
 		this.reflogComment = reflogComment;
+		useDefaultReflogMessage = false;
 		return this;
 	}
 
