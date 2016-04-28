e71fb5312fc560808b26c83a78114b1a75d312d8
Stefan Beller <sbeller@google.com>
2016-04-28 15:52:45 -0700

    RepoCommand: record manifest groups as submodule labels
    Git submodule commands will soon have the ability to make a finer
    selection on which submodules to operate on via labels to each submodule.
    
    The RepoCommand which translates repo manifest files into submodules
    should propagate this information along.
    
    Change-Id: I0f55b45786b6b8fc3d5be62d7f6aab9ac00ed60e
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
@@ -43,6 +43,7 @@
 package org.eclipse.jgit.gitrepo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -747,6 +748,53 @@ public class RepoCommandTest extends RepositoryTestCase {
 						c.getString("submodule", "with-branch", "branch"));
 				assertEquals("long branches work", "refs/heads/master",
 						c.getString("submodule", "with-long-branch", "branch"));
+			}
+		}
+	}
+
+
+	@Test
+	public void testRecordSubmoduleLabels() throws Exception {
+		try (
+				Repository remoteDb = createBareRepository();
+				Repository tempDb = createWorkRepository()) {
+			StringBuilder xmlContent = new StringBuilder();
+			xmlContent
+				.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+				.append("<manifest>")
+				.append("<remote name=\"remote1\" fetch=\".\" />")
+				.append("<default revision=\"master\" remote=\"remote1\" />")
+				.append("<project path=\"test\" ")
+					.append("revision=\"master\" ")
+					.append("name=\"").append(notDefaultUri).append("\" ")
+					.append("groups=\"a1,a2\" />")
+				.append("</manifest>");
+			JGitTestUtil.writeTrashFile(tempDb, "manifest.xml",
+				xmlContent.toString());
+
+			RepoCommand command = new RepoCommand(remoteDb);
+			command.setPath(tempDb.getWorkTree().getAbsolutePath() + "/manifest.xml")
+				.setURI(rootUri)
+				.setRecordSubmoduleLabels(true)
+				.call();
+			// Clone it
+			File directory = createTempDirectory("testBareRepo");
+			try (Repository localDb = Git.cloneRepository()
+					.setDirectory(directory)
+					.setURI(remoteDb.getDirectory().toURI().toString()).call()
+					.getRepository();) {
+				// The .gitmodules file should exist
+				File gitmodules = new File(localDb.getWorkTree(),
+						".gitmodules");
+				assertTrue("The .gitmodules file should exist",
+						gitmodules.exists());
+				FileBasedConfig c = new FileBasedConfig(gitmodules,
+						FS.DETECTED);
+				c.load();
+
+				String[] expect = new String[] {"a1", "a2"};
+				assertArrayEquals("recording labels works", expect,
+						c.getStringList("submodule", "test", "label"));
 			}
 		}
 	}
