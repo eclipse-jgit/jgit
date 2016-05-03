06c3ce02f5d2dafbc40ac249dafa536749005da0
Christian Halstrick <christian.halstrick@sap.com>
2016-05-03 13:41:39 +0200

    Add configuration parameter switch to builtin hooks/filters
    If the configuration parameter filter.<filterDriverName>.useJGitBuiltin
    is set to true then for all corresponding filters JGit will try to
    execute the builtin filter instead of the filter-command which is
    defined in git configuration. If set to false JGit will not try to
    execute builtin filters for the specified filter driver.
    
    Example: The repo config contains the following lines
    
      [filter "lfs"]
        clean = git-lfs clean -- %f
        smudge = git-lfs smudge -- %f
        useJGitBuiltin = true
    
    Addtionally the .gitattributes file in the root of the working tree
    contains:
    
        *.bin filter=lfs
    
    In this case when new content is added similar to "git add 1.bin" then
    the following will happen:
    
      - jgit will check whether a builtin command factory was registered
        for the command "jgit://builtin/lfs/clean". If that is true the
        factory is used to create a builtin command and that command is
        used to filter the content
      - Otherwise jgit will call the external program "git lfs clean ..."
        to do the filtering
    
    Change-Id: Idadb1db06b1e89e7031d7ed6319904973c367d38

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/TreeWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/TreeWalk.java
@@ -313,6 +313,8 @@ public class TreeWalk implements AutoCloseable, AttributesProvider {
 
 	private Config config;
 
+	private Set<String> registeredCommands;
+
 	/**
 	 * Create a new tree walker for a given repository.
 	 *
@@ -357,6 +359,7 @@ public class TreeWalk implements AutoCloseable, AttributesProvider {
 		if (repo != null) {
 			config = repo.getConfig();
 			attributesNodeProvider = repo.createAttributesNodeProvider();
+			registeredCommands = Repository.listRegisteredCommands();
 		} else {
 			config = null;
 			attributesNodeProvider = null;
@@ -1369,8 +1372,20 @@ public class TreeWalk implements AutoCloseable, AttributesProvider {
 			return filterCommand;
 		filterCommand = config.getString(Constants.ATTR_FILTER,
 				filterDriverName, filterCommandType);
-		if (filterCommand != null)
+		boolean useBuiltin = config.getBoolean(Constants.ATTR_FILTER,
+				filterDriverName, Constants.ATTR_FILTER_USE_BUILTIN, false);
+		if (useBuiltin) {
+			String builtinFilterCommand = Constants.BUILTIN_FILTER_PREFIX
+					+ filterDriverName
+					+ "/" + filterCommandType;
+			if (registeredCommands != null
+					&& registeredCommands.contains(builtinFilterCommand)) {
+				filterCommand = builtinFilterCommand;
+			}
+		}
+		if (filterCommand != null) {
 			filterCommandsByNameDotType.put(key, filterCommand);
+		}
 		return filterCommand;
 	}
 }
