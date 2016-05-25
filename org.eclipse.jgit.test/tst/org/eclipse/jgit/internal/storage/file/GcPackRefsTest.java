ab2ac82e2eb8548ebb73df4c336e3a9ae3b848a2
Marco Miller <marco.miller@ericsson.com>
2016-05-25 16:45:13 -0400

    RefDirectory: remove ref lock file for following ref dir removal
    Before this fix, ref directory removal did not work. That was because
    the ref lock file was still in the leaf directory at deletion time.
    Hence no deep ref directories were ever deleted, which negatively
    impacted performance under large directory structure circumstances.
    
    This fix removes the ref lock file before attempting to delete the ref
    directory (which includes it); applies to the leaf directory solely.
    The other deep parent directories are therefore now successfully
    deleted in turn, since leaf's content (lock file) gets removed first.
    
    The ref lock file is still also removed in the calling block's finally
    clause, just in case, as before. Such double-unlock brought by this
    fix is harmless (a no-op).
    
    A new (private) RefDirectory#delete method is introduced to support
    this gc-specific case; other RefDirectory#delete callers remain
    untouched.
    
    Change-Id: I47ba1eeb9bcf0cb93d2ed105d84fea2dac756a5a
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcPackRefsTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcPackRefsTest.java
@@ -45,11 +45,14 @@ package org.eclipse.jgit.internal.storage.file;
 
 import static java.lang.Integer.valueOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
@@ -78,6 +81,17 @@ public class GcPackRefsTest extends GcTestCase {
 
 		gc.packRefs();
 		assertSame(repo.exactRef("refs/tags/t").getStorage(), Storage.PACKED);
+	}
+
+	@Test
+	public void emptyRefDirectoryDeleted() throws Exception {
+		String ref = "dir/ref";
+		tr.branch(ref).commit().create();
+		String name = repo.findRef(ref).getName();
+		Path dir = repo.getDirectory().toPath().resolve(name).getParent();
+
+		gc.packRefs();
+		assertFalse(Files.exists(dir));
 	}
 
 	@Test
