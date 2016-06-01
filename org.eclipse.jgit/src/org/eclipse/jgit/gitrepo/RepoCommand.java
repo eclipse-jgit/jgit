51556c6efb1c0d69609cce38fc43042b59b47abb
Matthias Sohn <matthias.sohn@sap.com>
2016-06-01 10:16:00 +0200

    Fix javadoc errors and unused imports introduced by ddd0fe25
    Change-Id: I2d17115843e453c71febd4a58527effe658b48fd
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
@@ -51,7 +51,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
