9daae8118940f2505818b1b31d093bf17c064c21
Dan Wang <dwwang@google.com>
2016-06-03 16:39:45 -0700

    Preliminary implementation of option strings
    Example usage:
    $ ./jgit push --push-options "Reviewer=j.doe@example.org" --push-options "<arbitrary string>" origin HEAD:refs/for/master
    
    Change-Id: I6797e50681054dce3bd179e80b731aef5e200d77
    Signed-off-by: Dan Wang <dwwang@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PushProcess.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PushProcess.java
@@ -49,6 +49,7 @@ import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jgit.errors.MissingObjectException;
@@ -87,6 +88,9 @@ class PushProcess {
 	/** an outputstream to write messages to */
 	private final OutputStream out;
 
+	/** A list of option strings associated with this push */
+	private List<String> pushOptions;
+
 	/**
 	 * Create process for specified transport and refs updates specification.
 	 *
@@ -122,6 +126,7 @@ class PushProcess {
 		this.transport = transport;
 		this.toPush = new HashMap<String, RemoteRefUpdate>();
 		this.out = out;
+		this.pushOptions = transport.getPushOptions();
 		for (final RemoteRefUpdate rru : toPush) {
 			if (this.toPush.put(rru.getRemoteName(), rru) != null)
 				throw new TransportException(MessageFormat.format(
@@ -150,7 +155,6 @@ class PushProcess {
 		try {
 			monitor.beginTask(PROGRESS_OPENING_CONNECTION,
 					ProgressMonitor.UNKNOWN);
-
 			final PushResult res = new PushResult();
 			connection = transport.openPush();
 			try {
@@ -293,5 +297,14 @@ class PushProcess {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Gets the list of option strings associated with this push.
+	 *
+	 * @return pushOptions
+	 */
+	public List<String> getPushOptions() {
+		return pushOptions;
 	}
 }
