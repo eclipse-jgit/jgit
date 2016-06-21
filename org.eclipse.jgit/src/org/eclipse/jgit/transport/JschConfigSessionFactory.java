a638c64851093161dbe4f2bfed220270417b462f
markdingram <markdingram@gmail.com>
2016-06-21 17:31:20 +0100

    Add configureJSch method to allow configuration of JSch objects
    Example use case is to set a different IdentityRepository, for example,
    a RemoteIdentityRepository to allow SSH Agent supplied credentials.
    
    Change-Id: Id4a4afd64464e452ffe6d6ad49036f9e283b4655
    Signed-off-by: markdingram <markdingram@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/JschConfigSessionFactory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/JschConfigSessionFactory.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016, Mark Ingram <markdingram@gmail.com>
  * Copyright (C) 2009, Constantine Plotnikov <constantine.plotnikov@gmail.com>
  * Copyright (C) 2008-2009, Google Inc.
  * Copyright (C) 2009, Google, Inc.
@@ -220,6 +221,17 @@ public abstract class JschConfigSessionFactory extends SshSessionFactory {
 	}
 
 	/**
+	 * Provide additional configuration for the JSch instance.
+	 * This method could be used to supply a preferred IdentityRepository.
+	 *
+	 * @param jsch
+	 *              jsch instance
+	*/
+	protected void configureJSch(JSch jsch) {
+		// No additional configuration required.
+	}
+
+	/**
 	 * Provide additional configuration for the session based on the host
 	 * information. This method could be used to supply {@link UserInfo}.
 	 *
@@ -257,6 +269,7 @@ public abstract class JschConfigSessionFactory extends SshSessionFactory {
 		JSch jsch = byIdentityFile.get(identityKey);
 		if (jsch == null) {
 			jsch = new JSch();
+			configureJSch(jsch);
 			jsch.setHostKeyRepository(defaultJSch.getHostKeyRepository());
 			jsch.addIdentity(identityKey);
 			byIdentityFile.put(identityKey, jsch);
@@ -274,6 +287,7 @@ public abstract class JschConfigSessionFactory extends SshSessionFactory {
 	 */
 	protected JSch createDefaultJSch(FS fs) throws JSchException {
 		final JSch jsch = new JSch();
+		configureJSch(jsch);
 		knownHosts(jsch, fs);
 		identities(jsch, fs);
 		return jsch;
