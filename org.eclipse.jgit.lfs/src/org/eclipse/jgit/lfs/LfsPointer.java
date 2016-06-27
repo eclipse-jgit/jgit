95d167a260d46048744ec70b17c710f406765512
Christian Halstrick <christian.halstrick@sap.com>
2016-06-27 16:00:44 +0200

    Add builtin LFS smudge filter for local case
    Adds a JGit builtin implementation of the "git lfs smudge" filter. This
    filter should do the same as the one described in [1] besides that it
    only supports the local case when the lfs objects are already present in
    the media directory. Remote cases where download of LFS objects from an
    LFS server is needed will be done in a later commit.
    
    [1]
    https://github.com/github/git-lfs/blob/master/docs/man/git-lfs-smudge.1.ronn
    Change-Id: I8ff661d4edd3667ef7f86f3b4fa33e568eb4c8f4

--- org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/LfsPointer.java
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/LfsPointer.java
@@ -42,10 +42,15 @@
  */
 package org.eclipse.jgit.lfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
 import org.eclipse.jgit.lfs.lib.Constants;
+import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.lfs.lib.LongObjectId;
 
 /**
@@ -111,5 +116,45 @@ public class LfsPointer {
 			ps.print("size "); //$NON-NLS-1$
 			ps.println(size);
 		}
+	}
+
+	/**
+	 * Try to parse the data provided by an InputStream to the format defined by
+	 * {@link #VERSION}
+	 *
+	 * @param in
+	 *            the {@link InputStream} from where to read the data
+	 * @return a {@link LfsPointer} or <code>null</code> if the stream was not
+	 *         parseable as LFSPointer
+	 * @throws IOException
+	 */
+	@Nullable
+	public static LfsPointer parseLfsPointer(InputStream in)
+			throws IOException {
+		boolean versionLine = false;
+		LongObjectId id = null;
+		long si = -1;
+
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(in))) {
+			for (String s = br.readLine(); s != null; s = br.readLine()) {
+				if (s.startsWith("#") || s.length() == 0) { //$NON-NLS-1$
+					continue;
+				} else if (s.startsWith("version") && s.length() > 8 //$NON-NLS-1$
+						&& s.substring(8).trim().equals(VERSION)) {
+					versionLine = true;
+				} else if (s.startsWith("oid sha256:")) {
+					id = LongObjectId.fromString(s.substring(11).trim());
+				} else if (s.startsWith("size") && s.length() > 5) { //$NON-NLS-1$
+					si = Long.parseLong(s.substring(5).trim());
+				} else {
+					return null;
+				}
+			}
+			if (versionLine && id != null && si > -1) {
+				return new LfsPointer(id, si);
+			}
+		}
+		return null;
 	}
 }
