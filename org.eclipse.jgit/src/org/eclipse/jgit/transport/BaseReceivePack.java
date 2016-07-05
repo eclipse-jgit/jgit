7f2fb42c9a0c6b9b2ca5c42b80dd638bac9e2f76
Shawn Pearce <spearce@spearce.org>
2016-07-05 13:00:02 -0700

    ReceivePack: catch InvalidObjectIdException while parsing commands
    The "shallow $id" parsing can also throw InvalidObjectIdException,
    just like the parseCommand() block.  Combine these together into the
    outer level catch to avoid code duplication.
    
    With the try-catch removed the parseCommand() can be shortened,
    and a small formatting error can be corrected here too.
    
    Change-Id: I6839b95f0bc4fbf4d2c213331ebb9bd24ab2af65

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/BaseReceivePack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/BaseReceivePack.java
@@ -1127,27 +1127,20 @@ public abstract class BaseReceivePack {
 				}
 			}
 			pushCert = certParser.build();
-		} catch (PackProtocolException e) {
+		} catch (PackProtocolException | InvalidObjectIdException e) {
 			sendError(e.getMessage());
 			throw e;
 		}
 	}
 
 	static ReceiveCommand parseCommand(String line) throws PackProtocolException {
-          if (line == null || line.length() < 83) {
+		if (line == null || line.length() < 83) {
 			throw new PackProtocolException(
 					JGitText.get().errorInvalidProtocolWantedOldNewRef);
 		}
-		String oldStr = line.substring(0, 40);
-		String newStr = line.substring(41, 81);
-		ObjectId oldId, newId;
-		try {
-			oldId = ObjectId.fromString(oldStr);
-			newId = ObjectId.fromString(newStr);
-		} catch (InvalidObjectIdException e) {
-			throw new PackProtocolException(
-					JGitText.get().errorInvalidProtocolWantedOldNewRef, e);
-		}
+
+		ObjectId oldId = ObjectId.fromString(line.substring(0, 40));
+		ObjectId newId = ObjectId.fromString(line.substring(41, 81));
 		String name = line.substring(82);
 		if (!Repository.isValidRefName(name)) {
 			throw new PackProtocolException(
