b74f55d8bf8157a967b6399dcd36d5e6cb2ea45e
David Pursehouse <david.pursehouse@gmail.com>
2016-07-21 18:45:27 +0900

    Annotate Sets#of with @SafeVarArgs to prevent heap pollution warning
    This prevents the warning:
    
      Potential heap pollution via varargs parameter
    
    The method doesn't do any casting of types that would cause the heap
    pollution, so it should be safe to add @SafeVarArgs.
    
    See [1] for information about this warning.
    
    [1] http://stackoverflow.com/a/12462259/381622
    
    Change-Id: Ic6d252915ea44b4f1c385afecb98906cd2c54382
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.test/src/org/eclipse/jgit/lib/Sets.java
+++ org.eclipse.jgit.test/src/org/eclipse/jgit/lib/Sets.java
@@ -47,6 +47,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Sets {
+	@SafeVarargs
 	public static <T> Set<T> of(T... elements) {
 		Set<T> ret = new HashSet<T>();
 		for (T element : elements)
