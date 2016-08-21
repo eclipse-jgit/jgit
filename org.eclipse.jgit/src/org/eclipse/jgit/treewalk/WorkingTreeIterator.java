991ab48067c72c3a1df6cb2ddd71eea8cb01ae45
Masaya Suzuki <masayasuzuki@google.com>
2016-08-21 15:27:30 -0700

    Use FS#lastModified instead of File#lastModified
    Bug: 500058. This fixes the tests failed in JDK8.
    
    FS uses java.nio API to get file attributes. The timestamps obtained
    from that API are more precise than the ones from
    java.io.File#lastModified() since Java8.
    
    This difference accidentally makes JGit detect newly added files as
    smudged. Use the precised timestamp to avoid this false positive.
    
    WorkingTreeIterator needs a temporary hack because of this timestamp
    preciseness. It reuses idBuffer in DirCacheEntry if the metadata is
    matched. The problem here is that the metadata comparison of GIT_LINK
    (submodules) is incorrect in many cases, and therefore the idBuffer
    reuse is not valid. FileTreeIterator uses file timestamps as
    lastModified metadata. For submodules, the timestamp of the submodule
    directory is meaningless. It won't indicate the changes in submodule
    repository refs. This was caught by
    CommitCommandTest#commitSubmoduleUpdate when FileSnapshot started using
    the precise timestamp. The reason that this was not problem before was
    that the index entries were almost always detected as smudged during the
    test, and WorkingTreeIterator didn't reuse the idBuffer.
    
    Temporarily disable the idBuffer reuse for submodules.
    
    Change-Id: I9e587583c85cb6efa7562ad6c5f26577869a2e7c
    Signed-off-by: Masaya Suzuki <masayasuzuki@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
@@ -259,7 +259,12 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 		if (contentIdFromPtr == ptr)
 			return contentId;
 
-		if (state.walk != null) {
+		// The metadata comparison of submodules are not reliable because of the
+		// last modified timestamp. The submodule timestamp is the timestamp of
+		// submodule directory, not the timestamp of submodule index. Even if the
+		// submodule has changed the index, it's not reflected in the metadata.
+		if (state.walk != null
+				&& (mode & FileMode.TYPE_MASK) != FileMode.TYPE_GITLINK) {
 			// If there is a matching DirCacheIterator, we can reuse
 			// its idBuffer, but only if we appear to be clean against
 			// the cached index information for the path.
