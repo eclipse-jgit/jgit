dd66a061bb1d79506009f2ed29a4ba8c40745a45
David Pursehouse <david.pursehouse@gmail.com>
2016-09-21 09:07:24 +0200

    Suppress deprecation warnings related to UploadPackLogger
    UploadPackLogger is deprecated but will not be removed until
    JGit version 5.0.
    
    Suppress the unavoidable deprecation warnings on usages of the
    interface that are kept for backwards compatibility.
    
    Add a TODO so that we don't forget to remove it in 5.0.
    
    Change-Id: Id248002b9bdf23db192427196d54c722a012106c
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPackLogger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPackLogger.java
@@ -56,7 +56,7 @@ import org.eclipse.jgit.internal.storage.pack.PackWriter;
  * @deprecated use {@link PostUploadHook} instead
  */
 @Deprecated
-public interface UploadPackLogger {
+public interface UploadPackLogger { // TODO remove in JGit 5.0
 	/** A simple no-op logger. */
 	public static final UploadPackLogger NULL = new UploadPackLogger() {
 		public void onPackStatistics(PackWriter.Statistics stats) {
