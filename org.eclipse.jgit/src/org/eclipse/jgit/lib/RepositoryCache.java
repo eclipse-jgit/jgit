e0a8b5b16790d065fee59d6771fc4c950bd55fd6
David Pursehouse <david.pursehouse@gmail.com>
2016-09-22 16:54:33 +0200

    RepositoryCache#unregisterAndCloseRepository: Remove unused db parameter
    Change-Id: Ibee48cba85089324dc8db45066d311ad5db4f3ee
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RepositoryCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RepositoryCache.java
@@ -150,7 +150,7 @@ public class RepositoryCache {
 	public static void close(@NonNull final Repository db) {
 		if (db.getDirectory() != null) {
 			FileKey key = FileKey.exact(db.getDirectory(), db.getFS());
-			cache.unregisterAndCloseRepository(key, db);
+			cache.unregisterAndCloseRepository(key);
 		}
 	}
 
@@ -302,8 +302,7 @@ public class RepositoryCache {
 			&& (System.currentTimeMillis() - db.closedAt.get() > expireAfter);
 	}
 
-	private void unregisterAndCloseRepository(final Key location,
-			Repository db) {
+	private void unregisterAndCloseRepository(final Key location) {
 		synchronized (lockFor(location)) {
 			Repository oldDb = unregisterRepository(location);
 			if (oldDb != null) {
