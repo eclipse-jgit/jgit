7008f677f02ddc2f2456f2c2ab1ff5d1dd855a60
David Pursehouse <david.pursehouse@gmail.com>
2016-10-13 11:18:43 +0900

    HttpClientConnection: Register connection socket factory for http
    As mentioned in [1] it is necessary to register a socket connection
    factory to prevent the "http protocol is not supported" error.
    
    [1] http://stackoverflow.com/a/36354792/381622
    
    Change-Id: I3314b7343b0fa3aa845f323eb17b2abe25dfa253
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
+++ org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
@@ -54,6 +54,7 @@ import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.Proxy;
+import java.net.ProxySelector;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -94,6 +95,7 @@ import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
 import org.eclipse.jgit.transport.http.HttpConnection;
 import org.eclipse.jgit.transport.http.apache.internal.HttpApacheText;
 import org.eclipse.jgit.util.TemporaryBuffer;
@@ -161,8 +163,10 @@ public class HttpClientConnection implements HttpConnection {
 				clientBuilder.setConnectionManager(
 						new BasicHttpClientConnectionManager(registry));
 			}
-			clientBuilder.setDefaultRequestConfig(configBuilder.build());
-			client = clientBuilder.build();
+			client = clientBuilder
+					.setRoutePlanner(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
+					.setDefaultRequestConfig(configBuilder.build())
+					.build();
 		}
 
 		return client;
