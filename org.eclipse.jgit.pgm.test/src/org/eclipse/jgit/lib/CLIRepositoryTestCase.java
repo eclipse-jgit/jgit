9ed2d949bb9db2ff2abf1d4c7980933ce0914b28
David Pursehouse <david.pursehouse@gmail.com>
2016-10-19 12:51:14 +0900

    CLIRepositoryTestCase: Remove unused 'trash' member
    Change-Id: I813f3de5f059e6e5cd34af20fce1e117bfe55b55
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.pgm.test/src/org/eclipse/jgit/lib/CLIRepositoryTestCase.java
+++ org.eclipse.jgit.pgm.test/src/org/eclipse/jgit/lib/CLIRepositoryTestCase.java
@@ -62,15 +62,11 @@ public class CLIRepositoryTestCase extends LocalDiskRepositoryTestCase {
 	/** Test repository, initialized for this test case. */
 	protected Repository db;
 
-	/** Working directory of {@link #db}. */
-	protected File trash;
-
 	@Override
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
 		db = createWorkRepository();
-		trash = db.getWorkTree();
 	}
 
 	/**
