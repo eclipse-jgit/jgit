954ee72884eb481afd618d300c95d5f0343d030a
Christian Halstrick <christian.halstrick@sap.com>
2016-10-20 16:13:24 +0200

    Move constants used for config-files to ConfigConstants
    Change-Id: I7d8db4bfa1a851afd599bb8eaa8f8273204d2e1d

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
@@ -392,13 +392,6 @@ public final class Constants {
 	public static final String ATTR_FILTER_TYPE_SMUDGE = "smudge";
 
 	/**
-	 * Whether to use JGit's implementations of filters and hooks
-	 *
-	 * @since 4.6
-	 */
-	public static final String ATTR_FILTER_USE_BUILTIN = "useJGitBuiltin";
-
-	/**
 	 * Builtin filter commands start with this prefix
 	 *
 	 * @since 4.6
