eclipse-jgit/jgit

commit e67d59df3fb8ae1efe94a54efce36784f7cc83e8

StreamCopyThread: Do not let flush interrupt a write
flush calls interrupt() to interrupt a pending read and trigger a
flush.  Unfortunately that interrupt() call can also interrupt a
pending write, putting Jsch in a bad state and triggering "Short read
of block" errors.  Add locking to ensure the flush only interrupts
reads as intended.

Change-Id: Ib105d9e107ae43549ced7e6da29c22ee41cde9d8

org.eclipse.jgit/src/org/eclipse/jgit/util/io/StreamCopyThread.java
@@ -61,6 +61,9 @@ public class StreamCopyThread extends Thread {
 
 	private final AtomicInteger flushCount = new AtomicInteger(0);
 
+	/** Lock held by flush to avoid interrupting a write. */
+	private final Object writeLock;
+
 	/**
 	 * Create a thread to copy data from an input stream to an output stream.
 	 *
@@ -75,6 +78,7 @@ public class StreamCopyThread extends Thread {
 		setName(Thread.currentThread().getName() + "-StreamCopy"); //$NON-NLS-1$
 		src = i;
 		dst = o;
+		writeLock = new Object();
 	}
 
 	/**
@@ -86,7 +90,9 @@ public class StreamCopyThread extends Thread {
 	 */
 	public void flush() {
 		flushCount.incrementAndGet();
-		interrupt();
+		synchronized (writeLock) {
+			interrupt();
+		}
 	}
 
 	/**
@@ -118,12 +124,12 @@ public class StreamCopyThread extends Thread {
 			for (;;) {
 				try {
 					if (readInterrupted) {
-						try {
+						synchronized (writeLock) {
+							boolean interruptedAgain = Thread.interrupted();
 							dst.flush();
-						} catch (InterruptedIOException e) {
-							// There was a new flush() call during flush previous bytes
-							// need continue read/write/flush for the new bytes
-							continue;
+							if (interruptedAgain) {
+								interrupt();
+							}
 						}
 						readInterrupted = false;
 						if (!flushCount.compareAndSet(flushCountBeforeRead, 0)) {
@@ -148,20 +154,26 @@ public class StreamCopyThread extends Thread {
 					if (n < 0)
 						break;
 
-					boolean writeInterrupted = false;
-					for (;;) {
-						try {
-							dst.write(buf, 0, n);
-						} catch (InterruptedIOException wakey) {
-							writeInterrupted = true;
+					synchronized (writeLock) {
+						if (isInterrupted()) {
 							continue;
 						}
 
-						// set interrupt status, which will be checked
-						// when we block in src.read
-						if (writeInterrupted || flushCount.get() > 0)
-							interrupt();
-						break;
+						boolean writeInterrupted = false;
+						for (;;) {
+							try {
+								dst.write(buf, 0, n);
+							} catch (InterruptedIOException wakey) {
+								writeInterrupted = true;
+								continue;
+							}
+
+							// set interrupt status, which will be checked
+							// when we block in src.read
+							if (writeInterrupted || flushCount.get() > 0)
+								interrupt();
+							break;
+						}
 					}
 				} catch (IOException e) {
 					break;

commit 32499070b37bb3b4907909cda248f4ed066e73c0

StreamCopyThread: Do not drop data when flush is observed before writing
A caller runs

	pipe.write(some data);
	pipe.flush();
	copyThread.flush();

After the pipe.flush(), the StreamCopyThread reads the data that was
written and prepares to copy it out.  If the copyThread.flush() call
interrupts the copyThread before it acquires writeLock and starts
writing, we throw away the data we just read to fulfill the flush.
Oops.

Noticed during the review of e67d59df3fb8ae1efe94a54efce36784f7cc83e8
(StreamCopyThread: Do not let flush interrupt a write, 2016-11-04),
which introduced this bug.

Change-Id: I4aceb5610e1bfb251046097adf46bca54bc1d998

org.eclipse.jgit/src/org/eclipse/jgit/util/io/StreamCopyThread.java
@@ -155,11 +155,7 @@ public class StreamCopyThread extends Thread {
 						break;
 
 					synchronized (writeLock) {
-						if (isInterrupted()) {
-							continue;
-						}
-
-						boolean writeInterrupted = false;
+						boolean writeInterrupted = Thread.interrupted();
 						for (;;) {
 							try {
 								dst.write(buf, 0, n);