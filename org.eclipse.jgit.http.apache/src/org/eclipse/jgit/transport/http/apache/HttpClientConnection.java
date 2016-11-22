ccbf9357f9a970c50cc0a19d3c3ee12c083a7660
Zhen Chen <czhen@google.com>
2016-11-21 17:15:15 -0800

    Fix content length in HttpClientConnection
    Per the interface specification, the getContentLength method should
    return -1 if content length is unknown or greater than
    Integer.MAX_VALUE.
    
    Change-Id: Iaa36b5c146a8d654e364635fa0bd2d14129baf17
    Signed-off-by: Zhen Chen <czhen@google.com>

--- org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
+++ org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
@@ -323,8 +323,16 @@ public class HttpClientConnection implements HttpConnection {
 	}
 
 	public int getContentLength() {
-		return Integer.parseInt(resp.getFirstHeader("content-length") //$NON-NLS-1$
-				.getValue());
+		Header contentLength = resp.getFirstHeader("content-length"); //$NON-NLS-1$
+		if (contentLength == null) {
+			return -1;
+		} else {
+			long l = Long.parseLong(contentLength.getValue());
+			if (l > Integer.MAX_VALUE) {
+				return -1;
+			}
+			return (int) l;
+		}
 	}
 
 	public void setInstanceFollowRedirects(boolean followRedirects) {
