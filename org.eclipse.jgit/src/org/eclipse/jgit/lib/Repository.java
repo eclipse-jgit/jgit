8157cc5ad7ac19e04eec26d45e7a3b566433dcfb
Wim Jongman <wim.jongman@remainsoftware.com>
2017-01-06 02:27:43 +0100

    Bug 509878 - Provide a way to fix the branch name in the CreateBranchWizard
    Generic normalization method for a possible invalid branch name. 
    The method compresses dividers between spaces, then replaces spaces  
    and non word characters with underscores.
    
    This method is needed in preparation for subsequent EGit changes.
    
    Change-Id: Ic0d12f098f90f912a45bcc5693d6accf751d4e58
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -1887,4 +1887,25 @@ public abstract class Repository implements AutoCloseable {
 	public void autoGC(ProgressMonitor monitor) {
 		// default does nothing
 	}
+
+	/**
+	 * Normalizes the passed branch name into a possible valid branch name.
+	 *
+	 * @param name
+	 *            the name to normalize
+	 * @param trim
+	 *            Trim (effectively right of String) before normalizing
+	 * @return the normalized String
+	 */
+	public static String normalizeBranchName(String name, boolean trim) {
+		if (trim) {
+			name = name.trim();
+		}
+		return name.replaceAll("\\s+([_:-])\\s+", "$1") //$NON-NLS-1$//$NON-NLS-2$
+				.replaceAll(":", "-") //$NON-NLS-1$//$NON-NLS-2$
+				.replaceAll("\\s+", "_") //$NON-NLS-1$//$NON-NLS-2$
+				.replaceAll("_{2,}", "_") //$NON-NLS-1$//$NON-NLS-2$
+				.replaceAll("^_", "") //$NON-NLS-1$//$NON-NLS-2$
+				.replaceAll("[^\\w-]", ""); //$NON-NLS-1$ //$NON-NLS-2$
+	}
 }
