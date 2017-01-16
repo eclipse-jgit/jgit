653fc4789bde5497fbb6c08c723ca04517af70bb
Philipp Marx <smigfu@googlemail.com>
2017-01-16 11:31:46 +0530

    Release references on DfsBlockCache#remove
    If a DfsPackFile is being closed the DfsBlockCache should remove all
    references to it from its internal data structures (clock, table,
    packs). If this not done the Refs will still be referenced and can’t be
    GC’ed.
    
    Signed-off-by: Philipp Marx <smigfu@googlemail.com>
    Change-Id: Iaff6456d44ddafb8d804b3cf67060c9fdc661d19

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCache.java
@@ -405,6 +405,16 @@ public final class DfsBlockCache {
 					dead.value = null;
 					live -= dead.size;
 					dead.pack.cachedSize.addAndGet(-dead.size);
+
+					int slot = slot(dead.pack, dead.position);
+					for (HashEntry entry = table
+							.get(slot); entry != null; entry = entry.next) {
+						if (entry.ref == dead) {
+							table.compareAndSet(slot, entry, clean(entry));
+							break;
+						}
+					}
+
 					statEvict++;
 				} while (maxBytes < live);
 				clockHand = prev;
@@ -507,6 +517,51 @@ public final class DfsBlockCache {
 
 	void remove(DfsPackFile pack) {
 		packCache.remove(pack.getPackDescription());
+		removeRef(pack.key);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void removeRef(DfsPackKey key) {
+		clockLock.lock();
+		try {
+			Ref prev = clockHand;
+			Ref hand = clockHand.next;
+			Ref last = null;
+			long live = liveBytes;
+			while (last != clockHand) {
+				if (prev == hand) {
+					break;
+				}
+				last = hand;
+				if (hand.pack == key) {
+					Ref dead = hand;
+					hand = hand.next;
+					prev.next = hand;
+					dead.next = null;
+					dead.value = null;
+					live -= dead.size;
+					dead.pack.cachedSize.addAndGet(-dead.size);
+
+					int slot = slot(dead.pack, dead.position);
+					for (HashEntry entry = table
+							.get(slot); entry != null; entry = entry.next) {
+						if (entry.ref == dead) {
+							table.compareAndSet(slot, entry, clean(entry));
+							break;
+						}
+					}
+
+					statEvict++;
+				} else {
+					prev = hand;
+					hand = prev.next;
+				}
+			}
+			liveBytes = live;
+			clockHand = prev;
+		} finally {
+			clockLock.unlock();
+		}
 	}
 
 	private int slot(DfsPackKey pack, long position) {
