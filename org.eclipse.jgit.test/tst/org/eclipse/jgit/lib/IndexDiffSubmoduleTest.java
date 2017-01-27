07719e2e4ed016993dfd047c90863bdbee0404fc
David Pursehouse <david.pursehouse@gmail.com>
2017-01-27 19:20:15 +0900

    IndexDiffSubmoduleTest: Don't explicitly close repositories
    - submoduleStandalone gets closed by SubmoduleAddCommand#call()
    
    - submodule_db gets closed by AddCommand#call()
    
    Explicitly closing them results in the use count going negative.
    
    Change-Id: Ib6f525b644fdeaaf1934df39cc2d3583a0d883dc
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/IndexDiffSubmoduleTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/IndexDiffSubmoduleTest.java
@@ -90,9 +90,7 @@ public class IndexDiffSubmoduleTest extends RepositoryTestCase {
 				.setPath("modules/submodule")
 				.setURI(submoduleStandalone.getDirectory().toURI().toString())
 				.call();
-		submoduleStandalone.close();
 		submodule_trash = submodule_db.getWorkTree();
-		addRepoToClose(submodule_db);
 		writeTrashFile("fileInRoot", "root");
 		Git rootGit = Git.wrap(db);
 		rootGit.add().addFilepattern("fileInRoot").call();
