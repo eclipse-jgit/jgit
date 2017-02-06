f8d232213cb502214dc7d490b1bc612f88f0635f
Matthias Sohn <matthias.sohn@sap.com>
2017-02-07 00:22:57 +0100

    Branch normalizer should not normalize already valid branch names
    Change-Id: Ib746655e32a37c4ad323f1d12ac0817de8fa56cf
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ValidRefNameTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ValidRefNameTest.java
@@ -329,4 +329,18 @@ public class ValidRefNameTest {
 				Repository.normalizeBranchName("Bug 1#$  2345 - Hello World")
 						.equals("Bug_12345-Hello_World"));
 	}
+
+	@Test
+	public void testNormalizeAlreadyValidRefName() {
+		assertEquals(true,
+				Repository.normalizeBranchName("refs/heads/m.a.s.t.e.r")
+						.equals("refs/heads/m.a.s.t.e.r"));
+	}
+
+	@Test
+	public void testNormalizeTrimmedUnicodeAlreadyValidRefName() {
+		assertEquals(true,
+				Repository.normalizeBranchName(" \u00e5ngstr\u00f6m\t")
+						.equals("\u00e5ngstr\u00f6m"));
+	}
 }
