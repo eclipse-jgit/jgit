d9d8c507a48e040e3b346a1386b7eac55e2aa345
David Pursehouse <david.pursehouse@gmail.com>
2017-02-09 15:08:47 +0900

    RefLeaseSpec: Fix Eclipse errors
    - Remove unused import
    
    - Remove unused private constructor
    
    - Add Javadoc for public constructor
    
    Change-Id: I1253e9fe863ca0f63182461ee87357fbf726ea2e
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/RefLeaseSpec.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/RefLeaseSpec.java
@@ -45,8 +45,6 @@ package org.eclipse.jgit.transport;
 
 import java.io.Serializable;
 
-import org.eclipse.jgit.internal.JGitText;
-
 /**
  * Describes the expected value for a ref being pushed.
  * @since 4.7
@@ -60,12 +58,14 @@ public class RefLeaseSpec implements Serializable {
 	/** Local commitish to get expected value from. */
 	private final String expected;
 
-	private RefLeaseSpec(final RefLeaseSpec p) {
-		ref = p.getRef();
-		expected = p.getExpected();
-	}
-
-	public RefLeaseSpec(final String ref, final String expected) {
+	/**
+	 *
+	 * @param ref
+	 *            ref being pushed
+	 * @param expected
+	 *            the expected value of the ref
+	 */
+	public RefLeaseSpec(String ref, String expected) {
 		this.ref = ref;
 		this.expected = expected;
 	}
