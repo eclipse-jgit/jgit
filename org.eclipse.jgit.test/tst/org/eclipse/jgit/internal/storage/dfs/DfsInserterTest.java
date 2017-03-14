88eaf99e2a1eb205139f215a60c7f32eb00802d8
Jiahui Jiang <qzhzm173227@hotmail.com>
2017-03-14 19:12:29 -0400

    Refactor DfsBlockCache and make it pluggable
    - Made a DfsBlockCache abstract class
    - Renamed the original DfsBlockCache into DefaultDfsBlockCache
      and refactor to make it extend DfsBlockCache
    
    Change-Id: I20a28f275b599651a9d9072933f372dce1f803ca
    Signed-off-by: Jiahui Jiang <qzhzm173227@hotmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/dfs/DfsInserterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/dfs/DfsInserterTest.java
@@ -109,7 +109,7 @@ public class DfsInserterTest {
 	@Test
 	public void testReadFromInserterLargerObjects() throws IOException {
 		db.getObjectDatabase().getReaderOptions().setStreamFileThreshold(512);
-		DfsBlockCache.reconfigure(new DfsBlockCacheConfig()
+		DefaultDfsBlockCache.reconfigure(new DefaultDfsBlockCacheConfig()
 			.setBlockSize(512)
 			.setBlockLimit(2048));
 
