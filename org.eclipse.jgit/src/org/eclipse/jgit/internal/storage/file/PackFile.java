b587cfcf46476c9e1017ade84ef0d6d73a6a245d
Matthias Sohn <matthias.sohn@sap.com>
2017-03-25 02:33:06 +0100

    Only mark packfile invalid if exception signals permanent problem
    Add NoPackSignatureException and UnsupportedPackVersionException to
    explicitly mark permanent unrecoverable problems with a pack 
    
    Assume problem with a pack is permanent only if we are sure the
    exception signals a non-transient problem we can't recover from:
    - EOFException: file ended unexpectedly
    - AccessDeniedException: we lack permissions
    - NoSuchFileException: file has gone missing
    - CorruptObjectException: we detected corruption
    - PackMismatchException: pack no longer matches its index
    - UnpackException: unpacking failed
    - Error: serious problems applications should not try to recover from
    
    Change-Id: I2c416ce2b0e23255c4fb03a3f9a0ee237f7a484a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackFile.java
@@ -56,6 +56,8 @@ import java.io.InterruptedIOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel.MapMode;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.NoSuchFileException;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collections;
@@ -70,9 +72,13 @@ import java.util.zip.Inflater;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.errors.LargeObjectException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.NoPackSignatureException;
 import org.eclipse.jgit.errors.PackInvalidException;
 import org.eclipse.jgit.errors.PackMismatchException;
 import org.eclipse.jgit.errors.StoredObjectRepresentationNotAvailableException;
+import org.eclipse.jgit.errors.UnpackException;
+import org.eclipse.jgit.errors.UnsupportedPackIndexVersionException;
+import org.eclipse.jgit.errors.UnsupportedPackVersionException;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.internal.storage.pack.BinaryDelta;
 import org.eclipse.jgit.internal.storage.pack.ObjectToPack;
@@ -642,15 +648,23 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 			// max open files)
 			openFail(!packFile.exists());
 			throw fn;
-		} catch (IOException ioe) {
+		} catch (EOFException | AccessDeniedException | NoSuchFileException
+				| CorruptObjectException | NoPackSignatureException
+				| PackMismatchException | UnpackException
+				| UnsupportedPackIndexVersionException
+				| UnsupportedPackVersionException pe) {
+			// exceptions signaling permanent problems with a pack
 			openFail(true);
-			throw ioe;
-		} catch (RuntimeException re) {
+			throw pe;
+		} catch (IOException | RuntimeException ge) {
+			// generic exceptions could be transient so we should not mark the
+			// pack invalid to avoid false MissingObjectExceptions
+			openFail(false);
+			throw ge;
+		} catch (Error e) {
+			// don't try to recover from fatal errors
 			openFail(true);
-			throw re;
-		} catch (Error re) {
-			openFail(true);
-			throw re;
+			throw e;
 		}
 	}
 
@@ -717,28 +731,32 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 
 		fd.seek(0);
 		fd.readFully(buf, 0, 12);
-		if (RawParseUtils.match(buf, 0, Constants.PACK_SIGNATURE) != 4)
-			throw new IOException(JGitText.get().notAPACKFile);
+		if (RawParseUtils.match(buf, 0, Constants.PACK_SIGNATURE) != 4) {
+			throw new NoPackSignatureException(JGitText.get().notAPACKFile);
+		}
 		final long vers = NB.decodeUInt32(buf, 4);
 		final long packCnt = NB.decodeUInt32(buf, 8);
-		if (vers != 2 && vers != 3)
-			throw new IOException(MessageFormat.format(
+		if (vers != 2 && vers != 3) {
+			throw new UnsupportedPackVersionException(MessageFormat.format(
 					JGitText.get().unsupportedPackVersion, Long.valueOf(vers)));
+		}
 
-		if (packCnt != idx.getObjectCount())
+		if (packCnt != idx.getObjectCount()) {
 			throw new PackMismatchException(MessageFormat.format(
 					JGitText.get().packObjectCountMismatch,
 					Long.valueOf(packCnt), Long.valueOf(idx.getObjectCount()),
 					getPackFile()));
+		}
 
 		fd.seek(length - 20);
 		fd.readFully(buf, 0, 20);
-		if (!Arrays.equals(buf, packChecksum))
+		if (!Arrays.equals(buf, packChecksum)) {
 			throw new PackMismatchException(MessageFormat.format(
 					JGitText.get().packObjectCountMismatch
 					, ObjectId.fromRaw(buf).name()
 					, ObjectId.fromRaw(idx.packChecksum).name()
 					, getPackFile()));
+		}
 	}
 
 	ObjectLoader load(final WindowCursor curs, long pos)
