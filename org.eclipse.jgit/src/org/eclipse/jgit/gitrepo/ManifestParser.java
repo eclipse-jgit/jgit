b65a764b6bc5d28f59084a3af98df04fdf6373ba
Matthias Sohn <matthias.sohn@sap.com>
2017-03-31 00:38:36 +0200

    Remove unused import from ManifestParser
    Change-Id: Ie60ef9c7bc6ce0fdf017949ebfb9a21753e70506
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/ManifestParser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/ManifestParser.java
@@ -46,7 +46,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
