fc24c5e18d654164f1616bb5a30cffd146b9e8b4
Matthias Sohn <matthias.sohn@sap.com>
2017-04-09 23:37:04 +0200

    Add missing @Override annotations in AppServer.TestMappedLoginService
    Change-Id: Ia728f1d033ecdf96b22945bc1ae38e5cb11064dd
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.junit.http/src/org/eclipse/jgit/junit/http/AppServer.java
+++ org.eclipse.jgit.junit.http/src/org/eclipse/jgit/junit/http/AppServer.java
@@ -185,10 +185,12 @@ public class AppServer {
 			putUser(username, new Password(password), new String[] { role });
 		}
 
+		@Override
 		protected String[] loadRoleInfo(KnownUser user) {
 			return null;
 		}
 
+		@Override
 		protected KnownUser loadUserInfo(String usrname) {
 			return null;
 		}
