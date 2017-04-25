bbbb63ac15173565468e429cf73771d56cc10b0d
Masaya Suzuki <masayasuzuki@google.com>
2017-04-25 12:25:37 -0700

    Remove isChunked and ContentLength check
    Whether it's chunked encoding or not, we need to discard the request
    body to comply with the HTTP spec.
    
    Change-Id: I593998b9cd55ad886ff1ec94358b14af2a60a7a8
    Signed-off-by: Masaya Suzuki <masayasuzuki@google.com>

--- org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/ServletUtils.java
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/ServletUtils.java
@@ -127,12 +127,10 @@ public final class ServletUtils {
 	 *            the request whose body must be consumed.
 	 */
 	public static void consumeRequestBody(HttpServletRequest req) {
-		if (0 < req.getContentLength() || isChunked(req)) {
-			try {
-				consumeRequestBody(req.getInputStream());
-			} catch (IOException e) {
-				// Ignore any errors obtaining the input stream.
-			}
+		try {
+			consumeRequestBody(req.getInputStream());
+		} catch (IOException e) {
+			// Ignore any errors obtaining the input stream.
 		}
 	}
 
