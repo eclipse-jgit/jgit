ee479092dc7f2c1fa3f7031aabd916fabd59718c
Zhen Chen <czhen@google.com>
2017-06-20 15:22:09 -0700

    Add dfs fsck implementation
    JGit already had some fsck-like classes like ObjectChecker which can
    check for an individual object.
    
    The read-only FsckPackParser which will parse all objects within a pack
    file and check it with ObjectChecker. It will also check the pack index
    file against the object information from the pack parser.
    
    Change-Id: Ifd8e0d28eb68ff0b8edd2b51b2fa3a50a544c855
    Signed-off-by: Zhen Chen <czhen@google.com>

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/JGitTestUtil.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/JGitTestUtil.java
@@ -258,4 +258,27 @@ public abstract class JGitTestUtil {
 				target);
 	}
 
+	/**
+	 * Concatenate byte arrays.
+	 *
+	 * @param b
+	 *            byte arrays to combine together.
+	 * @return a single byte array that contains all bytes copied from input
+	 *         byte arrays.
+	 * @since 4.9
+	 */
+	public static byte[] concat(byte[]... b) {
+		int n = 0;
+		for (byte[] a : b) {
+			n += a.length;
+		}
+
+		byte[] data = new byte[n];
+		n = 0;
+		for (byte[] a : b) {
+			System.arraycopy(a, 0, data, n, a.length);
+			n += a.length;
+		}
+		return data;
+	}
 }
