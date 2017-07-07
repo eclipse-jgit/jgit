8223a7e6463fa530602e6c967343c2536ce9aae6
Dave Borowitz <dborowitz@google.com>
2017-07-07 14:43:57 -0400

    Temporarily @Ignore flaky CommitCommandTest methods
    Change-Id: Ia2c42d014323bd29b85bf76f1a20c83f612406d7
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>
    (cherry picked from commit e93b0026ced10c956e76daed038f2560a33b5baf)

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CommitCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CommitCommandTest.java
@@ -76,6 +76,7 @@ import org.eclipse.jgit.submodule.SubmoduleWalk;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.eclipse.jgit.util.FS;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -291,6 +292,7 @@ public class CommitCommandTest extends RepositoryTestCase {
 		}
 	}
 
+	@Ignore("very flaky when run with Hudson")
 	@Test
 	public void commitUpdatesSmudgedEntries() throws Exception {
 		try (Git git = new Git(db)) {
@@ -347,6 +349,7 @@ public class CommitCommandTest extends RepositoryTestCase {
 		}
 	}
 
+	@Ignore("very flaky when run with Hudson")
 	@Test
 	public void commitIgnoresSmudgedEntryWithDifferentId() throws Exception {
 		try (Git git = new Git(db)) {
