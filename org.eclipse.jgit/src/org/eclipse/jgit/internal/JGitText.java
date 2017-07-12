df82d9e4c2b4af31f5746c6e3c7816178d06b0b7
Shawn Pearce <spearce@spearce.org>
2017-07-12 14:01:50 -0700

    reftable: scan and lookup reftable files
    ReftableReader provides sequential scanning support over all
    references, a range of references within a subtree (such as
    "refs/heads/"), and lookup of a single reference.  Reads can be
    accelerated by an index block, if it was created by the writer.
    
    The BlockSource interface provides an abstraction to read from the
    reftable's backing storage, supporting a future commit to connect
    to JGit DFS and the DfsBlockCache.
    
    Change-Id: Ib0dc5fa937d0c735f2a9ff4439d55c457fea7aa8

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -426,6 +426,9 @@ public class JGitText extends TranslationBundle {
 	/***/ public String invalidPathReservedOnWindows;
 	/***/ public String invalidReflogRevision;
 	/***/ public String invalidRefName;
+	/***/ public String invalidReftableBlock;
+	/***/ public String invalidReftableCRC;
+	/***/ public String invalidReftableFile;
 	/***/ public String invalidRemote;
 	/***/ public String invalidShallowObject;
 	/***/ public String invalidStageForPath;
@@ -754,6 +757,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String unsupportedOperationNotAddAtEnd;
 	/***/ public String unsupportedPackIndexVersion;
 	/***/ public String unsupportedPackVersion;
+	/***/ public String unsupportedReftableVersion;
 	/***/ public String unsupportedRepositoryDescription;
 	/***/ public String updateRequiresOldIdAndNewId;
 	/***/ public String updatingHeadFailed;
