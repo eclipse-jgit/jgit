6690d73a9beaa6e5ba1edb297a6b3eeed14e36f8
Shawn Pearce <spearce@spearce.org>
2017-07-17 09:35:14 -0700

    reftable: DfsReftable reads through DfsBlockCache
    DfsBlockCache directly shares its internal byte[] with ReftableReader,
    avoding copying between the DfsBlockCache and the BlockReader
    instances used by ReftableReader.
    
    Change-Id: Icaa4f40052b26f952681414653a8b5314b7c2c23

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableConstants.java
@@ -43,12 +43,14 @@
 
 package org.eclipse.jgit.internal.storage.reftable;
 
-class ReftableConstants {
+/** Constants for the reftable file format. */
+public class ReftableConstants {
 	static final byte[] FILE_HEADER_MAGIC = { '\1', 'R', 'E', 'F' };
-
-	static final int FILE_HEADER_LEN = 8;
-	static final int FILE_FOOTER_LEN = 36;
 	static final byte VERSION_1 = (byte) 1;
+
+	/** Length in bytes of the file header, at position 8. */
+	public static final int FILE_HEADER_LEN = 8;
+	static final int FILE_FOOTER_LEN = 36;
 
 	static final byte FILE_BLOCK_TYPE = '\1';
 	static final byte REF_BLOCK_TYPE = 'r';
