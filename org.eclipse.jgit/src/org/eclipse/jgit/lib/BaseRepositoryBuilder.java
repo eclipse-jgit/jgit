7e40b83bffdad953d455d4bed7d5f300c5daaa25
Jacob Keller <jacob.keller@gmail.com>
2017-07-21 17:35:48 -0700

    BaseRepositoryBuilder: setGitDir should honor .git symref files
    The current implementation of .setGitDir does not honor .git symref
    files. This can cause problems because developers may assume that it
    will properly handle these cases, and it makes JGit behave differently
    from C Git.
    
    In C Git, using --git-dir or setting the GIT_DIR environment variable
    will properly follow a .git file symref, but JGit does not behave the
    same way.
    
    Modify .setGitDir() to take into account the symrefs itself, and change
    the callers in BaseRepositoryBuilder to account for this moved logic.
    
    Change-Id: Iea6c05cb605a36d14806d9af60883c8dd79dd094
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/BaseRepositoryBuilder.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/BaseRepositoryBuilder.java
@@ -171,13 +171,26 @@ public class BaseRepositoryBuilder<B extends BaseRepositoryBuilder, R extends Re
 	 * The meta directory stores the objects, references, and meta files like
 	 * {@code MERGE_HEAD}, or the index file. If {@code null} the path is
 	 * assumed to be {@code workTree/.git}.
+	 * <p>
+	 * This function will honor a .git file which is a symRef to the real
+	 * git directory.
 	 *
 	 * @param gitDir
 	 *            {@code GIT_DIR}, the repository meta directory.
 	 * @return {@code this} (for chaining calls).
+	 * @throws IOException
+	 *             the repository could not be accessed to configure the rest of
+	 *             the builder's parameters.
 	 */
-	public B setGitDir(File gitDir) {
-		this.gitDir = gitDir;
+	public B setGitDir(File gitDir) throws IOException {
+		if (gitDir == null || !gitDir.isFile()) {
+			this.gitDir = gitDir;
+		} else {
+			// We were given a gitfile so we need to resolve the
+			// symRef to find the real gitDir.
+			File workTree = gitDir.getParentFile();
+			this.gitDir = getSymRef(workTree, gitDir, safeFS());
+		}
 		this.config = null;
 		return self();
 	}
@@ -355,8 +368,11 @@ public class BaseRepositoryBuilder<B extends BaseRepositoryBuilder, R extends Re
 	 * already set in this builder.
 	 *
 	 * @return {@code this} (for chaining calls).
+	 * @throws IOException
+	 *             the repository could not be accessed to configure the rest of
+	 *             the builder's parameters.
 	 */
-	public B readEnvironment() {
+	public B readEnvironment() throws IOException {
 		return readEnvironment(SystemReader.getInstance());
 	}
 
@@ -371,8 +387,11 @@ public class BaseRepositoryBuilder<B extends BaseRepositoryBuilder, R extends Re
 	 * @param sr
 	 *            the SystemReader abstraction to access the environment.
 	 * @return {@code this} (for chaining calls).
+	 * @throws IOException
+	 *             the repository could not be accessed to configure the rest of
+	 *             the builder's parameters.
 	 */
-	public B readEnvironment(SystemReader sr) {
+	public B readEnvironment(SystemReader sr) throws IOException {
 		if (getGitDir() == null) {
 			String val = sr.getenv(GIT_DIR_KEY);
 			if (val != null)
@@ -512,19 +531,17 @@ public class BaseRepositoryBuilder<B extends BaseRepositoryBuilder, R extends Re
 			FS tryFS = safeFS();
 			while (current != null) {
 				File dir = new File(current, DOT_GIT);
-				if (FileKey.isGitRepository(dir, tryFS)) {
-					setGitDir(dir);
-					break;
-				} else if (dir.isFile()) {
-					try {
-						setGitDir(getSymRef(current, dir, tryFS));
+				try {
+					if (FileKey.isGitRepository(dir, tryFS) || dir.isFile()) {
+						setGitDir(dir);
 						break;
-					} catch (IOException ignored) {
-						// Continue searching if gitdir ref isn't found
+					} else if (FileKey.isGitRepository(current, tryFS)) {
+						setGitDir(current);
+						break;
 					}
-				} else if (FileKey.isGitRepository(current, tryFS)) {
-					setGitDir(current);
 					break;
+				} catch (IOException ignored) {
+					// Continue searching if gitdir ref isn't found
 				}
 
 				current = current.getParentFile();
@@ -601,10 +618,7 @@ public class BaseRepositoryBuilder<B extends BaseRepositoryBuilder, R extends Re
 		// location
 		if (getGitDir() == null && getWorkTree() != null) {
 			File dotGit = new File(getWorkTree(), DOT_GIT);
-			if (!dotGit.isFile())
-				setGitDir(dotGit);
-			else
-				setGitDir(getSymRef(getWorkTree(), dotGit, safeFS()));
+			setGitDir(dotGit);
 		}
 	}
 
