0226e0e514740dca200f1f71721b79174f8c96e1
Joan Goyeau <joan@goyeau.com>
2017-07-24 11:59:33 +0100

    Fix default directory set when setDirectory wasn't called.
    Bug: 519883
    Change-Id: I46716e9626b4c4adc0806a7c8df6914309040b94
    Signed-off-by: Joan Goyeau <joan@goyeau.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
@@ -155,6 +155,17 @@ public class CloneCommandTest extends RepositoryTestCase {
 	}
 
 	@Test
+	public void testCloneRepositoryDefaultDirectory() throws IOException,
+			JGitInternalException, GitAPIException {
+		CloneCommand command = Git.cloneRepository();
+		command.setURI(fileUri());
+		Git git2 = command.call();
+		addRepoToClose(git2.getRepository());
+		assertEquals(git.getRepository().getWorkTree().getName(),
+				git2.getRepository().getWorkTree().getName());
+	}
+
+	@Test
 	public void testCloneRepositoryExplicitGitDirNonStd() throws IOException,
 			JGitInternalException, GitAPIException {
 		File directory = createTempDirectory("testCloneRepository");
