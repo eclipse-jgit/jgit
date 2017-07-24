d11914ea7ef13183df9d119469c83e5d74099507
Shawn Pearce <spearce@spearce.org>
2017-07-24 08:49:31 -0700

    reftable: optimize virtual reftable from collection of refs
    One use for Reftable.from() is during a transition from a
    large collection of references in a DFS system to reftable.
    ByteArrayOutputStream will be inefficient for 866k refs,
    but a custom block based buffer that can perform zero-copy
    reads into the ReftableReader can manage large ref sets.
    
    Change-Id: I3f9f6d1313d93ee4e660a99cee36a6293a6f52cd

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/reftable/ReftableTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/reftable/ReftableTest.java
@@ -123,6 +123,18 @@ public class ReftableTest {
 		}
 	}
 
+	@SuppressWarnings("boxing")
+	@Test
+	public void largeVirtualTableFromRefs() throws IOException {
+		List<Ref> refs = new ArrayList<>();
+		for (int i = 1; i <= 5670; i++) {
+			refs.add(ref(String.format("refs/heads/%04d", i), i));
+		}
+		Reftable t = Reftable.from(refs);
+		assertScan(refs, t);
+		assertSeek(refs, t);
+	}
+
 	@Test
 	public void estimateCurrentBytes() throws IOException {
 		Ref exp = ref(MASTER, 1);
@@ -296,17 +308,7 @@ public class ReftableTest {
 		byte[] table = write(refs);
 		assertTrue(stats.refIndexKeys() > 0);
 		assertTrue(stats.refIndexSize() > 0);
-
-		ReftableReader t = read(table);
-		try (RefCursor rc = t.allRefs()) {
-			for (Ref exp : refs) {
-				assertTrue("has " + exp.getName(), rc.next());
-				Ref act = rc.getRef();
-				assertEquals(exp.getName(), act.getName());
-				assertEquals(exp.getObjectId(), act.getObjectId());
-			}
-			assertFalse(rc.next());
-		}
+		assertScan(refs, read(table));
 	}
 
 	@SuppressWarnings("boxing")
@@ -320,17 +322,7 @@ public class ReftableTest {
 		byte[] table = write(refs);
 		assertTrue(stats.refIndexKeys() > 0);
 		assertTrue(stats.refIndexSize() > 0);
-
-		ReftableReader t = read(table);
-		for (Ref exp : refs) {
-			try (RefCursor rc = t.seek(exp.getName())) {
-				assertTrue("has " + exp.getName(), rc.next());
-				Ref act = rc.getRef();
-				assertEquals(exp.getName(), act.getName());
-				assertEquals(exp.getObjectId(), act.getObjectId());
-				assertFalse(rc.next());
-			}
-		}
+		assertSeek(refs, read(table));
 	}
 
 	@SuppressWarnings("boxing")
@@ -346,17 +338,7 @@ public class ReftableTest {
 		assertEquals(0, stats.refIndexSize());
 		assertEquals(4, stats.refBlockCount());
 		assertEquals(table.length, stats.totalBytes());
-
-		ReftableReader t = read(table);
-		try (RefCursor rc = t.allRefs()) {
-			for (Ref exp : refs) {
-				assertTrue("has " + exp.getName(), rc.next());
-				Ref act = rc.getRef();
-				assertEquals(exp.getName(), act.getName());
-				assertEquals(exp.getObjectId(), act.getObjectId());
-			}
-			assertFalse(rc.next());
-		}
+		assertScan(refs, read(table));
 	}
 
 	@SuppressWarnings("boxing")
@@ -370,17 +352,7 @@ public class ReftableTest {
 		byte[] table = write(refs);
 		assertEquals(0, stats.refIndexKeys());
 		assertEquals(4, stats.refBlockCount());
-
-		ReftableReader t = read(table);
-		for (Ref exp : refs) {
-			try (RefCursor rc = t.seek(exp.getName())) {
-				assertTrue("has " + exp.getName(), rc.next());
-				Ref act = rc.getRef();
-				assertEquals(exp.getName(), act.getName());
-				assertEquals(exp.getObjectId(), act.getObjectId());
-				assertFalse(rc.next());
-			}
-		}
+		assertSeek(refs, read(table));
 	}
 
 	@Test
@@ -563,6 +535,32 @@ public class ReftableTest {
 		}
 	}
 
+
+	private static void assertScan(List<Ref> refs, Reftable t)
+			throws IOException {
+		try (RefCursor rc = t.allRefs()) {
+			for (Ref exp : refs) {
+				assertTrue("has " + exp.getName(), rc.next());
+				Ref act = rc.getRef();
+				assertEquals(exp.getName(), act.getName());
+				assertEquals(exp.getObjectId(), act.getObjectId());
+			}
+			assertFalse(rc.next());
+		}
+	}
+
+	private static void assertSeek(List<Ref> refs, Reftable t)
+			throws IOException {
+		for (Ref exp : refs) {
+			try (RefCursor rc = t.seek(exp.getName())) {
+				assertTrue("has " + exp.getName(), rc.next());
+				Ref act = rc.getRef();
+				assertEquals(exp.getName(), act.getName());
+				assertEquals(exp.getObjectId(), act.getObjectId());
+				assertFalse(rc.next());
+			}
+		}
+	}
 
 	private static Ref ref(String name, int id) {
 		return new ObjectIdRef.PeeledNonTag(PACKED, name, id(id));
