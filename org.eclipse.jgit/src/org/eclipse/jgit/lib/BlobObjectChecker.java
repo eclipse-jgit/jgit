a5afcb26cf38a430a865effa8face95e78a24ee0
Masaya Suzuki <masayasuzuki@google.com>
2017-08-09 23:30:03 -0700

    Add BlobObjectChecker
    Some repositories can have a policy that do not accept certain blobs. To
    check if the incoming pack file contains such blobs, ObjectChecker can
    be used. However, this ObjectChecker is not called by PackParser if the
    blob is stored as a whole. This is because the object can be so large
    that it doesn't fit in memory.
    
    This change introduces BlobObjectChecker. This interface takes chunks of
    a blob instead of the entire object. ObjectChecker can optionally return
    a BlobObjectChecker. This won't change existing ObjectChecker
    implementation; existing implementation continues to receive deltified
    blob objects only.
    
    Change-Id: Ic33a92c2de42bd7a89786a4da26b7a648b25218d
    Signed-off-by: Masaya Suzuki <masayasuzuki@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/BlobObjectChecker.java
@@ -0,0 +1,46 @@
+package org.eclipse.jgit.lib;
+
+import org.eclipse.jgit.errors.CorruptObjectException;
+
+/**
+ * Verifies that a blob object is a valid object.
+ * <p>
+ * Unlike trees, commits and tags, there's no validity of blobs. Implementers
+ * can optionally implement this blob checker to reject certain blobs.
+ */
+public interface BlobObjectChecker {
+	/** No-op implementation of {@link BlobObjectChecker}. */
+	public static BlobObjectChecker NULL_CHECKER = new BlobObjectChecker() {
+		@Override
+		public void update(byte[] in, int p, int len) {
+			// Empty implementation.
+		}
+
+		@Override
+		public void endBlob(AnyObjectId id) {
+			// Empty implementation.
+		}
+	};
+
+	/**
+	 * Check a new fragment of the blob.
+	 *
+	 * @param in
+	 *            input array of bytes.
+	 * @param p
+	 *            offset to start at from {@code in}.
+	 * @param len
+	 *            number of bytes from {@code p}.
+	 */
+	void update(byte[] in, int p, int len);
+
+	/**
+	 * Finalize the blob checking.
+	 *
+	 * @param id
+	 *            identity of the object being checked.
+	 * @throws CorruptObjectException
+	 *             if any error was detected.
+	 */
+	void endBlob(AnyObjectId id) throws CorruptObjectException;
+}
