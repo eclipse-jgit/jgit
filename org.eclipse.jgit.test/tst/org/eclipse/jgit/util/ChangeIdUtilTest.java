ee56e5fc042a903b03773608ede62731452420ca
David Pursehouse <david.pursehouse@gmail.com>
2017-09-30 10:56:42 +0100

    ChangeIdUtilTest: Remove unused notestCommitDashV
    This test was never being run. Since it was introduced it was
    named "notest.." which meant it didn't run with JUnit3, and
    since it is not annotated @Test it also doesn't run with JUnit4.
    
    When compiling with Bazel 0.6.0, error-prone raises an error
    that the public method is not annotated with @Ignore or @Test.
    
    Given that the test has never been run anyway, we can just
    remove it.
    
    Bug: 525415
    Change-Id: Ie9a54f89fe42e0c201f547ff54ff1d419ce37864
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/util/ChangeIdUtilTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/util/ChangeIdUtilTest.java
@@ -583,23 +583,6 @@ public class ChangeIdUtilTest {
 						SOB1));
 	}
 
-	public void notestCommitDashV() throws Exception {
-		assertEquals("a\n" + //
-				"\n" + //
-				"Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n" + //
-				SOB1 + //
-				SOB2, //
-				call("a\n" + //
-						"\n" + //
-						SOB1 + //
-						SOB2 + //
-						"\n" + //
-						"# on branch master\n" + //
-						"diff --git a/src b/src\n" + //
-						"new file mode 100644\n" + //
-						"index 0000000..c78b7f0\n"));
-	}
-
 	@Test
 	public void testWithEndingURL() throws Exception {
 		assertEquals("a\n" + //
