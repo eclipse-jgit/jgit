6615f07baf6b39608966cb593b58db31bcb650a6
Han-Wen Nienhuys <hanwen@google.com>
2017-10-23 14:41:00 +0200

    Introduce ObjectLoader.Filter for delegation
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I78ada3f4023c4304b19f94fb044024f0f0b2cbbe

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectLoader.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectLoader.java
@@ -323,4 +323,37 @@ public abstract class ObjectLoader {
 			return new ObjectStream.SmallStream(this);
 		}
 	}
+
+
+	/**
+	 * Delegation for ObjectLoader.
+	 */
+	public static abstract class Filter extends ObjectLoader {
+		protected abstract ObjectLoader delegate();
+
+		@Override
+		public int getType() {
+			return delegate().getType();
+		}
+
+		@Override
+		public long getSize() {
+			return delegate().getSize();
+		}
+
+		@Override
+		public boolean isLarge() {
+			return delegate().isLarge();
+		}
+
+		@Override
+		public byte[] getCachedBytes() {
+			return delegate().getCachedBytes();
+		}
+
+		@Override
+		public ObjectStream openStream() throws IOException {
+			return delegate().openStream();
+		}
+	}
 }
