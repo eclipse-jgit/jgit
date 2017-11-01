5ce1cc3d43a85acfaa2237968dd0d2a29beb07b0
Dave Borowitz <dborowitz@google.com>
2017-11-01 10:42:46 -0400

    ObjectDirectory: Factor a method to close open pack handles
    Change-Id: I9490c4583fdf801de9c0bb09595ebb8fb4926988

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/ObjectDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/ObjectDirectory.java
@@ -969,6 +969,21 @@ public class ObjectDirectory extends FileObjectDatabase {
 		return nameSet;
 	}
 
+	void closeAllPackHandles(File packFile) {
+		// if the packfile already exists (because we are rewriting a
+		// packfile for the same set of objects maybe with different
+		// PackConfig) then make sure we get rid of all handles on the file.
+		// Windows will not allow for rename otherwise.
+		if (packFile.exists()) {
+			for (PackFile p : getPacks()) {
+				if (packFile.getPath().equals(p.getPackFile().getPath())) {
+					p.close();
+					break;
+				}
+			}
+		}
+	}
+
 	AlternateHandle[] myAlternates() {
 		AlternateHandle[] alt = alternates.get();
 		if (alt == null) {
