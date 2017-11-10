f59918671508319cb5990c66191f43be50733518
Markus Duft <markus.duft@ssi-schaefer.com>
2017-11-10 11:10:28 +0100

    LFS: pre-push upload support
    Replaces any pre-push hook if jgit-builtin LFS support is enabled for
    the current repo (or user/system).
    
    Change-Id: Ie7d2b90e26e948d9cca3d05a7a19489488c75895
    Signed-off-by: Markus Duft <markus.duft@ssi-schaefer.com>

--- org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/internal/LfsText.java
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/internal/LfsText.java
@@ -75,5 +75,7 @@ public class LfsText extends TranslationBundle {
 	/***/ public String lfsNeedDownloadNoUrl;
 	/***/ public String serverFailure;
 	/***/ public String wrongAmoutOfDataReceived;
+	/***/ public String wrongAmoutOfDataWritten;
 	/***/ public String userConfigInvalid;
+	/***/ public String missingLocalObject;
 }
