a7c18023453a06c004eea00eeb28aa33b847ce9b
Zhen Chen <czhen@google.com>
2017-11-10 16:05:28 -0800

    Add a setting for fsck to check connectivity only
    The object checks may take a long time and sometimes we are only
    interested in connectivity check.
    
    Change-Id: I654e8fdccdb16d796f920088429d188cc96734bc
    Signed-off-by: Zhen Chen <czhen@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsFsck.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsFsck.java
@@ -69,6 +69,7 @@ public class DfsFsck {
 	private final DfsRepository repo;
 	private final DfsObjDatabase objdb;
 	private ObjectChecker objChecker = new ObjectChecker();
+	private boolean connectivityOnly = false;
 
 	/**
 	 * Initialize DFS fsck.
@@ -97,7 +98,9 @@ public class DfsFsck {
 		}
 
 		FsckError errors = new FsckError();
-		checkPacks(pm, errors);
+		if (!connectivityOnly) {
+			checkPacks(pm, errors);
+		}
 		checkConnectivity(pm, errors);
 		return errors;
 	}
@@ -173,5 +176,14 @@ public class DfsFsck {
 	 */
 	public void setObjectChecker(ObjectChecker objChecker) {
 		this.objChecker = objChecker;
+	}
+
+	/**
+	 * @param connectivityOnly
+	 *            if {@code true} the fsck only check for connectivity.
+	 *            default is false.
+	 */
+	public void setConnectivityOnly(boolean connectivityOnly) {
+		this.connectivityOnly = connectivityOnly;
 	}
 }
