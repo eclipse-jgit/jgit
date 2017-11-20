e1adfee5f52023b9716899e6b6561fef1bb1fc1c
Marc Strapetz <marc.strapetz@syntevo.com>
2017-11-20 18:21:22 +0100

    ConfigTest: fix on Windows
    Change-Id: I37a2ef611aef97faf1b891a9660c1745435a915d
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ConfigTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ConfigTest.java
@@ -818,7 +818,7 @@ public class ConfigTest {
 	@Test
 	public void testIncludeTooManyRecursions() throws IOException {
 		File config = tmp.newFile("config");
-		String include = "[include]\npath=" + config.toPath() + "\n";
+		String include = "[include]\npath=" + pathToString(config) + "\n";
 		Files.write(config.toPath(), include.getBytes());
 		FileBasedConfig fbConfig = new FileBasedConfig(null, config,
 				FS.DETECTED);
@@ -838,8 +838,8 @@ public class ConfigTest {
 		File other = tmp.newFile("config.other");
 
 		String fooBar = "[foo]\nbar=true\n";
-		String includeMore = "[include]\npath=" + more.toPath() + "\n";
-		String includeOther = "path=" + other.toPath() + "\n";
+		String includeMore = "[include]\npath=" + pathToString(more) + "\n";
+		String includeOther = "path=" + pathToString(other) + "\n";
 		String fooPlus = fooBar + includeMore + includeOther;
 		Files.write(config.toPath(), fooPlus.getBytes());
 
@@ -849,10 +849,18 @@ public class ConfigTest {
 		String otherMore = "[other]\nmore=bar\n";
 		Files.write(other.toPath(), otherMore.getBytes());
 
-		Config parsed = parse("[include]\npath=" + config.toPath() + "\n");
+		Config parsed = parse("[include]\npath=" + pathToString(config) + "\n");
 		assertTrue(parsed.getBoolean("foo", "bar", false));
 		assertEquals("bar", parsed.getString("foo", null, "more"));
 		assertEquals("bar", parsed.getString("other", null, "more"));
+	}
+
+	private static String pathToString(File file) {
+		final String path = file.getPath();
+		if (SystemReader.getInstance().isWindows()) {
+			return path.replace('\\', '/');
+		}
+		return path;
 	}
 
 	private static void assertReadLong(long exp) throws ConfigInvalidException {
