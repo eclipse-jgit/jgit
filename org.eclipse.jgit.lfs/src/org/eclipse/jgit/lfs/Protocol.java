cdc66c9fa240d5e2970be369b6fd05cc873ea284
Markus Duft <markus.duft@ssi-schaefer.com>
2017-12-05 10:46:43 +0100

    LFS: Dramatically improve checkout speed with SSH authentication
    SSH Authentication is quite expensive (~120ms on localhost against
    Gerrit with LFS plugin). The SSH authentication typically also sends a
    validity time of the returned token, which allows to re-use it for a
    certain time, avoiding the expensive authentication on every download
    request. This improves checkout times by large factors depending on the
    LFS object amount/sizes.
    
    Also make sure that all instances of Gson used by LFS are configured in
    the same way.
    
    Change-Id: I422c94c37021b4322789b3829fa0185e25d683f2
    Signed-off-by: Markus Duft <markus.duft@ssi-schaefer.com>

--- org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/Protocol.java
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/Protocol.java
@@ -46,6 +46,10 @@ package org.eclipse.jgit.lfs;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * This interface describes the network protocol used between lfs client and lfs
  * server
@@ -97,6 +101,15 @@ public interface Protocol {
 		public Map<String, String> header;
 	}
 
+	/**
+	 * An action with an additional expiration timestamp
+	 *
+	 * @since 4.11
+	 */
+	class ExpiringAction extends Action {
+		public String expiresAt;
+	}
+
 	/** Describes an error to be returned by the LFS batch API */
 	class Error {
 		public int code;
@@ -138,4 +151,17 @@ public interface Protocol {
 	 * Path to the LFS objects servlet.
 	 */
 	String OBJECTS_LFS_ENDPOINT = "/objects/batch"; //$NON-NLS-1$
+
+	/**
+	 * @return a {@link Gson} instance suitable for handling this
+	 *         {@link Protocol}
+	 *
+	 * @since 4.11
+	 */
+	public static Gson gson() {
+		return new GsonBuilder()
+				.setFieldNamingPolicy(
+						FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+				.disableHtmlEscaping().create();
+	}
 }
