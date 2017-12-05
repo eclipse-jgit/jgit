4da06ed92ccc8d08106408da5b3a9b6c090ccc96
Markus Duft <markus.duft@ssi-schaefer.com>
2017-12-05 09:30:48 +0100

    Progress reporting for checkout.
    The reason for the change is LFS: when using a lot of LFS files,
    checkout can take quite some time on larger repositories. To avoid
    "hanging" UI, provide progress reporting.
    
    The feature is also useful without LFS, so it is independent of it.
    
    Change-Id: I021e764241f3c107eaf2771f6b5785245b146b42
    Signed-off-by: Markus Duft <markus.duft@ssi-schaefer.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
@@ -358,6 +358,7 @@ public class CloneCommand extends TransportCommand<CloneCommand, Git> {
 			DirCache dc = clonedRepo.lockDirCache();
 			DirCacheCheckout co = new DirCacheCheckout(clonedRepo, dc,
 					commit.getTree());
+			co.setProgressMonitor(monitor);
 			co.checkout();
 			if (cloneSubmodules)
 				cloneSubmodules(clonedRepo);
