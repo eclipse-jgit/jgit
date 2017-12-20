89dc044178e0891ee0c919a436e51e32e1b58541
Stephen Lawson <slawson@ptc.com>
2017-12-20 08:48:49 +0000

    Applies "sparse-checkout" rules on a checkout.
    Adds support for sparse checkout. During checkout the file
    "sparse-checkout" is honoured as described in:
    https://git-scm.com/docs/git-read-tree
    
    To verify this change:
    - Create a local repository with file 'A.txt' and file 'B.txt'.
    - Stage and Commit these files to the repository.
    - Add an empty file called 'sparse-checkout' to your $GIT_DIR/info/
    directory.
    - Open the sparse-checkout file in a text editor and add enter 'A.txt'
    (without quotes) on the first line. Save the file.
    - Set your repository config to 'core.sparseCheckout=true'.
    - Perform a hard reset to 'master'.
    - Only file 'A' should exist in $GIT_DIR.
    
    Bug: 383772
    Change-Id: I5ec95c3c20c34caa2fc8b04742843392841fd6d5
    Signed-off-by: Stephen Lawson <slawson@ptc.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -817,4 +817,5 @@ public class JGitText extends TranslationBundle {
 	/***/ public String years0MonthsAgo;
 	/***/ public String yearsAgo;
 	/***/ public String yearsMonthsAgo;
+	/***/ public String indexEntryNotExtendable;
 }
