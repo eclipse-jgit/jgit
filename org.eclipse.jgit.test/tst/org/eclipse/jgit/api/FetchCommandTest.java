cdc8d714b2d4cfef0f15266040ad6bab7994f4bb
Marc Strapetz <marc.strapetz@syntevo.com>
2017-12-30 11:33:41 +0100

    Fetch(Process): should tolerate duplicate refspecs
    Bug: 529314
    Change-Id: I91eaeda8a988d4786908fba6de00478cfc47a2a2
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/FetchCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/FetchCommandTest.java
@@ -56,6 +56,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.FetchResult;
+import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.TrackingRefUpdate;
@@ -221,5 +222,30 @@ public class FetchCommandTest extends RepositoryTestCase {
 				.getTrackingRefUpdate(Constants.R_TAGS + tagName);
 		assertEquals(RefUpdate.Result.FORCED, update.getResult());
 		assertEquals(tagRef2.getObjectId(), db.resolve(tagName));
+	}
+
+	@Test
+	public void fetchAddAndDeleteBranchesWithDuplicateRefspec()
+			throws Exception {
+		final String branchName = "branch";
+		final String remoteBranchName = "test/" + branchName;
+		remoteGit.commit().setMessage("commit").call();
+		Ref branchRef = remoteGit.branchCreate().setName(branchName).call();
+
+		final String spec1 = "+refs/heads/*:refs/remotes/test/*";
+		final String spec2 = "refs/heads/*:refs/remotes/test/*";
+		final StoredConfig config = db.getConfig();
+		RemoteConfig remoteConfig = new RemoteConfig(config, "test");
+		remoteConfig.addFetchRefSpec(new RefSpec(spec1));
+		remoteConfig.addFetchRefSpec(new RefSpec(spec2));
+		remoteConfig.update(config);
+
+		git.fetch().setRemote("test").setRefSpecs(spec1).call();
+		assertEquals(branchRef.getObjectId(), db.resolve(remoteBranchName));
+
+		remoteGit.branchDelete().setBranchNames(branchName).call();
+		git.fetch().setRemote("test").setRefSpecs(spec1)
+				.setRemoveDeletedRefs(true).call();
+		assertEquals(null, db.resolve(branchName));
 	}
 }
