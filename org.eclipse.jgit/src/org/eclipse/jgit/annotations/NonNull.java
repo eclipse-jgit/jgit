55eba8d0f55464ca84d676828f67a6fe14b2454d
David Pursehouse <david.pursehouse@gmail.com>
2018-01-18 13:20:49 +0900

    NonNull: Switch to TYPE_USE
    Since JGit now requires Java 8, we can switch to TYPE_USE instead
    of explicitly specifying the target type.
    
    Change-Id: I373d47c3d92507459685789df1fad0933d5625ff
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/annotations/NonNull.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/annotations/NonNull.java
@@ -43,12 +43,8 @@
 
 package org.eclipse.jgit.annotations;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -63,7 +59,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.CLASS)
-@Target({ FIELD, METHOD, PARAMETER, LOCAL_VARIABLE })
+@Target(ElementType.TYPE_USE)
 public @interface NonNull {
 	// marker annotation with no members
 }
