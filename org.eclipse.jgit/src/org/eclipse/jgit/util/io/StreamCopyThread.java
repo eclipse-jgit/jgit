480ac053f354b5f96d70ef209fcc178e4337957f
Carsten Pfeiffer <carsten.pfeiffer@gebit.de>
2018-01-22 13:46:59 +0100

    Kill the external process when we're done instead of waiting forever
    BUG: 529463
    Change-Id: Iaf6fe20a1c759ac5e91a2393d7bc40a94f859e84
    Signed-off-by: Carsten Pfeiffer <carsten.pfeiffer@gebit.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/io/StreamCopyThread.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/io/StreamCopyThread.java
@@ -54,6 +54,8 @@ import java.io.OutputStream;
 public class StreamCopyThread extends Thread {
 	private static final int BUFFER_SIZE = 1024;
 
+	private final Process process;
+
 	private final InputStream src;
 
 	private final OutputStream dst;
@@ -66,6 +68,8 @@ public class StreamCopyThread extends Thread {
 	/**
 	 * Create a thread to copy data from an input stream to an output stream.
 	 *
+	 * @param p
+	 *            process being monitored to be able to close it.
 	 * @param i
 	 *            stream to copy from. The thread terminates when this stream
 	 *            reaches EOF. The thread closes this stream before it exits.
@@ -73,8 +77,10 @@ public class StreamCopyThread extends Thread {
 	 *            stream to copy into. The destination stream is automatically
 	 *            closed when the thread terminates.
 	 */
-	public StreamCopyThread(final InputStream i, final OutputStream o) {
+	public StreamCopyThread(final Process p, final InputStream i,
+			final OutputStream o) {
 		setName(Thread.currentThread().getName() + "-StreamCopy"); //$NON-NLS-1$
+		process = p;
 		src = i;
 		dst = o;
 		writeLock = new Object();
@@ -109,6 +115,7 @@ public class StreamCopyThread extends Thread {
 			if (isAlive()) {
 				done = true;
 				interrupt();
+				process.destroy();
 			} else
 				break;
 		}
