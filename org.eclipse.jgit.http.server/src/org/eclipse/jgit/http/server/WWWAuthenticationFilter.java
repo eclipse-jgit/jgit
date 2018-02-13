b8a97b790c5de68e6d2d17bf096b0d510055fa47
Hossein Pursultani <hossein@freshbits.io>
2018-02-13 12:31:30 +1100

    Fix missing WWW-Authenticate header
    This change-set includes resolution and fix for missing WWW-Authenticate
    header.
    
    SmartClientSmartServerTest is updated to assert the missing header when
    the status code is 401. As a result, `testPush_NotAuthorized` test fails.
    
    To fix the issue,  WWWAuthenticationFilter is added to the chain to
    ensure that the header is included in response when the status code is
    401.
    
    Also, to set the realm name, an additional init parameter is added to
    GitServlet,
    
    Bug: 531075
    Change-Id: I5b6d802ecfbc2077be7f52a18b9ab7ea92664fa0
    Signed-off-by: Hossein Pursultani <hossein@freshbits.io>

---
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/WWWAuthenticationFilter.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2009-2010, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.eclipse.jgit.util.HttpSupport.HDR_WWW_AUTHENTICATE;
+
+/**
+ * In case of HTTP 401 status, ensures WWW-Authentication header is included
+ * in response header.
+ *
+ * @since 4.11
+ */
+public class WWWAuthenticationFilter implements Filter {
+
+    private String realmName;
+
+    public WWWAuthenticationFilter(String realmName) {
+        this.realmName = realmName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        // Do nothing.
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void destroy() {
+        // Do nothing.
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            HttpServletResponse rsp = (HttpServletResponse) response;
+            if (rsp.getStatus() == SC_UNAUTHORIZED && rsp.getHeader(HDR_WWW_AUTHENTICATE) == null) {
+                rsp.setHeader(HDR_WWW_AUTHENTICATE, String.format("Basic realm=\"%s\"", realmName));
+            }
+        }
+    }
+
+
+}
