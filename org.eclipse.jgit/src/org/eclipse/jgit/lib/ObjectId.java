7323c3cd178a16e479a6a1bdb20f18e1211aaa95
David Pursehouse <david.pursehouse@gmail.com>
2018-02-21 11:40:58 +0900

    ObjectId: Remove unused readObject/writeObject methods
    Change-Id: Ie2e6ebbeb093bcb5eaa5c49e83ec01919b9c636f
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectId.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectId.java
@@ -294,20 +294,4 @@ public class ObjectId extends AnyObjectId implements Serializable {
 	public ObjectId toObjectId() {
 		return this;
 	}
-
-	private void writeObject(ObjectOutputStream os) throws IOException {
-		os.writeInt(w1);
-		os.writeInt(w2);
-		os.writeInt(w3);
-		os.writeInt(w4);
-		os.writeInt(w5);
-	}
-
-	private void readObject(ObjectInputStream ois) throws IOException {
-		w1 = ois.readInt();
-		w2 = ois.readInt();
-		w3 = ois.readInt();
-		w4 = ois.readInt();
-		w5 = ois.readInt();
-	}
 }
