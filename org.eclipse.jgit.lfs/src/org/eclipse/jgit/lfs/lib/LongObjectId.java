06740fd01a726d9aa6d837d6ff18a70d46cc81d2
David Pursehouse <david.pursehouse@gmail.com>
2018-02-21 11:41:12 +0900

    LongObjectId: Remove unused readObject/writeObject methods
    Change-Id: I861bf67b2f532c76e87f0d6af8d60c26b744c967
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/lib/LongObjectId.java
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/lib/LongObjectId.java
@@ -292,18 +292,4 @@ public class LongObjectId extends AnyLongObjectId implements Serializable {
 	public LongObjectId toObjectId() {
 		return this;
 	}
-
-	private void writeObject(ObjectOutputStream os) throws IOException {
-		os.writeLong(w1);
-		os.writeLong(w2);
-		os.writeLong(w3);
-		os.writeLong(w4);
-	}
-
-	private void readObject(ObjectInputStream ois) throws IOException {
-		w1 = ois.readLong();
-		w2 = ois.readLong();
-		w3 = ois.readLong();
-		w4 = ois.readLong();
-	}
 }
