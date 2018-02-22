ee487e354f7ac986f52e8881083ea091fbca95ed
Jonathan Tan <jonathantanmy@google.com>
2018-02-22 10:24:19 -0800

    Implement protocol v2, ls-refs in UploadPack
    Implement support for protocol v2 of the fetch-pack/upload-pack
    protocol, including the "ls-refs" command and its "symrefs" and "peel"
    parameters. This protocol is described in the Git project in
    "Documentation/technical/protocol-v2.txt" (as of the time of writing, in
    the bw/protocol-v2 branch).
    
    This patch only incorporates support for protocol v2 for users that use
    UploadPack#upload (that is, with a bidirectional pipe). Support for
    users that use UploadPack through other methods (for example,
    UploadPack#sendAdvertisedRefs with a non-bidirectional pipe) will come
    in a subsequent patch.
    
    This patch also includes modifications to PacketLineIn and PacketLineOut
    to support DELIM pkt-lines, which are required by protocol v2.
    
    Change-Id: I8abc6bcc6ed4a88c165677ff1245625aca01267b
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
@@ -1,9 +1,13 @@
 package org.eclipse.jgit.transport;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collections;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import org.eclipse.jgit.errors.TransportException;
 import org.eclipse.jgit.internal.storage.dfs.DfsGarbageCollector;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
@@ -14,6 +18,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevBlob;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.transport.UploadPack.RequestPolicy;
 import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException;
 import org.eclipse.jgit.transport.resolver.ServiceNotEnabledException;
@@ -315,5 +320,135 @@ public class UploadPackTest {
 			tn.fetch(NullProgressMonitor.INSTANCE,
 					Collections.singletonList(new RefSpec(commit.name())));
 		}
+	}
+
+	private static ByteArrayInputStream send(String... lines) throws Exception {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		PacketLineOut pckOut = new PacketLineOut(os);
+		for (String line : lines) {
+			if (line == PacketLineIn.END) {
+				pckOut.end();
+			} else if (line == PacketLineIn.DELIM) {
+				pckOut.writeDelim();
+			} else {
+				pckOut.writeString(line);
+			}
+		}
+		byte[] a = os.toByteArray();
+		return new ByteArrayInputStream(a);
+	}
+
+	/*
+	 * Invokes UploadPack with protocol v2 and sends it the given lines.
+	 * Returns UploadPack's output stream, not including the capability
+	 * advertisement by the server.
+	 */
+	private ByteArrayInputStream uploadPackV2(String... inputLines) throws Exception {
+		ByteArrayOutputStream send = new ByteArrayOutputStream();
+		PacketLineOut pckOut = new PacketLineOut(send);
+		for (String line : inputLines) {
+			if (line == PacketLineIn.END) {
+				pckOut.end();
+			} else if (line == PacketLineIn.DELIM) {
+				pckOut.writeDelim();
+			} else {
+				pckOut.writeString(line);
+			}
+		}
+
+		UploadPack up = new UploadPack(server);
+		up.setUseProtocolV2(true);
+
+		ByteArrayOutputStream recv = new ByteArrayOutputStream();
+		up.upload(new ByteArrayInputStream(send.toByteArray()), recv, null);
+
+		ByteArrayInputStream recvStream = new ByteArrayInputStream(recv.toByteArray());
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		// capability advertisement (always sent)
+		assertThat(pckIn.readString(), is("version 2"));
+		assertThat(pckIn.readString(), is("ls-refs"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+		return recvStream;
+	}
+
+	@Test
+	public void testV2LsRefs() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		server.updateRef("HEAD").link("refs/heads/master");
+		RevTag tag = remote.tag("tag", tip);
+		remote.update("refs/tags/tag", tag);
+
+		ByteArrayInputStream recvStream = uploadPackV2("command=ls-refs\n", PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " HEAD"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(pckIn.readString(), is(tag.toObjectId().getName() + " refs/tags/tag"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+	}
+
+	@Test
+	public void testV2LsRefsSymrefs() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		server.updateRef("HEAD").link("refs/heads/master");
+		RevTag tag = remote.tag("tag", tip);
+		remote.update("refs/tags/tag", tag);
+
+		ByteArrayInputStream recvStream = uploadPackV2("command=ls-refs\n", PacketLineIn.DELIM, "symrefs", PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " HEAD symref-target:refs/heads/master"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(pckIn.readString(), is(tag.toObjectId().getName() + " refs/tags/tag"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+	}
+
+	@Test
+	public void testV2LsRefsPeel() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		server.updateRef("HEAD").link("refs/heads/master");
+		RevTag tag = remote.tag("tag", tip);
+		remote.update("refs/tags/tag", tag);
+
+		ByteArrayInputStream recvStream = uploadPackV2("command=ls-refs\n", PacketLineIn.DELIM, "peel", PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " HEAD"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(
+			pckIn.readString(),
+			is(tag.toObjectId().getName() + " refs/tags/tag peeled:"
+				+ tip.toObjectId().getName()));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+	}
+
+	@Test
+	public void testV2LsRefsMultipleCommands() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		server.updateRef("HEAD").link("refs/heads/master");
+		RevTag tag = remote.tag("tag", tip);
+		remote.update("refs/tags/tag", tag);
+
+		ByteArrayInputStream recvStream = uploadPackV2(
+			"command=ls-refs\n", PacketLineIn.DELIM, "symrefs", "peel", PacketLineIn.END,
+			"command=ls-refs\n", PacketLineIn.DELIM, PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " HEAD symref-target:refs/heads/master"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(
+			pckIn.readString(),
+			is(tag.toObjectId().getName() + " refs/tags/tag peeled:"
+				+ tip.toObjectId().getName()));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " HEAD"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(pckIn.readString(), is(tag.toObjectId().getName() + " refs/tags/tag"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
 	}
 }
