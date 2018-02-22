8917f1728cc5a50e742fd221292ed2ecde70ca09
Jonathan Tan <jonathantanmy@google.com>
2018-02-22 13:58:29 -0800

    Teach UploadPack "ref-prefix" in "ls-refs"
    Add support for the "ref-prefix" parameter in the "ls-refs" command in
    the fetch-pack/upload-pack protocol v2.
    
    Change-Id: If9cf93b2646f75d50a11b5f482594f014d59a836
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
@@ -461,4 +461,43 @@ public class UploadPackTest {
 		assertThat(pckIn.readString(), is(tag.toObjectId().getName() + " refs/tags/tag"));
 		assertTrue(pckIn.readString() == PacketLineIn.END);
 	}
+
+	@Test
+	public void testV2LsRefsRefPrefix() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		remote.update("other", tip);
+		remote.update("yetAnother", tip);
+
+		ByteArrayInputStream recvStream = uploadPackV2(
+			"command=ls-refs\n",
+			PacketLineIn.DELIM,
+			"ref-prefix refs/heads/maste",
+			"ref-prefix refs/heads/other",
+			PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/other"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+	}
+
+	@Test
+	public void testV2LsRefsRefPrefixNoSlash() throws Exception {
+		RevCommit tip = remote.commit().message("message").create();
+		remote.update("master", tip);
+		remote.update("other", tip);
+
+		ByteArrayInputStream recvStream = uploadPackV2(
+			"command=ls-refs\n",
+			PacketLineIn.DELIM,
+			"ref-prefix refs/heads/maste",
+			"ref-prefix r",
+			PacketLineIn.END);
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/master"));
+		assertThat(pckIn.readString(), is(tip.toObjectId().getName() + " refs/heads/other"));
+		assertTrue(pckIn.readString() == PacketLineIn.END);
+	}
 }
