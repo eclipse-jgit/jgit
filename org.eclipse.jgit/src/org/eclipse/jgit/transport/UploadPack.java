638b75c3e251d1c6e1a78c059a184748675b5a2c
Matthias Sohn <matthias.sohn@sap.com>
2018-03-10 00:31:07 +0100

    Remove deprecated Statistics, UploadPackLogger, UploadPackLoggerChain
    Use PackStatistics and PostUploadHook and PostUploadHookChain instead.
    Also remove
    - UploadPack#getPackStatistics replaced by #getStatistics
    - UploadPack#getLogger and UploadPack#setLogger
    
    Change-Id: I70881c539af3094d68d594f19983dea0973604e8
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -317,9 +317,6 @@ public class UploadPack {
 
 	private PackStatistics statistics;
 
-	@SuppressWarnings("deprecation")
-	private UploadPackLogger logger = UploadPackLogger.NULL;
-
 	/**
 	 * Create a new pack upload for an open repository.
 	 *
@@ -639,29 +636,6 @@ public class UploadPack {
 	}
 
 	/**
-	 * Get the configured logger.
-	 *
-	 * @return the configured logger.
-	 * @deprecated Use {@link #getPreUploadHook()}.
-	 */
-	@Deprecated
-	public UploadPackLogger getLogger() {
-		return logger;
-	}
-
-	/**
-	 * Set the logger.
-	 *
-	 * @param logger
-	 *            the logger instance. If null, no logging occurs.
-	 * @deprecated Use {@link #setPreUploadHook(PreUploadHook)}.
-	 */
-	@Deprecated
-	public void setLogger(UploadPackLogger logger) {
-		this.logger = logger;
-	}
-
-	/**
 	 * Check whether the client expects a side-band stream.
 	 *
 	 * @return true if the client has advertised a side-band capability, false
@@ -734,21 +708,6 @@ public class UploadPack {
 				}
 			}
 		}
-	}
-
-	/**
-	 * Get the PackWriter's statistics if a pack was sent to the client.
-	 *
-	 * @return statistics about pack output, if a pack was sent. Null if no pack
-	 *         was sent, such as during the negotiation phase of a smart HTTP
-	 *         connection, or if the client was already up-to-date.
-	 * @since 3.0
-	 * @deprecated Use {@link #getStatistics()}.
-	 */
-	@Deprecated
-	public PackWriter.Statistics getPackStatistics() {
-		return statistics == null ? null
-				: new PackWriter.Statistics(statistics);
 	}
 
 	/**
@@ -1669,7 +1628,6 @@ public class UploadPack {
 			statistics = pw.getStatistics();
 			if (statistics != null) {
 				postUploadHook.onPostUpload(statistics);
-				logger.onPackStatistics(new PackWriter.Statistics(statistics));
 			}
 			pw.close();
 		}
