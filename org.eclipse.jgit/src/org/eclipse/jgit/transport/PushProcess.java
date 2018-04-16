cbb2e65dbcf4c4cf9751184f07d5bc4e58bf17e5
David Pursehouse <david.pursehouse@gmail.com>
2018-04-16 16:51:32 +0200

    PushProcess: Remove unused import of HashMap
    Bug: 533621
    Change-Id: Ie125818d5fc8fef5800108db2ca21151d8d3bfa3
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PushProcess.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PushProcess.java
@@ -48,7 +48,6 @@ import java.io.OutputStream;
 import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
