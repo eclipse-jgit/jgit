2c226550ea618bc9aef813e888e8b2d8da56df43
Jonathan Tan <jonathantanmy@google.com>
2018-05-02 17:17:04 -0700

    Disallow unknown args to "fetch" in protocol v2
    The fetch command of protocol v2 tolerates unknown arguments, but not
    ls-refs. Tighten fetch to not allow such arguments, and add tests to
    verify this for both ls-refs and fetch.
    
    Change-Id: I321161d568bd638252fab1a47b06b924d472a669
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -980,8 +980,10 @@ public class UploadPack {
 							MessageFormat.format(JGitText.get().invalidDepth,
 									Integer.valueOf(depth)));
 				}
+			} else {
+				throw new PackProtocolException(MessageFormat
+						.format(JGitText.get().unexpectedPacketLine, line));
 			}
-			// else ignore it
 		}
 		rawOut.stopBuffering();
 
