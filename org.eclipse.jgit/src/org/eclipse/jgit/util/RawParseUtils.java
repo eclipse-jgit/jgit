a086fe705c848a5e444fbf6ed1dd197d479e2a22
Han-Wen Nienhuys <hanwen@google.com>
2018-05-14 17:21:39 +0200

    Fixup javadoc and formatting in RawText and RawParseUtils
    Change-Id: I9d6002941a33ec204d29e4fd920dde965387bb24
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/util/RawParseUtils.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/RawParseUtils.java
@@ -648,19 +648,24 @@ public final class RawParseUtils {
 	}
 
 	/**
-	 * Like {@link #lineMap(byte[], int, int)} but throw {@link BinaryBlobException} if a null char
+	 * Like {@link #lineMap(byte[], int, int)} but throw {@link BinaryBlobException} if a NUL char
 	 * is encountered.
-	 * @param buf  buffer to scan.
-	 * @param ptr position within the buffer corresponding to the first byte of
+	 *
+	 * @param buf
+	 *            buffer to scan.
+	 * @param ptr
+	 *            position within the buffer corresponding to the first byte of
 	 *            line 1.
-	 * @param end  1 past the end of the content within <code>buf</code>.
+	 * @param end
+	 *            1 past the end of the content within <code>buf</code>.
 	 * @return a line map indexing the start position of each line, or a map representing the entire
 	 *            array as a single line if a '\0' is found.
-	 * @throws BinaryBlobException
+	 * @throws BinaryBlobException if a NUL char is found. The exception is intended for control flow,
+	 *            and lacks a stacktrace.
 	 *
 	 * @since 5.0
 	 */
-	public static final IntList lineMapOrBinary(final byte[] buf, int ptr, int end)
+	public static final IntList lineMapOrBinary(byte[] buf, int ptr, int end)
 			throws BinaryBlobException {
 		IntList map = lineMapOrNull(buf, ptr, end);
 		if (map == null) {
