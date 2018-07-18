33ac33ae4261e60a79af55ca3a4c58319c33d40a
Andre Bossert <anb0s@anbos.de>
2018-07-18 19:52:10 +0200

    Use constants for Git repository folders and files
    - added new constants that are used together with $GIT_DIR returned by
    method getDirectory()
    - added new constants related to new variable $GIT_COMMON_DIR needed for
    worktree support
    - added description to constants related to $GIT_DIR and $GIT_COMMON_DIR
    - replaced some "magic" strings by constants if used with method
    getDirectory()
    - reformated Constants.java for better readability and consistency with
    guidelines.
    
    see:
    https://git-scm.com/docs/gitrepository-layout
    https://git-scm.com/docs/git
    
    This prepares for git worktree support with JGit change
    "https://git.eclipse.org/r/#/c/67873/"
    
    Bug: 477475
    Change-Id: I7662957f3840446873d4c01c5ef87e251d5d26ed
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
@@ -49,8 +49,10 @@ package org.eclipse.jgit.internal.storage.file;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.eclipse.jgit.lib.Constants.HEAD;
 import static org.eclipse.jgit.lib.Constants.LOGS;
+import static org.eclipse.jgit.lib.Constants.LOGS_REFS;
 import static org.eclipse.jgit.lib.Constants.OBJECT_ID_STRING_LENGTH;
 import static org.eclipse.jgit.lib.Constants.PACKED_REFS;
+import static org.eclipse.jgit.lib.Constants.REFS;
 import static org.eclipse.jgit.lib.Constants.R_HEADS;
 import static org.eclipse.jgit.lib.Constants.R_REFS;
 import static org.eclipse.jgit.lib.Constants.R_TAGS;
@@ -214,9 +216,9 @@ public class RefDirectory extends RefDatabase {
 		final FS fs = db.getFS();
 		parent = db;
 		gitDir = db.getDirectory();
-		refsDir = fs.resolve(gitDir, R_REFS);
+		refsDir = fs.resolve(gitDir, REFS);
 		logsDir = fs.resolve(gitDir, LOGS);
-		logsRefsDir = fs.resolve(gitDir, LOGS + '/' + R_REFS);
+		logsRefsDir = fs.resolve(gitDir, LOGS_REFS);
 		packedRefsFile = fs.resolve(gitDir, PACKED_REFS);
 
 		looseRefs.set(RefList.<LooseRef> emptyList());
