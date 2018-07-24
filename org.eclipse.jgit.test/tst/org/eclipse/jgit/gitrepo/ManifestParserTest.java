a6757915a948ecb7b176c42af3cf5b878c9d7ca1
Masaya Suzuki <masayasuzuki@google.com>
2018-07-24 12:22:11 -0700

    Support remove-project node in the manifest parser
    This directive removes the projects added by other directives.
    
    https://gerrit.googlesource.com/git-repo/+/65b0ba5aa0447f7ee25103828115662b1eb80ff9/manifest_xml.py#614
    
    Change-Id: I90d7b9fc711b586dec05f475422d34d67d909986
    Signed-off-by: Masaya Suzuki <masayasuzuki@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/ManifestParserTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/ManifestParserTest.java
@@ -145,6 +145,37 @@ public class ManifestParserTest {
 		}
 	}
 
+	@Test
+	public void testRemoveProject() throws Exception {
+		StringBuilder xmlContent = new StringBuilder();
+		xmlContent.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+				.append("<manifest>")
+				.append("<remote name=\"remote1\" fetch=\".\" />")
+				.append("<default revision=\"master\" remote=\"remote1\" />")
+				.append("<project path=\"foo\" name=\"foo\" />")
+				.append("<project path=\"bar\" name=\"bar\" />")
+				.append("<remove-project name=\"foo\" />")
+				.append("<project path=\"foo\" name=\"baz\" />")
+				.append("</manifest>");
+
+		ManifestParser parser = new ManifestParser(null, null, "master",
+				"https://git.google.com/", null, null);
+		parser.read(new ByteArrayInputStream(
+				xmlContent.toString().getBytes(CHARSET)));
+
+		Set<String> results = new HashSet<>();
+		results.add("bar");
+		results.add("baz");
+		for (RepoProject proj : parser.getProjects()) {
+			assertTrue("Unexpected project is included: " + proj.getName(),
+					results.contains(proj.getName()));
+			results.remove(proj.getName());
+		}
+		assertTrue(
+				"Expected projects are not included",
+				results.isEmpty());
+	}
+
 	void testNormalize(String in, String want) {
 		URI got = ManifestParser.normalizeEmptyPath(URI.create(in));
 		if (!got.toString().equals(want)) {
