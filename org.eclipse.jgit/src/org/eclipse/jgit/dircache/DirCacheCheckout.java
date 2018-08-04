d2e49bc55198483f6b4107b66d88ae126f8bd9f5
René Scheibe <rene.scheibe@gmail.com>
2018-08-04 13:19:17 +0200

    Fix "reset -hard" bug that folders could not be deleted
    The deleted code is not required as removed files are deleted correctly in
    doCheckout() anyway.
    
    The deleted code failed in case a non-empty directory had to be deleted.
    file.delete() returned false, triggering an exception.
    
    Bug: 479266
    Change-Id: I011bb3882ff0c35b238aa3eccad7889041210277
    Signed-off-by: René Scheibe <rene.scheibe@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
@@ -1298,14 +1298,6 @@ public class DirCacheCheckout {
 						JGitText.get().cannotDeleteFile, c));
 			removeEmptyParents(conflict);
 		}
-		for (String r : removed) {
-			File file = new File(repo.getWorkTree(), r);
-			if (!file.delete())
-				throw new CheckoutConflictException(
-						MessageFormat.format(JGitText.get().cannotDeleteFile,
-								file.getAbsolutePath()));
-			removeEmptyParents(file);
-		}
 	}
 
 	/**
