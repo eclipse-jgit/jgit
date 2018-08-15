289f116dbb54880a2829698aa45046975f3a7bea
Dave Borowitz <dborowitz@google.com>
2018-08-15 15:42:19 -0700

    Update javadoc for EMPTY_BLOB_ID
    Found original version with:
    
      $ git describe --contains $(git blame fc4d4b5340911989e444b150ae6a859f486feba9 org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java | grep EMPTY_BLOB_ID | cut -d' ' -f1)
      v0.9.1~164
    
    Change-Id: I1f86bde42615c8b64064894bde44f82660b38d0f

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
@@ -687,7 +687,11 @@ public final class Constants {
 	 */
 	public static final String COMMIT_EDITMSG = "COMMIT_EDITMSG";
 
-	/** objectid for the empty blob */
+	/**
+	 * Well-known object ID for the empty blob.
+	 *
+	 * @since 0.9.1
+	 */
 	public static final ObjectId EMPTY_BLOB_ID = ObjectId
 			.fromString("e69de29bb2d1d6434b8b29ae775ad8c2e48c5391");
 
