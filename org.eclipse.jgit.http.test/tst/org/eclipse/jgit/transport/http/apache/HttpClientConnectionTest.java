c5eb8602b77642b808e1c2a5429a9f76acd0f38e
David Pursehouse <david.pursehouse@gmail.com>
2018-08-27 08:43:27 +0900

    HttpClientConnectionTest: Reformat with the Eclipse style
    Change-Id: I1066dea0bbb4573aabaf417365fb46772f0f4d0e
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.http.test/tst/org/eclipse/jgit/transport/http/apache/HttpClientConnectionTest.java
+++ org.eclipse.jgit.http.test/tst/org/eclipse/jgit/transport/http/apache/HttpClientConnectionTest.java
@@ -57,76 +57,81 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HttpClientConnectionTest {
-    @Test
-    public void testGetHeaderFieldsAllowMultipleValues() throws MalformedURLException {
-        HttpResponse responseMock = new HttpResponseMock();
-        String headerField = "WWW-Authenticate";
-        responseMock.addHeader(headerField, "Basic");
-        responseMock.addHeader(headerField, "Digest");
-        responseMock.addHeader(headerField, "NTLM");
-        HttpClientConnection connection = new HttpClientConnection("http://0.0.0.0/");
-        connection.resp = responseMock;
-        List<String> headerValues = connection.getHeaderFields().get(headerField);
-        assertEquals(3, headerValues.size());
-        assertTrue(headerValues.contains("Basic"));
-        assertTrue(headerValues.contains("Digest"));
-        assertTrue(headerValues.contains("NTLM"));
-    }
+	@Test
+	public void testGetHeaderFieldsAllowMultipleValues()
+			throws MalformedURLException {
+		HttpResponse responseMock = new HttpResponseMock();
+		String headerField = "WWW-Authenticate";
+		responseMock.addHeader(headerField, "Basic");
+		responseMock.addHeader(headerField, "Digest");
+		responseMock.addHeader(headerField, "NTLM");
+		HttpClientConnection connection = new HttpClientConnection(
+				"http://0.0.0.0/");
+		connection.resp = responseMock;
+		List<String> headerValues = connection.getHeaderFields()
+				.get(headerField);
+		assertEquals(3, headerValues.size());
+		assertTrue(headerValues.contains("Basic"));
+		assertTrue(headerValues.contains("Digest"));
+		assertTrue(headerValues.contains("NTLM"));
+	}
 
-    private class HttpResponseMock extends AbstractHttpMessage implements HttpResponse {
-        @Override
-        public StatusLine getStatusLine() {
-            return null;
-        }
+	private class HttpResponseMock extends AbstractHttpMessage
+			implements HttpResponse {
+		@Override
+		public StatusLine getStatusLine() {
+			return null;
+		}
 
-        @Override
-        public void setStatusLine(StatusLine statusLine) {
+		@Override
+		public void setStatusLine(StatusLine statusLine) {
 
-        }
+		}
 
-        @Override
-        public void setStatusLine(ProtocolVersion protocolVersion, int i) {
+		@Override
+		public void setStatusLine(ProtocolVersion protocolVersion, int i) {
 
-        }
+		}
 
-        @Override
-        public void setStatusLine(ProtocolVersion protocolVersion, int i, String s) {
+		@Override
+		public void setStatusLine(ProtocolVersion protocolVersion, int i,
+				String s) {
 
-        }
+		}
 
-        @Override
-        public void setStatusCode(int i) throws IllegalStateException {
+		@Override
+		public void setStatusCode(int i) throws IllegalStateException {
 
-        }
+		}
 
-        @Override
-        public void setReasonPhrase(String s) throws IllegalStateException {
+		@Override
+		public void setReasonPhrase(String s) throws IllegalStateException {
 
-        }
+		}
 
-        @Override
-        public HttpEntity getEntity() {
-            return null;
-        }
+		@Override
+		public HttpEntity getEntity() {
+			return null;
+		}
 
-        @Override
-        public void setEntity(HttpEntity httpEntity) {
+		@Override
+		public void setEntity(HttpEntity httpEntity) {
 
-        }
+		}
 
-        @Override
-        public Locale getLocale() {
-            return null;
-        }
+		@Override
+		public Locale getLocale() {
+			return null;
+		}
 
-        @Override
-        public void setLocale(Locale locale) {
+		@Override
+		public void setLocale(Locale locale) {
 
-        }
+		}
 
-        @Override
-        public ProtocolVersion getProtocolVersion() {
-            return null;
-        }
-    }
+		@Override
+		public ProtocolVersion getProtocolVersion() {
+			return null;
+		}
+	}
 }
