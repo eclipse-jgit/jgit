8142828dece3991bd0a811f41acec0ca1c801e63
Thomas Wolf <thomas.wolf@paranor.ch>
2018-09-21 22:43:34 +0200

    Factor out a JSch-independent ssh config parser
    Move the bulk of the basic parsing and host entry handling into a
    new class OpenSshConfigFile that has no dependencies on any concrete
    ssh implementation. Make the existing OpenSshConfig use the new
    parser.
    
    Introduce a new interface SshConstants collecting all the various
    ssh-related string literals. Also use TreeMaps with a case-insensitive
    key comparator instead of converting keys to uppercase. Add a test
    to verify that keys are matched case-insensitively.
    
    Most of the parsing code was simply moved, except that the new
    parser supports looking up entries given host name, port, and user
    name, and can thus handle more %-substitutions correctly. This
    feature is not yet used and cannot be used with JSch since JSch
    only has a ConfigRepository.getConfig(String) interface.
    
    The split is still worth the trouble as it opens the way to using
    another ssh client altogether. Apache MINA sshd, for instance,
    resolves host entries giving host name, port, and user name.
    
    (Apache MINA has a built-in ssh config handling, but that has
    problems, too: its pattern matching is case-insensitive, and its
    merging of host entries if several match is not the same as in
    OpenSsh. But with this refactoring, it will be possible to plug in
    OpenSshConfigFile into an Apache MINA sshd  client without dragging
    along JSch.)
    
    One test case that doesn't make sense anymore has been removed. It
    tested that repeatedly querying for a host entry returned the same
    object. That is no longer true since the caching has been moved to
    a deeper level.
    
    Bug: 520927
    Change-Id: I6381d52b29099595e6eaf8b05c786aeeaefbf9cc
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/OpenSshConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/OpenSshConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, 2017, Google Inc.
+ * Copyright (C) 2008, 2018, Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
