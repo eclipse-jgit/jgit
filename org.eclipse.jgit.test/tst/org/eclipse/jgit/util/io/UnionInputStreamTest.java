74e7fd7fd83761a04a19eab87cd63f96c30a1d32
David Pursehouse <david.pursehouse@gmail.com>
2018-10-12 11:07:48 +0900

    UnionInputStreamTest: Suppress ErrorProne warning about missing synchronized
    Error Prone reports that the unsynchronized method skip overrides the
    synchronized method in ByteArrayInputStream [1].
    
    This is a test class, so we can just suppress the warning as recommended
    in [1].
    
    Note that the suppression causes a warning in Eclipse, because it doesn't
    recognize the "UnsynchronizedOverridesSynchronized" as a valid value for
    the @SuppressWarnings annotation [2].
    
    [1] https://errorprone.info/bugpattern/UnsynchronizedOverridesSynchronized
    [2] https://bugs.eclipse.org/bugs/show_bug.cgi?id=392045
    
    Change-Id: I3e798b448211f1363729091f72fb0ef6a873e599
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/util/io/UnionInputStreamTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/util/io/UnionInputStreamTest.java
@@ -167,6 +167,7 @@ public class UnionInputStreamTest {
 
 		u.add(new ByteArrayInputStream(new byte[] { 20, 30 }) {
 			@Override
+			@SuppressWarnings("UnsynchronizedOverridesSynchronized") // Only used in tests
 			public long skip(long n) {
 				return 0;
 			}
