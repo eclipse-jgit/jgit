6aa1bae4a782ad266823640ca520270208c48e91
Ivan Frade <ifrade@google.com>
2018-10-30 11:51:49 -0700

    RepoCommand.RemoteReader: Add method to read contents and mode of file
    The RepoCommand.RemoteReader interface doesn't offer access to the mode
    of a file. Caller can only default to mark the copied objects as regular
    files, losing e.g. the executable bit (if set).
    
    Add a new method readFileWithMode that returns the contents and mode of
    the remote file. It supersedes the readFile method, that is marked as
    deprecated.
    
    Now callers can set correctly the file mode of the copied file.
    
    Change-Id: I8fce01e4bc5707434c0cbc4aebbae1b6b64756f0
    Signed-off-by: Ivan Frade <ifrade@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
@@ -62,7 +62,8 @@ import java.util.Map;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.gitrepo.RepoCommand.DefaultRemoteReader;
+import org.eclipse.jgit.gitrepo.RepoCommand.RemoteFile;
 import org.eclipse.jgit.junit.JGitTestUtil;
 import org.eclipse.jgit.junit.RepositoryTestCase;
 import org.eclipse.jgit.lib.BlobBasedConfig;
@@ -170,19 +171,10 @@ public class RepoCommandTest extends RepositoryTestCase {
 		}
 
 		@Override
-		public byte[] readFile(String uri, String refName, String path)
-			throws GitAPIException, IOException {
+		public RemoteFile readFileWithMode(String uri, String ref, String path)
+				throws GitAPIException, IOException {
 			Repository repo = uriRepoMap.get(uri);
-
-			String idStr = refName + ":" + path;
-			ObjectId id = repo.resolve(idStr);
-			if (id == null) {
-				throw new RefNotFoundException(
-					String.format("repo %s does not have %s", repo.toString(), idStr));
-			}
-			try (ObjectReader reader = repo.newObjectReader()) {
-				return reader.open(id).getCachedBytes(Integer.MAX_VALUE);
-			}
+			return DefaultRemoteReader.readFileWithMode(repo, ref, path);
 		}
 	}
 
