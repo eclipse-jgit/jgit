6696094093c79cb1c39fec4f017b983a13cbf0b0
Medha Bhargav Prabhala <mprabhala@salesforce.com>
2018-12-03 20:13:27 +0100

    Implement signing commits using BouncyCastle
    Change-Id: I35ee3d4b06d9d479475ab2e51b29bed49661bbdc
    Also-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Signed-off-by: Medha Bhargav Prabhala <mprabhala@salesforce.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -284,6 +284,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String createNewFileFailed;
 	/***/ public String createRequiresZeroOldId;
 	/***/ public String credentialPassword;
+	/***/ public String credentialPassphrase;
 	/***/ public String credentialUsername;
 	/***/ public String daemonAlreadyRunning;
 	/***/ public String daysAgo;
@@ -384,6 +385,13 @@ public class JGitText extends TranslationBundle {
 	/***/ public String gcLogExists;
 	/***/ public String gcTooManyUnpruned;
 	/***/ public String gitmodulesNotFound;
+	/***/ public String gpgFailedToParseSecretKey;
+	/***/ public String gpgNoCredentialsProvider;
+	/***/ public String gpgNoKeyring;
+	/***/ public String gpgNoKeyInLegacySecring;
+	/***/ public String gpgNoPublicKeyFound;
+	/***/ public String gpgNoSecretKeyFound;
+	/***/ public String gpgNoSecretKeyForPublicKey;
 	/***/ public String headRequiredToStash;
 	/***/ public String hoursAgo;
 	/***/ public String httpConfigCannotNormalizeURL;
@@ -849,4 +857,5 @@ public class JGitText extends TranslationBundle {
 	/***/ public String years0MonthsAgo;
 	/***/ public String yearsAgo;
 	/***/ public String yearsMonthsAgo;
+	/***/ public String gpgKeyUrish;
 }
