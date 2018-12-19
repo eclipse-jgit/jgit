50bbcab5e6d5d9b0881f4a544d98bb8b12269929
Wim Jongman <wim.jongman@remainsoftware.com>
2018-12-19 12:57:32 +0100

    Bug 542907 - Add ] character to branch name validator
    * Fixed
    
    Change-Id: I4ad0026b6ebffdbc25bb89319ce1a62021c6f2df
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ValidRefNameTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/ValidRefNameTest.java
@@ -292,6 +292,8 @@ public class ValidRefNameTest {
 		assertNormalized("Bug 12345   -   Hello______ World!",
 				"Bug_12345_Hello_World!");
 		assertNormalized("_Bug 12345 - Hello World!", "Bug_12345_Hello_World!");
+		assertNormalized("[Bug 12345] - Hello World!",
+				"Bug_12345-Hello_World!");
 	}
 
 	@Test
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -1473,6 +1473,7 @@ public abstract class Repository implements AutoCloseable {
 			case '?':
 			case '*':
 			case '[':
+			case ']':
 			case '@':
 			case '<':
 			case '>':
