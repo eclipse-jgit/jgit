006283b65b00c9abfa74ecb561760ea54da19c46
Minh Thai <mthai@google.com>
2019-01-09 12:53:51 -0800

    Lazily open ReadableChannel in BlockBasedFile.getOrLoadBlock
    To avoid opening the readable channel in case of DfsBlockCache
    hits. Also cleaning up typos around DfsBlockCache.
    
    Change-Id: I615e349cb4838387c1e6743cdc384d1b81b54369
    Signed-off-by: Minh Thai <mthai@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/BlockBasedFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/BlockBasedFile.java
@@ -129,8 +129,8 @@ abstract class BlockBasedFile {
 	}
 
 	DfsBlock getOrLoadBlock(long pos, DfsReader ctx) throws IOException {
-		try (ReadableChannel rc = ctx.db.openFile(desc, ext)) {
-			return cache.getOrLoad(this, pos, ctx, () -> rc);
+		try (LazyChannel c = new LazyChannel(ctx)) {
+			return cache.getOrLoad(this, pos, ctx, c);
 		}
 	}
 
@@ -202,5 +202,36 @@ abstract class BlockBasedFile {
 
 	static long elapsedMicros(long start) {
 		return (System.nanoTime() - start) / 1000L;
+	}
+
+	/**
+	 * A supplier of readable channel that opens the channel lazily.
+	 */
+	private class LazyChannel implements AutoCloseable, DfsBlockCache.ReadableChannelSupplier {
+		final DfsReader ctx;
+		ReadableChannel rc = null;
+
+		LazyChannel(DfsReader ctx) {
+			this.ctx = ctx;
+		}
+
+		@Override
+		public ReadableChannel get() throws IOException {
+			if (rc == null) {
+				synchronized (this) {
+					if (rc == null) {
+						rc = ctx.db.openFile(desc, ext);
+					}
+				}
+			}
+			return rc;
+		}
+
+		@Override
+		public void close() throws IOException {
+			if (rc != null) {
+				rc.close();
+			}
+		}
 	}
 }
