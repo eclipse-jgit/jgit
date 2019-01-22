8ef88d5462192ee2a73517d0d81cbdaae5107247
Jonathan Tan <jonathantanmy@google.com>
2019-01-22 13:17:12 -0800

    UploadPackTest: refactor capability config test
    UploadPackTest.java contains tests that check behavior when
    "allowfilter" and "allowrefinwant" are not set, are set, and are not set
    but the client insists on using them anyway. Because another capability
    is to be included in a subsequent patch, refactor the common code in
    these tests.
    
    Remove setBoolean calls with "false", as they are no-ops.
    
    Also take the opportunity to eliminate the overspecification of the
    "fetch=" line returned by the capability advertisement.
    
    Change-Id: I289bbd11c902a513cd8d53bc34767e61ebbd5f17
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
@@ -1,5 +1,6 @@
 package org.eclipse.jgit.transport;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
@@ -509,54 +510,62 @@ public class UploadPackTest {
 		assertTrue(PacketLineIn.isEnd(pckIn.readString()));
 	}
 
+	private void checkAdvertisedIfAllowed(String configSection, String configName,
+			String fetchCapability) throws Exception {
+		server.getConfig().setBoolean(configSection, null, configName, true);
+		ByteArrayInputStream recvStream =
+				uploadPackV2Setup(null, null, null, PacketLineIn.end());
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is("version 2"));
+
+		ArrayList<String> lines = new ArrayList<>();
+		String line;
+		while (!PacketLineIn.isEnd((line = pckIn.readString()))) {
+			if (line.startsWith("fetch=")) {
+				assertThat(
+					Arrays.asList(line.substring(6).split(" ")),
+					containsInAnyOrder(fetchCapability, "shallow"));
+				lines.add("fetch");
+			} else {
+				lines.add(line);
+			}
+		}
+		assertThat(lines, containsInAnyOrder("ls-refs", "fetch", "server-option"));
+	}
+
+	private void checkUnadvertisedIfUnallowed(String fetchCapability) throws Exception {
+		ByteArrayInputStream recvStream =
+				uploadPackV2Setup(null, null, null, PacketLineIn.end());
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+
+		assertThat(pckIn.readString(), is("version 2"));
+
+		ArrayList<String> lines = new ArrayList<>();
+		String line;
+		while (!PacketLineIn.isEnd((line = pckIn.readString()))) {
+			if (line.startsWith("fetch=")) {
+				assertThat(
+					Arrays.asList(line.substring(6).split(" ")),
+					hasItems("shallow"));
+				lines.add("fetch");
+			} else {
+				lines.add(line);
+			}
+		}
+		assertThat(lines, hasItems("ls-refs", "fetch", "server-option"));
+	}
+
 	@Test
 	public void testV2CapabilitiesAllowFilter() throws Exception {
-		server.getConfig().setBoolean("uploadpack", null, "allowfilter", true);
-		ByteArrayInputStream recvStream =
-				uploadPackV2Setup(null, null, null, PacketLineIn.end());
-		PacketLineIn pckIn = new PacketLineIn(recvStream);
-
-		assertThat(pckIn.readString(), is("version 2"));
-		assertThat(
-				Arrays.asList(pckIn.readString(), pckIn.readString(),
-						pckIn.readString()),
-				// TODO(jonathantanmy) This check overspecifies the
-				// order of the capabilities of "fetch".
-				hasItems("ls-refs", "fetch=filter shallow", "server-option"));
-		assertTrue(PacketLineIn.isEnd(pckIn.readString()));
+		checkAdvertisedIfAllowed("uploadpack", "allowfilter", "filter");
+		checkUnadvertisedIfUnallowed("filter");
 	}
 
 	@Test
-	public void testV2CapabilitiesRefInWant() throws Exception {
-		server.getConfig().setBoolean("uploadpack", null, "allowrefinwant", true);
-		ByteArrayInputStream recvStream =
-				uploadPackV2Setup(null, null, null, PacketLineIn.end());
-		PacketLineIn pckIn = new PacketLineIn(recvStream);
-
-		assertThat(pckIn.readString(), is("version 2"));
-		assertThat(
-				Arrays.asList(pckIn.readString(), pckIn.readString(),
-						pckIn.readString()),
-				// TODO(jonathantanmy) This check overspecifies the
-				// order of the capabilities of "fetch".
-				hasItems("ls-refs", "fetch=ref-in-want shallow",
-						"server-option"));
-		assertTrue(PacketLineIn.isEnd(pckIn.readString()));
-	}
-
-	@Test
-	public void testV2CapabilitiesRefInWantNotAdvertisedIfUnallowed() throws Exception {
-		server.getConfig().setBoolean("uploadpack", null, "allowrefinwant", false);
-		ByteArrayInputStream recvStream =
-				uploadPackV2Setup(null, null, null, PacketLineIn.end());
-		PacketLineIn pckIn = new PacketLineIn(recvStream);
-
-		assertThat(pckIn.readString(), is("version 2"));
-		assertThat(
-				Arrays.asList(pckIn.readString(), pckIn.readString(),
-						pckIn.readString()),
-				hasItems("ls-refs", "fetch=shallow", "server-option"));
-		assertTrue(PacketLineIn.isEnd(pckIn.readString()));
+	public void testV2CapabilitiesAllowRefInWant() throws Exception {
+		checkAdvertisedIfAllowed("uploadpack", "allowrefinwant", "ref-in-want");
+		checkUnadvertisedIfUnallowed("ref-in-want");
 	}
 
 	@Test
@@ -1522,361 +1531,34 @@ public class UploadPackTest {
 		assertTrue(client.getObjectDatabase().has(small.toObjectId()));
 	}
 
-	abstract class TreeBuilder {
-		abstract void addElements(DirCacheBuilder dcBuilder) throws Exception;
-
-		RevTree build() throws Exception {
-			DirCache dc = DirCache.newInCore();
-			DirCacheBuilder dcBuilder = dc.builder();
-			addElements(dcBuilder);
-			dcBuilder.finish();
-			ObjectId id;
-			try (ObjectInserter ins =
-					remote.getRepository().newObjectInserter()) {
-				id = dc.writeTree(ins);
-				ins.flush();
-			}
-			return remote.getRevWalk().parseTree(id);
-		}
-	}
-
-	class DeepTreePreparator {
-		RevBlob blobLowDepth = remote.blob("lo");
-		RevBlob blobHighDepth = remote.blob("hi");
-
-		RevTree subtree = remote.tree(remote.file("1", blobHighDepth));
-		RevTree rootTree = (new TreeBuilder() {
-				@Override
-				void addElements(DirCacheBuilder dcBuilder) throws Exception {
-					dcBuilder.add(remote.file("1", blobLowDepth));
-					dcBuilder.addTree(new byte[] {'2'}, DirCacheEntry.STAGE_0,
-							remote.getRevWalk().getObjectReader(), subtree);
-				}
-			}).build();
-		RevCommit commit = remote.commit(rootTree);
-
-		DeepTreePreparator() throws Exception {}
-	}
-
-	private void uploadV2WithTreeDepthFilter(
-			long depth, ObjectId... wants) throws Exception {
-		server.getConfig().setBoolean("uploadpack", null, "allowfilter", true);
-
-		List<String> input = new ArrayList<>();
-		input.add("command=fetch\n");
-		input.add(PacketLineIn.delimiter());
-		for (ObjectId want : wants) {
-			input.add("want " + want.getName() + "\n");
-		}
-		input.add("filter tree:" + depth + "\n");
-		input.add("done\n");
-		input.add(PacketLineIn.end());
-		ByteArrayInputStream recvStream =
-				uploadPackV2(RequestPolicy.ANY, /*refFilter=*/null,
-							 /*hook=*/null, input.toArray(new String[0]));
-		PacketLineIn pckIn = new PacketLineIn(recvStream);
-		assertThat(pckIn.readString(), is("packfile"));
-		parsePack(recvStream);
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth0() throws Exception {
-		DeepTreePreparator preparator = new DeepTreePreparator();
-		remote.update("master", preparator.commit);
-
-		uploadV2WithTreeDepthFilter(0, preparator.commit.toObjectId());
-
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.rootTree.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.subtree.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.blobLowDepth.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.blobHighDepth.toObjectId()));
-		assertEquals(1, stats.getTreesTraversed());
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth1_serverHasBitmap() throws Exception {
-		DeepTreePreparator preparator = new DeepTreePreparator();
-		remote.update("master", preparator.commit);
-
-		// The bitmap should be ignored since we need to track the depth while
-		// traversing the trees.
-		generateBitmaps(server);
-
-		uploadV2WithTreeDepthFilter(1, preparator.commit.toObjectId());
-
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.rootTree.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.subtree.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.blobLowDepth.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.blobHighDepth.toObjectId()));
-		assertEquals(1, stats.getTreesTraversed());
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth2() throws Exception {
-		DeepTreePreparator preparator = new DeepTreePreparator();
-		remote.update("master", preparator.commit);
-
-		uploadV2WithTreeDepthFilter(2, preparator.commit.toObjectId());
-
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.rootTree.toObjectId()));
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.subtree.toObjectId()));
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.blobLowDepth.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.blobHighDepth.toObjectId()));
-		assertEquals(2, stats.getTreesTraversed());
-	}
-
-	/**
-	 * Creates a commit with the following files:
-	 * <pre>
-	 * a/x/b/foo
-	 * x/b/foo
-	 * </pre>
-	 * which has an identical tree in two locations: once at / and once at /a
-	 */
-	class RepeatedSubtreePreparator {
-		RevBlob foo = remote.blob("foo");
-		RevTree subtree3 = remote.tree(remote.file("foo", foo));
-		RevTree subtree2 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'b'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree3);
-			}
-		}).build();
-		RevTree subtree1 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'x'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree2);
-			}
-		}).build();
-		RevTree rootTree = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'a'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree1);
-				dcBuilder.addTree(new byte[] {'x'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree2);
-			}
-		}).build();
-		RevCommit commit = remote.commit(rootTree);
-
-		RepeatedSubtreePreparator() throws Exception {}
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth_iterateOverTreeAtTwoLevels()
+	private void checkV2FetchWhenNotAllowed(String fetchLine, String expectedMessage)
 			throws Exception {
-		// Test tree:<depth> where a tree is iterated to twice - once where a
-		// subentry is too deep to be included, and again where the blob inside
-		// it is shallow enough to be included.
-		RepeatedSubtreePreparator preparator = new RepeatedSubtreePreparator();
-		remote.update("master", preparator.commit);
-
-		uploadV2WithTreeDepthFilter(4, preparator.commit.toObjectId());
-
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.foo.toObjectId()));
-	}
-
-	/**
-	 * Creates a commit with the following files:
-	 * <pre>
-	 * a/x/b/foo
-	 * b/u/c/baz
-	 * y/x/b/foo
-	 * z/v/c/baz
-	 * </pre>
-	 * which has two pairs of identical trees:
-	 * <ul>
-	 * <li>one at /a and /y
-	 * <li>one at /b/u and /z/v
-	 * </ul>
-	 * Note that this class defines unique 8 trees (rootTree and subtree1-7)
-	 * which means PackStatistics should report having visited 8 trees.
-	 */
-	class RepeatedSubtreeAtSameLevelPreparator {
-		RevBlob foo = remote.blob("foo");
-
-		/** foo */
-		RevTree subtree1 = remote.tree(remote.file("foo", foo));
-
-		/** b/foo */
-		RevTree subtree2 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'b'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree1);
-			}
-		}).build();
-
-		/** x/b/foo */
-		RevTree subtree3 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'x'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree2);
-			}
-		}).build();
-
-		RevBlob baz = remote.blob("baz");
-
-		/** baz */
-		RevTree subtree4 = remote.tree(remote.file("baz", baz));
-
-		/** c/baz */
-		RevTree subtree5 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'c'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree4);
-			}
-		}).build();
-
-		/** u/c/baz */
-		RevTree subtree6 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'u'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree5);
-			}
-		}).build();
-
-		/** v/c/baz */
-		RevTree subtree7 = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'v'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree5);
-			}
-		}).build();
-
-		RevTree rootTree = (new TreeBuilder() {
-			@Override
-			void addElements(DirCacheBuilder dcBuilder) throws Exception {
-				dcBuilder.addTree(new byte[] {'a'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree3);
-				dcBuilder.addTree(new byte[] {'b'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree6);
-				dcBuilder.addTree(new byte[] {'y'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree3);
-				dcBuilder.addTree(new byte[] {'z'}, DirCacheEntry.STAGE_0,
-						remote.getRevWalk().getObjectReader(), subtree7);
-			}
-		}).build();
-		RevCommit commit = remote.commit(rootTree);
-
-		RepeatedSubtreeAtSameLevelPreparator() throws Exception {}
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth_repeatTreeAtSameLevelIncludeFile()
-			throws Exception {
-		RepeatedSubtreeAtSameLevelPreparator preparator =
-				new RepeatedSubtreeAtSameLevelPreparator();
-		remote.update("master", preparator.commit);
-
-		uploadV2WithTreeDepthFilter(5, preparator.commit.toObjectId());
-
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.foo.toObjectId()));
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.baz.toObjectId()));
-		assertEquals(8, stats.getTreesTraversed());
-	}
-
-	@Test
-	public void testV2FetchFilterTreeDepth_repeatTreeAtSameLevelExcludeFile()
-			throws Exception {
-		RepeatedSubtreeAtSameLevelPreparator preparator =
-				new RepeatedSubtreeAtSameLevelPreparator();
-		remote.update("master", preparator.commit);
-
-		uploadV2WithTreeDepthFilter(4, preparator.commit.toObjectId());
-
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.foo.toObjectId()));
-		assertFalse(client.getObjectDatabase()
-				.has(preparator.baz.toObjectId()));
-		assertEquals(8, stats.getTreesTraversed());
-	}
-
-	@Test
-	public void testWantFilteredObject() throws Exception {
-		RepeatedSubtreePreparator preparator = new RepeatedSubtreePreparator();
-		remote.update("master", preparator.commit);
-
-		// Specify wanted blob objects that are deep enough to be filtered. We
-		// should still upload them.
-		uploadV2WithTreeDepthFilter(
-				3,
-				preparator.commit.toObjectId(),
-				preparator.foo.toObjectId());
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.foo.toObjectId()));
-
-		client = newRepo("client");
-		// Specify a wanted tree object that is deep enough to be filtered. We
-		// should still upload it.
-		uploadV2WithTreeDepthFilter(
-				2,
-				preparator.commit.toObjectId(),
-				preparator.subtree3.toObjectId());
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.foo.toObjectId()));
-		assertTrue(client.getObjectDatabase()
-				.has(preparator.subtree3.toObjectId()));
-	}
-
-	@Test
-	public void testV2FetchFilterWhenNotAllowed() throws Exception {
 		RevCommit commit = remote.commit().message("0").create();
 		remote.update("master", commit);
 
-		server.getConfig().setBoolean("uploadpack", null, "allowfilter", false);
-
 		thrown.expect(PackProtocolException.class);
-		thrown.expectMessage("unexpected filter blob:limit=5");
+		thrown.expectMessage(expectedMessage);
 		uploadPackV2(
 			"command=fetch\n",
 			PacketLineIn.delimiter(),
 			"want " + commit.toObjectId().getName() + "\n",
-			"filter blob:limit=5\n",
+			fetchLine,
 			"done\n",
 				PacketLineIn.end());
 	}
 
 	@Test
-	public void testV2FetchWantRefIfNotAllowed() throws Exception {
-		RevCommit one = remote.commit().message("1").create();
-		remote.update("one", one);
+	public void testV2FetchFilterWhenNotAllowed() throws Exception {
+		checkV2FetchWhenNotAllowed(
+			"filter blob:limit=5\n",
+			"unexpected filter blob:limit=5");
+	}
 
-		try {
-			uploadPackV2(
-				"command=fetch\n",
-				PacketLineIn.delimiter(),
-				"want-ref refs/heads/one\n",
-				"done\n",
-					PacketLineIn.end());
-		} catch (PackProtocolException e) {
-			assertThat(
-				e.getMessage(),
-				containsString("unexpected want-ref refs/heads/one"));
-			return;
-		}
-		fail("expected PackProtocolException");
+	@Test
+	public void testV2FetchWantRefIfNotAllowed() throws Exception {
+		checkV2FetchWhenNotAllowed(
+			"want-ref refs/heads/one\n",
+			"unexpected want-ref refs/heads/one");
 	}
 
 	@Test
