0e7f900ef0ef09f943810a9454c844e6d5f93635
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:46:05 +0100

    Add config reader for user-defined difftools
    see: http://git-scm.com/docs/git-difftool
    
    * add config reader for user-defined difftools
      * diff.tool
      * diff.guitool
      * difftool.prompt
      * difftool.trustExitCode
      * difftool.<tool>.path
      * difftool.<tool>.cmd
    * add pre-defined difftools
    * implemented "git difftool --tool-help" to verify config reader and
    pre-defined difftools
    
    Bug: 356832
    Change-Id: Idde8fddbef61f3378ee565c6321570b3962d0e1d
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/DiffToolManager.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/DiffToolManager.java
@@ -45,6 +45,7 @@ package org.eclipse.jgit.diffmergetool;
 
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jgit.lib.Repository;
 
@@ -55,6 +56,8 @@ import org.eclipse.jgit.lib.Repository;
  */
 public class DiffToolManager {
 
+	private final DiffToolConfig config;
+
 	private Map<String, IDiffTool> predefinedTools;
 
 	private Map<String, IDiffTool> userDefinedTools;
@@ -63,7 +66,7 @@ public class DiffToolManager {
 	 * @param db the repository database
 	 */
 	public DiffToolManager(Repository db) {
-		// TODO: read the config
+		config = db.getConfig().get(DiffToolConfig.KEY);
 		setupPredefinedTools();
 		setupUserDefinedTools();
 	}
@@ -94,6 +97,13 @@ public class DiffToolManager {
 	}
 
 	/**
+	 * @return the tool names
+	 */
+	public Set<String> getToolNames() {
+		return config.getToolNames();
+	}
+
+	/**
 	 * @return the user defined tools
 	 */
 	public Map<String, IDiffTool> getUserDefinedTools() {
@@ -118,10 +128,32 @@ public class DiffToolManager {
 
 	private void setupPredefinedTools() {
 		predefinedTools = new TreeMap<>();
+		for (PreDefinedDiffTools tool : PreDefinedDiffTools.values()) {
+			predefinedTools
+					.put(tool.name(),
+							new PreDefinedDiffTool(tool.name(), tool.getPath(),
+									tool.getParameters()));
+		}
 	}
 
 	private void setupUserDefinedTools() {
 		userDefinedTools = new TreeMap<>();
+		Map<String, IDiffTool> userTools = config.getTools();
+		for (String name : userTools.keySet()) {
+			IDiffTool userTool = userTools.get(name);
+			// if difftool.<name>.cmd is defined we have user defined tool
+			if (userTool.getCommand() != null) {
+				userDefinedTools.put(name, userTool);
+			} else if (userTool.getPath() != null) {
+				// if difftool.<name>.path is defined we just overload the path
+				// of predefined tool
+				PreDefinedDiffTool predefTool = (PreDefinedDiffTool) predefinedTools
+						.get(name);
+				if (predefTool != null) {
+					predefTool.setPath(userTool.getPath());
+				}
+			}
+		}
 	}
 
 }
