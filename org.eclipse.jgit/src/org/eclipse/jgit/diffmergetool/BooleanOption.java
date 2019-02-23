456d8b3115975836a291ab6ad4b0616c527a54a8
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:25:10 +0100

    Add command line support for "git difftool"
    see: http://git-scm.com/docs/git-difftool
    
    * add command line support for "jgit difftool"
      * show supported commands with "jgit difftool --help"
      * added "git difftool --tool-help" to show the tools (empty now)
      * prepare for all other commands
    
    Bug: 356832
    Change-Id: Ice0c13ef7953a20feaf25e7746d62b94ff4e89e5
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/BooleanOption.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018-2020, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * Optional boolean
+ *
+ * @since 5.7
+ */
+public enum BooleanOption {
+	/**
+	 * the option is not defined and is default false
+	 */
+	NOT_DEFINED_FALSE(),
+
+	/**
+	 * the option is not defined and is default true
+	 */
+	NOT_DEFINED_TRUE(),
+
+	/**
+	 * the option is disabled
+	 */
+	FALSE(),
+
+	/**
+	 * the option is enabled
+	 */
+	TRUE();
+
+	BooleanOption() {
+	}
+
+	/**
+	 * @return boolean value of the option
+	 */
+	public boolean toBoolean() {
+		switch (this) {
+		case TRUE:
+		case NOT_DEFINED_TRUE:
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	/**
+	 * @return true if the the option was defined and false if not
+	 */
+	public boolean isDefined() {
+		switch (this) {
+		case TRUE:
+		case FALSE:
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	/**
+	 * @param value
+	 *            the boolean value as input
+	 * @return the defined boolean option
+	 */
+	public static BooleanOption defined(boolean value) {
+		return value ? BooleanOption.TRUE : BooleanOption.FALSE;
+	}
+
+	/**
+	 * @param value
+	 *            the boolean value as default input
+	 * @return the not defined boolean option
+	 */
+	public static BooleanOption notDefined(boolean value) {
+		return value ? BooleanOption.NOT_DEFINED_TRUE
+				: BooleanOption.NOT_DEFINED_FALSE;
+	}
+
+}
