2b3948d20b04245b92b63ae395b293ed4e775b7b
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:46:05 +0100

    Add config reader for user-defined difftools
    see: http://git-scm.com/docs/git-difftool
    
    * add config reader for user-defined difftools
      * diff.tool
      * diff.guitool
      * difftool.prompt
      * difftool.trustExitCode
      * difftool.<tool>.path
      * difftool.<tool>.cmd
    * add pre-defined command line difftools
    * implemented "git difftool --tool-help" to verify config reader and
    pre-defined difftools
    
    Bug: 356832
    Change-Id: Idde8fddbef61f3378ee565c6321570b3962d0e1d
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/DiffTools.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/DiffTools.java
@@ -13,6 +13,7 @@ package org.eclipse.jgit.diffmergetool;
 
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jgit.lib.Repository;
 
@@ -22,6 +23,8 @@ import org.eclipse.jgit.lib.Repository;
  * @since 5.7
  */
 public class DiffTools {
+
+    private final DiffToolConfig config;
 
 	private Map<String, ExternalDiffTool> predefinedTools;
 
@@ -34,6 +37,7 @@ public class DiffTools {
 	 *            the repository database
 	 */
 	public DiffTools(Repository db) {
+	    config = db.getConfig().get(DiffToolConfig.KEY);
 		setupPredefinedTools();
 		setupUserDefinedTools();
 	}
@@ -64,6 +68,13 @@ public class DiffTools {
 	}
 
 	/**
+	 * @return the tool names
+	 */
+	public Set<String> getToolNames() {
+		return config.getToolNames();
+	}
+
+	/**
 	 * @return the user defined tools
 	 */
 	public Map<String, ExternalDiffTool> getUserDefinedTools() {
@@ -86,10 +97,32 @@ public class DiffTools {
 
 	private void setupPredefinedTools() {
 		predefinedTools = new TreeMap<>();
+		for (CommandLineDiffTool tool : CommandLineDiffTool.values()) {
+			predefinedTools
+					.put(tool.name(),
+							new PreDefinedDiffTool(tool.name(), tool.getPath(),
+									tool.getParameters()));
+		}
 	}
 
 	private void setupUserDefinedTools() {
 		userDefinedTools = new TreeMap<>();
+		Map<String, ExternalDiffTool> userTools = config.getTools();
+		for (String name : userTools.keySet()) {
+			ExternalDiffTool userTool = userTools.get(name);
+			// if difftool.<name>.cmd is defined we have user defined tool
+			if (userTool.getCommand() != null) {
+				userDefinedTools.put(name, userTool);
+			} else if (userTool.getPath() != null) {
+				// if difftool.<name>.path is defined we just overload the path
+				// of predefined tool
+				PreDefinedDiffTool predefTool = (PreDefinedDiffTool) predefinedTools
+						.get(name);
+				if (predefTool != null) {
+					predefTool.setPath(userTool.getPath());
+				}
+			}
+		}
 	}
 
 }
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/PreDefinedDiffTool.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018-2020, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * The pre-defined diff tool.
+ *
+ * @since 5.7
+ */
+@SuppressWarnings("nls")
+public class PreDefinedDiffTool extends UserDefinedDiffTool {
+
+	/**
+	 * Create a pre-defined diff tool
+	 *
+	 * @param name
+	 *            the name
+	 * @param path
+	 *            the path
+	 * @param parameters
+	 *            the tool parameters as one string that is used together with
+	 *            path as command
+	 */
+	public PreDefinedDiffTool(String name, String path, String parameters) {
+		super(name, path, parameters);
+	}
+
+	/**
+	 * @param path
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	/**
+	 * @return the diff tool command
+	 */
+	@Override
+	public String getCommand() {
+		return path + " " + super.getCommand();
+	}
+
+}
