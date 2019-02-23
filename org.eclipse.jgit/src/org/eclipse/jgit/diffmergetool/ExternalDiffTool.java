2e33de950fdbcfef1eb38ec222d9f68f0037f960
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:25:10 +0100

    Add command line support for "git difftool"
    see: http://git-scm.com/docs/git-difftool
    
    * add command line support for "jgit difftool"
      * show supported commands with "jgit difftool --help"
      * added "git difftool --tool-help" to show the tools (empty now)
      * prepare for all other commands
    
    Bug: 356832
    Change-Id: Ice0c13ef7953a20feaf25e7746d62b94ff4e89e5
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/ExternalDiffTool.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018-2020, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * The external tool interface.
+ *
+ * @since 5.7
+ */
+public interface ExternalDiffTool {
+
+	/**
+	 * @return the tool name
+	 */
+	String getName();
+
+	/**
+	 * @return the tool path
+	 */
+	String getPath();
+
+	/**
+	 * @return the tool command
+	 */
+	String getCommand();
+
+}
