682e8cffbb62338970303311c71c3396d3cd7f8e
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:25:10 +0100

    Add command line support for "git difftool"
    see: http://git-scm.com/docs/git-difftool
    
    * add command line support for "jgit difftool"
    * show supported commands with "jgit difftool --help"
    * added "git difftool --tool-help" to show the tools (empty now)
    * prepare for all other commands
    
    Bug: 356832
    Change-Id: Ice0c13ef7953a20feaf25e7746d62b94ff4e89e5
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/diffmergetool/DiffTools.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2018-2021, Andre Bossert <andre.bossert@siemens.com>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.diffmergetool;
+
+import java.util.TreeMap;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.internal.BooleanTriState;
+
+/**
+ * Manages diff tools.
+ */
+public class DiffTools {
+
+	private Map<String, ExternalDiffTool> predefinedTools;
+
+	private Map<String, ExternalDiffTool> userDefinedTools;
+
+	/**
+	 * Creates the external diff-tools manager for given repository.
+	 *
+	 * @param repo
+	 *            the repository database
+	 */
+	public DiffTools(Repository repo) {
+		setupPredefinedTools();
+		setupUserDefinedTools();
+	}
+
+	/**
+	 * Compare two versions of a file.
+	 *
+	 * @param newPath
+	 *            the new file path
+	 * @param oldPath
+	 *            the old file path
+	 * @param newId
+	 *            the new object ID
+	 * @param oldId
+	 *            the old object ID
+	 * @param toolName
+	 *            the selected tool name (can be null)
+	 * @param prompt
+	 *            the prompt option
+	 * @param gui
+	 *            the GUI option
+	 * @param trustExitCode
+	 *            the "trust exit code" option
+	 * @return the return code from executed tool
+	 */
+	public int compare(String newPath, String oldPath, String newId,
+			String oldId, String toolName, BooleanTriState prompt,
+			BooleanTriState gui, BooleanTriState trustExitCode) {
+		return 0;
+	}
+
+	/**
+	 * @return the tool names
+	 */
+	public Set<String> getToolNames() {
+		return Collections.emptySet();
+	}
+
+	/**
+	 * @return the user defined tools
+	 */
+	public Map<String, ExternalDiffTool> getUserDefinedTools() {
+		return Collections.unmodifiableMap(userDefinedTools);
+	}
+
+	/**
+	 * @return the available predefined tools
+	 */
+	public Map<String, ExternalDiffTool> getAvailableTools() {
+		return Collections.unmodifiableMap(predefinedTools);
+	}
+
+	/**
+	 * @return the NOT available predefined tools
+	 */
+	public Map<String, ExternalDiffTool> getNotAvailableTools() {
+		return Collections.unmodifiableMap(new TreeMap<>());
+	}
+
+	/**
+	 * @param gui
+	 *            use the diff.guitool setting ?
+	 * @return the default tool name
+	 */
+	public String getDefaultToolName(BooleanTriState gui) {
+		return gui != BooleanTriState.UNSET ? "my_gui_tool" //$NON-NLS-1$
+				: "my_default_toolname"; //$NON-NLS-1$
+	}
+
+	/**
+	 * @return is interactive (config prompt enabled) ?
+	 */
+	public boolean isInteractive() {
+		return false;
+	}
+
+	private void setupPredefinedTools() {
+		predefinedTools = new TreeMap<>();
+	}
+
+	private void setupUserDefinedTools() {
+		userDefinedTools = new TreeMap<>();
+	}
+
+}
