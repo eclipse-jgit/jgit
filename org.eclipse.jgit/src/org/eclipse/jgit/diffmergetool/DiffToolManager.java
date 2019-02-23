f6294e6603d9cdf299260bc572e7b290f56ecead
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:25:10 +0100

    Add command line support for "git difftool"
    see: http://git-scm.com/docs/git-difftool
    
    * add command line support for "jgit difftool"
      * show supported commands with "jgit difftool --help"
      * added "git difftool --tool-help" to show the tools (empty now)
      * prepare for all other commands
    
    Bug: 356832
    Change-Id: Ice0c13ef7953a20feaf25e7746d62b94ff4e89e5
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/DiffToolManager.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2018-2019, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+import java.util.TreeMap;
+import java.util.Map;
+
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Manages diff tools.
+ *
+ * @since 5.7
+ */
+public class DiffToolManager {
+
+	private Map<String, IDiffTool> predefinedTools;
+
+	private Map<String, IDiffTool> userDefinedTools;
+
+	/**
+	 * @param db the repository database
+	 */
+	public DiffToolManager(Repository db) {
+		// TODO: read the config
+		setupPredefinedTools();
+		setupUserDefinedTools();
+	}
+
+	/**
+	 * @param newPath
+	 *            the new file path
+	 * @param oldPath
+	 *            the old file path
+	 * @param newId
+	 *            the new object ID
+	 * @param oldId
+	 *            the old object ID
+	 * @param toolName
+	 *            the selected tool name (can be null)
+	 * @param prompt
+	 *            the prompt option
+	 * @param gui
+	 *            the GUI option
+	 * @param trustExitCode
+	 *            the "trust exit code" option
+	 * @return the return code from executed tool
+	 */
+	public int compare(String newPath, String oldPath, String newId,
+			String oldId, String toolName, BooleanOption prompt,
+			BooleanOption gui, BooleanOption trustExitCode) {
+		return 0;
+	}
+
+	/**
+	 * @return the user defined tools
+	 */
+	public Map<String, IDiffTool> getUserDefinedTools() {
+		return userDefinedTools;
+	}
+
+	/**
+	 * @return the available predefined tools
+	 */
+	public Map<String, IDiffTool> getAvailableTools() {
+		// TODO: change to return only available tools instead of all
+		return predefinedTools;
+	}
+
+	/**
+	 * @return the NOT available predefined tools
+	 */
+	public Map<String, IDiffTool> getNotAvailableTools() {
+		// TODO: return not available tools
+		return new TreeMap<>();
+	}
+
+	private void setupPredefinedTools() {
+		predefinedTools = new TreeMap<>();
+	}
+
+	private void setupUserDefinedTools() {
+		userDefinedTools = new TreeMap<>();
+	}
+
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/IDiffTool.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018-2019, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * The difftool interface.
+ *
+ * @since 5.7
+ */
+public interface IDiffTool {
+
+	/**
+	 * @return the tool name
+	 */
+	abstract public String getName();
+
+	/**
+	 * @return the tool path
+	 */
+	abstract String getPath();
+
+	/**
+	 * @return the tool command
+	 */
+	abstract public String getCommand();
+
+}
