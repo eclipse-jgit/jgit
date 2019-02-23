79be99f5a3319965a683219b8c39276a5cb1c2ba
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:46:05 +0100

    Add config reader for user-defined difftools
    see: http://git-scm.com/docs/git-difftool
    
    * add config reader for user-defined difftools
      * diff.tool
      * diff.guitool
      * difftool.prompt
      * difftool.trustExitCode
      * difftool.<tool>.path
      * difftool.<tool>.cmd
    * add pre-defined difftools
    * implemented "git difftool --tool-help" to verify config reader and
    pre-defined difftools
    
    Bug: 356832
    Change-Id: Idde8fddbef61f3378ee565c6321570b3962d0e1d
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/UserDefinedDiffTool.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2018-2020, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * The user-defined diff tool.
+ *
+ * @since 5.7
+ */
+public class UserDefinedDiffTool implements IDiffTool {
+
+	/**
+	 * the diff tool name
+	 */
+	private final String name;
+
+	/**
+	 * the diff tool path
+	 */
+	protected String path;
+
+	/**
+	 * the diff tool command
+	 */
+	private String cmd;
+
+	/**
+	 * Creates the diff tool
+	 *
+	 * @param name
+	 *            the name
+	 * @param path
+	 *            the path
+	 * @param cmd
+	 *            the command
+	 */
+	public UserDefinedDiffTool(final String name, final String path,
+			final String cmd) {
+		this.name = name;
+		this.path = path;
+		this.cmd = cmd;
+	}
+
+	/**
+	 * @return the diff tool name
+	 */
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the diff tool path
+	 */
+	@Override
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * @return the diff tool command
+	 */
+	@Override
+	public String getCommand() {
+		return cmd;
+	}
+
+}
