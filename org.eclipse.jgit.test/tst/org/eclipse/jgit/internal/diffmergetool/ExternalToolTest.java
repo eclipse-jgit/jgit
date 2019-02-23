dbbf509f31e57c307339f973c1ad7800f2060ee8
Andre Bossert <andre.bossert@siemens.com>
2019-02-23 15:25:10 +0100

    Add command line support for "git difftool"
    see: http://git-scm.com/docs/git-difftool
    
    * add command line support for "jgit difftool"
    * show supported commands with "jgit difftool --help"
    * added "git difftool --tool-help" to show the tools (empty now)
    * prepare for all other commands
    
    Bug: 356832
    Change-Id: Ice0c13ef7953a20feaf25e7746d62b94ff4e89e5
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/diffmergetool/ExternalToolTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2020-2021, Simeon Andreev <simeon.danailov.andreev@gmail.com> and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.diffmergetool;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.eclipse.jgit.junit.RepositoryTestCase;
+import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.util.FS_POSIX;
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+
+/**
+ * Base test case for external merge and diff tool tests.
+ */
+public class ExternalToolTest extends RepositoryTestCase {
+
+	protected static final String DEFAULT_CONTENT = "line1";
+
+	protected File localFile;
+
+	protected File remoteFile;
+
+	protected File mergedFile;
+
+	protected File baseFile;
+
+	protected File commandResult;
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		localFile = writeTrashFile("localFile.txt", DEFAULT_CONTENT + "\n");
+		localFile.deleteOnExit();
+		remoteFile = writeTrashFile("remoteFile.txt", DEFAULT_CONTENT + "\n");
+		remoteFile.deleteOnExit();
+		mergedFile = writeTrashFile("mergedFile.txt", "");
+		mergedFile.deleteOnExit();
+		baseFile = writeTrashFile("baseFile.txt", "");
+		baseFile.deleteOnExit();
+		commandResult = writeTrashFile("commandResult.txt", "");
+		commandResult.deleteOnExit();
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		Files.delete(localFile.toPath());
+		Files.delete(remoteFile.toPath());
+		Files.delete(mergedFile.toPath());
+		Files.delete(baseFile.toPath());
+		Files.delete(commandResult.toPath());
+
+		super.tearDown();
+	}
+
+
+	protected static void assumePosixPlatform() {
+		Assume.assumeTrue(
+				"This test can run only in Linux tests",
+				FS.DETECTED instanceof FS_POSIX);
+	}
+}
