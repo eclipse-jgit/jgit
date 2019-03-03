e848f10a8bdab401245e8019f41d455ebb587adf
Andre Bossert <andre.bossert@siemens.com>
2019-03-03 15:25:11 +0100

    Add config reader for user-defined mergetools
    see: https://git-scm.com/docs/git-mergetool
    see: https://git-scm.com/docs/git-config
    
    * add config reader for user-defined mergetools
      * merge.tool
      * merge.guitool
      * mergetool.prompt
      * mergetool.keepBackup
      * mergetool.keepTemporaries
      * mergetool.writeToTemp
      * mergetool.<tool>.path
      * mergetool.<tool>.cmd
      * mergetool.<tool>.trustExitCode
    
    Will be done in next commits:
     * implement fallback (merge) of difftool config to mergetool config
     * add pre-defined mergetools like done in C-Git and combine them with
    difftools
     * add command line support for "git mergetool"
     * implement mergetool merge function (execute external tool)
     * integrate with Eclipse UI (EGit)
    
    Bug: 356832
    Change-Id: Ic4f07376630713d8e06cbad284d9c72c9ecc0405
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -62,22 +62,24 @@ public final class ConfigConstants {
 	/** The "diff" section */
 	public static final String CONFIG_DIFF_SECTION = "diff";
 
-	/** The "tool" key within "diff" section */
+	/** The "tool" key within "diff" or "merge" section */
 	public static final String CONFIG_KEY_TOOL = "tool";
 
-	/** The "guitool" key within "diff" section */
+	/** The "guitool" key within "diff" or "merge" section */
 	public static final String CONFIG_KEY_GUITOOL = "guitool";
 
 	/** The "difftool" section */
 	public static final String CONFIG_DIFFTOOL_SECTION = "difftool";
 
-	/** The "prompt" key within "difftool" section */
+	/** The "prompt" key within "difftool" or "mergetool" section */
 	public static final String CONFIG_KEY_PROMPT = "prompt";
 
-	/** The "trustExitCode" key within "difftool" section */
+	/**
+	 * The "trustExitCode" key within "difftool" or "mergetool.<name>." section
+	 */
 	public static final String CONFIG_KEY_TRUST_EXIT_CODE = "trustExitCode";
 
-	/** The "cmd" key within "difftool.*." section */
+	/** The "cmd" key within "difftool.*." or "mergetool.*." section */
 	public static final String CONFIG_KEY_CMD = "cmd";
 
 	/** The "dfs" section */
@@ -131,8 +133,27 @@ public final class ConfigConstants {
 	 */
 	public static final String CONFIG_MERGE_SECTION = "merge";
 
+	/** The "mergetool" section */
+	public static final String CONFIG_MERGETOOL_SECTION = "mergetool";
+
+	/**
+	 * The "keepBackup" key within "mergetool" section
+	 */
+	public static final String CONFIG_KEY_KEEP_BACKUP = "keepBackup";
+
+	/**
+	 * The "keepTemporaries" key within "mergetool" section
+	 */
+	public static final String CONFIG_KEY_KEEP_TEMPORARIES = "keepTemporaries";
+
+	/**
+	 * The "writeToTemp" key within "mergetool" section
+	 */
+	public static final String CONFIG_KEY_WRITE_TO_TEMP = "writeToTemp";
+
 	/**
 	 * The "filter" section
+	 *
 	 * @since 4.6
 	 */
 	public static final String CONFIG_FILTER_SECTION = "filter";
