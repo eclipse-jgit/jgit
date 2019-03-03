161a1d8af90ed8fc3d15a936543c8d3489c6ecb0
Andre Bossert <andre.bossert@siemens.com>
2019-03-03 15:25:11 +0100

    Add config reader for user-defined mergetools
    see: https://git-scm.com/docs/git-mergetool
    see: https://git-scm.com/docs/git-config
    
    * add config reader for user-defined mergetools
      * merge.tool
      * merge.guitool
      * mergetool.prompt
      * mergetool.keepBackup
      * mergetool.keepTemporaries
      * mergetool.writeToTemp
      * mergetool.<tool>.path
      * mergetool.<tool>.cmd
      * mergetool.<tool>.trustExitCode
    
    Will be done in next commits:
     * implement fallback (merge) of difftool config to mergetool config
     * add pre-defined mergetools like done in C-Git and combine them with
    difftools
     * add command line support for "git mergetool"
     * implement mergetool merge function (execute external tool)
     * integrate with Eclipse UI (EGit)
    
    Change-Id: Ic4f07376630713d8e06cbad284d9c72c9ecc0405
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/UserDefinedMergeTool.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/UserDefinedMergeTool.java
@@ -44,28 +44,19 @@
 package org.eclipse.jgit.diffmergetool;
 
 /**
- * The user-defined diff tool.
+ * The user-defined merge tool.
  *
  */
-public class UserDefinedDiffTool implements IDiffTool {
+public class UserDefinedMergeTool extends UserDefinedDiffTool
+		implements IMergeTool {
 
 	/**
-	 * the tool name
+	 * the merge tool "trust exit code" option
 	 */
-	protected final String name;
+	protected final BooleanOption trustExitCode;
 
 	/**
-	 * the tool path
-	 */
-	protected String path;
-
-	/**
-	 * the tool command
-	 */
-	protected String cmd;
-
-	/**
-	 * Creates the diff tool
+	 * Creates the merge tool
 	 *
 	 * @param name
 	 *            the name
@@ -73,36 +64,28 @@ public class UserDefinedDiffTool implements IDiffTool {
 	 *            the path
 	 * @param cmd
 	 *            the command
+	 * @param trustExitCode
+	 *            the "trust exit code" option
 	 */
-	public UserDefinedDiffTool(final String name, final String path,
-			final String cmd) {
-		this.name = name;
-		this.path = path;
-		this.cmd = cmd;
+	public UserDefinedMergeTool(final String name, final String path,
+			final String cmd, final BooleanOption trustExitCode) {
+		super(name, path, cmd);
+		this.trustExitCode = trustExitCode;
 	}
 
 	/**
-	 * @return the tool name
+	 * @return the "trust exit code" flag
 	 */
 	@Override
-	public String getName() {
-		return name;
+	public boolean isTrustExitCode() {
+		return trustExitCode.toBoolean();
 	}
 
 	/**
-	 * @return the tool path
+	 * @return the "trust exit code" option
 	 */
-	@Override
-	public String getPath() {
-		return path;
-	}
-
-	/**
-	 * @return the tool command
-	 */
-	@Override
-	public String getCommand() {
-		return cmd;
+	public BooleanOption getTrustExitCode() {
+		return trustExitCode;
 	}
 
 }
