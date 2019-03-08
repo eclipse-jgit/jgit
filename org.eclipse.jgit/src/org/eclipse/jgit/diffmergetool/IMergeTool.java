9ac291ef863dfec5ad848703b8b66a87850b416d
Andre Bossert <andre.bossert@siemens.com>
2019-03-08 21:32:57 +0100

    Add config reader for user-defined mergetools
    see: https://git-scm.com/docs/git-mergetool
    see: https://git-scm.com/docs/git-config
    
    * add config reader for user-defined mergetools
      * merge.tool
      * merge.guitool
      * mergetool.prompt
      * mergetool.keepBackup
      * mergetool.keepTemporaries
      * mergetool.writeToTemp
      * mergetool.<tool>.path
      * mergetool.<tool>.cmd
      * mergetool.<tool>.trustExitCode
    
    Bug: 356832
    Change-Id: Ic4f07376630713d8e06cbad284d9c72c9ecc0405
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/diffmergetool/IMergeTool.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2018-2019, Andre Bossert <andre.bossert@siemens.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.diffmergetool;
+
+/**
+ * The merge tool interface.
+ *
+ * @since 5.7
+ */
+public interface IMergeTool extends IDiffTool {
+
+	/**
+	 * @return the tool "trust exit code" option
+	 */
+	public boolean isTrustExitCode();
+
+}
