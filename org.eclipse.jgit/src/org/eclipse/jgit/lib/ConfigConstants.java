24171b05f0db8487d0a1dc40072cc313bb6e2087
Andre Bossert <andre.bossert@siemens.com>
2019-03-08 21:32:57 +0100

    Add config reader for user-defined mergetools
    see: https://git-scm.com/docs/git-mergetool
    see: https://git-scm.com/docs/git-config
    
    * add config reader for user-defined mergetools
      * merge.tool
      * merge.guitool
      * mergetool.prompt
      * mergetool.keepBackup
      * mergetool.keepTemporaries
      * mergetool.writeToTemp
      * mergetool.<tool>.path
      * mergetool.<tool>.cmd
      * mergetool.<tool>.trustExitCode
    
    Bug: 356832
    Change-Id: Ic4f07376630713d8e06cbad284d9c72c9ecc0405
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -31,14 +31,14 @@ public final class ConfigConstants {
 	public static final String CONFIG_DIFF_SECTION = "diff";
 
 	/**
-	 * The "tool" key within "diff" section
+	 * The "tool" key within "diff" or "merge" section
 	 *
 	 * @since 6.1
 	 */
 	public static final String CONFIG_KEY_TOOL = "tool";
 
 	/**
-	 * The "guitool" key within "diff" section
+	 * The "guitool" key within "diff" or "merge" section
 	 *
 	 * @since 6.1
 	 */
@@ -52,14 +52,14 @@ public final class ConfigConstants {
 	public static final String CONFIG_DIFFTOOL_SECTION = "difftool";
 
 	/**
-	 * The "prompt" key within "difftool" section
+	 * The "prompt" key within "difftool" or "mergetool" section
 	 *
 	 * @since 6.1
 	 */
 	public static final String CONFIG_KEY_PROMPT = "prompt";
 
 	/**
-	 * The "trustExitCode" key within "difftool" section
+	 * The "trustExitCode" key within "difftool" or "mergetool.<name>." section
 	 *
 	 * @since 6.1
 	 */
@@ -122,6 +122,34 @@ public final class ConfigConstants {
 	 * @since 4.9
 	 */
 	public static final String CONFIG_MERGE_SECTION = "merge";
+
+	/**
+	 * The "mergetool" section
+	 *
+	 * @since 5.13
+	 */
+	public static final String CONFIG_MERGETOOL_SECTION = "mergetool";
+
+	/**
+	 * The "keepBackup" key within "mergetool" section
+	 *
+	 * @since 5.13
+	 */
+	public static final String CONFIG_KEY_KEEP_BACKUP = "keepBackup";
+
+	/**
+	 * The "keepTemporaries" key within "mergetool" section
+	 *
+	 * @since 5.13
+	 */
+	public static final String CONFIG_KEY_KEEP_TEMPORARIES = "keepTemporaries";
+
+	/**
+	 * The "writeToTemp" key within "mergetool" section
+	 *
+	 * @since 5.13
+	 */
+	public static final String CONFIG_KEY_WRITE_TO_TEMP = "writeToTemp";
 
 	/**
 	 * The "filter" section
