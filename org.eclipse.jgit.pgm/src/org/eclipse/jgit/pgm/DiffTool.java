02c91424b6188b01750ef662a1ecb04dc7b2c678
Andre Bossert <andre.bossert@siemens.com>
2019-03-08 22:22:58 +0100

    Add command line support for "git mergetool"
    see: https://git-scm.com/docs/git-mergetool
    see: https://git-scm.com/docs/git-config
    
    * add command line support for "git mergetool"
      * add option handling for "--tool-help", "--tool=<mytool>",
    "--[no-]prompt",  "--[no-]gui"
      * handle prompt
      * add MergeToolManager
      * add pre-defined mergetools
      * print merge actions --> no execute, will be done later
    
    Bug: 356832
    Change-Id: I6e505ffc3d03f75ecf4bba452a25d25dfcf5793f
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/DiffTool.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/DiffTool.java
@@ -120,7 +120,7 @@ class DiffTool extends TextBuiltin {
 
 	private BooleanOption gui = BooleanOption.NOT_DEFINED_FALSE;
 
-	@Option(name = "--gui", aliases = { "-g" }, usage = "usage_Gui")
+	@Option(name = "--gui", aliases = { "-g" }, usage = "usage_DiffGuiTool")
 	void setGui(@SuppressWarnings("unused") boolean on) {
 		gui = BooleanOption.TRUE;
 	}
