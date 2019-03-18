83ee8fa33d76f37c2ad96feb270f140648afaacf
Matthew DeVore <matvore@gmail.com>
2019-03-18 07:15:52 -0700

    Support for tree:<depth> filter in core Git
    Change-Id: Ic312fee22d60e32cfcad59da56980e90ae2cae6a
    Signed-off-by: Matthew DeVore <matvore@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/ProtocolV2Parser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/ProtocolV2Parser.java
@@ -271,7 +271,13 @@ final class ProtocolV2Parser {
 
 	/*
 	 * Process the content of "filter" line from the protocol. It has a shape
-	 * like "blob:none" or "blob:limit=N", with limit a positive number.
+	 * like:
+	 *
+	 * <ul>
+	 *   <li>"blob:none"
+	 *   <li>"blob:limit=N", with N &gt;= 0
+	 *   <li>"tree:DEPTH", with DEPTH &gt;= 0
+	 * </ul>
 	 *
 	 * @param filterLine
 	 *            the content of the "filter" line in the protocol
@@ -282,30 +288,31 @@ final class ProtocolV2Parser {
 	 */
 	static FilterSpec parseFilterLine(String filterLine)
 			throws PackProtocolException {
-		long blobLimit = -1;
-
 		if (filterLine.equals("blob:none")) { //$NON-NLS-1$
-			blobLimit = 0;
+			return FilterSpec.blobFilter(0);
 		} else if (filterLine.startsWith("blob:limit=")) { //$NON-NLS-1$
+			long blobLimit = -1;
 			try {
 				blobLimit = Long
 						.parseLong(filterLine.substring("blob:limit=".length())); //$NON-NLS-1$
-			} catch (NumberFormatException e) {
-				throw new PackProtocolException(MessageFormat
-						.format(JGitText.get().invalidFilter, filterLine));
+			} catch (NumberFormatException e) {}
+			if (blobLimit >= 0) {
+				return FilterSpec.blobFilter(blobLimit);
+			}
+		} else if (filterLine.startsWith("tree:")) { //$NON-NLS-1$
+			long treeDepthLimit = -1;
+			try {
+				treeDepthLimit = Long
+						.parseLong(filterLine.substring("tree:".length())); //$NON-NLS-1$
+			} catch (NumberFormatException e) {}
+			if (treeDepthLimit >= 0) {
+				return FilterSpec.treeDepthFilter(treeDepthLimit);
 			}
 		}
-		/*
-		 * We must have (1) either "blob:none" or "blob:limit=" set (because we
-		 * only support blob size limits for now), and (2) if the latter, then
-		 * it must be nonnegative. Throw if (1) or (2) is not met.
-		 */
-		if (blobLimit < 0) {
-			throw new PackProtocolException(
-					MessageFormat.format(JGitText.get().invalidFilter, filterLine));
-		}
 
-		return FilterSpec.blobFilter(blobLimit);
+		// Did not match any known filter format.
+		throw new PackProtocolException(
+				MessageFormat.format(JGitText.get().invalidFilter, filterLine));
 	}
 
 }
