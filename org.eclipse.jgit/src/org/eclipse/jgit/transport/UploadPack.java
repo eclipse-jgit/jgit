1dc7c7e18efbff3531e11ff598e7becb9b5e81eb
Matthew DeVore <matvore@gmail.com>
2019-03-18 07:15:52 -0700

    Support for tree:<depth> filter in core Git
    Change-Id: Ic312fee22d60e32cfcad59da56980e90ae2cae6a
    Signed-off-by: Matthew DeVore <matvore@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -2120,7 +2120,8 @@ public class UploadPack {
 			}
 			pw.setUseBitmaps(
 					req.getDepth() == 0
-							&& req.getClientShallowCommits().isEmpty());
+							&& req.getClientShallowCommits().isEmpty()
+							&& req.getFilterSpec().getTreeDepthLimit() == -1);
 			pw.setClientShallowCommits(req.getClientShallowCommits());
 			pw.setReuseDeltaCommits(true);
 			pw.setDeltaBaseAsOffset(
