6c29e69c0972bfbc91d785eadf83c5b15f36c5a5
Masaya Suzuki <masayasuzuki@google.com>
2019-04-15 10:43:28 -0700

    Do not double count incoming edges
    The upstream generator can produce multiple commits, and
    TopoSortGenerator is broken when it's used with such generator.
    Deduplicate the commits.
    
    Change-Id: I8722015a8f506dbe17393a103a37e577378b117c
    Signed-off-by: Masaya Suzuki <masayasuzuki@google.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/DepthWalkTopoSortGeneratorTest.java
@@ -0,0 +1,35 @@
+package org.eclipse.jgit.revwalk;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+
+/**
+ * Test for detecting a bug that happen only in the combination of DepthWalk and
+ * topological sort.
+ */
+@RunWith(JUnit4.class)
+public class DepthWalkTopoSortGeneratorTest extends RevWalkTestCase {
+	@Test
+	public void visitMultipleTimes() throws Exception {
+		RevCommit a = commit();
+		RevCommit b = commit(a);
+		RevCommit c = commit(b);
+		RevCommit d = commit(c);
+
+		try (DepthWalk.RevWalk depthWalk = new DepthWalk.RevWalk(
+				rw.getObjectReader(), Integer.MAX_VALUE)) {
+			depthWalk.sort(RevSort.TOPO);
+			depthWalk.markRoot(depthWalk.lookupCommit(d));
+			depthWalk.markStart(depthWalk.lookupCommit(b));
+			depthWalk.lookupCommit(b).add(depthWalk.getUnshallowFlag());
+			assertEquals(depthWalk.next().toObjectId(), d.toObjectId());
+			assertEquals(depthWalk.next().toObjectId(), c.toObjectId());
+			assertEquals(depthWalk.next().toObjectId(), b.toObjectId());
+			assertEquals(depthWalk.next().toObjectId(), a.toObjectId());
+			assertNull(depthWalk.next());
+		}
+	}
+}
