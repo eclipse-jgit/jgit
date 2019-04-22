5ac3e7376325a5cd31aab93d2b04fb98a3812860
Ivan Frade <ifrade@google.com>
2019-04-22 11:37:43 -0700

    ReachabilityChecker: Default implementation with a RevWalk
    It is common to check if a certain commit is reachable from some
    starting points. For example gitiles does it to check if a commit
    is visible to a user based on its permissions.
    
    Offer this functionality in JGit.
    
    Split the interface as the next commit will introduce an implementation
    using bitmap indices.
    
    Change-Id: I0933b305c8d734f7a64502910ff4d9ef4fc92ae1
    Signed-off-by: Ivan Frade <ifrade@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/ReachabilityChecker.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019, Google LLC.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.revwalk;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+
+/**
+ * Check if a commit is reachable from a collection of starting commits
+ *
+ * @since 5.5
+ */
+public interface ReachabilityChecker {
+	/**
+	 * Checks if the target commit is reachable from any of the starter commits.
+	 *
+	 * @param target
+	 *            commit to reach
+	 * @param starters
+	 *            commits known to be reachable
+	 * @return true if the target is reachable from any of the starters
+	 *
+	 * @throws org.eclipse.jgit.errors.MissingObjectException
+	 *             the supplied commit does not exist.
+	 * @throws org.eclipse.jgit.errors.IncorrectObjectTypeException
+	 *             the supplied id is not a commit or an annotated tag.
+	 * @throws java.io.IOException
+	 *             a pack file or loose object could not be read.
+	 */
+	boolean isReachable(RevCommit target, Collection<ObjectId> starters)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException;
+}
