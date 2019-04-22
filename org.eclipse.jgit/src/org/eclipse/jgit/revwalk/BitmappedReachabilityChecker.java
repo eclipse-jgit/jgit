855d082f6a96f362f91737e51f396c326704ac13
Ivan Frade <ifrade@google.com>
2019-04-22 15:38:45 -0700

    BitmappedReachabilityChecker: Reachability check using bitmaps
    The "basic" reachability check walks the graph starting from the tips
    marking things as "uninteresting". If the target commit is marked as
    "uninteresting" it was reached; it is reachable from those tips.
    
    This requires a lot of walking and can be solved directly with bitmaps.
    Most of the time the bitmaps are already calculated or a short walk
    away.
    
    This should improve the performance of reachability checks, for example
    in Gitiles.
    
    Change-Id: I83d33271f58d95d2dc9ed151967b3eda513c99f7
    Signed-off-by: Ivan Frade <ifrade@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/BitmappedReachabilityChecker.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2019, Google LLC
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.eclipse.jgit.revwalk;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.BitmapIndex.BitmapBuilder;
+import org.eclipse.jgit.lib.ObjectId;
+
+/**
+ * Checks the reachability using bitmaps.
+ * <p>
+ * A target is reachable if it is present in any of the bitmaps of the starters.
+ * <p>
+ * The {@link BitmapCalculator} takes care of building the bitmap of a commit if
+ * it doesn't have one. See its documentation for more details.
+ *
+ * @since 5.5
+ */
+public class BitmappedReachabilityChecker implements ReachabilityChecker {
+
+	private final RevWalk walk;
+
+	/**
+	 * @param walk
+	 *            walk on the repository to get or create the bitmaps for the
+	 *            commits
+	 */
+	public BitmappedReachabilityChecker(RevWalk walk) {
+		this.walk = walk;
+	}
+
+
+	@Override
+	public boolean isReachable(RevCommit target, Collection<ObjectId> starters)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		BitmapCalculator calculator = new BitmapCalculator(walk,
+				walk.getObjectReader().getBitmapIndex(), null);
+		BitmapBuilder seen = null;
+		for (ObjectId starter: starters) {
+			seen = calculator.getBitmapFor(Arrays.asList(starter), seen, false);
+			if (seen.contains(target)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
