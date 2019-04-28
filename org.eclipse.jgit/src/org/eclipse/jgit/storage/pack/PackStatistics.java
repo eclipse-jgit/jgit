eclipse-jgit/jgit

commit 175e66548b78e6d22ad27d62eeb06a77ee4d139d

tree:<depth> should not traverse overly-deep trees
If we are traversing a tree which is too deep, then there is no need to
traverse the children. Skipping children is much faster than traversing
the possibly thousands of objects which are directly or indirectly
referenced by the tree.

Change-Id: I6d68cc1d35da48e3288b9cc80356a281ab36863d
Signed-off-by: Matthew DeVore <matvore@gmail.com>

org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackStatistics.java
@@ -266,6 +266,9 @@ public class PackStatistics {
 		/** Time in ms spent writing the pack. */
 		public long timeWriting;
 
+		/** Number of trees traversed in the walk when writing the pack. */
+		public long treesTraversed;
+
 		/**
 		 * Statistics about each object type in the pack (commits, tags, trees
 		 * and blobs.)
@@ -583,6 +586,14 @@ public class PackStatistics {
 	 */
 	public long getTimeWriting() {
 		return statistics.timeWriting;
+	}
+
+	/**
+	 * @return number of trees traversed in the walk when writing the pack.
+	 * @since 5.4
+	 */
+	public long getTreesTraversed() {
+		return statistics.treesTraversed;
 	}
 
 	/**

org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
@@ -45,6 +45,7 @@ import org.eclipse.jgit.revwalk.RevBlob;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.storage.pack.PackStatistics;
 import org.eclipse.jgit.transport.UploadPack.RequestPolicy;
 import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException;
 import org.eclipse.jgit.transport.resolver.ServiceNotEnabledException;
@@ -75,6 +76,8 @@ public class UploadPackTest {
 	private InMemoryRepository client;
 
 	private TestRepository<InMemoryRepository> remote;
+
+	private PackStatistics stats;
 
 	@Before
 	public void setUp() throws Exception {
@@ -460,6 +463,7 @@ public class UploadPackTest {
 
 		ByteArrayOutputStream recv = new ByteArrayOutputStream();
 		up.upload(send, recv, null);
+		stats = up.getStatistics();
 
 		return new ByteArrayInputStream(recv.toByteArray());
 	}
@@ -1630,6 +1634,7 @@ public class UploadPackTest {
 				.has(preparator.blobLowDepth.toObjectId()));
 		assertFalse(client.getObjectDatabase()
 				.has(preparator.blobHighDepth.toObjectId()));
+		assertEquals(1, stats.getTreesTraversed());
 	}
 
 	@Test
@@ -1651,6 +1656,7 @@ public class UploadPackTest {
 				.has(preparator.blobLowDepth.toObjectId()));
 		assertFalse(client.getObjectDatabase()
 				.has(preparator.blobHighDepth.toObjectId()));
+		assertEquals(1, stats.getTreesTraversed());
 	}
 
 	@Test
@@ -1668,6 +1674,7 @@ public class UploadPackTest {
 				.has(preparator.blobLowDepth.toObjectId()));
 		assertFalse(client.getObjectDatabase()
 				.has(preparator.blobHighDepth.toObjectId()));
+		assertEquals(2, stats.getTreesTraversed());
 	}
 
 	/**

org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
@@ -2100,6 +2100,12 @@ public class PackWriter implements AutoCloseable {
 	/**
 	 * Determines if the object should be omitted from the pack as a result of
 	 * its depth (probably because of the tree:<depth> filter).
+	 * <p>
+	 * Causes {@code walker} to skip traversing the current tree, which ought to
+	 * have just started traversal, assuming this method is called as soon as a
+	 * new depth is reached.
+	 * <p>
+	 * This method increments the {@code treesTraversed} statistic.
 	 *
 	 * @param obj
 	 *            the object to check whether it should be omitted.
@@ -2116,12 +2122,17 @@ public class PackWriter implements AutoCloseable {
 		// A blob is considered one level deeper than the tree that contains it.
 		if (obj.getType() == OBJ_BLOB) {
 			treeDepth++;
+		} else {
+			stats.treesTraversed++;
 		}
 
-		// TODO: Do not continue traversing the tree, since its children
-		// will also be too deep.
-		return filterSpec.getTreeDepthLimit() != -1 &&
-				treeDepth > filterSpec.getTreeDepthLimit();
+		if (filterSpec.getTreeDepthLimit() < 0 ||
+			treeDepth <= filterSpec.getTreeDepthLimit()) {
+			return false;
+		}
+
+		walker.skipTree();
+		return true;
 	}
 
 	// Adds the given object as an object to be packed, first performing

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/ObjectWalk.java
@@ -390,6 +390,17 @@ public class ObjectWalk extends RevWalk {
 	}
 
 	/**
+	 * Skips the current tree such that {@link #nextObject()} does not return
+	 * any objects inside it. This should be called right after
+	 * {@link #nextObject()} returns the tree.
+	 *
+	 * @since 5.4
+	 */
+	public void skipTree() {
+		currVisit.ptr = currVisit.buf.length;
+	}
+
+	/**
 	 * Pop the next most recent object.
 	 *
 	 * @return next most recent object; null if traversal is over.

commit 3d624919b6d2bd6da11adf3a09980abbf1e10ffe

Silence API errors and add missing @since tags caused by 175e6654
Change-Id: If55acad84015f988f1e974bc19c426828b98822b
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.jgit/.settings/.api_filters
@@ -22,6 +22,22 @@
             </message_arguments>
         </filter>
     </resource>
+    <resource path="src/org/eclipse/jgit/revwalk/ObjectWalk.java" type="org.eclipse.jgit.revwalk.ObjectWalk">
+        <filter comment="ignore the risk subclasses could define the same field and cause a name clash" id="336658481">
+            <message_arguments>
+                <message_argument value="org.eclipse.jgit.revwalk.ObjectWalk"/>
+                <message_argument value="SIMPLE_VISITATION_POLICY"/>
+            </message_arguments>
+        </filter>
+    </resource>
+    <resource path="src/org/eclipse/jgit/storage/pack/PackStatistics.java" type="org.eclipse.jgit.storage.pack.PackStatistics$Accumulator">
+        <filter comment="ignore the risk subclasses could define the same field and cause a name clash" id="336658481">
+            <message_arguments>
+                <message_argument value="org.eclipse.jgit.storage.pack.PackStatistics.Accumulator"/>
+                <message_argument value="treesTraversed"/>
+            </message_arguments>
+        </filter>
+    </resource>
     <resource path="src/org/eclipse/jgit/transport/Transport.java" type="org.eclipse.jgit.transport.Transport">
         <filter comment="Marked as final since overriding a deprecated stub is likely a mistake" id="421654647">
             <message_arguments>

org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackStatistics.java
@@ -266,7 +266,8 @@ public class PackStatistics {
 		/** Time in ms spent writing the pack. */
 		public long timeWriting;
 
-		/** Number of trees traversed in the walk when writing the pack. */
+		/** Number of trees traversed in the walk when writing the pack. 
+		 * @since 5.4*/
 		public long treesTraversed;
 
 		/**