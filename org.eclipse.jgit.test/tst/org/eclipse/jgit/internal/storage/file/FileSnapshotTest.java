5d95a214fbc609bb6a86f965d53ecd039a126c8b
Matthias Sohn <matthias.sohn@sap.com>
2019-07-12 09:49:13 +0200

    Add test for racy git detection in FileSnapshot
    Repeat the test 10000 times to get statistics if measured
    fsTimestampResolution is working in practice to detect racy git
    situations.
    
    Change-Id: Iabe7af1a7211ca58480f8902d4fa4e366932fc77
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/FileSnapshotTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/FileSnapshotTest.java
@@ -42,9 +42,13 @@
  */
 package org.eclipse.jgit.internal.storage.file;
 
+import static org.eclipse.jgit.junit.JGitTestUtil.read;
+import static org.eclipse.jgit.junit.JGitTestUtil.write;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -54,17 +58,24 @@ import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.FileTime;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.concurrent.TimeUnit;
 
+import org.eclipse.jgit.junit.RepeatRule;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.util.SystemReader;
 import org.junit.After;
 import org.junit.Assume;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class FileSnapshotTest {
+
+	@Rule
+	public RepeatRule rule = new RepeatRule();
 
 	private Path trash;
 
@@ -203,6 +214,41 @@ public class FileSnapshotTest {
 
 		assertTrue(fs1.equals(fs2));
 		assertTrue(fs2.equals(fs1));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public void detectFileModifiedFindThreshold() throws IOException {
+		int failures = 0;
+		ArrayList<Long> deltas = new ArrayList<>();
+		long racyNanos = 0;
+		for (int i = 0; i < 10000; i++) {
+			File f = createFile("test").toFile();
+			write(f, "a");
+			FileSnapshot snapshot = FileSnapshot.save(f);
+			assertEquals("a", read(f));
+			write(f, "b");
+			if (!snapshot.isModified(f)) {
+				deltas.add(snapshot.wasDelta());
+				racyNanos = snapshot.wasRacyNanos();
+				failures++;
+			}
+		}
+		long min = 0;
+		long max = 0;
+		double avg = 0;
+		if (failures > 0) {
+			min = deltas.stream().min(Comparator.comparing(Long::longValue))
+				.get();
+			max = deltas.stream().max(Comparator.comparing(Long::longValue))
+				.get();
+			avg = deltas.stream().mapToDouble(a -> a).average()
+				.getAsDouble();
+			System.out.println(String.format(
+					"racyNanos=%,d, failures=%d, min=%,d, max=%,d, avg=%f",
+					racyNanos, failures, min, max, avg));
+		}
+		assertTrue(failures == 0);
 	}
 
 	private Path createFile(String string) throws IOException {
