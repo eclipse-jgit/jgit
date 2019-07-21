25f4fa63c6d3545a5eada7127a5083ccf2f3ae63
Han-Wen Nienhuys <hanwen@google.com>
2019-07-21 22:26:29 +0200

    WIP Move DFS specific code into new class DfsReftableBatchRefUpdate
    Most of the BatchRefUpdate for Reftable code here is not specific to
    the DFS repository implementation, and can be used for Reftable on
    the local filesystem.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I285fc0703f984afee2e37275c257130d0d2f548a

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableBatchRefUpdate.java
@@ -0,0 +1,360 @@
+/*
+ * Copyright (C) 2017, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.internal.storage.reftable;
+
+import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.revwalk.RevObject;
+import org.eclipse.jgit.revwalk.RevTag;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.transport.ReceiveCommand;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+
+import static org.eclipse.jgit.lib.Ref.Storage.NEW;
+import static org.eclipse.jgit.lib.Ref.Storage.PACKED;
+import static org.eclipse.jgit.transport.ReceiveCommand.Result.*;
+import static org.eclipse.jgit.transport.ReceiveCommand.Type.UPDATE_NONFASTFORWARD;
+
+/**
+ * {@link org.eclipse.jgit.lib.BatchRefUpdate} for Reftable based RefDatabase.
+ */
+public abstract class ReftableBatchRefUpdate extends BatchRefUpdate {
+    private final Lock lock;
+    private final RefDatabase refDb;
+    private final Repository repository;
+    private final CheckedGetReftables stack;
+
+    /**
+     * A supplier throwing IOException for the Reftable stack.
+     */
+    @FunctionalInterface
+    public interface CheckedGetReftables {
+        List<Reftable> get() throws IOException;
+    }
+
+    /**
+     * Initialize.
+     *
+     * @param refdb
+     *   The RefDatabase
+     * @param lock
+     *   A lock protecting the refdatabase's state
+     * @param repository
+     *   The repository on which this update will run
+     * @param stack
+     *   A supplier for the stack of reftables.
+     */
+    protected ReftableBatchRefUpdate(RefDatabase refdb, Lock lock,
+                                     Repository repository,
+                                     CheckedGetReftables stack) {
+        super(refdb);
+        this.refDb = refdb;
+        this.lock = lock;
+        this.repository = repository;
+        this.stack = stack;
+    }
+
+    /** Extracts and peels the refs out of the ReceiveCommands */
+    protected static List<Ref> toNewRefs(RevWalk rw, List<ReceiveCommand> pending)
+            throws IOException {
+        List<Ref> refs = new ArrayList<>(pending.size());
+        for (ReceiveCommand cmd : pending) {
+            String name = cmd.getRefName();
+            ObjectId newId = cmd.getNewId();
+            String newSymref = cmd.getNewSymref();
+            if (AnyObjectId.equals(ObjectId.zeroId(), newId)
+                    && newSymref == null) {
+                refs.add(new ObjectIdRef.Unpeeled(NEW, name, null));
+                continue;
+            } else if (newSymref != null) {
+                refs.add(new SymbolicRef(name,
+                        new ObjectIdRef.Unpeeled(NEW, newSymref, null)));
+                continue;
+            }
+
+            RevObject obj = rw.parseAny(newId);
+            RevObject peel = null;
+            if (obj instanceof RevTag) {
+                peel = rw.peel(obj);
+            }
+            if (peel != null) {
+                refs.add(new ObjectIdRef.PeeledTag(PACKED, name, newId,
+                        peel.copy()));
+            } else {
+                refs.add(new ObjectIdRef.PeeledNonTag(PACKED, name, newId));
+            }
+        }
+        return refs;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute(RevWalk rw, ProgressMonitor pm, List<String> options) {
+        List<ReceiveCommand> pending = getPending();
+        if (pending.isEmpty()) {
+            return;
+        }
+        if (options != null) {
+            setPushOptions(options);
+        }
+        try {
+            if (!checkObjectExistence(rw, pending)) {
+                return;
+            }
+            if (!checkNonFastForwards(rw, pending)) {
+                return;
+            }
+
+            lock.lock();
+            try {
+                if (!checkExpected(pending)) {
+                    return;
+                }
+                if (!checkConflicting(pending)) {
+                    return;
+                }
+                if (!blockUntilTimestamps(MAX_WAIT)) {
+                    return;
+                }
+                applyUpdates(rw, pending);
+                for (ReceiveCommand cmd : pending) {
+                    cmd.setResult(OK);
+                }
+            } finally {
+                lock.unlock();
+            }
+        } catch (IOException e) {
+            pending.get(0).setResult(LOCK_FAILURE, "io error"); //$NON-NLS-1$
+            ReceiveCommand.abort(pending);
+        }
+    }
+
+    private static boolean matchOld(ReceiveCommand cmd, @Nullable Ref ref) {
+        if (ref == null) {
+            return AnyObjectId.equals(ObjectId.zeroId(), cmd.getOldId())
+                    && cmd.getOldSymref() == null;
+        } else if (ref.isSymbolic()) {
+            return ref.getTarget().getName().equals(cmd.getOldSymref());
+        }
+        ObjectId id = ref.getObjectId();
+        if (id == null) {
+            id = ObjectId.zeroId();
+        }
+        return cmd.getOldId().equals(id);
+    }
+
+    /**
+     * Implements the storage-specific part of the update.
+     */
+    protected abstract void applyUpdates(RevWalk rw, List<ReceiveCommand> pending)
+            throws IOException;
+
+    private List<ReceiveCommand> getPending() {
+        return ReceiveCommand.filter(getCommands(), NOT_ATTEMPTED);
+    }
+
+    private boolean checkObjectExistence(RevWalk rw,
+                                         List<ReceiveCommand> pending) throws IOException {
+        for (ReceiveCommand cmd : pending) {
+            try {
+                if (!cmd.getNewId().equals(ObjectId.zeroId())) {
+                    rw.parseAny(cmd.getNewId());
+                }
+            } catch (MissingObjectException e) {
+                // ReceiveCommand#setResult(Result) converts REJECTED to
+                // REJECTED_NONFASTFORWARD, even though that result is also
+                // used for a missing object. Eagerly handle this case so we
+                // can set the right result.
+                cmd.setResult(REJECTED_MISSING_OBJECT);
+                ReceiveCommand.abort(pending);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkNonFastForwards(RevWalk rw, List<ReceiveCommand> pending) throws IOException {
+        if (isAllowNonFastForwards()) {
+            return true;
+        }
+        for (ReceiveCommand cmd : pending) {
+            cmd.updateType(rw);
+            if (cmd.getType() == UPDATE_NONFASTFORWARD) {
+                cmd.setResult(REJECTED_NONFASTFORWARD);
+                ReceiveCommand.abort(pending);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkConflicting(List<ReceiveCommand> pending)
+            throws IOException {
+        Set<String> names = new HashSet<>();
+        for (ReceiveCommand cmd : pending) {
+            names.add(cmd.getRefName());
+        }
+
+        boolean ok = true;
+        for (ReceiveCommand cmd : pending) {
+            String name = cmd.getRefName();
+            if (refDb.isNameConflicting(name)) {
+                cmd.setResult(LOCK_FAILURE);
+                ok = false;
+            } else {
+                int s = name.lastIndexOf('/');
+                while (0 < s) {
+                    if (names.contains(name.substring(0, s))) {
+                        cmd.setResult(LOCK_FAILURE);
+                        ok = false;
+                        break;
+                    }
+                    s = name.lastIndexOf('/', s - 1);
+                }
+            }
+        }
+        if (!ok && isAtomic()) {
+            ReceiveCommand.abort(pending);
+            return false;
+        }
+        return ok;
+    }
+
+    private boolean checkExpected(List<ReceiveCommand> pending)
+            throws IOException {
+        for (ReceiveCommand cmd : pending) {
+            if (!matchOld(cmd, refDb.exactRef(cmd.getRefName()))) {
+                cmd.setResult(LOCK_FAILURE);
+                if (isAtomic()) {
+                    ReceiveCommand.abort(pending);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    protected ReftableWriter.Stats write(OutputStream os, ReftableConfig cfg,
+                                         long updateIndex, List<Ref> newRefs, List<ReceiveCommand> pending)
+            throws IOException {
+        ReftableWriter writer = new ReftableWriter(cfg)
+                .setMinUpdateIndex(updateIndex).setMaxUpdateIndex(updateIndex)
+                .begin(os).sortAndWriteRefs(newRefs);
+        if (!isRefLogDisabled()) {
+            writeLog(writer, updateIndex, pending);
+        }
+        writer.finish();
+        return writer.getStats();
+    }
+
+    private void writeLog(ReftableWriter writer, long updateIndex,
+List<ReceiveCommand> pending) throws IOException {
+        Map<String, ReceiveCommand> cmds = new HashMap<>();
+        List<String> byName = new ArrayList<>(pending.size());
+        for (ReceiveCommand cmd : pending) {
+            cmds.put(cmd.getRefName(), cmd);
+            byName.add(cmd.getRefName());
+        }
+        Collections.sort(byName);
+
+        PersonIdent ident = getRefLogIdent();
+        if (ident == null) {
+            ident = new PersonIdent(repository);
+        }
+        for (String name : byName) {
+            ReceiveCommand cmd = cmds.get(name);
+            if (isRefLogDisabled(cmd)) {
+                continue;
+            }
+            String msg = getRefLogMessage(cmd);
+            if (isRefLogIncludingResult(cmd)) {
+                String strResult = toResultString(cmd);
+                if (strResult != null) {
+                    msg = msg.isEmpty() ? strResult : msg + ": " + strResult; //$NON-NLS-1$
+                }
+            }
+            writer.writeLog(name, updateIndex, ident, cmd.getOldId(),
+                    cmd.getNewId(), msg);
+        }
+    }
+
+    private String toResultString(ReceiveCommand cmd) {
+        switch (cmd.getType()) {
+        case CREATE:
+            return ReflogEntry.PREFIX_CREATED;
+        case UPDATE:
+            // Match the behavior of a single RefUpdate. In that case, setting
+            // the force bit completely bypasses the potentially expensive
+            // isMergedInto check, by design, so the reflog message may be
+            // inaccurate.
+            //
+            // Similarly, this class bypasses the isMergedInto checks when the
+            // force bit is set, meaning we can't actually distinguish between
+            // UPDATE and UPDATE_NONFASTFORWARD when isAllowNonFastForwards()
+            // returns true.
+            return isAllowNonFastForwards() ? ReflogEntry.PREFIX_FORCED_UPDATE
+                    : ReflogEntry.PREFIX_FAST_FORWARD;
+        case UPDATE_NONFASTFORWARD:
+            return ReflogEntry.PREFIX_FORCED_UPDATE;
+        default:
+            return null;
+        }
+    }
+
+    protected long nextUpdateIndex() throws IOException {
+        long updateIndex = 0;
+        for (Reftable r : stack.get()) {
+            if (r instanceof ReftableReader) {
+                updateIndex = Math.max(updateIndex,
+                        ((ReftableReader) r).maxUpdateIndex());
+            }
+        }
+        return updateIndex + 1;
+    }
+}
