368b8ac4bb52c8c1b3520c69869e05c531d8023e
Han-Wen Nienhuys <hanwen@google.com>
2019-07-24 17:36:25 +0200

    Introduce interface ReftableStack
    This can capture commonalities between DFS and FS based reftable
    implementations.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I9e8f8d3bc289ed3840c42e436c30a8def236ddcb

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableStack.java
@@ -0,0 +1,18 @@
+package org.eclipse.jgit.internal.storage.reftable;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ReftableStack {
+    /**
+     * Returns the logical timestamp at which the next reftable in the stack should start.
+     */
+    long nextUpdateIndex() throws IOException;
+
+    /**
+     * Get unmodifiable list of tables
+     *
+     * @return unmodifiable list of tables
+     */
+    List<Reftable> readers();
+}
