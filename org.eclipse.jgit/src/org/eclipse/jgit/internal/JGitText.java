6f358836a49f60da8ecc0c594bf9f0369f558053
Thomas Wolf <thomas.wolf@paranor.ch>
2019-08-06 18:31:35 +0200

    Make blame work correctly on merge conflicts
    When a conflicting file was blamed, JGit would not identify lines
    coming from the merge parents. The main cause for this was that
    Blame and BlameCommand simply added the first DirCacheEntry found
    for a file to its queue of candidates (blobs or commits) to consider.
    In case of a conflict this typically is the merge base commit, and
    comparing a auto-merged contents against that base would yield
    incorrect results.
    
    Such cases have to be handled specially. The candidate to be
    considered by the blame must use the working tree contents, but
    at the same time behave like a merge commit/candidate with HEAD
    and the MERGE_HEADs as parents. Canonical git does something very
    similar, see [1].
    
    Implement that and add tests.
    
    I first did this for the JGit pgm Blame command. When I then tried
    to do the same in BlameCommand, I noticed that the latter also
    included some fancy but incomplete CR-LF handling. In order to
    be able to use the new BlameGenerator.prepareHead() also in
    BlameCommand this CR-LF handling was also moved into BlameGenerator
    and corrected in doing so.
    
    (Just considering the git config settings was not good enough,
    CR-LF behavior can also be influenced by .gitattributes, and even
    by whether the file in the index has CR-LF. To correctly determine
    CR-LF handling for check-in one needs to do a TreeWalk with at
    least a FileTreeIterator and a DirCacheIterator.)
    
    Blame and diff operate in general on raw text, so a working tree
    file should also be run through the clean filters. This wasn't done
    in JGit before, and isn't done with this change either. I'm a bit
    unsure whether it would do the right thing with LFS files. Canonical
    git _does_ run the working tree content though the clean filters.
    
    [1] https://github.com/git/git/blob/v2.22.0/blame.c#L174
    
    Bug: 434330
    Change-Id: I9d763dd6ba478b0b6ebf9456049d6301f478ef7c
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -558,6 +558,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String nonBareLinkFilesNotSupported;
 	/***/ public String noPathAttributesFound;
 	/***/ public String noSuchRef;
+	/***/ public String noSuchRefKnown;
 	/***/ public String noSuchSubmodule;
 	/***/ public String notABoolean;
 	/***/ public String notABundle;
