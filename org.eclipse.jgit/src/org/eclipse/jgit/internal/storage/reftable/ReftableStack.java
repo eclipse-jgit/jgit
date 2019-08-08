527bb2c9d4b74e259942042ba5aed9da4503e83c
Han-Wen Nienhuys <hanwen@google.com>
2019-08-08 20:59:32 +0200

    Implement FileReftableDatabase
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I1837f268e91c6b446cb0155061727dbaccb714b8

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableStack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableStack.java
@@ -1,7 +1,10 @@
 package org.eclipse.jgit.internal.storage.reftable;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
-public interface ReftableStack {
+public interface ReftableStack extends AutoCloseable {
     long nextUpdateIndex() throws IOException;
+    List<Reftable> readers();
 }
