82d5cb06defaa6c299df270d584a1bba251bc955
Han-Wen Nienhuys <hanwen@google.com>
2019-08-12 17:33:51 +0200

    file: implement FileReftableDatabase
    FileReftableDatabase is based on FileReftableStack, which does
    compactions inline. This is simple, and has good median performance,
    but every so often it will rewrite the entire ref database.
    
    For testing, a FileReftableTest (mirroring RefUpdateTest) is added to
    check for Reftable specific behavior. This must be done separately, as
    reflogs have different semantics.
    
    Add a reftable flavor of BatchRefUpdateTest.
    
    Add a FileReftableStackTest to exercise compaction.
    
    Add FileRepository#convertToReftable so existing testdata can be
    reused.
    
    Change-Id: I1837f268e91c6b446cb0155061727dbaccb714b8
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsReftableBatchRefUpdate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsReftableBatchRefUpdate.java
@@ -46,7 +46,6 @@ package org.eclipse.jgit.internal.storage.dfs;
 import org.eclipse.jgit.internal.storage.dfs.DfsObjDatabase.PackSource;
 import org.eclipse.jgit.internal.storage.io.BlockSource;
 import org.eclipse.jgit.internal.storage.pack.PackExt;
-import org.eclipse.jgit.internal.storage.reftable.Reftable;
 import org.eclipse.jgit.internal.storage.reftable.ReftableBatchRefUpdate;
 import org.eclipse.jgit.internal.storage.reftable.ReftableCompactor;
 import org.eclipse.jgit.internal.storage.reftable.ReftableConfig;
