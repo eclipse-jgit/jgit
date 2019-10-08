7961dfbfbbf3cdc0f9cd01624fef0fa85141cbd5
Demetr Starshov <dstarshov@google.com>
2019-10-08 16:53:22 -0700

    ReceivePack: Moves connectivity check to separate class
    Move all connectivity check to separate classes. Set default to be
    FullConnectivityChecker i.e. checker which will check connectivity
    from all advertised refs. Add ability to set other connectivity
    checker which can use different approach.
    
    Signed-off-by: Demetr Starshov <dstarshov@google.com>
    Change-Id: Ibb107dbfbdde8ad109be25b5ecf42be7660ef736

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/ConnectivityChecker.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019, Google LLC  and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.transport;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ProgressMonitor;
+
+/**
+ *
+ * Checks that a received pack only depends on objects which are reachable from
+ * a defined set of references.
+ *
+ * @since 5.6
+ */
+public interface ConnectivityChecker {
+
+	/**
+	 * Checks connectivity of the commit graph after pack uploading.
+	 *
+	 * @param receivePack
+	 * @param haves
+	 *            Set of references known for client.
+	 * @param monitor
+	 *            Monitor to publish progress to.
+	 * @throws IOException
+	 *             an error occurred during connectivity checking.
+	 *
+	 */
+	void checkConnectivity(ReceivePack receivePack,
+			Set<ObjectId> haves, ProgressMonitor monitor)
+			throws IOException;
+
+}
