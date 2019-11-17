07ac0f6575d432d6e0f99ab5f89d3a1ecc7f71c8
Han-Wen Nienhuys <hanwen@google.com>
2019-11-17 15:05:57 -0800

    Fix RepositoryCache#FileKey#isGitRepository
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I778c2be01d96aaf135affae4b457b5fe5b483bee

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/FileReftableTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/FileReftableTest.java
@@ -73,6 +73,7 @@ import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.RefUpdate.Result;
 import org.eclipse.jgit.lib.ReflogEntry;
 import org.eclipse.jgit.lib.ReflogReader;
+import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.test.resources.SampleDataRepositoryTestCase;
 import org.eclipse.jgit.transport.ReceiveCommand;
@@ -500,6 +501,12 @@ public class FileReftableTest extends SampleDataRepositoryTestCase {
 			String want = "Branch: renamed b to new/name";
 			assertEquals(want, last.getComment());
 		}
+	}
+
+	@Test
+	public void isGitRepository() throws IOException {
+		assertTrue(new File(db.getDirectory(), "HEAD").delete());
+		assertTrue(RepositoryCache.FileKey.isGitRepository(db.getDirectory(), db.getFS()));
 	}
 
 	@Test
