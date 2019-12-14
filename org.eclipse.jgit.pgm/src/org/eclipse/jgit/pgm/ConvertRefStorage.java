dbe915def18d9b4dbadfc972d648aaa38462cd17
Matthias Sohn <matthias.sohn@sap.com>
2019-12-14 23:06:47 +0100

    pgm: add option to configure if convert-ref-storage should create backup
    Change-Id: I926f116797508a5982961dfd30c2f765d9536eee
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/ConvertRefStorage.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/ConvertRefStorage.java
@@ -45,6 +45,7 @@ package org.eclipse.jgit.pgm;
 
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.ExplicitBooleanOptionHandler;
 
 @Command(common = true, usage = "usage_convertRefStorage")
 class ConvertRefStorage extends TextBuiltin {
@@ -52,9 +53,13 @@ class ConvertRefStorage extends TextBuiltin {
 	@Option(name = "--format", usage = "usage_convertRefStorageFormat")
 	private String format = "reftable"; //$NON-NLS-1$
 
+	@Option(name = "--backup", handler = ExplicitBooleanOptionHandler.class, aliases = {
+			"-b" }, usage = "usage_convertRefStorageBackup")
+	private boolean backup = true;
+
 	/** {@inheritDoc} */
 	@Override
 	protected void run() throws Exception {
-		((FileRepository) db).convertRefStorage(format, true, true);
+		((FileRepository) db).convertRefStorage(format, true, backup);
 	}
 }
