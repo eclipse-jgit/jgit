db50a8193e146e30528781d56d2b1591c8c49a0f
Julian Ruppel <julian.ruppel@sap.com>
2020-01-04 15:10:43 +0100

    Support commit.template config property
    Adds functionality to read the git commit.template property.
    
    Bug: 446355
    Change-Id: I2bcc7121bda707bb54ed7ec6dbd86c1ad1439535
    Signed-off-by: Julian Ruppel <julian.ruppel@sap.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CommitConfig.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2020, Julian Ruppel <julian.ruppel@sap.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.lib;
+
+import java.util.Optional;
+
+import org.eclipse.jgit.lib.Config.SectionParser;
+
+/**
+ * The standard "commit" configuration parameters.
+ *
+ * @since 5.7
+ */
+public class CommitConfig {
+	/**
+	 * Key for {@link Config#get(SectionParser)}.
+	 */
+	public static final Config.SectionParser<CommitConfig> KEY = CommitConfig::new;
+
+	private String commitTemplatePath;
+
+	private CommitConfig(Config rc) {
+		commitTemplatePath = getCommitTemplatePathInternal(rc);
+	}
+
+
+	/**
+	 * Get the commit template as defined in the git variables and
+	 * configurations.
+	 *
+	 * @return the commit template as defined in the git variables and
+	 *         configurations. (might be null)
+	 * @since 5.7
+	 */
+	public Optional<String> getCommitTemplatePath() {
+		return Optional.ofNullable(commitTemplatePath);
+	}
+
+	/**
+	 *
+	 * @param rc
+	 * @return path to template from the local and global configurations
+	 */
+	private static String getCommitTemplatePathInternal(Config rc) {
+		return rc.getString(
+				ConfigConstants.CONFIG_COMMIT_SECTION, null,
+				ConfigConstants.CONFIG_KEY_COMMIT_TEMPLATE);
+	}
+
+}
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -111,7 +111,14 @@ public final class ConfigConstants {
 	public static final String CONFIG_COMMIT_SECTION = "commit";
 
 	/**
+	 * The "template" key
+	 * @since 5.7
+	 */
+	public static final String CONFIG_KEY_COMMIT_TEMPLATE = "template";
+
+	/**
 	 * The "gpgSign" key
+	 *
 	 * @since 5.2
 	 */
 	public static final String CONFIG_KEY_GPGSIGN = "gpgSign";
