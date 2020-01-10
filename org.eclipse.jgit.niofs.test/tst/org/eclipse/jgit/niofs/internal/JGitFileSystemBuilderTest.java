447e2f6098ee383322a3a4ebe3c38dffa96f873b
Alexandre Porcelli <alex@porcelli.me>
2020-01-11 00:27:42 +0100

    [WIP] NIO2 filesystem based on JGit storage
    - original implementation [1]
    - relicensed from Apache 2.0 to EDL 1.0
    - packages renamed to org.eclipse.jgit.niofs.*
    - non-API packages renamed to org.eclipse.jgit.niofs.internal.*
    - build refactored to JGit Maven build also supporting OSGi runtime
    - update dependency versions to the versions used in JGit already
    - move definition of Maven dependency versions to dependency management
      section in parent pom
    - replace SystemUtils.IS_OS_WINDOWS by SystemReader to get rid of
      dependency to commons-lang3
    
    [1] https://github.com/porcelli/jgit-nio2
    
    Change-Id: Ic8712a091e3941634c80b19ba0e316d9c98dae3e
    Signed-off-by: Alexandre Porcelli <alex@porcelli.me>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.niofs.test/tst/org/eclipse/jgit/niofs/internal/JGitFileSystemBuilderTest.java
@@ -0,0 +1,47 @@
+package org.eclipse.jgit.niofs.internal;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+
+import org.eclipse.jgit.niofs.JGitFileSystemBuilder;
+import org.eclipse.jgit.util.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+@Ignore
+public class JGitFileSystemBuilderTest {
+
+    @AfterClass
+    @BeforeClass
+    public static void cleanup() {
+        try {
+            FileUtils.delete(new File(".niogit"),
+                             FileUtils.RECURSIVE);
+        } catch (IOException ex) {
+            //ignore
+        }
+    }
+
+    @Test
+    public void testSimpleBuilderSample() throws IOException {
+        final FileSystem fs = JGitFileSystemBuilder.newFileSystem("myrepo");
+
+        Path foo = fs.getPath("/foo");
+        Files.createDirectory(foo);
+
+        Path hello = foo.resolve("hello.txt"); // /foo/hello.txt
+
+        Files.write(hello, Collections.singletonList("hello world"), StandardCharsets.UTF_8);
+
+        assertEquals("hello world", Files.readAllLines(hello).get(0));
+    }
+}
