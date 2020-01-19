8c4f608cc332734c5f24657040f3c04ee3a2a6af
Andre Bossert <andre.bossert@siemens.com>
2020-01-19 20:57:23 +0100

    Adapt diff- and merge tool code for PGM and EGit usage
    see: https://git-scm.com/docs/git-mergetool
    
    * DiffTools and MergeTools
      * store FS, gitDir and workTree for usage without
    git repository (for EGit preferences)
      * add getUserDefinedToolNames() and getPredefinedToolNames()
      * replace getToolNames() with getAllToolNames() that combines the two
    lists and put default tool name (diff.tool or merge.tool) as first
    element (for EGit preferences)
    * FileElement: refactoring of getFile() and friends to have midName
    (LOCAL, REMOTE etc.) always added to the temp file name (also for EGit)
    * FileElement: added directory attribute that is used in getFile() to
    return path with workDir as parent
    * DiffTool and MergeTool
      * added errw.flush(), because sometimes stderr is not printed in case
    of die()
      * print e.getMessage() always to stderr
    * Moved toolname and prompt logic into managers
    
    Bug: 356832
    Change-Id: I71e7f4dc362169a7612ca4f6546a021bc4b2b5f4
    Signed-off-by: Andre Bossert <andre.bossert@siemens.com>
    Signed-off-by: Tim Neumann <Tim.Neumann@advantest.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/diffmergetool/DiffTools.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/diffmergetool/DiffTools.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018-2022, Andre Bossert <andre.bossert@siemens.com>
+ * Copyright (C) 2019, Tim Neumann <tim.neumann@advantest.com>
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
@@ -10,23 +11,31 @@
 
 package org.eclipse.jgit.internal.diffmergetool;
 
-import java.util.TreeMap;
-import java.util.Collections;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.lib.internal.BooleanTriState;
+import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FS.ExecutionResult;
-import org.eclipse.jgit.util.StringUtils;
 
 /**
  * Manages diff tools.
  */
 public class DiffTools {
 
-	private final Repository repo;
+	private final FS fs;
+
+	private final File gitDir;
+
+	private final File workTree;
 
 	private final DiffToolConfig config;
 
@@ -41,10 +50,103 @@ public class DiffTools {
 	 *            the repository
 	 */
 	public DiffTools(Repository repo) {
-		this.repo = repo;
-		config = repo.getConfig().get(DiffToolConfig.KEY);
+		this(repo, repo.getConfig());
+	}
+
+	/**
+	 * Creates the external merge-tools manager for given configuration.
+	 *
+	 * @param config
+	 *            the git configuration
+	 */
+	public DiffTools(StoredConfig config) {
+		this(null, config);
+	}
+
+	private DiffTools(Repository repo, StoredConfig config) {
+		this.config = config.get(DiffToolConfig.KEY);
+		this.gitDir = repo == null ? null : repo.getDirectory();
+		this.fs = repo == null ? FS.DETECTED : repo.getFS();
+		this.workTree = repo == null ? null : repo.getWorkTree();
 		predefinedTools = setupPredefinedTools();
-		userDefinedTools = setupUserDefinedTools(config, predefinedTools);
+		userDefinedTools = setupUserDefinedTools(predefinedTools);
+	}
+
+	/**
+	 * Compare two versions of a file.
+	 *
+	 * @param localFile
+	 *            The local/left version of the file.
+	 * @param remoteFile
+	 *            The remote/right version of the file.
+	 * @param toolName
+	 *            Optionally the name of the tool to use. If not given the
+	 *            default tool will be used.
+	 * @param prompt
+	 *            Optionally a flag whether to prompt the user before compare.
+	 *            If not given the default will be used.
+	 * @param gui
+	 *            A flag whether to prefer a gui tool.
+	 * @param trustExitCode
+	 *            Optionally a flag whether to trust the exit code of the tool.
+	 *            If not given the default will be used.
+	 * @param promptHandler
+	 *            The handler to use when needing to prompt the user if he wants
+	 *            to continue.
+	 * @param noToolHandler
+	 *            The handler to use when needing to inform the user, that no
+	 *            tool is configured.
+	 * @return the optioanl result of executing the tool if it was executed
+	 * @throws ToolException
+	 *             when the tool fails
+	 */
+	public Optional<ExecutionResult> compare(FileElement localFile,
+			FileElement remoteFile, Optional<String> toolName,
+			BooleanTriState prompt, boolean gui, BooleanTriState trustExitCode,
+			PromptContinueHandler promptHandler,
+			InformNoToolHandler noToolHandler) throws ToolException {
+
+		String toolNameToUse;
+
+		if (toolName.isPresent()) {
+			toolNameToUse = toolName.get();
+		} else {
+			toolNameToUse = getDefaultToolName(gui);
+
+			if (toolNameToUse == null || toolNameToUse.isEmpty()) {
+				noToolHandler.inform(new ArrayList<>(predefinedTools.keySet()));
+				toolNameToUse = getFirstAvailableTool();
+			}
+		}
+
+		boolean doPrompt;
+		if (prompt != BooleanTriState.UNSET) {
+			doPrompt = prompt == BooleanTriState.TRUE;
+		} else {
+			doPrompt = isInteractive();
+		}
+
+		if (doPrompt) {
+			if (!promptHandler.prompt(toolNameToUse)) {
+				return Optional.empty();
+			}
+		}
+
+		boolean trust;
+		if (trustExitCode != BooleanTriState.UNSET) {
+			trust = trustExitCode == BooleanTriState.TRUE;
+		} else {
+			trust = config.isTrustExitCode();
+		}
+
+		ExternalDiffTool tool = getTool(toolNameToUse);
+		if (tool == null) {
+			throw new ToolException(
+					"External diff tool is not defined: " + toolNameToUse); //$NON-NLS-1$
+		}
+
+		return Optional.of(
+				compare(localFile, remoteFile, tool, trust));
 	}
 
 	/**
@@ -54,56 +156,70 @@ public class DiffTools {
 	 *            the local file element
 	 * @param remoteFile
 	 *            the remote file element
-	 * @param mergedFile
-	 *            the merged file element, it's path equals local or remote
-	 *            element path
-	 * @param toolName
-	 *            the selected tool name (can be null)
-	 * @param prompt
-	 *            the prompt option
-	 * @param gui
-	 *            the GUI option
+	 * @param tool
+	 *            the selected tool
 	 * @param trustExitCode
 	 *            the "trust exit code" option
 	 * @return the execution result from tool
 	 * @throws ToolException
 	 */
 	public ExecutionResult compare(FileElement localFile,
-			FileElement remoteFile, FileElement mergedFile, String toolName,
-			BooleanTriState prompt, BooleanTriState gui,
-			BooleanTriState trustExitCode) throws ToolException {
+			FileElement remoteFile, ExternalDiffTool tool,
+			boolean trustExitCode) throws ToolException {
 		try {
 			// prepare the command (replace the file paths)
-			String command = ExternalToolUtils.prepareCommand(
-					guessTool(toolName, gui).getCommand(), localFile,
-					remoteFile, mergedFile, null);
+			String command = ExternalToolUtils.prepareCommand(tool.getCommand(),
+					localFile, remoteFile, null, null);
 			// prepare the environment
-			Map<String, String> env = ExternalToolUtils.prepareEnvironment(repo,
-					localFile, remoteFile, mergedFile, null);
-			boolean trust = config.isTrustExitCode();
-			if (trustExitCode != BooleanTriState.UNSET) {
-				trust = trustExitCode == BooleanTriState.TRUE;
-			}
+			Map<String, String> env = ExternalToolUtils.prepareEnvironment(
+					gitDir, localFile, remoteFile, null, null);
 			// execute the tool
-			CommandExecutor cmdExec = new CommandExecutor(repo.getFS(), trust);
-			return cmdExec.run(command, repo.getWorkTree(), env);
+			CommandExecutor cmdExec = new CommandExecutor(fs, trustExitCode);
+			return cmdExec.run(command, workTree, env);
 		} catch (IOException | InterruptedException e) {
 			throw new ToolException(e);
 		} finally {
 			localFile.cleanTemporaries();
 			remoteFile.cleanTemporaries();
-			mergedFile.cleanTemporaries();
 		}
 	}
 
 	/**
-	 * @return the tool names
+	 * Get user defined tool names.
+	 *
+	 * @return the user defined tool names
 	 */
-	public Set<String> getToolNames() {
-		return config.getToolNames();
+	public Set<String> getUserDefinedToolNames() {
+		return userDefinedTools.keySet();
 	}
 
 	/**
+	 * Get predefined tool names.
+	 *
+	 * @return the predefined tool names
+	 */
+	public Set<String> getPredefinedToolNames() {
+		return predefinedTools.keySet();
+	}
+
+	/**
+	 * Get all tool names.
+	 *
+	 * @return the all tool names (default or available tool name is the first
+	 *         in the set)
+	 */
+	public Set<String> getAllToolNames() {
+		String defaultName = getDefaultToolName(false);
+		if (defaultName == null) {
+			defaultName = getFirstAvailableTool();
+		}
+		return ExternalToolUtils.createSortedToolSet(defaultName,
+				getUserDefinedToolNames(), getPredefinedToolNames());
+	}
+
+	/**
+	 * Get user defined tools map.
+	 *
 	 * @return the user defined tools
 	 */
 	public Map<String, ExternalDiffTool> getUserDefinedTools() {
@@ -111,6 +227,8 @@ public class DiffTools {
 	}
 
 	/**
+	 * Get predefined tools map.
+	 *
 	 * @param checkAvailability
 	 *            true: for checking if tools can be executed; ATTENTION: this
 	 *            check took some time, do not execute often (store the map for
@@ -124,57 +242,47 @@ public class DiffTools {
 		if (checkAvailability) {
 			for (ExternalDiffTool tool : predefinedTools.values()) {
 				PreDefinedDiffTool predefTool = (PreDefinedDiffTool) tool;
-				predefTool.setAvailable(ExternalToolUtils.isToolAvailable(repo,
-						predefTool.getPath()));
+				predefTool.setAvailable(ExternalToolUtils.isToolAvailable(fs,
+						gitDir, workTree, predefTool.getPath()));
 			}
 		}
 		return Collections.unmodifiableMap(predefinedTools);
 	}
 
 	/**
+	 * Get first available tool name.
+	 *
 	 * @return the name of first available predefined tool or null
 	 */
 	public String getFirstAvailableTool() {
-		String name = null;
 		for (ExternalDiffTool tool : predefinedTools.values()) {
-			if (ExternalToolUtils.isToolAvailable(repo, tool.getPath())) {
-				name = tool.getName();
-				break;
+			if (ExternalToolUtils.isToolAvailable(fs, gitDir, workTree,
+					tool.getPath())) {
+				return tool.getName();
 			}
 		}
-		return name;
+		return null;
 	}
 
 	/**
+	 * Get default (gui-)tool name.
+	 *
 	 * @param gui
 	 *            use the diff.guitool setting ?
 	 * @return the default tool name
 	 */
-	public String getDefaultToolName(BooleanTriState gui) {
-		return gui != BooleanTriState.UNSET ? "my_gui_tool" //$NON-NLS-1$
+	public String getDefaultToolName(boolean gui) {
+		return gui ? config.getDefaultGuiToolName()
 				: config.getDefaultToolName();
 	}
 
 	/**
+	 * Is interactive diff (prompt enabled) ?
+	 *
 	 * @return is interactive (config prompt enabled) ?
 	 */
 	public boolean isInteractive() {
 		return config.isPrompt();
-	}
-
-	private ExternalDiffTool guessTool(String toolName, BooleanTriState gui)
-			throws ToolException {
-		if (StringUtils.isEmptyOrNull(toolName)) {
-			toolName = getDefaultToolName(gui);
-		}
-		ExternalDiffTool tool = null;
-		if (!StringUtils.isEmptyOrNull(toolName)) {
-			tool = getTool(toolName);
-		}
-		if (tool == null) {
-			throw new ToolException("Unknown diff tool '" + toolName + "'"); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-		return tool;
 	}
 
 	private ExternalDiffTool getTool(final String name) {
@@ -193,10 +301,10 @@ public class DiffTools {
 		return tools;
 	}
 
-	private static Map<String, ExternalDiffTool> setupUserDefinedTools(
-			DiffToolConfig cfg, Map<String, ExternalDiffTool> predefTools) {
+	private Map<String, ExternalDiffTool> setupUserDefinedTools(
+			Map<String, ExternalDiffTool> predefTools) {
 		Map<String, ExternalDiffTool> tools = new TreeMap<>();
-		Map<String, ExternalDiffTool> userTools = cfg.getTools();
+		Map<String, ExternalDiffTool> userTools = config.getTools();
 		for (String name : userTools.keySet()) {
 			ExternalDiffTool userTool = userTools.get(name);
 			// if difftool.<name>.cmd is defined we have user defined tool
