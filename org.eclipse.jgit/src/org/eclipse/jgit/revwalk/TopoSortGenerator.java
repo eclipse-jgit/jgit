6cbe25dfa0e90e51019e3f43bc90c40455ea11d2
Alex Spradlin <alexaspradlin@google.com>
2020-02-11 13:27:51 -0800

    RevWalk: Stop mixing lines of history in topo sort
    The current topological sort algorithm in TopoSortGenerator for RevWalk
    may mix multiple lines of history, producing results that differ from
    C git's. This is caused by the use of the TOPO_DELAY flag, which
    prevents adding parent commits to the front of the priority queue unless
    they have been previously popped.
    
    Allow unpopping parent commits without requiring that they be popped
    beforehand using the TOPO_QUEUED flag. Modify tests that assert the
    incorrect TopoSortGenerator output.
    
    Change-Id: I4ee03c7a8e5265d61230b2a01ae3858745b2432b
    Signed-off-by: Alex Spradlin <alexaspradlin@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TopoSortGenerator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TopoSortGenerator.java
@@ -17,7 +17,9 @@ import org.eclipse.jgit.errors.MissingObjectException;
 
 /** Sorts commits in topological order. */
 class TopoSortGenerator extends Generator {
-	private static final int TOPO_DELAY = RevWalk.TOPO_DELAY;
+	private static final int TOPO_QUEUED = RevWalk.TOPO_QUEUED;
+
+        private static final int UNINTERESTING = RevWalk.UNINTERESTING;
 
 	private final FIFORevQueue pending;
 
@@ -48,11 +50,15 @@ class TopoSortGenerator extends Generator {
 				break;
 			}
 			for (RevCommit p : c.parents) {
-				p.inDegree++;
+                                if ((c.flags & UNINTERESTING) == 0) {
+                                        p.inDegree++;
+                                }
+
 				if (firstParent) {
 					break;
 				}
 			}
+                        c.flags |= TOPO_QUEUED;
 			pending.add(c);
 		}
 	}
@@ -79,26 +85,33 @@ class TopoSortGenerator extends Generator {
 				// At least one of our children is missing. We delay
 				// production until all of our children are output.
 				//
-				c.flags |= TOPO_DELAY;
 				continue;
 			}
 
-			// All of our children have already produced,
-			// so it is OK for us to produce now as well.
-			//
+			if ((c.flags & TOPO_QUEUED) == 0) {
+				// c is a parent that already produced or a parent that
+				// was never in the priority queue and should never produce.
+				//
+				continue;
+			}
+
 			for (RevCommit p : c.parents) {
-				if (--p.inDegree == 0 && (p.flags & TOPO_DELAY) != 0) {
+				// All of our children have already produced,
+				// so it is OK for us to produce now as well.
+				//
+				if (--p.inDegree == 0 && (p.flags & TOPO_QUEUED) != 0) {
 					// This parent tried to come before us, but we are
 					// his last child. unpop the parent so it goes right
 					// behind this child.
 					//
-					p.flags &= ~TOPO_DELAY;
 					pending.unpop(p);
 				}
 				if (firstParent) {
 					break;
 				}
 			}
+
+                        c.flags &= ~TOPO_QUEUED;
 			return c;
 		}
 	}
