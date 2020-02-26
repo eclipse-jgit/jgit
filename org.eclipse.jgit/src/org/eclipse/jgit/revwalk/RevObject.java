e40c38ab08837473375c571c2f07ab680fc1985d
Alex Spradlin <alexaspradlin@google.com>
2020-02-26 13:49:41 -0500

    Revert "RevWalk: stop mixing lines of history in topo sort"
    This reverts commit b5e764abd21bd4593287361a625ecc49bc0efd77.
    
    PlotWalk uses the TopoSortGenerator, which is causing problems for EGit users
    who rely on the emission of commits being somewhat based on date as in the
    previous topo-sort algorithm.
    
    Bug: 560529
    Change-Id: I3dbd3598a7aeb960de3fc39352699b4f11a8c226
    Signed-off-by: Alex Spradlin <alexaspradlin@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevObject.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevObject.java
@@ -151,7 +151,7 @@ public abstract class RevObject extends ObjectIdOwnerMap.Entry {
 	 *            buffer to append a debug description of core RevFlags onto.
 	 */
 	protected void appendCoreFlags(StringBuilder s) {
-		s.append((flags & RevWalk.TOPO_QUEUED) != 0 ? 'o' : '-');
+		s.append((flags & RevWalk.TOPO_DELAY) != 0 ? 'o' : '-');
 		s.append((flags & RevWalk.TEMP_MARK) != 0 ? 't' : '-');
 		s.append((flags & RevWalk.REWRITE) != 0 ? 'r' : '-');
 		s.append((flags & RevWalk.UNINTERESTING) != 0 ? 'u' : '-');
