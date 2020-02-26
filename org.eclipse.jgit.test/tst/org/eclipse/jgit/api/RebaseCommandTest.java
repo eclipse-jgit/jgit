5d74b0135e842aeb7515ee2a608205853e7d1e04
Alex Spradlin <alexaspradlin@google.com>
2020-02-26 13:49:41 -0500

    Revert "RevWalk: stop mixing lines of history in topo sort"
    This reverts commit b5e764abd21bd4593287361a625ecc49bc0efd77.
    
    Plotwalk uses the TopoSortGenerator, which is causing problems for EGit users
    who rely on the emission of commits being somewhat based on date as in the
    previous topo-sort algorithm.
    
    Bug: 560529
    
    Change-Id: I3dbd3598a7aeb960de3fc39352699b4f11a8c226
    Signed-off-by: Alex Spradlin <alexaspradlin@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RebaseCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/RebaseCommandTest.java
@@ -563,10 +563,10 @@ public class RebaseCommandTest extends RepositoryTestCase {
 			RevCommit newD = rw.next();
 			assertDerivedFrom(newD, d);
 			assertEquals(2, newD.getParentCount());
-			RevCommit newE = rw.next();
-			assertEquals(e, newE);
 			RevCommit newC = rw.next();
 			assertDerivedFrom(newC, c);
+			RevCommit newE = rw.next();
+			assertEquals(e, newE);
 			assertEquals(newC, newD.getParent(0));
 			assertEquals(e, newD.getParent(1));
 			assertEquals(g, rw.next());
