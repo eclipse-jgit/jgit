90bfebd27479a0c4dec9b957e4231f68affebc7f
Yunjie Li <yunjieli@google.com>
2020-03-25 10:59:06 -0700

    ReceivePackStats: Add size and count of unnecessary pushed objects.
    Since there is no negotiation for a push, the client is probably sending redundant objects and bytes which already exist in the server.
    
    Add more metrics in the stats to quantify it. Duplicated size and number to measure the size and the number of duplicated objects which should not be pushed.
    
    Change-Id: Iaacd4761ee9366a0a7ec4e26c508eff45c8744de
    Signed-off-by: Yunjie Li <yunjieli@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PackParser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PackParser.java
@@ -1147,6 +1147,8 @@ public abstract class PackParser {
 					}
 					sz -= n;
 				}
+				stats.addNumBytesDuplicated(sz);
+				stats.incrementDuplicatedObjects();
 			}
 		} catch (MissingObjectException notLocal) {
 			// This is OK, we don't have a copy of the object locally
