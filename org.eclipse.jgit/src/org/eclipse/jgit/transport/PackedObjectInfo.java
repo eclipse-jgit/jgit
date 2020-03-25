a92ad2797566e40c96fd24421535361125813868
Yunjie Li <yunjieli@google.com>
2020-03-25 10:59:06 -0700

    ReceivePackStats: Add size and count of unnecessary pushed objects.
    Since there is no negotiation for a push, the client is probably sending
    redundant objects and bytes which already exist in the server.
    
    Add more metrics in the stats to quantify it. Duplicated size and number
    to measure the size and the number of duplicated objects which should
    not be pushed.
    
    Change-Id: Iaacd4761ee9366a0a7ec4e26c508eff45c8744de
    Signed-off-by: Yunjie Li <yunjieli@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PackedObjectInfo.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PackedObjectInfo.java
@@ -29,6 +29,9 @@ public class PackedObjectInfo extends ObjectIdOwnerMap.Entry {
 
 	private int type = Constants.OBJ_BAD;
 
+	// Size of the object before inflating;
+	private long size;
+
 	PackedObjectInfo(final long headerOffset, final int packedCRC,
 			final AnyObjectId id) {
 		super(id);
@@ -107,5 +110,13 @@ public class PackedObjectInfo extends ObjectIdOwnerMap.Entry {
 	 */
 	public void setType(int type) {
 		this.type = type;
+	}
+
+	void setSize(long sizeBeforeInflating) {
+		this.size = sizeBeforeInflating;
+	}
+
+	long getSize() {
+		return size;
 	}
 }
