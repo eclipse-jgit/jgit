b80efe80b83b86028bbdb4ccbf3c21e5240b1d00
Michael Dardis <git@md-5.net>
2020-04-24 22:41:39 +0200

    Decouple JSch from JGit Core
    Motivation: JSch serves as 'default' implementations of the SSH
    transport. If a client application does not use it then there is no need
    to pull in this dependency.
    
    Move the classes depending on JSch to an OSGi fragment extending the
    org.eclipse.jgit bundle and keep them in the same package as before
    since moving them to another package would break API. Defer moving them
    to a separate package to the next major release.
    
    Add a new feature org.eclipse.jgit.ssh.jsch feature to enable
    installation. With that users can now decide which of the ssh client
    integrations they want to install.
    
    Bug: 553625
    Change-Id: I5979c8a9dbbe878a2e8ac0fbfde7230059d74dc2
    Signed-off-by: Michael Dardis <git@md-5.net>
    Signed-off-by: David Ostrovsky <david@ostrovsky.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -655,7 +655,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String sourceRefNotSpecifiedForRefspec;
 	/***/ public String squashCommitNotUpdatingHEAD;
 	/***/ public String sshCommandFailed;
-	/***/ public String sshUserNameError;
 	/***/ public String sslFailureExceptionMessage;
 	/***/ public String sslFailureInfo;
 	/***/ public String sslFailureCause;
@@ -716,7 +715,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String transportProtoSSH;
 	/***/ public String transportProtoTest;
 	/***/ public String transportProvidedRefWithNoObjectId;
-	/***/ public String transportSSHRetryInterrupt;
 	/***/ public String treeEntryAlreadyExists;
 	/***/ public String treeFilterMarkerTooManyFilters;
 	/***/ public String treeWalkMustHaveExactlyTwoTrees;
