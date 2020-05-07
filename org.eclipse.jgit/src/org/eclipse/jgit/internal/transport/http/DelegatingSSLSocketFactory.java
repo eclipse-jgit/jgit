9e676dd69db1d6c6da6216d377685781eb447371
Demetr Starshov <dstarshov@google.com>
2020-05-06 17:36:34 -0700

    Moving transport/internal -> internal/transport
    Moving transport related internal classes into dedicated subpackage in
    o/e/j/internal package.
    
    Signed-off-by: Demetr Starshov <dstarshov@google.com>
    Change-Id: I21ed029d359f5f7d8298f102efbb4b1dcdf404ad

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/transport/http/DelegatingSSLSocketFactory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/transport/http/DelegatingSSLSocketFactory.java
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-package org.eclipse.jgit.transport.internal;
+package org.eclipse.jgit.internal.transport.http;
 
 import java.io.IOException;
 import java.net.InetAddress;
