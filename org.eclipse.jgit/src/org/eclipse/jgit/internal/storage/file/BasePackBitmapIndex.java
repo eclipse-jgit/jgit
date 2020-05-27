06a90fdf2e593897c20dae66081e903e4b1b7574
Yunjie Li <yunjieli@google.com>
2020-05-27 10:29:20 -0700

    Revert "PackBitmapIndex: Not buffer inflated bitmap in BasePackBitmapIndex"
    This reverts commit 3aee92478c2cbc67cd921533437b824e43ed9798, which
    increased fetch latency significantly.
    
    Change-Id: Id31a94dff83bf7ab2121718ead819bd08306a0b6
    Signed-off-by: Yunjie Li <yunjieli@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/BasePackBitmapIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/BasePackBitmapIndex.java
@@ -72,6 +72,7 @@ abstract class BasePackBitmapIndex extends PackBitmapIndex {
 				if (r instanceof EWAHCompressedBitmap) {
 					out = out.xor((EWAHCompressedBitmap) r);
 					out.trim();
+					bitmapContainer = out;
 					return out;
 				}
 				xb = (XorCompressedBitmap) r;
