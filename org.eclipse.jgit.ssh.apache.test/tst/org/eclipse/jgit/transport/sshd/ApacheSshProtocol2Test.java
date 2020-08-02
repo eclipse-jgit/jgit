e0cc1e46f88c6ee7c2b827402c48414fa45ebb4f
Thomas Wolf <thomas.wolf@paranor.ch>
2020-08-02 19:22:05 +0200

    Client-side protocol V2 support for fetching
    Make all transports request protocol V2 when fetching. Depending on
    the transport, set the GIT_PROTOCOL environment variable (file and
    ssh), pass the Git-Protocol header (http), or set the hidden
    "\0version=2\0" (git anon). We'll fall back to V0 if the server
    doesn't reply with a version 2 answer.
    
    A user can control which protocol the client requests via the git
    config protocol.version; if not set, JGit requests protocol V2 for
    fetching. Pushing always uses protocol V0 still.
    
    In the API, there is only a new Transport.openFetch() version that
    takes a collection of RefSpecs plus additional patterns to construct
    the Ref prefixes for the "ls-refs" command in protocol V2. If none
    are given, the server will still advertise all refs, even in protocol
    V2.
    
    BasePackConnection.readAdvertisedRefs() handles falling back to
    protocol V0. It newly returns true if V0 was used and the advertised
    refs were read, and false if V2 is used and an explicit "ls-refs" is
    needed. (This can't be done transparently inside readAdvertisedRefs()
    because a "stateless RPC" transport like TransportHttp may need to
    open a new connection for writing.)
    
    BasePackFetchConnection implements the changes needed for the protocol
    V2 "fetch" command (stateless protocol, simplified ACK handling,
    delimiters, section headers).
    
    In TransportHttp, change readSmartHeaders() to also recognize the
    "version 2" packet line as a valid smart server indication.
    
    Adapt tests, and run all the HTTP tests not only with both HTTP
    connection factories (JDK and Apache HttpClient) but also with both
    protocol V0 and V2. The SSH test are much slower and much more focused
    on the SSH protocol and SSH key handling. Factor out two very simple
    cloning and pulling tests and make those run with protocol V2.
    
    Bug: 553083
    Change-Id: I357c7f5daa7efb2872f1c64ee6f6d54229031ae1
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.jgit.ssh.apache.test/tst/org/eclipse/jgit/transport/sshd/ApacheSshProtocol2Test.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Thomas Wolf <thomas.wolf@paranor.ch> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.transport.sshd;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import org.eclipse.jgit.junit.ssh.SshBasicTestBase;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.util.FS;
+
+public class ApacheSshProtocol2Test extends SshBasicTestBase {
+
+	@Override
+	protected SshSessionFactory createSessionFactory() {
+		SshdSessionFactory result = new SshdSessionFactory(new JGitKeyCache(),
+				null);
+		// The home directory is mocked at this point!
+		result.setHomeDirectory(FS.DETECTED.userHome());
+		result.setSshDirectory(sshDir);
+		return result;
+	}
+
+	@Override
+	protected void installConfig(String... config) {
+		File configFile = new File(sshDir, Constants.CONFIG);
+		if (config != null) {
+			try {
+				Files.write(configFile.toPath(), Arrays.asList(config));
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
+			}
+		}
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		StoredConfig config = ((Repository) db).getConfig();
+		config.setInt("protocol", null, "version", 2);
+		config.save();
+	}
+}
