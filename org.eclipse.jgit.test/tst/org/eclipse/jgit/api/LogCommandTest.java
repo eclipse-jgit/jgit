aa2191f171958faf785957b2a5a28d063b1cc64f
Ryan Zhao <raistlinzx@gmail.com>
2020-11-18 19:40:02 +0800

    LogCommand: Make revFilter/skip/maxCount effective at the same time
    Originally, setRevFilter() and setSkip()/setMaxCount() could not be
    called at the same time. the revFilter will be lost in method call().
    should make all of them effective.
    
    Change-Id: I555af4255ab4447e819af824887d94087ee90ca8
    Signed-off-by: Ryan Zhao <raistlinzx@gmail.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/LogCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/LogCommandTest.java
@@ -232,6 +232,33 @@ public class LogCommandTest extends RepositoryTestCase {
 		assertFalse(i.hasNext());
 	}
 
+	@Test
+	public void logNoMergeCommitsWithSkipAndMaxCount() throws Exception {
+		setCommitsAndMerge();
+		Git git = Git.wrap(db);
+
+		Iterable<RevCommit> commits = git.log().all().call();
+		Iterator<RevCommit> i = commits.iterator();
+		RevCommit commit = i.next();
+		assertEquals("merge s0 with m1", commit.getFullMessage());
+		commit = i.next();
+		assertEquals("s0", commit.getFullMessage());
+		commit = i.next();
+		assertEquals("m1", commit.getFullMessage());
+		commit = i.next();
+		assertEquals("m0", commit.getFullMessage());
+		assertFalse(i.hasNext());
+
+		commits = git.log().setRevFilter(RevFilter.NO_MERGES).setSkip(2)
+				.setMaxCount(1)
+				.call();
+		i = commits.iterator();
+		commit = i.next();
+		assertEquals("m0", commit.getFullMessage());
+
+		assertFalse(i.hasNext());
+	}
+
 	private void setCommitsAndMerge() throws Exception {
 		Git git = Git.wrap(db);
 		writeTrashFile("file1", "1\n2\n3\n4\n");
