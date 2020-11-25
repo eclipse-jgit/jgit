b6775e7bc3593ef5f2716f8380ad0f8a504d80b2
Matthias Sohn <matthias.sohn@sap.com>
2020-11-25 17:00:09 +0100

    Ensure that GC#deleteOrphans respects pack lock
    If pack or index files are guarded by a pack lock (.keep file)
    deleteOrphans() should not touch the respective files protected by the
    lock file. Otherwise it may interfere with PackInserter concurrently
    inserting a new pack file and its index.
    
    The problem was caused by the following race.
    
    All mentioned files are located in  "objects/pack/".
    File endings relevant in "pack" dir:
      .pack
      .keep
      .idx
      .bitmap
    
    When ReceivePack receives a pack file it executes the following steps:
    
    ReceivePack.service():
      receivePackAndCheckConnectivity():
        receivePack():
          receive the pack
          parse the pack, returns packLock (.keep file)
          PackInserter.flush():
            write tmpPck file:  "insert_<random>.pack"
            write tmpIdx file:  "insert_<random>.idx"
            real pack name: "pack-<SHA1>.pack"
            real index name: "pack-<SHA1>.idx"
            atomic rename tmpPack to realPack
            atomic rename tmpIdx to tmpIdx
      execute commands
      unlock pack by removing .keep file
      trigger auto gc if enabled
    
    When PackInserter.flush() renames the temporary pack to the final
    "pack-xxx.pack" file  the temporary pack index file "insert_xxx.idx"
    has no matching .pack file with the same base name for a short interval.
    If deleteOrphans() ran during that interval it deduced the pack index
    file was orphaned. Subsequently the missing pack index caused
    MissingObjectExceptions since objects contained in the pack couldn't be
    looked up anymore.
    
    Bug: https://bugs.chromium.org/p/gerrit/issues/detail?id=13544
    Change-Id: I559c81e4b1d7c487f92a751bd78b987d32c98719
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
@@ -115,6 +115,8 @@ public class GC {
 
 	private static final String INDEX_EXT = "." + PackExt.INDEX.getExtension(); //$NON-NLS-1$
 
+	private static final String KEEP_EXT = "." + PackExt.KEEP.getExtension(); //$NON-NLS-1$
+
 	private static final int DEFAULT_AUTOPACKLIMIT = 50;
 
 	private static final int DEFAULT_AUTOLIMIT = 6700;
@@ -961,7 +963,10 @@ public class GC {
 			fileNames = files.map(path -> path.getFileName().toString())
 					.filter(name -> (name.endsWith(PACK_EXT)
 							|| name.endsWith(BITMAP_EXT)
-							|| name.endsWith(INDEX_EXT)))
+							|| name.endsWith(INDEX_EXT)
+							|| name.endsWith(KEEP_EXT)))
+					// sort files with same base name in the order:
+					// .pack, .keep, .index, .bitmap to avoid look ahead
 					.sorted(Collections.reverseOrder())
 					.collect(Collectors.toList());
 		} catch (IOException e1) {
@@ -972,11 +977,16 @@ public class GC {
 		}
 
 		String base = null;
+		boolean keep = false;
 		for (String n : fileNames) {
-			if (n.endsWith(PACK_EXT)) {
+			if (n.endsWith(PACK_EXT) || n.endsWith(KEEP_EXT)) {
 				base = n.substring(0, n.lastIndexOf('.'));
+				keep = true;
 			} else {
-				if (base == null || !n.startsWith(base)) {
+				if ((base != null) && !n.startsWith(base)) {
+					keep = false;
+				}
+				if (!keep && (base == null || !n.startsWith(base))) {
 					try {
 						Files.delete(packDir.resolve(n));
 					} catch (IOException e) {
