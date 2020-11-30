65df54abf848c343f62fd1e71001019ac9946096
Thomas Wolf <thomas.wolf@paranor.ch>
2020-11-30 12:14:22 +0100

    TransportHttp: shared SSLContext during fetch or push
    TransportHttp makes several HTTP requests. The SSLContext and socket
    factory must be shared over these requests, otherwise authentication
    information may not be propagated correctly from one request to the
    next. This is important for authentication mechanisms that rely on
    client-side state, like NEGOTIATE (either NTLM, if the underlying HTTP
    library supports it, or Kerberos). In particular, SPNEGO cannot
    authenticate on a POST request; the authentication must come from the
    initial GET request, which implies that the POST request must use the
    same SSLContext and socket factory that was used for the GET.
    
    Change the way HTTPS connections are configured. Introduce the concept
    of a GitSession, which is a client-side HTTP session over several HTTPS
    requests. TransportHttp creates such a session and uses it to configure
    all HTTP requests during that session (fetch or push). This gives a way
    to abstract away the differences between JDK and Apache HTTP connections
    and to configure SSL setup outside.
    
    A GitSession can maintain state and thus give all HTTP requests in a
    session the same socket factory.
    
    Introduce an extension interface HttpConnectionFactory2 that adds a
    method to obtain a new GitSession. Implement this for both existing
    HTTP connection factories. Change TransportHttp to use the new
    GitSession to configure HTTP connections.
    
    The old methods for disabling SSL verification still exist to support
    possibly external connection and connection factory implementations
    that do not make use of the new GitSession yet.
    
    Bug: 535850
    Change-Id: Iedf67464e4e353c1883447c13c86b5a838e678f1
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/http/HttpConnectionFactory2.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Thomas Wolf <thomas.wolf@paranor.ch> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.transport.http;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.eclipse.jgit.annotations.NonNull;
+
+/**
+ * A {@link HttpConnectionFactory} that supports client-side sessions that can
+ * maintain state and configure connections.
+ *
+ * @since 5.11
+ */
+public interface HttpConnectionFactory2 extends HttpConnectionFactory {
+
+	/**
+	 * Creates a new {@link GitSession} instance that can be used with
+	 * connections created by this {@link HttpConnectionFactory} instance.
+	 *
+	 * @return a new {@link GitSession}
+	 */
+	@NonNull
+	GitSession newSession();
+
+	/**
+	 * A {@code GitSession} groups the multiple HTTP connections
+	 * {@link org.eclipse.jgit.transport.TransportHttp TransportHttp} uses for
+	 * the requests it makes during a git fetch or push. A {@code GitSession}
+	 * can maintain client-side HTTPS state and can configure individual
+	 * connections.
+	 */
+	interface GitSession {
+
+		/**
+		 * Configure a just created {@link HttpConnection}.
+		 *
+		 * @param connection
+		 *            to configure; created by the same
+		 *            {@link HttpConnectionFactory} instance
+		 * @param sslVerify
+		 *            whether SSL is to be verified
+		 * @return the configured {@connection}
+		 * @throws IOException
+		 *             if the connection cannot be configured
+		 * @throws GeneralSecurityException
+		 *             if the connection cannot be configured
+		 */
+		@NonNull
+		HttpConnection configure(@NonNull HttpConnection connection,
+				boolean sslVerify) throws IOException, GeneralSecurityException;
+
+		/**
+		 * Closes the {@link GitSession}, releasing any internal state.
+		 */
+		void close();
+	}
+}
