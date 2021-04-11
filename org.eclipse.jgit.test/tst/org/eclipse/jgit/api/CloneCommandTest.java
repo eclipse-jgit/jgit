be7e839b29578e39f3e4a6f55af1779f7a3d8faf
Matthias Sohn <matthias.sohn@sap.com>
2021-04-11 23:50:29 +0200

    Add clone test only containing a non-standard ref
    Bug: 572737
    Change-Id: Iaaed6110a34a94f826d10a97aa4144b07470c7e5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/CloneCommandTest.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -859,6 +860,30 @@ public class CloneCommandTest extends RepositoryTestCase {
 		db = createWorkRepository();
 		trash = db.getWorkTree();
 		git = new Git(db);
+
+		File directory = createTempDirectory("testCloneRepository");
+		CloneCommand command = Git.cloneRepository().setDirectory(directory)
+				.setURI(fileUri());
+		Git git2 = command.call();
+		Repository clonedRepo = git2.getRepository();
+		addRepoToClose(clonedRepo);
+
+		Map<AnyObjectId, Set<Ref>> allRefs = clonedRepo
+				.getAllRefsByPeeledObjectId();
+		assertTrue(allRefs.isEmpty());
+		Ref head = clonedRepo.exactRef(Constants.HEAD);
+		assertNull(head.getObjectId());
+	}
+
+	@Test
+	public void testCloneRepositoryNonStandardRef() throws Exception {
+		// delete all branches and tags and prune orphaned objects so that we
+		// only have non-standard ref "refs/meta/foo/bar"
+		git.checkout().setName("refs/meta/foo/bar").call();
+		git.branchDelete().setBranchNames("test", "master").setForce(true)
+				.call();
+		git.tagDelete().setTags("tag-for-blob", "tag-initial").call();
+		git.gc().setExpire(new Date()).call();
 
 		File directory = createTempDirectory("testCloneRepository");
 		CloneCommand command = Git.cloneRepository().setDirectory(directory)
