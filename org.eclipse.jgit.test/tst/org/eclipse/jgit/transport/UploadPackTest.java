ff6cd1ec07bdb60eb30c7d09677f51880b8126bd
kylezhao <kylezhao@tencent.com>
2021-05-06 11:26:15 +0800

    UploadPack: allow fetching any sha1 in protocol v0/v1
    For CGit client, Git server must send capability
    allow-tip-sha1-in-want or allow-reachable-sha1-in-want
    to allow fetching any sha1 in the protocol v0/v1.
    
    Bug: 573390
    Change-Id: Ieae0f6d700a00b6561ab1eb61e32956286676a52
    Signed-off-by: kylezhao <kylezhao@tencent.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/UploadPackTest.java
@@ -361,6 +361,22 @@ public class UploadPackTest {
 		}
 	}
 
+	@Test
+	public void testFetchV0CapabilitiesRequestPolicyAny() throws Exception {
+		ByteArrayInputStream recvStream = uploadPackSetup(
+				TransferConfig.ProtocolVersion.V0.version(),
+				(UploadPack up) -> {
+					up.setRequestPolicy(RequestPolicy.ANY);
+				}, PacketLineIn.end());
+		PacketLineIn pckIn = new PacketLineIn(recvStream);
+		String packetLine = pckIn.readString();
+		assertTrue(packetLine
+				.contains(GitProtocolConstants.OPTION_ALLOW_TIP_SHA1_IN_WANT));
+		assertTrue(packetLine.contains(
+				GitProtocolConstants.OPTION_ALLOW_REACHABLE_SHA1_IN_WANT));
+		assertTrue(PacketLineIn.isEnd(pckIn.readString()));
+	}
+
 	/*
 	 * Invokes UploadPack with specified protocol version and sends it the given lines,
 	 * and returns UploadPack's output stream.
