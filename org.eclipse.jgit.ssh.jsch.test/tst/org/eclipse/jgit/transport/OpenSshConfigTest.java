e82d9854973238e3349325f94d1f84b96ed5b988
Thomas Wolf <thomas.wolf@paranor.ch>
2021-05-15 18:25:52 +0200

    SSH config: fix whitespace handling
    Use Character.isWhitespace() instead of Character.isSpaceChar() to
    treat TABs as whitespace, too.
    
    Change-Id: Iffc59c13357d981ede6a1e0feb6ea6ff03fb3064
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/OpenSshConfigTest.java
+++ org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/OpenSshConfigTest.java
@@ -572,4 +572,21 @@ public class OpenSshConfigTest extends RepositoryTestCase {
 		assertArrayEquals(new Object[] { "/foo", "/bar", "/baz" },
 				h.getConfig().getValues("IdentityFile"));
 	}
+
+	@Test
+	public void testWhitespace() throws Exception {
+		config("Host foo \tbar   baz\nPort 29418\n");
+		Host h = osc.lookup("foo");
+		assertNotNull(h);
+		assertEquals(29418, h.getPort());
+		h = osc.lookup("bar");
+		assertNotNull(h);
+		assertEquals(29418, h.getPort());
+		h = osc.lookup("baz");
+		assertNotNull(h);
+		assertEquals(29418, h.getPort());
+		h = osc.lookup("\tbar");
+		assertNotNull(h);
+		assertEquals(22, h.getPort());
+	}
 }
