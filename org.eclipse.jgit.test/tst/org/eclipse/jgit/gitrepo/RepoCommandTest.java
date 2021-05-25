63216f57a5c8209920924b962dd2836d7920c47e
Ivan Frade <ifrade@google.com>
2021-05-24 17:31:25 -0700

    RepoCommand: Do not set 'branch' if the revision is a tag
    The "branch" field in the .gitmodules is the signal for gerrit to keep
    the superproject autoupdated. Tags are immutable and there is no need to
    track them, plus the cgit client requires the field to be a "remote
    branch name" but not a tag.
    
    Do not set the "branch" field if the revision is a tag. Keep those tags
    in another field ("ref") as they help other tools to find the commit in
    the destination repository.
    
    We can still have false negatives when a refname is not fully qualified,
    but this check covers e.g. the most common case in android.
    
    Note that the javadoc of #setRecordRemoteBranch already mentions that
    "submodules that request a tag will not have branch name recorded".
    
    Change-Id: Ib1c321a4d3b7f8d51ca2ea204f72dc0cfed50c37
    Signed-off-by: Ivan Frade <ifrade@google.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
@@ -749,7 +749,53 @@ public class RepoCommandTest extends RepositoryTestCase {
 			String gitlink = localDb.resolve(Constants.HEAD + ":foo").name();
 			assertEquals("The gitlink is same as remote head",
 					oldCommitId.name(), gitlink);
+
+			File dotmodules = new File(localDb.getWorkTree(),
+					Constants.DOT_GIT_MODULES);
+			assertTrue(dotmodules.exists());
+			// The .gitmodules file should have "branch" lines
+			String gitModulesContents = Files.readString(dotmodules.toPath(),
+					UTF_8);
+			assertTrue(gitModulesContents.contains("branch = branch"));
 		}
+	}
+
+	@Test
+	public void testRevisionBare_ignoreTags() throws Exception {
+		Repository remoteDb = createBareRepository();
+		Repository tempDb = createWorkRepository();
+
+		StringBuilder xmlContent = new StringBuilder();
+		xmlContent.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+				.append("<manifest>")
+				.append("<remote name=\"remote1\" fetch=\".\" />")
+				.append("<default revision=\"").append("refs/tags/" + TAG)
+				.append("\" remote=\"remote1\" />")
+				.append("<project path=\"foo\" name=\"")
+				.append(defaultUri)
+				.append("\" />").append("</manifest>");
+		JGitTestUtil.writeTrashFile(tempDb, "manifest.xml",
+				xmlContent.toString());
+		RepoCommand command = new RepoCommand(remoteDb);
+		command.setPath(
+				tempDb.getWorkTree().getAbsolutePath() + "/manifest.xml")
+				.setURI(rootUri).call();
+		// Clone it
+		File directory = createTempDirectory("testReplaceManifestBare");
+		File dotmodules;
+		try (Repository localDb = Git.cloneRepository().setDirectory(directory)
+				.setURI(remoteDb.getDirectory().toURI().toString()).call()
+				.getRepository()) {
+			dotmodules = new File(localDb.getWorkTree(),
+					Constants.DOT_GIT_MODULES);
+			assertTrue(dotmodules.exists());
+		}
+
+		// The .gitmodules file should not have "branch" lines
+		String gitModulesContents = Files.readString(dotmodules.toPath(),
+				UTF_8);
+		assertFalse(gitModulesContents.contains("branch"));
+		assertTrue(gitModulesContents.contains("ref = refs/tags/" + TAG));
 	}
 
 	@Test
