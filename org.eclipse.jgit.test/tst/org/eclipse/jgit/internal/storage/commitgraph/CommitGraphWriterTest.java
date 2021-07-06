2193b684834b872a42be410da4274b8b192561c5
kylezhao <kylezhao@tencent.com>
2021-07-06 20:20:14 +0800

    CommitGraph: implement commit-graph writer
    Teach JGit to write a commit-graph formatted file by walking commit
    graph from specified commit objects.
    
    See: https://git-scm.com/docs/commit-graph-format/2.21.0
    
    Bug: 574368
    Change-Id: I34f9f28f8729080c275f86215ebf30b2d05af41d
    Signed-off-by: kylezhao <kylezhao@tencent.com>
    Reviewed-by: Martin Fick <mfick@codeaurora.org>
    Reviewed-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphWriterTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2021, Tencent.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.commitgraph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.junit.RepositoryTestCase;
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommitGraphWriterTest extends RepositoryTestCase {
+
+	private TestRepository<FileRepository> tr;
+
+	private CommitGraphConfig config;
+
+	private ByteArrayOutputStream os;
+
+	private CommitGraphWriter writer;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		os = new ByteArrayOutputStream();
+		config = new CommitGraphConfig(db);
+		tr = new TestRepository<>(db, new RevWalk(db), mockSystemReader);
+	}
+
+	@Test
+	public void testConstructor() {
+		writer = new CommitGraphWriter(config, db.newObjectReader());
+		assertTrue(config.isComputeGeneration());
+		assertTrue(writer.isComputeGeneration());
+		assertEquals(0, writer.getCommitCnt());
+	}
+
+	@Test
+	public void testModifySettings() {
+		config.setComputeGeneration(false);
+		assertFalse(config.isComputeGeneration());
+
+		writer = new CommitGraphWriter(config, db.newObjectReader());
+		assertFalse(writer.isComputeGeneration());
+		writer.setComputeGeneration(true);
+		assertTrue(writer.isComputeGeneration());
+	}
+
+	@Test
+	public void testWriterWithExtraEdgeList() throws Exception {
+		RevCommit root = commit();
+		RevCommit a = commit(root);
+		RevCommit b = commit(root);
+		RevCommit c = commit(root);
+		RevCommit tip = commit(a, b, c);
+
+		Set<ObjectId> wants = Collections.singleton(tip);
+		NullProgressMonitor m = NullProgressMonitor.INSTANCE;
+		writer = new CommitGraphWriter(config, db.newObjectReader());
+		writer.prepareCommitGraph(m, m, wants);
+
+		assertTrue(writer.willWriteExtraEdgeList());
+		assertEquals(5, writer.getCommitCnt());
+
+		writer.writeCommitGraph(m, os);
+		byte[] data = os.toByteArray();
+		assertTrue(data.length > 0);
+	}
+
+	@Test
+	public void testWriterWithoutExtraEdgeList() throws Exception {
+		RevCommit root = commit();
+		RevCommit a = commit(root);
+		RevCommit b = commit(root);
+		RevCommit tip = commit(a, b);
+
+		Set<ObjectId> wants = Collections.singleton(tip);
+		NullProgressMonitor m = NullProgressMonitor.INSTANCE;
+		writer = new CommitGraphWriter(config, db.newObjectReader());
+		writer.prepareCommitGraph(m, m, wants);
+
+		assertFalse(writer.willWriteExtraEdgeList());
+		assertEquals(4, writer.getCommitCnt());
+
+		writer.writeCommitGraph(m, os);
+		byte[] data = os.toByteArray();
+		assertTrue(data.length > 0);
+	}
+
+	RevCommit commit(RevCommit... parents) throws Exception {
+		return tr.commit(parents);
+	}
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CommitGraph.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2021, Tencent.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.lib;
+
+/**
+ * The CommitGraph is a supplemental data structure that accelerates commit
+ * graph walks.
+ * <p>
+ * If a user downgrades or disables the <code>core.commitGraph</code> config
+ * setting, then the existing object database is sufficient.
+ * </p>
+ * <p>
+ * It stores the commit graph structure along with some extra metadata to speed
+ * up graph walks. By listing commit OIDs in lexicographic order, we can
+ * identify an integer position for each commit and refer to the parents of a
+ * commit using those integer positions. We use binary search to find initial
+ * commits and then use the integer positions for fast lookups during the walk.
+ * </p>
+ *
+ * @since 6.1
+ */
+public interface CommitGraph {
+
+	/**
+	 * We use GENERATION_UNKNOWN({@value}) to mark commits not in the
+	 * commit-graph file.
+	 */
+	int GENERATION_UNKNOWN = Integer.MAX_VALUE;
+
+	/**
+	 * If a commit-graph file was written by a version of Git that did not
+	 * compute generation numbers, then those commits will have generation
+	 * number represented by GENERATION_NOT_COMPUTED({@value}).
+	 */
+	int GENERATION_NOT_COMPUTED = 0;
+
+	/**
+	 * Get the metadata of a commit.
+	 *
+	 * @param commit
+	 *            the commit object id to inspect.
+	 * @return the metadata of a commit or null if it's not found.
+	 */
+	CommitData getCommitData(AnyObjectId commit);
+
+	/**
+	 * Get the metadata of a commit。
+	 *
+	 * @param graphPos
+	 *            the position in the commit-graph of the object.
+	 * @return the metadata of a commit or null if it's not found.
+	 */
+	CommitData getCommitData(int graphPos);
+
+	/**
+	 * Get the object at the commit-graph position.
+	 *
+	 * @param graphPos
+	 *            the position in the commit-graph of the object.
+	 * @return the ObjectId or null if it's not found.
+	 */
+	ObjectId getObjectId(int graphPos);
+
+	/**
+	 * Obtain the total number of commits described by this commit-graph.
+	 *
+	 * @return number of commits in this commit-graph
+	 */
+	long getCommitCnt();
+
+	/**
+	 * Metadata of a commit in commit data chunk.
+	 */
+	interface CommitData {
+
+		/**
+		 * Get a reference to this commit's tree.
+		 *
+		 * @return tree of this commit.
+		 */
+		ObjectId getTree();
+
+		/**
+		 * Obtain an array of all parents.
+		 * <p>
+		 * The method only provides the positions of parents in commit-graph,
+		 * call {@link CommitGraph#getObjectId(int)} to get the real objectId.
+		 *
+		 * @return the array of parents.
+		 */
+		int[] getParents();
+
+		/**
+		 * Time from the "committer" line.
+		 *
+		 * @return commit time
+		 */
+		long getCommitTime();
+
+		/**
+		 * Get the generation number of the commit.
+		 * <p>
+		 * If A and B are commits with generation numbers N and M, respectively,
+		 * and N <= M, then A cannot reach B. That is, we know without searching
+		 * that B is not an ancestor of A because it is further from a root
+		 * commit than A.
+		 * <p>
+		 * Conversely, when checking if A is an ancestor of B, then we only need
+		 * to walk commits until all commits on the walk boundary have
+		 * generation number at most N. If we walk commits using a priority
+		 * queue seeded by generation numbers, then we always expand the
+		 * boundary commit with highest generation number and can easily detect
+		 * the stopping condition.
+		 * <p>
+		 * We use {@link #GENERATION_UNKNOWN} to mark commits not in the
+		 * commit-graph file. If a commit-graph file was written without
+		 * computing generation numbers, then those commits will have generation
+		 * number represented by {@link #GENERATION_NOT_COMPUTED}.
+		 *
+		 * @return the generation number
+		 */
+		int getGeneration();
+	}
+}
