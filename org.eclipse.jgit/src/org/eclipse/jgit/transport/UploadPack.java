2ff5f6f6eaf7857f54b0d925d7b10238333fa523
Fabio Ponciroli <ponch78@gmail.com>
2021-07-08 18:31:10 +0200

    Reload pack list upon stale file handle
    Create a repository and share it over NFS, using JGit
    server-side. Clones may fail when a repacking is happening
    concurrently.
    
    This is the scenario where the clone could fail:
    1. The repository has a packfile that satisfies all the BLOBs requested;
    2. JGit chose to send the pack 'as-is';
    3. The GC removes the packfile *after* it has been cached in memory;
    4. The git-upload-pack starts transferring the whole packfile and
    finds a stale file handle instead.
    
    The issue arises since the cached packs are not aligned anymore
    with the one on filesystem. A retry client side wouldn't help since
    it won't trigger a refresh of the in memory packfiles server side.
    JGit itself doesn't have a way to detect the inconsistency between
    cached packs and the filesystem.
    
    Using the preserve pack functionality would only postpone the
    issue after the jgit gc --prune-preserved stage, since the cached
    pack list isn't getting refreshed.
    
    Fix it by refreshing the cached packs upon a write pack failure
    and retry during a PackWriter::writePack before giving up.
    
    More details about the issue can be found in the related ticket [1]
    
    [1]: https://bugs.eclipse.org/bugs/show_bug.cgi?id=573770
    
    Bug: 573770
    Change-Id: I78e1c7d3e042eaef64e85bc546af207478f2e334

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -225,6 +225,12 @@ public class UploadPack {
 	private int timeout;
 
 	/**
+	* Max number of write pack to attempt. Sensible empirical value after
+	* which giving up in completing the write pack.
+	*/
+	private static final int MAX_WRITE_PACK_ATTEMPTS = 5;
+
+	/**
 	 * Is the client connection a bi-directional socket or pipe?
 	 * <p>
 	 * If true, this class assumes it can perform multiple read and write cycles
@@ -2272,139 +2278,146 @@ public class UploadPack {
 		PackConfig cfg = packConfig;
 		if (cfg == null)
 			cfg = new PackConfig(db);
-		@SuppressWarnings("resource") // PackWriter is referenced in the finally
-										// block, and is closed there
-		final PackWriter pw = new PackWriter(cfg, walk.getObjectReader(),
-				accumulator);
-		try {
-			pw.setIndexDisabled(true);
-			if (req.getFilterSpec().isNoOp()) {
-				pw.setUseCachedPacks(true);
-			} else {
-				pw.setFilterSpec(req.getFilterSpec());
-				pw.setUseCachedPacks(false);
-			}
-			pw.setUseBitmaps(
-					req.getDepth() == 0
-							&& req.getClientShallowCommits().isEmpty()
-							&& req.getFilterSpec().getTreeDepthLimit() == -1);
-			pw.setClientShallowCommits(req.getClientShallowCommits());
-			pw.setReuseDeltaCommits(true);
-			pw.setDeltaBaseAsOffset(
-					req.getClientCapabilities().contains(OPTION_OFS_DELTA));
-			pw.setThin(req.getClientCapabilities().contains(OPTION_THIN_PACK));
-			pw.setReuseValidatingObjects(false);
-
-			// Objects named directly by references go at the beginning
-			// of the pack.
-			if (commonBase.isEmpty() && refs != null) {
-				Set<ObjectId> tagTargets = new HashSet<>();
-				for (Ref ref : refs.values()) {
-					if (ref.getPeeledObjectId() != null)
-						tagTargets.add(ref.getPeeledObjectId());
-					else if (ref.getObjectId() == null)
-						continue;
-					else if (ref.getName().startsWith(Constants.R_HEADS))
-						tagTargets.add(ref.getObjectId());
-				}
-				pw.setTagTargets(tagTargets);
-			}
-
-			RevWalk rw = walk;
-			if (req.getDepth() > 0 || req.getDeepenSince() != 0 || !deepenNots.isEmpty()) {
-				int walkDepth = req.getDepth() == 0 ? Integer.MAX_VALUE
-						: req.getDepth() - 1;
-				pw.setShallowPack(req.getDepth(), unshallowCommits);
-
-				@SuppressWarnings("resource") // Ownership is transferred below
-				DepthWalk.RevWalk dw = new DepthWalk.RevWalk(
-						walk.getObjectReader(), walkDepth);
-				dw.setDeepenSince(req.getDeepenSince());
-				dw.setDeepenNots(deepenNots);
-				dw.assumeShallow(req.getClientShallowCommits());
-				rw = dw;
-			}
-
-			if (wantAll.isEmpty()) {
-				pw.preparePack(pm, wantIds, commonBase,
-						req.getClientShallowCommits());
-			} else {
-				walk.reset();
-
-				ObjectWalk ow = rw.toObjectWalkWithSameObjects();
-				pw.preparePack(pm, ow, wantAll, commonBase, PackWriter.NONE);
-				rw = ow;
-			}
-
-			if (req.getClientCapabilities().contains(OPTION_INCLUDE_TAG)
-					&& allTags != null) {
-				for (Ref ref : allTags) {
-					ObjectId objectId = ref.getObjectId();
-					if (objectId == null) {
-						// skip unborn branch
-						continue;
-					}
-
-					// If the object was already requested, skip it.
-					if (wantAll.isEmpty()) {
-						if (wantIds.contains(objectId))
-							continue;
-					} else {
-						RevObject obj = rw.lookupOrNull(objectId);
-						if (obj != null && obj.has(WANT))
-							continue;
-					}
-
-					if (!ref.isPeeled())
-						ref = db.getRefDatabase().peel(ref);
-
-					ObjectId peeledId = ref.getPeeledObjectId();
-					objectId = ref.getObjectId();
-					if (peeledId == null || objectId == null)
-						continue;
-
-					objectId = ref.getObjectId();
-					if (pw.willInclude(peeledId) && !pw.willInclude(objectId)) {
-						RevObject o = rw.parseAny(objectId);
-						addTagChain(o, pw);
-						pw.addObject(o);
-					}
-				}
-			}
-
-			if (pckOut.isUsingSideband()) {
-				if (req instanceof FetchV2Request &&
-						cachedPackUriProvider != null &&
-						!((FetchV2Request) req).getPackfileUriProtocols().isEmpty()) {
-					FetchV2Request reqV2 = (FetchV2Request) req;
-					pw.setPackfileUriConfig(new PackWriter.PackfileUriConfig(
-							pckOut,
-							reqV2.getPackfileUriProtocols(),
-							cachedPackUriProvider));
+		for (int writePackAttempts = 0; writePackAttempts < MAX_WRITE_PACK_ATTEMPTS; writePackAttempts++) {
+			@SuppressWarnings("resource") // PackWriter is referenced in the finally
+			                              // block, and is closed there
+			PackWriter pw = new PackWriter(cfg, walk.getObjectReader(),
+					accumulator);
+			try {
+				pw.setIndexDisabled(true);
+				if (req.getFilterSpec().isNoOp()) {
+					pw.setUseCachedPacks(true);
 				} else {
-					// PackWriter will write "packfile-uris\n" and "packfile\n"
-					// for us if provided a PackfileUriConfig. In this case, we
-					// are not providing a PackfileUriConfig, so we have to
-					// write this line ourselves.
-					pckOut.writeString(
-							GitProtocolConstants.SECTION_PACKFILE + '\n');
+					pw.setFilterSpec(req.getFilterSpec());
+					pw.setUseCachedPacks(false);
 				}
-			}
-			pw.enableSearchForReuseTimeout();
-			pw.writePack(pm, NullProgressMonitor.INSTANCE, packOut);
+				pw.setUseBitmaps(
+						req.getDepth() == 0
+								&& req.getClientShallowCommits().isEmpty()
+								&& req.getFilterSpec().getTreeDepthLimit() == -1);
+				pw.setClientShallowCommits(req.getClientShallowCommits());
+				pw.setReuseDeltaCommits(true);
+				pw.setDeltaBaseAsOffset(
+						req.getClientCapabilities().contains(OPTION_OFS_DELTA));
+				pw.setThin(req.getClientCapabilities().contains(OPTION_THIN_PACK));
+				pw.setReuseValidatingObjects(false);
 
-			if (msgOut != NullOutputStream.INSTANCE) {
-				String msg = pw.getStatistics().getMessage() + '\n';
-				msgOut.write(Constants.encode(msg));
-				msgOut.flush();
-			}
+				// Objects named directly by references go at the beginning
+				// of the pack.
+				if (commonBase.isEmpty() && refs != null) {
+					Set <ObjectId> tagTargets = new HashSet <>();
+					for (Ref ref : refs.values()) {
+						if (ref.getPeeledObjectId() != null)
+							tagTargets.add(ref.getPeeledObjectId());
+						else if (ref.getObjectId() == null)
+							continue;
+						else if (ref.getName().startsWith(Constants.R_HEADS))
+							tagTargets.add(ref.getObjectId());
+					}
+					pw.setTagTargets(tagTargets);
+				}
 
-		} finally {
-			statistics = pw.getStatistics();
-			if (statistics != null) {
-				postUploadHook.onPostUpload(statistics);
+				RevWalk rw = walk;
+				if (req.getDepth() > 0 || req.getDeepenSince() != 0 || !deepenNots.isEmpty()) {
+					int walkDepth = req.getDepth() == 0 ? Integer.MAX_VALUE
+							: req.getDepth() - 1;
+					pw.setShallowPack(req.getDepth(), unshallowCommits);
+
+					@SuppressWarnings("resource") // Ownership is transferred below
+					DepthWalk.RevWalk dw = new DepthWalk.RevWalk(
+							walk.getObjectReader(), walkDepth);
+					dw.setDeepenSince(req.getDeepenSince());
+					dw.setDeepenNots(deepenNots);
+					dw.assumeShallow(req.getClientShallowCommits());
+					rw = dw;
+				}
+
+				if (wantAll.isEmpty()) {
+					pw.preparePack(pm, wantIds, commonBase,
+							req.getClientShallowCommits());
+				} else {
+					walk.reset();
+
+					ObjectWalk ow = rw.toObjectWalkWithSameObjects();
+					pw.preparePack(pm, ow, wantAll, commonBase, PackWriter.NONE);
+					rw = ow;
+				}
+
+				if (req.getClientCapabilities().contains(OPTION_INCLUDE_TAG)
+						&& allTags != null) {
+					for (Ref ref : allTags) {
+						ObjectId objectId = ref.getObjectId();
+						if (objectId == null) {
+							// skip unborn branch
+							continue;
+						}
+
+						// If the object was already requested, skip it.
+						if (wantAll.isEmpty()) {
+							if (wantIds.contains(objectId))
+								continue;
+						} else {
+							RevObject obj = rw.lookupOrNull(objectId);
+							if (obj != null && obj.has(WANT))
+								continue;
+						}
+
+						if (!ref.isPeeled())
+							ref = db.getRefDatabase().peel(ref);
+
+						ObjectId peeledId = ref.getPeeledObjectId();
+						objectId = ref.getObjectId();
+						if (peeledId == null || objectId == null)
+							continue;
+
+						objectId = ref.getObjectId();
+						if (pw.willInclude(peeledId) && !pw.willInclude(objectId)) {
+							RevObject o = rw.parseAny(objectId);
+							addTagChain(o, pw);
+							pw.addObject(o);
+						}
+					}
+				}
+
+				if (pckOut.isUsingSideband()) {
+					if (req instanceof FetchV2Request &&
+							cachedPackUriProvider != null &&
+							!((FetchV2Request) req).getPackfileUriProtocols().isEmpty()) {
+						FetchV2Request reqV2 = (FetchV2Request) req;
+						pw.setPackfileUriConfig(new PackWriter.PackfileUriConfig(
+								pckOut,
+								reqV2.getPackfileUriProtocols(),
+								cachedPackUriProvider));
+					} else {
+						// PackWriter will write "packfile-uris\n" and "packfile\n"
+						// for us if provided a PackfileUriConfig. In this case, we
+						// are not providing a PackfileUriConfig, so we have to
+						// write this line ourselves.
+						pckOut.writeString(
+								GitProtocolConstants.SECTION_PACKFILE + '\n');
+					}
+				}
+				pw.enableSearchForReuseTimeout();
+				pw.writePack(pm, NullProgressMonitor.INSTANCE, packOut);
+
+				if (msgOut != NullOutputStream.INSTANCE) {
+					String msg = pw.getStatistics().getMessage() + '\n';
+					msgOut.write(Constants.encode(msg));
+					msgOut.flush();
+				}
+
+			} catch (TriggerRefreshPackListException e) {
+				db.getObjectDatabase()
+					.refreshPackList();
+				continue;
+			} finally {
+				statistics = pw.getStatistics();
+				if (statistics != null) {
+					postUploadHook.onPostUpload(statistics);
+				}
+				pw.close();
 			}
-			pw.close();
+			break;
 		}
 	}
 
