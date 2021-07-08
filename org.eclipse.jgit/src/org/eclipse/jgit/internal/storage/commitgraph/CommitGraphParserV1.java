63583fd24a2225e299284ce2f7930877ae7b6723
kylezhao <kylezhao@tencent.com>
2021-07-08 20:19:13 +0800

    CommitGraph: implement commit-graph read
    Git introduced a new file storing the topology and some metadata of
    the commits in the repo (commitGraph). With this data, git can browse
    commit history without parsing the pack, speeding up e.g.
    reachability checks.
    
    This change teaches JGit to read commit-graph-format file, following
    the upstream format([1]).
    
    JGit can read a commit-graph file from a buffered stream, which means
    that we can provide this feature for both FileRepository and
    DfsRepository.
    
    I splitted the file parsing from the interface to later plug the
    multiple commit-graph files[2] implementation, though this feature
    is not supported in the current implementation.
    
    [1] https://git-scm.com/docs/commit-graph-format/2.21.0
    [2] https://git-scm.com/docs/commit-graph/2.23.0#_commit_graphs_chains
    
    Bug: 574368
    Change-Id: Ib5c0d6678cb242870a0f5841bd413ad3885e95f6
    Signed-off-by: kylezhao <kylezhao@tencent.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphParserV1.java
@@ -0,0 +1,404 @@
+/*
+ * Copyright (C) 2021, Tencent.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.commitgraph;
+
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.CHUNK_ID_COMMIT_DATA;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.CHUNK_ID_EXTRA_EDGE_LIST;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.CHUNK_ID_OID_FANOUT;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.CHUNK_ID_OID_LOOKUP;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.CHUNK_LOOKUP_WIDTH;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.COMMIT_DATA_WIDTH;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.COMMIT_GRAPH_MAGIC;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.GRAPH_EDGE_LAST_MASK;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.GRAPH_EXTRA_EDGES_NEEDED;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.GRAPH_LAST_EDGE;
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraphConstants.GRAPH_NO_PARENT;
+import static org.eclipse.jgit.lib.Constants.OBJECT_ID_LENGTH;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.jgit.errors.CommitGraphFormatException;
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.util.IO;
+import org.eclipse.jgit.util.NB;
+
+/**
+ * Support for the commit-graph v1 format.
+ *
+ * @see CommitGraph
+ */
+class CommitGraphParserV1 implements CommitGraph {
+
+	private static final int FANOUT = 256;
+
+	private final long commitCnt;
+
+	private final int hashLength;
+
+	private final int commitDataLength;
+
+	private long[] oidFanout;
+
+	private byte[][] oidLookup;
+
+	private byte[][] commitData;
+
+	private byte[] extraEdgeList;
+
+	CommitGraphParserV1(InputStream fd, byte[] hdr) throws IOException {
+		int magic = NB.decodeInt32(hdr, 0);
+		if (magic != COMMIT_GRAPH_MAGIC) {
+			throw new CommitGraphFormatException(
+					JGitText.get().notACommitGraph);
+		}
+
+		// Read the hash version (1 byte)
+		// 1 => SHA-1
+		// 2 => SHA-256 nonsupport now
+		int hashVersion = hdr[5];
+		if (hashVersion != 1) {
+			throw new CommitGraphFormatException(
+					JGitText.get().incorrectOBJECT_ID_LENGTH);
+		}
+
+		hashLength = OBJECT_ID_LENGTH;
+		commitDataLength = hashLength + COMMIT_DATA_WIDTH;
+
+		// Read the number of "chunkOffsets" (1 byte)
+		int numberOfChunks = hdr[6];
+
+		// hdr[7] is the number of base commit-graphs, which is not supported in
+		// current version
+
+		byte[] lookupBuffer = new byte[CHUNK_LOOKUP_WIDTH
+				* (numberOfChunks + 1)];
+		IO.readFully(fd, lookupBuffer, 0, lookupBuffer.length);
+
+		List<ChunkSegment> chunks = new ArrayList<>();
+		Set<Integer> ids = new HashSet<>();
+		for (int i = 0; i <= numberOfChunks; i++) {
+			int id = NB.decodeInt32(lookupBuffer, i * 12);
+			if (ids.contains(Integer.valueOf(id))) {
+				throw new CommitGraphFormatException(MessageFormat.format(
+						JGitText.get().commitGraphChunkRepeated,
+						Integer.toHexString(id)));
+			}
+			long offset = NB.decodeInt64(lookupBuffer, i * 12 + 4);
+			chunks.add(new ChunkSegment(id, offset));
+			ids.add(Integer.valueOf(id));
+		}
+
+		oidLookup = new byte[FANOUT][];
+		commitData = new byte[FANOUT][];
+		long maxChunkOffset = chunks.get(numberOfChunks).offset;
+		for (int i = 0; i < numberOfChunks; i++) {
+			int chunkId = chunks.get(i).id;
+			long chunkOffset = chunks.get(i).offset;
+			long length = chunks.get(i + 1).offset - chunkOffset;
+			long lengthRead;
+			if (chunkOffset < 0 || chunkOffset > maxChunkOffset) {
+				throw new CommitGraphFormatException(MessageFormat.format(
+						JGitText.get().commitGraphChunkImproperOffset,
+						Integer.toHexString(chunkId),
+						Long.valueOf(chunkOffset)));
+			}
+			switch (chunkId) {
+			case CHUNK_ID_OID_FANOUT:
+				lengthRead = loadChunkOidFanout(fd);
+				break;
+			case CHUNK_ID_OID_LOOKUP:
+				lengthRead = loadChunkDataBasedOnFanout(fd, hashLength,
+						oidLookup);
+				break;
+			case CHUNK_ID_COMMIT_DATA:
+				lengthRead = loadChunkDataBasedOnFanout(fd, commitDataLength,
+						commitData);
+				break;
+			case CHUNK_ID_EXTRA_EDGE_LIST:
+				lengthRead = loadChunkExtraEdgeList(fd, length);
+				break;
+			default:
+				throw new CommitGraphFormatException(MessageFormat.format(
+						JGitText.get().commitGraphChunkUnknown,
+						Integer.toHexString(chunkId)));
+			}
+			if (length != lengthRead) {
+				throw new CommitGraphFormatException(MessageFormat.format(
+						JGitText.get().commitGraphChunkImproperOffset,
+						Integer.toHexString(chunks.get(i + 1).id),
+						Long.valueOf(chunks.get(i + 1).offset)));
+			}
+		}
+
+		if (oidFanout == null) {
+			throw new CommitGraphFormatException(
+					JGitText.get().commitGraphOidFanoutNeeded);
+		}
+		commitCnt = oidFanout[FANOUT - 1];
+	}
+
+	private long loadChunkOidFanout(InputStream fd) throws IOException {
+		int fanoutLen = FANOUT * 4;
+		byte[] fanoutTable = new byte[fanoutLen];
+		IO.readFully(fd, fanoutTable, 0, fanoutLen);
+		oidFanout = new long[FANOUT];
+		for (int k = 0; k < oidFanout.length; k++) {
+			oidFanout[k] = NB.decodeUInt32(fanoutTable, k * 4);
+		}
+		return fanoutLen;
+	}
+
+	private long loadChunkDataBasedOnFanout(InputStream fd, int itemLength,
+			byte[][] chunkData) throws IOException {
+		if (oidFanout == null) {
+			throw new CommitGraphFormatException(
+					JGitText.get().commitGraphOidFanoutNeeded);
+		}
+		long readedLength = 0;
+		for (int k = 0; k < oidFanout.length; k++) {
+			long n;
+			if (k == 0) {
+				n = oidFanout[k];
+			} else {
+				n = oidFanout[k] - oidFanout[k - 1];
+			}
+			if (n > 0) {
+				long len = n * itemLength;
+				if (len > Integer.MAX_VALUE - 8) { // http://stackoverflow.com/a/8381338
+					throw new CommitGraphFormatException(
+							JGitText.get().commitGraphFileIsTooLargeForJgit);
+				}
+
+				chunkData[k] = new byte[(int) len];
+
+				IO.readFully(fd, chunkData[k], 0, chunkData[k].length);
+				readedLength += len;
+			}
+		}
+		return readedLength;
+	}
+
+	private long loadChunkExtraEdgeList(InputStream fd, long len)
+			throws IOException {
+		if (len > Integer.MAX_VALUE - 8) { // http://stackoverflow.com/a/8381338
+			throw new CommitGraphFormatException(
+					JGitText.get().commitGraphFileIsTooLargeForJgit);
+		}
+		extraEdgeList = new byte[(int) len];
+		IO.readFully(fd, extraEdgeList, 0, extraEdgeList.length);
+		return len;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public int findGraphPosition(AnyObjectId objId) {
+		int levelOne = objId.getFirstByte();
+		byte[] data = oidLookup[levelOne];
+		if (data == null) {
+			return -1;
+		}
+		int high = data.length / (hashLength);
+		int low = 0;
+		do {
+			int mid = (low + high) >>> 1;
+			int pos = objIdOffset(mid);
+			int cmp = objId.compareTo(data, pos);
+			if (cmp < 0) {
+				high = mid;
+			} else if (cmp == 0) {
+				if (levelOne == 0) {
+					return mid;
+				}
+				return (int) (mid + oidFanout[levelOne - 1]);
+			} else {
+				low = mid + 1;
+			}
+		} while (low < high);
+		return -1;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public ObjectId getObjectId(int graphPos) {
+		if (graphPos < 0 || graphPos > commitCnt) {
+			return null;
+		}
+		int levelOne = findLevelOne(graphPos);
+		int p = getLevelTwo(graphPos, levelOne);
+		int dataIdx = objIdOffset(p);
+		return ObjectId.fromRaw(oidLookup[levelOne], dataIdx);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public CommitGraph.CommitData getCommitData(int graphPos) {
+		int levelOne = findLevelOne(graphPos);
+		int p = getLevelTwo(graphPos, levelOne);
+		int dataIdx = commitDataOffset(p);
+		byte[] data = this.commitData[levelOne];
+
+		if (graphPos < 0) {
+			return null;
+		}
+
+		// parse tree
+		ObjectId tree = ObjectId.fromRaw(data, dataIdx);
+
+		// parse date
+		long dateHigh = NB.decodeUInt32(data, dataIdx + hashLength + 8) & 0x3;
+		long dateLow = NB.decodeUInt32(data, dataIdx + hashLength + 12);
+		long commitTime = dateHigh << 32 | dateLow;
+
+		// parse generation
+		int generation = NB.decodeInt32(data, dataIdx + hashLength + 8) >> 2;
+
+		boolean noParents = false;
+		int[] pList = new int[0];
+		int edgeValue = NB.decodeInt32(data, dataIdx + hashLength);
+		if (edgeValue == GRAPH_NO_PARENT) {
+			noParents = true;
+		}
+
+		// parse parents
+		if (!noParents) {
+			pList = new int[1];
+			int parent = edgeValue;
+			pList[0] = parent;
+
+			edgeValue = NB.decodeInt32(data, dataIdx + hashLength + 4);
+			if (edgeValue != GRAPH_NO_PARENT) {
+				if ((edgeValue & GRAPH_EXTRA_EDGES_NEEDED) != 0) {
+					int pptr = edgeValue & GRAPH_EDGE_LAST_MASK;
+					int[] eList = findExtraEdgeList(pptr);
+					if (eList == null) {
+						return null;
+					}
+					int[] old = pList;
+					pList = new int[eList.length + 1];
+					pList[0] = old[0];
+					for (int i = 0; i < eList.length; i++) {
+						parent = eList[i];
+						pList[i + 1] = parent;
+					}
+				} else {
+					parent = edgeValue;
+					pList = new int[] { pList[0], parent };
+				}
+			}
+		}
+		int[] parents = pList;
+		return new CommitData(tree, parents, commitTime, generation);
+	}
+
+	/**
+	 * Find the list of commit-graph position in extra edge list chunk.
+	 * <p>
+	 * The extra edge list chunk store the second through nth parents for all
+	 * octopus merges.
+	 *
+	 * @param pptr
+	 *            the start position to iterate of extra edge list chunk
+	 * @return the list of commit-graph position or null if not found
+	 */
+	int[] findExtraEdgeList(int pptr) {
+		if (extraEdgeList == null) {
+			return null;
+		}
+		int maxOffset = extraEdgeList.length - 4;
+		int offset = pptr * 4;
+		if (offset < 0 || offset > maxOffset) {
+			return null;
+		}
+		int[] pList = new int[32];
+		int count = 0;
+		int parentPosition;
+		for (;;) {
+			if (count >= pList.length) {
+				int[] old = pList;
+				pList = new int[pList.length + 32];
+				System.arraycopy(old, 0, pList, 0, count);
+			}
+			if (offset > maxOffset) {
+				return null;
+			}
+			parentPosition = NB.decodeInt32(extraEdgeList, offset);
+			if ((parentPosition & GRAPH_LAST_EDGE) != 0) {
+				pList[count] = parentPosition & GRAPH_EDGE_LAST_MASK;
+				count++;
+				break;
+			}
+			pList[count++] = parentPosition;
+			offset += 4;
+		}
+		int[] old = pList;
+		pList = new int[count];
+		System.arraycopy(old, 0, pList, 0, count);
+
+		return pList;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public long getCommitCnt() {
+		return commitCnt;
+	}
+
+	private int findLevelOne(long nthPosition) {
+		int levelOne = Arrays.binarySearch(oidFanout, nthPosition + 1);
+		if (levelOne >= 0) {
+			// If we hit the bucket exactly the item is in the bucket, or
+			// any bucket before it which has the same object count.
+			//
+			long base = oidFanout[levelOne];
+			while (levelOne > 0 && base == oidFanout[levelOne - 1]) {
+				levelOne--;
+			}
+		} else {
+			// The item is in the bucket we would insert it into.
+			//
+			levelOne = -(levelOne + 1);
+		}
+		return levelOne;
+	}
+
+	private int getLevelTwo(long nthPosition, int levelOne) {
+		long base = levelOne > 0 ? oidFanout[levelOne - 1] : 0;
+		return (int) (nthPosition - base);
+	}
+
+	private int objIdOffset(int mid) {
+		return hashLength * mid;
+	}
+
+	private int commitDataOffset(int mid) {
+		return commitDataLength * mid;
+	}
+
+	private class ChunkSegment {
+		final int id;
+
+		final long offset;
+
+		private ChunkSegment(int id, long offset) {
+			this.id = id;
+			this.offset = offset;
+		}
+	}
+}
