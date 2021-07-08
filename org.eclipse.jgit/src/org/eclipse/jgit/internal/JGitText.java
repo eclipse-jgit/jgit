2f4c9d221eaad34f4e514589eb41360171d7424a
kylezhao <kylezhao@tencent.com>
2021-07-08 20:19:13 +0800

    CommitGraph: implement commit-graph read
    This change teaches JGit to use a single commit-graph file to
    accelerate commit graph walks. When new commits are added to the
    repo, the commit-graph gets further and further behind, and then we
    write the whole commit-graph file again to include new commits.
    
    Multiple commit-graph files are not supported in the current
    implementation. With this feature, git will write a commit-graph after
    every git fetch command that downloads a pack-file from a remote.
    
    JGit can read a commit-graph file from a buffered stream, which means
    that we can provide this feature for both FileRepository and
    DfsRepository.
    
    Bug: 574368
    Change-Id: Ib5c0d6678cb242870a0f5841bd413ad3885e95f6
    Signed-off-by: kylezhao <kylezhao@tencent.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -169,7 +169,12 @@ public class JGitText extends TranslationBundle {
 	/***/ public String commandClosedStderrButDidntExit;
 	/***/ public String commandRejectedByHook;
 	/***/ public String commandWasCalledInTheWrongState;
+	/***/ public String commitGraphChunkImproperOffset;
+	/***/ public String commitGraphChunkRepeated;
+	/***/ public String commitGraphChunkUnknown;
+	/***/ public String commitGraphFileIsTooLargeForJgit;
 	/***/ public String commitGraphWritingCancelled;
+	/***/ public String commitGraphOidFanoutNeeded;
 	/***/ public String commitMessageNotSpecified;
 	/***/ public String commitOnRepoWithoutHEADCurrentlyNotSupported;
 	/***/ public String commitAmendOnInitialNotPossible;
@@ -525,6 +530,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String noSuchSubmodule;
 	/***/ public String notABoolean;
 	/***/ public String notABundle;
+	/***/ public String notACommitGraph;
 	/***/ public String notADIRCFile;
 	/***/ public String notAGitDirectory;
 	/***/ public String notAPACKFile;
@@ -797,6 +803,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String unmergedPath;
 	/***/ public String unmergedPaths;
 	/***/ public String unpackException;
+	/***/ public String unreadableCommitGraph;
 	/***/ public String unreadablePackIndex;
 	/***/ public String unrecognizedPackExtension;
 	/***/ public String unrecognizedRef;
@@ -804,6 +811,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String unsupportedAlternates;
 	/***/ public String unsupportedArchiveFormat;
 	/***/ public String unsupportedCommand0;
+	/***/ public String unsupportedCommitGraphVersion;
 	/***/ public String unsupportedEncryptionAlgorithm;
 	/***/ public String unsupportedEncryptionVersion;
 	/***/ public String unsupportedGC;
