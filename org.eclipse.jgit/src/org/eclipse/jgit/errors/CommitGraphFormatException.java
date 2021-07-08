fd1b14f8ef48815e0a2c5d36920d26a98dd4bd07
kylezhao <kylezhao@tencent.com>
2021-07-08 20:19:13 +0800

    CommitGraph: implement commit-graph read
    This change teaches JGit to use a single commit-graph file to
    accelerates commit graph walks. When new commits are added to the
    repo, the commit-graph gets further and further behind, and then we
    write the whole commit-graph file again to make it includes new
    commits.
    Multiple commit-graph Files are not supported in the current
    implementation. With this feature, git will write a commit-graph after
     every git fetch command that downloads a pack-file from a remote.
    
    JGit can read a commit-graph file from a buffered stream, which means
    that we can provide this feature for both FileRepository and
    DfsRepository.
    
    Bug: 574368
    Change-Id: Ib5c0d6678cb242870a0f5841bd413ad3885e95f6
    Signed-off-by: kylezhao <kylezhao@tencent.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/errors/CommitGraphFormatException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2021, Tencent.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.errors;
+
+import java.io.IOException;
+
+/**
+ * Thrown when a commit-graph file's format is different than we expected
+ *
+ */
+public class CommitGraphFormatException extends IOException {
+
+	/**
+	 * Construct an exception.
+	 *
+	 * @param why
+	 *            description of the type of error.
+	 */
+	public CommitGraphFormatException(String why) {
+		super(why);
+	}
+}
