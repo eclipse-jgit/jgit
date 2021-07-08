26ce9629ac473b6d9035eac9fadce5aeda77e75e
Fabio Ponciroli <ponch78@gmail.com>
2021-07-08 18:31:10 +0200

    Reload pack list upon stale file handle
    If you create a repository and share it over NFS, the Git
    clones may fail.
    
    This is the scenario where the clone could fail:
    1. The repository have a packfile that satisfy all the BLOBs requested
    2. JGit chose to send the pack 'as-is'
    3. The GC removes the packfile *after* it has been cached in memory
    
    Refresh the cached packs and retry during a PackWriter::writePack before
    giving up.
    
    Bug: 573770
    Change-Id: I78e1c7d3e042eaef64e85bc546af207478f2e334

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPackSendPackException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021, Fabio Ponciroli <ponch@gerritforge.com>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.transport;
+
+import java.io.IOException;
+
+/**
+ * Generic Exception encountered during an Upload Pack operation.
+ *
+ */
+public class UploadPackSendPackException extends IOException {
+    private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param why
+	 *            root cause exception.
+	 */
+    public UploadPackSendPackException(Throwable why) {
+        initCause(why);
+    }
+
+	@Override
+	public synchronized Throwable fillInStackTrace() {
+		return this;
+	}
+}
