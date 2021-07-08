30a14520183331a87ed00a48a455c33c645330cc
Fabio Ponciroli <ponch78@gmail.com>
2021-07-08 18:31:10 +0200

    Reload pack list upon stale file handle
    If you create a repository and share it over NFS, the Git
    clones may fail.
    
    This is the scenario where the clone could fail:
    1. The repository have a packfile that satisfy all the BLOBs requested
    2. JGit chose to send the pack 'as-is'
    3. The GC removes the packfile *after* it has been cached in memory
    
    Refresh the cached packs and retry during a PackWriter::writePack before
    giving up.
    
    Bug: 573770
    Change-Id: I78e1c7d3e042eaef64e85bc546af207478f2e334

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/StaleFileHandleOnPackfile.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021, Fabio Ponciroli <ponch@gerritforge.com>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.pack;
+
+import org.eclipse.jgit.internal.storage.file.Pack;
+import org.eclipse.jgit.transport.SendPackException;
+
+import java.io.IOException;
+
+/**
+ * Indicates that a stale file handle error has been raised against a pack file
+ *
+ */
+public class StaleFileHandleOnPackfile extends SendPackException {
+    private static final long serialVersionUID = 1L;
+
+    private final Pack pack;
+
+    /**
+	 * <p>
+	 * Constructor for StaleFileHandleOnPackfile.
+	 * </p>
+	 *
+	 * @param why
+	 *            root cause exception.
+	 *
+	 * @param missingPack
+	 *            pack which file on disk has gone missing.
+	 */
+    public StaleFileHandleOnPackfile(Throwable why, Pack missingPack) {
+        super(why);
+        this.pack = missingPack;
+    }
+
+    /**
+     * Get the pack which file on disk has gone missing.
+     *
+     * @return Pack object which file on disk has gone missing.
+     */
+    public Pack getPack() {
+        return pack;
+    }
+
+    /**
+     * Get the original IOEXception thrown.
+     *
+     * @return original IOEXception thrown.
+     */
+    public IOException getOriginalException() {
+        return (IOException) this.getCause();
+    }
+}
