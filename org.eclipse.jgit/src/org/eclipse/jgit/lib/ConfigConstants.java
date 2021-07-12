6690e1dcbf527592b049717da486adc278588843
kylezhao <kylezhao@tencent.com>
2021-07-12 17:07:13 +0800

    CommitGraph: automatically write commit-graph files when gc
    Add a 'gc.writeCommitGraph' config setting that triggers writing a
    commit-graph file after any non-trivial 'git gc' command. Defaults to
    false while the commit-graph feature matures.
    
    Bug: 574368
    Change-Id: Ic94cd69034c524285c938414610f2e152198e06e
    Signed-off-by: kylezhao <kylezhao@tencent.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -758,4 +758,10 @@ public final class ConfigConstants {
 	 */
 	public static final String CONFIG_KEY_COMPUTE_GENERATION = "computeGeneration";
 
+	/**
+	 * The "writeCommitGraph" key
+	 *
+	 * @since 5.13
+	 */
+	public static final String CONFIG_KEY_WRITE_COMMIT_GRAPH = "writeCommitGraph";
 }
