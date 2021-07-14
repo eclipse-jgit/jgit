09cc0669933316e93fd555ac31b1f6bc58d4ae0a
kylezhao <kylezhao@tencent.com>
2021-07-14 19:02:12 +0800

    RevWalk: integrate commit-graph with commit parsing
    Teach JGit to inspect a commit graph file to supply the contents of a
    RevCommit when calling RevCommit.parseHeaders(), including parents,
    the root tree, the commit date and the generation number.
    By loading commits from the graph instead of parsing commit buffers,
    we save a lot of time on long commit walks.
    
    The following is the performance results for a copy of the Linux
    repository with this change.
    
    | Command                       |   berfore  |    after   |
    |-------------------------------|------------|------------|
    | rev-list -1000                |  16ms      |    2ms     |
    | rev-list -10000               |  96ms      |    12ms    |
    | rev-list -50000               |  482ms     |    46ms    |
    | rev-list -100000              |  936ms     |    85ms    |
    | rev-list -200000              |  1844ms    |    178ms   |
    | rev-list -500000              |  3919ms    |    445ms   |
    
    If core.commitGraph is false, then do not check graph files.
    When core.commitGraph is true, it's important to know that RevCommit
    may not contain buffer after calling RevCommit.parseHeaders().
    To ensure the existing features of RevWalk are not affected, an
    additional check in RevWalk.next() is necessary.
    
    Bug: 574368
    Change-Id: I90d0f64af24f3acc3eae6da984eae302d338f5ee
    Signed-off-by: kylezhao <kylezhao@tencent.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
@@ -30,6 +30,7 @@ import org.eclipse.jgit.errors.RevWalkException;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.AsyncObjectLoaderQueue;
+import org.eclipse.jgit.lib.CommitGraph;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.MutableObjectId;
 import org.eclipse.jgit.lib.NullProgressMonitor;
@@ -175,6 +176,10 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 	private RevFilter filter;
 
 	private TreeFilter treeFilter;
+
+	private CommitGraph commitGraph;
+
+	private boolean commitGraphLoaded = false;
 
 	private boolean retainBody = true;
 
@@ -598,7 +603,11 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 	 */
 	public RevCommit next() throws MissingObjectException,
 			IncorrectObjectTypeException, IOException {
-		return pending.next();
+		RevCommit commit = pending.next();
+		if (commit != null && isRetainBody()) {
+			commit.parseBody(this);
+		}
+		return commit;
 	}
 
 	/**
@@ -1129,6 +1138,15 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		}
 	}
 
+	CommitGraph getCommitGraph() {
+		if (commitGraphLoaded) {
+			return commitGraph;
+		}
+		commitGraph = reader.getCommitGraph();
+		commitGraphLoaded = true;
+		return commitGraph;
+	}
+
 	/**
 	 * Asynchronous object parsing.
 	 *
@@ -1521,6 +1539,8 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		queue = new DateRevQueue(firstParent);
 		pending = new StartGenerator(this);
 		shallowCommitsInitialized = false;
+		commitGraphLoaded = false;
+		commitGraph = null;
 	}
 
 	/**
