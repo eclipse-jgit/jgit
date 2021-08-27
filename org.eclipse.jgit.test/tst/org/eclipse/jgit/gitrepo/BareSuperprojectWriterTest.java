d0f4238671df6fd67907354b1948cfd2fb2d7f76
Ivan Frade <ifrade@google.com>
2021-08-27 15:58:20 -0700

    RepoCommand: Allow to set attributes to the .gitmodules file
    The superproject doesn't know from what exact version of the manifest it
    has been created. Developers need to correlate commit times to find the
    version and this is not reliable as some manifest update can fail to
    commit to the superproject. This can be solved adding the manifest
    commit sha1 and/or the source branch as git-attributes of the
    .gitmodules.
    
    In general, setting metadata seems useful and there is no need to limit
    it to a specific key.
    
    Offer an API to set extra key/value pairs as .gitattributes of the
    .gitmodules.
    
    Change-Id: I6b61c27c641b6f7f66122bf78bb9f6d9ee9fdd84

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/BareSuperprojectWriterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/BareSuperprojectWriterTest.java
@@ -3,11 +3,16 @@ package org.eclipse.jgit.gitrepo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.eclipse.jgit.gitrepo.BareSuperprojectWriter.BareWriterConfig;
 import org.eclipse.jgit.gitrepo.RepoCommand.RemoteReader;
@@ -41,7 +46,7 @@ public class BareSuperprojectWriterTest extends RepositoryTestCase {
 
 			BareSuperprojectWriter w = new BareSuperprojectWriter(bareRepo,
 					null, "refs/heads/master", author, mockRemoteReader,
-					BareWriterConfig.getDefault());
+					BareWriterConfig.getDefault(), new HashMap<>());
 
 			RevCommit commit = w.write(Arrays.asList(repoProject));
 
@@ -54,6 +59,109 @@ public class BareSuperprojectWriterTest extends RepositoryTestCase {
 					containsInAnyOrder(is("\tbranch = refs/heads/branch-x"),
 							is("\tpath = path/to"),
 							is("\turl = http://example.com/a")));
+		}
+	}
+
+	@Test
+	public void write_recordSubmoduleLabels() throws Exception {
+		try (Repository bareRepo = createBareRepository()) {
+			RepoProject repoProject = new RepoProject("subprojectX", "path/to",
+					SHA1_A, "remote", "groupA,groupB");
+			repoProject.setUrl("http://example.com/a");
+
+			BareSuperprojectWriter w = new BareSuperprojectWriter(bareRepo,
+					null, "refs/heads/master", author, null,
+					BareWriterConfig.getDefault(), new HashMap<>());
+
+			RevCommit commit = w.write(Arrays.asList(repoProject));
+
+			String contents = readContents(bareRepo, commit, ".gitattributes");
+			Optional<String> subprojectXLine = Arrays
+					.asList(contents.split("\n")).stream()
+					.filter(line -> line.startsWith("/path/to ")).findFirst();
+			assertTrue(subprojectXLine.isPresent());
+			assertTrue(subprojectXLine.get().contains(" groupA"));
+			assertTrue(subprojectXLine.get().contains(" groupB"));
+		}
+	}
+
+	@Test
+	public void write_doNotRecordSubmoduleLabels_noExtraAttr()
+			throws Exception {
+		try (Repository bareRepo = createBareRepository()) {
+			RepoProject repoProject = new RepoProject("subprojectX", "path/to",
+					SHA1_A, "remote", "groupA,groupB");
+			repoProject.setUrl("http://example.com/a");
+
+			BareWriterConfig cfg = BareWriterConfig.getDefault();
+			cfg.recordSubmoduleLabels = false;
+
+			BareSuperprojectWriter w = new BareSuperprojectWriter(bareRepo,
+					null, "refs/heads/master", author, null,
+					cfg, new HashMap<>());
+
+			RevCommit commit = w.write(Arrays.asList(repoProject));
+
+			// No attributes, no submodule labels -> no .gitattributes files
+			String idStr = commit.getId().name() + ":.gitattributes";
+			ObjectId modId = bareRepo.resolve(idStr);
+			assertTrue(modId == null);
+		}
+	}
+
+	@Test
+	public void write_doNotRecordSubmoduleLabels_withExtraAttr()
+			throws Exception {
+		try (Repository bareRepo = createBareRepository()) {
+			RepoProject repoProject = new RepoProject("subprojectX", "path/to",
+					SHA1_A, "remote", "groupA,groupB");
+			repoProject.setUrl("http://example.com/a");
+
+			BareWriterConfig cfg = BareWriterConfig.getDefault();
+			cfg.recordSubmoduleLabels = false;
+
+			Map<String, String> gitModulesAttr = new HashMap<>();
+			gitModulesAttr.put("a-key", "a-value");
+
+			BareSuperprojectWriter w = new BareSuperprojectWriter(bareRepo,
+					null, "refs/heads/master", author, null,
+					BareWriterConfig.getDefault(), gitModulesAttr);
+
+			RevCommit commit = w.write(Arrays.asList(repoProject));
+
+			String contents = readContents(bareRepo, commit, ".gitattributes");
+			Optional<String> gitModulesLine = Arrays
+					.asList(contents.split("\n")).stream()
+					.filter(line -> line.startsWith("path/to ")).findFirst();
+			assertFalse(gitModulesLine.isPresent());
+		}
+	}
+
+	@Test
+	public void write_setGitModulesAttributes()
+			throws Exception {
+		try (Repository bareRepo = createBareRepository()) {
+			RepoProject repoProject = new RepoProject("subprojectX", "path/to",
+					SHA1_A, "remote", "groupA,groupB");
+			repoProject.setUrl("http://example.com/a");
+
+			Map<String, String> attr = new HashMap<>();
+			attr.put("a-key", "a-value");
+			attr.put("-b-key", null);
+
+			BareSuperprojectWriter w = new BareSuperprojectWriter(bareRepo,
+					null, "refs/heads/master", author, null,
+					BareWriterConfig.getDefault(), attr);
+
+			RevCommit commit = w.write(Arrays.asList(repoProject));
+
+			String contents = readContents(bareRepo, commit, ".gitattributes");
+			Optional<String> gitModulesLine = Arrays
+					.asList(contents.split("\n")).stream()
+					.filter(line -> line.startsWith(".gitmodules")).findFirst();
+			assertTrue(gitModulesLine.isPresent());
+			assertTrue(gitModulesLine.get().contains(" a-key=a-value"));
+			assertTrue(gitModulesLine.get().contains(" -b-key"));
 		}
 	}
 
