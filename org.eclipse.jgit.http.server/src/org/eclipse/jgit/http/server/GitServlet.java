45c13094bc572cfbc51efaef5b3e89165d54c9b8
Antonio Barone <syntonyze@gmail.com>
2021-09-01 12:04:07 +0200

    GitServlet: allow to override default error handlers
    GitServlet delegates repository access over HTTP to the GitFilter
    servlet.
    
    GitServlet, in turn, can be extended by jgit consumers to provide custom
    logic when handling such operations.
    
    This is the case, for example, with Gerrit Code Review, which provides
    custom behavior with a GitOverHttpServlet [1].
    
    Among possible customizations, the ability of specifying a custom error
    handler for UploadPack and ReceivePack was already introduced in
    GitFilter by Idd3b87d6b and I9c708aa5a2, respectively.
    
    However the `setUploadPackErrorHandler` and `setReceivePackErrorHandler`
    methods were never added to the GitServlet.
    
    Expose the `setUploadPackErrorHandler` and `setReceivePackErrorHandler`
    methods to the GitServlet, so that consumers of the jgit library might
    specify custom error handlers.
    
    [1] https://gerrit.googlesource.com/gerrit/+/refs/heads/stable-3.2/java/com/google/gerrit/httpd/GitOverHttpServlet.java#95
    
    Change-Id: I712d485ff68b662b48c71ef75650c5a155950d23

--- org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/GitServlet.java
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/GitServlet.java
@@ -113,6 +113,16 @@ public class GitServlet extends MetaServlet {
 	}
 
 	/**
+	 * Set a custom error handler for git-upload-pack.
+	 *
+	 * @param h
+	 *            A custom error handler for git-upload-pack.
+	 */
+	public void setUploadPackErrorHandler(UploadPackErrorHandler h) {
+		gitFilter.setUploadPackErrorHandler(h);
+	}
+
+	/**
 	 * Add upload-pack filter
 	 *
 	 * @param filter
@@ -134,6 +144,16 @@ public class GitServlet extends MetaServlet {
 	 */
 	public void setReceivePackFactory(ReceivePackFactory<HttpServletRequest> f) {
 		gitFilter.setReceivePackFactory(f);
+	}
+
+	/**
+	 * Set a custom error handler for git-receive-pack.
+	 *
+	 * @param h
+	 *            A custom error handler for git-receive-pack.
+	 */
+	public void setReceivePackErrorHandler(ReceivePackErrorHandler h) {
+		gitFilter.setReceivePackErrorHandler(h);
 	}
 
 	/**
