aa65ffa33974339e0ededf815bf48be5aaded32f
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-22 00:38:24 +0100

    Add GetExactRefBenchmark
    Introduce a new benchmark that shows a typical use-case
    of opening a cached repository and fetching one ref from
    a repository with a high number of refs.
    
    This specific benchmark is tailored to the Gerrit use-case
    of reading frequently individual refs SHA1s from the All-Users
    repository.
    
    Bug: 576165
    Change-Id: Ie59a87ca9f0191ff30229688e3430332e2f59a4f

---
+++ org.eclipse.jgit.benchmarks/src/org/eclipse/jgit/benchmarks/GetExactRefBenchmark.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2021, Luca Milanesio <luca.milanesio@gmail.com> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.benchmarks;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.util.FileUtils;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@State(Scope.Thread)
+public class GetExactRefBenchmark {
+	Path repoPath;
+	Path testDir;
+	Path workDir;
+	Git git;
+	int numBranches = 10000;
+	List<String> branches = new ArrayList<>(numBranches);
+	AtomicInteger branchIndex = new AtomicInteger();
+	boolean trustFolderStat = true;
+
+	@Setup
+	public void setupBenchmark() throws IOException, GitAPIException {
+		testDir = Files.createTempDirectory("dir");
+		workDir = testDir.resolve("testrepo");
+		repoPath = workDir.resolve(".git");
+		git = Git.init().setDirectory(workDir.toFile()).setGitDir(repoPath.toFile()).call();
+		git.getRepository().getConfig().setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_KEY_TRUSTFOLDERSTAT, trustFolderStat);
+
+		git.commit().setMessage("First commit").call();
+
+		System.out.println("Preparing a test repository " + repoPath + " with " + numBranches + " branches ...");
+		for (int branchIdx = 0; branchIdx < numBranches;) {
+			String branchName = "branch-" + branchIdx;
+			branches.add(branchName);
+			git.checkout().setCreateBranch(true).setName(branchName).call();
+			git.commit().setMessage("Test commit on " + branchName).call();
+			branchIdx++;
+			if ((branchIdx) % 1000 == 0) {
+				System.out.print(String.format("\r%d%% of branches created", (100 * branchIdx) / numBranches));
+			}
+		}
+	}
+
+	@TearDown
+	public void teardown() throws IOException {
+		FileUtils.delete(testDir.toFile(), FileUtils.RECURSIVE | FileUtils.RETRY);
+	}
+
+	@Benchmark
+	@BenchmarkMode({ Mode.AverageTime })
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+	public Ref testOpenRepositoryAndGetExactRef() throws IOException {
+		try (Repository repo = RepositoryCache.open(RepositoryCache.FileKey.lenient(repoPath.toFile(), FS.DETECTED))) {
+			String branchName = branches.get(branchIndex.incrementAndGet() % numBranches);
+			return repo.exactRef(branchName);
+		}
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(GetExactRefBenchmark.class.getSimpleName())
+				// .addProfiler(StackProfiler.class)
+				// .addProfiler(GCProfiler.class)
+				.forks(1).jvmArgs("-ea").build();
+		new Runner(opt).run();
+	}
+}
