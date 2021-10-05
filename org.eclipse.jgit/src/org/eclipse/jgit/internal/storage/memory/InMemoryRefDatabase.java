062ebd54e91103d5723364d6e1735ae601a83664
Matthias Sohn <matthias.sohn@sap.com>
2021-10-06 00:28:23 +0200

    Add InMemoryRefDatabase to be used as a Ref cache
    - can wrap another RefDatabase which persists refs, implemented for
    RefDirectory only
    - loads all refs from the wrapped RefDatabase into a TernarySearchTree
    to enable fast lookup of exact ref and refs prefix searches
    - reloads refs incrementally on RefUpdate, BatchRefUpdate and RefRename
    - re-resolves ref  from wrapped RefDatabase after RefUpdate finished in
    order to update cache entry
    
    Bug: 576165
    Change-Id: Ic9bb57576847500f5b0c1a68956749be3ba80f6d

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/memory/InMemoryRefDatabase.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2021, Matthias Sohn <matthias.sohn@sap.com> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.memory;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.locks.ReadWriteLock;
+
+import org.eclipse.jgit.internal.storage.file.RefDirectory;
+import org.eclipse.jgit.lib.BatchRefUpdate;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefCache;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.SymbolicRef;
+
+/**
+ * An in-memory RefDatabase which can be used as a cache for another wrapped
+ * RefDatabase
+ */
+public class InMemoryRefDatabase extends RefDatabase
+		implements RefCache {
+
+	private final RefDatabase refDb;
+
+	private final TernarySearchTree<Ref> cache;
+
+	/**
+	 * @param repo
+	 *            the repository
+	 */
+	public InMemoryRefDatabase(Repository repo) {
+		this.refDb = repo.getRefDatabase();
+		this.cache = new TernarySearchTree<>();
+	}
+
+	/**
+	 * Reload all refs using the loader
+	 *
+	 * @return number of reloaded refs
+	 */
+	public int reload() {
+		try {
+			return cache.reload(refDb.getRefs(ALL).entrySet());
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	@Override
+	public void create() throws IOException {
+		refDb.create();
+		reload();
+	}
+
+	@Override
+	public void close() {
+		refDb.close();
+		cache.clear();
+	}
+
+	@Override
+	public boolean isNameConflicting(String name) throws IOException {
+		return refDb.isNameConflicting(name);
+	}
+
+	@Override
+	public RefUpdate newUpdate(String name, boolean detach)
+			throws IOException {
+		RefUpdate update = refDb.newUpdate(name, detach);
+		update.setRefCache(this);
+		return update;
+	}
+
+	@Override
+	public BatchRefUpdate newBatchUpdate() {
+		BatchRefUpdate batchUpdate = refDb.newBatchUpdate();
+		batchUpdate.setRefCache(this);
+		return batchUpdate;
+	}
+
+	@Override
+	public RefRename newRename(String fromName, String toName)
+			throws IOException {
+		RefRename rename = refDb.newRename(fromName, toName);
+		rename.setRefCache(this);
+		return rename;
+	}
+
+	@Override
+	public Ref exactRef(String name) throws IOException {
+		Ref ref = readAndResolve(name);
+		return ref;
+	}
+
+	private Ref readAndResolve(String name) throws IOException {
+		Ref ref = cache.get(name);
+		if (ref != null) {
+			ref = resolve(ref, 0);
+		}
+		return ref;
+	}
+
+	private Ref resolve(final Ref ref, int depth)
+			throws IOException {
+		if (ref.isSymbolic()) {
+			Ref dst = ref.getTarget();
+			if (MAX_SYMBOLIC_REF_DEPTH <= depth) {
+				return null; // claim it doesn't exist
+			}
+
+			dst = cache.get(dst.getName());
+			if (dst == null) {
+				return ref;
+			}
+
+			dst = resolve(dst, depth + 1);
+			if (dst == null) {
+				return null;
+			}
+			return new SymbolicRef(ref.getName(), dst);
+		}
+		return ref;
+	}
+
+	@Override
+	public Map<String, Ref> getRefs(String prefix) throws IOException {
+		Map<String, Ref> refs = new HashMap<>();
+		for (String key : cache.getKeysWithPrefix(prefix)) {
+			Ref ref = readAndResolve(key);
+			refs.put(key, ref);
+		}
+		return refs;
+	}
+
+	@Override
+	public List<Ref> getAdditionalRefs() throws IOException {
+		List<Ref> addtlRefs = new LinkedList<>();
+		for (String name : additionalRefsNames) {
+			Ref r = exactRef(name);
+			if (r != null) {
+				addtlRefs.add(r);
+			}
+		}
+		return addtlRefs;
+	}
+
+	@Override
+	public Ref peel(Ref ref) throws IOException {
+		return ref.getLeaf();
+	}
+
+	/**
+	 * Write reflog, use only in tests
+	 *
+	 * @param force
+	 * @param update
+	 * @param msg
+	 * @param deref
+	 * @throws IOException
+	 */
+	public void log(boolean force, RefUpdate update, String msg, boolean deref)
+			throws IOException {
+		if (refDb instanceof RefDirectory) {
+			((RefDirectory) refDb).log(force, update, msg, deref);
+		}
+	}
+
+	@Override
+	public ReadWriteLock getLock() {
+		return cache.getLock();
+	}
+
+	@Override
+	public void reload(Iterable<Entry<String, Ref>> newRefs) {
+		cache.reload(newRefs);
+	}
+
+	@Override
+	public void insert(Ref ref) {
+		cache.insert(ref.getName(), ref);
+		if (ref.isSymbolic()) {
+			Ref leaf = ref.getLeaf();
+			cache.insert(leaf.getName(), leaf);
+		}
+	}
+
+	@Override
+	public void delete(String refName) {
+		cache.delete(refName);
+	}
+
+	@Override
+	public void rename(Ref oldRef, Ref newRef) {
+		cache.delete(oldRef.getName());
+		cache.insert(newRef.getName(), newRef);
+	}
+}
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDatabase.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDatabase.java
@@ -432,7 +432,7 @@ public abstract class RefDatabase {
 	 * <p>
 	 * The default implementation is not efficient. Implementors of {@link RefDatabase}
 	 * should override this method directly if a better implementation is possible.
-	 *
+	 * 
 	 * @param include string that names of refs should start with; may be empty.
 	 * @param excludes strings that names of refs can't start with; may be empty.
 	 * @return immutable list of refs whose names start with {@code prefix} and none
