afa1b86e006eb1089f5c3ccd784c4ca1a9528412
Matthias Sohn <matthias.sohn@sap.com>
2021-10-06 00:28:23 +0200

    [WIP] Add InMemoryRefDatabase to be used as a Ref cache
    - wraps another RefDatabase which persists refs
    - loads all refs from the wrapped RefDatabase into a TernarySearchTree
    to enable fast prefix searches
    - reloads all refs when it's notified via a RefsChangedEvent
    - incremental updates aren't implemented yet
    
    Bug: 576165
    Change-Id: Ic9bb57576847500f5b0c1a68956749be3ba80f6d

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/memory/InMemoryRefDatabase.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2021, Matthias Sohn <matthias.sohn@sap.com> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.memory;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jgit.events.RefsChangedEvent;
+import org.eclipse.jgit.events.RefsChangedListener;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * An in-memory RefDatabase which can be used as a cache for another wrapped
+ * RefDatabase
+ */
+public class InMemoryRefDatabase extends RefDatabase
+		implements RefsChangedListener {
+	private static final String[] additionalRefsNames = new String[] {
+			Constants.MERGE_HEAD, Constants.FETCH_HEAD, Constants.ORIG_HEAD,
+			Constants.CHERRY_PICK_HEAD };
+
+	private final Repository repo;
+
+	private final RefDatabase refDb;
+
+	private TernarySearchTree<Ref> refCache;
+
+	/**
+	 * @param repo
+	 *            the repository
+	 */
+	public InMemoryRefDatabase(Repository repo) {
+		this.repo = repo;
+		this.refDb = repo.getRefDatabase();
+		this.refCache = new TernarySearchTree<>();
+	}
+
+	@Override
+	public void create() throws IOException {
+		refCache.insert(refDb.getRefs(ALL));
+		repo.getListenerList().addRefsChangedListener(this);
+	}
+
+	@Override
+	public void close() {
+		refDb.close();
+		refCache.clear();
+	}
+
+	@Override
+	public boolean isNameConflicting(String name) throws IOException {
+		return refDb.isNameConflicting(name);
+	}
+
+	@Override
+	public RefUpdate newUpdate(String name, boolean detach)
+			throws IOException {
+		return refDb.newUpdate(name, detach);
+	}
+
+	@Override
+	public RefRename newRename(String fromName, String toName)
+			throws IOException {
+		return refDb.newRename(fromName, toName);
+	}
+
+	@Override
+	public Ref exactRef(String name) throws IOException {
+		Ref ref = refCache.get(name);
+		return ref;
+	}
+
+	@Override
+	public Map<String, Ref> getRefs(String prefix) throws IOException {
+		Map<String, Ref> refs = new HashMap<>();
+		for (String key : refCache.getKeysWithPrefix(prefix)) {
+			refs.put(key, refCache.get(key));
+		}
+		return refs;
+	}
+
+	@Override
+	public List<Ref> getAdditionalRefs() throws IOException {
+		List<Ref> addtlRefs = new LinkedList<>();
+		for (String name : additionalRefsNames) {
+			Ref r = exactRef(name);
+			if (r != null) {
+				addtlRefs.add(r);
+			}
+		}
+		return addtlRefs;
+	}
+
+	@Override
+	public Ref peel(Ref ref) throws IOException {
+		return ref.getLeaf();
+	}
+
+	@Override
+	public void onRefsChanged(RefsChangedEvent event) {
+		try {
+			TernarySearchTree<Ref> newRefCache = new TernarySearchTree<>();
+			newRefCache.insert(refDb.getRefs(ALL));
+			refCache = TernarySearchTree.replace(refCache, newRefCache);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+}
