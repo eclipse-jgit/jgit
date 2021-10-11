3cb24fac152add5bb3b8eb24574b7ff4c0700439
Matthias Sohn <matthias.sohn@sap.com>
2021-10-11 11:08:29 +0200

    Add getRefs benchmark for existing repositories
    Change-Id: Ie1958c726b7d2a85251171c1f64b9bf889532c5c

---
+++ org.eclipse.jgit.benchmarks/src/org/eclipse/jgit/benchmarks/GetRefsBenchmarkExistingRepo.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2021, Luca Milanesio <luca.milanesio@gmail.com> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.benchmarks;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.internal.storage.memory.InMemoryRefDatabase;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryCache;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.util.FS;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@State(Scope.Thread)
+public class GetRefsBenchmarkExistingRepo {
+
+	@State(Scope.Benchmark)
+	public static class BenchmarkState {
+
+//		boolean useRefTable = false;
+
+		@Param({ "~/tmp/git/jgit.git", "~/tmp/git/gerrit.git",
+				"~/tmp/git/go.git", "~/tmp/git/large-repo-sap01.git",
+				"~/tmp/git/All-Users-Eclipse.git",
+				"~/tmp/git/All-Users-sap01.git",
+				"~/tmp/git/All-Users-sap02.git" })
+		String path;
+
+		@Param({ "true", "false" })
+		boolean trustFolderStat;
+
+		Repository repo;
+
+		InMemoryRefDatabase refCache;
+
+		private List<Ref> allRefs;
+
+		private Random rnd;
+
+		private static String userHome = System.getProperty("user.home");
+
+		private static String expandHome(String path) {
+			if (path.charAt(0) == '~') {
+				return userHome + path.substring(1);
+			}
+			return path;
+		}
+
+		@Setup
+		@SuppressWarnings("boxing")
+		public void setupBenchmark() throws IOException {
+			Path repoPath = Path.of(expandHome(path));
+			repo = RepositoryCache.open(RepositoryCache.FileKey
+					.lenient(repoPath.toFile(), FS.DETECTED));
+
+			StoredConfig cfg = repo.getConfig();
+			cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+					ConfigConstants.CONFIG_KEY_TRUSTFOLDERSTAT,
+					trustFolderStat);
+			cfg.save();
+
+			allRefs = repo.getRefDatabase().getRefs();
+			System.out.println(String.format("repo: '%s', number of refs: %d",
+					path, allRefs.size()));
+			refCache = new InMemoryRefDatabase(repo);
+			rnd = new SecureRandom();
+			rnd.setSeed(System.currentTimeMillis());
+		}
+
+		public String getRandomRefName() {
+			return allRefs.get(rnd.nextInt(allRefs.size())).getName();
+		}
+
+		public String getRandomRefsPrefix() {
+			String prefix = path.contains("All-Users") ? "refs/users"
+					: "refs/changes";
+			return prefix + rnd.nextInt(100);
+		}
+
+		@TearDown
+		public void teardown() {
+			repo.close();
+		}
+	}
+
+	@Benchmark
+	@BenchmarkMode({ Mode.AverageTime })
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
+	public void testGetExactRef(Blackhole blackhole, BenchmarkState state)
+			throws IOException {
+		String refName = state.getRandomRefName();
+		blackhole.consume(state.repo.exactRef(refName));
+	}
+
+	@Benchmark
+	@BenchmarkMode({ Mode.AverageTime })
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
+	public void testGetExactRefCached(Blackhole blackhole, BenchmarkState state)
+			throws IOException {
+		String refName = state.getRandomRefName();
+		blackhole.consume(state.refCache.exactRef(refName));
+	}
+
+	@Benchmark
+	@BenchmarkMode({ Mode.AverageTime })
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
+	public void testGetRefsByPrefix(Blackhole blackhole, BenchmarkState state)
+			throws IOException {
+		String randomChangePrefix = state.getRandomRefsPrefix();
+		blackhole.consume(state.repo.getRefDatabase()
+				.getRefsByPrefix(randomChangePrefix));
+	}
+
+	@Benchmark
+	@BenchmarkMode({ Mode.AverageTime })
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+	@Measurement(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
+	public void testGetRefsByPrefixCached(Blackhole blackhole,
+			BenchmarkState state) throws IOException {
+		String randomChangePrefix = state.getRandomRefsPrefix();
+		blackhole.consume(state.refCache.getRefsByPrefix(randomChangePrefix));
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(GetRefsBenchmarkExistingRepo.class.getSimpleName())
+				// .addProfiler(StackProfiler.class)
+				// .addProfiler(GCProfiler.class)
+				.forks(1).jvmArgs("-ea").build();
+		new Runner(opt).run();
+	}
+}
