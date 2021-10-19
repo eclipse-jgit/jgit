eclipse-jgit/jgit

commit 9683bc71b6c05ce1ca2b5d6c6f7d74fff3db8429

Fix split package in bundle org.eclipse.jgit.ssh.jsch
The package org.eclipse.jgit.transport was split between
org.eclipse.jgit and org.eclipse.jgit.ssh.jsch.

Bug: 564544
Change-Id: I91d38e67c65ed97a880f8dc8f9559663b9eec33b

org.eclipse.jgit.ssh.jsch/resources/META-INF/services/org.eclipse.jgit.transport.SshSessionFactory
@@ -1,1 +1,0 @@
-org.eclipse.jgit.transport.JschConfigSessionFactory

org.eclipse.jgit.pgm/META-INF/MANIFEST.MF
@@ -43,6 +43,7 @@ Import-Package: javax.servlet;version="[3.1.0,4.0.0)",
  org.eclipse.jgit.transport;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.transport.http.apache;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.transport.resolver;version="[6.0.0,6.1.0)",
+ org.eclipse.jgit.transport.ssh.jsch;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.transport.sshd;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.treewalk;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.treewalk.filter;version="[6.0.0,6.1.0)",

org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/TextBuiltin.java
@@ -36,8 +36,8 @@ import org.eclipse.jgit.pgm.internal.CLIText;
 import org.eclipse.jgit.pgm.internal.SshDriver;
 import org.eclipse.jgit.pgm.opt.CmdLineParser;
 import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.transport.JschConfigSessionFactory;
 import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory;
 import org.eclipse.jgit.transport.sshd.DefaultProxyDataFactory;
 import org.eclipse.jgit.transport.sshd.JGitKeyCache;
 import org.eclipse.jgit.transport.sshd.SshdSessionFactory;

org.eclipse.jgit.ssh.jsch.test/META-INF/MANIFEST.MF
@@ -13,6 +13,7 @@ Import-Package: com.jcraft.jsch;version="[0.1.54,0.2.0)",
  org.eclipse.jgit.junit.ssh;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.lib;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.transport;version="[6.0.0,6.1.0)",
+ org.eclipse.jgit.transport.ssh.jsch;version="[6.0.0,6.1.0)",
  org.eclipse.jgit.util;version="[6.0.0,6.1.0)",
  org.hamcrest;version="[1.1.0,3.0.0)",
  org.junit;version="[4.13,5.0.0)",

org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/ssh/jsch/JSchSshProtocol2Test.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018, Thomas Wolf <thomas.wolf@paranor.ch> and others
+ * Copyright (C) 2020 Thomas Wolf <thomas.wolf@paranor.ch> and others
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
@@ -9,7 +9,7 @@
  */
 
 //TODO(ms): move to org.eclipse.jgit.ssh.jsch in 6.0
-package org.eclipse.jgit.transport;
+package org.eclipse.jgit.transport.ssh.jsch;
 
 import static org.junit.Assert.assertTrue;
 
@@ -20,19 +20,22 @@ import java.nio.file.Files;
 import java.util.Arrays;
 
 import org.eclipse.jgit.errors.TransportException;
-import org.eclipse.jgit.junit.ssh.SshTestBase;
+import org.eclipse.jgit.junit.ssh.SshBasicTestBase;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.transport.OpenSshConfig.Host;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.RemoteSession;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig.Host;
 import org.eclipse.jgit.util.FS;
-import org.junit.experimental.theories.Theories;
-import org.junit.runner.RunWith;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
-@RunWith(Theories.class)
-public class JSchSshTest extends SshTestBase {
+public class JSchSshProtocol2Test extends SshBasicTestBase {
 
 	private class TestSshSessionFactory extends JschConfigSessionFactory {
 
@@ -83,4 +86,11 @@ public class JSchSshTest extends SshTestBase {
 		return new OpenSshConfig(getTemporaryDirectory(), configFile);
 	}
 
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		StoredConfig config = ((Repository) db).getConfig();
+		config.setInt("protocol", null, "version", 2);
+		config.save();
+	}
 }

org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/ssh/jsch/JSchSshTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Thomas Wolf <thomas.wolf@paranor.ch> and others
+ * Copyright (C) 2018, Thomas Wolf <thomas.wolf@paranor.ch> and others
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
@@ -8,8 +8,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-//TODO(ms): move to org.eclipse.jgit.ssh.jsch in 6.0
-package org.eclipse.jgit.transport;
+package org.eclipse.jgit.transport.ssh.jsch;
 
 import static org.junit.Assert.assertTrue;
 
@@ -20,18 +19,23 @@ import java.nio.file.Files;
 import java.util.Arrays;
 
 import org.eclipse.jgit.errors.TransportException;
-import org.eclipse.jgit.junit.ssh.SshBasicTestBase;
+import org.eclipse.jgit.junit.ssh.SshTestBase;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.StoredConfig;
-import org.eclipse.jgit.transport.OpenSshConfig.Host;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.RemoteSession;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig.Host;
 import org.eclipse.jgit.util.FS;
+import org.junit.experimental.theories.Theories;
+import org.junit.runner.RunWith;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
-public class JSchSshProtocol2Test extends SshBasicTestBase {
+@RunWith(Theories.class)
+public class JSchSshTest extends SshTestBase {
 
 	private class TestSshSessionFactory extends JschConfigSessionFactory {
 
@@ -82,11 +86,4 @@ public class JSchSshProtocol2Test extends SshBasicTestBase {
 		return new OpenSshConfig(getTemporaryDirectory(), configFile);
 	}
 
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		StoredConfig config = ((Repository) db).getConfig();
-		config.setInt("protocol", null, "version", 2);
-		config.save();
-	}
 }

org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/ssh/jsch/JschConfigSessionFactoryTest.java
@@ -9,7 +9,7 @@
  */
 
 //TODO(ms): move to org.eclipse.jgit.ssh.jsch in 6.0
-package org.eclipse.jgit.transport;
+package org.eclipse.jgit.transport.ssh.jsch;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jgit.junit.MockSystemReader;
+import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.SystemReader;
 import org.junit.After;

org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/ssh/jsch/OpenSshConfigTest.java
@@ -8,8 +8,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-//TODO(ms): move to org.eclipse.jgit.ssh.jsch in 6.0
-package org.eclipse.jgit.transport;
+package org.eclipse.jgit.transport.ssh.jsch;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertArrayEquals;
@@ -29,7 +28,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jgit.junit.RepositoryTestCase;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.transport.OpenSshConfig.Host;
+import org.eclipse.jgit.transport.SshConstants;
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig.Host;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.util.SystemReader;

commit f698fbf919c67edac4b3999cc932c1f22da1dffd

JSch: fix service publication for ServiceLoader
The file name in META-INF/services must be the fully qualified
interface name; the content the fully qualified implementation class
name.

This was broken in commit 9683bc71.

Add a test for the default factory being found by the ServiceLoader.

Change-Id: I1f180d7f60e5c1e74a39bbd9a5f0099bd8343e21
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.jgit.ssh.jsch.test/tst/org/eclipse/jgit/transport/ssh/jsch/ServiceLoaderTest.java
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2021, Thomas Wolf <thomas.wolf@paranor.ch> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.transport.ssh.jsch;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.junit.Test;
+
+public class ServiceLoaderTest {
+
+	@Test
+	public void testDefaultFactoryFound() {
+		SshSessionFactory defaultFactory = SshSessionFactory.getInstance();
+		assertNotNull(defaultFactory);
+	}
+}