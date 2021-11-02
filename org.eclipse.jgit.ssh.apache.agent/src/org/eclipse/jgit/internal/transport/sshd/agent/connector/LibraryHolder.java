a09c15ff38fddfcd20eefa6ffe718a0e8d19f316
Thomas Wolf <thomas.wolf@paranor.ch>
2021-11-02 18:48:25 +0100

    sshd: add support for ssh-agent
    Add a simple SSH agent connector using JNA. Include com.sum.jna and
    com.sun.jna.platform in the target platform.
    
    JNA is used to communicate through Unix domain sockets with ssh-agent,
    and if on Windows, to communicate via shared memory with Pageant.
    
    The new bundle o.e.j.ssh.apache.agent is an OSGi fragment so that
    the java.util.ServiceLoader can find the provided factory without
    further ado in OSGi environments.
    
    Adapt both maven and bazel builds to include the new bundle.
    
    Manually tested on OS X, CentOS 7, and Win10 with Pageant 0.76. Tested
    by installing JGit built from this change into freshly downloaded
    Eclipse 2021-12 M1, and then doing git fetches via SSH with different
    ~/.ssh/config settings (explicit IdentityFile, without any but a key in
    the agent, with no keys and a key in the agent and IdentitiesOnly=yes
    (must fail)).
    
    Bug: 541274
    Bug: 541275
    Change-Id: I34e85467293707dbad1eb44d1f40fc2e70ba3622
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.jgit.ssh.apache.agent/src/org/eclipse/jgit/internal/transport/sshd/agent/connector/LibraryHolder.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2021, Thomas Wolf <thomas.wolf@paranor.ch> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.transport.sshd.agent.connector;
+
+import java.text.MessageFormat;
+
+import com.sun.jna.LastErrorException;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Kernel32Util;
+import com.sun.jna.platform.win32.User32;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Delay loading the native libraries until needed.
+ */
+class LibraryHolder {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(LibraryHolder.class);
+
+	private static LibraryHolder INSTANCE;
+
+	private static boolean libraryLoaded = false;
+
+	public static synchronized LibraryHolder getLibrary() {
+		if (!libraryLoaded) {
+			libraryLoaded = true;
+			try {
+				INSTANCE = new LibraryHolder();
+			} catch (Exception | UnsatisfiedLinkError
+					| NoClassDefFoundError e) {
+				LOG.error(Texts.get().logErrorLoadLibrary, e);
+			}
+		}
+		return INSTANCE;
+	}
+
+	User32 user;
+
+	Kernel32 kernel;
+
+	private LibraryHolder() {
+		user = User32.INSTANCE;
+		kernel = Kernel32.INSTANCE;
+	}
+
+	String systemError(String pattern) {
+		int lastError = kernel.GetLastError();
+		String msg;
+		try {
+			msg = Kernel32Util.formatMessageFromLastErrorCode(lastError);
+		} catch (Exception e) {
+			String err = e instanceof LastErrorException
+					? Integer.toString(((LastErrorException) e).getErrorCode())
+					: Texts.get().errUnknown;
+			msg = MessageFormat.format(Texts.get().errLastError,
+					Integer.toString(lastError), err);
+			LOG.error(msg, e);
+		}
+		return MessageFormat.format(pattern, Integer.toString(lastError), msg);
+	}
+
+}
---
+++ org.eclipse.jgit.ssh.apache.agent/src/org/eclipse/jgit/internal/transport/sshd/agent/connector/UnixSockets.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2021, Thomas Wolf <thomas.wolf@paranor.ch> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.transport.sshd.agent.connector;
+
+import com.sun.jna.LastErrorException;
+import com.sun.jna.Library;
+import com.sun.jna.Structure;
+import com.sun.jna.platform.unix.LibCAPI;
+
+/**
+ * Low-level Unix/Linux JNA socket API.
+ */
+interface UnixSockets extends LibCAPI, Library {
+
+	/**
+	 * Library to load. These functions live in libc.
+	 */
+	String LIBRARY_NAME = "c"; //$NON-NLS-1$
+
+	/**
+	 * Command to set the close-on-exec flag on a file descriptor via
+	 * {@link #fcntl(int, int, int)}.
+	 */
+	int F_SETFD = 2;
+
+	/**
+	 * Specifies that a file descriptor shall not be inherited by child
+	 * processes.
+	 */
+	int FD_CLOEXEC = 1;
+
+	/**
+	 * Creates a socket and returns a file descriptor for it.
+	 *
+	 * @param domain
+	 *            socket domain; use {@link Sockets#AF_UNIX}
+	 * @param type
+	 *            socket type; use {@link Sockets#SOCK_STREAM}
+	 * @param protocol
+	 *            socket communication protocol; use
+	 *            {@link Sockets#DEFAULT_PROTOCOL}.
+	 * @return file descriptor for the socket; should be closed eventually, or
+	 *         -1 on error.
+	 * @throws LastErrorException
+	 *             on errors
+	 * @see LibCAPI#close(int)
+	 */
+	int socket(int domain, int type, int protocol) throws LastErrorException;
+
+	/**
+	 * Simple binding to fcntl; used to set the FD_CLOEXEC flag. On OS X, we
+	 * cannot include SOCK_CLOEXEC in the socket() call.
+	 *
+	 * @param fd
+	 *            file descriptor to operate on
+	 * @param command
+	 *            set to {@link #F_SETFD}
+	 * @param flag
+	 *            zero to clear the close-on-exec flag, {@link #FD_CLOEXEC} to
+	 *            set it
+	 * @return -1 on error, otherwise a value >= 0
+	 * @throws LastErrorException
+	 */
+	int fcntl(int fd, int command, int flag) throws LastErrorException;
+
+	/**
+	 * Connects a file descriptor, which must refer to a socket, to a
+	 * {@link Sockets.SockAddr}.
+	 *
+	 * @param fd
+	 *            file descriptor of the socket, as returned by
+	 *            {@link #socket(int, int, int)}
+	 * @param addr
+	 *            address to connect to
+	 * @param addrLen
+	 *            Length of {@code addr}, use {@link Structure#size()}
+	 * @return 0 on success; -1 otherwise
+	 * @throws LastErrorException
+	 *             on errors
+	 */
+	int connect(int fd, Sockets.SockAddr addr, int addrLen)
+			throws LastErrorException;
+
+	/**
+	 * Read data from a file descriptor.
+	 *
+	 * @param fd
+	 *            file descriptor to read from
+	 * @param buf
+	 *            buffer to read into
+	 * @param bufLen
+	 *            maximum number of bytes to read; at most length of {@code buf}
+	 * @return number of bytes actually read; zero for EOF, -1 on error
+	 * @throws LastErrorException
+	 *             on errors
+	 */
+	LibCAPI.ssize_t read(int fd, byte[] buf, LibCAPI.size_t bufLen)
+			throws LastErrorException;
+
+	/**
+	 * Write data to a file descriptor.
+	 *
+	 * @param fd
+	 *            file descriptor to write to
+	 * @param data
+	 *            data to write
+	 * @param dataLen
+	 *            number of bytes to write
+	 * @return number of bytes actually written; -1 on error
+	 * @throws LastErrorException
+	 *             on errors
+	 */
+	LibCAPI.ssize_t write(int fd, byte[] data, LibCAPI.size_t dataLen)
+			throws LastErrorException;
+}
