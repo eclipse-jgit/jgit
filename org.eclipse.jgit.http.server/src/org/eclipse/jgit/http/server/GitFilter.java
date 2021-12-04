994434a1cac37d1569bbbbc13f363dff2a819071
Matthias Sohn <matthias.sohn@sap.com>
2021-12-04 18:34:48 +0100

    Add missing @since tags
    Change-Id: Iaeca1d6154c1383ebe27b723e8814e262328eb48

--- org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/GitFilter.java
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/GitFilter.java
@@ -164,6 +164,7 @@ public class GitFilter extends MetaFilter {
 	 *
 	 * @param h
 	 *            A custom error handler for git-receive-pack.
+	 * @since 5.7
 	 */
 	public void setReceivePackErrorHandler(ReceivePackErrorHandler h) {
 		assertNotInitialized();
