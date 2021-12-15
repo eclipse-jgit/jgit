2a1c358ce6b953f0b225b83f140da416431cc59d
Ivan Frade <ifrade@google.com>
2021-12-14 16:35:54 -0800

    DfsPackParser: Create object indices if config says so
    The DfsInserter writes the pack and its indices in the flush() method,
    but when the writing happens via DfsPackParser, it is the parser which
    writes the pack and indices. When combined with a parser, flushing the
    inserter is a noop.
    
    Add the writing of the object size index to the packparser#parse
    method, mirroring how the primary index is written.
    
    Change-Id: I52c5db153fea7e4a8ecd8b3d5de7ad21f7f81a60

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/dfs/DfsPackParserTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2023, Google LLC and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.dfs;
+
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_MIN_BYTES_OBJ_SIZE_INDEX;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_PACK_SECTION;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.zip.Deflater;
+
+import org.eclipse.jgit.internal.storage.dfs.DfsObjDatabase.PackList;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.transport.PackParser;
+import org.eclipse.jgit.util.NB;
+import org.eclipse.jgit.util.TemporaryBuffer;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DfsPackParserTest {
+  private InMemoryRepository repo;
+
+
+  @Before
+  public void setUp() throws Exception {
+    DfsRepositoryDescription desc = new DfsRepositoryDescription("test");
+    repo = new InMemoryRepository(desc);
+    repo.getConfig().setInt(CONFIG_PACK_SECTION, null,
+        CONFIG_KEY_MIN_BYTES_OBJ_SIZE_INDEX, 0);
+  }
+
+  @Test
+  public void parse_writeObjSizeIdx() throws IOException {
+    TemporaryBuffer.Heap pack = new TemporaryBuffer.Heap(1024);
+
+    // Sha1 of the blob "a"
+    ObjectId blobA = ObjectId.fromString("2e65efe2a145dda7ee51d1741299f848e5bf752e");
+
+    packHeader(pack, 2);
+    pack.write((Constants.OBJ_BLOB) << 4 | 1);
+    deflate(pack, new byte[] { 'a' });
+
+    pack.write((Constants.OBJ_REF_DELTA) << 4 | 4);
+    blobA.copyRawTo(pack);
+    deflate(pack, new byte[] { 0x1, 0x1, 0x1, 'b' });
+    digest(pack);
+
+    try (ObjectInserter ins = repo.newObjectInserter()) {
+      PackParser parser = ins.newPackParser(pack.openInputStream());
+      parser.parse(NullProgressMonitor.INSTANCE, NullProgressMonitor.INSTANCE);
+      ins.flush();
+    }
+
+    DfsReader reader = repo.getObjectDatabase().newReader();
+    PackList packList = repo.getObjectDatabase().getPackList();
+    assertEquals(1, packList.packs.length);
+    assertEquals(1, packList.packs[0].getIndexedObjectSize(reader, blobA));
+  }
+  
+  //TODO(ifrade): Duplicated with PackParserTest. Consolidate in a TestPackWriter.
+  private static void packHeader(TemporaryBuffer.Heap tinyPack, int cnt)
+      throws IOException {
+    final byte[] hdr = new byte[8];
+    NB.encodeInt32(hdr, 0, 2);
+    NB.encodeInt32(hdr, 4, cnt);
+
+    tinyPack.write(Constants.PACK_SIGNATURE);
+    tinyPack.write(hdr, 0, 8);
+  }
+
+
+  private static void deflate(TemporaryBuffer.Heap tinyPack,
+      final byte[] content)
+      throws IOException {
+    Deflater deflater = new Deflater();
+    byte[] buf = new byte[128];
+    deflater.setInput(content, 0, content.length);
+    deflater.finish();
+    do {
+      final int n = deflater.deflate(buf, 0, buf.length);
+      if (n > 0)
+        tinyPack.write(buf, 0, n);
+    } while (!deflater.finished());
+  }
+
+  private static void digest(TemporaryBuffer.Heap buf) throws IOException {
+    MessageDigest md = Constants.newMessageDigest();
+    md.update(buf.toByteArray());
+    buf.write(md.digest());
+  }
+}
