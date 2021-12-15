cedcb72a28defec44a9261b3ef50b603f9e6d890
Ivan Frade <ifrade@google.com>
2021-12-14 16:36:54 -0800

    PackObjectSizeIndex: interface and impl for the object-size index
    Operations like "clone --filter=blob:limit=N" or the "object-info"
    command need to read the size of the objects from the storage. An
    index would provide those sizes at ones rather than having to seek in
    the packfile.
    
    Introduce an interface for the Object-size index. This index returns
    the inflated size of an object. Not all objects could be indexed (to
    limit memory usage).
    
    This implementation indexes only blobs (no trees, nor
    commits) *above* certain size threshold (configurable). Lower
    threshold adds more objects to the index, consumes more memory and
    provides better performance. 0 means "all blobs" and -1 "disabled".
    
    If we don't index everything, for the filter use case is more
    efficient to index the biggest objects first: the set is small and
    most objects are filtered by NOT being in the index. For the
    object-size, the more objects in the index the better, regardless
    their size. All together, it is more helpful to index above threshold.
    
    Change-Id: I9ed608ac240677e199b90ca40d420bcad9231489

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackObjectSizeIndex.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022, Google LLC and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.file;
+
+/**
+ * Index of object sizes in a pack
+ *
+ * It is not guaranteed that the implementation contains the sizes of all
+ * objects (e.g. it could store only objects over certain threshold).
+ */
+public interface PackObjectSizeIndex {
+
+	/**
+	 * Returns the inflated size of the object.
+	 *
+	 * @param idxOffset
+	 *            position in the pack (as returned from PackIndex)
+	 * @return size of the object, -1 if not found in the index.
+	 */
+	long getSize(int idxOffset);
+
+	/**
+	 * Number of objects in the index
+	 *
+	 * @return number of objects in the index
+	 */
+	long getObjectCount();
+
+	PackObjectSizeIndex EMPTY = new PackObjectSizeIndex() {
+		@Override
+		public long getSize(int idxOffset) {
+			return -1;
+		}
+
+		@Override
+		public long getObjectCount() {
+			return 0;
+		}
+	};
+}
