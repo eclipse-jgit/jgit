4b6fe1dd621c25dc5b9daa1ed18d7a0d9af41faa
Ivan Frade <ifrade@google.com>
2021-12-28 14:23:40 -0800

    PackWriter: offer to write an object-size index for the pack
    PackWriter callers tell the writer what do the want to include in the
    pack and invoke #writePack(). Afterwards, they can invoke #writeIndex()
    to write the corresponding pack index.
    
    Mirror this for the object-size index, adding a #writeObjectSizeIndex()
    method.
    
    Change-Id: Ic319975c72c239cd6488303f7d4cced797e6fe00

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/PackWriterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/PackWriterTest.java
@@ -505,6 +505,89 @@ public class PackWriterTest extends SampleDataRepositoryTestCase {
 	}
 
 	@Test
+	public void testWriteObjectSizeIndex_noDeltas() throws Exception {
+		config.setMinBytesForObjSizeIndex(0);
+		HashSet<ObjectId> interesting = new HashSet<>();
+		interesting.add(ObjectId
+				.fromString("82c6b885ff600be425b4ea96dee75dca255b69e7"));
+
+		NullProgressMonitor m1 = NullProgressMonitor.INSTANCE;
+		writer = new PackWriter(config, db.newObjectReader());
+		writer.setUseBitmaps(false);
+		writer.setThin(false);
+		writer.setIgnoreMissingUninteresting(false);
+		writer.preparePack(m1, interesting, NONE);
+		writer.writePack(m1, m1, os);
+
+		PackIndex idx;
+		try (ByteArrayOutputStream is = new ByteArrayOutputStream()) {
+			writer.writeIndex(is);
+			idx = PackIndex.read(new ByteArrayInputStream(is.toByteArray()));
+		}
+
+		PackObjectSizeIndex objSizeIdx;
+		try (ByteArrayOutputStream objSizeStream = new ByteArrayOutputStream()) {
+			writer.writeObjectSizeIndex(objSizeStream);
+			objSizeIdx = PackObjectSizeIndexLoader.load(
+					new ByteArrayInputStream(objSizeStream.toByteArray()));
+
+		}
+		writer.close();
+
+		ObjectId knownBlob1 = ObjectId
+				.fromString("5b6e7c66c276e7610d4a73c70ec1a1f7c1003259");
+		ObjectId knownBlob2 = ObjectId
+				.fromString("6ff87c4664981e4397625791c8ea3bbb5f2279a3");
+		assertEquals(18009, objSizeIdx.getSize(idx.findPosition(knownBlob1)));
+		assertEquals(18787, objSizeIdx.getSize(idx.findPosition(knownBlob2)));
+	}
+
+	@Test
+	public void testWriteObjectSizeIndex_withDeltas() throws Exception {
+		config.setDeltaCompress(true);
+		config.setMinBytesForObjSizeIndex(0);
+
+		PackIndex idx;
+		PackObjectSizeIndex objSizeIdx;
+
+		// TestRepository will close repo
+		FileRepository repo = createBareRepository();
+		ArrayList<RevObject> blobs = new ArrayList<>();
+		try (TestRepository<FileRepository> testRepo = new TestRepository<>(
+				repo)) {
+			blobs.add(testRepo.blob(genDeltableData(1000)));
+			blobs.add(testRepo.blob(genDeltableData(1005)));
+			try (PackWriter pw = new PackWriter(config,
+					repo.newObjectReader())) {
+				NullProgressMonitor m = NullProgressMonitor.INSTANCE;
+				pw.preparePack(blobs.iterator());
+				pw.writePack(m, m, os);
+
+				try (ByteArrayOutputStream is = new ByteArrayOutputStream()) {
+					pw.writeIndex(is);
+					idx = PackIndex
+							.read(new ByteArrayInputStream(is.toByteArray()));
+				}
+
+				try (ByteArrayOutputStream objSizeStream = new ByteArrayOutputStream()) {
+					pw.writeObjectSizeIndex(objSizeStream);
+					objSizeIdx = PackObjectSizeIndexLoader
+							.load(new ByteArrayInputStream(
+									objSizeStream.toByteArray()));
+
+				}
+				PackStatistics stats = pw.getStatistics();
+				assertEquals(1, stats.getTotalDeltas());
+				assertTrue("Delta bytes not set.",
+						stats.byObjectType(OBJ_BLOB).getDeltaBytes() > 0);
+			}
+		}
+
+		assertEquals(1000, objSizeIdx.getSize(idx.findPosition(blobs.get(0))));
+		assertEquals(1005, objSizeIdx.getSize(idx.findPosition(blobs.get(1))));
+	}
+
+	@Test
 	public void testExclude() throws Exception {
 		// TestRepository closes repo
 		FileRepository repo = createBareRepository();
