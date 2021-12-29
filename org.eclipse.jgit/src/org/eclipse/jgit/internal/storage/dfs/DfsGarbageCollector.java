f7be83515190afd0daaf78d805f2e4fc03e1333d
Ivan Frade <ifrade@google.com>
2021-12-29 15:39:41 -0800

    DFSGarbargeCollector: Write object size indices
    PackWriter knows how to add an object size index to the pack, but the
    garbage collector is not using it yet.
    
    Teach DfsGarbabgeCollector to write the object size index on
    writePack(). Disable by default in the unreachable-garbage pack.
    
    Callers control the content/presence of the index through the
    PackConfig option (minBytesForObjSizeIndex) for all other packs, so
    there is no need of a specific flag in DfsGarbageCollector.
    
    Change-Id: I86f5f17310e6913381125bec4caab32dc45b7c9d

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
@@ -20,6 +20,7 @@ import static org.eclipse.jgit.internal.storage.dfs.DfsPackCompactor.configureRe
 import static org.eclipse.jgit.internal.storage.pack.PackExt.BITMAP_INDEX;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.COMMIT_GRAPH;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.INDEX;
+import static org.eclipse.jgit.internal.storage.pack.PackExt.OBJECT_SIZE_INDEX;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.PACK;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.REFTABLE;
 import static org.eclipse.jgit.internal.storage.pack.PackWriter.NONE;
@@ -578,6 +579,7 @@ public class DfsGarbageCollector {
 		cfg.setDeltaCompress(false);
 		cfg.setBuildBitmaps(false);
 		cfg.setWriteReverseIndex(false);
+		cfg.setMinBytesForObjSizeIndex(-1);
 
 		try (PackWriter pw = new PackWriter(cfg, ctx);
 				RevWalk pool = new RevWalk(ctx)) {
@@ -679,6 +681,15 @@ public class DfsGarbageCollector {
 			pack.setFileSize(INDEX, cnt.getCount());
 			pack.setBlockSize(INDEX, out.blockSize());
 			pack.setIndexVersion(pw.getIndexVersion());
+		}
+
+		try (DfsOutputStream out = objdb.writeFile(pack,
+				OBJECT_SIZE_INDEX)) {
+			CountingOutputStream cnt = new CountingOutputStream(out);
+			pw.writeObjectSizeIndex(cnt);
+			pack.addFileExt(OBJECT_SIZE_INDEX);
+			pack.setFileSize(OBJECT_SIZE_INDEX, cnt.getCount());
+			pack.setBlockSize(OBJECT_SIZE_INDEX, out.blockSize());
 		}
 
 		if (pw.prepareBitmapIndex(pm)) {
