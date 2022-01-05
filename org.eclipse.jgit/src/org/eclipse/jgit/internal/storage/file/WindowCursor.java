27ad732253a1d31c7650fa602fa980daccb46e49
Ivan Frade <ifrade@google.com>
2022-01-05 11:09:35 -0800

    WindowCursor: Use the object size index when possible
    The object size index has the size of all blobs over certain threshold
    already in memory. For these objects, it should be faster to read the
    size from this index than going to storage (IO).
    
    Use the index when available and possible inside #getObjectSize() (which
    returns the exact size of the object) and #isSmallerThan() (which is a
    more relaxed check that the size is below certain threshold).
    
    Change-Id: Iae9ff8848c236be21ca3dbe5606cbd29409de608

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/WindowCursor.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/WindowCursor.java
@@ -55,6 +55,8 @@ final class WindowCursor extends ObjectReader implements ObjectReuseAsIs {
 
 	private DeltaBaseCache baseCache;
 
+	private Pack lastPack;
+
 	@Nullable
 	private final ObjectInserter createdFromInserter;
 
@@ -155,9 +157,7 @@ final class WindowCursor extends ObjectReader implements ObjectReuseAsIs {
 		return db.getShallowCommits();
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public long getObjectSize(AnyObjectId objectId, int typeHint)
+	private long getObjectSizeStorage(AnyObjectId objectId, int typeHint)
 			throws MissingObjectException, IncorrectObjectTypeException,
 			IOException {
 		long sz = db.getObjectSize(this, objectId);
@@ -168,6 +168,63 @@ final class WindowCursor extends ObjectReader implements ObjectReuseAsIs {
 			throw new MissingObjectException(objectId.copy(), typeHint);
 		}
 		return sz;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public long getObjectSize(AnyObjectId objectId, int typeHint)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		// Async queue uses hint OBJ_ANY
+		if (typeHint != Constants.OBJ_BLOB) {
+			return getObjectSizeStorage(objectId, typeHint);
+		}
+
+		Pack pack = findPack(objectId);
+		if (pack == null) {
+			// Non-packed object (e.g. loose or in alternates)
+			return getObjectSizeStorage(objectId, typeHint);
+		}
+
+		long sz = pack.getIndexedObjectSize(objectId);
+		if (sz >= 0) {
+			return sz;
+		}
+		return getObjectSizeStorage(objectId, typeHint);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean isNotLargerThan(AnyObjectId objectId, int typeHint,
+			long size)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		if (typeHint != Constants.OBJ_BLOB) {
+			return getObjectSizeStorage(objectId, typeHint) <= size;
+		}
+
+		Pack pack = findPack(objectId);
+		if (pack == null || !pack.hasObjSizeIndex()) {
+			// Non-packed object (e.g. loose or in alternates)
+			return getObjectSizeStorage(objectId, typeHint) <= size;
+		}
+
+		return pack.getIndexedObjectSize(objectId) <= size;
+	}
+
+	private Pack findPack(AnyObjectId objectId) throws IOException {
+		if (lastPack != null && lastPack.hasObject(objectId)) {
+			return lastPack;
+		}
+
+		for (Pack p : db.getPacks()) {
+			if (p.hasObject(objectId)) {
+				lastPack = p;
+				return p;
+			}
+		}
+
+		return null;
 	}
 
 	/** {@inheritDoc} */
