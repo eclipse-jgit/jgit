7089a87ec1d6aff685d53befe8ecb9ec01b80965
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2022-02-01 17:05:02 +0000

    wip
    Change-Id: Ic1018b214be1e83d895979ee6c7cbce3f6765f6f

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/BasePackConnectionTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/BasePackConnectionTest.java
@@ -16,10 +16,16 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.eclipse.jgit.errors.NoRemoteRepositoryException;
+import org.eclipse.jgit.errors.TransportException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectIdRef;
@@ -28,6 +34,49 @@ import org.eclipse.jgit.lib.SymbolicRef;
 import org.junit.Test;
 
 public class BasePackConnectionTest {
+
+	@Test
+	public void foo() throws URISyntaxException, TransportException {
+		TransportLocal tl = new TransportLocal(new URIish("dummy"), new java.io.File("")) {
+			@Override
+			public FetchConnection openFetch() throws TransportException {
+				URIish uri = null;
+				try {
+					uri = new URIish("foo");
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				NoRemoteRepositoryException e = new NoRemoteRepositoryException(uri, "REPO: not found");
+				e.initCause(new java.io.EOFException("Short read of block"));
+				throw e;
+			}
+		};
+		class BpcTest extends BasePackPushConnection {
+			BpcTest(PackTransport packTransport) {
+				super(packTransport);
+				foo();
+			}
+			public boolean entryPoint() throws TransportException {
+				return readAdvertisedRefs();
+			}
+			void foo() {
+				class Pli extends PacketLineIn {
+					public Pli() {
+						super(InputStream.nullInputStream());
+					}
+
+					@Override
+					public String readString() throws IOException {
+						throw new EOFException("Short read of block");
+					}
+				}
+				pckIn = new Pli();
+			}
+
+		}
+		BpcTest bpc = new BpcTest(tl);
+		bpc.entryPoint();
+	}
 
 	@Test
 	public void testUpdateWithSymRefsAdds() {
