13cd0525f511e5c088efd4265fd8e762f129b8a2
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2022-02-04 16:13:27 +0000

    Stop initCause throwing in readAdvertisedRefs
    BasePackConnection::readAdvertisedRefsImpl was creating an exception by
    calling `noRepository`, and then blindly calling `initCause` on it. As
    `noRepository` can be overridden, it's not guaranteed to be missing a
    cause.
    
    BasePackPushConnection overrides `noRepository` and initiates a fetch,
    which may throw a `NoRemoteRepositoryException` with a cause.
    
    In this case calling `initCause` threw an `IllegalStateException`.
    
    In order to throw the correct exception, we now return the
    BasePackPushConnection exception and suppress the one thrown in
    BasePackConnection.
    
    Bug: 578511
    Change-Id: Ic1018b214be1e83d895979ee6c7cbce3f6765f6f

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/BasePackPushConnectionTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Darius Jokilehto <darius.jokilehto+os@gmail.com>
+ * Copyright (c) 2022 Antonio Barone <syntonyze@gmail.com>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.transport;
+
+import org.eclipse.jgit.errors.NoRemoteRepositoryException;
+import org.eclipse.jgit.errors.TransportException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class BasePackPushConnectionTest {
+	@Test
+	public void testNoRepositoryPropagatesNoRemoteRepositoryException() {
+		NoRemoteRepositoryException rootException = new NoRemoteRepositoryException(
+				new URIish(), "not found");
+		IOException suppressedException = new IOException("not read");
+
+		try (FailingBasePackPushConnection fbppc = new FailingBasePackPushConnection(
+				rootException)) {
+			TransportException result = fbppc.noRepository(suppressedException);
+
+			assertEquals(rootException, result);
+			assertEquals(1, result.getSuppressed().length);
+			assertEquals(suppressedException, result.getSuppressed()[0]);
+		}
+	}
+
+	private static class FailingBasePackPushConnection
+			extends BasePackPushConnection {
+		FailingBasePackPushConnection(TransportException transportException) {
+			super(new TransportLocal(new URIish(), new java.io.File("")) {
+				@Override
+				public FetchConnection openFetch() throws TransportException {
+					throw transportException;
+				}
+			});
+		}
+	}
+}
