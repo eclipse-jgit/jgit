fd832a92c3d50604730c76b60daffc679b560007
Thomas Wolf <thomas.wolf@paranor.ch>
2022-02-13 23:24:14 +0100

    Simplify implementation of WorkingTreeIterator
    All the filtering in WorkingTreeIterator is for check-in, i.e., clean
    filtering. The implementation was in some parts too general, passing
    around an OperationType. But since it's always CHECKIN_OP, that's not
    actually necessary.
    
    Change-Id: I73f8bc059e485a073e456962868f52b3a3db4fc1
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2010, Christian Halstrick <christian.halstrick@sap.com>
  * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
- * Copyright (C) 2012-2021, Robin Rosenberg and others
+ * Copyright (C) 2012, 2022, Robin Rosenberg and others
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
@@ -387,8 +387,8 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 				state.initializeReadBuffer();
 
 				final long len = e.getLength();
-				InputStream filteredIs = possiblyFilteredInputStream(e, is, len,
-						OperationType.CHECKIN_OP);
+				InputStream filteredIs = possiblyFilteredInputStream(e, is,
+						len);
 				return computeHash(filteredIs, canonLen);
 			} finally {
 				safeClose(is);
@@ -400,23 +400,17 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 	}
 
 	private InputStream possiblyFilteredInputStream(final Entry e,
-			final InputStream is, final long len) throws IOException {
-		return possiblyFilteredInputStream(e, is, len, null);
-
-	}
-
-	private InputStream possiblyFilteredInputStream(final Entry e,
-			final InputStream is, final long len, OperationType opType)
+			final InputStream is, final long len)
 			throws IOException {
 		if (getCleanFilterCommand() == null
-				&& getEolStreamType(opType) == EolStreamType.DIRECT) {
+				&& getEolStreamType() == EolStreamType.DIRECT) {
 			canonLen = len;
 			return is;
 		}
 
 		if (len <= MAXIMUM_FILE_SIZE_TO_READ_FULLY) {
 			ByteBuffer rawbuf = IO.readWholeStream(is, (int) len);
-			rawbuf = filterClean(rawbuf.array(), rawbuf.limit(), opType);
+			rawbuf = filterClean(rawbuf.array(), rawbuf.limit());
 			canonLen = rawbuf.limit();
 			return new ByteArrayInputStream(rawbuf.array(), 0, (int) canonLen);
 		}
@@ -426,14 +420,13 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 				return is;
 			}
 
-		final InputStream lenIs = filterClean(e.openInputStream(),
-				opType);
+			final InputStream lenIs = filterClean(e.openInputStream());
 		try {
 			canonLen = computeLength(lenIs);
 		} finally {
 			safeClose(lenIs);
 		}
-		return filterClean(is, opType);
+		return filterClean(is);
 	}
 
 	private static void safeClose(InputStream in) {
@@ -455,23 +448,19 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 		}
 	}
 
-	private ByteBuffer filterClean(byte[] src, int n, OperationType opType)
+	private ByteBuffer filterClean(byte[] src, int n)
 			throws IOException {
 		InputStream in = new ByteArrayInputStream(src);
 		try {
-			return IO.readWholeStream(filterClean(in, opType), n);
+			return IO.readWholeStream(filterClean(in), n);
 		} finally {
 			safeClose(in);
 		}
 	}
 
-	private InputStream filterClean(InputStream in) throws IOException {
-		return filterClean(in, null);
-	}
-
-	private InputStream filterClean(InputStream in, OperationType opType)
+	private InputStream filterClean(InputStream in)
 			throws IOException {
-		in = handleAutoCRLF(in, opType);
+		in = EolStreamTypeUtil.wrapInputStream(in, getEolStreamType());
 		String filterCommand = getCleanFilterCommand();
 		if (filterCommand != null) {
 			if (FilterCommandRegistry.isRegistered(filterCommand)) {
@@ -507,11 +496,6 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 			return result.getStdout().openInputStreamWithAutoDestroy();
 		}
 		return in;
-	}
-
-	private InputStream handleAutoCRLF(InputStream in, OperationType opType)
-			throws IOException {
-		return EolStreamTypeUtil.wrapInputStream(in, getEolStreamType(opType));
 	}
 
 	/**
@@ -1423,28 +1407,11 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 	 * @since 4.3
 	 */
 	public EolStreamType getEolStreamType() throws IOException {
-		return getEolStreamType(null);
-	}
-
-	/**
-	 * @param opType
-	 *            The operationtype (checkin/checkout) which should be used
-	 * @return the eol stream type for the current entry or <code>null</code> if
-	 *         it cannot be determined. When state or state.walk is null or the
-	 *         {@link TreeWalk} is not based on a {@link Repository} then null
-	 *         is returned.
-	 * @throws IOException
-	 */
-	private EolStreamType getEolStreamType(OperationType opType)
-			throws IOException {
 		if (eolStreamTypeHolder == null) {
 			EolStreamType type = null;
 			if (state.walk != null) {
-				type = state.walk.getEolStreamType(opType);
-				OperationType operationType = opType != null ? opType
-						: state.walk.getOperationType();
-				if (OperationType.CHECKIN_OP.equals(operationType)
-						&& EolStreamType.AUTO_LF.equals(type)
+				type = state.walk.getEolStreamType(OperationType.CHECKIN_OP);
+				if (EolStreamType.AUTO_LF.equals(type)
 						&& hasCrLfInIndex(getDirCacheIterator())) {
 					// If text=auto (or core.autocrlf=true) and the file has
 					// already been committed with CR/LF, then don't convert.
