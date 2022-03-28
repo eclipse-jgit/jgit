5a0df63b01536e8ffc00cc715574cacf4b053e15
yunjieli <yunjieli@google.com>
2022-03-28 14:47:02 -0700

    Fetch: Introduce negative refspecs.
    Implement negative refspecs in JGit fetch, following C Git. Git
    supports negative refspecs in source only while this change supports
    them in both source and destination.
    
    If one branch is equal to any branch or matches any pattern in the
    negative refspecs collection, the branch will not be fetched even if
    it's in the toFetch collection.
    
    With this feature, users can express more complex patterns.
    
    Change-Id: Iaa1cd4de5c08c273e198b72e12e3dadae7be709f

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/Transport.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/Transport.java
@@ -1256,36 +1256,32 @@ public abstract class Transport implements AutoCloseable {
 	 * {@link org.eclipse.jgit.transport.RemoteConfig} with fetch RefSpecs
 	 * defined.
 	 *
-	 * @param monitor
-	 *            progress monitor to inform the user about our processing
-	 *            activity. Must not be null. Use
-	 *            {@link org.eclipse.jgit.lib.NullProgressMonitor} if progress
-	 *            updates are not interesting or necessary.
-	 * @param toFetch
-	 *            specification of refs to fetch locally. May be null or the
-	 *            empty collection to use the specifications from the
-	 *            RemoteConfig. Source for each RefSpec can't be null.
-	 * @param branch
-	 *            the initial branch to check out when cloning the repository.
-	 *            Can be specified as ref name (<code>refs/heads/master</code>),
-	 *            branch name (<code>master</code>) or tag name
-	 *            (<code>v1.2.3</code>). The default is to use the branch
-	 *            pointed to by the cloned repository's HEAD and can be
-	 *            requested by passing {@code null} or <code>HEAD</code>.
+	 * @param monitor progress monitor to inform the user about our processing
+	 *                activity. Must not be null. Use
+	 *                {@link org.eclipse.jgit.lib.NullProgressMonitor} if
+	 *                progress updates are not interesting or necessary.
+	 * @param toFetch specification of refs to fetch locally. May be null or the
+	 *                empty collection to use the specifications from the
+	 *                RemoteConfig. Source for each RefSpec can't be null.
+	 * 
+	 * @param branch  the initial branch to check out when cloning the
+	 *                repository. Can be specified as ref name
+	 *                (<code>refs/heads/master</code>), branch name
+	 *                (<code>master</code>) or tag name (<code>v1.2.3</code>).
+	 *                The default is to use the branch pointed to by the cloned
+	 *                repository's HEAD and can be requested by passing
+	 *                {@code null} or <code>HEAD</code>.
 	 * @return information describing the tracking refs updated.
-	 * @throws org.eclipse.jgit.errors.NotSupportedException
-	 *             this transport implementation does not support fetching
-	 *             objects.
-	 * @throws org.eclipse.jgit.errors.TransportException
-	 *             the remote connection could not be established or object
-	 *             copying (if necessary) failed or update specification was
-	 *             incorrect.
+	 * @throws org.eclipse.jgit.errors.NotSupportedException this transport
+	 *         implementation does not support fetching objects.
+	 * @throws org.eclipse.jgit.errors.TransportException the remote connection
+	 *         could not be established or object copying (if necessary) failed
+	 *         or update specification was incorrect.
 	 * @since 5.11
 	 */
 	public FetchResult fetch(final ProgressMonitor monitor,
 			Collection<RefSpec> toFetch, String branch)
-			throws NotSupportedException,
-			TransportException {
+			throws NotSupportedException, TransportException {
 		if (toFetch == null || toFetch.isEmpty()) {
 			// If the caller did not ask for anything use the defaults.
 			//
