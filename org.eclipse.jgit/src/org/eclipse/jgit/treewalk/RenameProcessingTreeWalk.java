6c768334ebd04e9b8591c1a218007f629233b62e
Marija Savtchouk <mariasavtchouk@google.com>
2022-04-04 17:23:11 +0100

    Another attempt to fix renames
    Change-Id: If204a51f25cf17bc9c008260ad6ec7a9b86b7425

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/RenameProcessingTreeWalk.java
@@ -0,0 +1,83 @@
+package org.eclipse.jgit.treewalk;
+
+import java.util.Arrays;
+import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+
+public class RenameProcessingTreeWalk  extends NameConflictTreeWalk {
+
+  private static final int TREE_MODE = FileMode.TREE.getBits();
+
+  private boolean fastMinHasMatch;
+
+  private String renamePath;
+
+  /**
+   * Create a new tree walker for a given repository.
+   *
+   * @param repo the repository the walker will obtain data from.
+   */
+  public RenameProcessingTreeWalk(Repository repo) {
+    super(repo);
+  }
+
+  /**
+   * Create a new tree walker for a given repository.
+   *
+   * @param repo the repository the walker will obtain data from.
+   * @param or the reader the walker will obtain tree data from.
+   * @since 4.3
+   */
+  public RenameProcessingTreeWalk(@Nullable Repository repo, ObjectReader or) {
+    super(repo, or);
+  }
+
+  /**
+   * Create a new tree walker for a given repository.
+   *
+   * @param or the reader the walker will obtain tree data from.
+   */
+  public RenameProcessingTreeWalk(ObjectReader or) {
+    super(or);
+  }
+
+  public void swapRenameTree(int nth, CanonicalTreeParser tree) {
+    trees[nth] = tree;
+    trees[nth].matches = currentHead;
+  }
+
+  public void setPathName(String pathName) {
+    this.renamePath = pathName;
+  }
+
+  @Override
+  public byte[] getRawPath() {
+    return renamePath != null ? renamePath.getBytes() : super.getRawPath();
+  }
+
+  @Override
+  public String getPathString() {
+    return renamePath != null ? renamePath : super.getPathString();
+  }
+
+  AbstractTreeIterator[] matchTrees;
+  public void swapRenames(AbstractTreeIterator[] swapTrees) {
+    matchTrees = new AbstractTreeIterator[swapTrees.length];
+    for (int i = 0; i < swapTrees.length; i++) {
+      matchTrees[i] = this.trees[i].matches;
+      if (swapTrees[i] == null && this.getTree(i, CanonicalTreeParser.class) != null) {
+        this.trees[i].matches = null;
+      }
+    }
+  }
+  public void swapMatchBack() {
+    if (matchTrees != null) {
+      for (int i = 0; i < matchTrees.length; i++) {
+        this.trees[i].matches = matchTrees[i];
+      }
+      matchTrees = null;
+    }
+  }
+}
