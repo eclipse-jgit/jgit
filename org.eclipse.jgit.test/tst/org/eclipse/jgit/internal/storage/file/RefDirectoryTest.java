baf096251e12ff75a616b0dd523a992d984f3059
Luca Milanesio <luca.milanesio@gmail.com>
2022-04-08 14:08:05 +0100

    Optimise loading of PackedRefList when content hasn't changed
    When using core.trustfolderstat set to false or when the last
    update is within the filesystem resolution, the packedrefs
    are loaded at every lookup (by ref or by prefix) which is proven
    to be very expensive:
    1. the full packedrefs is loaded from disk every time
    2. the full content is used for SHA1 computation
    3. all PackedRefList objects are create in memory
    
    Keep a copy of the raw binary content in the PackedRefList so that
    it can be checked byte-by-byte with the one read from disk and, if
    that hasn't changed, avoid to perform 2 and 3.
    
    The refs benchmark test (GetRefsBenchmark) on a 500k-refs
    (33 MBytes packed-refs) repository on a local SSD with
    core.trustfolderstat=false shows:
    - Before this change: 250ms /ref-lookup
    - With this change: 22ms /ref-lookup
    
    On a shared NFS drive on the same repo:
    - Before this change: 500ms /ref-lookup
    - After this change: 120ms /ref-lookup
    
    The associated performance figure is therefore between 4x and 10x
    times faster, even though there is a memory footprint involved.
    
    Make the choice between speed vs. memory overhead with the
    core.comparePackedRefsBySha1 setting, which is true by default
    for backward compatibilty.
    Also specify that setting the flat to false has memory footprint
    implications but a 10x faster load and comparison performance.
    
    Added a specific test in RefDirectoryTest to demonstrate that
    when reading the same packed-refs file twice would avoid the
    reloading of the PackedRefList and recomputation of the SHA1
    when core.comparepackedrefsbysha1 is set to false.
    By default instead the PackedRefList is recreated twice
    for the purpose of comparing the two objects by SHA1.
    
    Also, the test demonstrate that the optimisation is valid
    also when trustfolderstats is true (default) because of the
    racy reads due to the underlying filesystem resolution.
    
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>
    Change-Id: Ia378f8f2284411fdecb01c10f8faa5665c6fb579

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/RefDirectoryTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/RefDirectoryTest.java
@@ -44,10 +44,12 @@ import org.eclipse.jgit.junit.LocalDiskRepositoryTestCase;
 import org.eclipse.jgit.junit.Repeat;
 import org.eclipse.jgit.junit.TestRepository;
 import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Ref.Storage;
 import org.eclipse.jgit.lib.RefDatabase;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.util.FS;
@@ -1335,6 +1337,39 @@ public class RefDirectoryTest extends LocalDiskRepositoryTestCase {
 		}
 		Ref ref = refdir.findRef("refs/heads/master");
 		assertEquals(Storage.LOOSE, ref.getStorage());
+	}
+
+	@Test
+	public void testReadPackedRefs_ReloadPackedRefListByDefault() throws IOException {
+		writePackedRefs(A.name() + " refs/heads/master\n");
+		RefDirectory testRefDir = (RefDirectory) refdir.getRepository().getRefDatabase();
+
+		RefDirectory.PackedRefList packedRefsList = testRefDir.getPackedRefs();
+		RefDirectory.PackedRefList readPackedRefsList = testRefDir.readPackedRefs(packedRefsList);
+
+		assertEquals(readPackedRefsList.getId(), packedRefsList.getId());
+		assertNotSame(packedRefsList, readPackedRefsList);
+	}
+
+	@Test
+	public void testReadPackedRefs_CompareByContentAvoidReloadPackedRefList() throws IOException {
+		StoredConfig repoConfig = refdir.getRepository().getConfig();
+		repoConfig.setBoolean(
+				ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_KEY_COMPARE_PACKED_REFS_BY_SHA1, false);
+		repoConfig.save();
+
+		writePackedRefs(A.name() + " refs/heads/master\n");
+		// Reopen the repository to reload the amended config
+		try (FileRepository testRepository = new FileRepository(diskRepo.getDirectory())) {
+			RefDirectory testRefDir = (RefDirectory) testRepository.getRefDatabase();
+
+			RefDirectory.PackedRefList packedRefsList = testRefDir.getPackedRefs();
+			RefDirectory.PackedRefList readPackedRefsList = testRefDir.readPackedRefs(packedRefsList);
+
+			assertEquals(readPackedRefsList.getId(), packedRefsList.getId());
+			assertSame(packedRefsList, readPackedRefsList);
+		}
 	}
 
 	private void writeLooseRef(String name, AnyObjectId id) throws IOException {
