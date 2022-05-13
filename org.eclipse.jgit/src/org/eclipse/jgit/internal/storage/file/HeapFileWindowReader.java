8d284d09f7e7cea992acea3b953b317891d162c1
Matthias Sohn <matthias.sohn@sap.com>
2022-05-13 23:39:39 +0200

    Extract interface for reading pack file pages from WindowCache
    WindowCache currently supports two ways to cache pages of git pack
    files:
    - read and copy page to a byte array allocated on the Java heap
    - map page into virtual memory using NIO virtual memory mapping and
      access it via ByteBuffer. This approach turned out to be prone to
      crashes since Java doesn't provide an official API for unmapping.
    
    Extract an interface FileWindowReader and move the existing
    implementations into separate classes.
    
    This is a preparation to introduce mapping pack files using Java 17
    MemorySegment which is a part of project Panama's foreign memory API.
    
    Change-Id: Ic6110e4048273cc53f8b967b8a5ebf370bf1f452

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/HeapFileWindowReader.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2008, 2009 Google Inc.
+ * Copyright (C) 2008, 2022 Shawn O. Pearce <spearce@spearce.org> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.file;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Read from file and copy window of data to ByteArrayWindow allocated on the
+ * heap
+ */
+public class HeapFileWindowReader implements FileWindowReader {
+
+	private final Pack pack;
+	private RandomAccessFile fd;
+
+	private long length;
+
+	HeapFileWindowReader(Pack pack) {
+		this.pack = pack;
+	}
+
+	@Override
+	public FileWindowReader open() throws IOException {
+		fd = new RandomAccessFile(pack.getPackFile(), "r"); //$NON-NLS-1$
+		length = fd.length();
+		return this;
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (fd != null) {
+			try {
+				fd.close();
+			} catch (IOException err) {
+				// Ignore a close event. We had it open only for reading.
+				// There should not be errors related to network buffers
+				// not flushed, etc.
+			}
+			fd = null;
+		}
+	}
+
+	@Override
+	public long length() {
+		return length;
+	}
+
+	@Override
+	public ByteWindow read(long pos, int size) throws IOException {
+		if (length < pos + size) {
+			size = (int) (length - pos);
+		}
+		final byte[] buf = new byte[size];
+		fd.seek(pos);
+		fd.readFully(buf, 0, size);
+		return new ByteArrayWindow(pack, pos, buf);
+	}
+
+	@Override
+	public void readRaw(byte[] b, long off, int len) throws IOException {
+		fd.seek(off);
+		fd.readFully(b, 0, len);
+	}
+
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/MmapNioFileWindowReader.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2008, 2009 Google Inc.
+ * Copyright (C) 2008, 2022 Shawn O. Pearce <spearce@spearce.org> and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.file;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel.MapMode;
+
+/**
+ * Map window of data to virtual memory using NIO and access it via ByteBuffer
+ */
+class MmapNioFileWindowReader implements FileWindowReader {
+	private final Pack pack;
+	private RandomAccessFile fd;
+	private long length;
+
+	MmapNioFileWindowReader(Pack pack) {
+		this.pack = pack;
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (fd != null) {
+			try {
+				fd.close();
+			} catch (IOException err) {
+				// Ignore a close event. We had it open only for reading.
+				// There should not be errors related to network buffers
+				// not flushed, etc.
+			}
+			fd = null;
+		}
+	}
+
+	@Override
+	public FileWindowReader open() throws IOException {
+		fd = new RandomAccessFile(pack.getPackFile(), "r"); //$NON-NLS-1$
+		length = fd.length();
+		return this;
+	}
+
+	@Override
+	public long length() throws IOException {
+		return length;
+	}
+
+	@Override
+	public ByteWindow read(long pos, int size) throws IOException {
+		if (length < pos + size) {
+			size = (int) (length - pos);
+		}
+
+		MappedByteBuffer map;
+		try {
+			map = fd.getChannel().map(MapMode.READ_ONLY, pos, size);
+		} catch (IOException ioe1) {
+			// The most likely reason this failed is the JVM has run out
+			// of virtual memory. We need to discard quickly, and try to
+			// force the GC to finalize and release any existing mappings.
+			//
+			System.gc();
+			System.runFinalization();
+			map = fd.getChannel().map(MapMode.READ_ONLY, pos, size);
+		}
+
+		if (map.hasArray()) {
+			return new ByteArrayWindow(pack, pos, map.array());
+		}
+		return new ByteBufferWindow(pack, pos, map);
+	}
+
+	@Override
+	public void readRaw(byte[] b, long off, int len) throws IOException {
+		fd.seek(off);
+		fd.readFully(b, 0, len);
+	}
+
+}
