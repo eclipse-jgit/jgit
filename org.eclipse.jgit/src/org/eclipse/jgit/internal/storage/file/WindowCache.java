bd4f446b71c1d479cf7b1081f5d5ba9b374d1ac7
Matthias Sohn <matthias.sohn@sap.com>
2022-05-13 23:39:39 +0200

    Extract interface for reading pack file pages from WindowCache
    WindowCache currently supports two ways to cache pages of git pack files:
    - read page and cache it in a byte array on the Java heap
    - mmap page into memory using NIOs FileChannel.map() method
    
    Extract an interface FileWindowReader and move the existing implementations
    into separate classes.
    
    This is a preparation to introduce mapping pack files using Java 17
    MemorySegment which is a part of project Panama's foreign memory API.
    
    Change-Id: Ic6110e4048273cc53f8b967b8a5ebf370bf1f452

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/WindowCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/WindowCache.java
@@ -423,8 +423,6 @@ public class WindowCache {
 
 	private final long maxBytes;
 
-	private final boolean mmap;
-
 	private final int windowSizeShift;
 
 	private final int windowSize;
@@ -463,7 +461,7 @@ public class WindowCache {
 
 		maxFiles = cfg.getPackedGitOpenFiles();
 		maxBytes = cfg.getPackedGitLimit();
-		mmap = cfg.isPackedGitMMAP();
+		FileWindowReaderFactory.setMmap(cfg.isPackedGitMMAP());
 		windowSizeShift = bits(cfg.getPackedGitWindowSize());
 		windowSize = 1 << windowSizeShift;
 		useStrongRefs = cfg.isPackedGitUseStrongRefs();
@@ -507,12 +505,11 @@ public class WindowCache {
 
 	private ByteWindow load(Pack pack, long offset) throws IOException {
 		long startTime = System.nanoTime();
-		if (pack.beginWindowCache())
+		if (pack.beginWindowCache()) {
 			statsRecorder.recordOpenFiles(1);
+		}
 		try {
-			if (mmap)
-				return pack.mmap(offset, windowSize);
-			ByteArrayWindow w = pack.read(offset, windowSize);
+			ByteWindow w = pack.read(offset, windowSize);
 			statsRecorder.recordLoadSuccess(System.nanoTime() - startTime);
 			return w;
 		} catch (IOException | RuntimeException | Error e) {
