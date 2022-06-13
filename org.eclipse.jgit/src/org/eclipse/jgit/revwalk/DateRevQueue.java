2296e84a427992cbf57c8ea3614a566560ae8f98
Luca Milanesio <luca.milanesio@gmail.com>
2022-06-13 23:09:55 +0100

    Introduce a PriorityQueue for sorting RevCommits by commit ts
    The DateRevQueue used a tailor-made algorithm to keep
    RevCommits sorted by reversed commit ts, which had a O(n*n/2)
    complexity and caused the explosion of the Git fetch times to
    tens of seconds.
    
    The standard Java PriorityQueue provides a O(n*log(n)) complexity
    and scales much better with the increase of the number of
    RevCommits.
    Introduce a new implementation of the DateRevQueue base on the
    java PriorityQueue.
    
    Benchmark results:
    ```
    Mode: avgt Units: us/op Cnt:10
    (numCommits)(usePriorityQueue)  Score    Error
    8500               true         0.154 ± 0.035
    8500               false        34.659 ± 26.249
    85000              true         0.265 ± 0.038
    85000              false        33.482 ± 24.839
    450000             true         0.404 ± 0.055
    450000             false        33.172 ± 24.175
    850000             true         0.367 ± 0.192
    850000             false        28.935 ± 23.175
    ```
    
    Memory allocation results:
    ```
    Number of commits loaded: 850 000
    Custom implementation: 378 245 120 Bytes
    Priority queue implementation: 340 495 616 Bytes
    ```
    
    Bug: 580137
    Change-Id: I8b33df6e9ee88933098ecc81ce32bdb189715041
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/DateRevQueue.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/DateRevQueue.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>,
- * Copyright (C) 2013, Gustaf Lundh <gustaf.lundh@sonymobile.com> and others
+ * Copyright (C) 2023, GerritForge Ltd
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
@@ -11,123 +10,83 @@
 
 package org.eclipse.jgit.revwalk;
 
-import java.io.IOException;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
- * A queue of commits sorted by commit time order.
+ * A queue of commits sorted by commit time order using a Java PriorityQueue.
+ * For the commits with the same commit time insertion order will be preserved.
  */
 public class DateRevQueue extends AbstractRevQueue {
-	private static final int REBUILD_INDEX_COUNT = 1000;
+	private PriorityQueue<RevCommitEntry> queue;
 
-	private Entry head;
+	private final AtomicInteger sequence = new AtomicInteger(1);
 
-	private Entry free;
-
-	private int inQueue;
-
-	private int sinceLastIndex;
-
-	private Entry[] index;
-
-	private int first;
-
-	private int last = -1;
-
-	/** Create an empty date queue. */
+	/**
+	 * Create an empty date queue.
+	 */
 	public DateRevQueue() {
-		super(false);
+		this(false);
 	}
 
-	DateRevQueue(boolean firstParent) {
+	/**
+	 * Create an empty date queue.
+	 *
+	 * @param firstParent
+	 *            treat first element as a parent
+	 *
+	 * @since 6.6
+	 */
+	public DateRevQueue(boolean firstParent) {
 		super(firstParent);
+		initPriorityQueue();
+	}
+
+	private void initPriorityQueue() {
+		sequence.set(1);
+		queue = new PriorityQueue<>(Comparator.comparingInt(
+						(RevCommitEntry ent) -> ent.getEntry().getCommitTime())
+				.reversed()
+				.thenComparingInt(RevCommitEntry::getInsertSequenceNumber));
 	}
 
 	DateRevQueue(Generator s) throws MissingObjectException,
 			IncorrectObjectTypeException, IOException {
-		super(s.firstParent);
+		this(s.firstParent);
 		for (;;) {
 			final RevCommit c = s.next();
-			if (c == null)
+			if (c == null) {
 				break;
+			}
 			add(c);
 		}
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void add(RevCommit c) {
-		sinceLastIndex++;
-		if (++inQueue > REBUILD_INDEX_COUNT
-				&& sinceLastIndex > REBUILD_INDEX_COUNT)
-			buildIndex();
-
-		Entry q = head;
-		final long when = c.commitTime;
-
-		if (first <= last && index[first].commit.commitTime > when) {
-			int low = first, high = last;
-			while (low <= high) {
-				int mid = (low + high) >>> 1;
-				int t = index[mid].commit.commitTime;
-				if (t < when)
-					high = mid - 1;
-				else if (t > when)
-					low = mid + 1;
-				else {
-					low = mid - 1;
-					break;
-				}
-			}
-			low = Math.min(low, high);
-			while (low > first && when == index[low].commit.commitTime)
-				--low;
-			q = index[low];
+		// PriorityQueue does not accept null values. To keep the same behaviour
+		// do the same check and throw the same exception before creating entry
+		if (c == null) {
+			throw new NullPointerException("Null revCommit");
 		}
-
-		final Entry n = newEntry(c);
-		if (q == null || (q == head && when > q.commit.commitTime)) {
-			n.next = q;
-			head = n;
-		} else {
-			Entry p = q.next;
-			while (p != null && p.commit.commitTime >= when) {
-				q = p;
-				p = q.next;
-			}
-			n.next = q.next;
-			q.next = n;
-		}
+		queue.add(new RevCommitEntry(sequence.getAndIncrement(), c));
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public RevCommit next() {
-		final Entry q = head;
-		if (q == null)
-			return null;
-
-		if (index != null && q == index[first])
-			index[first++] = null;
-		inQueue--;
-
-		head = q.next;
-		freeEntry(q);
-		return q.commit;
-	}
-
-	private void buildIndex() {
-		sinceLastIndex = 0;
-		first = 0;
-		index = new Entry[inQueue / 100 + 1];
-		int qi = 0, ii = 0;
-		for (Entry q = head; q != null; q = q.next) {
-			if (++qi % 100 == 0)
-				index[ii++] = q;
-		}
-		last = ii - 1;
+		RevCommitEntry entry = queue.poll();
+		return entry == null ? null : entry.getEntry();
 	}
 
 	/**
@@ -136,36 +95,29 @@ public class DateRevQueue extends AbstractRevQueue {
 	 * @return the next available commit; null if there are no commits left.
 	 */
 	public RevCommit peek() {
-		return head != null ? head.commit : null;
+		RevCommitEntry entry = queue.peek();
+		return entry == null ? null : entry.getEntry();
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void clear() {
-		head = null;
-		free = null;
-		index = null;
-		inQueue = 0;
-		sinceLastIndex = 0;
-		last = -1;
+		sequence.set(1);
+		queue.clear();
 	}
 
 	@Override
 	boolean everbodyHasFlag(int f) {
-		for (Entry q = head; q != null; q = q.next) {
-			if ((q.commit.flags & f) == 0)
-				return false;
-		}
-		return true;
+		return queue.stream().map(RevCommitEntry::getEntry)
+				.noneMatch(c -> (c.flags & f) == 0);
 	}
 
 	@Override
 	boolean anybodyHasFlag(int f) {
-		for (Entry q = head; q != null; q = q.next) {
-			if ((q.commit.flags & f) != 0)
-				return true;
-		}
-		return false;
+		return queue.stream().map(RevCommitEntry::getEntry)
+				.anyMatch(c -> (c.flags & f) != 0);
 	}
 
 	@Override
@@ -173,33 +125,34 @@ public class DateRevQueue extends AbstractRevQueue {
 		return outputType | SORT_COMMIT_TIME_DESC;
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String toString() {
 		final StringBuilder s = new StringBuilder();
-		for (Entry q = head; q != null; q = q.next)
-			describe(s, q.commit);
+		for (RevCommitEntry e : queue) {
+			describe(s, e.getEntry());
+		}
 		return s.toString();
 	}
 
-	private Entry newEntry(RevCommit c) {
-		Entry r = free;
-		if (r == null)
-			r = new Entry();
-		else
-			free = r.next;
-		r.commit = c;
-		return r;
-	}
+	private static class RevCommitEntry {
+		private final int insertSequenceNumber;
 
-	private void freeEntry(Entry e) {
-		e.next = free;
-		free = e;
-	}
+		private final RevCommit entry;
 
-	static class Entry {
-		Entry next;
+		public RevCommitEntry(int insertSequenceNumber, RevCommit entry) {
+			this.insertSequenceNumber = insertSequenceNumber;
+			this.entry = entry;
+		}
 
-		RevCommit commit;
+		public int getInsertSequenceNumber() {
+			return insertSequenceNumber;
+		}
+
+		public RevCommit getEntry() {
+			return entry;
+		}
 	}
 }
