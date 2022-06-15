cd4097e85cdfe53592f322c4e7eb9d9e48bffe1d
Ronald Bhuleskar <funronald@google.com>
2022-06-15 14:37:21 -0700

    Add the ability to override parents on RevCommit.
    This makes RevCommit extensible to allow having different structure of
    child-parent relationship. This change is a pre-requsite for having a
    FilteredRevCommit that overrides parents from the RevCommit. That then
    provides a cheaper way to walk over a subset of RevCommits instead of
    an expensive way that applies filters while walking over selected
    commits. Useful with Blame which works on a single file and that can be
    made performant, if we know all the commits needed by the Blame
    algorithm. So Blame algorithm can avoid walking over finding what
    commits to blame on.
    
    This change makes parents field on RevCommit private and exposes it
    thrrough overrideable methods such as getParents, getParent at index,
    getParentCount and setParents. All other files other than RevCommit are
    updating the usages of accessing RevCommits parents.
    
    Change-Id: I2d13b001c599cc4ebc92d1ab6e07b07acb3b7fe5

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevCommitWithOverriddenParentTest.java
@@ -0,0 +1,160 @@
+package org.eclipse.jgit.revwalk;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.util.Arrays;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RevCommitWithOverriddenParentTest {
+	private TestRepository<InMemoryRepository> tr;
+
+	private RevWalk rw;
+
+	@Before
+	public void setUp() throws Exception {
+		tr = new TestRepository<>(
+				new InMemoryRepository(new DfsRepositoryDescription("test")));
+		rw = tr.getRevWalk();
+	}
+
+	@Test
+	public void testParseBody() throws Exception {
+		RevCommit a = tr.commit().add("a", "foo").create();
+		RevCommit b = tr.commit().parent(a).add("b", "bar").create();
+		RevCommit c = tr.commit().parent(b).message("commit3").add("a", "foo'")
+				.create();
+
+		RevCommit cBar = new RevCommit(c.getId()) {
+			@Override
+			public int getParentCount() {
+				return 1;
+			}
+
+			@Override
+			public RevCommit getParent(int nth) {
+				return a;
+			}
+
+			@Override
+			public RevCommit[] getParents() {
+				return new RevCommit[] { a };
+			}
+		};
+
+		rw.parseBody(cBar);
+		assertEquals(a, Arrays.stream(cBar.getParents()).findFirst().get());
+		assertEquals("commit3", cBar.getFullMessage());
+		assertEquals("foo'", blobAsString(cBar, "a"));
+	}
+
+	@Test
+	public void testParseCommit() throws Exception {
+		RevCommit a = tr.commit().add("a", "foo").create();
+		RevCommit b = tr.commit().parent(a).add("b", "bar").create();
+		RevCommit c = tr.commit().parent(b).message("commit3").add("a", "foo'")
+				.create();
+
+		RevCommit cBar = new RevCommit(c.getId()) {
+			@Override
+			public int getParentCount() {
+				return 1;
+			}
+
+			@Override
+			public RevCommit getParent(int nth) {
+				return a;
+			}
+
+			@Override
+			public RevCommit[] getParents() {
+				return new RevCommit[] { a };
+			}
+		};
+
+		rw.updateCommit(cBar);
+		RevCommit parsed = rw.parseCommit(cBar.getId());
+
+		assertEquals(c.getId(), parsed.getId());
+		assertArrayEquals(cBar.getParents(), parsed.getParents());
+	}
+
+	@Test
+	public void testFilter() throws Exception {
+		RevCommit a = tr.commit().add("a", "foo").create();
+		RevCommit b = tr.commit().parent(a).add("b", "bar").create();
+		RevCommit c = tr.commit().parent(b).message("commit3").add("a", "foo'")
+				.create();
+
+		RevCommit cBar = new RevCommit(c.getId()) {
+			@Override
+			public int getParentCount() {
+				return 1;
+			}
+
+			@Override
+			public RevCommit getParent(int nth) {
+				return a;
+			}
+
+			@Override
+			public RevCommit[] getParents() {
+				return new RevCommit[] { a };
+			}
+		};
+
+		rw.setRevFilter(RevFilter.ALL);
+		rw.markStart(cBar);
+		assertSame(cBar, rw.next());
+		assertSame(a, rw.next());
+		assertNull(rw.next());
+	}
+
+	@Test
+	public void testFlag() throws Exception {
+		RevCommit root = tr.commit().add("todelete", "to be deleted").create();
+		RevCommit orig = tr.commit().parent(root).rm("todelete")
+				.add("foo", "foo contents").add("bar", "bar contents")
+				.add("dir/baz", "baz contents").create();
+
+		RevCommit commitOrigBar = new RevCommit(orig.getId()) {
+			@Override
+			public int getParentCount() {
+				return 1;
+			}
+
+			@Override
+			public RevCommit getParent(int nth) {
+				return root;
+			}
+
+			@Override
+			public RevCommit[] getParents() {
+				return new RevCommit[] { root };
+			}
+		};
+
+		assertEquals(RevObject.PARSED, orig.flags);
+		assertEquals(0, commitOrigBar.flags);
+		commitOrigBar.parseBody(rw);
+		assertEquals(RevObject.PARSED, commitOrigBar.flags);
+	}
+
+	private String blobAsString(AnyObjectId treeish, String path)
+			throws Exception {
+		RevObject obj = tr.get(rw.parseTree(treeish), path);
+		assertSame(RevBlob.class, obj.getClass());
+		ObjectLoader loader = rw.getObjectReader().open(obj);
+		return new String(loader.getCachedBytes(), UTF_8);
+	}
+}
