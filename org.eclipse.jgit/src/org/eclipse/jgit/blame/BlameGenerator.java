3d9eecd5a9fcd63819718643d7c88adf05a4e544
Ronald Bhuleskar <funronald@google.com>
2022-06-22 15:01:35 -0700

    Option to pass start RevCommit to be blamed on to the BlameGenerator.
    This can allow passing a FilteredRevCommit which is the filtered list of
    commit graph making it easier for Blame to work on. This can
    significantly improve blame performance since blame can skip expensive
    RevWalk.
    
    Change-Id: I2df9843dde0f589f5fea6cedaaff52e313eea6de

--- org.eclipse.jgit/src/org/eclipse/jgit/blame/BlameGenerator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/blame/BlameGenerator.java
@@ -402,6 +402,32 @@ public class BlameGenerator implements AutoCloseable {
 	 * revision (if the index is interesting), and finally the working tree copy
 	 * (if the working tree is interesting).
 	 *
+	 * @param blameCommit ordered commits to use instead of RevWalk.
+	 * @return {@code this}
+	 * @throws java.io.IOException
+	 *             the repository cannot be read.
+	 */
+	public BlameGenerator push(RevCommit blameCommit)
+			throws IOException {
+    if (!find(blameCommit, resultPath)) return this;
+
+    Candidate c = new Candidate(getRepository(), blameCommit, resultPath);
+    c.sourceBlob = idBuf.toObjectId();
+    c.loadText(reader);
+    c.regionList = new Region(0, 0, c.sourceText.size());
+    remaining = c.sourceText.size();
+    push(c);
+    return this;
+  }
+
+	/**
+	 * Push a candidate object onto the generator's traversal stack.
+	 * <p>
+	 * Candidates should be pushed in history order from oldest-to-newest.
+	 * Applications should push the starting commit first, then the index
+	 * revision (if the index is interesting), and finally the working tree copy
+	 * (if the working tree is interesting).
+	 *
 	 * @param description
 	 *            description of the blob revision, such as "Working Tree".
 	 * @param id
