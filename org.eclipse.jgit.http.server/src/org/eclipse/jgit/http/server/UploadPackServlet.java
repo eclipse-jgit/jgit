d58977784979cc370ac9de53b613226e8c375700
Matthias Sohn <matthias.sohn@sap.com>
2022-06-29 14:58:17 +0200

    UploadPack: don't prematurely terminate timer in case of error
    In uploadWithExceptionPropagation don't prematurely terminate timer in
    case of error to enable reporting it to the client. Expose a close
    method so that callers can terminate it at the appropriate time.
    
    If the timer is already terminated when trying to report it to the
    client this failed with the error java.lang.IllegalStateException:
    "Timer already terminated".
    
    Bug: 579670
    Change-Id: I95827442ccb0f9b1ede83630cf7c51cf619c399a

--- org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/UploadPackServlet.java
+++ org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/UploadPackServlet.java
@@ -195,6 +195,8 @@ class UploadPackServlet extends HttpServlet {
 				log(up.getRepository(), e.getCause());
 				consumeRequestBody(req);
 				out.close();
+			} finally {
+				up.close();
 			}
 		};
 
