c8ae4bd8ab329944a21ef7cbab44b1dc40ba7cbf
Nitzan Gur-Furman <nitzan@google.com>
2022-06-30 16:18:10 +0300

    Create util class for handling both local and remote repositories IO, to make future support in remote repositories easier.
    This class currently extracts some logic from ResolveMerger. Logic related to StreamSupplier was copied from ApplyCommand, which will be integrated in a following change.
    
    Change-Id: I8dc5a582433fc9891038c628385d3970b5a8984b

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/InCoreSupport.java
@@ -0,0 +1,47 @@
+package org.eclipse.jgit.util;
+
+import java.io.IOException;
+import java.util.Optional;
+import org.eclipse.jgit.attributes.Attributes;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+/**
+ * Classes with this interface support both local and remote depositories (unlike most of the
+ * project, which only supports local repositories).
+ */
+public interface InCoreSupport {
+
+  /**
+   * Initiates {@link RepoIOHandler} for the given local repository.
+   * <p>
+   * Use the result object to handle any IO related actions.
+   *
+   * @param local    repository
+   * @param dirCache if set, use the provided dir cache. Otherwise, use the default repository one
+   * @return an IO handler.
+   * @throws IOException might occur when reading the attributes.
+   */
+  public static RepoIOHandler initRepoIOHandlerForLocal(Repository local,
+      DirCache dirCache) {
+    return new RepoIOHandler(local, dirCache);
+  }
+
+  /**
+   * Initiates {@link RepoIOHandler} for the given remote repository.
+   * <p>
+   * Use the result object to handle any IO related actions.
+   *
+   * @param remote   repository
+   * @param dirCache if set, use the provided dir cache. Otherwise, creates a new one
+   * @param oi       to use for writing the modified objects with.
+   * @return an IO handler.
+   * @throws IOException might occur when reading the attributes.
+   */
+  public static RepoIOHandler initRepoIOHandlerForRemote(Repository remote,
+      DirCache dirCache, ObjectInserter oi) {
+    return new RepoIOHandler(remote, dirCache, oi);
+  }
+}
