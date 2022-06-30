8aa98f09524bf95959c77f1b764bec682d690511
Nitzan Gur-Furman <nitzan@google.com>
2022-06-30 16:18:10 +0300

    WIP PatchApplier
    Change-Id: Id60a88232f05d0367787d038d2518c670cdb543f

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/InCoreSupport.java
@@ -0,0 +1,21 @@
+package org.eclipse.jgit.util;
+
+import java.io.IOException;
+import org.eclipse.jgit.attributes.Attributes;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+public interface InCoreSupport {
+
+  static org.eclipse.jgit.util.InCoreHandler initInCoreHandler(Repository repo, ObjectInserter oi, ObjectReader reader,
+      boolean inCore, boolean shouldUpdateIndex, Attributes attributes) throws IOException {
+    String smudgeCommand = null;
+    if(!inCore) {
+      TreeWalk walk = new TreeWalk(repo);
+      smudgeCommand = walk.getSmudgeCommand(attributes);
+    }
+    return new org.eclipse.jgit.util.InCoreHandler(repo, oi, reader, inCore, shouldUpdateIndex, smudgeCommand);
+  }
+}
