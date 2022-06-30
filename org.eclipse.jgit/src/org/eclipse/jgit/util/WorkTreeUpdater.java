662cce3d63273e647f26bea1ba32ab398df42d8a
Nitzan Gur-Furman <nitzan@google.com>
2022-06-30 16:18:10 +0300

    Move ApplyCommand logic to a utility class, and add inCore support to it.
    Change-Id: Id60a88232f05d0367787d038d2518c670cdb543f

--- org.eclipse.jgit/src/org/eclipse/jgit/util/WorkTreeUpdater.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/WorkTreeUpdater.java
@@ -18,8 +18,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.text.MessageFormat;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -42,6 +44,7 @@ import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.CoreConfig.AutoCRLF;
 import org.eclipse.jgit.lib.CoreConfig.EolStreamType;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
@@ -191,6 +194,7 @@ public class WorkTreeUpdater implements Closeable {
 
 		this.inCore = true;
 		this.reader = oi.newReader();
+		this.workingTreeOptions = repo.getConfig().get(WorkingTreeOptions.KEY);
 		if (repo != null) {
 			this.inCoreFileSizeLimit = setInCoreFileSizeLimit(repo.getConfig());
 		}
@@ -416,6 +420,55 @@ public class WorkTreeUpdater implements Closeable {
 	}
 
 	/**
+	 * Renames the given file
+	 *
+	 * @param origin     to be renamed
+	 * @param originPath to rename from
+	 * @param dest       file
+	 * @throws IOException if the file cannot be renamed
+	 */
+	public void renameFile(File origin, String originPath, File dest) throws IOException {
+		markAsModified(originPath);
+
+		if (inCore) {
+			// insertToIndex() is expected to be called for this file next. Index updating is done there.
+			return;
+		}
+		if (origin == null || dest == null) {
+			throw new IOException(JGitText.get().renameFileFailedNullFiles);
+		}
+		try {
+			FileUtils.mkdirs(dest.getParentFile(), true);
+			FileUtils.rename(origin, dest, StandardCopyOption.ATOMIC_MOVE);
+		} catch (IOException e) {
+			throw new IOException(
+					MessageFormat.format(JGitText.get().renameFileFailed, origin, dest), e);
+		}
+	}
+
+	/**
+	 * Copies the given file
+	 *
+	 * @param origin   to be copied
+	 * @param dest     file
+	 * @param destPath to copy to
+	 * @throws IOException if the file cannot be copied
+	 */
+	public void copyFile(File origin, File dest, String destPath) throws IOException {
+		markAsModified(destPath);
+
+		if (inCore) {
+			// insertToIndex() is expected to be called for this file next. Index updating is done there.
+			return;
+		}
+		if (origin == null || dest == null) {
+			throw new IOException(JGitText.get().copyFileFailedNullFiles);
+		}
+		FileUtils.mkdirs(dest.getParentFile(), true);
+		Files.copy(origin.toPath(), dest.toPath());
+	}
+
+	/**
 	 * Remembers the {@link CheckoutMetadata} for the given path; it may be needed in {@link
 	 * #checkout()} or in {@link #revertModifiedFiles()}.
 	 *
@@ -431,7 +484,9 @@ public class WorkTreeUpdater implements Closeable {
 		if (inCore || map == null) {
 			return;
 		}
-		map.put(path, new CheckoutMetadata(streamType, smudgeCommand));
+		if (!map.containsKey(path)) {
+			map.put(path, new CheckoutMetadata(streamType, smudgeCommand));
+		}
 	}
 
 	/**
@@ -461,17 +516,19 @@ public class WorkTreeUpdater implements Closeable {
 					result.failedToDelete.add(path);
 				}
 			}
-			result.modifiedFiles.add(path);
+			markAsModified(path);
 		}
 	}
 
 	/**
 	 * Marks the given path as modified in the operation.
 	 *
-	 * @param path to mark as modified
+	 * @param path          to mark as modified
 	 */
 	public void markAsModified(String path) {
-		result.modifiedFiles.add(path);
+		if (!result.modifiedFiles.contains(path)) {
+			result.modifiedFiles.add(path);
+		}
 	}
 
 	/**
@@ -494,7 +551,7 @@ public class WorkTreeUpdater implements Closeable {
 			} else {
 				DirCacheCheckout.checkoutEntry(
 						repo, dirCacheEntry, reader, false, checkoutMetadata.get(entry.getKey()));
-				result.modifiedFiles.add(entry.getKey());
+				markAsModified(entry.getKey());
 			}
 		}
 	}
@@ -552,6 +609,7 @@ public class WorkTreeUpdater implements Closeable {
 		if (inCore) {
 			return;
 		}
+		markAsModified(path);
 		CheckoutMetadata checkoutMetadata = new CheckoutMetadata(streamType, smudgeCommand);
 		if (safeWrite) {
 			try (org.eclipse.jgit.util.TemporaryBuffer buffer =
@@ -575,6 +633,8 @@ public class WorkTreeUpdater implements Closeable {
 	 *
 	 * @param inputStream        with the content to be updated
 	 * @param path               of the file to be updated
+	 * @param streamType         for writing the content
+	 * @param smudgeCommand      for formatting the content
 	 * @param fileMode           of the modified file
 	 * @param entryStage         of the new entry
 	 * @param lastModified       instant of the modified file
@@ -586,14 +646,16 @@ public class WorkTreeUpdater implements Closeable {
 	public DirCacheEntry insertToIndex(
 			InputStream inputStream,
 			byte[] path,
+			EolStreamType streamType,
+			String smudgeCommand,
 			FileMode fileMode,
 			int entryStage,
 			Instant lastModified,
 			int len,
 			Attribute lfsAttribute) throws IOException {
 		StreamLoader contentLoader = createStreamLoader(() -> inputStream, len);
-		return insertToIndex(contentLoader, path, fileMode, entryStage, lastModified, len,
-				lfsAttribute);
+		return insertToIndex(contentLoader, path, streamType, smudgeCommand, fileMode, entryStage,
+				lastModified, len, lfsAttribute);
 	}
 
 	/**
@@ -601,6 +663,8 @@ public class WorkTreeUpdater implements Closeable {
 	 *
 	 * @param resultStreamLoader with the content to be updated
 	 * @param path               of the file to be updated
+	 * @param streamType         for writing the content
+	 * @param smudgeCommand      for formatting the content
 	 * @param fileMode           of the modified file
 	 * @param entryStage         of the new entry
 	 * @param lastModified       instant of the modified file
@@ -612,12 +676,15 @@ public class WorkTreeUpdater implements Closeable {
 	public DirCacheEntry insertToIndex(
 			StreamLoader resultStreamLoader,
 			byte[] path,
+			EolStreamType streamType,
+			String smudgeCommand,
 			FileMode fileMode,
 			int entryStage,
 			Instant lastModified,
 			int len,
 			Attribute lfsAttribute) throws IOException {
-		return addExistingToIndex(insertResult(resultStreamLoader, lfsAttribute),
+		return addExistingToIndex(insertResult(resultStreamLoader, lfsAttribute, streamType,
+						smudgeCommand),
 				path, fileMode, entryStage, lastModified, len);
 	}
 
@@ -640,7 +707,7 @@ public class WorkTreeUpdater implements Closeable {
 			Instant lastModified,
 			int len) {
 		DirCacheEntry dce = new DirCacheEntry(path, entryStage);
-		dce.setFileMode(fileMode);
+		dce.setFileMode(fileMode == FileMode.MISSING ? FileMode.REGULAR_FILE : fileMode);
 		if (lastModified != null) {
 			dce.setLastModified(lastModified);
 		}
@@ -648,19 +715,30 @@ public class WorkTreeUpdater implements Closeable {
 
 		dce.setObjectId(objectId);
 		builder.add(dce);
+
+		markAsModified(new String(path, StandardCharsets.UTF_8));
 		return dce;
 	}
 
-	private ObjectId insertResult(StreamLoader resultStreamLoader, Attribute lfsAttribute)
+	private ObjectId insertResult(StreamLoader resultStreamLoader, Attribute lfsAttribute,
+			EolStreamType streamType, String smudgeCommand)
 			throws IOException {
-		try (LfsInputStream is =
-				org.eclipse.jgit.util.LfsFactory.getInstance()
-						.applyCleanFilter(
-								repo,
-								resultStreamLoader.data.load(),
-								resultStreamLoader.size,
-								lfsAttribute)) {
-			return inserter.insert(OBJ_BLOB, is.getLength(), is);
+		CheckoutMetadata checkoutMetadata = new CheckoutMetadata(
+				workingTreeOptions.getAutoCRLF() == AutoCRLF.TRUE ? EolStreamType.AUTO_CRLF : streamType,
+				smudgeCommand);
+		try (org.eclipse.jgit.util.TemporaryBuffer buffer =
+				new org.eclipse.jgit.util.TemporaryBuffer.LocalFile(null)) {
+			DirCacheCheckout.getContent(
+					repo, null, checkoutMetadata, resultStreamLoader, workingTreeOptions, buffer);
+			try (LfsInputStream is =
+					org.eclipse.jgit.util.LfsFactory.getInstance()
+							.applyCleanFilter(
+									repo,
+									buffer.openInputStream(),
+									buffer.length(),
+									lfsAttribute)) {
+				return inserter.insert(OBJ_BLOB, is.getLength(), is);
+			}
 		}
 	}
 
@@ -670,7 +748,7 @@ public class WorkTreeUpdater implements Closeable {
 	 * @return non-null repository instance
 	 * @throws java.lang.NullPointerException if the handler was constructed without a repository.
 	 */
-	private Repository nonNullRepo() throws NullPointerException {
+	public Repository nonNullRepo() throws NullPointerException {
 		if (repo == null) {
 			throw new NullPointerException(JGitText.get().repositoryIsRequired);
 		}
