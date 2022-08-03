6be97cc8b8a02261ce447ea3b1d034689313a4fd
Ronald Bhuleskar <funronald@google.com>
2022-08-03 16:41:43 -0700

    Adds FilteredRevCommit that can overwrites its parents in the DAG.
    This is a rollforward of https://git.eclipse.org/r/c/jgit/jgit/+/194354 with a fix.
    Change-Id: I1ea63a3b56074099688fc45d6a22943a8ae3c2ae

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/FilteredRevCommit.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2022, Google LLC.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @since 6.3
+ */
+package org.eclipse.jgit.revwalk;
+
+import org.eclipse.jgit.lib.ObjectId;
+
+/** A filtered commit reference that overrides its parent in the DAG. */
+public class FilteredRevCommit extends RevCommit {
+	private RevCommit[] overriddenParents;
+
+	/**
+	 * Create a new commit reference for the given id.
+	 *
+	 * @param id
+	 *            object name for the commit.
+	 */
+	public FilteredRevCommit(ObjectId id) {
+		this(id, NO_PARENTS);
+	}
+
+	/**
+	 * Create a new commit reference wrapping an underlying commit reference.
+	 *
+	 * @param commit
+	 *            commit that is being wrapped
+	 */
+	public FilteredRevCommit(RevCommit commit) {
+		this(commit, NO_PARENTS);
+	}
+
+	/**
+	 * Create a new commit reference wrapping an underlying commit reference.
+	 *
+	 * @param commit
+	 *            commit that is being wrapped
+	 * @param parents
+	 *            overridden parents for the commit
+	 */
+	public FilteredRevCommit(RevCommit commit, RevCommit... parents) {
+		this(commit.getId(), parents);
+		this.flags = commit.flags;
+		this.flags = this.flags &= ~RevObject.PARSED;
+		this.commitTime = commit.commitTime;
+	}
+
+	/**
+	 * Create a new commit reference wrapping an underlying commit reference.
+	 *
+	 * @param id
+	 *            object name for the commit.
+	 * @param parents
+	 *            overridden parents for the commit
+	 */
+	public FilteredRevCommit(ObjectId id, RevCommit... parents) {
+		super(id);
+		this.overriddenParents = parents;
+	}
+
+	/**
+	 * Update parents on the commit
+	 *
+	 * @param overriddenParents
+	 *            parents to be overwritten
+	 */
+	public void setParents(RevCommit... overriddenParents) {
+		this.overriddenParents = overriddenParents;
+	}
+
+	/**
+	 * Get the number of parent commits listed in this commit.
+	 *
+	 * @return number of parents; always a positive value but can be 0 if it has
+	 *         no parents.
+	 */
+	@Override
+	public int getParentCount() {
+		return overriddenParents.length;
+	}
+
+	/**
+	 * Get the nth parent from this commit's parent list.
+	 *
+	 * @param nth
+	 *            parent index to obtain. Must be in the range 0 through
+	 *            {@link #getParentCount()}-1.
+	 * @return the specified parent.
+	 * @throws java.lang.ArrayIndexOutOfBoundsException
+	 *             an invalid parent index was specified.
+	 */
+	@Override
+	public RevCommit getParent(int nth) {
+		return overriddenParents[nth];
+	}
+
+	/**
+	 * Obtain an array of all parents (<b>NOTE - THIS IS NOT A COPY</b>).
+	 *
+	 * <p>
+	 * This method is exposed only to provide very fast, efficient access to
+	 * this commit's parent list. Applications relying on this list should be
+	 * very careful to ensure they do not modify its contents during their use
+	 * of it.
+	 *
+	 * @return the array of parents.
+	 */
+	@Override
+	public RevCommit[] getParents() {
+		return overriddenParents;
+	}
+}
