3adfb6b3848fd520b002925dd47541083acec65b
Nitzan Gur-Furman <nitzan@google.com>
2022-08-08 15:45:59 +0200

    Reapply "Create util class for work tree updating in both filesystem and index."
    This reverts commit 5709317f71ccaf26eceaa896150f203879b634b8.
    
    Add a bugfix with test-case for deletions in ResolveMergers
    instantiated with just an ObjectInserter as argument.
    
    Create util class for work tree updating in both filesystem and index.
    
    This class intends to make future support in index updating easier.
    
    This class currently extracts some logic from ResolveMerger. Logic
    related to StreamSupplier was copied from ApplyCommand, which will be
    integrated in a following change.
    
    Co-authored-by: Nitzan Gur-Furman <nitzan@google.com>
    Co-authored-by: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: Ideaefd51789a382a8b499d1ca7ae0146d032f48b

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
@@ -195,9 +195,6 @@ public class RecursiveMerger extends ResolveMerger {
 			inCore = oldIncore;
 			dircache = oldDircache;
 			workingTreeIterator = oldWTreeIt;
-			toBeCheckedOut.clear();
-			toBeDeleted.clear();
-			modifiedFiles.clear();
 			unmergedPaths.clear();
 			mergeResults.clear();
 			failingPaths.clear();
