62fec834e4ac6c606ae5487ac98185581521c0d9
Ronald Bhuleskar <funronald@google.com>
2022-09-22 17:49:01 -0700

    Rollforward: Adds FilteredRevCommit that can overwrites its parents in the DAG.
    This reverts commit 7c4a5421ccd16b84b1ea4f3c1e566bfdf9c0e30d.
    
    Change-Id: Iee1b68f28e25667e4da8b9183e143e1d90758142

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/FilteredRevCommit.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2022, Google LLC.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.revwalk;
+
+import org.eclipse.jgit.lib.AnyObjectId;
+
+/**
+ * A filtered commit reference that overrides its parent in the DAG.
+ *
+ * @since 6.5
+ */
+public class FilteredRevCommit extends RevCommit {
+	private RevCommit[] overriddenParents;
+
+	/**
+	 * Create a new commit reference wrapping an underlying commit reference.
+	 *
+	 * @param commit
+	 *            commit that is being wrapped
+	 */
+	public FilteredRevCommit(AnyObjectId commit) {
+		this(commit, NO_PARENTS);
+	}
+
+	/**
+	 * Create a new commit reference wrapping an underlying commit reference.
+	 *
+	 * @param commit
+	 *            commit that is being wrapped
+	 * @param parents
+	 *            overridden parents for the commit
+	 */
+	public FilteredRevCommit(AnyObjectId commit, RevCommit... parents) {
+		super(commit);
+		this.overriddenParents = parents;
+		this.parents = NO_PARENTS;
+	}
+
+	/**
+	 * Update parents on the commit
+	 *
+	 * @param overriddenParents
+	 *            parents to be overwritten
+	 */
+	public void setParents(RevCommit... overriddenParents) {
+		this.overriddenParents = overriddenParents;
+	}
+
+	/**
+	 * Get the number of parent commits listed in this commit.
+	 *
+	 * @return number of parents; always a positive value but can be 0 if it has
+	 *         no parents.
+	 */
+	@Override
+	public int getParentCount() {
+		return overriddenParents.length;
+	}
+
+	/**
+	 * Get the nth parent from this commit's parent list.
+	 *
+	 * @param nth
+	 *            parent index to obtain. Must be in the range 0 through
+	 *            {@link #getParentCount()}-1.
+	 * @return the specified parent.
+	 * @throws java.lang.ArrayIndexOutOfBoundsException
+	 *             an invalid parent index was specified.
+	 */
+	@Override
+	public RevCommit getParent(int nth) {
+		return overriddenParents[nth];
+	}
+
+	/**
+	 * Obtain an array of all parents (<b>NOTE - THIS IS NOT A COPY</b>).
+	 *
+	 * <p>
+	 * This method is exposed only to provide very fast, efficient access to
+	 * this commit's parent list. Applications relying on this list should be
+	 * very careful to ensure they do not modify its contents during their use
+	 * of it.
+	 *
+	 * @return the array of parents.
+	 */
+	@Override
+	public RevCommit[] getParents() {
+		return overriddenParents;
+	}
+}
--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RewriteGenerator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RewriteGenerator.java
@@ -11,6 +11,8 @@
 package org.eclipse.jgit.revwalk;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
@@ -38,10 +40,13 @@ class RewriteGenerator extends Generator {
 
 	private final FIFORevQueue pending;
 
+	private final Map<RevCommit, FilteredRevCommit> transformedCommits;
+
 	RewriteGenerator(Generator s) {
 		super(s.firstParent);
 		source = s;
 		pending = new FIFORevQueue(s.firstParent);
+		transformedCommits = new HashMap<>();
 	}
 
 	@Override
@@ -58,10 +63,10 @@ class RewriteGenerator extends Generator {
 	@Override
 	RevCommit next() throws MissingObjectException,
 			IncorrectObjectTypeException, IOException {
-		RevCommit c = pending.next();
+		FilteredRevCommit c = (FilteredRevCommit) pending.next();
 
 		if (c == null) {
-			c = source.next();
+			c = transform(source.next());
 			if (c == null) {
 				// We are done: Both the source generator and our internal list
 				// are completely exhausted.
@@ -78,11 +83,14 @@ class RewriteGenerator extends Generator {
 			final RevCommit oldp = pList[i];
 			final RevCommit newp = rewrite(oldp);
 			if (firstParent) {
+				c = transform(c);
 				if (newp == null) {
-					c.parents = RevCommit.NO_PARENTS;
+					c.setParents(RevCommit.NO_PARENTS);
 				} else {
-					c.parents = new RevCommit[] { newp };
+					c.setParents(newp);
 				}
+				// produced all commits
+				transformedCommits.clear();
 				return c;
 			}
 			if (oldp != newp) {
@@ -91,7 +99,8 @@ class RewriteGenerator extends Generator {
 			}
 		}
 		if (rewrote) {
-			c.parents = cleanup(pList);
+			c = transform(c);
+			c.setParents(cleanup(pList));
 		}
 		return c;
 	}
@@ -111,7 +120,7 @@ class RewriteGenerator extends Generator {
 		for (RevCommit parent : c.getParents()) {
 			while ((parent.flags & RevWalk.TREE_REV_FILTER_APPLIED) == 0) {
 
-				RevCommit n = source.next();
+				FilteredRevCommit n = transform(source.next());
 
 				if (n != null) {
 					pending.add(n);
@@ -129,6 +138,9 @@ class RewriteGenerator extends Generator {
 	private RevCommit rewrite(RevCommit p) throws MissingObjectException,
 			IncorrectObjectTypeException, IOException {
 		for (;;) {
+			if (transformedCommits.containsKey(p)) {
+				p = transformedCommits.get(p);
+			}
 
 			if (p.getParentCount() > 1) {
 				// This parent is a merge, so keep it.
@@ -159,8 +171,23 @@ class RewriteGenerator extends Generator {
 
 			applyFilterToParents(p.getParent(0));
 			p = p.getParent(0);
-
 		}
+	}
+
+	private FilteredRevCommit transform(RevCommit c) {
+		if (c == null) {
+			return null;
+		}
+
+		if (c instanceof FilteredRevCommit) {
+			return (FilteredRevCommit) c;
+		}
+
+		if (!transformedCommits.containsKey(c)) {
+			transformedCommits.put(c, new FilteredRevCommit(c, c.getParents()));
+		}
+
+		return transformedCommits.get(c);
 	}
 
 	private RevCommit[] cleanup(RevCommit[] oldList) {
