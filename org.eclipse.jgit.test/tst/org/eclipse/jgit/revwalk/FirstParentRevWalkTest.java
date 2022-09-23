310412a2679d44f82a84b908f986109e16af32c3
Ronald Bhuleskar <funronald@google.com>
2022-09-22 17:49:01 -0700

    Rollforward: Adds FilteredRevCommit that can overwrites its parents in the DAG.
    FilteredRevCommit is a subclass of RevCommit that has an ability to
    override its parents. This is a pre-work for blame improvement as it
    avoids walking the commit graph if we already have commits to be blamed.
    
    The change was introduced in I1ea63a3b56074099688fc45d6a22943a8ae3c2ae
    but was rolled back in Ia6b651dd11b0a4b02d5e52247eb4bf13adf94e27 due to
    egit test failures. This change is a rollforward with the fix and more
    cleanup and addressed outstanding comments.
    
    Change-Id: Iee1b68f28e25667e4da8b9183e143e1d90758142

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/FirstParentRevWalkTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/FirstParentRevWalkTest.java
@@ -10,6 +10,8 @@
 
 package org.eclipse.jgit.revwalk;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -423,9 +425,41 @@ public class FirstParentRevWalkTest extends RevWalkTestCase {
 		rw.sort(RevSort.TOPO, true);
 		rw.setTreeFilter(PathFilterGroup.createFromStrings("0"));
 		markStart(d);
-		assertCommit(d, rw.next());
-		assertCommit(c, rw.next());
-		assertCommit(b, rw.next());
+
+		assertEquals(d, rw.next());
+		assertEquals(c, rw.next());
+		assertEquals(b, rw.next());
+		assertNull(rw.next());
+	}
+
+	@Test
+	public void testWithTopoSortAndTreeFilter_shouldUseFilteredRevCommits()
+			throws Exception {
+		RevCommit a = commit();
+		RevCommit b = commit(tree(file("0", blob("b"))), a);
+		RevCommit c = commit(tree(file("0", blob("c"))), b, a);
+		RevCommit d = commit(tree(file("0", blob("d"))), c);
+
+		rw.reset();
+		rw.setFirstParent(true);
+		rw.sort(RevSort.TOPO, true);
+		rw.setTreeFilter(PathFilterGroup.createFromStrings("0"));
+		markStart(d);
+
+		RevCommit x = rw.next();
+		assertThat(x, instanceOf(FilteredRevCommit.class));
+		assertEquals(1, x.getParentCount());
+		assertEquals(c, x.getParent(0));
+
+		RevCommit y = rw.next();
+		assertThat(y, instanceOf(FilteredRevCommit.class));
+		assertEquals(1, y.getParentCount());
+		assertEquals(b, y.getParent(0));
+
+		RevCommit z = rw.next();
+		assertThat(z, instanceOf(FilteredRevCommit.class));
+		assertEquals(0, z.getParentCount());
+
 		assertNull(rw.next());
 	}
 
@@ -441,8 +475,8 @@ public class FirstParentRevWalkTest extends RevWalkTestCase {
 		rw.sort(RevSort.TOPO, true);
 		rw.setTreeFilter(PathFilterGroup.createFromStrings("0"));
 		markStart(d);
-		assertCommit(d, rw.next());
-		assertCommit(c, rw.next());
+		assertEquals(d, rw.next());
+		assertEquals(c, rw.next());
 		assertNull(rw.next());
 	}
 
@@ -458,9 +492,9 @@ public class FirstParentRevWalkTest extends RevWalkTestCase {
 		rw.sort(RevSort.TOPO_KEEP_BRANCH_TOGETHER, true);
 		rw.setTreeFilter(PathFilterGroup.createFromStrings("0"));
 		markStart(d);
-		assertCommit(d, rw.next());
-		assertCommit(c, rw.next());
-		assertCommit(b, rw.next());
+		assertEquals(d, rw.next());
+		assertEquals(c, rw.next());
+		assertEquals(b, rw.next());
 		assertNull(rw.next());
 	}
 
@@ -476,8 +510,8 @@ public class FirstParentRevWalkTest extends RevWalkTestCase {
 		rw.sort(RevSort.TOPO_KEEP_BRANCH_TOGETHER, true);
 		rw.setTreeFilter(PathFilterGroup.createFromStrings("0"));
 		markStart(d);
-		assertCommit(d, rw.next());
-		assertCommit(c, rw.next());
+		assertEquals(d, rw.next());
+		assertEquals(c, rw.next());
 		assertNull(rw.next());
 	}
 }
