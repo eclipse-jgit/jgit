e02ce7ef26709df4ee8866a2f178ab1e7bb81a5d
Thomas Wolf <twolf@apache.org>
2022-10-31 22:44:50 +0100

    RawText.isBinary(): handle complete buffer correctly
    Make sure we always get consistent results, whether or not we have the
    full data in the buffer.
    
    Change-Id: Ieb379a0c375ad3dd352e63ac2f23bda6ef16c215
    Signed-off-by: Thomas Wolf <twolf@apache.org>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/RawTextTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/diff/RawTextTest.java
@@ -18,8 +18,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.util.RawParseUtils;
@@ -242,6 +244,38 @@ public class RawTextTest {
 		RawText rt = new RawText(Constants.encodeASCII("\nfoo"));
 		assertEquals("\n", rt.getLineDelimiter());
 		assertTrue(rt.isMissingNewlineAtEnd());
+	}
+
+	@Test
+	public void testCrAtLimit() throws Exception {
+		int limit = RawText.getBufferSize();
+		byte[] data = new byte[RawText.getBufferSize() + 2];
+		data[0] = 'A';
+		for (int i = 1; i < limit - 1; i++) {
+			if (i % 7 == 0) {
+				data[i] = '\n';
+			} else {
+				data[i] = (byte) ('A' + i % 7);
+			}
+		}
+		data[limit - 1] = '\r';
+		data[limit] = '\n';
+		data[limit + 1] = 'A';
+		assertTrue(RawText.isBinary(data, limit, true));
+		assertFalse(RawText.isBinary(data, limit, false));
+		assertFalse(RawText.isBinary(data, data.length, true));
+		byte[] buf = Arrays.copyOf(data, limit);
+		try (ByteArrayInputStream in = new ByteArrayInputStream(buf)) {
+			assertTrue(RawText.isBinary(in));
+		}
+		byte[] buf2 = Arrays.copyOf(data, limit + 1);
+		try (ByteArrayInputStream in = new ByteArrayInputStream(buf2)) {
+			assertFalse(RawText.isBinary(in));
+		}
+		byte[] buf3 = Arrays.copyOf(data, limit + 2);
+		try (ByteArrayInputStream in = new ByteArrayInputStream(buf3)) {
+			assertFalse(RawText.isBinary(in));
+		}
 	}
 
 	private static RawText t(String text) {
