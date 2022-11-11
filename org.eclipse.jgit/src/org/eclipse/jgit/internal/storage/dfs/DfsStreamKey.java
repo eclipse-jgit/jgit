fbd4ae1a25a4add1629d62f86aa074364f52aebe
Anna Papitto <annapapitto@google.com>
2022-11-11 10:44:37 -0800

    DfsStreamKey: Replace ForReverseIndex to separate metrics.
    Keys used for identifying reverse indexes in the DfsBlockCache use a
    custom subclass ForReverseIndex because there was no PackExt for them.
    This conflates BlockCacheMetrics for reverse indexes with those for
    packs, since the key falls back onto 0 when there is no extension.
    
    Replace the custom ForReverseIndex with a DfsStreamKey usage to bring
    keys for the new REVERSE_INDEX extension in line with INDEX and BITMAP
    and separate reverse index and pack BlockCacheMetrics.
    
    Change-Id: I305e2c16d2a8cb2a824855ea92e0c9a9b188fce5
    Signed-off-by: Anna Papitto <annapapitto@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsStreamKey.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsStreamKey.java
@@ -95,19 +95,4 @@ public abstract class DfsStreamKey {
 			return false;
 		}
 	}
-
-	static final class ForReverseIndex extends DfsStreamKey {
-		private final DfsStreamKey idxKey;
-
-		ForReverseIndex(DfsStreamKey idxKey) {
-			super(idxKey.hash + 1, null);
-			this.idxKey = idxKey;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			return o instanceof ForReverseIndex
-					&& idxKey.equals(((ForReverseIndex) o).idxKey);
-		}
-	}
 }
