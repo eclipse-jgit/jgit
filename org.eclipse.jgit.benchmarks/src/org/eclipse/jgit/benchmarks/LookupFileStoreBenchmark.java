dd1eaec42d2f1826149bbac639a554982dc5b1f2
Matthias Sohn <matthias.sohn@sap.com>
2022-11-14 23:23:55 +0100

    [benchmarks] Remove profiler configuration
    Profiler configuration can be added when required. It was commented out
    in most benchmarks.
    
    Change-Id: I725f98757f7d4d2ba2589658e34e2fd6fbbbedee

--- org.eclipse.jgit.benchmarks/src/org/eclipse/jgit/benchmarks/LookupFileStoreBenchmark.java
+++ org.eclipse.jgit.benchmarks/src/org/eclipse/jgit/benchmarks/LookupFileStoreBenchmark.java
@@ -56,8 +56,6 @@ public class LookupFileStoreBenchmark {
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(LookupFileStoreBenchmark.class.getSimpleName())
-				.addProfiler(StackProfiler.class)
-				// .addProfiler(GCProfiler.class)
 				.forks(1).jvmArgs("-ea").build();
 		new Runner(opt).run();
 	}
