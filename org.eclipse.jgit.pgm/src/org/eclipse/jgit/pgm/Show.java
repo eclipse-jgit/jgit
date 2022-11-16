d03e8679c5eb2ce14333fc4f5d23e8afb1ff84f2
alerthak <a2terminator@mail.ru>
2022-11-16 22:22:04 +0300

    DiffFormatter: optionally show binary differences
    Before adding two options, if binary data is found in the differences,
    the differences were always displayed like this:
    Binary files /dev/null and b/2 differ
    Regardless of the --text or --binary flags
    After adding all the results correspond to git.
    
    Change-Id: I27042752947ecc971cca34a1bc105e5bd7d30813

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Show.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Show.java
@@ -155,6 +155,16 @@ class Show extends TextBuiltin {
 		diffFmt.setNewPrefix(""); //$NON-NLS-1$
 	}
 
+	@Option(name = "--text", usage = "usage_binaryAsText")
+	void asText(@SuppressWarnings("unused") boolean on) {
+		diffFmt.setAsText(true);
+	}
+
+	@Option(name = "--binary", usage = "usage_binaryAsBinary")
+	void asBinary(@SuppressWarnings("unused") boolean on) {
+		diffFmt.setAsBinary(true);
+	}
+
 	// END -- Options shared with Diff
 
 	Show() {
