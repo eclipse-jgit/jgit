94a8c418762abb80ef4d3a13efe0119bf53de186
kylezhao <kylezhao@tencent.com>
2022-11-18 11:12:41 +0800

    Use commit-graph for normal revision walk and merge-base calculation
    Teach JGit to use RevCommit#parseHeadersInGraph() to speed up the
    commits traversal. With this feature, we can get the result of
    RevWalk#next() faster when RevFilter#requiresCommitBody() is false, such
    as `git log --before=<time>` and `git merge-base` command.
    
    To ensure the existing features of RevWalk are not affected, add an
    additional check for commit body in RevWalk#next().
    
    The following is the performance results for a copy of the Linux
    repository with this change:
    
    | Command                       |   berfore  |    after   |
    |-------------------------------|------------|------------|
    | rev-list -1000                |  16ms      |    2ms     |
    | rev-list -10000               |  96ms      |    12ms    |
    | rev-list -50000               |  482ms     |    46ms    |
    | rev-list -100000              |  936ms     |    85ms    |
    | rev-list -200000              |  1844ms    |    178ms   |
    | rev-list -500000              |  3919ms    |    445ms   |
    
    (without the time to initialize the repository)
    
    Bug: 574368
    Change-Id: Ic03122e671882f0bba37353dde61f6e7431ef6c4
    Signed-off-by: kylezhao <kylezhao@tencent.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
@@ -321,7 +321,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		if ((c.flags & SEEN) != 0)
 			return;
 		if ((c.flags & PARSED) == 0)
-			c.parseHeaders(this);
+			c.parseHeadersInGraph(this);
 		c.flags |= SEEN;
 		roots.add(c);
 		queue.add(c);
@@ -611,7 +611,11 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 	 */
 	public RevCommit next() throws MissingObjectException,
 			IncorrectObjectTypeException, IOException {
-		return pending.next();
+		RevCommit commit = pending.next();
+		if (commit != null && isRetainBody()) {
+			commit.parseBody(this);
+		}
+		return commit;
 	}
 
 	/**
