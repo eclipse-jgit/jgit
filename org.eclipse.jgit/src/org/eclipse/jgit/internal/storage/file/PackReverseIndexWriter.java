ca73608ac1a758f51569887c3ba6723e82aea1ce
Anna Papitto <annapapitto@google.com>
2022-11-21 15:41:20 -0800

    PackReverseIndexWriter: add base class and version 1 class
    The reverse index for a pack is used to quickly find an object's
    position in the pack's forward index based on that object's pack offset.
    It is currently computed from the forward index by sorting the index
    entries by the corresponding pack offset. This computation uses
    insertion sort, which has an average runtime of O(n^2).
    
    Cgit persists a pack reverse index file
    to avoid recomputing the reverse index ordering. Instead they write a
    file with format
    https://git-scm.com/docs/pack-format#_pack_rev_files_have_the_format
    which can later be read and parsed into the in-memory reverse index
    each time it is needed.
    
    PackReverseIndexWriter writes out a reverse index file to be read when
    needed. Subclass PackReverseIndexWriterV1 writes a file with the
    official version 1 format.
    
    Change-Id: I6437745777a16f723e2f1cfcce4e0d94e599dcee
    Signed-off-by: Anna Papitto <annapapitto@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackReverseIndexWriter.java
@@ -0,0 +1,71 @@
+// TODO: copyright?
+
+package org.eclipse.jgit.internal.storage.file;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.DigestOutputStream;
+import java.text.MessageFormat;
+import java.util.List;
+
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.transport.PackedObjectInfo;
+
+/**
+ * TODO
+ */
+public abstract class PackReverseIndexWriter {
+
+	protected static byte[] MAGIC = { 'R', 'I', 'D', 'X' };
+
+	protected final DigestOutputStream out;
+	protected final DataOutput dataOutput;
+
+	protected List<? extends PackedObjectInfo> objectsSortedByIndexPosition;
+	private byte[] packChecksum;
+
+	protected static final int VERSION_1 = 1;
+	private static final int DEFAULT_VERSION = VERSION_1;
+
+	protected PackReverseIndexWriter(OutputStream dst) {
+		out = new DigestOutputStream(dst instanceof BufferedOutputStream ? dst : new BufferedOutputStream(dst),
+				Constants.newMessageDigest());
+		dataOutput = new SimpleDataOutput(out);
+	}
+
+	public static PackReverseIndexWriter createWriter(final OutputStream dst) {
+		return createWriter(dst, DEFAULT_VERSION);
+	}
+
+	public static PackReverseIndexWriter createWriter(final OutputStream dst, final int version) {
+		if (version == VERSION_1) {
+			return new PackReverseIndexWriterV1(dst);
+		} else {
+			throw new IllegalArgumentException(
+					MessageFormat.format(JGitText.get().unsupportedPackReverseIndexVersion, version));
+		}
+	}
+
+	public void write(List<? extends PackedObjectInfo> objectsSortedByIndexPosition, byte[] packChecksum)
+			throws IOException {
+		this.objectsSortedByIndexPosition = objectsSortedByIndexPosition;
+		this.packChecksum = packChecksum;
+		writeHeader();
+		writeBody();
+		writeFooter();
+		out.flush();
+	}
+
+	protected abstract void writeHeader() throws IOException;
+
+	protected abstract void writeBody() throws IOException;
+
+	private void writeFooter() throws IOException {
+		out.write(packChecksum);
+		byte[] selfChecksum = out.getMessageDigest().digest();
+		out.write(selfChecksum);
+	}
+}
