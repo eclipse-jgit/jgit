ad0da67dd4e8e270c35de299562673406854535d
Anna Papitto <annapapitto@google.com>
2022-11-21 15:41:20 -0800

    PackReverseIndexWriter: write out version 1 reverse index file
    The reverse index for a pack is used to quickly find an object's
    position in the pack's forward index based on that object's pack offset.
    It is currently computed from the forward index by sorting the index
    entries by the corresponding pack offset. This computation uses
    insertion sort, which has an average runtime of O(n^2).
    
    Cgit persists a pack reverse index file
    to avoid recomputing the reverse index ordering. Instead they write a
    file with format
    https://git-scm.com/docs/pack-format#_pack_rev_files_have_the_format
    which can later be read and parsed into the in-memory reverse index
    each time it is needed.
    
    PackReverseIndexWriter writes out a reverse index file to be read when
    needed. Subclass PackReverseIndexWriterV1 writes a file with the
    official version 1 format.
    
    Change-Id: I6437745777a16f723e2f1cfcce4e0d94e599dcee
    Signed-off-by: Anna Papitto <annapapitto@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackReverseIndexWriterV1.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022, Google LLC and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.file;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+/**
+ * Writes reverse index files following the version 1 format.
+ * <p>
+ * The file format is specified at https://git-scm.com/docs/pack-format#_pack_rev_files_have_the_format.
+ */
+final class PackReverseIndexWriterV1 extends PackReverseIndexWriter {
+	// TODO comment about GIT_HASH_SHA1 == 1 and GIT_HASH_SHA256 == 2
+	private static final int OID_VERSION_SHA1 = 1;
+	private static final int DEFAULT_OID_VERSION = OID_VERSION_SHA1;
+
+	PackReverseIndexWriterV1(final OutputStream dst) {
+		super(dst);
+	}
+
+	protected void writeHeader() throws IOException {
+		out.write(MAGIC);
+		dataOutput.writeInt(VERSION_1);
+		dataOutput.writeInt(DEFAULT_OID_VERSION);
+	}
+
+	protected void writeBody() throws IOException {
+		int[] indexPositionsSortedByOffset = IntStream.range(0, objectsSortedByIndexPosition.size()).boxed()
+				.sorted(Comparator.comparingLong(
+						indexPosition -> objectsSortedByIndexPosition.get((int) indexPosition).getOffset()))
+				.mapToInt(i -> i).toArray();
+
+		for (int indexPosition : indexPositionsSortedByOffset) {
+			dataOutput.writeInt(indexPosition);
+		}
+	}
+}
