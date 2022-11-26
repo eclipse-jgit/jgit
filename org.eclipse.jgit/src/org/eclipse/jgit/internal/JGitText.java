1b93d1f6134abb10b798b0777c7f847f8a17ea85
alerthak <a2terminator@mail.ru>
2022-11-26 03:56:14 +0300

    Add RestoreCommand to JGit API
    [pgm] Add restore command
    
    Change-Id: Ib6b528d60e11f74a6df790af2b2cd9267db404d0

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -328,6 +328,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String exceptionCaughtDuringExecutionOfPushCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfResetCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfRevertCommand;
+	/***/ public String exceptionCaughtDuringExecutionOfRestoreCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfRmCommand;
 	/***/ public String exceptionCaughtDuringExecutionOfTagCommand;
 	/***/ public String exceptionHookExecutionInterrupted;
@@ -602,6 +603,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String patchApplyException;
 	/***/ public String patchFormatException;
 	/***/ public String pathNotConfigured;
+	/***/ public String pathspecDidNotMatch;
 	/***/ public String peeledLineBeforeRef;
 	/***/ public String peeledRefIsRequired;
 	/***/ public String peerDidNotSupplyACompleteObjectGraph;
