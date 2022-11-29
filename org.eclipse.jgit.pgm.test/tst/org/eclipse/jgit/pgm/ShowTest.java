c9be75caf8726af42f1ad60468d7703c8ed0d38e
alerthak <a2terminator@mail.ru>
2022-11-29 10:11:46 +0300

    [pgm] Add options --raw to diff, log, show
    Change-Id: I2becd064720d6642922bf0099901388dd4dc50a6

--- org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ShowTest.java
+++ org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/ShowTest.java
@@ -61,4 +61,23 @@ public class ShowTest extends CLIRepositoryTestCase {
 				"Date:   Sat Aug 15 20:12:58 2009 -0330", "", "    modified",
 				"M\ta"), result);
 	}
+
+	@Test
+	public void testShowRaw() throws Exception {
+		String result = toString(execute("git show --raw"));
+		assertEquals(toString("commit ecdf62e777b7413fc463c20e935403d424410ab2",
+				"Author: GIT_COMMITTER_NAME <GIT_COMMITTER_EMAIL>",
+				"Date:   Sat Aug 15 20:12:58 2009 -0330", "", "    modified",
+				":100644 100644 2e65efe2a 59ef8d134 M\ta"), result);
+	}
+
+	@Test
+	public void testShowRawNoAbbrev() throws Exception {
+		String result = toString(execute("git show --raw --no-abbrev"));
+		assertEquals(toString("commit ecdf62e777b7413fc463c20e935403d424410ab2",
+				"Author: GIT_COMMITTER_NAME <GIT_COMMITTER_EMAIL>",
+				"Date:   Sat Aug 15 20:12:58 2009 -0330", "", "    modified",
+				":100644 100644 2e65efe2a145dda7ee51d1741299f848e5bf752e 59ef8d134f97de87ebcac8e3a0c32d78c81e842e M\ta")
+				, result);
+	}
 }
