ad56e7a84de86ae8df882af8a5ae7dfaf70198fc
Kaushik Lingarkar <quic_kaushikl@quicinc.com>
2022-12-02 13:21:02 -0800

    Introduce core.trustPackedRefsModificationTime config
    Currently, we always read packed-refs file when 'trustFolderStat'
    is false. Introduce a new config 'trustPackedRefsModificationTime'
    which takes precedence over trustFolderStat when reading packed
    refs. Possible values for this new config are:
    
    * always: Trust packed-refs modification time
    * after_open: Same as 'always', but refresh the modification time
                  of packed-refs file before trusting it
    * never: Always read the packed-refs file
    * unset: Fallback to trustFolderStat to determine if the modification
             time of packed-refs can be trusted
    
    Folks whose repositories are on NFS and have traditionally been
    setting trustFolderStat=false can now get some performance improvement
    with trustPackedRefsModificationTime=after_open as it refreshes the
    modification time of packed-refs file (at least on some NFS clients)
    before considering it.
    
    For example, consider a repository on NFS with ~500k packed-refs. Here
    are some stats which illustrate the improvement with this new config
    when reading packed refs on NFS:
    
    trustFolderStat=true trustPackedRefsModificationTime=unset: 0.2ms
    trustFolderStat=false trustPackedRefsModificationTime=unset: 155ms
    trustFolderStat=false trustPackedRefsModificationTime=after_open: 1.5ms
    
    Change-Id: I00da88e4cceebbcf3475be0fc0011ff65767c111
    Signed-off-by: Kaushik Lingarkar <quic_kaushikl@quicinc.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
@@ -116,6 +116,24 @@ public class CoreConfig {
 		ALWAYS
 	}
 
+	/**
+	 * Permissible values for {@code core.trustPackedRefsModificationTime}.
+	 *
+	 * @since 6.1
+	 */
+	public enum TrustPackedRefsModificationTime {
+		/** Do not trust packed-refs modification time. */
+		NEVER,
+
+		/** Trust packed-refs modification time. */
+		ALWAYS,
+
+		/** Open and close packed-refs file to refresh its modification time and then trust it. */
+		AFTER_OPEN,
+
+		UNSET
+	}
+
 	private final int compression;
 
 	private final int packIndexVersion;
