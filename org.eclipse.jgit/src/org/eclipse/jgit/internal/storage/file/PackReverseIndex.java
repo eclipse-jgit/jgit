271ae12ba030d61fbf3ceedeefe9c1a47be1e233
Anna Papitto <annapapitto@google.com>
2022-12-08 11:43:52 -0800

    PackReverseIndexV1: reverse index parsed from version 1 file
    The reverse index for a pack is used to quickly find an object's
    position in the pack's forward index based on that object's pack offset.
    It is currently computed from the forward index by sorting the index
    entries by the corresponding pack offset. This computation uses
    insertion sort, which has an average runtime of O(n^2).
    
    Cgit persists a pack reverse index file
    to avoid recomputing the reverse index ordering. Instead they write a
    file with format
    https://git-scm.com/docs/pack-format#_pack_rev_files_have_the_format
    which can later be read and parsed into the in-memory reverse index
    each time it is needed.
    
    PackReverseIndexV1 parses a reverse index file with the official
    version 1 format into an in-memory representation of the reverse index
    which implements methods to find an object's forward index position
    from its offset in logorithmic time.
    
    Change-Id: Ib9353da3f69113fc0c13a61f6bb5df1e00ad0d82
    Signed-off-by: Anna Papitto <annapapitto@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackReverseIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackReverseIndex.java
@@ -10,8 +10,18 @@
 
 package org.eclipse.jgit.internal.storage.file;
 
+import java.io.DataInput;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.text.MessageFormat;
+import java.util.Arrays;
+
 import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.util.IO;
 
 /**
  * <p>
@@ -24,6 +34,56 @@ import org.eclipse.jgit.lib.ObjectId;
  * @see Pack
  */
 public abstract class PackReverseIndex {
+	/**
+	 * Magic bytes that uniquely identify git reverse index files.
+	 */
+	public static byte[] MAGIC = { 'R', 'I', 'D', 'X' };
+	/**
+	 * The version number of the first reverse index file version.
+	 */
+	public static final int VERSION_1 = 1;
+
+	/**
+	 * Read an in-memory pack reverse index from the given input stream. This
+	 * has a linear runtime.
+	 *
+	 * @param src               the input stream to read the contents from
+	 * @param objectCount       the number of objects in the corresponding pack
+	 * @param packIndexSupplier a function to lazily get the corresponding
+	 *                          forward index
+	 * @return the reverse index instance
+	 * @throws IOException if reading from the input stream fails
+	 */
+	public static PackReverseIndex read(InputStream src, long objectCount,
+			PackBitmapIndex.SupplierWithIOException<PackIndex> packIndexSupplier)
+			throws IOException {
+		final DigestInputStream digestIn =
+				new DigestInputStream(src, Constants.newMessageDigest());
+
+		final byte[] magic = new byte[MAGIC.length];
+		IO.readFully(digestIn, magic);
+		if (!Arrays.equals(magic, MAGIC)) {
+			throw new IOException(
+					MessageFormat.format(JGitText.get().expectedGot,
+							Arrays.toString(MAGIC), Arrays.toString(magic)));
+		}
+
+		DataInput dataIn = new SimpleDataInput(digestIn);
+		int version = dataIn.readInt();
+		switch (version) {
+		case VERSION_1:
+			PackReverseIndexV1 ri =
+					new PackReverseIndexV1(digestIn, objectCount,
+							packIndexSupplier);
+			ri.parse();
+			return ri;
+		default:
+			throw new IOException(MessageFormat.format(
+					JGitText.get().unsupportedPackReverseIndexVersion,
+					version));
+		}
+	}
+
 	/**
 	 * Compute an in-memory pack reverse index from the in-memory pack forward
 	 * index. This computation uses insertion sort, which has a quadratic
@@ -49,16 +109,15 @@ public abstract class PackReverseIndex {
 	 * Search for the next offset to the specified offset in this pack (reverse)
 	 * index.
 	 *
-	 * @param offset
-	 *            start offset of previous object (must be valid-existing
-	 *            offset).
-	 * @param maxOffset
-	 *            maximum offset in a pack (returned when there is no next
-	 *            offset).
+	 * @param offset    start offset of previous object (must be valid-existing
+	 *                  offset).
+	 * @param maxOffset maximum offset in a pack (returned when there is no next
+	 *                  offset).
 	 * @return offset of the next object in a pack or maxOffset if provided
-	 *         offset was the last one.
-	 * @throws org.eclipse.jgit.errors.CorruptObjectException
-	 *             when there is no object with the provided offset.
+	 * offset was the last one.
+	 * @throws org.eclipse.jgit.errors.CorruptObjectException when there is no
+	 *                                                        object with the
+	 *                                                        provided offset.
 	 */
 	public abstract long findNextOffset(long offset, long maxOffset)
 			throws CorruptObjectException;
