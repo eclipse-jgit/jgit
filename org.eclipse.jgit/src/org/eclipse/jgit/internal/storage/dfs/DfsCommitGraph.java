168b8fdc60bf08abc9c9291248be0544bc7045a5
Xing Huang <xingkhuang@google.com>
2022-12-12 14:15:01 -0600

    DfsObjDatabase boilderplate for supporting commit graphs
    Currently DfsObjDatabase doesn't contain any information about commit graph, we need these fields to track the reader (DfsCommitGraph) of each commit graph and be able to invoke their own reader.
    
    Adding List<DfsCommitGraph> into as a field into DfsObjDatabase and adjust the boilerplate code. All existing operations related to RefTables are mirrored.
    
    Change-Id: I921a34083d3351cca86bb45427702c9f82c14d19
    Signed-off-by: Xing Huang <xingkhuang@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsCommitGraph.java
@@ -0,0 +1,50 @@
+package org.eclipse.jgit.internal.storage.dfs;
+
+import static org.eclipse.jgit.internal.storage.pack.PackExt.COMMIT_GRAPH;
+
+/**
+ * A commit graph stored in
+ * {@link org.eclipse.jgit.internal.storage.dfs.DfsBlockCache}.
+ */
+public class DfsCommitGraph extends BlockBasedFile {
+	/**
+	 * Construct a reader for an existing commit graph.
+	 *
+	 * @param desc
+	 *            description of the commit graph within the DFS.
+	 */
+	public DfsCommitGraph(DfsPackDescription desc) {
+		this(DfsBlockCache.getInstance(), desc);
+	}
+
+	/**
+	 * Construct a reader for an existing commit graph.
+	 *
+	 * @param cache
+	 *            cache that will store the commit graph data.
+	 * @param desc
+	 *            description of the commit grpah within the DFS.
+	 */
+	public DfsCommitGraph(DfsBlockCache cache, DfsPackDescription desc) {
+		super(cache, desc, COMMIT_GRAPH);
+
+		int bs = desc.getBlockSize(COMMIT_GRAPH);
+		if (bs > 0) {
+			setBlockSize(bs);
+		}
+
+		long sz = desc.getFileSize(COMMIT_GRAPH);
+		length = sz > 0 ? sz : -1;
+	}
+
+	/**
+	 * Get description that was originally used to configure this file.
+	 *
+	 * @return description that was originally used to configure this file.
+	 */
+	public DfsPackDescription getPackDescription() {
+		return desc;
+	}
+
+	// TODO given a DfsReader, return a CommitGraphReader using CacheSource
+}
