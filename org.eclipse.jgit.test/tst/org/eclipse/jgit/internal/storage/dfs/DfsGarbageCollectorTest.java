174dc5772843a93b370b4013958fe32a69b5ca98
Xing Huang <xingkhuang@google.com>
2023-01-30 11:12:21 -0600

    CommitGraph: adding writing to DfsGarbageCollector and reading to DfsPackFile
    This change makes Dfs fraction capable of writing commit graph file into a stream and loading commit graph file from DfsBlockCache or disk.
    
    The garbage collector generates a commit graph with all heads/tags/nonHeads, then the commit graph can be loaded via the pack file from disk or form cache.
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: I3f94997377986d21a56b300d8358dd27be37f5de

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollectorTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollectorTest.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+import org.eclipse.jgit.internal.storage.commitgraph.CommitGraph;
 import org.eclipse.jgit.internal.storage.dfs.DfsObjDatabase.PackSource;
 import org.eclipse.jgit.internal.storage.reftable.RefCursor;
 import org.eclipse.jgit.internal.storage.reftable.ReftableConfig;
@@ -976,8 +977,103 @@ public class DfsGarbageCollectorTest {
 		assertNull(refdb.exactRef(NEXT));
 	}
 
+	@Test
+	public void gcWriteCommitGraphWithHeadTagNonHead() throws Exception {
+		String tag = "refs/tags/tag1";
+		String head = "refs/heads/head1";
+		String nonHead = "refs/something/nonHead";
+
+		RevCommit commit0 = git.branch(tag).commit().message("0").noParents()
+				.create();
+		RevCommit commit1 = git.branch(head).commit().message("1")
+				.parent(commit0).create();
+		RevCommit commit2 = git.branch(nonHead).commit().message("2")
+				.parent(commit0).create();
+
+		gcWithCommitGraph();
+
+		assertEquals(2, odb.getPacks().length);
+		DfsPackFile gcPack = odb.getPacks()[0];
+		DfsPackDescription desc = gcPack.getPackDescription();
+		assertEquals(GC, desc.getPackSource());
+		assertTrue("commit0 in pack", isObjectInPack(commit0, gcPack));
+		assertTrue("commit1 in pack", isObjectInPack(commit1, gcPack));
+		assertTrue("commit2 in pack", isObjectInPack(commit1, gcPack));
+
+		DfsReader reader = odb.newReader();
+		CommitGraph cg = gcPack.getCommitGraph(reader);
+		assertNotNull(cg);
+		gcPack.wipeInMemoryCommitGraph();
+		// Invoke cache hit
+		CommitGraph cachedCG = gcPack.getCommitGraph(reader);
+		assertNotNull(cachedCG);
+		assertTrue("commit graph is retrieved from cache once",
+				reader.stats.commitGraphCacheHit == 1);
+		assertTrue("commit graph have been read from disk once",
+				reader.stats.readCommitGraph == 1);
+		assertTrue("commit graph read contains content",
+				reader.stats.readCommitGraphBytes > 0);
+		assertTrue("commit graph read time is recorded",
+				reader.stats.readCommitGraphMicros > 0);
+
+		assertTrue("all commits in commit graph", cg.getCommitCnt() == 3);
+		// GC packed
+		assertTrue("tag referenced commit is in graph",
+				cg.findGraphPosition(commit0) != -1);
+		assertTrue("head referenced commit is in graph",
+				cg.findGraphPosition(commit1) != -1);
+		// GC_REST packed
+		assertTrue("nonHead referenced commit is in graph",
+				cg.findGraphPosition(commit2) != -1);
+	}
+
+	@Test
+	public void noCommitGraphGeneratedWithGCRestAndUG() throws Exception {
+		String nonHead = "refs/something/nonHead";
+		RevCommit commit0 = commit().message("0").create();
+		RevCommit commit1 = commit().message("1").parent(commit0).create();
+		git.update(nonHead, commit0);
+
+		gcWithCommitGraph();
+
+		// The repository should have a GC_REST pack with commit0 and an
+		// UNREACHABLE_GARBAGE pack with commit1.
+		assertEquals(2, odb.getPacks().length);
+		boolean gcRestPackFound = false;
+		boolean garbagePackFound = false;
+		for (DfsPackFile pack : odb.getPacks()) {
+			DfsPackDescription d = pack.getPackDescription();
+			switch (d.getPackSource()) {
+			case GC_REST:
+				gcRestPackFound = true;
+				assertTrue("has commit0", isObjectInPack(commit0, pack));
+				assertFalse("no commit1", isObjectInPack(commit1, pack));
+				assertNull(pack.getCommitGraph(odb.newReader()));
+				break;
+			case UNREACHABLE_GARBAGE:
+				garbagePackFound = true;
+				assertFalse("no commit0", isObjectInPack(commit0, pack));
+				assertTrue("has commit1", isObjectInPack(commit1, pack));
+				assertNull(pack.getCommitGraph(odb.newReader()));
+				break;
+			default:
+				fail("unexpected " + d.getPackSource());
+				break;
+			}
+		}
+
+		assertTrue("gc pack found", gcRestPackFound);
+		assertTrue("garbage pack found", garbagePackFound);
+	}
+
 	private TestRepository<InMemoryRepository>.CommitBuilder commit() {
 		return git.commit();
+	}
+
+	private void gcWithCommitGraph() throws IOException {
+		DfsGarbageCollector gc = new DfsGarbageCollector(repo);
+		gc.setWriteCommitGraph(true);
+		run(gc);
 	}
 
 	private void gcNoTtl() throws IOException {
