3fc4bd1b36dc652b5523ac8f6e474f61c998d86b
Kaushik Lingarkar <quic_kaushikl@quicinc.com>
2023-03-07 13:10:00 -0800

    RefDirectory: Make pack() and commitPackRefs() void
    There are no more callers (since Iae71cb3) of these methods that need
    the returned value. These methods should not have been returning
    anything in the first place as that can introduce bugs such as the
    one described in Iae71cb3.
    
    Change-Id: I1d083a91603da803a106cfb1506925a82c2ef809
    Signed-off-by: Kaushik Lingarkar <quic_kaushikl@quicinc.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
@@ -697,17 +697,17 @@ public class RefDirectory extends RefDatabase {
 		pack(refs, Collections.emptyMap());
 	}
 
-	PackedRefList pack(Map<String, LockFile> heldLocks) throws IOException {
-		return pack(heldLocks.keySet(), heldLocks);
+	void pack(Map<String, LockFile> heldLocks) throws IOException {
+		pack(heldLocks.keySet(), heldLocks);
 	}
 
-	private PackedRefList pack(Collection<String> refs,
+	private void pack(Collection<String> refs,
 			Map<String, LockFile> heldLocks) throws IOException {
 		for (LockFile ol : heldLocks.values()) {
 			ol.requireLock();
 		}
 		if (refs.isEmpty()) {
-			return null;
+			return;
 		}
 		FS fs = parent.getFS();
 
@@ -748,12 +748,11 @@ public class RefDirectory extends RefDatabase {
 				}
 				if (!dirty) {
 					// All requested refs were already packed accurately
-					return packed;
+					return;
 				}
 
 				// The new content for packed-refs is collected. Persist it.
-				PackedRefList result = commitPackedRefs(lck, cur, packed,
-						false);
+				commitPackedRefs(lck, cur, packed,false);
 
 				// Now delete the loose refs which are now packed
 				for (String refName : refs) {
@@ -804,7 +803,6 @@ public class RefDirectory extends RefDatabase {
 				}
 				// Don't fire refsChanged. The refs have not change, only their
 				// storage.
-				return result;
 			} finally {
 				lck.unlock();
 			}
@@ -1024,7 +1022,7 @@ public class RefDirectory extends RefDatabase {
 		return new StringBuilder(end - off).append(src, off, end).toString();
 	}
 
-	PackedRefList commitPackedRefs(final LockFile lck, final RefList<Ref> refs,
+	void commitPackedRefs(final LockFile lck, final RefList<Ref> refs,
 			final PackedRefList oldPackedList, boolean changed)
 			throws IOException {
 		// Can't just return packedRefs.get() from this method; it might have been
@@ -1078,7 +1076,6 @@ public class RefDirectory extends RefDatabase {
 				result.set(newPackedList);
 			}
 		}.writePackedRefs();
-		return result.get();
 	}
 
 	private Ref readRef(String name, RefList<Ref> packed) throws IOException {
