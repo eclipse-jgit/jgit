060dcf1ccab6c43e4d5cc19524fafa3e82e351e0
Matthias Sohn <matthias.sohn@sap.com>
2023-04-09 20:51:15 +0200

    ListTagCommand: implement git tag --contains
    Change-Id: I07e57ba098eace9656393837fad4cb3590f31b22

--- org.eclipse.jgit/src/org/eclipse/jgit/api/ListTagCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/ListTagCommand.java
@@ -16,9 +16,13 @@ import java.util.List;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 
 /**
@@ -29,6 +33,9 @@ import org.eclipse.jgit.revwalk.RevWalk;
  */
 public class ListTagCommand extends GitCommand<List<Ref>> {
 
+	private final RevWalk rw;
+	private RevCommit commit;
+
 	/**
 	 * Constructor for ListTagCommand.
 	 *
@@ -37,21 +44,49 @@ public class ListTagCommand extends GitCommand<List<Ref>> {
 	 */
 	protected ListTagCommand(Repository repo) {
 		super(repo);
+		rw = new RevWalk(repo);
+	}
+
+	/**
+	 * Only list tags which contain the specified commit.
+	 *
+	 * @param commit
+	 *            the specified commit
+	 * @return this command
+	 * @throws IOException
+	 * @throws IncorrectObjectTypeException
+	 * @throws MissingObjectException
+	 *
+	 * @since 6.6
+	 */
+	public ListTagCommand setContains(AnyObjectId commit)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		checkCallable();
+		this.commit = rw.parseCommit(commit);
+		return this;
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public List<Ref> call() throws GitAPIException {
 		checkCallable();
-		List<Ref> tags = new ArrayList<>();
-		try (RevWalk revWalk = new RevWalk(repo)) {
+		List<Ref> tags;
+		try {
 			List<Ref> refList = repo.getRefDatabase()
 					.getRefsByPrefix(Constants.R_TAGS);
-			for (Ref ref : refList) {
-				tags.add(ref);
+			if (commit != null) {
+				// if body is retained #getMergedInto needs to access data not
+				// available in commit graph which is slower
+				rw.setRetainBody(false);
+				tags = rw.getMergedInto(commit, refList);
+			} else {
+				tags = new ArrayList<>(refList);
 			}
 		} catch (IOException e) {
 			throw new JGitInternalException(e.getMessage(), e);
+		} finally {
+			rw.close();
 		}
 		Collections.sort(tags,
 				(Ref o1, Ref o2) -> o1.getName().compareTo(o2.getName()));
