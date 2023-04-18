f8b25c81373924829eb3af368917e366f89d11af
Jonathan Tan <jonathantanmy@google.com>
2023-04-18 15:20:02 -0700

    CommitGraphWriter: write changed-path filters
    Add support for writing the BIDX and BDAT chunks of the commit graph
    file, as described in man gitformat-commit-graph(5). The ability to read
    such chunks will be added in a subsequent commit.
    
    This work is based on earlier work by Kyle Zhao
    (Ib863782af209f26381e3ca0a2c119b99e84b679c).
    
    Change-Id: Ic18e6f0eeec7da1e1ff31751aabda5e6952dbe6e
    Signed-off-by: kylezhao <kylezhao@tencent.com>
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/ChangedPathFilter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2023, Google LLC
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.commitgraph;
+
+import org.apache.commons.codec.digest.MurmurHash3;
+
+/**
+ * A changed path filter for a commit, read from a commit graph file.
+ *
+ * @since 6.7
+ */
+public class ChangedPathFilter {
+	/**
+	 * The number of times a path is hashed, as described in man
+	 * gitformat-commit-graph(5). The value of this constant is the only value
+	 * JGit currently supports.
+	 */
+	public static final int PATH_HASH_COUNT = 7;
+
+	/**
+	 * The minimum bits per entry, as described in man
+	 * gitformat-commit-graph(5). The value of this constant is the only value
+	 * JGit currently supports.
+	 */
+	public static final int BITS_PER_ENTRY = 10;
+
+	/**
+	 * Seed value as described in man gitformat-commit-graph(5).
+	 */
+	private static final int SEED1 = 0x293ae76f;
+
+	/**
+	 * Seed value as described in man gitformat-commit-graph(5).
+	 */
+	private static final int SEED2 = 0x7e646e2c;
+
+	/**
+	 * Hash the given path multiple times, setting bits corresponding to the
+	 * hash outputs.
+	 *
+	 * @param changedPathFilterData
+	 *            bits will be set on this array
+	 * @param path
+	 *            data to be hashed
+	 * @param offset
+	 *            offset into path
+	 * @param length
+	 *            length of path to use
+	 */
+	public static void add(byte[] changedPathFilterData, byte[] path,
+			int offset, int length) {
+
+		int hash0 = MurmurHash3.hash32x86(path, offset, length, SEED1);
+		int hash1 = MurmurHash3.hash32x86(path, offset, length, SEED2);
+		for (int i = 0; i < PATH_HASH_COUNT; i++) {
+			int pos = Integer.remainderUnsigned(hash0 + i * hash1,
+					changedPathFilterData.length * 8);
+			changedPathFilterData[pos / 8] |= (byte) (1 << (pos % 8));
+		}
+	}
+}
