b4375477ba9fee4a93ad6a0315f9e694b59cf8a0
Anna Papitto <annapapitto@google.com>
2023-04-27 11:01:30 -0700

    PackReverseIndexV1: reverse index parsed from version 1 file
    The reverse index for a pack is used to quickly find an object's
    position in the pack's forward index based on that object's pack offset.
    It is currently computed from the forward index by sorting the index
    entries by the corresponding pack offset. This computation uses
    insertion sort, which has an average runtime of O(n^2).
    
    Cgit persists a pack reverse index file
    to avoid recomputing the reverse index ordering. Instead they write a
    file with format
    https://git-scm.com/docs/pack-format#_pack_rev_files_have_the_format
    which can later be read and parsed into the in-memory reverse index
    each time it is needed.
    
    PackReverseIndexV1 parses a reverse index file with the official
    version 1 format into an in-memory representation of the reverse index
    which implements methods to find an object's forward index position
    from its offset in logorithmic time.
    
    Change-Id: Ib9353da3f69113fc0c13a61f6bb5df1e00ad0d82
    Signed-off-by: Anna Papitto <annapapitto@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -866,6 +866,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String unsupportedEncryptionVersion;
 	/***/ public String unsupportedGC;
 	/***/ public String unsupportedMark;
+	/***/ public String unsupportedObjectIdVersion;
 	/***/ public String unsupportedOperationNotAddAtEnd;
 	/***/ public String unsupportedPackIndexVersion;
 	/***/ public String unsupportedPackReverseIndexVersion;
