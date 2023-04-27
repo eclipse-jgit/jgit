d569d71009f00e03e9e15307bc3c677d4a544506
Anna Papitto <annapapitto@google.com>
2023-04-27 11:01:30 -0700

    PackWriter: write the PackReverseIndex file
    PackWriter offers the ability to write out the pack file and its various
    index files, except for the newly introduced file-based reverse index.
    
    Now that PackReverseIndexWriter can write reverse index files,
    PackWriter#writeReverseIndex will write one for a pack if the
    corresponding config flag PackConfig#writeReverseIndex is on.
    
    Change-Id: Ib75dd2bbfb9ee9366d5aacb46700d8cf8af4823a
    Signed-off-by: Anna Papitto <annapapitto@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -745,6 +745,11 @@ public final class ConfigConstants {
 	public static final String CONFIG_KEY_BITMAP_RECENT_COMMIT_COUNT = "bitmaprecentcommitspan";
 
 	/**
+	 * The "pack.writeReverseIndex" key
+	 */
+	public static final String CONFIG_KEY_WRITE_REVERSE_INDEX = "writeReverseIndex";
+
+	/**
 	 * The "pack.buildBitmaps" key
 	 * @since 5.8
 	 */
