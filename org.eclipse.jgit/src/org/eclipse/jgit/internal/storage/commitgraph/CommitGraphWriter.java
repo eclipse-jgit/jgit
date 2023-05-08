46a0d5de3a972ca1d9a83b8b0acc536498a86288
Jonathan Tan <jonathantanmy@google.com>
2023-05-08 13:51:28 -0700

    CommitGraphWriter: reuse changed path filters
    Teach CommitGraphWriter to reuse changed path filters that have been
    read from the commit graph file whenever possible.
    
    Change-Id: I1acbfa1613ca7198386a49209028886af360ddb6
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphWriter.java
@@ -47,6 +47,7 @@ import org.eclipse.jgit.internal.storage.io.CancellableDigestOutputStream;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevCommitCG;
 import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.util.NB;
@@ -374,46 +375,56 @@ public class CommitGraphWriter {
 		int dataHeaderSize = data.size();
 
 		for (RevCommit cmit : graphCommits) {
-			boolean tooMany = false;
-			HashSet<ByteBuffer> paths = new HashSet<>();
-			try (TreeWalk walk = new TreeWalk(null,
-					graphCommits.getObjectReader())) {
-				walk.setRecursive(true);
-				if (cmit.getParentCount() == 0) {
-					walk.addTree(new EmptyTreeIterator());
-				} else {
-					walk.addTree(cmit.getParent(0).getTree());
-				}
-				walk.addTree(cmit.getTree());
-				treeWalk: while (walk.next()) {
-					if (walk.idEqual(0, 1)) {
-						continue;
-					}
-					byte[] rawPath = walk.getRawPath();
-					paths.add(ByteBuffer.wrap(rawPath));
-					for (int i = 0; i < rawPath.length; i++) {
-						if (rawPath[i] == '/') {
-							paths.add(ByteBuffer.wrap(rawPath, 0, i));
-						}
-						if (paths.size() > MAX_CHANGED_PATHS) {
-							tooMany = true;
-							break treeWalk;
-						}
-					}
-				}
-			}
-			if (tooMany) {
-				data.write(0xff);
-			} else if (paths.isEmpty()) {
-				data.write(0);
+			if (cmit instanceof RevCommitCG) {
+				ChangedPathFilter cpf = ((RevCommitCG) cmit)
+						.getChangedPathFilter();
+				cpf.writeTo(data);
+				changedPathFiltersReused++;
 			} else {
-				byte[] bloom = new byte[-Math.floorDiv(
-						-paths.size() * ChangedPathFilter.BITS_PER_ENTRY, 8)];
-				for (ByteBuffer path : paths) {
-					ChangedPathFilter.add(bloom, path.array(), path.position(),
-							path.limit() - path.position());
+				boolean tooMany = false;
+				HashSet<ByteBuffer> paths = new HashSet<>();
+				try (TreeWalk walk = new TreeWalk(null,
+						graphCommits.getObjectReader())) {
+					walk.setRecursive(true);
+					if (cmit.getParentCount() == 0) {
+						walk.addTree(new EmptyTreeIterator());
+					} else {
+						walk.addTree(cmit.getParent(0).getTree());
+					}
+					walk.addTree(cmit.getTree());
+					treeWalk: while (walk.next()) {
+						if (walk.idEqual(0, 1)) {
+							continue;
+						}
+						byte[] rawPath = walk.getRawPath();
+						paths.add(ByteBuffer.wrap(rawPath));
+						for (int i = 0; i < rawPath.length; i++) {
+							if (rawPath[i] == '/') {
+								paths.add(ByteBuffer.wrap(rawPath, 0, i));
+							}
+							if (paths.size() > MAX_CHANGED_PATHS) {
+								tooMany = true;
+								break treeWalk;
+							}
+						}
+					}
 				}
-				data.write(bloom);
+				if (tooMany) {
+					data.write(0xff);
+				} else if (paths.isEmpty()) {
+					data.write(0);
+				} else {
+					byte[] bloom = new byte[-Math.floorDiv(
+							-paths.size() * ChangedPathFilter.BITS_PER_ENTRY,
+							8)];
+					for (ByteBuffer path : paths) {
+						ChangedPathFilter.add(bloom, path.array(),
+								path.position(),
+								path.limit() - path.position());
+					}
+					data.write(bloom);
+				}
+				changedPathFiltersComputed++;
 			}
 			NB.encodeInt32(scratch, 0, data.size() - dataHeaderSize);
 			index.write(scratch);
