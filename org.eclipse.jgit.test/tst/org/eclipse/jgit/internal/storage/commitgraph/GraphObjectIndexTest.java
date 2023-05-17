db1d4bdc98315c8dde78a9ce80ffcf09787f91d1
Ivan Frade <ifrade@google.com>
2023-05-17 16:09:55 -0700

    GraphObjectIndexTest: cover empty bucket case
    Change-Id: I664140c3b67381e21f2fc24ead82fe1bfc135e1e

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/commitgraph/GraphObjectIndexTest.java
@@ -0,0 +1,18 @@
+package org.eclipse.jgit.internal.storage.commitgraph;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GraphObjectIndexTest {
+
+  @Test
+  public void findGraphPosition_noObjInBucket() throws CommitGraphFormatException {
+    GraphObjectIndex idx = new GraphObjectIndex(100,
+        new byte[256], new byte[] {});
+    int graphPosition = idx.findGraphPosition(
+        ObjectId.fromString("731dfd4c5eb6f88b98e983b9b0551b3562a0c46c"));
+    assertEquals(-1, graphPosition);
+  }
+
+}
