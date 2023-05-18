b674d35f0fc754e11a2fe8267d344db343ead89c
Ivan Frade <ifrade@google.com>
2023-05-18 15:17:37 -0700

    WIP: test for graphobjectindextest
    Change-Id: Ie59a64fec5ffe7347a2a096326aecab912d5b87b

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/commitgraph/GraphObjectIndexTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/commitgraph/GraphObjectIndexTest.java
@@ -9,7 +9,7 @@ public class GraphObjectIndexTest {
   @Test
   public void findGraphPosition_noObjInBucket() throws CommitGraphFormatException {
     GraphObjectIndex idx = new GraphObjectIndex(100,
-        new byte[256], new byte[] {});
+        new byte[256 * 4], new byte[] {});
     int graphPosition = idx.findGraphPosition(
         ObjectId.fromString("731dfd4c5eb6f88b98e983b9b0551b3562a0c46c"));
     assertEquals(-1, graphPosition);
