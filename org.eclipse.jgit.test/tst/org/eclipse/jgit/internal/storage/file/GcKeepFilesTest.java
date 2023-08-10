4c9bdbe273246f2a1775a79f109fd17ae591ccf8
Antonio Barone <syntonyze@gmail.com>
2023-08-10 12:24:54 +0300

    Expose ArrayIndexOutOfBoundsException via test
    Introduce test exposing the ArrayIndexOutOfBounds exception being thrown
    during repacking phase, when keep file exists.
    
    Change-Id: I2fed5e7114e5145b3505cb6fda64c4ff3fecf26f

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcKeepFilesTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcKeepFilesTest.java
@@ -18,6 +18,7 @@ import java.util.Iterator;
 
 import org.eclipse.jgit.internal.storage.file.PackIndex.MutableEntry;
 import org.eclipse.jgit.internal.storage.pack.PackExt;
+import org.eclipse.jgit.junit.TestRepository;
 import org.eclipse.jgit.junit.TestRepository.BranchBuilder;
 import org.junit.Test;
 
@@ -71,5 +72,26 @@ public class GcKeepFilesTest extends GcTestCase {
 						"the following object is in both packfiles: "
 								+ e.toObjectId(),
 						ind2.hasObject(e.toObjectId()));
+	}
+
+	@Test
+	public void testKeepFileAllowsBitmapRemapping() throws Exception {
+		TestRepository<FileRepository>.BranchBuilder bb = tr.branch("refs/heads/master");
+		bb.commit().add("A", "A").create();
+		gc.gc();
+
+		bb.commit().add("B", "B").create();
+		gc.gc();
+
+		// Create a keep file, simulating a write operation ongoing
+		Iterator<Pack> packIt = repo.getObjectDatabase().getPacks()
+				.iterator();
+		Pack pack1 = packIt.next();
+		Pack singlePack = packIt.next();
+		PackFile keepFile = singlePack.getPackFile().create(PackExt.KEEP);
+		assertTrue(keepFile.createNewFile());
+
+		// BOOM!
+		gc.gc();
 	}
 }
