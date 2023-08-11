f5f4bf0ad97f67ff56db18033c0a0795b722e96e
Luca Milanesio <luca.milanesio@gmail.com>
2023-08-11 20:15:17 +0100

    Do not exclude objects in locked packs from bitmap processing
    Packfiles having an equivalent .keep file are associated with in-flight
    pushes that haven't been completed, with potentially a set of git
    objects not yet referenced by a ref.
    
    If the Git client is not up-to-date, it may result in pushing a
    packfile, generating a <packfile>.keep on the server, which
    may also contain existing commits due to the lack of Git protocol
    negotiation in the git-receive-pack.
    
    The Git protocol negotiation is the phase where the client and the
    server exchange the list of refs they have for trying to find a common
    base and minimise the amount of objects to be transferred.
    
    The repack phase in GC was previously skipping all objects that were
    contained in all packfiles having a <packfile>.keep file associated
    (aka "locked packfiles"), which did not take into consideration the
    fact that excluding the existing commits would have resulted in the
    generation of an invalid bitmap file.
    
    The code for excluding the objects in the locked packfiles was written
    well before the bitmap was introduced, hence could not consider a use
    case that did not exist at that time.
    
    However, when the bitmap was introduced, the exclusion of locked
    packfiles was not changed, hence creating a potential problem.
    The issue went unnoticed for many years because the bitmap generation
    was disabled when JGit noticed any locked packfiles; however, the
    bitmaps are enabled again since  Id722e68d9f , and the the issue is now
    visible and is impacting the GC repack phase.
    
    Introduce the '--pack-kept-objects' option in GC for including the
    objects contained in the locked packfiles during the repack phase,
    which is not an issue because of the following:
    
    - If there are any existing commits duplicated in the packfiles
      they will be just considered once anyway because the repack doesn't
      generate duplicates in the output packfile.
    
    - If there are any new commits that do not have any ref pointing to
      them, they will be automatically excluded from the output repacked
      packfile.
    
    The same identical solution is adopted in the C implementation of git
    in repack.c.
    
    Because the locked packfile is not pruned, any new commits not pointed
    by any refs will remain in the repository and there will not be any
    accidental pruning or object loss as it is today before this change.
    
    As a side-effect of this change, it is now potentially possible to still
    have duplicate BLOBs after GC when the keep packfile contained existing
    objects. However, it is way better to keep the duplication until the
    next GC phase rather than omitting existing objects from repacking and,
    therefore generating an invalid bitmap and incorrect packfile.
    
    Bug: 582292
    Bug: 582455
    Change-Id: Ide3445e652fcf256a7912f881cb898897c99b8f8

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcKeepFilesTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/GcKeepFilesTest.java
@@ -12,6 +12,7 @@ package org.eclipse.jgit.internal.storage.file;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
@@ -19,9 +20,12 @@ import java.util.Iterator;
 import org.eclipse.jgit.internal.storage.file.PackIndex.MutableEntry;
 import org.eclipse.jgit.internal.storage.pack.PackExt;
 import org.eclipse.jgit.junit.TestRepository.BranchBuilder;
+import org.eclipse.jgit.lib.ObjectId;
 import org.junit.Test;
 
 public class GcKeepFilesTest extends GcTestCase {
+	private static final int COMMIT_AND_TREE_OBJECTS = 2;
+
 	@Test
 	public void testKeepFiles() throws Exception {
 		BranchBuilder bb = tr.branch("refs/heads/master");
@@ -71,5 +75,77 @@ public class GcKeepFilesTest extends GcTestCase {
 						"the following object is in both packfiles: "
 								+ e.toObjectId(),
 						ind2.hasObject(e.toObjectId()));
+	}
+
+	@Test
+	public void testKeptObjectsAreIncluded() throws Exception {
+		BranchBuilder bb = tr.branch("refs/heads/master");
+		ObjectId commitObjectInLockedPack = bb.commit().create().toObjectId();
+		gc.gc();
+		stats = gc.getStatistics();
+		assertEquals(COMMIT_AND_TREE_OBJECTS, stats.numberOfPackedObjects);
+		assertEquals(1, stats.numberOfPackFiles);
+		assertTrue(getSinglePack().getPackFile().create(PackExt.KEEP).createNewFile());
+
+		bb.commit().create();
+		gc.setPackKeptObjects(true);
+		gc.gc();
+		stats = gc.getStatistics();
+		assertEquals(2 * COMMIT_AND_TREE_OBJECTS + 1,
+				stats.numberOfPackedObjects);
+		assertEquals(2, stats.numberOfPackFiles);
+
+		PackIndex lockedPackIdx = null;
+		PackIndex newPackIdx = null;
+		for (Pack pack : repo.getObjectDatabase().getPacks()) {
+			if (pack.getObjectCount() == COMMIT_AND_TREE_OBJECTS) {
+				lockedPackIdx = pack.getIndex();
+			} else {
+				newPackIdx = pack.getIndex();
+			}
+		}
+		assertNotNull(lockedPackIdx);
+		assertTrue(lockedPackIdx.hasObject(commitObjectInLockedPack));
+		assertNotNull(newPackIdx);
+		assertTrue(newPackIdx.hasObject(commitObjectInLockedPack));
+	}
+
+	@Test
+	public void testKeptObjectsAreNotIncludedByDefault() throws Exception {
+		BranchBuilder bb = tr.branch("refs/heads/master");
+		ObjectId commitObjectInLockedPack = bb.commit().create().toObjectId();
+		gc.gc();
+		stats = gc.getStatistics();
+		assertEquals(COMMIT_AND_TREE_OBJECTS, stats.numberOfPackedObjects);
+		assertEquals(1, stats.numberOfPackFiles);
+		assertTrue(getSinglePack().getPackFile().create(PackExt.KEEP).createNewFile());
+
+		bb.commit().create();
+		gc.gc();
+		stats = gc.getStatistics();
+		assertEquals(COMMIT_AND_TREE_OBJECTS + 1, stats.numberOfPackedObjects);
+		assertEquals(2, stats.numberOfPackFiles);
+
+		PackIndex lockedPackIdx = null;
+		PackIndex newPackIdx = null;
+		for (Pack pack : repo.getObjectDatabase().getPacks()) {
+			if (pack.getObjectCount() == COMMIT_AND_TREE_OBJECTS) {
+				lockedPackIdx = pack.getIndex();
+			} else {
+				newPackIdx = pack.getIndex();
+			}
+		}
+		assertNotNull(lockedPackIdx);
+		assertTrue(lockedPackIdx.hasObject(commitObjectInLockedPack));
+		assertNotNull(newPackIdx);
+		assertFalse(newPackIdx.hasObject(commitObjectInLockedPack));
+	}
+
+	private Pack getSinglePack() {
+		Iterator<Pack> packIt = repo.getObjectDatabase().getPacks()
+				.iterator();
+		Pack singlePack = packIt.next();
+		assertFalse(packIt.hasNext());
+		return singlePack;
 	}
 }
