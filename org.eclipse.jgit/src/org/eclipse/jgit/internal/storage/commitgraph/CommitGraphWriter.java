45ccae2f2fea68d56a319f23b3a29147a4e18455
Ivan Frade <ifrade@google.com>
2023-09-08 13:23:09 -0700

    CommitGraphWriter: Decouple Stats from computing bloom filters
    The public stats object is created only to be populated by the computation of
    bloom filters.
    
    Make the computation return its numbers with the results and copy them
    to the stats when needed. This eliminates the side effects
    from the computation and to add more data to the stats later.
    
    Change-Id: I7a5e55fc3a17f5a294edf3a3b725b2d9c0280a5a

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/commitgraph/CommitGraphWriter.java
@@ -118,13 +118,12 @@ public class CommitGraphWriter {
 	 */
 	public Stats write(@NonNull ProgressMonitor monitor,
 			@NonNull OutputStream commitGraphStream) throws IOException {
-		Stats stats = new Stats();
 		if (graphCommits.size() == 0) {
-			return stats;
+			return Stats.EMPTY;
 		}
 
 		BloomFilterChunks bloomFilterChunks = generateChangedPathFilters
-				? computeBloomFilterChunks(stats)
+				? computeBloomFilterChunks()
 				: null;
 		List<ChunkHeader> chunks = createChunks(bloomFilterChunks);
 		long expectedSize = calculateExpectedSize(chunks);
@@ -152,7 +151,7 @@ public class CommitGraphWriter {
 		} finally {
 			monitor.endTask();
 		}
-		return stats;
+		return Stats.from(bloomFilterChunks);
 	}
 
 	private List<ChunkHeader> createChunks(@Nullable BloomFilterChunks bloomFilterChunks)
@@ -405,12 +404,14 @@ public class CommitGraphWriter {
 		return Optional.of(paths);
 	}
 
-	private BloomFilterChunks computeBloomFilterChunks(Stats stats)
+	private BloomFilterChunks computeBloomFilterChunks()
 			throws MissingObjectException, IncorrectObjectTypeException,
 			CorruptObjectException, IOException {
 
 		ByteArrayOutputStream index = new ByteArrayOutputStream();
 		ByteArrayOutputStream data = new ByteArrayOutputStream();
+		long filtersReused = 0;
+		long filtersComputed =0;
 
 		// Allocate scratch buffer for converting integers into
 		// big-endian bytes.
@@ -428,9 +429,9 @@ public class CommitGraphWriter {
 			for (RevCommit cmit : graphCommits) {
 				ChangedPathFilter cpf = cmit.getChangedPathFilter(rw);
 				if (cpf != null) {
-					stats.changedPathFiltersReused++;
+					filtersReused++;
 				} else {
-					stats.changedPathFiltersComputed++;
+					filtersComputed++;
 					Optional<HashSet<ByteBuffer>> paths = computeBloomFilterPaths(
 							graphCommits.getObjectReader(), cmit);
 					if (paths.isEmpty()) {
@@ -443,7 +444,7 @@ public class CommitGraphWriter {
 				NB.encodeInt32(scratch, 0, data.size() - dataHeaderSize);
 				index.write(scratch);
 			}
-			return new BloomFilterChunks(index, data);
+			return new BloomFilterChunks(index, data, filtersReused, filtersComputed);
 		}
 	}
 
@@ -493,10 +494,18 @@ public class CommitGraphWriter {
 
 		final ByteArrayOutputStream data;
 
+		final long filtersReused;
+
+		final long filtersComputed;
+
 		BloomFilterChunks(ByteArrayOutputStream index,
-				ByteArrayOutputStream data) {
+				ByteArrayOutputStream data,
+				long filtersReused,
+				long filtersComputed) {
 			this.index = index;
 			this.data = data;
+			this.filtersReused = filtersReused;
+			this.filtersComputed = filtersComputed;
 		}
 	}
 
@@ -504,6 +513,19 @@ public class CommitGraphWriter {
 	 * Statistics collected during a single commit graph write.
 	 */
 	public static class Stats {
+
+		static final Stats EMPTY = new Stats();
+
+		static final Stats from(@Nullable BloomFilterChunks bloomFilterChunks) {
+			Stats stats = new Stats();
+			if (bloomFilterChunks != null) {
+				stats.changedPathFiltersComputed = bloomFilterChunks.filtersComputed;
+				stats.changedPathFiltersReused = bloomFilterChunks.filtersReused;
+			}
+			return stats;
+		}
+
+		private Stats() {};
 
 		private long changedPathFiltersReused = 0;
 
