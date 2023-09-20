bc99aa829cdeff33fc2d9d0ebe03343f3bac9e05
Matthias Sohn <matthias.sohn@sap.com>
2023-09-20 22:18:01 +0200

    [errorprone] TagCommand: Remove unused parameter of private method
    See https://errorprone.info/bugpattern/UnusedVariable
    
    Change-Id: I7e8aae3494ce60dbeb474f6bead0817c36276e69

--- org.eclipse.jgit/src/org/eclipse/jgit/api/TagCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/TagCommand.java
@@ -33,7 +33,6 @@ import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.RefUpdate.Result;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.jgit.lib.TagBuilder;
 import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevWalk;
@@ -108,9 +107,7 @@ public class TagCommand extends GitCommand<Ref> {
 	public Ref call() throws GitAPIException, ConcurrentRefUpdateException,
 			InvalidTagNameException, NoHeadException {
 		checkCallable();
-
-		RepositoryState state = repo.getRepositoryState();
-		processOptions(state);
+		processOptions();
 
 		try (RevWalk revWalk = new RevWalk(repo)) {
 			// if no id is set, we should attempt to use HEAD
@@ -197,9 +194,6 @@ public class TagCommand extends GitCommand<Ref> {
 	 * Sets default values for not explicitly specified options. Then validates
 	 * that all required data has been provided.
 	 *
-	 * @param state
-	 *            the state of the repository we are working on
-	 *
 	 * @throws InvalidTagNameException
 	 *             if the tag name is null or invalid
 	 * @throws ServiceUnavailableException
@@ -208,7 +202,7 @@ public class TagCommand extends GitCommand<Ref> {
 	 *             if the tag should be signed but {@code gpg.format} is not
 	 *             {@link GpgFormat#OPENPGP}
 	 */
-	private void processOptions(RepositoryState state)
+	private void processOptions()
 			throws InvalidTagNameException, ServiceUnavailableException,
 			UnsupportedSigningFormatException {
 		if (name == null
