5138b97b604f0bb50940186ebb6c11b32ef9551b
David Ostrovsky <david@ostrovsky.org>
2023-10-05 23:04:00 +0200

    TestRepository: Add getInstant method
    Error Prone is flagging Date-API as obsolete and recommends to migrate
    to Instant and LocalDate. Given that more JGit users starting to migrate
    to new Time API, offer getInstant method.
    
    Change-Id: Ie010b76d1c213cd0a645f716783ed2d57fc78071

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/TestRepository.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/TestRepository.java
@@ -20,6 +20,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.MessageDigest;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -199,6 +200,16 @@ public class TestRepository<R extends Repository> implements AutoCloseable {
 	 */
 	public Date getDate() {
 		return new Date(mockSystemReader.getCurrentTime());
+	}
+
+	/**
+	 * Get instant
+	 *
+	 * @return current instant.
+	 * @since 6.8
+	 */
+	public Instant getInstant() {
+		return Instant.ofEpochMilli(mockSystemReader.getCurrentTime());
 	}
 
 	/**
