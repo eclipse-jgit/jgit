641725b049fd2088675786e231fa72161583c9dc
Ronald Bhuleskar <funronald@google.com>
2023-10-25 13:10:33 -0700

    RevWalk: Use RewriteGenerator when TreeRevFilter is defined with a rewrite flag.
    When a TreeRevFilter is used in a revWalk even with rewriteParent flag
    enabled the filtered result doesn't rewrite parents. The rewrite parent
    logic is handled by RewriteGenerator which only was kicked in
    StartGenerator when treeFilter is set.
    
    The change here updates StartGenerator to honor rewriteParent on a
    revFilter. Given a RevFilter can also have a TreeRevFilter that can
    exclude commits in the resultant DAT. It also accounts for AND/OR
    revFilters that has TreeRevFilter nested in.
    
    Change-Id: I4e4ff67fb279edbcc3461496b132cea774fb742f

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TreeRevFilter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TreeRevFilter.java
@@ -73,6 +73,29 @@ public class TreeRevFilter extends RevFilter {
 	}
 
 	/**
+	 * @param walker
+	 * @param t
+	 * @param rewriteFlag
+	 */
+	/**
+	 * Create a {@link org.eclipse.jgit.revwalk.filter.RevFilter} from a
+	 * {@link org.eclipse.jgit.treewalk.filter.TreeFilter}.
+	 *
+	 * @param walker
+	 *            walker used for reading trees.
+	 * @param t
+	 *            filter to compare against any changed paths in each commit. If
+	 *            a {@link org.eclipse.jgit.revwalk.FollowFilter}, will be
+	 *            replaced with a new filter following new paths after a rename.
+	 * @param rewriteFlag
+	 *            flag to color commits to be removed from the simplified DAT.
+	 * @since 6.9
+	 */
+	public TreeRevFilter(RevWalk walker, TreeFilter t, boolean rewriteFlag) {
+		this(walker, t, rewriteFlag ? RevWalk.REWRITE : 0);
+	}
+
+	/**
 	 * Create a filter for the first phase of a parent-rewriting limited
 	 * revision walk.
 	 * <p>
@@ -281,6 +304,14 @@ public class TreeRevFilter extends RevFilter {
 	@Override
 	public boolean requiresCommitBody() {
 		return false;
+	}
+
+	/**
+	 * @return true if the rewrite flag is set, false otherwise.
+	 */
+	@Override
+	public boolean isRewriteEnabled() {
+		return this.rewriteFlag != 0;
 	}
 
 	/**
