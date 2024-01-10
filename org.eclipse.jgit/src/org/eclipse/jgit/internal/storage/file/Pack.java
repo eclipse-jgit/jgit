a4a8e6ce9f0426afc870b8c668ae12d83c6b2476
Luca Milanesio <luca.milanesio@gmail.com>
2024-01-10 21:32:47 +0000

    [DO NOT MERGE] Noop change for checking tests execution baseline
    Change-Id: Ie525542cbd6c0503a4328a6d4ee7b10ffac31ed5

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/Pack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/Pack.java
@@ -5,7 +5,7 @@
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0 which is available at
- * https://www.eclipse.org/org/documents/edl-v10.php.
+ * https://www.eclipse.org/org/documents/edl-v10.php .
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
