20eb8339c8fc72de118568177dbdf94b14ff5acd
Laura Hamelin <haowl@google.com>
2024-02-27 09:40:24 -0800

    DfsBlockCache: move cache table specific implementations to a new class
    This move of the cache table specific implementation to its own class
    and extracting the method definition to an interface will allow for
    additional reworking of the dfs block cache with the goal of letting
    users implement their own context specific cache tables.
    
    This work does not intend to change how the dfs block cache is accessed,
    limiting the change to DfsBlockCache internal changes only.
    
    Change-Id: Ief6755d1dcf54b4f73f9fe8d22ccb9e2952cb0c4
    Signed-off-by: Laura Hamelin <haowl@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCacheTable.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2024, Google LLC and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.dfs;
+
+import java.io.IOException;
+
+public interface DfsBlockCacheTable {
+	/**
+	 * Quickly check if the cache contains block 0 of the given stream.
+	 * <p>
+	 * This can be useful for sophisticated pre-read algorithms to quickly
+	 * determine if a file is likely already in cache, especially small
+	 * reftables which may be smaller than a typical DFS block size.
+	 *
+	 * @param key
+	 *            the file to check.
+	 * @return true if block 0 (the first block) is in the cache.
+	 */
+	boolean hasBlock0(DfsStreamKey key);
+
+	/**
+	 * Look up a cached object, creating and loading it if it doesn't exist.
+	 *
+	 * @param file
+	 *            the pack that "contains" the cached object.
+	 * @param position
+	 *            offset within <code>pack</code> of the object.
+	 * @param dfsReader
+	 *            current thread's reader.
+	 * @param fileChannel
+	 *            supplier for channel to read {@code pack}.
+	 * @return the object reference.
+	 * @throws IOException
+	 *             the reference was not in the cache and could not be loaded.
+	 */
+	DfsBlock getOrLoad(BlockBasedFile file, long position, DfsReader dfsReader,
+			DfsBlockCache.ReadableChannelSupplier fileChannel)
+			throws IOException;
+
+	/**
+	 * Look up a cached object, creating and loading it if it doesn't exist.
+	 *
+	 * @param key
+	 *            the stream key of the pack.
+	 * @param position
+	 *            the position in the key. The default should be 0.
+	 * @param loader
+	 *            the function to load the reference.
+	 * @return the object reference.
+	 * @throws IOException
+	 *             the reference was not in the cache and could not be loaded.
+	 */
+	<T> DfsBlockCache.Ref<T> getOrLoadRef(DfsStreamKey key, long position,
+			DfsBlockCache.RefLoader<T> loader) throws IOException;
+
+	void put(DfsBlock v);
+
+	<T> DfsBlockCache.Ref<T> put(DfsStreamKey key, long pos, long size, T v);
+
+	<T> DfsBlockCache.Ref<T> putRef(DfsStreamKey key, long size, T v);
+
+	boolean contains(DfsStreamKey key, long position);
+
+	<T> T get(DfsStreamKey key, long position);
+
+	DfsBlockCacheStats getDfsBlockCacheStats();
+}
