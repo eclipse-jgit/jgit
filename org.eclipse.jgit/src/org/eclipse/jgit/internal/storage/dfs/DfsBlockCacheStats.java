1bf3eecb7c0fe196f4247b28ddeaf636be1b092b
Laura Hamelin <haowl@google.com>
2024-02-27 09:40:24 -0800

    DfsBlockCache: move cache table specific implementations to a new class
    This move of the cache table specific implementation to its own class and
    extracting the method definition to an interface will allow for additional
    reworking of the dfs block cache with the goal of letting users implement their
    own context specific cache tables.
    
    This work does not intend to change how the dfs block cache is accessed,
    limiting the change to DfsBlockCache internal changes only.
    
    Change-Id: Ief6755d1dcf54b4f73f9fe8d22ccb9e2952cb0c4
    Signed-off-by: Laura Hamelin <haowl@google.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCacheStats.java
@@ -0,0 +1,138 @@
+package org.eclipse.jgit.internal.storage.dfs;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.jgit.internal.storage.pack.PackExt;
+
+class DfsBlockCacheStats {
+	/**
+	 * Number of times a block was found in the cache, per pack file extension.
+	 */
+	private final AtomicReference<AtomicLong[]> statHit;
+
+	/**
+	 * Number of times a block was not found, and had to be loaded, per pack
+	 * file extension.
+	 */
+	private final AtomicReference<AtomicLong[]> statMiss;
+
+	/**
+	 * Number of blocks evicted due to cache being full, per pack file
+	 * extension.
+	 */
+	private final AtomicReference<AtomicLong[]> statEvict;
+
+	/**
+	 * Number of bytes currently loaded in the cache, per pack file extension.
+	 */
+	private final AtomicReference<AtomicLong[]> liveBytes;
+
+	DfsBlockCacheStats() {
+		statHit = new AtomicReference<>(newCounters());
+		statMiss = new AtomicReference<>(newCounters());
+		statEvict = new AtomicReference<>(newCounters());
+		liveBytes = new AtomicReference<>(newCounters());
+	}
+
+	void incrementHit(DfsStreamKey key) {
+		getStat(statHit, key).incrementAndGet();
+	}
+
+	void incrementMiss(DfsStreamKey key) {
+		getStat(statMiss, key).incrementAndGet();
+	}
+
+	void incrementEvict(DfsStreamKey key) {
+		getStat(statEvict, key).incrementAndGet();
+	}
+
+	void addToLiveBytes(DfsStreamKey key, long size) {
+		getStat(liveBytes, key).addAndGet(size);
+	}
+
+	long[] getCurrentSize() {
+		return getStatVals(liveBytes);
+	}
+
+	long[] getHitCount() {
+		return getStatVals(statHit);
+	}
+
+	long[] getMissCount() {
+		return getStatVals(statMiss);
+	}
+
+	long[] getTotalRequestCount() {
+		AtomicLong[] hit = statHit.get();
+		AtomicLong[] miss = statMiss.get();
+		long[] cnt = new long[Math.max(hit.length, miss.length)];
+		for (int i = 0; i < hit.length; i++) {
+			cnt[i] += hit[i].get();
+		}
+		for (int i = 0; i < miss.length; i++) {
+			cnt[i] += miss[i].get();
+		}
+		return cnt;
+	}
+
+	long[] getHitRatio() {
+		AtomicLong[] hit = statHit.get();
+		AtomicLong[] miss = statMiss.get();
+		long[] ratio = new long[Math.max(hit.length, miss.length)];
+		for (int i = 0; i < ratio.length; i++) {
+			if (i >= hit.length) {
+				ratio[i] = 0;
+			} else if (i >= miss.length) {
+				ratio[i] = 100;
+			} else {
+				long hitVal = hit[i].get();
+				long missVal = miss[i].get();
+				long total = hitVal + missVal;
+				ratio[i] = total == 0 ? 0 : hitVal * 100 / total;
+			}
+		}
+		return ratio;
+	}
+
+	long[] getEvictions() {
+		return getStatVals(statEvict);
+	}
+
+	private static AtomicLong[] newCounters() {
+		AtomicLong[] ret = new AtomicLong[PackExt.values().length];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = new AtomicLong();
+		}
+		return ret;
+	}
+
+	private static long[] getStatVals(AtomicReference<AtomicLong[]> stat) {
+		AtomicLong[] stats = stat.get();
+		long[] cnt = new long[stats.length];
+		for (int i = 0; i < stats.length; i++) {
+			cnt[i] = stats[i].get();
+		}
+		return cnt;
+	}
+
+	private static AtomicLong getStat(AtomicReference<AtomicLong[]> stats,
+			DfsStreamKey key) {
+		int pos = key.packExtPos;
+		while (true) {
+			AtomicLong[] vals = stats.get();
+			if (pos < vals.length) {
+				return vals[pos];
+			}
+			AtomicLong[] expect = vals;
+			vals = new AtomicLong[Math.max(pos + 1, PackExt.values().length)];
+			System.arraycopy(expect, 0, vals, 0, expect.length);
+			for (int i = expect.length; i < vals.length; i++) {
+				vals[i] = new AtomicLong();
+			}
+			if (stats.compareAndSet(expect, vals)) {
+				return vals[pos];
+			}
+		}
+	}
+}
