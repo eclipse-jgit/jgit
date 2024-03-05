6df1bbff8c37d830bea3cfd0e0c1d4b7ed7897ce
Sam Delmerico <delmerico@google.com>
2024-03-04 16:39:59 -0800

    allow override of PackBitmapIndexBuilder
    To afford ObjectDatabase overrides more control over bitmap index
    writing, PackWriter.prepareBitmapIndex now takes an argument which
    provides a PackBitmapIndexBuilder when given the list of objects in the
    pack.
    
    The current PackBitmapIndexBuilder does xor-compression of the bitmaps
    as they are added in PackBitmapIndexBuilder.processBitmapForWrite. A
    caller can override this behavior by passing their own implementation of
    PackBitmapIndexBuilder.
    
    Change-Id: I7e348c921ac6572f8b6d404bef97c7e57f5fe4fe

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectDatabase.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectDatabase.java
@@ -16,6 +16,8 @@ import java.util.Set;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.internal.storage.file.PackBitmapIndexBuilder;
+import org.eclipse.jgit.internal.storage.pack.PackWriter.PackBitmapIndexBuilderConstructor;
 
 /**
  * Abstraction of arbitrary object storage.
@@ -202,4 +204,15 @@ public abstract class ObjectDatabase implements AutoCloseable {
 	 * @since 6.1
 	 */
 	public abstract long getApproximateObjectCount();
+
+	/**
+	 * Get a functional interface that provides a PackBitmapIndexBuilder when
+	 * given a list of pack objects.
+	 *
+	 * @return a functional interface that does additional processing to newly
+	 *         created bitmaps
+	 */
+	public PackBitmapIndexBuilderConstructor getBitmapIndexBuilderConstructor() {
+		return PackBitmapIndexBuilder::new;
+	}
 }
