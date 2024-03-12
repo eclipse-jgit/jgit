acd14c04578c1f321df3d0e9f0ca97973a1bd8a9
Sam Delmerico <delmerico@google.com>
2024-03-12 12:02:45 -0700

    PackWriter: new PackBitmapIndexBuilder interface
    Change-Id: Ib51f8991cc01813240b1b5cb2417f9850416c98b

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
@@ -58,6 +58,7 @@ import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.SearchForReuseTimeout;
 import org.eclipse.jgit.errors.StoredObjectRepresentationNotAvailableException;
 import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.internal.storage.file.PackBitmapIndex;
 import org.eclipse.jgit.internal.storage.file.PackBitmapIndexBuilder;
 import org.eclipse.jgit.internal.storage.file.PackBitmapIndexWriterV1;
 import org.eclipse.jgit.internal.storage.file.PackIndexWriter;
@@ -67,6 +68,7 @@ import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.AsyncObjectSizeQueue;
 import org.eclipse.jgit.lib.BatchingProgressMonitor;
 import org.eclipse.jgit.lib.BitmapIndex;
+import org.eclipse.jgit.lib.BitmapIndex.Bitmap;
 import org.eclipse.jgit.lib.BitmapIndex.BitmapBuilder;
 import org.eclipse.jgit.lib.BitmapObject;
 import org.eclipse.jgit.lib.Constants;
@@ -97,6 +99,8 @@ import org.eclipse.jgit.transport.PacketLineOut;
 import org.eclipse.jgit.transport.WriteAbortedException;
 import org.eclipse.jgit.util.BlockList;
 import org.eclipse.jgit.util.TemporaryBuffer;
+
+import com.googlecode.javaewah.EWAHCompressedBitmap;
 
 /**
  * <p>
@@ -280,7 +284,7 @@ public class PackWriter implements AutoCloseable {
 
 	private Collection<? extends ObjectId> unshallowObjects;
 
-	private PackBitmapIndexBuilder writeBitmaps;
+	private PackBitmapIndexBuilderForWriting writeBitmaps;
 
 	private CRC32 crc32;
 
@@ -2688,4 +2692,155 @@ public class PackWriter implements AutoCloseable {
 			this.cachedPackUriProvider = cachedPackUriProvider;
 		}
 	}
+
+	/**
+	 * A PackBitmapIndexBuilder that is specifically used for constructing the
+	 * index for later writing to storage.
+	 */
+	public interface PackBitmapIndexBuilderForWriting extends PackBitmapIndex {
+		/**
+		 * Processes a commit and prepares its bitmap to write to the bitmap
+		 * index file.
+		 *
+		 * @param commit
+		 *            the commit for which the bitmap pertains
+		 * @param bitmap
+		 *            the bitmap for the commit
+		 * @param flags
+		 *            flags to pass along to the
+		 *            {@link org.eclipse.jgit.internal.storage.file.BasePackBitmapIndex.StoredBitmap}
+		 *            constructor
+		 */
+		void processBitmapForWrite(BitmapCommit commit, Bitmap bitmap,
+				int flags);
+
+		/**
+		 * Stores the bitmap for the objectId.
+		 *
+		 * @param objectId
+		 *            the object id key for the bitmap.
+		 * @param bitmap
+		 *            bitmap for the object
+		 * @param flags
+		 *            flags to pass along to the
+		 *            {@link org.eclipse.jgit.internal.storage.file.BasePackBitmapIndex.StoredBitmap}
+		 *            constructor
+		 */
+		void addBitmap(AnyObjectId objectId, EWAHCompressedBitmap bitmap,
+				int flags);
+
+		/**
+		 * Remove all the bitmaps entries added.
+		 *
+		 * @param size
+		 *            the expected number of bitmap entries to be written.
+		 */
+		void resetBitmaps(int size);
+
+		ObjectIdOwnerMap<ObjectIdOwnerMap.Entry> getObjectSet();
+
+		/**
+		 * Get the commit object bitmap.
+		 *
+		 * @return the commit object bitmap.
+		 */
+		EWAHCompressedBitmap getCommits();
+
+		/**
+		 * Get the tree object bitmap.
+		 *
+		 * @return the tree object bitmap.
+		 */
+		EWAHCompressedBitmap getTrees();
+
+		/**
+		 * Get the blob object bitmap.
+		 *
+		 * @return the blob object bitmap.
+		 */
+		EWAHCompressedBitmap getBlobs();
+
+		/**
+		 * Get the tag object bitmap.
+		 *
+		 * @return the tag object bitmap.
+		 */
+		EWAHCompressedBitmap getTags();
+
+		/**
+		 * Get list of compressed entries that need to be written.
+		 *
+		 * These entries may be xor-compressed depending on the implementation.
+		 *
+		 * @return a list of the compressed entries.
+		 */
+		List<StoredEntry> getCompressedBitmaps();
+
+		/** Data object for the on disk representation of a bitmap entry. */
+		public static final class StoredEntry {
+			private final long objectId;
+
+			private final EWAHCompressedBitmap bitmap;
+
+			private final int xorOffset;
+
+			private final int flags;
+
+			/**
+			 * @param objectId
+			 *            object id key associated with the bitmap
+			 * @param bitmap
+			 *            bitmap for the object id
+			 * @param xorOffset
+			 *            offset of the StoredEntry against which this entry is
+			 *            xor-compressed
+			 * @param flags
+			 *            flags for the bitmap
+			 */
+			public StoredEntry(long objectId, EWAHCompressedBitmap bitmap,
+					int xorOffset, int flags) {
+				this.objectId = objectId;
+				this.bitmap = bitmap;
+				this.xorOffset = xorOffset;
+				this.flags = flags;
+			}
+
+			/**
+			 * Get the bitmap
+			 *
+			 * @return the bitmap
+			 */
+			public EWAHCompressedBitmap getBitmap() {
+				return bitmap;
+			}
+
+			/**
+			 * Get the xorOffset
+			 *
+			 * @return the xorOffset
+			 */
+			public int getXorOffset() {
+				return xorOffset;
+			}
+
+			/**
+			 * Get the flags
+			 *
+			 * @return the flags
+			 */
+			public int getFlags() {
+				return flags;
+			}
+
+			/**
+			 * Get the ObjectId
+			 *
+			 * @return the ObjectId
+			 */
+			public long getObjectId() {
+				return objectId;
+			}
+		}
+	}
+
 }
