88d355882e3b1f2d0a48b5508320eb449515be63
Xing Huang <xingkhuang@google.com>
2024-04-10 16:44:26 -0500

    NoMergeFilter: adding rewrite support
    ***WORK IN PROCESS***
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: I8c6808daeac4bc1c640160a01df1609816d9bb9b

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/filter/RevFilter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/filter/RevFilter.java
@@ -165,8 +165,10 @@ public abstract class RevFilter {
 
 	private static final class NoMergesFilter extends RevFilter {
 		@Override
-		public boolean include(RevWalk walker, RevCommit c) {
-			return c.getParentCount() < 2;
+		public boolean include(RevWalk walker, RevCommit cmit)
+				throws IOException {
+			return rewriteWrapper(walker, cmit,
+					(w, c) -> c.getParentCount() < 2);
 		}
 
 		@Override
@@ -177,6 +179,11 @@ public abstract class RevFilter {
 		@Override
 		public boolean requiresCommitBody() {
 			return false;
+		}
+
+		@Override
+		public boolean supportParentRewrite() {
+			return true;
 		}
 
 		@Override
