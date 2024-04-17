518fc7dddedbfc439366ecc8aac07738ea5ae0c4
Daniele Sassoli <danielesassoli@gmail.com>
2024-04-17 17:45:33 +0100

    Add support for verification of CMS Signatures
    This is a first step towards supporting CMS Certificate verifications.
    For now only a basic verification of the validity of the certificate
    has been implemented. This includes verifying that:
    - The certificate was in date at the time of the signature.
    - The public key of the certificate corresponds with the signature.
    - The messageDigest of the signature corresponds to the commit content.
    
    This is currently only support for the show command.
    Future steps will include:
    - Verification of the certificate at push time.
    - Verification with the Certificate Authority that the certificate has
    not been revoked.
    
    Bug: jgit-49
    Change-Id: Id768dcd735cee56745c5de1df90b6215312ae47a

---
+++ org.eclipse.jgit.gpg.bc/src/org/eclipse/jgit/gpg/bc/internal/VerificationResult.java
@@ -0,0 +1,78 @@
+package org.eclipse.jgit.gpg.bc.internal;
+
+import org.eclipse.jgit.annotations.NonNull;
+import org.eclipse.jgit.lib.GpgSignatureVerifier;
+
+import java.util.Date;
+
+class VerificationResult implements GpgSignatureVerifier.SignatureVerification {
+
+    private final Date creationDate;
+
+    private final String signer;
+
+    private final String keyUser;
+
+    private final String fingerprint;
+
+    private final boolean verified;
+
+    private final boolean expired;
+
+    private final @NonNull GpgSignatureVerifier.TrustLevel trustLevel;
+
+    private final String message;
+
+    public VerificationResult(Date creationDate, String signer,
+                              String fingerprint, String user, boolean verified,
+                              boolean expired, @NonNull GpgSignatureVerifier.TrustLevel trust, String message) {
+        this.creationDate = creationDate;
+        this.signer = signer;
+        this.fingerprint = fingerprint;
+        this.keyUser = user;
+        this.verified = verified;
+        this.expired = expired;
+        this.trustLevel = trust;
+        this.message = message;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public String getSigner() {
+        return signer;
+    }
+
+    @Override
+    public String getKeyUser() {
+        return keyUser;
+    }
+
+    @Override
+    public String getKeyFingerprint() {
+        return fingerprint;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return expired;
+    }
+
+    @Override
+    public GpgSignatureVerifier.TrustLevel getTrustLevel() {
+        return trustLevel;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean getVerified() {
+        return verified;
+    }
+}
