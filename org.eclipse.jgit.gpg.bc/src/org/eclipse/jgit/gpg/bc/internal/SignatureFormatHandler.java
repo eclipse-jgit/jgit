bd71335ef29bce070804dc4e85541112146ae91a
Luca Milanesio <luca.milanesio@gmail.com>
2024-04-25 14:16:38 +0100

    Extract the GPG-specific logic into a separate signature format handler
    During the review of Id768dcd735c it emerged that the right place for
    adding additional signature formats is inside the current GPG signature
    verifier which would allow to parse the signature only once and
    selecting the correct handler of the signature automatically.
    
    Rename BouncyCastleGpgSignatureVerifier as BouncyCastleSignatureVerifier
    by removing the "Gpg" word in it, so that it will be clearer that it
    supports more than just "Gpg".
    
    The existing class name renames for backward compatibility, but flagged
    as deprecated.
    
    All the internal specifics about Gpg are removed and extracted into a
    separate new class GpgSignatureFormatHandler which is responsible for:
    - Parsing the signature from the raw data
    - Validate the signature against the payload
    
    The introduction of a generic interface SignatureFormatHandler will
    allow to support more signature formats.
    
    Bug: jgit-49
    Change-Id: Ic543ad1c443a6a30be65b2c63495902e48161734

---
+++ org.eclipse.jgit.gpg.bc/src/org/eclipse/jgit/gpg/bc/internal/SignatureFormatHandler.java
@@ -0,0 +1,106 @@
+package org.eclipse.jgit.gpg.bc.internal;
+
+import org.bouncycastle.openpgp.PGPException;
+import org.eclipse.jgit.annotations.NonNull;
+import org.eclipse.jgit.lib.GpgConfig;
+import org.eclipse.jgit.lib.GpgSignatureVerifier;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
+interface SignatureFormatHandler<T> {
+	Optional<ParsedSignature<T>> parseSignature(@NonNull GpgConfig config, byte[] data,
+					byte[] signatureData) throws IOException, PGPException;
+
+	SignatureFormatHandler.VerificationResult verifySignature(byte[] data, BouncyCastleGpgPublicKey publicKey, SignatureFormatHandler.ParsedSignature<T> signature, String fingerprint, String user);
+
+	class ParsedSignature<T> {
+		final Date creationTime;
+		final String keyId;
+		final String fingerprint;
+		final String signer;
+		final T lowLevelSignature;
+
+		public ParsedSignature(Date creationTime, String keyId, String fingerprint, String signer, T lowLevelSignature) {
+			this.creationTime = creationTime;
+			this.keyId = keyId;
+			this.fingerprint = fingerprint;
+			this.signer = signer;
+			this.lowLevelSignature = lowLevelSignature;
+		}
+	}
+
+	class VerificationResult implements GpgSignatureVerifier.SignatureVerification {
+
+		private final Date creationDate;
+
+		private final String signer;
+
+		private final String keyUser;
+
+		private final String fingerprint;
+
+		private final boolean verified;
+
+		private final boolean expired;
+
+		private final @NonNull GpgSignatureVerifier.TrustLevel trustLevel;
+
+		private final String message;
+
+		public VerificationResult(Date creationDate, String signer,
+					  String fingerprint, String user, boolean verified,
+					  boolean expired, @NonNull GpgSignatureVerifier.TrustLevel trust, String message) {
+			this.creationDate = creationDate;
+			this.signer = signer;
+			this.fingerprint = fingerprint;
+			this.keyUser = user;
+			this.verified = verified;
+			this.expired = expired;
+			this.trustLevel = trust;
+			this.message = message;
+		}
+
+		@Override
+		public Date getCreationDate() {
+			return creationDate;
+		}
+
+		@Override
+		public String getSigner() {
+			return signer;
+		}
+
+		@Override
+		public String getKeyUser() {
+			return keyUser;
+		}
+
+		@Override
+		public String getKeyFingerprint() {
+			return fingerprint;
+		}
+
+		@Override
+		public boolean isExpired() {
+			return expired;
+		}
+
+		@Override
+		public GpgSignatureVerifier.TrustLevel getTrustLevel() {
+			return trustLevel;
+		}
+
+		@Override
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public boolean getVerified() {
+			return verified;
+		}
+	}
+
+}
