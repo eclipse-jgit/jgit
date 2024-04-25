9814480adde654d54e3b1b97c9c360b638b0e9b8
Luca Milanesio <luca.milanesio@gmail.com>
2024-04-25 23:34:26 +0100

    Extract the GPG-specific logic into a separate signature format handler
    During the review of Id768dcd735c it emerged that the right place for
    adding additional signature formats is inside the current GPG signature
    verifier which would allow to parse the signature only once and
    selecting the correct handler of the signature automatically.
    
    Rename BouncyCastleGpgSignatureVerifier as BouncyCastleSignatureVerifier
    and its associated Factory, removing the "Gpg" word in it, so that it
    will be clearer that it supports more than just "Gpg".
    
    The existing class name renames for backward compatibility, but flagged
    as deprecated.
    
    All the internal specifics about Gpg are removed and extracted into a
    separate new class GpgSignatureParser which is responsible for:
    - Parsing the signature from the raw data
    - Validate the signature against the payload
    
    The introduction of a generic interface SignatureParser will
    allow to support more signature formats. The selection of the correct
    parser per signature data is performed by the SignatureParserProvider
    which keeps an in-memory map of singletons on a per-signature prefix
    basis.
    
    Change-Id: Ib635baae9cd3ceef54a03eb102d11ea9daa091dd

--- org.eclipse.jgit.gpg.bc/src/org/eclipse/jgit/gpg/bc/internal/BouncyCastleSignatureVerifierFactory.java
+++ org.eclipse.jgit.gpg.bc/src/org/eclipse/jgit/gpg/bc/internal/BouncyCastleSignatureVerifierFactory.java
@@ -7,19 +7,22 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+
 package org.eclipse.jgit.gpg.bc.internal;
 
 import org.eclipse.jgit.lib.GpgSignatureVerifier;
 import org.eclipse.jgit.lib.GpgSignatureVerifierFactory;
 
+
 /**
- * A {@link GpgSignatureVerifierFactory} that creates
- * {@link GpgSignatureVerifier} instances that verify GPG signatures using
+ * A {@link BouncyCastleSignatureVerifierFactory} that creates
+ * {@link GpgSignatureVerifier} instances that verify signatures using
  * BouncyCastle and that do cache public keys.
- *
- * @deprecated Use {{@link BouncyCastleSignatureVerifierFactory} which exposes the same methods
  */
-@Deprecated(since = "6.9.1", forRemoval = true)
-public final class BouncyCastleGpgSignatureVerifierFactory
-		extends BouncyCastleSignatureVerifierFactory {
+public class BouncyCastleSignatureVerifierFactory extends GpgSignatureVerifierFactory {
+
+	@Override
+	public GpgSignatureVerifier getVerifier() {
+		return new BouncyCastleSignatureVerifier();
+	}
 }
