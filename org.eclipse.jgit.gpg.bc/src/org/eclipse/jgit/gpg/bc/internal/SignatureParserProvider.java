5afe77034f19da0591941a7a89786870d3e40715
Luca Milanesio <luca.milanesio@gmail.com>
2024-04-25 23:34:26 +0100

    Extract the GPG-specific logic into a separate signature format handler
    During the review of Id768dcd735c it emerged that the right place for
    adding additional signature formats is inside the current GPG signature
    verifier which would allow to parse the signature only once and
    selecting the correct handler of the signature automatically.
    
    Rename BouncyCastleGpgSignatureVerifier as BouncyCastleSignatureVerifier
    and its associated Factory, removing the "Gpg" word in it, so that it
    will be clearer that it supports more than just "Gpg".
    
    The existing class name renames for backward compatibility, but flagged
    as deprecated.
    
    All the internal specifics about Gpg are removed and extracted into a
    separate new class GpgSignatureParser which is responsible for:
    - Parsing the signature from the raw data
    - Validate the signature against the payload
    
    The introduction of a generic interface SignatureParser will
    allow to support more signature formats. The selection of the correct
    parser per signature data is performed by the SignatureParserProvider
    which keeps an in-memory map of singletons on a per-signature prefix
    basis.
    
    Change-Id: Ib635baae9cd3ceef54a03eb102d11ea9daa091dd

---
+++ org.eclipse.jgit.gpg.bc/src/org/eclipse/jgit/gpg/bc/internal/SignatureParserProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024, GerritForge Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.gpg.bc.internal;
+
+import org.eclipse.jgit.annotations.Nullable;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+class SignatureParserProvider {
+
+	private static Map<String, SignatureParser> signatureParserByPrefixMap = new HashMap<>();
+
+	static void registerSignatureParser(String signaturePrefix, SignatureParser signatureParser) {
+		SignatureParser previousParser = signatureParserByPrefixMap.put(signaturePrefix.trim(), signatureParser);
+		if (previousParser != null) {
+			previousParser.clear();
+		}
+	}
+
+	@Nullable
+	static SignatureParser get(byte[] signatureData) {
+		String signatureString = new String(signatureData, StandardCharsets.UTF_8);
+		int firstLineEnd = signatureString.indexOf('\n');
+		if (firstLineEnd <= 0) {
+			return null;
+		}
+		String prefix = signatureString.substring(0, firstLineEnd).trim();
+		return signatureParserByPrefixMap.get(prefix);
+	}
+
+	public static void clear() {
+		signatureParserByPrefixMap.values().forEach(SignatureParser::clear);
+	}
+}
