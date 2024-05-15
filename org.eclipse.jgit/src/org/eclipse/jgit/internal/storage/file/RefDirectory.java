a9a1c42650fd3da79ef954137825ffcaf920d8bd
Luca Milanesio <luca.milanesio@gmail.com>
2024-05-15 14:55:22 +0100

    [DO NOT MERGE] Avoid computing SHA1 on packed-refs
    For large mono-repos, the packed-refs file size is huge
    and its reading/writing can happen very often, consuming
    significant % of CPU time due to the SHA1 computation of
    its value.
    
    The Git protocol and format does not require any SHA1
    on packed-refs, therefore its computation can be avoided
    altogether and JGit can reply on simple array comparison
    for packed-refs.
    
    The experimental speedup is enabled with a Java
    system property ghs.jgit.packed-ref.skip-sha1=true.
    
    Change-Id: Ib071c1044f7b6af05c63b7fb9a8fe4f9411ab733

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
@@ -26,6 +26,7 @@ import static org.eclipse.jgit.lib.Ref.Storage.NEW;
 import static org.eclipse.jgit.lib.Ref.Storage.PACKED;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -126,6 +127,8 @@ public class RefDirectory extends RefDatabase {
 	private final FileRepository parent;
 
 	private final File gitDir;
+
+	private static final boolean skipPackedRefsSha1 = Boolean.getBoolean("ghs.jgit.packed-ref.skip-sha1");
 
 	final File refsDir;
 
@@ -975,28 +978,48 @@ public class RefDirectory extends RefDatabase {
 
 	private PackedRefList refreshPackedRefs(PackedRefList curList)
 			throws IOException {
-		final PackedRefList newList = readPackedRefs();
-		if (packedRefs.compareAndSet(curList, newList) && !curList.id.equals(
-				newList.id)) {
+		final PackedRefList newList = readPackedRefs(curList);
+		if (packedRefs.compareAndSet(curList, newList) && !curList.equals(
+				newList)) {
 			modCnt.incrementAndGet();
 		}
 		return newList;
 	}
 
-	private PackedRefList readPackedRefs() throws IOException {
+	private PackedRefList readPackedRefs(PackedRefList oldPackedRefs) throws IOException {
 		try {
 			PackedRefList result = FileUtils.readWithRetries(packedRefsFile,
 					f -> {
 						FileSnapshot snapshot = FileSnapshot.save(f);
-						MessageDigest digest = Constants.newMessageDigest();
-						try (BufferedReader br = new BufferedReader(
+
+						if (skipPackedRefsSha1) {
+							byte[] packedRefsContent = Files
+								.readAllBytes(f.toPath());
+							if (oldPackedRefs.hasTheSamePackedRefsBytes(packedRefsContent)) {
+								return oldPackedRefs;
+							}
+
+							try (BufferedReader br = new BufferedReader(
 								new InputStreamReader(
-										new DigestInputStream(
-												new FileInputStream(f), digest),
-										UTF_8))) {
-							return new PackedRefList(parsePackedRefs(br),
+									new ByteArrayInputStream(packedRefsContent),
+									UTF_8))) {
+								return new PackedRefList(parsePackedRefs(br), snapshot,
+									null,
+									packedRefsContent);
+							}
+						} else {
+							MessageDigest digest = Constants.newMessageDigest();
+
+							try (BufferedReader br = new BufferedReader(
+								new InputStreamReader(
+									new DigestInputStream(
+										new FileInputStream(f), digest),
+									UTF_8))) {
+								return new PackedRefList(parsePackedRefs(br),
 									snapshot,
-									ObjectId.fromRaw(digest.digest()));
+									ObjectId.fromRaw(digest.digest()),
+									null);
+							}
 						}
 					});
 			return result != null ? result : NO_PACKED_REFS;
@@ -1010,7 +1033,7 @@ public class RefDirectory extends RefDatabase {
 
 	void compareAndSetPackedRefs(PackedRefList curList, PackedRefList newList) {
 		if (packedRefs.compareAndSet(curList, newList)
-				&& !curList.id.equals(newList.id)) {
+				&& !curList.equals(newList)) {
 			modCnt.incrementAndGet();
 		}
 	}
@@ -1099,9 +1122,10 @@ public class RefDirectory extends RefDatabase {
 				if (!lck.commit())
 					throw new ObjectWritingException(MessageFormat.format(JGitText.get().unableToWrite, name));
 
-				byte[] digest = Constants.newMessageDigest().digest(content);
+				ObjectId packedListSha1 =  skipPackedRefsSha1 ? null :
+					ObjectId.fromRaw(Constants.newMessageDigest().digest(content));
 				PackedRefList newPackedList = new PackedRefList(
-						refs, lck.getCommitSnapshot(), ObjectId.fromRaw(digest));
+						refs, lck.getCommitSnapshot(), packedListSha1, content);
 				packedRefs.compareAndSet(oldPackedList, newPackedList);
 				if (changed) {
 					modCnt.incrementAndGet();
@@ -1454,16 +1478,47 @@ public class RefDirectory extends RefDatabase {
 
 		private final ObjectId id;
 
-		private PackedRefList(RefList<Ref> src, FileSnapshot s, ObjectId i) {
+		private final byte[] packedRefsBytes;
+
+		private PackedRefList(RefList<Ref> src, FileSnapshot s, ObjectId i, byte[] packedRefsBytes) {
 			super(src);
 			snapshot = s;
 			id = i;
+			this.packedRefsBytes = packedRefsBytes;
+		}
+
+		private boolean hasTheSamePackedRefsBytes(byte[] cmpPackedRefsBytes) {
+			return packedRefsBytes != null && cmpPackedRefsBytes != null &&
+				Arrays.equals(packedRefsBytes, cmpPackedRefsBytes);
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			if(!(other instanceof PackedRefList)) {
+				return false;
+			}
+
+			PackedRefList otherPackedRefList = (PackedRefList) other;
+			if(id != null) {
+				return id.equals(otherPackedRefList.id);
+			}
+
+			return hasTheSamePackedRefsBytes(otherPackedRefList.packedRefsBytes);
+		}
+
+		@Override
+		public int hashCode() {
+			if(id != null) {
+				return id.hashCode();
+			}
+
+			return Arrays.hashCode(packedRefsBytes);
 		}
 	}
 
 	private static final PackedRefList NO_PACKED_REFS = new PackedRefList(
 			RefList.emptyList(), FileSnapshot.MISSING_FILE,
-			ObjectId.zeroId());
+			ObjectId.zeroId(), null);
 
 	private static LooseSymbolicRef newSymbolicRef(FileSnapshot snapshot,
 			String name, String target) {
