e46e74ab507dfe497b527d253ceb106029656106
Xing Huang <xingkhuang@google.com>
2024-05-16 14:01:07 -0500

    DfsPackFile: produce the correct Commit Graph based on Bloom Filter flag
    DfsPackFile uses DfsBlockCache as an optimization to cache any loaded
    packExt for serving; only loading things from disk when the designated
    streamKey unable to find data in the block cache. Bloom Filter data
    resides in Commit Graph has its own native git config flag,
    commitGraph.readChangedPaths. Atm, this flag is not respected by
    DfsPackFile, resulting in DfsPackFile continue to serve old Bloom Filter
    data even after the user adjusted the flag state.
    
    Added a subKey for corresponding with the current state of the
    commitGraph.readChangedPaths flag, then use this subKey as part of the
    Commit Graph retrieval from DfsPackFile.
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: I3b7ac99b96c795990ed64d7605ea4ee128bb1c54

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackFile.java
@@ -284,22 +284,35 @@ public final class DfsPackFile extends BlockBasedFile {
 			return null;
 		}
 
+		StoredConfig repoConfig = ctx.db.getRepository().getConfig();
+		boolean readChangedPathFilters = repoConfig.getBoolean(
+				ConfigConstants.CONFIG_COMMIT_GRAPH_SECTION,
+				ConfigConstants.CONFIG_KEY_READ_CHANGED_PATHS, false);
+
+		boolean cpfFlagMismatch = false;
 		if (commitGraph != null) {
-			return commitGraph;
+			boolean containCpf = commitGraph.getChangedPathFilter(0) != null;
+			if (containCpf == readChangedPathFilters) {
+				return commitGraph;
+			}
+			cpfFlagMismatch = true;
 		}
 
-		DfsStreamKey commitGraphKey = desc.getStreamKey(COMMIT_GRAPH);
+		String bloomFilterSubKey = readChangedPathFilters ? "withBf" : "noBf";
+		DfsStreamKey commitGraphKey = desc.getStreamKey(COMMIT_GRAPH,
+				bloomFilterSubKey);
 		AtomicBoolean cacheHit = new AtomicBoolean(true);
 		DfsBlockCache.Ref<CommitGraph> cgref = cache
 				.getOrLoadRef(commitGraphKey, REF_POSITION, () -> {
 					cacheHit.set(false);
-					return loadCommitGraph(ctx, commitGraphKey);
+					return loadCommitGraph(ctx, commitGraphKey,
+							readChangedPathFilters);
 				});
 		if (cacheHit.get()) {
 			ctx.stats.commitGraphCacheHit++;
 		}
 		CommitGraph cg = cgref.get();
-		if (commitGraph == null && cg != null) {
+		if ((commitGraph == null && cg != null) || cpfFlagMismatch) {
 			commitGraph = cg;
 		}
 		ctx.emitIndexLoad(desc, COMMIT_GRAPH, commitGraph);
@@ -1296,13 +1309,10 @@ public final class DfsPackFile extends BlockBasedFile {
 	}
 
 	private DfsBlockCache.Ref<CommitGraph> loadCommitGraph(DfsReader ctx,
-			DfsStreamKey cgkey) throws IOException {
+			DfsStreamKey cgkey, boolean readChangedPathFilters)
+			throws IOException {
 		ctx.stats.readCommitGraph++;
 		long start = System.nanoTime();
-		StoredConfig repoConfig = ctx.db.getRepository().getConfig();
-		boolean readChangedPathFilters = repoConfig.getBoolean(
-				ConfigConstants.CONFIG_COMMIT_GRAPH_SECTION,
-				ConfigConstants.CONFIG_KEY_READ_CHANGED_PATHS, false);
 		try (ReadableChannel rc = ctx.db.openFile(desc, COMMIT_GRAPH)) {
 			long size;
 			CommitGraph cg;
