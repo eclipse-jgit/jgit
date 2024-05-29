5674bf23dc09e5be0d5e418ee10d337967de36b3
Fabio Ponciroli <ponch78@gmail.com>
2024-05-29 20:40:54 +0100

    [DO NOT MERGE] Make RefDirectory batching non-static
    The batching of the updates to RefDirectory needs to be
    on a per-repo (or per-packed-refs) basis and not a shared counter
    across the whole JVM.
    
    Removing the 'static' and making the 'lastPackRefsWrite' a final
    AtomicReference<> does not impact the batching and keeps
    the repo-specific batching mechanism with the same level
    of performance.
    
    Change-Id: Ie510540be474fa8cd10efe3539c92afeca8c7a7c

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/RefDirectory.java
@@ -185,8 +185,8 @@ public class RefDirectory extends RefDatabase {
 	 * filesystem during a read operation.
 	 */
 	private final AtomicInteger modCnt = new AtomicInteger();
-	private static final Map<String, AtomicInteger> refsPackingCnt = new ConcurrentHashMap<>();
-	private static Map<String, Instant> lastPackRefsWrite = new ConcurrentHashMap<>();
+	private final AtomicInteger refsPackingCnt = new AtomicInteger();
+	private final AtomicReference<Instant> lastPackRefsWrite = new AtomicReference<>();
 
 	/**
 	 * Last {@link #modCnt} that we sent to listeners.
@@ -894,6 +894,10 @@ public class RefDirectory extends RefDatabase {
 
 
 	private boolean isDifferenceMoreThanOneSecond(Instant instant1, Instant instant2) {
+		if(instant1 == null || instant2 == null) {
+			return true;
+		}
+
 		Duration duration = Duration.between(instant1, instant2);
 		return duration.getSeconds() > 1;
 	}
@@ -1171,14 +1175,11 @@ public class RefDirectory extends RefDatabase {
 
 				if(batchPackedRefsWrite) {
 				  Instant currentTime = Instant.now();
-				  String packedRefFilePath = packedRefsFile.getAbsolutePath();
-				  if (refsPackingCnt.computeIfAbsent(packedRefFilePath, (key) -> new AtomicInteger())
-						.incrementAndGet() % 20 != 0
-							&& !isDifferenceMoreThanOneSecond(lastPackRefsWrite
-								.computeIfAbsent(packedRefFilePath, (key) -> currentTime), currentTime)) {
+				  if (refsPackingCnt.incrementAndGet() % 20 != 0
+							&& !isDifferenceMoreThanOneSecond(lastPackRefsWrite.getAndSet(currentTime), currentTime)) {
 						return;
 					}
-					lastPackRefsWrite.put(packedRefFilePath,currentTime);
+					lastPackRefsWrite.set(currentTime);
 				}
 				lck.setFSync(true);
 				lck.setNeedSnapshot(true);
