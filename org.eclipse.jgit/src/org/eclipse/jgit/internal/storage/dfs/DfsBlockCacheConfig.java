c9afcb11bcf4a99a3acdc9409628053db550c13f
Laura Hamelin <haowl@google.com>
2024-06-07 16:12:18 -0700

    dfs: add PackExtBlockCacheTables configuration
    Generates DfsPackExtBlockCacheTables according to configurations
    defined in [core "dfs.*"] git config sections.  The current
    pack ext cache tables implementation supports the same parameters
    the ClockBlockCacheTable (current default implementation).
    
    Format:
    
    [core "dfs.packCache"]
      packExtensions = "PACK"
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
    
    [core "dfs.multipleExtensionCache"]
      packExtensions = "INDEX REFTABLE BITMAP_INDEX"
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
    
    Change-Id: I0e534e6d78b684832e3d3d269cee2590aa0f1911

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCacheConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsBlockCacheConfig.java
@@ -11,18 +11,26 @@
 package org.eclipse.jgit.internal.storage.dfs;
 
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_CORE_SECTION;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_DFS_CACHE_PREFIX;
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_DFS_SECTION;
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_BLOCK_LIMIT;
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_BLOCK_SIZE;
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_CONCURRENCY_LEVEL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_PACK_EXTENSIONS;
 import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_STREAM_RATIO;
 
 import java.text.MessageFormat;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
+import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.internal.storage.pack.PackExt;
 import org.eclipse.jgit.lib.Config;
@@ -42,14 +50,21 @@ public class DfsBlockCacheConfig {
 	public static final int DEFAULT_CACHE_HOT_MAX = 1;
 
 	private long blockLimit;
+
 	private int blockSize;
+
 	private double streamRatio;
+
 	private int concurrencyLevel;
 
 	private Consumer<Long> refLock;
+
 	private Map<PackExt, Integer> cacheHotMap;
 
 	private IndexEventConsumer indexEventConsumer;
+
+	@Nullable
+	private List<DfsBlockCachePackExtConfig> packExtCacheConfigurations = null;
 
 	/**
 	 * Create a default configuration.
@@ -77,10 +92,10 @@ public class DfsBlockCacheConfig {
 	 * Set maximum number bytes of heap memory to dedicate to caching pack file
 	 * data.
 	 * <p>
-	 * It is strongly recommended to set the block limit to be an integer multiple
-	 * of the block size. This constraint is not enforced by this method (since
-	 * it may be called before {@link #setBlockSize(int)}), but it is enforced by
-	 * {@link #fromConfig(Config)}.
+	 * It is strongly recommended to set the block limit to be an integer
+	 * multiple of the block size. This constraint is not enforced by this
+	 * method (since it may be called before {@link #setBlockSize(int)}), but it
+	 * is enforced by {@link #fromConfig(Config)}.
 	 *
 	 * @param newLimit
 	 *            maximum number bytes of heap memory to dedicate to caching
@@ -89,9 +104,9 @@ public class DfsBlockCacheConfig {
 	 */
 	public DfsBlockCacheConfig setBlockLimit(long newLimit) {
 		if (newLimit <= 0) {
-			throw new IllegalArgumentException(MessageFormat.format(
-					JGitText.get().blockLimitNotPositive,
-					Long.valueOf(newLimit)));
+			throw new IllegalArgumentException(
+					MessageFormat.format(JGitText.get().blockLimitNotPositive,
+							Long.valueOf(newLimit)));
 		}
 		blockLimit = newLimit;
 		return this;
@@ -239,59 +254,68 @@ public class DfsBlockCacheConfig {
 		return this;
 	}
 
+	@Nullable
+	DfsBlockCacheConfig setPackExtCacheConfigurations(
+			List<DfsBlockCachePackExtConfig> packExtCacheConfigurations) {
+		this.packExtCacheConfigurations = packExtCacheConfigurations;
+		return this;
+	}
+
+	@Nullable
+	List<DfsBlockCachePackExtConfig> getPackExtCacheConfigurations() {
+		return packExtCacheConfigurations;
+	}
+
 	/**
 	 * Update properties by setting fields from the configuration.
 	 * <p>
 	 * If a property is not defined in the configuration, then it is left
 	 * unmodified.
 	 * <p>
-	 * Enforces certain constraints on the combination of settings in the config,
-	 * for example that the block limit is a multiple of the block size.
+	 * Enforces certain constraints on the combination of settings in the
+	 * config, for example that the block limit is a multiple of the block size.
 	 *
 	 * @param rc
 	 *            configuration to read properties from.
 	 * @return {@code this}
 	 */
 	public DfsBlockCacheConfig fromConfig(Config rc) {
-		long cfgBlockLimit = rc.getLong(
-				CONFIG_CORE_SECTION,
-				CONFIG_DFS_SECTION,
-				CONFIG_KEY_BLOCK_LIMIT,
-				getBlockLimit());
-		int cfgBlockSize = rc.getInt(
-				CONFIG_CORE_SECTION,
-				CONFIG_DFS_SECTION,
-				CONFIG_KEY_BLOCK_SIZE,
+		DfsBlockCacheConfig config = fromConfig(CONFIG_CORE_SECTION,
+				CONFIG_DFS_SECTION, rc);
+		if (rc.getSubsections(CONFIG_CORE_SECTION).stream().anyMatch(
+				section -> section.startsWith(CONFIG_DFS_CACHE_PREFIX))) {
+			config.setPackExtCacheConfigurations(
+					DfsBlockCachePackExtConfig.fromConfigs(rc));
+		}
+		return config;
+	}
+
+	private DfsBlockCacheConfig fromConfig(String section, String subSection,
+			Config rc) {
+		long cfgBlockLimit = rc.getLong(section, subSection,
+				CONFIG_KEY_BLOCK_LIMIT, getBlockLimit());
+		int cfgBlockSize = rc.getInt(section, subSection, CONFIG_KEY_BLOCK_SIZE,
 				getBlockSize());
 		if (cfgBlockLimit % cfgBlockSize != 0) {
 			throw new IllegalArgumentException(MessageFormat.format(
 					JGitText.get().blockLimitNotMultipleOfBlockSize,
-					Long.valueOf(cfgBlockLimit),
-					Long.valueOf(cfgBlockSize)));
+					Long.valueOf(cfgBlockLimit), Long.valueOf(cfgBlockSize)));
 		}
 
 		setBlockLimit(cfgBlockLimit);
 		setBlockSize(cfgBlockSize);
 
-		setConcurrencyLevel(rc.getInt(
-				CONFIG_CORE_SECTION,
-				CONFIG_DFS_SECTION,
-				CONFIG_KEY_CONCURRENCY_LEVEL,
-				getConcurrencyLevel()));
+		setConcurrencyLevel(rc.getInt(section, subSection,
+				CONFIG_KEY_CONCURRENCY_LEVEL, getConcurrencyLevel()));
 
-		String v = rc.getString(
-				CONFIG_CORE_SECTION,
-				CONFIG_DFS_SECTION,
-				CONFIG_KEY_STREAM_RATIO);
+		String v = rc.getString(section, subSection, CONFIG_KEY_STREAM_RATIO);
 		if (v != null) {
 			try {
 				setStreamRatio(Double.parseDouble(v));
 			} catch (NumberFormatException e) {
 				throw new IllegalArgumentException(MessageFormat.format(
-						JGitText.get().enumValueNotSupported3,
-						CONFIG_CORE_SECTION,
-						CONFIG_DFS_SECTION,
-						CONFIG_KEY_STREAM_RATIO, v), e);
+						JGitText.get().enumValueNotSupported3, section,
+						subSection, CONFIG_KEY_STREAM_RATIO, v), e);
 			}
 		}
 		return this;
@@ -344,6 +368,92 @@ public class DfsBlockCacheConfig {
 		 */
 		default boolean shouldReportEvictedEvent() {
 			return false;
+		}
+	}
+
+	/**
+	 * Type that holds the mapping of multiple pack extension to a single block
+	 * cache config. This mapping is especially useful when configuring multiple
+	 * pack extension types use the same block cache instance.
+	 * <p>
+	 * <code>
+	 * The current pack ext cache tables implementation supports the same
+	 * parameters the ClockBlockCacheTable (current default implementation).
+	 *
+	 * Format:
+	 * [core "dfs.packCache"]
+	 *   packExtensions = "PACK"
+	 *   blockSize = 512
+	 *   blockLimit = 100
+	 *   concurrencyLevel = 5
+	 *
+	 * [core "dfs.multipleExtensionCache"]
+	 *   packExtensions = "INDEX REFTABLE BITMAP_INDEX"
+	 *   blockSize = 512
+	 *   blockLimit = 100
+	 *   concurrencyLevel = 5
+	 * </code>
+	 */
+	static class DfsBlockCachePackExtConfig {
+		// Set of pack extensions that will map to the cache instance.
+		private final Set<PackExt> packExts;
+
+		// Configuration for the cache instance.
+		private final DfsBlockCacheConfig packExtCacheConfiguration;
+
+		DfsBlockCachePackExtConfig(Set<PackExt> packExts,
+				DfsBlockCacheConfig packExtCacheConfiguration) {
+			this.packExts = packExts;
+			this.packExtCacheConfiguration = packExtCacheConfiguration;
+		}
+
+		Set<PackExt> getPackExts() {
+			return packExts;
+		}
+
+		DfsBlockCacheConfig getPackExtCacheConfiguration() {
+			return packExtCacheConfiguration;
+		}
+
+		private static List<DfsBlockCachePackExtConfig> fromConfigs(
+				Config config) {
+			Set<String> extensionsSeen = new HashSet<>();
+			List<DfsBlockCachePackExtConfig> cacheConfigs = new ArrayList<>();
+			List<String> subSections = config
+					.getSubsections(CONFIG_CORE_SECTION).stream()
+					.filter(section -> section
+							.startsWith(CONFIG_DFS_CACHE_PREFIX))
+					.collect(Collectors.toList());
+			for (String subSection : subSections) {
+				String[] extensions = config.getString(CONFIG_CORE_SECTION,
+						subSection, CONFIG_KEY_PACK_EXTENSIONS).split(" ");
+				Set<PackExt> packExts = new HashSet<>(extensions.length);
+				for (String extension : extensions) {
+					if (extensionsSeen.contains(extension)) {
+						throw new IllegalArgumentException(MessageFormat.format(
+								JGitText.get().duplicatePackExtensionsSet,
+								CONFIG_CORE_SECTION, subSection,
+								CONFIG_KEY_PACK_EXTENSIONS, extension));
+					}
+					extensionsSeen.add(extension);
+					try {
+						packExts.add(PackExt.valueOf(extension));
+					} catch (IllegalArgumentException e) {
+						throw new IllegalArgumentException(
+								MessageFormat.format(
+										JGitText.get().unknownPackExtension,
+										CONFIG_CORE_SECTION, subSection,
+										CONFIG_KEY_PACK_EXTENSIONS, extension),
+								e);
+					}
+				}
+				DfsBlockCacheConfig dfsBlockCacheConfig = new DfsBlockCacheConfig();
+				dfsBlockCacheConfig.fromConfig(CONFIG_CORE_SECTION, subSection,
+						config);
+				cacheConfigs.add(new DfsBlockCachePackExtConfig(packExts,
+						dfsBlockCacheConfig));
+			}
+			return cacheConfigs;
 		}
 	}
 }
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -77,8 +77,12 @@ public final class ConfigConstants {
 	/** The "dfs" section */
 	public static final String CONFIG_DFS_SECTION = "dfs";
 
+	/** The dfs cache subsection prefix */
+	public static final String CONFIG_DFS_CACHE_PREFIX = "dfs.";
+
 	/**
 	 * The "receive" section
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_RECEIVE_SECTION = "receive";
@@ -97,6 +101,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "rebase" section
+	 * 
 	 * @since 3.2
 	 */
 	public static final String CONFIG_REBASE_SECTION = "rebase";
@@ -106,6 +111,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "repack" section
+	 * 
 	 * @since 5.13.3
 	 */
 	public static final String CONFIG_REPACK_SECTION = "repack";
@@ -122,12 +128,14 @@ public final class ConfigConstants {
 
 	/**
 	 * The "pull" section
+	 * 
 	 * @since 3.5
 	 */
 	public static final String CONFIG_PULL_SECTION = "pull";
 
 	/**
 	 * The "merge" section
+	 * 
 	 * @since 4.9
 	 */
 	public static final String CONFIG_MERGE_SECTION = "merge";
@@ -162,24 +170,28 @@ public final class ConfigConstants {
 
 	/**
 	 * The "filter" section
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_FILTER_SECTION = "filter";
 
 	/**
 	 * The "gpg" section
+	 * 
 	 * @since 5.2
 	 */
 	public static final String CONFIG_GPG_SECTION = "gpg";
 
 	/**
 	 * The "protocol" section
+	 * 
 	 * @since 5.9
 	 */
 	public static final String CONFIG_PROTOCOL_SECTION = "protocol";
 
 	/**
 	 * The "format" key
+	 * 
 	 * @since 5.2
 	 */
 	public static final String CONFIG_KEY_FORMAT = "format";
@@ -200,6 +212,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "commit" section
+	 * 
 	 * @since 5.2
 	 */
 	public static final String CONFIG_COMMIT_SECTION = "commit";
@@ -255,6 +268,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "quotePath" key.
+	 * 
 	 * @since 5.6
 	 */
 	public static final String CONFIG_KEY_QUOTE_PATH = "quotePath";
@@ -267,18 +281,21 @@ public final class ConfigConstants {
 
 	/**
 	 * The "auto" key
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_KEY_AUTO = "auto";
 
 	/**
 	 * The "autogc" key
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_KEY_AUTOGC = "autogc";
 
 	/**
 	 * The "autopacklimit" key
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_KEY_AUTOPACKLIMIT = "autopacklimit";
@@ -331,8 +348,12 @@ public final class ConfigConstants {
 	/** The "deltaBaseCacheLimit" key */
 	public static final String CONFIG_KEY_DELTA_BASE_CACHE_LIMIT = "deltaBaseCacheLimit";
 
+	/** The "packExtensions" key */
+	public static final String CONFIG_KEY_PACK_EXTENSIONS = "packExtensions";
+
 	/**
 	 * The "symlinks" key
+	 * 
 	 * @since 3.3
 	 */
 	public static final String CONFIG_KEY_SYMLINKS = "symlinks";
@@ -352,36 +373,42 @@ public final class ConfigConstants {
 
 	/**
 	 * The "packedGitMmap" key
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_KEY_PACKED_GIT_MMAP = "packedgitmmap";
 
 	/**
 	 * The "packedGitWindowSize" key
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_KEY_PACKED_GIT_WINDOWSIZE = "packedgitwindowsize";
 
 	/**
 	 * The "packedGitLimit" key
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_KEY_PACKED_GIT_LIMIT = "packedgitlimit";
 
 	/**
 	 * The "packedGitOpenFiles" key
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_KEY_PACKED_GIT_OPENFILES = "packedgitopenfiles";
 
 	/**
 	 * The "packedGitUseStrongRefs" key
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_KEY_PACKED_GIT_USE_STRONGREFS = "packedgitusestrongrefs";
 
 	/**
 	 * The "packedIndexGitUseStrongRefs" key
+	 * 
 	 * @since 6.7
 	 */
 	public static final String CONFIG_KEY_PACKED_INDEX_GIT_USE_STRONGREFS = "packedindexgitusestrongrefs";
@@ -420,6 +447,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "autostash" key
+	 * 
 	 * @since 3.2
 	 */
 	public static final String CONFIG_KEY_AUTOSTASH = "autostash";
@@ -442,10 +470,14 @@ public final class ConfigConstants {
 	 */
 	public static final String CONFIG_KEY_ALWAYS = "always";
 
-	/** The "never" key (used to configure {@link #CONFIG_KEY_AUTOSETUPREBASE} */
+	/**
+	 * The "never" key (used to configure {@link #CONFIG_KEY_AUTOSETUPREBASE}
+	 */
 	public static final String CONFIG_KEY_NEVER = "never";
 
-	/** The "local" key (used to configure {@link #CONFIG_KEY_AUTOSETUPREBASE} */
+	/**
+	 * The "local" key (used to configure {@link #CONFIG_KEY_AUTOSETUPREBASE}
+	 */
 	public static final String CONFIG_KEY_LOCAL = "local";
 
 	/** The "createchangeid" key */
@@ -462,6 +494,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "ignore" key
+	 * 
 	 * @since 3.6
 	 */
 	public static final String CONFIG_KEY_IGNORE = "ignore";
@@ -474,18 +507,21 @@ public final class ConfigConstants {
 
 	/**
 	 * The "skiphash" key
+	 * 
 	 * @since 5.13.2
 	 */
 	public static final String CONFIG_KEY_SKIPHASH = "skiphash";
 
 	/**
 	 * The "hidedotfiles" key
+	 * 
 	 * @since 3.5
 	 */
 	public static final String CONFIG_KEY_HIDEDOTFILES = "hidedotfiles";
 
 	/**
 	 * The "dirnogitlinks" key
+	 * 
 	 * @since 4.3
 	 */
 	public static final String CONFIG_KEY_DIRNOGITLINKS = "dirNoGitLinks";
@@ -498,6 +534,7 @@ public final class ConfigConstants {
 
 	/**
 	 * The "prunepackexpire" key
+	 * 
 	 * @since 4.3
 	 */
 	public static final String CONFIG_KEY_PRUNEPACKEXPIRE = "prunepackexpire";
@@ -518,12 +555,14 @@ public final class ConfigConstants {
 
 	/**
 	 * The "aggressiveDepth" key
+	 * 
 	 * @since 3.6
 	 */
 	public static final String CONFIG_KEY_AGGRESSIVE_DEPTH = "aggressiveDepth";
 
 	/**
 	 * The "aggressiveWindow" key
+	 * 
 	 * @since 3.6
 	 */
 	public static final String CONFIG_KEY_AGGRESSIVE_WINDOW = "aggressiveWindow";
@@ -550,12 +589,14 @@ public final class ConfigConstants {
 
 	/**
 	 * The "renamelimit" key in the "diff" section
+	 * 
 	 * @since 3.0
 	 */
 	public static final String CONFIG_KEY_RENAMELIMIT = "renamelimit";
 
 	/**
 	 * The "trustfolderstat" key in the "core" section
+	 * 
 	 * @since 3.6
 	 */
 	public static final String CONFIG_KEY_TRUSTFOLDERSTAT = "trustfolderstat";
@@ -576,49 +617,58 @@ public final class ConfigConstants {
 
 	/**
 	 * The "noprefix" key in the "diff" section
+	 * 
 	 * @since 3.0
 	 */
 	public static final String CONFIG_KEY_NOPREFIX = "noprefix";
 
 	/**
 	 * A "renamelimit" value in the "diff" section
+	 * 
 	 * @since 3.0
 	 */
 	public static final String CONFIG_RENAMELIMIT_COPY = "copy";
 
 	/**
 	 * A "renamelimit" value in the "diff" section
+	 * 
 	 * @since 3.0
 	 */
 	public static final String CONFIG_RENAMELIMIT_COPIES = "copies";
 
 	/**
 	 * The "renames" key in the "diff" section
+	 * 
 	 * @since 3.0
 	 */
 	public static final String CONFIG_KEY_RENAMES = "renames";
 
 	/**
-	 * The "inCoreLimit" key in the "merge" section. It's a size limit (bytes) used to
-	 * control a file to be stored in {@code Heap} or {@code LocalFile} during the merge.
+	 * The "inCoreLimit" key in the "merge" section. It's a size limit (bytes)
+	 * used to control a file to be stored in {@code Heap} or {@code LocalFile}
+	 * during the merge.
+	 * 
 	 * @since 4.9
 	 */
 	public static final String CONFIG_KEY_IN_CORE_LIMIT = "inCoreLimit";
 
 	/**
 	 * The "prune" key
+	 * 
 	 * @since 3.3
 	 */
 	public static final String CONFIG_KEY_PRUNE = "prune";
 
 	/**
 	 * The "streamBuffer" key
+	 * 
 	 * @since 4.0
 	 */
 	public static final String CONFIG_KEY_STREAM_BUFFER = "streamBuffer";
 
 	/**
 	 * The "streamRatio" key
+	 * 
 	 * @since 4.0
 	 */
 	public static final String CONFIG_KEY_STREAM_RATIO = "streamRatio";
@@ -626,30 +676,35 @@ public final class ConfigConstants {
 	/**
 	 * Flag in the filter section whether to use JGit's implementations of
 	 * filters and hooks
+	 * 
 	 * @since 4.6
 	 */
 	public static final String CONFIG_KEY_USEJGITBUILTIN = "useJGitBuiltin";
 
 	/**
 	 * The "fetchRecurseSubmodules" key
+	 * 
 	 * @since 4.7
 	 */
 	public static final String CONFIG_KEY_FETCH_RECURSE_SUBMODULES = "fetchRecurseSubmodules";
 
 	/**
 	 * The "recurseSubmodules" key
+	 * 
 	 * @since 4.7
 	 */
 	public static final String CONFIG_KEY_RECURSE_SUBMODULES = "recurseSubmodules";
 
 	/**
 	 * The "required" key
+	 * 
 	 * @since 4.11
 	 */
 	public static final String CONFIG_KEY_REQUIRED = "required";
 
 	/**
 	 * The "lfs" section
+	 * 
 	 * @since 4.11
 	 */
 	public static final String CONFIG_SECTION_LFS = "lfs";
@@ -677,22 +732,24 @@ public final class ConfigConstants {
 
 	/**
 	 * The "filesystem" section
+	 * 
 	 * @since 5.1.9
 	 */
 	public static final String CONFIG_FILESYSTEM_SECTION = "filesystem";
 
 	/**
 	 * The "timestampResolution" key
+	 * 
 	 * @since 5.1.9
 	 */
 	public static final String CONFIG_KEY_TIMESTAMP_RESOLUTION = "timestampResolution";
 
 	/**
 	 * The "minRacyThreshold" key
+	 * 
 	 * @since 5.1.9
 	 */
 	public static final String CONFIG_KEY_MIN_RACY_THRESHOLD = "minRacyThreshold";
-
 
 	/**
 	 * The "refStorage" key
@@ -710,42 +767,49 @@ public final class ConfigConstants {
 
 	/**
 	 * The extensions.refStorage key
+	 * 
 	 * @since 5.7
 	 */
 	public static final String CONFIG_KEY_REFSTORAGE = "refStorage";
 
 	/**
 	 * The "reftable" refStorage format
+	 * 
 	 * @since 5.7
 	 */
 	public static final String CONFIG_REF_STORAGE_REFTABLE = "reftable";
 
 	/**
 	 * The "jmx" section
+	 * 
 	 * @since 5.1.13
 	 */
 	public static final String CONFIG_JMX_SECTION = "jmx";
 
 	/**
 	 * The "pack.bigfilethreshold" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BIGFILE_THRESHOLD = "bigfilethreshold";
 
 	/**
 	 * The "pack.bitmapContiguousCommitCount" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BITMAP_CONTIGUOUS_COMMIT_COUNT = "bitmapcontiguouscommitcount";
 
 	/**
 	 * The "pack.bitmapDistantCommitSpan" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BITMAP_DISTANT_COMMIT_SPAN = "bitmapdistantcommitspan";
 
 	/**
 	 * The "pack.bitmapExcessiveBranchCount" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BITMAP_EXCESSIVE_BRANCH_COUNT = "bitmapexcessivebranchcount";
@@ -759,18 +823,21 @@ public final class ConfigConstants {
 
 	/**
 	 * The "pack.bitmapExcludedRefsPrefixes" key
+	 * 
 	 * @since 5.13.2
 	 */
 	public static final String CONFIG_KEY_BITMAP_EXCLUDED_REFS_PREFIXES = "bitmapexcludedrefsprefixes";
 
 	/**
 	 * The "pack.bitmapInactiveBranchAgeInDays" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BITMAP_INACTIVE_BRANCH_AGE_INDAYS = "bitmapinactivebranchageindays";
 
 	/**
 	 * The "pack.bitmapRecentCommitSpan" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BITMAP_RECENT_COMMIT_COUNT = "bitmaprecentcommitspan";
@@ -784,84 +851,98 @@ public final class ConfigConstants {
 
 	/**
 	 * The "pack.buildBitmaps" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_BUILD_BITMAPS = "buildbitmaps";
 
 	/**
 	 * The "pack.cutDeltaChains" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_CUT_DELTACHAINS = "cutdeltachains";
 
 	/**
 	 * The "pack.deltaCacheLimit" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_DELTA_CACHE_LIMIT = "deltacachelimit";
 
 	/**
 	 * The "pack.deltaCacheSize" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_DELTA_CACHE_SIZE = "deltacachesize";
 
 	/**
 	 * The "pack.deltaCompression" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_DELTA_COMPRESSION = "deltacompression";
 
 	/**
 	 * The "pack.depth" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_DEPTH = "depth";
 
 	/**
 	 * The "pack.minSizePreventRacyPack" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_MIN_SIZE_PREVENT_RACYPACK = "minsizepreventracypack";
 
 	/**
 	 * The "pack.reuseDeltas" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_REUSE_DELTAS = "reusedeltas";
 
 	/**
 	 * The "pack.reuseObjects" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_REUSE_OBJECTS = "reuseobjects";
 
 	/**
 	 * The "pack.singlePack" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_SINGLE_PACK = "singlepack";
 
 	/**
 	 * The "pack.threads" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_THREADS = "threads";
 
 	/**
 	 * The "pack.waitPreventRacyPack" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_WAIT_PREVENT_RACYPACK = "waitpreventracypack";
 
 	/**
 	 * The "pack.window" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_WINDOW = "window";
 
 	/**
 	 * The "pack.windowMemory" key
+	 * 
 	 * @since 5.8
 	 */
 	public static final String CONFIG_KEY_WINDOW_MEMORY = "windowmemory";
