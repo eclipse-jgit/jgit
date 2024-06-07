9e91df512f743624d808306806ab7dc38fc09894
Laura Hamelin <haowl@google.com>
2024-06-07 16:12:18 -0700

    DfsBlockCacheConfig: add PackExtBlockCacheTable configuration
    Generates PackExtBlockCacheTable according to configurations
    defined in [core "dfs.*"] git config sections. The current
    pack ext cache tables implementation supports the same parameters
    the ClockBlockCacheTable (current default implementation).
    
    Configuration falls back to the defaults defined in the
    DfsBlockCacheConfig.java file when not set on each cache
    table configuration.
    
    Format for individual cache tables:
    
    [core "dfs"] // Configuration for the "default" cache table.
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
      (...)
    
    [core "dfs.multipleExtensionCache"]
      packExtensions = "INDEX REFTABLE BITMAP_INDEX"
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
      (...)
    
    [core "dfs.packCache"]
      packExtensions = "PACK"
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
      (...)
    
    [core "dfs.multipleExtensionCache"]
      packExtensions = "INDEX REFTABLE BITMAP_INDEX"
      blockSize = 512
      blockLimit = 100
      concurrencyLevel = 5
      (...)
    
    Change-Id: I0e534e6d78b684832e3d3d269cee2590aa0f1911

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -315,6 +315,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String downloadCancelled;
 	/***/ public String downloadCancelledDuringIndexing;
 	/***/ public String duplicateAdvertisementsOf;
+	/***/ public String duplicatePackExtensionsSet;
 	/***/ public String duplicateRef;
 	/***/ public String duplicateRefAttribute;
 	/***/ public String duplicateRemoteRefUpdateIsIllegal;
@@ -569,6 +570,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String noMergeHeadSpecified;
 	/***/ public String nonBareLinkFilesNotSupported;
 	/***/ public String nonCommitToHeads;
+	/***/ public String noPackExtConfigurationGiven;
 	/***/ public String noPathAttributesFound;
 	/***/ public String noSuchRef;
 	/***/ public String noSuchRefKnown;
@@ -858,6 +860,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String unknownObjectInIndex;
 	/***/ public String unknownObjectType;
 	/***/ public String unknownObjectType2;
+	/***/ public String unknownPackExtension;
 	/***/ public String unknownPositionEncoding;
 	/***/ public String unknownRefStorageFormat;
 	/***/ public String unknownRepositoryFormat;
