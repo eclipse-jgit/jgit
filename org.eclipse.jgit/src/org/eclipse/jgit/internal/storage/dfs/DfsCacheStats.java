cd449a9a8a6135fda785c982305640f82b4a269f
Laura Hamelin <haowl@google.com>
2024-06-10 13:42:03 -0700

    dfs: use PackExt BlockCacheTables in DfsBlockCache
    Adds the usage of DfsPackExtBlockCacheTables to the
    DfsBlockCache, replacing the current DfsBlockCacheTable
    when PackExtCacheConfigurations exists.
    When no PackExtCacheConfigurations exists the current
    DfsBlockCacheTable implementation will be used.
    
    Change-Id: I42222a0cb43785baba907a49077dd9874d19d891

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsCacheStats.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024, Google LLC and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.dfs;
+
+import java.util.Arrays;
+
+import org.eclipse.jgit.internal.storage.pack.PackExt;
+
+/**
+ * Class holding the array of pack extension stats values returned by the Block
+ * Cache tables. Offers utilities for manipulating multiple {@code long[]}
+ * arrays of the same {@code long[PackExt.values().length]} format.
+ */
+class DfsCacheStats {
+	private final long[] values;
+
+	DfsCacheStats() {
+		values = emptyPackStats();
+	}
+
+	DfsCacheStats(long[] values) {
+		this.values = Arrays.copyOf(values, values.length);
+	}
+
+	long[] getValues() {
+		return values;
+	}
+
+	void add(long[] otherValues) {
+		for (int i = 0; i < values.length && i < otherValues.length; i++) {
+			values[i] += otherValues[i];
+		}
+	}
+
+	private static long[] emptyPackStats() {
+		long[] values = new long[PackExt.values().length];
+		for (int i = 0; i < PackExt.values().length; i++) {
+			values[i] = 0;
+		}
+		return values;
+	}
+}
