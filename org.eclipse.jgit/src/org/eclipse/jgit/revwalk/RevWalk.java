a81f51957ce55b9728330c35ace9b26285749b85
Xing Huang <xingkhuang@google.com>
2024-06-11 14:42:49 -0500

    RevWalk: Store RevFilter metrics within a new RevFilterStats class
    TreeRevFilter is an internally created RevFilter by StartGenerator when
    the invoker wants to filter commits by a TreeFilter. The
    ChangedPathsFilter metrics stored within TreeRevFilter are impossible
    for the invoker to get since the invoker does not hold any reference to
    the TreeRevFilter.
    
    Since all RevFilters have access to RevWalk via the default Include()
    method, creating a RevFilterStats as a data container within Revwalk to
    store and provide access to any metrics generated by RevFilters.
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: Ib8eff35e8e4f88efd4cb7ad40e5b603a4dbf50ff

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
@@ -206,6 +206,8 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 	private boolean firstParent;
 
+	private RevFilterStats revFilterStats;
+
 	boolean shallowCommitsInitialized;
 
 	private enum GetMergedIntoStrategy {
@@ -248,6 +250,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		treeFilter = TreeFilter.ALL;
 		this.closeReader = closeReader;
 		commitGraph = null;
+		revFilterStats = new RevFilterStats();
 	}
 
 	static AbstractRevQueue newDateRevQueue(boolean firstParent) {
@@ -833,6 +836,15 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 	boolean getRewriteParents() {
 		return rewriteParents;
+	}
+
+	/**
+	 * Get stats recorded within the RevFilter used in the RevWalk.
+	 *
+	 * @return {@link RevFilterStats} with stats recorded by RevFilters.
+	 */
+	public RevFilterStats getRevFilterStats() {
+		return revFilterStats;
 	}
 
 	/**
@@ -1617,6 +1629,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		roots.clear();
 		queue = newDateRevQueue(firstParent);
 		pending = new StartGenerator(this);
+		revFilterStats = new RevFilterStats();
 	}
 
 	/**
@@ -1639,6 +1652,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 		queue = newDateRevQueue(firstParent);
 		pending = new StartGenerator(this);
 		shallowCommitsInitialized = false;
+		revFilterStats = new RevFilterStats();
 	}
 
 	/**
@@ -1827,6 +1841,121 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 			} else {
 				lookupCommit(id).parents = RevCommit.NO_PARENTS;
 			}
+		}
+	}
+
+	/**
+	 * Statistics related RevFilter collected during the lifecycle of RevWalk.
+	 */
+	public static class RevFilterStats {
+
+		private long changedPathFilterTruePositive = 0;
+
+		private long changedPathFilterFalsePositive = 0;
+
+		private long changedPathFilterNegative = 0;
+
+		private long numCommitsThroughTreeRevFilter = 0;
+
+		private long numTreesParsedInTreeRevFilter = 0;
+
+		private RevFilterStats() {
+		}
+
+		/**
+		 * Increment the changedPathFilterTruePositive count
+		 * @since 7.0
+		 */
+		public void incrementChangedPathFilterTruePositive() {
+			changedPathFilterTruePositive++;
+		}
+
+		/**
+		 * Increment the changedPathFilterFalsePositive count
+		 * @since 7.0
+		 */
+		public void incrementChangedPathFilterFalsePositive() {
+			changedPathFilterFalsePositive++;
+		}
+
+		/**
+		 * Increment the changedPathFilterNegative count
+		 * @since 7.0
+		 */
+		public void incrementChangedPathFilterNegative() {
+			changedPathFilterNegative++;
+		}
+
+		/**
+		 * Increment the numCommitsThroughTreeRevFilter count
+		 * @since 7.0
+		 */
+		public void incrementCommitsThroughTreeRevFilter() {
+			numCommitsThroughTreeRevFilter++;
+		}
+
+		/**
+		 * Increment the numTreesParsedInTreeRevFilter count
+		 * @param numTrees
+		 * 			number of trees parsed
+		 * @since 7.0
+		 */
+		public void incrementNumTreesParsedInTreeRevFilter(int numTrees) {
+			numTreesParsedInTreeRevFilter += numTrees;
+		}
+
+		/**
+		 * Return how many times a changed path filter correctly predicted that
+		 * a path was changed in a commit, for statistics gathering purposes.
+		 *
+		 * @return count of true positives
+		 * @since 7.0
+		 */
+		public long getChangedPathFilterTruePositive() {
+			return changedPathFilterTruePositive;
+		}
+
+		/**
+		 * Return how many times a changed path filter wrongly predicted that a
+		 * path was changed in a commit, for statistics gathering purposes.
+		 *
+		 * @return count of false positives
+		 * @since 7.0
+		 */
+		public long getChangedPathFilterFalsePositive() {
+			return changedPathFilterFalsePositive;
+		}
+
+		/**
+		 * Return how many times a changed path filter predicted that a path was
+		 * not changed in a commit (allowing that commit to be skipped), for
+		 * statistics gathering purposes.
+		 *
+		 * @return count of negatives
+		 * @since 7.0
+		 */
+		public long getChangedPathFilterNegative() {
+			return changedPathFilterNegative;
+		}
+
+		/**
+		 * Return how many times a commit was evaluated by treeRevFilter
+		 *
+		 * @return count of treeRevFilter include calls
+		 * @since 7.0
+		 */
+		public long getNumCommitsThroughTreeRevFilter() {
+			return numCommitsThroughTreeRevFilter;
+		}
+
+		/**
+		 * Return how many times a tree was parsed within TreeRevFilter
+		 *
+		 * @return count of trees parsed within TreeRevFilter
+		 * @since 7.0
+		 */
+		public long getNumTreesParsedInTreeRevFilter() {
+			return numTreesParsedInTreeRevFilter;
 		}
 	}
 }
