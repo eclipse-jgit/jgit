5caf6c4c0d8b732b832a84a91f60aefb8fc8936c
Xing Huang <xingkhuang@google.com>
2024-06-11 14:42:49 -0500

    RevWalk: Store RevFilter metrics within a new RevFilterStats class
    TreeRevFilter is an internally created RevFilter by StartGenerator when
    the invoker wants to filter commits by a TreeFilter. The
    ChangedPathsFilter metrics stored within TreeRevFilter are impossible
    for the invoker to get since the invoker does not hold any reference to
    the TreeRevFilter.
    
    Since all RevFilters have access to RevWalk via the default Include()
    method, creating a RevFilterStats as a data container within Revwalk to
    store and provide access to any metrics generated by RevFilters.
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: Ib8eff35e8e4f88efd4cb7ad40e5b603a4dbf50ff

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TreeRevFilter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TreeRevFilter.java
@@ -49,12 +49,6 @@ public class TreeRevFilter extends RevFilter {
 
 	private final AtomicBoolean changedPathFilterUsed = new AtomicBoolean();
 
-	private long changedPathFilterTruePositive = 0;
-
-	private long changedPathFilterFalsePositive = 0;
-
-	private long changedPathFilterNegative = 0;
-
 	/**
 	 * Create a {@link org.eclipse.jgit.revwalk.filter.RevFilter} from a
 	 * {@link org.eclipse.jgit.treewalk.filter.TreeFilter}.
@@ -110,6 +104,7 @@ public class TreeRevFilter extends RevFilter {
 			throws StopWalkException, MissingObjectException,
 			IncorrectObjectTypeException, IOException {
 		c.flags |= FILTER_APPLIED;
+		walker.getRevFilterStats().incrementCommitsThroughTreeRevFilter();
 		// Reset the tree filter to scan this commit and parents.
 		//
 		RevCommit[] pList = c.getParents();
@@ -125,6 +120,8 @@ public class TreeRevFilter extends RevFilter {
 		}
 		trees[nParents] = c.getTree();
 		tw.reset(trees);
+		walker.getRevFilterStats()
+				.incrementNumTreesParsedInTreeRevFilter(trees.length);
 
 		if (nParents == 1) {
 			// We have exactly one parent. This is a very common case.
@@ -144,14 +141,17 @@ public class TreeRevFilter extends RevFilter {
 				}
 				if (this.changedPathFilterUsed.getAndSet(false)) {
 					if (chgs > 0) {
-						changedPathFilterTruePositive++;
+						walker.getRevFilterStats()
+								.incrementChangedPathFilterTruePositive();
 					} else {
-						changedPathFilterFalsePositive++;
+						walker.getRevFilterStats()
+								.incrementChangedPathFilterFalsePositive();
 					}
 				}
 			} else {
 				if (this.changedPathFilterUsed.getAndSet(false)) {
-					changedPathFilterNegative++;
+					walker.getRevFilterStats()
+							.incrementChangedPathFilterNegative();
 				}
 			}
 
@@ -267,40 +267,6 @@ public class TreeRevFilter extends RevFilter {
 	@Override
 	public boolean requiresCommitBody() {
 		return false;
-	}
-
-	/**
-	 * Return how many times a changed path filter correctly predicted that a
-	 * path was changed in a commit, for statistics gathering purposes.
-	 *
-	 * @return count of true positives
-	 * @since 6.7
-	 */
-	public long getChangedPathFilterTruePositive() {
-		return changedPathFilterTruePositive;
-	}
-
-	/**
-	 * Return how many times a changed path filter wrongly predicted that a path
-	 * was changed in a commit, for statistics gathering purposes.
-	 *
-	 * @return count of false positives
-	 * @since 6.7
-	 */
-	public long getChangedPathFilterFalsePositive() {
-		return changedPathFilterFalsePositive;
-	}
-
-	/**
-	 * Return how many times a changed path filter predicted that a path was not
-	 * changed in a commit (allowing that commit to be skipped), for statistics
-	 * gathering purposes.
-	 *
-	 * @return count of negatives
-	 * @since 6.7
-	 */
-	public long getChangedPathFilterNegative() {
-		return changedPathFilterNegative;
 	}
 
 	private void updateFollowFilter(ObjectId[] trees, DiffConfig cfg,
