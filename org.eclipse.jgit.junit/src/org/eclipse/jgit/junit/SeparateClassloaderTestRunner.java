44f45a10d433ad72f550a175a71c0424031a3b2d
Ivan Frade <ifrade@google.com>
2024-07-08 14:21:45 -0700

    [errorprone] SeparateClassloadertTestRunner: use #split(String,int)
    Errorprone recommends to use String.split(String, int) as it has a
    less surprising behaviour with empty entries.
    
    https: //errorprone.info/bugpattern/StringSplitter
    Change-Id: I48a01ee18d66bbb4a177aee576629dc5132d4a38

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/SeparateClassloaderTestRunner.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/SeparateClassloaderTestRunner.java
@@ -44,7 +44,7 @@ public class SeparateClassloaderTestRunner extends BlockJUnit4ClassRunner {
 		try {
 			String pathSeparator = System.getProperty("path.separator");
 			String[] classPathEntries = System.getProperty("java.class.path")
-					.split(pathSeparator);
+					.split(pathSeparator, -1);
 			URL[] urls = new URL[classPathEntries.length];
 			for (int i = 0; i < classPathEntries.length; i++) {
 				urls[i] = Paths.get(classPathEntries[i]).toUri().toURL();
