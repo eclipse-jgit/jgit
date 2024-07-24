70766ea7cfb0e80428155d28c133761912529e34
Laura Hamelin <haowl@google.com>
2024-07-24 14:01:38 -0700

    dfs: add method to get all underlying block cache table stats
    Make available all underlying cache table stats for the used cache table
    implementation.
    
    The existing cache table stats implementation only allows a "global"
    view of the cache table statistics; it does not differentiate between
    all possible underlying cache tables used.
    
    This allows callers to get the block cache stats broken down per
    underlying table. These cache stats are intended to be used for
    monitoring all cache tables independently.
    
    Change-Id: I261b3f2849857172397657e5c674b11e09807f27

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/PackExtBlockCacheTable.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/PackExtBlockCacheTable.java
@@ -200,6 +200,13 @@ class PackExtBlockCacheTable implements DfsBlockCacheTable {
 		return name;
 	}
 
+	@Override
+	public List<BlockCacheStats> getAllBlockCacheStats() {
+		return blockCacheTableList.stream().flatMap(
+				cacheTable -> cacheTable.getAllBlockCacheStats().stream())
+				.collect(Collectors.toList());
+	}
+
 	private DfsBlockCacheTable getTable(PackExt packExt) {
 		return extBlockCacheTables.getOrDefault(packExt,
 				defaultBlockCacheTable);
