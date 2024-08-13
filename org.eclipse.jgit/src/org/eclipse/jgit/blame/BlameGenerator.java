1b1292c352172e40a2bd210bb1a50cd331b4b6b6
Xing Huang <xingkhuang@google.com>
2024-08-13 16:26:03 -0500

    BlameGenerator: introduce Commit Graph based performance boosts
    BlameGenerator inflates the body all returning commits with its result()
    method, so there's no need to parse the body in majority of the
    iterating commits. Since the RevWalk is self-contained within the
    BlameGenerator and no customization to it can be done, we don't have to
    worry about a filter that might require commit bodies. By turning off
    the retainBody flag, all commits will be prioritized to be loaded from
    Commit Graph first. Additionally, we can take advantage of
    ChangedPathFilter within PathAnyDiffFilter to quickly skip commits that
    did not modify the file.
    
    If useCommitGraphOptimizations is true, disable RetainBody and use
    ChangedPathFilter. Added a blameGeneratorStats class to track the
    numbers of object parsing done within BlameGenerator and its
    ChangedPathFilter usage.
    
    Signed-off-by: Xing Huang <xingkhuang@google.com>
    Change-Id: I2c3bbd632cdc2d2d7ad011a4994ff7c9b6f73608

--- org.eclipse.jgit/src/org/eclipse/jgit/blame/BlameGenerator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/blame/BlameGenerator.java
@@ -58,6 +58,7 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.TreeWalk.OperationType;
+import org.eclipse.jgit.treewalk.filter.PathAnyDiffFilter;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.eclipse.jgit.util.IO;
@@ -143,6 +144,8 @@ public class BlameGenerator implements AutoCloseable {
 	private boolean useCache = true;
 
 	private final Stats stats = new Stats();
+
+	private boolean useCommitGraphOptimizations;
 
 	/**
 	 * Create a blame generator for the repository and path (relative to
@@ -267,6 +270,23 @@ public class BlameGenerator implements AutoCloseable {
 	}
 
 	/**
+	 * Enable Commit Graph related optimizations.
+	 * <p>
+	 * If true, all commits will be parsed from Commit Graph if Commit Graph is
+	 * available. Use ChangedPathFilter if available.
+	 *
+	 * @param useCommitGraph
+	 *            set useCommitGraphOptimizations.
+	 * @return {@code this}
+	 */
+	public BlameGenerator setUseCommitGraphOptimizations(
+			boolean useCommitGraph) {
+		useCommitGraphOptimizations = useCommitGraph;
+		revPool.setRetainBody(!useCommitGraph);
+		return this;
+	}
+
+	/**
 	 * Obtain the RenameDetector, allowing the application to configure its
 	 * settings for rename score and breaking behavior.
 	 *
@@ -291,9 +311,11 @@ public class BlameGenerator implements AutoCloseable {
 	 * Enable/disable the use of cache (if present). Enabled by default.
 	 * <p>
 	 * If caller need source line numbers, the generator cannot use the cache
-	 * (source lines are not there). Use this method to disable the cache in that case.
+	 * (source lines are not there). Use this method to disable the cache in
+	 * that case.
 	 *
-	 * @param useCache should this generator use the cache.
+	 * @param useCache
+	 *            should this generator use the cache.
 	 */
 	public void setUseCache(boolean useCache) {
 		this.useCache = useCache;
@@ -476,6 +498,7 @@ public class BlameGenerator implements AutoCloseable {
 					resultPath);
 			c.sourceBlob = id.toObjectId();
 			c.sourceText = new RawText(ldr.getCachedBytes(Integer.MAX_VALUE));
+			stats.blobsParsed++;
 			c.regionList = new Region(0, 0, c.sourceText.size());
 			remaining = c.sourceText.size();
 			push(c);
@@ -489,6 +512,7 @@ public class BlameGenerator implements AutoCloseable {
 		Candidate c = new Candidate(getRepository(), commit, resultPath);
 		c.sourceBlob = idBuf.toObjectId();
 		c.loadText(reader);
+		stats.blobsParsed++;
 		c.regionList = new Region(0, 0, c.sourceText.size());
 		remaining = c.sourceText.size();
 		push(c);
@@ -574,6 +598,7 @@ public class BlameGenerator implements AutoCloseable {
 				resultPath);
 		c.sourceBlob = idBuf.toObjectId();
 		c.loadText(reader);
+		stats.blobsParsed++;
 		c.regionList = new Region(0, 0, c.sourceText.size());
 		remaining = c.sourceText.size();
 		push(c);
@@ -646,13 +671,14 @@ public class BlameGenerator implements AutoCloseable {
 			Candidate n = pop();
 			if (n == null)
 				return done();
+
 			stats.candidatesVisited += 1;
 			if (blameCache != null && useCache) {
 				List<CacheRegion> cachedBlame = blameCache.get(repository,
 						n.sourceCommit, n.sourcePath.getPath());
 				if (cachedBlame != null) {
-					BlameRegionMerger rb = new BlameRegionMerger(repository, revPool,
-							cachedBlame);
+					BlameRegionMerger rb = new BlameRegionMerger(repository,
+							revPool, cachedBlame);
 					Candidate fullyBlamed = rb.mergeCandidate(n);
 					if (fullyBlamed != null) {
 						stats.cacheHit = true;
@@ -770,20 +796,60 @@ public class BlameGenerator implements AutoCloseable {
 			return split(n.getNextCandidate(0), n);
 		revPool.parseHeaders(parent);
 
-		if (find(parent, n.sourcePath)) {
-			if (idBuf.equals(n.sourceBlob))
+		boolean changedPathFilterUsed = false;
+		if (useCommitGraphOptimizations) {
+			RevCommit c = n.sourceCommit;
+			String path = n.sourcePath.getPath();
+			PathAnyDiffFilter pathAnyDiffFilter = PathAnyDiffFilter
+					.create(path);
+			boolean mightHaveChangedFile = pathAnyDiffFilter.shouldTreeWalk(c,
+					revPool);
+			changedPathFilterUsed = c.has(RevFlag.CHANGED_PATHS_FILTER_APPLIED);
+			if (changedPathFilterUsed) {
+				c.remove(RevFlag.CHANGED_PATHS_FILTER_APPLIED);
+			}
+			if (!mightHaveChangedFile) {
+				// commit didn't change the file or renamed the file.
+				stats.changedPathFilterNegative++;
 				return blameEntireRegionOnParent(n, parent);
+			}
+		}
+
+		// parent has access to the same file
+		if (find(parent, n.sourcePath)) {
+			if (idBuf.equals(n.sourceBlob)) {
+				// no change made by the commit
+				if (changedPathFilterUsed) {
+					stats.changedPathFilterFalsePositive++;
+				}
+				return blameEntireRegionOnParent(n, parent);
+			}
+			// some change made to the file by the commit
+			if (changedPathFilterUsed) {
+				stats.changedPathFilterTruePositive++;
+			}
 			return splitBlameWithParent(n, parent);
 		}
 
+		// candidate isn't a commit
 		if (n.sourceCommit == null)
 			return result(n);
 
+		// parent does not have access to the file
 		DiffEntry r = findRename(parent, n.sourceCommit, n.sourcePath);
-		if (r == null)
+		if (r == null) {
+			// commit did not rename the file, so commit created this file
+			if (changedPathFilterUsed) {
+				stats.changedPathFilterTruePositive++;
+			}
 			return result(n);
+		}
 
 		if (0 == r.getOldId().prefixCompare(n.sourceBlob)) {
+			// commit renamed the file but no content change
+			if (changedPathFilterUsed) {
+				stats.changedPathFilterTruePositive++;
+			}
 			// A 100% rename without any content change can also
 			// skip directly to the parent.
 			n.sourceCommit = parent;
@@ -792,11 +858,16 @@ public class BlameGenerator implements AutoCloseable {
 			return false;
 		}
 
+		// commit renamed the file and made content change
+		if (changedPathFilterUsed) {
+			stats.changedPathFilterTruePositive++;
+		}
 		Candidate next = n.create(getRepository(), parent,
 				PathFilter.create(r.getOldPath()));
 		next.sourceBlob = r.getOldId().toObjectId();
 		next.renameScore = r.getScore();
 		next.loadText(reader);
+		stats.blobsParsed++;
 		return split(next, n);
 	}
 
@@ -812,6 +883,7 @@ public class BlameGenerator implements AutoCloseable {
 		Candidate next = n.create(getRepository(), parent, n.sourcePath);
 		next.sourceBlob = idBuf.toObjectId();
 		next.loadText(reader);
+		stats.blobsParsed++;
 		return split(next, n);
 	}
 
@@ -915,6 +987,7 @@ public class BlameGenerator implements AutoCloseable {
 				editList = new EditList(0);
 			} else {
 				p.loadText(reader);
+				stats.blobsParsed++;
 				editList = diffAlgorithm.diff(textComparator, p.sourceText,
 						n.sourceText);
 			}
@@ -1169,6 +1242,7 @@ public class BlameGenerator implements AutoCloseable {
 	private boolean find(RevCommit commit, PathFilter path) throws IOException {
 		treeWalk.setFilter(path);
 		treeWalk.reset(commit.getTree());
+		stats.treesParsed++;
 		if (treeWalk.next() && isFile(treeWalk.getRawMode(0))) {
 			treeWalk.getObjectId(idBuf, 0);
 			return true;
@@ -1187,6 +1261,7 @@ public class BlameGenerator implements AutoCloseable {
 
 		treeWalk.setFilter(TreeFilter.ANY_DIFF);
 		treeWalk.reset(parent.getTree(), commit.getTree());
+		stats.treesParsed += 2;
 		List<DiffEntry> diffs = DiffEntry.scan(treeWalk);
 		FilteredRenameDetector filteredRenameDetector = new FilteredRenameDetector(
 				renameDetector);
@@ -1206,11 +1281,20 @@ public class BlameGenerator implements AutoCloseable {
 	 * Stats about the work done by the generator
 	 */
 	public static class Stats {
-
 		/** Candidates taken from the queue */
 		private int candidatesVisited;
 
 		private boolean cacheHit;
+
+		private int blobsParsed;
+
+		private int treesParsed;
+
+		private int changedPathFilterNegative;
+
+		private int changedPathFilterTruePositive;
+
+		private int changedPathFilterFalsePositive;
 
 		/**
 		 * Number of candidates taken from the queue
@@ -1234,5 +1318,58 @@ public class BlameGenerator implements AutoCloseable {
 			return cacheHit;
 		}
 
+		/**
+		 * Number of blobs parsed
+		 *
+		 * @return number of blobs parsed
+		 */
+		public int getBlobsParsed() {
+			return blobsParsed;
+		}
+
+		/**
+		 * Number of trees parsed
+		 *
+		 * @return number of trees parsed
+		 *
+		 * @since 7.2
+		 */
+		public int getTreesParsed() {
+			return treesParsed;
+		}
+
+		/**
+		 * Times the changedPathFilter said the commit does not touch a path
+		 * 
+		 * @return count of times the changed path filter returned false
+		 * @since 7.2
+		 */
+		public int getChangedPathFilterNegative() {
+			return changedPathFilterNegative;
+		}
+
+		/**
+		 * Times the changedPathFilter said the commit does contain a path, and
+		 * it was true.
+		 *
+		 * @return count of times the changed path filter returned true and
+		 *         later it was right.
+		 * @since 7.2
+		 */
+		public int getChangedPathFilterTruePositive() {
+			return changedPathFilterTruePositive;
+		}
+
+		/**
+		 * Times the changedPathFilter said the commit does contains a path, and
+		 * later it was not true.
+		 *
+		 * @return count of times the changed path filter returned true and
+		 *         later was wrong.
+		 * @since 7.2
+		 */
+		public int getChangedPathFilterFalsePositive() {
+			return changedPathFilterFalsePositive;
+		}
 	}
 }
