d6ce390a8450e405c0ef42664165a480a4cf3dfe
Antonio Barone <syntonyze@gmail.com>
2024-08-29 16:18:48 +0200

    Add `repackAndGenerateBitmap()` api
    Introduce a new `repackAndGenerateBitmap` method to allow repacking only
    the heads packfile along with its bitmap.
    
    Allow to improve git operations performance by providing a bitmap file
    without having to pay the price of a full GC (which would also repack
    non-heads refs).
    
    In order to speed up the bitmap generation even further, leverage the
    `quickMatchSearchForReuse` config so that search for reuse phase stops
    scanning upon finding the first matching object representation,
    potentially sacrificing finding the best representation for efficiency.
    
    Change-Id: I8c553fc96af3ffc758a8055b5e966e4f81962872

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/GC.java
@@ -788,6 +788,97 @@ public class GC {
 		}
 	}
 
+
+	/**
+	 * Packs all objects which reachable from any of the heads into one pack
+	 * file, always generating the associated bitmap.
+	 * Differently from the {@link #repack()} method, this method does not
+	 * repack non-heads refs and does not perform pruning.
+	 * Note that the repacking is always executed with the `quickMatchSearchForReuse` enabled, to minimize execution time.
+	 *
+	 * @return a collection of the newly created pack files
+	 * @throws java.io.IOException when during reading of refs, index, packfiles, objects,
+	 *                             reflog-entries or during writing to the packfiles
+	 *                             {@link java.io.IOException} occurs
+	 */
+	public Collection<Pack> repackAndGenerateBitmap() throws IOException {
+		Collection<Ref> refsBefore = getAllRefs();
+
+		Set<ObjectId> allHeadsAndTags = new HashSet<>();
+		Set<ObjectId> allHeads = new HashSet<>();
+		Set<ObjectId> allTags = new HashSet<>();
+		Set<ObjectId> nonHeads = new HashSet<>();
+		Set<ObjectId> tagTargets = new HashSet<>();
+		Set<ObjectId> indexObjects = listNonHEADIndexObjects();
+
+		Set<ObjectId> refsToExcludeFromBitmap = repo.getRefDatabase()
+				.getRefsByPrefix(pconfig.getBitmapExcludedRefsPrefixes())
+				.stream().map(Ref::getObjectId).collect(Collectors.toSet());
+
+		for (Ref ref : refsBefore) {
+			checkCancelled();
+			nonHeads.addAll(listRefLogObjects(ref, 0));
+			if (ref.isSymbolic() || ref.getObjectId() == null) {
+				continue;
+			}
+			if (isHead(ref)) {
+				allHeads.add(ref.getObjectId());
+			} else if (isTag(ref)) {
+				allTags.add(ref.getObjectId());
+			} else {
+				nonHeads.add(ref.getObjectId());
+			}
+			if (ref.getPeeledObjectId() != null) {
+				tagTargets.add(ref.getPeeledObjectId());
+			}
+		}
+
+		List<ObjectIdSet> excluded = new LinkedList<>();
+		for (Pack p : repo.getObjectDatabase().getPacks()) {
+			checkCancelled();
+			if (!shouldPackKeptObjects() && p.shouldBeKept()) {
+				excluded.add(p.getIndex());
+			}
+		}
+
+		// Don't exclude tags that are also branch tips
+		allTags.removeAll(allHeads);
+		allHeadsAndTags.addAll(allHeads);
+		allHeadsAndTags.addAll(allTags);
+
+		// Hoist all branch tips and tags earlier in the pack file
+		tagTargets.addAll(allHeadsAndTags);
+		nonHeads.addAll(indexObjects);
+
+		//TODO: is this needed? I think it should be removed
+		// Combine the GC_REST objects into the GC pack if requested
+		if (pconfig.getSinglePack()) {
+			allHeadsAndTags.addAll(nonHeads);
+			nonHeads.clear();
+		}
+
+		List<Pack> ret = new ArrayList<>(2);
+		Pack heads = null;
+		if (!allHeadsAndTags.isEmpty()) {
+			//  stops scanning upon finding the first matching object representation, making search for
+			// reuse faster.
+			pconfig.setQuickMatchSearchForReuse(true);
+			heads = writePack(allHeadsAndTags, PackWriter.NONE, allTags,
+					refsToExcludeFromBitmap, tagTargets, excluded, true);
+
+			//TODO: Should this go?
+			if (heads != null) {
+				ret.add(heads);
+				excluded.add(0, heads.getIndex());
+			}
+		}
+
+		deleteTempPacksIdx();
+
+		return ret;
+	}
+
+
 	/**
 	 * Packs all objects which reachable from any of the heads into one pack
 	 * file. Additionally all objects which are not reachable from any head but
