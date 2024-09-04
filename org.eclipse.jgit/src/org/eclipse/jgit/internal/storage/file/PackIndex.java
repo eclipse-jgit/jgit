7b24f50d6dc7417d7392d398236798c6e5c28cb0
jackdt@google.com <jackdt@google.com>
2024-09-04 16:07:23 -0700

    Change MutableEntry to be an abstract class with protected visibility.
    The previous version could only be subclassed by package-local classes,
    because the offset field and ensureId method were package visible.
    
    I also made the classes static, which involved simplifying the
    EntriesIterator class and moving the logic for next() into
    MutableEntry.readNext. This also allowed me to remove the ensureId
    method.
    
    Change-Id: I35010d1f80237e421dd51b8d3d61a8ecb03e0d01

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackIndex.java
@@ -301,10 +301,11 @@ public interface PackIndex
 	 * in pack (both mutable).
 	 *
 	 */
-	class MutableEntry {
-		final MutableObjectId idBuffer = new MutableObjectId();
-
-		long offset;
+	abstract class MutableEntry {
+		/** Buffer of the ObjectId visited by the EntriesIterator. */
+		protected final MutableObjectId idBuffer = new MutableObjectId();
+		/** Offset into the packfile of the current object. */
+		protected long offset;
 
 		/**
 		 * Returns offset for this index object entry
@@ -321,7 +322,6 @@ public interface PackIndex
 		 * @return hex string describing the object id of this entry.
 		 */
 		public String name() {
-			ensureId();
 			return idBuffer.name();
 		}
 
@@ -331,7 +331,6 @@ public interface PackIndex
 		 * @return a copy of the object id.
 		 */
 		public ObjectId toObjectId() {
-			ensureId();
 			return idBuffer.toObjectId();
 		}
 
@@ -341,32 +340,48 @@ public interface PackIndex
 		 * @return a complete copy of this entry, that won't modify
 		 */
 		public MutableEntry cloneEntry() {
-			final MutableEntry r = new MutableEntry();
-			ensureId();
+			final MutableEntry r = new MutableEntry() {
+				@Override
+				protected void readNext() {}
+			};
 			r.idBuffer.fromObjectId(idBuffer);
 			r.offset = offset;
 			return r;
 		}
 
-		void ensureId() {
-			// Override in implementations.
-		}
+		/**
+		 * Advances the internal iterator to the next object.
+		 */
+		protected abstract void readNext();
 	}
 
 	/**
 	 * Base implementation of the iterator over index entries.
 	 */
 	abstract class EntriesIterator implements Iterator<MutableEntry> {
+                /** Subclasses need to fill the buffer when calling next(). */
 		protected final MutableEntry entry = initEntry();
 
 		private final long objectCount;
 
+                /**
+                 * Default constructor.
+                 *
+                 * @param objectCount the number of objects in the PackFile.
+                 */
 		protected EntriesIterator(long objectCount) {
 			this.objectCount = objectCount;
 		}
 
+                /** Counts number of entries accessed so far. */
 		protected long returnedNumber = 0;
 
+                /**
+                 * Initializes the buffer. Subclasses override this to provide
+                 * implementation specific buffers.
+                 *
+                 * @return the MutableEntry buffer used by this Iterator.
+                 */
 		protected abstract MutableEntry initEntry();
 
 		@Override
@@ -379,7 +394,11 @@ public interface PackIndex
 		 * element.
 		 */
 		@Override
-		public abstract MutableEntry next();
+		public MutableEntry next() {
+			entry.readNext();
+			returnedNumber++;
+			return entry;
+		}
 
 		@Override
 		public void remove() {
