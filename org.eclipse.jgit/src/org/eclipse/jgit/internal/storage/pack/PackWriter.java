d886d1716a6e0c0d20f300cae2ae1c83fd32e721
Sam Delmerico <delmerico@google.com>
2024-10-07 14:34:03 -0700

    PackWriter: writeIndex takes an PackIndexWriter
    PackWriter.writeIndex now takes an interface which will perform
    the writing of the index to an arbitrary backing storage.
    
    Change-Id: I582d2f3d25d6adb2da243d6d0d7bc405a97d6183

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
@@ -118,7 +118,7 @@ import org.eclipse.jgit.util.TemporaryBuffer;
  * {@link #preparePack(ProgressMonitor, Set, Set)}, and streaming with
  * {@link #writePack(ProgressMonitor, ProgressMonitor, OutputStream)}. If the
  * pack is being stored as a file the matching index can be written out after
- * writing the pack by {@link #writeIndex(OutputStream)}. An optional bitmap
+ * writing the pack by {@link #writeIndex(PackIndexWriter)}. An optional bitmap
  * index can be made by calling {@link #prepareBitmapIndex(ProgressMonitor)}
  * followed by {@link #writeBitmapIndex(PackBitmapIndexWriter)}.
  * </p>
@@ -1092,19 +1092,33 @@ public class PackWriter implements AutoCloseable {
 	 * Writing an index is only required for local pack storage. Packs sent on
 	 * the network do not need to create an index.
 	 *
-	 * @param indexStream
-	 *            output for the index data. Caller is responsible for closing
-	 *            this stream.
+	 * @param os
+	 *            an @code{OutputStream} to write the index
 	 * @throws java.io.IOException
 	 *             the index data could not be written to the supplied stream.
 	 */
-	public void writeIndex(OutputStream indexStream) throws IOException {
+	public void writeIndex(OutputStream os) throws IOException {
+		writeIndex(BasePackIndexWriter.createVersion(os, getIndexVersion()));
+	}
+
+	/**
+	 * Create an index file to match the pack file just written.
+	 * <p>
+	 * Called after
+	 * {@link #writePack(ProgressMonitor, ProgressMonitor, OutputStream)}.
+	 * <p>
+	 * Writing an index is only required for local pack storage. Packs sent on
+	 * the network do not need to create an index.
+	 *
+	 * @param iw
+	 *            an @code{IPackIndexWriter} instance to write the index
+	 * @throws java.io.IOException
+	 *             the index data could not be written to the supplied stream.
+	 */
+	public void writeIndex(PackIndexWriter iw) throws IOException {
 		if (isIndexDisabled())
 			throw new IOException(JGitText.get().cachedPacksPreventsIndexCreation);
-
 		long writeStart = System.currentTimeMillis();
-		final BasePackIndexWriter iw = BasePackIndexWriter.createVersion(
-				indexStream, getIndexVersion());
 		iw.write(sortByName(), packcsum);
 		stats.timeWriting += System.currentTimeMillis() - writeStart;
 	}
@@ -2448,7 +2462,7 @@ public class PackWriter implements AutoCloseable {
 	 * object graph at selected commits. Writing a bitmap index is an optional
 	 * feature that not all pack users may require.
 	 * <p>
-	 * Called after {@link #writeIndex(OutputStream)}.
+	 * Called after {@link #writeIndex(PackIndexWriter)}.
 	 * <p>
 	 * To reduce memory internal state is cleared during this method, rendering
 	 * the PackWriter instance useless for anything further than a call to write
