6e2b9b8b38b45658318a82db0482e40c36c1777e
Fabio Ponciroli <ponch78@gmail.com>
2024-10-07 16:56:51 -0700

    MIDX implementation
    Allow object lookups through multi-pack-index [1].
    
    Note: the `multi-pack-index.v1` file used in the tests
    has been created on a local repository with the
    "git multi-pack-index" command [2].
    
    It is possible to inspect it using the CGit test-tools.
    You can build them by cloning the CGit repository [3]
    and running `make all`.
    
    Use `test-tool` as follow to inspect the MIDX:
    `./bin-wrappers/test-tool read-midx  --show-objects <objectsDir>`
    
    [1]: https://git-scm.com/docs/multi-pack-index
    [2]: https://git-scm.com/docs/git-multi-pack-index
    [3]: https://github.com/git/git
    
    Change-Id: I09def882e1e77e9f22c6236b0a035a1e80bc1ace

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/midx/ObjectOffsets.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2024, GerritForge Inc. and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.storage.file.midx;
+
+import org.eclipse.jgit.annotations.NonNull;
+import org.eclipse.jgit.util.NB;
+
+import static org.eclipse.jgit.internal.storage.file.midx.MultiPackIndexConstants.OBJECT_OFFSETS_DATA_WIDTH;
+
+/**
+ * Represent the collection of {@link MultiPackIndex.ObjectOffset}.
+ */
+public class ObjectOffsets {
+    private final byte[] data;
+
+    /**
+     * Initialize the ObjectOffsets.
+     *
+     * @param objectOffset content of ObjectOffset Chunk.
+     */
+    public ObjectOffsets(@NonNull byte[] objectOffset) {
+        this.data = objectOffset;
+    }
+
+    /**
+     * Get the metadata of a commit。
+     *
+     * @param position the position in the multi-pack-index of the object.
+     * @return the ObjectOffset.
+     */
+    public MultiPackIndex.ObjectOffset getObjectOffset(int position) {
+        int pos = position * OBJECT_OFFSETS_DATA_WIDTH;
+        final int packIntId = NB.decodeInt32(data, pos);
+        final int offset = NB.decodeInt32(data, pos + 4);
+        return new ObjectOffsetImpl(packIntId, offset);
+    }
+
+    private static class ObjectOffsetImpl implements MultiPackIndex.ObjectOffset {
+
+        private final int packIntId;
+        private final long offset;
+
+        public ObjectOffsetImpl(int packIntId, long offset) {
+            this.packIntId = packIntId;
+            this.offset = offset;
+        }
+
+        @Override
+        public int getPackIntId() {
+            return packIntId;
+        }
+
+        @Override
+        public long getOffset() {
+            return offset;
+        }
+    }
+}
