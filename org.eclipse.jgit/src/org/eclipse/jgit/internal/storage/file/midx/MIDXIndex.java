baacd20a9f2b7df1a9c249a73a274d5ee4fb8fca
Fabio Ponciroli <ponch78@gmail.com>
2024-10-07 16:56:51 -0700

    MIDX implementation
    Allow object lookups through multi-pack-index [1].
    
    [1]: https://git-scm.com/docs/multi-pack-index
    
    Change-Id: I09def882e1e77e9f22c6236b0a035a1e80bc1ace

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/midx/MIDXIndex.java
@@ -0,0 +1,102 @@
+package org.eclipse.jgit.internal.storage.file.midx;
+
+import org.eclipse.jgit.annotations.NonNull;
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.util.NB;
+
+public class MIDXIndex {
+
+    private static final int FANOUT = 256;
+
+    private final int hashLength;
+
+    private final int[] fanoutTable;
+
+    private final byte[] oidLookup;
+
+    private final long commitCnt;
+
+    /**
+     * Initialize the GraphObjectIndex.
+     *
+     * @param hashLength
+     *            length of object hash.
+     * @param oidFanout
+     *            content of OID Fanout Chunk.
+     * @param oidLookup
+     *            content of OID Lookup Chunk.
+     * @throws MIDXFormatException
+     *             commit-graph file's format is different from we expected.
+     */
+    MIDXIndex(int hashLength, @NonNull byte[] oidFanout,
+                     @NonNull byte[] oidLookup) throws MIDXFormatException {
+        this.hashLength = hashLength;
+        this.oidLookup = oidLookup;
+
+        int[] table = new int[FANOUT];
+        long uint32;
+        for (int k = 0; k < table.length; k++) {
+            uint32 = NB.decodeUInt32(oidFanout, k * 4);
+            if (uint32 > Integer.MAX_VALUE) {
+                throw new MIDXFormatException(
+                        JGitText.get().commitGraphFileIsTooLargeForJgit);
+            }
+            table[k] = (int) uint32;
+        }
+        this.fanoutTable = table;
+        this.commitCnt = table[FANOUT - 1];
+    }
+
+    /**
+     * Find the position in the commit-graph of the specified id.
+     *
+     * @param id
+     *            the id for which the commit-graph position will be found.
+     * @return the commit-graph position or -1 if the object was not found.
+     */
+    int findGraphPosition(AnyObjectId id) {
+        int levelOne = id.getFirstByte();
+        int high = fanoutTable[levelOne];
+        int low = 0;
+        if (levelOne > 0) {
+            low = fanoutTable[levelOne - 1];
+        }
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            int pos = objIdOffset(mid);
+            int cmp = id.compareTo(oidLookup, pos);
+            if (cmp < 0) {
+                high = mid;
+            } else if (cmp == 0) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Get the object at the commit-graph position.
+     *
+     * @param graphPos
+     *            the position in the commit-graph of the object.
+     * @return the ObjectId or null if it's not found.
+     */
+    ObjectId getObjectId(int graphPos) {
+        if (graphPos < 0 || graphPos >= commitCnt) {
+            return null;
+        }
+        return ObjectId.fromRaw(oidLookup, objIdOffset(graphPos));
+    }
+
+    long getCommitCnt() {
+        return commitCnt;
+    }
+
+    private int objIdOffset(int pos) {
+        return hashLength * pos;
+    }
+}
---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/midx/MIDXLoader.java
@@ -0,0 +1,130 @@
+package org.eclipse.jgit.internal.storage.file.midx;
+
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.internal.storage.commitgraph.CommitGraphFormatException;
+import org.eclipse.jgit.internal.storage.commitgraph.CommitGraphLoader;
+import org.eclipse.jgit.util.IO;
+import org.eclipse.jgit.util.NB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.eclipse.jgit.internal.storage.file.midx.MIDXConstants.*;
+
+public class MIDXLoader {
+    private final static Logger LOG = LoggerFactory
+            .getLogger(CommitGraphLoader.class);
+
+    public static MIDX read(InputStream fd)
+            throws CommitGraphFormatException, IOException {
+        byte[] hdr = new byte[12];
+        IO.readFully(fd, hdr, 0, hdr.length);
+
+        int magic = NB.decodeInt32(hdr, 0);
+
+        if (magic != MIDX_MAGIC) {
+            throw new MIDXFormatException(
+                    JGitText.get().notACommitGraph);
+        }
+
+        // Check MIDX version
+        int v = hdr[4];
+        if (v != 1) {
+            throw new MIDXFormatException(MessageFormat.format(
+                    JGitText.get().unsupportedCommitGraphVersion,
+                    v));
+        }
+
+        // Read the object Id version (1 byte)
+        // 1 => SHA-1
+        // 2 => SHA-256
+        // TODO: If the hash type does not match the repository's hash algorithm,
+        //    the multi-pack-index file should be ignored with a warning
+        //    presented to the user.
+        int commitIdVersion = hdr[5];
+        if (commitIdVersion != 1) {
+            throw new MIDXFormatException(
+                    JGitText.get().incorrectOBJECT_ID_LENGTH);
+        }
+
+        // Read the number of "chunkOffsets" (1 byte)
+        int numberOfChunks = hdr[6];
+
+        // Read the number of multi-pack-index files (1 byte)
+        // This value is currently always zero.
+        int numberOfMultiPackIndexFiles = hdr[7];
+
+        // Number of packfiles (4 bytes)
+        //TODO not used at the moment. Need to find out how to use it.
+        int numberOfPackFiles = NB.decodeInt32(hdr, 7);
+
+        byte[] lookupBuffer = new byte[CHUNK_LOOKUP_WIDTH
+                * (numberOfChunks + 1)];
+
+
+        IO.readFully(fd, lookupBuffer, 0, lookupBuffer.length);
+
+        List<ChunkSegment> chunks = new ArrayList<>(numberOfChunks + 1);
+        for (int i = 0; i <= numberOfChunks; i++) {
+            // chunks[numberOfChunks] is just a marker, in order to record the
+            // length of the last chunk.
+            int id = NB.decodeInt32(lookupBuffer, i * 12);
+            long offset = NB.decodeInt64(lookupBuffer, i * 12 + 4);
+            chunks.add(new ChunkSegment(id, offset));
+        }
+
+        MIDXBuilder builder = MIDXBuilder.builder();
+        for (int i = 0; i < numberOfChunks; i++) {
+            long chunkOffset = chunks.get(i).offset;
+            int chunkId = chunks.get(i).id;
+            long len = chunks.get(i + 1).offset - chunkOffset;
+
+            if (len > Integer.MAX_VALUE - 8) { // http://stackoverflow.com/a/8381338
+                throw new MIDXFormatException(
+                        JGitText.get().commitGraphFileIsTooLargeForJgit);
+            }
+
+            byte buffer[] = new byte[(int) len];
+            IO.readFully(fd, buffer, 0, buffer.length);
+
+            switch (chunkId) {
+                case MIDX_ID_OID_FANOUT:
+                    builder.addOidFanout(buffer);
+                    break;
+                case MIDX_ID_OID_LOOKUP:
+                    builder.addOidLookUp(buffer);
+                    break;
+                case MIDX_PACKFILE_NAMES:
+                    builder.addPackFileNames(buffer);
+                    break;
+                case MIDX_BITMAPPED_PACKFILES:
+                    builder.addBitmappedPackfiles(buffer);
+                    break;
+                case MIDX_OBJECT_OFFSETS:
+                    builder.addObjectOffsets(buffer);
+                    break;
+                default:
+                    LOG.warn(MessageFormat.format(
+                            JGitText.get().commitGraphChunkUnknown,
+                            Integer.toHexString(chunkId)));
+            }
+        }
+        return builder.build();
+    }
+
+    private static class ChunkSegment {
+        final int id;
+
+        final long offset;
+
+        private ChunkSegment(int id, long offset) {
+            this.id = id;
+            this.offset = offset;
+        }
+    }
+}
