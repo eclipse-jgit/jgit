656f3f0e4a8651885aceaa118aa6fcb2e2c26f8e
Nasser Grainawi <quic_nasserg@quicinc.com>
2024-10-08 14:45:36 -0700

    MIDX: Integrate with PackDirectory
    Change-Id: Ia1af201533fd30fdfc1aaf392bf7dfe5aed03301
    Signed-off-by: Nasser Grainawi <quic_nasserg@quicinc.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/midx/MultiPackIndexConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/midx/MultiPackIndexConstants.java
@@ -14,38 +14,43 @@ package org.eclipse.jgit.internal.storage.file.midx;
  * Constants relating to MultiPackIndex.
  */
 public class MultiPackIndexConstants {
+	/**
+	 * File name used for the multi-pack-index
+	 */
+	public static final String MIDX_FILE_NAME = "multi-pack-index";
 
-    static final int MULTI_PACK_INDEX_MAGIC = 0x4D494458; /* "MIDX" */
+	static final int MULTI_PACK_INDEX_MAGIC = 0x4D494458; /* "MIDX" */
 
-    static final int MULTI_PACK_INDEX_ID_OID_FANOUT = 0x4f494446; /* "OIDF" */
+	static final int MULTI_PACK_INDEX_ID_OID_FANOUT = 0x4f494446; /* "OIDF" */
 
-    static final int MULTI_PACK_INDEX_PACKFILE_NAMES = 0x504E414D; /* "PNAM" */
+	static final int MULTI_PACK_INDEX_PACKFILE_NAMES = 0x504E414D; /* "PNAM" */
 
-    static final int MULTI_PACK_INDEX_BITMAPPED_PACKFILES = 0x42544D50; /* "BTMP" */
+	static final int MULTI_PACK_INDEX_BITMAPPED_PACKFILES = 0x42544D50; /* "BTMP" */
 
-    static final int MULTI_PACK_INDEX_ID_OID_LOOKUP = 0x4f49444c; /* "OIDL" */
+	static final int MULTI_PACK_INDEX_ID_OID_LOOKUP = 0x4f49444c; /* "OIDL" */
 
-    static final int MULTI_PACK_INDEX_OBJECT_OFFSETS = 0x4F4F4646; /* "OOFF" */
+	static final int MULTI_PACK_INDEX_OBJECT_OFFSETS = 0x4F4F4646; /* "OOFF" */
 
-    // Optional
-    static final int MULTI_PACK_INDEX_OBJECT_LARGE_OFFSETS = 0x4C4F4646; /* "LOFF" */
+	// Optional
+	static final int MULTI_PACK_INDEX_OBJECT_LARGE_OFFSETS = 0x4C4F4646; /* "LOFF" */
 
-    // Optional
-    static final int MULTI_PACK_INDEX_BITMAP_PACK_ORDER = 0x52494458; /* "RIDX" */
+	// Optional
+	static final int MULTI_PACK_INDEX_BITMAP_PACK_ORDER = 0x52494458; /* "RIDX" */
 
-    /**
-     * First 4 bytes describe the chunk id. Value 0 is a terminating label.
-     * Other 8 bytes provide the byte-offset in current file for chunk to start.
-     */
-    static final int CHUNK_LOOKUP_WIDTH = 12;
+	/**
+	 * First 4 bytes describe the chunk id. Value 0 is a terminating label.
+	 * Other 8 bytes provide the byte-offset in current file for chunk to
+	 * start.
+	 */
+	static final int CHUNK_LOOKUP_WIDTH = 12;
 
-    /**
-     * Stores a table of two 4-byte unsigned integers in network order.
-     */
-    static final int BITMAP_PACKFILES_DATA_WIDTH = 8;
+	/**
+	 * Stores a table of two 4-byte unsigned integers in network order.
+	 */
+	static final int BITMAP_PACKFILES_DATA_WIDTH = 8;
 
-    /**
-     * Stores two 4-byte values for every object.
-     */
-    static final int OBJECT_OFFSETS_DATA_WIDTH = 8;
+	/**
+	 * Stores two 4-byte values for every object.
+	 */
+	static final int OBJECT_OFFSETS_DATA_WIDTH = 8;
 }
