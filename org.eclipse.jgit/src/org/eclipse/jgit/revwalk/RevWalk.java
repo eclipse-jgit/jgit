1fdb6a2bf8134be5278391739b7c0fdc7466da61
Xing Huang <xingkhuang@google.com>
2024-10-25 16:22:59 -0500

    TreeRevFilter: Enable merge commit BF serving
    We currently are generating Bloom Filters for merge commits
    via CommitGraphWriter, but they are not being served due to the lack of
    tests confirming their functionality. Serving them in TreeRevFilter
    requires a way to rewrite their parent list in the event that BF returns
    false.
    
    Adding parent rewrite when BF returns false on a merge commit. Adding
    additional monitoring related to serving BF for merge commits so their
    functionality matches the default treeWalk code path behavior.
    
    Change-Id: I1250d0cf2a670966a247821cd715292fe7701b66

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalk.java
@@ -1836,11 +1836,56 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 		private long numTreesParsedInTreeRevFilter = 0;
 
+		private long numMergeCommitsUsedBaseParentAsRedirect = 0;
+
+		private long numMergeCommitsUsedPullRequestParentAsRedirect = 0;
+
+		private long numMergeCommitsHadNoRedirect = 0;
+
+		private long numMergeCommitsHadNoDiffWithAnyInterestingParent = 0;
+
 		private RevFilterStats() {
 		}
 
 		/**
+		 * Increment the numMergeCommitsUsedBaseParentAsRedirect count
+		 *
+		 * @since 7.0
+		 */
+		public void incrementNumMergeCommitsUsedBaseParentAsRedirect() {
+			numMergeCommitsUsedBaseParentAsRedirect++;
+		}
+
+		/**
+		 * Increment the numMergeCommitsUsedPullRequestParentAsRedirect count
+		 *
+		 * @since 7.0
+		 */
+		public void incrementNumMergeCommitsUsedPullRequestParentAsRedirect() {
+			numMergeCommitsUsedPullRequestParentAsRedirect++;
+		}
+
+		/**
+		 * Increment the numMergeCommitsHadNoDiffWithAnyInterestingParent count
+		 *
+		 * @since 7.0
+		 */
+		public void incrementNumMergeCommitsHadNoDiffButNoInterestingParent() {
+			numMergeCommitsHadNoDiffWithAnyInterestingParent++;
+		}
+
+		/**
+		 * Increment the numMergeCommitsHadNoRedirect count
+		 *
+		 * @since 7.0
+		 */
+		public void incrementNumMergeCommitsHadNoRedirect() {
+			numMergeCommitsHadNoRedirect++;
+		}
+
+		/**
 		 * Increment the changedPathFilterTruePositive count
+		 *
 		 * @since 7.0
 		 */
 		public void incrementChangedPathFilterTruePositive() {
@@ -1849,6 +1894,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 		/**
 		 * Increment the changedPathFilterFalsePositive count
+		 *
 		 * @since 7.0
 		 */
 		public void incrementChangedPathFilterFalsePositive() {
@@ -1857,6 +1903,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 		/**
 		 * Increment the changedPathFilterNegative count
+		 *
 		 * @since 7.0
 		 */
 		public void incrementChangedPathFilterNegative() {
@@ -1865,6 +1912,7 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 		/**
 		 * Increment the numCommitsThroughTreeRevFilter count
+		 *
 		 * @since 7.0
 		 */
 		public void incrementCommitsThroughTreeRevFilter() {
@@ -1873,12 +1921,56 @@ public class RevWalk implements Iterable<RevCommit>, AutoCloseable {
 
 		/**
 		 * Increment the numTreesParsedInTreeRevFilter count
+		 *
 		 * @param numTrees
-		 * 			number of trees parsed
+		 *            number of trees parsed
 		 * @since 7.0
 		 */
 		public void incrementNumTreesParsedInTreeRevFilter(int numTrees) {
 			numTreesParsedInTreeRevFilter += numTrees;
+		}
+
+		/**
+		 * Return the number of merge commits used the base parent to redirect
+		 * the RevWalk
+		 *
+		 * @return count
+		 * @since 7.0
+		 */
+		public long getNumMergeCommitsUsedBaseParentAsRedirect() {
+			return numMergeCommitsUsedBaseParentAsRedirect;
+		}
+
+		/**
+		 * Return the number of merge commits used a pull request parent to
+		 * redirect the RevWalk
+		 *
+		 * @return count
+		 * @since 7.0
+		 */
+		public long getNumMergeCommitsUsedPullRequestParentAsRedirect() {
+			return numMergeCommitsUsedPullRequestParentAsRedirect;
+		}
+
+		/**
+		 * Return the number of merge commits did not need be redirected
+		 *
+		 * @return count
+		 * @since 7.0
+		 */
+		public long getNumMergeCommitsHadNoRedirect() {
+			return numMergeCommitsHadNoRedirect;
+		}
+
+		/**
+		 * Return the number of merge commits had no diff and had no interesting
+		 * parent to redirect
+		 *
+		 * @return count
+		 * @since 7.0
+		 */
+		public long getNumMergeCommitsHadNoDiffWithAnyInterestingParent() {
+			return numMergeCommitsHadNoDiffWithAnyInterestingParent;
 		}
 
 		/**
