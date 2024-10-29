7c092ab66e27b3ae68c7fed6d300b8645f7e4173
Saril Sudhakaran <saril.ks@gmail.com>
2024-10-29 00:17:01 -0500

    DfsGarbageCollector: Add setter for reflog expiration time.
    JGit reftable writer/compator knows how to prune the history, but the
    DfsGarbageCollector doesn't expose the time limit.
    
    Add a method to DfsGarbageCollector to set the reflog time limit.
    This value is then passed to the reftable compactor. Callers usually
    pass here the value from gc.reflogExpire.
    
    The reflog block length is stored in 24 bits [1], limiting the size to
    16MB. I have observed that in repositories with frequent commits,
    reflogs hit that size in 6-12 months.
    
    [1] https://git-scm.com/docs/reftable
    
    Bug: jgit-96
    
    Change-Id: I8b32d6d2b2e1d8af8fb7d9f86225d75f1877eb2f

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.EnumSet;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
@@ -100,6 +101,7 @@ public class DfsGarbageCollector {
 	private Set<ObjectId> allTags;
 	private Set<ObjectId> nonHeads;
 	private Set<ObjectId> tagTargets;
+	private Date refLogExpire;
 
 	/**
 	 * Initialize a garbage collector.
@@ -197,6 +199,22 @@ public class DfsGarbageCollector {
 	 */
 	public DfsGarbageCollector setReftableInitialMinUpdateIndex(long u) {
 		reftableInitialMinUpdateIndex = Math.max(u, 0);
+		return this;
+	}
+
+
+	/**
+	 *  Set time limit to the reflog history.
+         *  <p>
+         *  Garbage Collector prunes entries from reflog history older than {@code refLogExpire}
+         *  <p>
+	 *
+	 * @param refLogExpire
+	 *            instant in time which defines refLog expiration
+	 * @return {@code this}
+	 */
+	public DfsGarbageCollector setRefLogExpire(Date refLogExpire) {
+		this.refLogExpire = refLogExpire ;
 		return this;
 	}
 
@@ -741,6 +759,9 @@ public class DfsGarbageCollector {
 			compact.addAll(stack.readers());
 			compact.setIncludeDeletes(includeDeletes);
 			compact.setConfig(configureReftable(reftableConfig, out));
+			if(refLogExpire != null ){
+				compact.setReflogExpireOldestReflogTimeMillis(refLogExpire.getTime());
+			}
 			compact.compact();
 			pack.addFileExt(REFTABLE);
 			pack.setReftableStats(compact.getStats());
