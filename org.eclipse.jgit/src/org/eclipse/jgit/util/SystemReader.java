117b0ab4614b08f3c21cf20f33c9747c5ea556be
Ivan Frade <ifrade@google.com>
2024-11-04 14:21:24 -0800

    SystemReader: Offer methods with java.time API
    Error prone explains: The Date API is full of major design flaws and
    pitfalls and should be avoided at all costs. Prefer the java.time
    APIs, specifically, java.time.Instant (for physical time) and
    java.time.LocalDate[Time] (for civil time).
    
    Add to SystemReader methods to get the time and timezone in the new
    java.time classes (Instant/ZoneId) and mark as deprecated their old
    counterparts.
    
    The mapping of methods is:
    * #getCurrentTime -> #now (returns Instant instead of int)
    * #getTimezone -> #getTimeZoneAt (returns ZoneOffset intead of int)
    * #getTimeZone -> #getTimeZoneId (return ZoneId instead of TimeZone)
    
    Change-Id: Ic55b2f442a40046ff0ed24f61f566fc7416471be

--- org.eclipse.jgit/src/org/eclipse/jgit/util/SystemReader.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/SystemReader.java
@@ -25,6 +25,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.atomic.AtomicReference;
@@ -167,8 +170,18 @@ public abstract class SystemReader {
 		}
 
 		@Override
+		public Instant now() {
+			return Instant.now();
+		}
+
+		@Override
 		public int getTimezone(long when) {
 			return getTimeZone().getOffset(when) / (60 * 1000);
+		}
+
+		@Override
+		public ZoneOffset getTimeZoneAt(Instant when) {
+			return getTimeZoneId().getRules().getOffset(when);
 		}
 	}
 
@@ -228,8 +241,18 @@ public abstract class SystemReader {
 		}
 
 		@Override
+		public Instant now() {
+			return delegate.now();
+		}
+
+		@Override
 		public int getTimezone(long when) {
 			return delegate.getTimezone(when);
+		}
+
+		@Override
+		public ZoneOffset getTimeZoneAt(Instant when) {
+			return delegate.getTimeZoneAt(when);
 		}
 	}
 
@@ -501,8 +524,20 @@ public abstract class SystemReader {
 	 * Get the current system time
 	 *
 	 * @return the current system time
+	 *
+	 * @deprecated Use {@link #now()}
 	 */
+	@Deprecated
 	public abstract long getCurrentTime();
+
+	/**
+	 * Get the current system time
+	 *
+	 * @return the current system time
+	 *
+	 * @since 7.1
+	 */
+	public abstract Instant now();
 
 	/**
 	 * Get clock instance preferred by this system.
@@ -520,17 +555,42 @@ public abstract class SystemReader {
 	 * @param when
 	 *            a system timestamp
 	 * @return the local time zone
+	 *
+	 * @deprecated Use {@link #getTimeZoneAt(Instant)} instead.
 	 */
+	@Deprecated
 	public abstract int getTimezone(long when);
+
+	/**
+	 * Get the local time zone offset at "when" time
+	 *
+	 * @param when
+	 *            a system timestamp
+	 * @return the local time zone
+	 */
+	public abstract ZoneOffset getTimeZoneAt(Instant when);
 
 	/**
 	 * Get system time zone, possibly mocked for testing
 	 *
 	 * @return system time zone, possibly mocked for testing
 	 * @since 1.2
+	 *
+	 * @deprecated Use {@link #getTimeZoneId()}
 	 */
+	@Deprecated
 	public TimeZone getTimeZone() {
 		return TimeZone.getDefault();
+	}
+
+	/**
+	 * Get system time zone, possibly mocked for testing
+	 *
+	 * @return system time zone, possibly mocked for testing
+	 * @since 7.1
+	 */
+	public ZoneId getTimeZoneId() {
+		return ZoneId.systemDefault();
 	}
 
 	/**
