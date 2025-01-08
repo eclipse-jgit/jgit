8eecc57f8a84860c80c6e616feff591e926b76a0
youtirsin <youtirsin@qq.com>
2025-01-08 16:24:20 +0800

    Set repositoryformatversion config to 0 when convert ref storage format back to packed-refs.
    Change-Id: Id6ebb17e7fe1392785e51dcbdb11205f966fac96
    Signed-off-by: youtirsin <youtirsin@qq.com>

---
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/youtirsin/tests/reftable/TestRepoPaths.java
@@ -0,0 +1,19 @@
+package org.eclipse.jgit.youtirsin.tests.reftable;
+
+public class TestRepoPaths {
+
+    public static final String refTableRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/small/reftable";
+    public static final String packedRefRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/small/packed-ref";
+
+    public static final String refTableBigRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/big-repo/reftable-big-repo";
+    public static final String packedRefBigRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/big-repo/packed-ref-big-repo";
+
+    public static final String looseRefTableBigRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/big-repo-loose-refs/reftable-big-repo";
+    public static final String loosePackedRefBigRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/big-repo-loose-refs/packed-ref-big-repo";
+
+    public static final String refTableGitRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/git/reftable-git";
+    public static final String packedRefGitRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/git/packed-ref-git";
+
+    public static final String refTableTGitRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/tgit/reftable-tgit";
+    public static final String packedRefTGitRepoPath = "/Users/brocod/workspace/repos/local/reftable-test/tgit/packed-ref-tgit";
+}
