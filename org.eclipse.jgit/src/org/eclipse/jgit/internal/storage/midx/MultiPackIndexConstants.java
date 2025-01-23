072e93fded7229f00002232b33ed91a0ef614ddb
Ivan Frade <ifrade@google.com>
2025-01-23 15:49:30 -0800

    midx.MultiPackIndexWriter: a writer for the multipack index format
    Fromg git documentation[1]: While the pack-indexes provide fast lookup
    per packfile, this performance degrades as the number of packfiles
    increases, because abbreviations need to inspect every packfile and we
    are more likely to have a miss on our most-recently-used packfile. For
    some large repositories, repacking into a single packfile is not
    feasible due to storage space or excessive repack times. (...)
    
    The multi-pack-index (MIDX for short) stores a list of objects and
    their offsets into multiple packfiles. (...) Thus, we can provide
    O(log N) lookup time for any number of packfiles.
    
    This is a writer of the multipack index format. The test only verifies
    the "shape" of the file, when we get a parser we can check also the
    values (specially the large offset handling).
    
    On the JGit repository, the multipack index generated by this writer
    passes the validation of `git multi-pack-index verify`.
    
    [1] https://git-scm.com/docs/pack-format#_multi_pack_index_midx_files_have_the_following_format
    
    Change-Id: I1fca599c4ebf28154f28f039c2c4cfe75b2dc79d

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/midx/MultiPackIndexConstants.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025, Google Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.internal.storage.midx;
+
+class MultiPackIndexConstants {
+	static final int MIDX_SIGNATURE = 0x4d494458; /* MIDX */
+
+	static final byte MIDX_VERSION = 1;
+
+	/**
+	 * We infer the length of object IDs (OIDs) from this value:
+	 * 
+	 * <pre>
+	 * 1 => SHA-1
+	 * 2 => SHA-256
+	 * </pre>
+	 */
+	static final byte OID_HASH_VERSION = 1;
+
+	static final int MULTIPACK_INDEX_FANOUT_SIZE = 4 * 256;
+
+	/**
+	 * First 4 bytes describe the chunk id. Value 0 is a terminating label.
+	 * Other 8 bytes provide the byte-offset in current file for chunk to start.
+	 */
+	static final int CHUNK_LOOKUP_WIDTH = 12;
+
+	/** "PNAM" chunk */
+	static final int MIDX_CHUNKID_PACKNAMES = 0x504e414d;
+
+	/** "OIDF" chunk */
+	static final int MIDX_CHUNKID_OIDFANOUT = 0x4f494446;
+
+	/** "OIDL" chunk */
+	static final int MIDX_CHUNKID_OIDLOOKUP = 0x4f49444c;
+
+	/** "OOFF" chunk */
+	static final int MIDX_CHUNKID_OBJECTOFFSETS = 0x4f4f4646;
+
+	/** "LOFF" chunk */
+	static final int MIDX_CHUNKID_LARGEOFFSETS = 0x4c4f4646;
+
+	/** "RIDX" chunk */
+	static final int MIDX_CHUNKID_REVINDEX = 0x52494458;
+
+	private MultiPackIndexConstants() {
+	}
+}
