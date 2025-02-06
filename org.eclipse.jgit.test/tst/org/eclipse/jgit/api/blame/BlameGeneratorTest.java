22bc53c29992fd3b167e1f46850a80d8558d113b
Ivan Frade <ifrade@google.com>
2025-02-06 14:07:47 -0800

    BlameGenerator: cache and reuse blame results
    Blame goes into the history of a file until it blames all lines. If we
    have a blamed version of the file in cache, we can use it to resolve
    the lines not blamed yet and cut the calculation earlier. Gitiles
    already defines a blame cache and populates it with the final blame of
    a path.
    
    Bring the gitiles cache interface to JGit. Use the cache inside the
    blame. When processing a candidate, check if it is in the
    cache and if so fill the blame from the pending regions using the
    information of the cached copy.
    
    The generator doesn't populate the cache itself. Although it looks a
    bit weird, this is how gitiles is using the cache and we don't want to
    break it.
    
    We are bringing the interface and POJO from the gitiles cache to
    JGit. This changes the license from Apache 2.0 to BSD-3. The code is
    minimal (and no actual functionality) and Google is the copyright
    owner of both files, so this should be ok.
    
    Change-Id: Ia99b09d6d825e96940ca4200967958923bf647c7

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/blame/BlameGeneratorTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/blame/BlameGeneratorTest.java
@@ -13,30 +13,61 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.blame.BlameGenerator;
 import org.eclipse.jgit.blame.BlameResult;
+import org.eclipse.jgit.blame.cache.BlameCache;
+import org.eclipse.jgit.blame.cache.CacheRegion;
 import org.eclipse.jgit.junit.RepositoryTestCase;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /** Unit tests of {@link BlameGenerator}. */
 public class BlameGeneratorTest extends RepositoryTestCase {
+	private static final String FILE = "file.txt";
+
+	private Git git;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		git = new Git(db);
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		git.close();
+		super.tearDown();
+	}
+
 	@Test
 	public void testBoundLineDelete() throws Exception {
 		try (Git git = new Git(db)) {
 			String[] content1 = new String[] { "first", "second" };
-			writeTrashFile("file.txt", join(content1));
-			git.add().addFilepattern("file.txt").call();
+			writeTrashFile(FILE, join(content1));
+			git.add().addFilepattern(FILE).call();
 			RevCommit c1 = git.commit().setMessage("create file").call();
 
 			String[] content2 = new String[] { "third", "first", "second" };
-			writeTrashFile("file.txt", join(content2));
-			git.add().addFilepattern("file.txt").call();
+			writeTrashFile(FILE, join(content2));
+			git.add().addFilepattern(FILE).call();
 			RevCommit c2 = git.commit().setMessage("create file").call();
 
-			try (BlameGenerator generator = new BlameGenerator(db, "file.txt")) {
+			try (BlameGenerator generator = new BlameGenerator(db, FILE)) {
 				generator.push(null, db.resolve(Constants.HEAD));
 				assertEquals(3, generator.getResultContents().size());
 
@@ -47,7 +78,7 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 				assertEquals(1, generator.getResultEnd());
 				assertEquals(0, generator.getSourceStart());
 				assertEquals(1, generator.getSourceEnd());
-				assertEquals("file.txt", generator.getSourcePath());
+				assertEquals(FILE, generator.getSourcePath());
 
 				assertTrue(generator.next());
 				assertEquals(c1, generator.getSourceCommit());
@@ -56,7 +87,7 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 				assertEquals(3, generator.getResultEnd());
 				assertEquals(0, generator.getSourceStart());
 				assertEquals(2, generator.getSourceEnd());
-				assertEquals("file.txt", generator.getSourcePath());
+				assertEquals(FILE, generator.getSourcePath());
 
 				assertFalse(generator.next());
 			}
@@ -87,7 +118,8 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 			git.add().addFilepattern(FILENAME_2).call();
 			RevCommit c2 = git.commit().setMessage("change file2").call();
 
-			try (BlameGenerator generator = new BlameGenerator(db, FILENAME_2)) {
+			try (BlameGenerator generator = new BlameGenerator(db,
+					FILENAME_2)) {
 				generator.push(null, db.resolve(Constants.HEAD));
 				assertEquals(3, generator.getResultContents().size());
 
@@ -113,7 +145,8 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 			}
 
 			// and test again with other BlameGenerator API:
-			try (BlameGenerator generator = new BlameGenerator(db, FILENAME_2)) {
+			try (BlameGenerator generator = new BlameGenerator(db,
+					FILENAME_2)) {
 				generator.push(null, db.resolve(Constants.HEAD));
 				BlameResult result = generator.computeBlameResult();
 
@@ -136,21 +169,21 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 		try (Git git = new Git(db)) {
 			String[] content1 = new String[] { "first", "second", "third" };
 
-			writeTrashFile("file.txt", join(content1));
-			git.add().addFilepattern("file.txt").call();
+			writeTrashFile(FILE, join(content1));
+			git.add().addFilepattern(FILE).call();
 			git.commit().setMessage("create file").call();
 
 			String[] content2 = new String[] { "" };
 
-			writeTrashFile("file.txt", join(content2));
-			git.add().addFilepattern("file.txt").call();
+			writeTrashFile(FILE, join(content2));
+			git.add().addFilepattern(FILE).call();
 			git.commit().setMessage("create file").call();
 
-			writeTrashFile("file.txt", join(content1));
-			git.add().addFilepattern("file.txt").call();
+			writeTrashFile(FILE, join(content1));
+			git.add().addFilepattern(FILE).call();
 			RevCommit c3 = git.commit().setMessage("create file").call();
 
-			try (BlameGenerator generator = new BlameGenerator(db, "file.txt")) {
+			try (BlameGenerator generator = new BlameGenerator(db, FILE)) {
 				generator.push(null, db.resolve(Constants.HEAD));
 				assertEquals(3, generator.getResultContents().size());
 
@@ -164,10 +197,222 @@ public class BlameGeneratorTest extends RepositoryTestCase {
 		}
 	}
 
+	@Test
+	public void cache_empty() throws Exception {
+		// Baseline, blame without cache
+		RevCommit c1 = commit("first", "second", "third");
+		/*          */ commit("first", "second", "third", "fourth");
+		RevCommit c3 = commit("first", "other2", "other3", "fourth");
+		RevCommit c4 = commit("first", "other22", "other3", "other4");
+
+		try (BlameGenerator generator = new BlameGenerator(db, FILE,
+				new InMemoryBlameCache())) {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(4, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(1, generator.getResultStart());
+			assertEquals(2, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(3, generator.getResultStart());
+			assertEquals(4, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c3, generator.getSourceCommit());
+			assertEquals(2, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c1, generator.getSourceCommit());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(1, generator.getResultEnd());
+
+			assertFalse(generator.next());
+
+			assertEquals(4, generator.getStats().getCandidatesVisited());
+			assertFalse(generator.getStats().isCacheHit());
+		}
+	}
+
+	@Test
+	public void cache_tipInCache() throws Exception {
+		RevCommit c1 = commit("first", "second", "third");
+		/*          */ commit("first", "second", "third", "fourth");
+		RevCommit c3 = commit("first", "other2", "other3", "fourth");
+		RevCommit c4 = commit("first", "other22", "other3", "other4");
+
+		PersonIdent author = new PersonIdent("a", "a@b.com");
+		List<CacheRegion> c4Regions = new ArrayList<>(4);
+		c4Regions.add(new CacheRegion(FILE, c1, author, 0, 1));
+		c4Regions.add(new CacheRegion(FILE, c4, author, 1, 2));
+		c4Regions.add(new CacheRegion(FILE, c3, author, 2, 3));
+		c4Regions.add(new CacheRegion(FILE, c4, author, 3, 4));
+
+		InMemoryBlameCache cache = new InMemoryBlameCache();
+		cache.put(c4, FILE, c4Regions);
+		try (BlameGenerator generator = new BlameGenerator(db, FILE, cache)) {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(4, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c1, generator.getSourceCommit());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(1, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(1, generator.getResultStart());
+			assertEquals(2, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c3, generator.getSourceCommit());
+			assertEquals(2, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(3, generator.getResultStart());
+			assertEquals(4, generator.getResultEnd());
+
+			assertFalse(generator.next());
+
+			assertEquals(1, generator.getStats().getCandidatesVisited());
+			assertTrue(generator.getStats().isCacheHit());
+		}
+	}
+
+	@Test
+	public void cache_intermediateInCache() throws Exception {
+		RevCommit c1 = commit("first", "second", "third");
+		RevCommit c2 = commit("first", "second", "third", "fourth");
+		RevCommit c3 = commit("first", "other2", "other3", "fourth");
+		RevCommit c4 = commit("first", "other22", "other3", "other4");
+
+		PersonIdent author = new PersonIdent("a", "a@b.com");
+		List<CacheRegion> c2Regions = new ArrayList<>(4);
+		c2Regions.add(new CacheRegion(FILE, c1, author, 0, 1));
+		c2Regions.add(new CacheRegion(FILE, c2, author, 1, 3));
+		c2Regions.add(new CacheRegion(FILE, c1, author, 3, 4));
+
+		InMemoryBlameCache cache = new InMemoryBlameCache();
+		cache.put(c2, FILE, c2Regions);
+		try (BlameGenerator generator = new BlameGenerator(db, FILE, cache)) {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(4, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(1, generator.getResultStart());
+			assertEquals(2, generator.getResultEnd());
+			assertTrue(generator.next());
+
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(3, generator.getResultStart());
+			assertEquals(4, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c3, generator.getSourceCommit());
+			assertEquals(2, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c1, generator.getSourceCommit());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(1, generator.getResultEnd());
+
+			assertFalse(generator.next());
+			assertEquals(3, generator.getStats().getCandidatesVisited());
+			assertTrue(generator.getStats().isCacheHit());
+		}
+	}
+
+	@Test
+	public void cache_genCacheAndUseIt() throws Exception {
+		RevCommit c1 = commit("first", "second", "third");
+		RevCommit c2 = commit("first", "second", "third", "fourth");
+		RevCommit c3 = commit("first", "other2", "other3", "fourth");
+		RevCommit c4 = commit("first", "other22", "other3", "other4");
+
+		InMemoryBlameCache cache = blameAndCache(c2);
+		try (BlameGenerator generator = new BlameGenerator(db, FILE, cache)) {
+			generator.push(null, db.resolve(Constants.HEAD));
+			assertEquals(4, generator.getResultContents().size());
+
+			assertTrue(generator.next());
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(1, generator.getResultStart());
+			assertEquals(2, generator.getResultEnd());
+			assertTrue(generator.next());
+
+			assertEquals(c4, generator.getSourceCommit());
+			assertEquals(3, generator.getResultStart());
+			assertEquals(4, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c3, generator.getSourceCommit());
+			assertEquals(2, generator.getResultStart());
+			assertEquals(3, generator.getResultEnd());
+
+			assertTrue(generator.next());
+			assertEquals(c1, generator.getSourceCommit());
+			assertEquals(0, generator.getResultStart());
+			assertEquals(1, generator.getResultEnd());
+
+			assertFalse(generator.next());
+			assertEquals(3, generator.getStats().getCandidatesVisited());
+			assertTrue(generator.getStats().isCacheHit());
+		}
+	}
+
+	private InMemoryBlameCache blameAndCache(RevCommit commit)
+			throws IOException {
+		List<CacheRegion> regions = new ArrayList<>();
+		try (BlameGenerator generator = new BlameGenerator(db, FILE)) {
+			generator.push(null, commit);
+			while (generator.next()) {
+				regions.add(new CacheRegion(FILE,
+						generator.getSourceCommit().toObjectId(),
+						generator.getSourceAuthor(), generator.getResultStart(),
+						generator.getResultEnd()));
+			}
+		}
+		InMemoryBlameCache cache = new InMemoryBlameCache();
+		cache.put(commit, FILE, regions);
+		return cache;
+	}
+
+	private RevCommit commit(String... lines) throws Exception {
+		writeTrashFile(FILE, join(lines));
+		git.add().addFilepattern(FILE).call();
+		return git.commit().setMessage("a commit").call();
+	}
+
 	private static String join(String... lines) {
 		StringBuilder joined = new StringBuilder();
 		for (String line : lines)
 			joined.append(line).append('\n');
 		return joined.toString();
+	}
+
+	static class InMemoryBlameCache implements BlameCache {
+
+		record Key(String commitId, String path) {
+		}
+
+		Map<Key, List<CacheRegion>> cache = new HashMap<>();
+
+		@Override
+		public List<CacheRegion> get(Repository repo, ObjectId commitId,
+				String path) throws IOException {
+			return cache.get(new Key(commitId.name(), path));
+		}
+
+		void put(ObjectId commitId, String path,
+				List<CacheRegion> cachedRegions) {
+			cache.put(new Key(commitId.name(), path), cachedRegions);
+		}
 	}
 }
