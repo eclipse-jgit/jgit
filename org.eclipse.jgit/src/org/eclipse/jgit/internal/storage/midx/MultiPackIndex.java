258f086777cd544760140f03516215330b9ef583
Ivan Frade <ifrade@google.com>
2025-04-15 13:22:06 -0700

    MultiPackIndex: Add and implement #resolve() method
    The reader offers the resolve functionality to find object ids with
    certain prefix. The basic implementation iterates through packs and
    calls resolve in their indexes. With multipack index, we can answer
    the resolve directly for all the packs included.
    
    Offer a resolve() method and implement it in the multipack index.
    
    Change-Id: If5679652f149a41afe568c719ba40b291ae1b917

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/midx/MultiPackIndex.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/midx/MultiPackIndex.java
@@ -10,7 +10,12 @@
 
 package org.eclipse.jgit.internal.storage.midx;
 
+import java.io.IOException;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.AbbreviatedObjectId;
 import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
 
 /**
  * An index over multiple packs
@@ -46,6 +51,23 @@ public interface MultiPackIndex {
 	 * @return mutable instance with the location or null if not found.
 	 */
 	PackOffset find(AnyObjectId objectId);
+
+	/**
+	 * Find objects matching the prefix abbreviation.
+	 *
+	 * @param matches
+	 *            set to add any located ObjectIds to. This is an output
+	 *            parameter.
+	 * @param id
+	 *            prefix to search for.
+	 * @param matchLimit
+	 *            maximum number of results to return. At most this many
+	 *            ObjectIds should be added to matches before returning.
+	 * @throws java.io.IOException
+	 *             the index cannot be read.
+	 */
+	void resolve(Set<ObjectId> matches, AbbreviatedObjectId id, int matchLimit)
+			throws IOException;
 
 	/**
 	 * Memory size of this multipack index
