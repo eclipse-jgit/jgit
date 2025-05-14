84c5acce1cbb5c102a9e2ec3ec3d8cbf896a4d78
Ivan Frade <ifrade@google.com>
2025-05-14 13:30:17 -0700

    DfsPackDescription: Add field for required packs
    Change-Id: Iaf1a91dd80b5157fe7c79aff7981fa7f38041be3

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackDescription.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackDescription.java
@@ -15,6 +15,7 @@ import static org.eclipse.jgit.internal.storage.pack.PackExt.REFTABLE;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.internal.storage.commitgraph.CommitGraphWriter;
@@ -150,6 +151,9 @@ public class DfsPackDescription {
 	private int indexVersion;
 	private long estimatedPackSize;
 
+	// Packs required by this pack (because it is e.g. a multipack index)
+	private List<DfsPackDescription> requiredPacks;
+
 	/**
 	 * Initialize a description by pack name and repository.
 	 * <p>
@@ -207,6 +211,15 @@ public class DfsPackDescription {
 	 */
 	public boolean hasFileExt(PackExt ext) {
 		return (extensions & ext.getBit()) != 0;
+	}
+
+	/**
+	 * The basic pack name (no extension) for this pack
+	 *
+	 * @return pack name
+	 */
+	public String getPackName() {
+		return packName;
 	}
 
 	/**
@@ -525,6 +538,24 @@ public class DfsPackDescription {
 	public DfsPackDescription setIndexVersion(int version) {
 		indexVersion = version;
 		return this;
+	}
+
+	/**
+	 * Packs required by this pack (e.g because this is a multi-pack index)
+	 *
+	 * @return List of pack names without extension
+	 */
+	public List<DfsPackDescription> getRequiredPacks() {
+		return requiredPacks;
+	}
+
+	/**
+	 * Set the list of packs required by this one (usually for multi-pack index)
+	 *
+	 * @param requiredPacks list of pack names without extension
+	 */
+	public void setRequiredPacks(List<DfsPackDescription> requiredPacks) {
+		this.requiredPacks = requiredPacks;
 	}
 
 	@Override
