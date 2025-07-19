6a75c44f3b3ccb5f9a96e64825862b353aa86257
Martin Fick <mfick@nvidia.com>
2025-07-19 15:35:25 -0700

    Shortcut PackWriter reuse selection when possible
    The selection of object representations favors the delta representation
    in the oldest pack file. In light of that, it favors the smallest whole
    object representation. This was previously achieved by iterating over
    all the available representations of an object by iterating over the
    representations in each pack file from newest to oldest. This approach
    can be very expensive when there are many packfiles in a repo. Since
    this selection approach favors the oldest pack file, reverse the
    iteration so that we can find the same representation much quicker and
    then shortcut the search.
    
    For more background information as to how the selection process is
    intended to operate, see I9953ab8be54ceb8b588e1280d6f7edd688887747. This
    change hopes to additionally document some of that in the code.
    
    A small fetch from a repo with 580 packfiles, which before this change
    took around 48s, takes around 33s after this change. This fetch results
    in the same pack file, bit for bit, before and after this change.
    
    Change-Id: Iece58e0f2f6d04d8d302374428364547023a50f8
    Signed-off-by: Martin Fick <mfick@nvidia.com>

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/ShowPackDelta.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/ShowPackDelta.java
@@ -70,8 +70,9 @@ class ShowPackDelta extends TextBuiltin {
 
 		PackWriter pw = new PackWriter(reader) {
 			@Override
-			public void select(ObjectToPack otp, StoredObjectRepresentation next) {
+			public boolean select(ObjectToPack otp, StoredObjectRepresentation next) {
 				otp.select(next);
+				return true;
 			}
 		};
 
