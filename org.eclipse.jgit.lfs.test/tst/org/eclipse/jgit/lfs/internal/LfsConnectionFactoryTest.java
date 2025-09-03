14b64a014e60c8badfb118400b85d242e8bd5794
Stephan Wahlbrink <sw@wahlbrink.eu>
2025-09-03 23:58:48 +0200

    Align selection of the LFS URL more closely with git-lfs-config
    Main changes:
      - Add support for config 'remote.lfsdefault'
      - Add support for config 'remote.<remote>.lfsurl'
      - Add support for push variants of the config options
      - Add support for derived LFS URLs independent of the remote
        (was limited to 'origin')
    
    Change-Id: I928b4d314048d1ce1124a7460dd38e47e4518bb2

--- org.eclipse.jgit.lfs.test/tst/org/eclipse/jgit/lfs/internal/LfsConnectionFactoryTest.java
+++ org.eclipse.jgit.lfs.test/tst/org/eclipse/jgit/lfs/internal/LfsConnectionFactoryTest.java
@@ -9,6 +9,13 @@
  */
 package org.eclipse.jgit.lfs.internal;
 
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_LFSDEFAULT;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_LFSPUSHDEFAULT;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_LFSPUSHURL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_LFSURL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_PUSHURL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_URL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_REMOTE_SECTION;
 import static org.eclipse.jgit.lib.Constants.DEFAULT_REMOTE_NAME;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -83,42 +90,92 @@ public class LfsConnectionFactoryTest extends RepositoryTestCase {
 
 	@Test
 	public void lfsUrlFromRemoteUrlWithDotGit() throws Exception {
-		addRemoteUrl("https://localhost/repo.git");
+		addDefaultRemote("https://localhost/repo.git");
 		checkLfsUrl("https://localhost/repo.git/info/lfs");
 	}
 
 	@Test
 	public void lfsUrlFromRemoteUrlWithoutDotGit() throws Exception {
-		addRemoteUrl("https://localhost/repo");
+		addDefaultRemote("https://localhost/repo");
 		checkLfsUrl("https://localhost/repo.git/info/lfs");
 	}
 
 	@Test
 	public void lfsUrlFromLocalConfig() throws Exception {
-		addRemoteUrl("https://localhost/repo");
+		// starting with a singe remote without LFS specific configs, add step
+		// by step configs with higher priority for LFS
 
-		@SuppressWarnings("restriction")
-		StoredConfig cfg = db.getConfig();
-		cfg.setString(ConfigConstants.CONFIG_SECTION_LFS,
-				null,
-				ConfigConstants.CONFIG_KEY_URL,
-				"https://localhost/repo/lfs");
-		cfg.save();
+		// add remote -> single remote should be picked
+		String remote1 = "test1";
+		addRemote(remote1, "https://localhost/repo-test1");
+		checkLfsUrl("https://localhost/repo-test1.git/info/lfs");
+		checkLfsUploadUrl("https://localhost/repo-test1.git/info/lfs");
 
-		checkLfsUrl("https://localhost/repo/lfs");
+		setConfig(CONFIG_REMOTE_SECTION, remote1, CONFIG_KEY_PUSHURL,
+				"https://localhost/repo-test1/pushurl.git");
+		checkLfsUrl("https://localhost/repo-test1.git/info/lfs");
+		checkLfsUploadUrl("https://localhost/repo-test1/pushurl.git/info/lfs");
+
+		setConfig(CONFIG_REMOTE_SECTION, remote1, CONFIG_KEY_LFSURL,
+				"https://localhost/repo-test1/remote-lfsurl");
+		checkLfsUrl("https://localhost/repo-test1/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-test1/remote-lfsurl");
+
+		setConfig(CONFIG_REMOTE_SECTION, remote1, CONFIG_KEY_LFSPUSHURL,
+				"https://localhost/repo-test1/remote-lfspushurl");
+		checkLfsUrl("https://localhost/repo-test1/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-test1/remote-lfspushurl");
+
+		// add default remote -> default remote should be picked
+		String defaultRemote = DEFAULT_REMOTE_NAME;
+		addDefaultRemote("https://localhost/repo-origin");
+		checkLfsUrl("https://localhost/repo-origin.git/info/lfs");
+		checkLfsUploadUrl("https://localhost/repo-origin.git/info/lfs");
+
+		setConfig(CONFIG_REMOTE_SECTION, defaultRemote, CONFIG_KEY_PUSHURL,
+				"https://localhost/repo-origin/pushurl.git");
+		checkLfsUrl("https://localhost/repo-origin.git/info/lfs");
+		checkLfsUploadUrl("https://localhost/repo-origin/pushurl.git/info/lfs");
+
+		setConfig(CONFIG_REMOTE_SECTION, defaultRemote, CONFIG_KEY_LFSURL,
+				"https://localhost/repo-origin/remote-lfsurl");
+		checkLfsUrl("https://localhost/repo-origin/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-origin/remote-lfsurl");
+
+		setConfig(CONFIG_REMOTE_SECTION, defaultRemote, CONFIG_KEY_LFSPUSHURL,
+				"https://localhost/repo-origin/remote-lfspushurl");
+		checkLfsUrl("https://localhost/repo-origin/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-origin/remote-lfspushurl");
+
+		// set remote explicitly -> specified remote should be picked
+		setConfig(CONFIG_REMOTE_SECTION, null, CONFIG_KEY_LFSDEFAULT,
+				remote1);
+		checkLfsUrl("https://localhost/repo-test1/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-test1/remote-lfspushurl");
+
+		setConfig(CONFIG_REMOTE_SECTION, null, CONFIG_KEY_LFSPUSHDEFAULT,
+				defaultRemote);
+		checkLfsUrl("https://localhost/repo-test1/remote-lfsurl");
+		checkLfsUploadUrl("https://localhost/repo-origin/remote-lfspushurl");
+
+		// set LFS url independent of remotes
+		setConfig(ConfigConstants.CONFIG_SECTION_LFS, null, CONFIG_KEY_URL,
+				"https://localhost/repo/lfs-url");
+		checkLfsUrl("https://localhost/repo/lfs-url");
+		checkLfsUploadUrl("https://localhost/repo/lfs-url");
+
+		setConfig(ConfigConstants.CONFIG_SECTION_LFS, null, CONFIG_KEY_PUSHURL,
+				"https://localhost/repo/lfs-pushurl");
+		checkLfsUrl("https://localhost/repo/lfs-url");
+		checkLfsUploadUrl("https://localhost/repo/lfs-pushurl");
 	}
 
 	@Test
 	public void lfsUrlFromOriginConfig() throws Exception {
-		addRemoteUrl("https://localhost/repo");
+		addDefaultRemote("https://localhost/repo");
 
-		@SuppressWarnings("restriction")
-		StoredConfig cfg = db.getConfig();
-		cfg.setString(ConfigConstants.CONFIG_SECTION_LFS,
-				org.eclipse.jgit.lib.Constants.DEFAULT_REMOTE_NAME,
-				ConfigConstants.CONFIG_KEY_URL,
+		setConfig(CONFIG_REMOTE_SECTION, DEFAULT_REMOTE_NAME, CONFIG_KEY_LFSURL,
 				"https://localhost/repo/lfs");
-		cfg.save();
 
 		checkLfsUrl("https://localhost/repo/lfs");
 	}
@@ -170,8 +227,9 @@ public class LfsConnectionFactoryTest extends RepositoryTestCase {
 	@Test
 	public void checkGetLfsConnection_remote_lfsconfigFromWorkingDir()
 			throws Exception {
-		addRemoteUrl(ORIGIN_URL);
-		writeLfsConfig(LFS_SERVER_URL1, "lfs", DEFAULT_REMOTE_NAME, "url");
+		addDefaultRemote(ORIGIN_URL);
+		writeLfsConfig(LFS_SERVER_URL1, CONFIG_REMOTE_SECTION,
+				DEFAULT_REMOTE_NAME, "lfsurl");
 		checkLfsUrl(LFS_SERVER_URL1);
 	}
 
@@ -252,11 +310,15 @@ public class LfsConnectionFactoryTest extends RepositoryTestCase {
 		}
 	}
 
-	private void addRemoteUrl(String remotUrl) throws Exception {
+	private void addRemote(String remote, String remotUrl) throws Exception {
 		RemoteAddCommand add = git.remoteAdd();
+		add.setName(remote);
 		add.setUri(new URIish(remotUrl));
-		add.setName(org.eclipse.jgit.lib.Constants.DEFAULT_REMOTE_NAME);
 		add.call();
+	}
+
+	private void addDefaultRemote(String remotUrl) throws Exception {
+		addRemote(org.eclipse.jgit.lib.Constants.DEFAULT_REMOTE_NAME, remotUrl);
 	}
 
 	/**
@@ -308,10 +370,26 @@ public class LfsConnectionFactoryTest extends RepositoryTestCase {
 				"{lfs]\n    url = " + LFS_SERVER_URL1);
 	}
 
+	private void setConfig(String section, String subsection, String key,
+			String value) throws IOException {
+		@SuppressWarnings("restriction")
+		StoredConfig cfg = db.getConfig();
+		cfg.setString(section, subsection, key, value);
+		cfg.save();
+	}
+
 	private void checkLfsUrl(String lfsUrl) throws IOException {
 		HttpConnection lfsServerConn;
 		lfsServerConn = LfsConnectionFactory.getLfsConnection(db,
 				HttpSupport.METHOD_POST, Protocol.OPERATION_DOWNLOAD);
+
+		assertEquals(lfsUrl + Protocol.OBJECTS_LFS_ENDPOINT,
+				lfsServerConn.getURL().toString());
+	}
+
+	private void checkLfsUploadUrl(String lfsUrl) throws IOException {
+		HttpConnection lfsServerConn = LfsConnectionFactory.getLfsConnection(db,
+				HttpSupport.METHOD_POST, Protocol.OPERATION_UPLOAD);
 
 		assertEquals(lfsUrl + Protocol.OBJECTS_LFS_ENDPOINT,
 				lfsServerConn.getURL().toString());
