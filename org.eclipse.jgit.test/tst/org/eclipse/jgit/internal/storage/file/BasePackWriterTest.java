121e62bbd062437b5874938e93e1b1d5c36b1a29
Luca Milanesio <luca.milanesio@gmail.com>
2025-09-15 14:36:42 +0100

    BasePackWriterTest: Fix message on pack index content assertion
    When asserting that a PackIndex contained all the expected objects, the
    check for an ObjectId X contained in the PackIndex but not in the list
    of expected ones was notified as: "Pack index didn't contain the
    expected id X"
    
    The above message was exactly the opposite of what was asserted.
    
    The assertion failed when the ObjectId X, fetched from the loop of all
    objects in the PackIndex, was not in the list of expected objects, but
    the message stated that the ObjectId X was expected, which was wrong,
    and the PackIndex did not contain the ObjectId X, which was again wrong
    because it was coming *precisely* from the PackIndex.
    
    Use the correct informative message when the assertion fails, stating
    exactly that ObjectId X was coming from PackIndex, and it was not
    expected because it wasn't contained in the expected set.
    
    Change-Id: Iebdadbb6068e561f501031b4dccb406297e31648

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/BasePackWriterTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/internal/storage/file/BasePackWriterTest.java
@@ -598,8 +598,7 @@ public class BasePackWriterTest extends SampleDataRepositoryTestCase {
 				pi.getObjectCount());
 		for (int i = 0; i < pi.getObjectCount(); i++)
 			assertTrue(
-					"Pack index didn't contain the expected id "
-							+ pi.getObjectId(i),
+				"Pack index contained " + pi.getObjectId(i) + " which was unexpected",
 					expected.contains(pi.getObjectId(i)));
 	}
 
