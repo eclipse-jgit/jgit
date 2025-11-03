cfa12c2538fe2f45c37d9c047500fb59dbff7a26
Ivan Frade <ifrade@google.com>
2025-11-03 13:38:02 -0800

    Revert "PackList: keep the list of unused midxs"
    This reverts commit 82204b94d584e29825b7eb62f16b6d7975f71f34.
    
    Change-Id: Iba5c9b063efc573736e1ba5b6ddc0a656a6a6964

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/MidxPackFilter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/MidxPackFilter.java
@@ -9,13 +9,12 @@
  */
 package org.eclipse.jgit.internal.storage.dfs;
 
-import java.util.ArrayList;
+import org.eclipse.jgit.internal.storage.pack.PackExt;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import org.eclipse.jgit.internal.storage.pack.PackExt;
 
 /**
  * Format a flat list of packs and midxs into a valid list of packs.
@@ -36,25 +35,16 @@ public class MidxPackFilter {
 	 *
 	 * @param packs
 	 *            flat list of all packs in the repo, may include midx. Packs
-	 *            and other midxs covered by the midx appear also on their own.
-	 * @param unusedMidxs
-	 *            midx found but not used
+	 *            covered by the midx appear also on their own.
 	 * @return a list of packs without midxs
 	 */
 	public static List<DfsPackDescription> skipMidxs(
-			List<DfsPackDescription> packs,
-			List<DfsPackDescription> unusedMidxs) {
+			List<DfsPackDescription> packs) {
 		// Covered packs appear also on their own in the list, so we can just
 		// take the midx out.
-		List<DfsPackDescription> withoutMidx = new ArrayList<>();
-		for (DfsPackDescription pack : packs) {
-			if (pack.hasFileExt(PackExt.MULTI_PACK_INDEX)) {
-				unusedMidxs.add(pack);
-			} else {
-				withoutMidx.add(pack);
-			}
-		}
-		return withoutMidx;
+		return packs.stream()
+				.filter(desc -> !desc.hasFileExt(PackExt.MULTI_PACK_INDEX))
+				.collect(Collectors.toList());
 	}
 
 	/**
@@ -63,17 +53,12 @@ public class MidxPackFilter {
 	 * This verifies that all referenced packs by the midxs exist.
 	 *
 	 * @param packs
-	 *            flat list of all packs in the repo, may include midx. Packs *
-	 *            and other midxs covered by the midx appear also on their own.
-	 * @param unusedMidxs
-	 *            output parameter to report midxs that couldn't be used (e.g.
-	 *            missing covered packs).
+	 *            list of packs with maybe some midxs
 	 * @return midxs and uncovered packs. All the input packs if no midx. Ignore
 	 *         midxs with missing covered packs.
 	 */
 	public static List<DfsPackDescription> useMidx(
-			List<DfsPackDescription> packs,
-			List<DfsPackDescription> unusedMidxs) {
+			List<DfsPackDescription> packs) {
 		// Take the packs covered by the midxs out of the list
 		List<DfsPackDescription> midxs = packs.stream()
 				.filter(desc -> desc.hasFileExt(PackExt.MULTI_PACK_INDEX))
@@ -90,7 +75,7 @@ public class MidxPackFilter {
 			if (!inputPacks.containsAll(coveredPacks)) {
 				// This midx references packs not in the pack db.
 				// It could be part of a chain, so we just ignore all midxs
-				return skipMidxs(packs, unusedMidxs);
+				return skipMidxs(packs);
 			}
 			allCoveredPacks.addAll(coveredPacks);
 		}
