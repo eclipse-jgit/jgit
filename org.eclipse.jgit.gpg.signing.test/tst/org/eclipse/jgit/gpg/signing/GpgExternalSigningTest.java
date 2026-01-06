f89acccc31074d27eb2d6be20f3fd4474e15076c
David Baker Effendi <david@brokk.ai>
2026-01-06 16:21:48 +0200

    GpgSigning: Add org.eclipse.jgit.gpg.signing.test bundle
    Change-Id: I8ba53cfdd1be5801fd4ddc86b330529f0254bc9f

---
+++ org.eclipse.jgit.gpg.signing.test/tst/org/eclipse/jgit/gpg/signing/GpgExternalSigningTest.java
@@ -0,0 +1,11 @@
+package org.eclipse.jgit.gpg.signing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class GpgExternalSigningTest {
+	@Test
+	public void testPlaceholder() {
+		assertTrue(true);
+	}
+}
