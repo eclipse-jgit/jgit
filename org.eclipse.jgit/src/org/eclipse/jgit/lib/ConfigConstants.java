c062f95b1d68cb00cfa81b5f33b0e9bb303c1d4f
Luca Milanesio <luca.milanesio@gmail.com>
2026-01-08 14:24:28 +0000

    Set a limit to the number of search packs per object lookup
    When looking up an object by id, the pack list could change because of
    an ongoing receive-packs or other server-side operations. The
    PackDirectory.open() includes a loop for searching for any modifications
    in the underlying packfiles list, so that the search is retried if the
    list is changing.
    
    The do/while loop was potentially infinite as long as the pack list was
    different, opening the door to high system load and high latency in case
    of concurrency.
    
    Introduce a new pack.maxSearchPacksCount, defaulted to Integer.MAX_VALUE
    (2^31-1), which is used as upper bound for the number of packfiles
    scans.
    
    Change-Id: I8efdb1346bc08e1857eb275cdb33d32d2638259e
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -1140,4 +1140,9 @@ public final class ConfigConstants {
 	 * @since 7.5
 	 */
 	public static final String CONFIG_KEY_MULTIPACKINDEX = "multiPackIndex";
+
+	/**
+	 * The "rapidObjectPackLookup" key
+	 */
+	public static final String CONFIG_KEY_MAX_SEARCH_PACKS_COUNT = "maxSearchPacksCount";
 }
