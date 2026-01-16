46afb04c1f552efa7dfdcd66d5d0b4afb6fbf1cd
Adithya Chakilam <achakilam@nvidia.com>
2026-01-16 11:11:28 -0600

    pgm: add git forwarder command and pass-through routing core
    Introduce GitForwarder to parse the first pkt-line, extract request
    metadata, and proxy traffic via a routing listener with a fixed
    route helper.
    
    Change-Id: I17001e2ce085d3b02ee5f3d59f4b257d519b4b98

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/forwarder/RouteRequest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2026, Nvidia
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.transport.forwarder;
+
+import java.io.IOException;
+import java.net.Socket;
+import org.eclipse.jgit.annotations.NonNull;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Route request metadata.
+ *
+ * @param socket incoming socket
+ * @param commandInfo parsed command info
+ * @param sourceIp source IP address
+ * @param listenAddr listen address
+ *
+ * @since 7.7
+ */
+public record RouteRequest(
+		@NonNull Socket socket,
+		@NonNull CommandInfo commandInfo,
+		@NonNull String sourceIp,
+		@NonNull InetSocketAddress listenAddr) {
+
+	/**
+	 * Constructor for RouteRequest
+	 *
+	 * @param socket incoming socket
+	 * @param sourceIp source IP address
+	 * @param listenAddr listen address
+	 *
+	 * @throws IOException if failed to parse command infor from the socket.
+	 */
+	public RouteRequest(Socket socket, String sourceIp, InetSocketAddress listenAddr) throws IOException {
+		this(socket, new CommandInfo(socket), sourceIp, listenAddr);
+	}
+}
