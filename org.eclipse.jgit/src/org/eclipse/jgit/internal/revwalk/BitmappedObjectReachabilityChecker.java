17df297dbb7962825b279755e912b0e5156dc1f4
Adithya Chakilam <achakilam@nvidia.com>
2026-05-01 10:52:43 -0500

    Improve bitmap reachability check performance
    Speed up reachability validation by performing target
    tracking entirely through bitmap operations.
    
    This improved ACL check time during upload packs for partial
    clone on a repository having ~85 million objects from 10 hours
    to 5 hours.
    
    Change-Id: I5577f6c8637fd1a7e3cc81b3bba3ffb07c8f7cec

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/revwalk/BitmappedObjectReachabilityChecker.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/revwalk/BitmappedObjectReachabilityChecker.java
@@ -10,7 +10,6 @@
 package org.eclipse.jgit.internal.revwalk;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
@@ -20,6 +19,7 @@ import java.util.stream.Stream;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.BitmapIndex;
 import org.eclipse.jgit.lib.BitmapIndex.BitmapBuilder;
 import org.eclipse.jgit.revwalk.BitmapWalker;
 import org.eclipse.jgit.revwalk.ObjectReachabilityChecker;
@@ -55,11 +55,15 @@ public class BitmappedObjectReachabilityChecker
 	@Override
 	public Optional<RevObject> areAllReachable(Collection<RevObject> targets,
 			Stream<RevObject> starters) throws IOException {
-
 		try {
-			List<RevObject> remainingTargets = new ArrayList<>(targets);
+			BitmapIndex bitmapIndex = walk.getObjectReader().getBitmapIndex();
+			BitmapBuilder targetBitmap = bitmapIndex.newBitmapBuilder();
+			for (RevObject obj : targets) {
+				targetBitmap.addObject(obj, obj.getType());
+			}
+
 			BitmapWalker bitmapWalker = new BitmapWalker(walk,
-					walk.getObjectReader().getBitmapIndex(), null);
+					bitmapIndex, null);
 
 			Iterator<RevObject> starterIt = starters.iterator();
 			BitmapBuilder seen = null;
@@ -69,13 +73,14 @@ public class BitmappedObjectReachabilityChecker
 						true);
 				seen = seen == null ? visited : seen.or(visited);
 
-				remainingTargets.removeIf(seen::contains);
-				if (remainingTargets.isEmpty()) {
+				targetBitmap = targetBitmap.andNot(visited);
+
+				if (targetBitmap.cardinality() == 0) {
 					return Optional.empty();
 				}
 			}
 
-			return Optional.of(remainingTargets.get(0));
+			return Optional.of(walk.parseAny(targetBitmap.iterator().next().getObjectId()));
 		} catch (MissingObjectException | IncorrectObjectTypeException e) {
 			throw new IllegalStateException(e);
 		}
