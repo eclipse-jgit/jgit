0fa93d58a6ddcd90f1b7c7e92b36ca4a357e1f31
Florian Schwabe <Florian.Schwabe@vector.com>
2026-05-13 13:53:48 +0200

    ResolveMerger: Retrieve Attributes lazily during Three-way merge
    Instead of eagerly retrieving the attributes of the current path for the
    three trees (BASE, OURS, THEIRS), the attributes are now only retrieved
    lazily when needed.
    
    This can significantly improve performance of the Cherry-Pick, Merge,
    Apply Stash and Revert actions in repositories with large worktrees and
    many attribute rules.
    
    Bug: jgit-266
    Change-Id: I4698eee2ff36711267683a9925d1b1d268aac4a4

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/MergeAttributes.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2026 Vector Informatik GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.merge;
+
+import org.eclipse.jgit.attributes.Attributes;
+import org.eclipse.jgit.attributes.AttributesNodeProvider;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+/**
+ * Provides {@link Attributes} for the trees during content-merge of a three way
+ * merge
+ *
+ * @since 7.7
+ */
+public interface MergeAttributes {
+
+	/**
+	 * Attributes used when the {@link TreeWalk} has no
+	 * {@link AttributesNodeProvider}
+	 */
+	static MergeAttributes NO_ATTRIBUTES = new MergeAttributes() {
+
+		private static final Attributes EMPTY_ATTRIBUTES = new Attributes();
+
+		@Override
+		public Attributes base() {
+			return EMPTY_ATTRIBUTES;
+		}
+
+		@Override
+		public Attributes ours() {
+			return EMPTY_ATTRIBUTES;
+		}
+
+		@Override
+		public Attributes theirs() {
+			return EMPTY_ATTRIBUTES;
+		}
+	};
+
+	/**
+	 * Retrieve the {@link Attributes} for the common base between ours and
+	 * theirs
+	 *
+	 * @return {@link Attributes} for the common base between ours and theirs
+	 */
+	Attributes base();
+
+	/**
+	 * Retrieve the {@link Attributes} for the 'ours' side of the merge
+	 *
+	 * @return {@link Attributes} for the 'ours' side of the merge
+	 */
+	Attributes ours();
+
+	/**
+	 * Retrieve the {@link Attributes} for the 'theirs' side of the merge
+	 *
+	 * @return {@link Attributes} for the 'theirs' side of the merge
+	 */
+	Attributes theirs();
+
+}
