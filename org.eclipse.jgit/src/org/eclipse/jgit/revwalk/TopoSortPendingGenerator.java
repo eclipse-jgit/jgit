eclipse-jgit/jgit

commit ab51fe2bdb9d7f05287b686e75be7afc46d0c8f5

RevWalk: Use an incremental topological sorting algorithm
When RevSort TOPO is given, instead of the previous combination of
PendingGenerator->RewriteGenerator->TopoSortGenerator, a specialized
Generator is now used, which (given a commit-graph file is present)
doesn't need to fully process the entire history first

The new TopoSortPendingGenerator uses a modified version of the
three-walk algorithm used by git and performs the tasks previously done
by the Pending-, Rewrite- and TopoSortGenerator:
 - The RevFilter is applied in the explore Phase
 - Rewriting commits (only when rewriteParents is set) is performed
   after exploring, but before calculating in-degree numbers
 - Commits are only sorted topologically up to the lowest generation
   number seen up to that point

Change-Id: I7e38a8f866bb8f46e455aedd82c3985c256841e0

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TopoSortPendingGenerator.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (c) 2026 Vector Informatik GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.revwalk;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+
+class TopoSortPendingGenerator extends Generator {
+
+	private final int output;
+
+	private final TopoExplorePhase explorePhase;
+
+	private final TopoInDegreePhase inDegreePhase;
+
+	protected TopoSortPendingGenerator(RevWalk walker, AbstractRevQueue pending,
+			RevFilter filter, int output, boolean canDispose)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		super(walker.isFirstParent());
+
+		this.output = (output | SORT_COMMIT_TIME_DESC | SORT_TOPO)
+				& ~(NEEDS_REWRITE);
+		this.explorePhase = new TopoExplorePhase(walker, filter, canDispose);
+		this.inDegreePhase = new TopoInDegreePhase(walker, explorePhase,
+				(output & NEEDS_REWRITE) != 0);
+
+		inDegreePhase.initialize(pending);
+	}
+
+	@Override
+	int outputType() {
+		return output;
+	}
+
+	@Override
+	RevCommit next() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		while (true) {
+			RevCommit c = inDegreePhase.nextReady();
+
+			if (c == null) {
+				return null;
+			}
+
+			c.add(RevFlag.SEEN);
+
+			if (explorePhase.hasPassedFilter(c)) {
+				return c;
+			}
+		}
+	}
+
+}

org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/AbstractRevWalkWithCommitGraphTest.java
@@ -1,0 +1,150 @@
+/*
+ * Copyright (C) 2023, Tencent and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.revwalk;
+
+import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraph.EMPTY;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.internal.storage.file.GC;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.treewalk.filter.TreeFilter;
+
+public abstract class AbstractRevWalkWithCommitGraphTest extends RevWalkTestCase {
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		reinitializeRevWalk();
+		mockSystemReader.setJGitConfig(new MockConfig());
+	}
+
+	protected final void assertCommitCntInGraph(int expect) {
+		assertEquals(expect, rw.commitGraph().getCommitCnt());
+	}
+
+	protected final void assertCommits(List<RevCommit> expect,
+			List<RevCommit> actual) {
+		assertEquals(expect.size(), actual.size());
+
+		for (int i = 0; i < expect.size(); i++) {
+			RevCommit c1 = expect.get(i);
+			RevCommit c2 = actual.get(i);
+			assertEquals(c1.getId(), c2.getId());
+			assertEquals(c1.getTree(), c2.getTree());
+			assertEquals(c1.getCommitTime(), c2.getCommitTime());
+			assertArrayEquals(c1.getParents(), c2.getParents());
+			assertArrayEquals(c1.getRawBuffer(), c2.getRawBuffer());
+		}
+	}
+
+	protected final Ref branch(RevCommit commit, String name) throws Exception {
+		return Git.wrap(db).branchCreate().setName(name)
+				.setStartPoint(commit.name()).call();
+	}
+
+	protected final List<RevCommit> travel(TreeFilter treeFilter,
+			RevFilter revFilter, RevSort revSort, boolean enableCommitGraph,
+			String... starts)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException, AmbiguousObjectException {
+		db.getConfig().setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_COMMIT_GRAPH, enableCommitGraph);
+
+		try (RevWalk walk = new RevWalk(db)) {
+			walk.setTreeFilter(treeFilter);
+			walk.setRevFilter(revFilter);
+			walk.sort(revSort);
+			walk.setRetainBody(false);
+			for (String start : starts) {
+				walk.markStart(walk.lookupCommit(db.resolve(start)));
+			}
+			List<RevCommit> commits = new ArrayList<>();
+
+			if (enableCommitGraph) {
+				assertTrue(walk.commitGraph().getCommitCnt() > 0);
+			} else {
+				assertEquals(EMPTY, walk.commitGraph());
+			}
+
+			for (RevCommit commit : walk) {
+				commits.add(commit);
+			}
+			return commits;
+		}
+	}
+
+	protected final void enableAndWriteCommitGraph() throws Exception {
+		db.getConfig().setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
+				ConfigConstants.CONFIG_COMMIT_GRAPH, true);
+		db.getConfig().setBoolean(ConfigConstants.CONFIG_GC_SECTION, null,
+				ConfigConstants.CONFIG_KEY_WRITE_COMMIT_GRAPH, true);
+		db.getConfig().setBoolean(ConfigConstants.CONFIG_GC_SECTION, null,
+				ConfigConstants.CONFIG_KEY_WRITE_CHANGED_PATHS, true);
+		GC gc = new GC(db);
+		gc.gc().get();
+	}
+
+	protected final void reinitializeRevWalk() {
+		rw.close();
+		rw = new RevWalk(db);
+	}
+
+	private static final class MockConfig extends FileBasedConfig {
+		private MockConfig() {
+			super(null, null);
+		}
+
+		@Override
+		public void load() throws IOException, ConfigInvalidException {
+			// Do nothing
+		}
+
+		@Override
+		public void save() throws IOException {
+			// Do nothing
+		}
+
+		@Override
+		public boolean isOutdated() {
+			return false;
+		}
+
+		@Override
+		public String toString() {
+			return "MockConfig";
+		}
+
+		@Override
+		public boolean getBoolean(final String section, final String name,
+				final boolean defaultValue) {
+			if (section.equals(ConfigConstants.CONFIG_COMMIT_GRAPH_SECTION)
+					&& name.equals(
+							ConfigConstants.CONFIG_KEY_READ_CHANGED_PATHS)) {
+				return true;
+			}
+			return defaultValue;
+		}
+	}
+
+}

org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevWalkCommitGraphTest.java
@@ -11,7 +11,6 @@
 package org.eclipse.jgit.revwalk;
 
 import static java.util.Arrays.asList;
-import static org.eclipse.jgit.internal.storage.commitgraph.CommitGraph.EMPTY;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -20,22 +19,17 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.diff.DiffConfig;
-import org.eclipse.jgit.errors.ConfigInvalidException;
-import org.eclipse.jgit.internal.storage.file.GC;
 import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.filter.MessageRevFilter;
 import org.eclipse.jgit.revwalk.filter.RevFilter;
-import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
 import org.eclipse.jgit.treewalk.filter.ChangedPathTreeFilter;
 import org.eclipse.jgit.treewalk.filter.OrTreeFilter;
@@ -43,16 +37,7 @@ import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.junit.Test;
 
-public class RevWalkCommitGraphTest extends RevWalkTestCase {
-
-	private RevWalk rw;
-
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		rw = new RevWalk(db);
-		mockSystemReader.setJGitConfig(new MockConfig());
-	}
+public class RevWalkCommitGraphTest extends AbstractRevWalkWithCommitGraphTest {
 
 	@Test
 	public void testParseHeaders() throws Exception {
@@ -801,109 +786,4 @@ public class RevWalkCommitGraphTest extends RevWalkTestCase {
 						false, compare));
 	}
 
-	void assertCommitCntInGraph(int expect) {
-		assertEquals(expect, rw.commitGraph().getCommitCnt());
-	}
-
-	void assertCommits(List<RevCommit> expect, List<RevCommit> actual) {
-		assertEquals(expect.size(), actual.size());
-
-		for (int i = 0; i < expect.size(); i++) {
-			RevCommit c1 = expect.get(i);
-			RevCommit c2 = actual.get(i);
-
-			assertEquals(c1.getId(), c2.getId());
-			assertEquals(c1.getTree(), c2.getTree());
-			assertEquals(c1.getCommitTime(), c2.getCommitTime());
-			assertArrayEquals(c1.getParents(), c2.getParents());
-			assertArrayEquals(c1.getRawBuffer(), c2.getRawBuffer());
-		}
-	}
-
-	Ref branch(RevCommit commit, String name) throws Exception {
-		return Git.wrap(db).branchCreate().setName(name)
-				.setStartPoint(commit.name()).call();
-	}
-
-	List<RevCommit> travel(TreeFilter treeFilter, RevFilter revFilter,
-			RevSort revSort, boolean enableCommitGraph, String... starts)
-			throws Exception {
-		db.getConfig().setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
-				ConfigConstants.CONFIG_COMMIT_GRAPH, enableCommitGraph);
-
-		try (RevWalk walk = new RevWalk(db)) {
-			walk.setTreeFilter(treeFilter);
-			walk.setRevFilter(revFilter);
-			walk.sort(revSort);
-			walk.setRetainBody(false);
-			for (String start : starts) {
-				walk.markStart(walk.lookupCommit(db.resolve(start)));
-			}
-			List<RevCommit> commits = new ArrayList<>();
-
-			if (enableCommitGraph) {
-				assertTrue(walk.commitGraph().getCommitCnt() > 0);
-			} else {
-				assertEquals(EMPTY, walk.commitGraph());
-			}
-
-			for (RevCommit commit : walk) {
-				commits.add(commit);
-			}
-			return commits;
-		}
-	}
-
-	void enableAndWriteCommitGraph() throws Exception {
-		db.getConfig().setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
-				ConfigConstants.CONFIG_COMMIT_GRAPH, true);
-		db.getConfig().setBoolean(ConfigConstants.CONFIG_GC_SECTION, null,
-				ConfigConstants.CONFIG_KEY_WRITE_COMMIT_GRAPH, true);
-		db.getConfig().setBoolean(ConfigConstants.CONFIG_GC_SECTION, null,
-				ConfigConstants.CONFIG_KEY_WRITE_CHANGED_PATHS, true);
-		GC gc = new GC(db);
-		gc.gc().get();
-	}
-
-	private void reinitializeRevWalk() {
-		rw.close();
-		rw = new RevWalk(db);
-	}
-
-	private static final class MockConfig extends FileBasedConfig {
-		private MockConfig() {
-			super(null, null);
-		}
-
-		@Override
-		public void load() throws IOException, ConfigInvalidException {
-			// Do nothing
-		}
-
-		@Override
-		public void save() throws IOException {
-			// Do nothing
-		}
-
-		@Override
-		public boolean isOutdated() {
-			return false;
-		}
-
-		@Override
-		public String toString() {
-			return "MockConfig";
-		}
-
-		@Override
-		public boolean getBoolean(final String section, final String name,
-				final boolean defaultValue) {
-			if (section.equals(ConfigConstants.CONFIG_COMMIT_GRAPH_SECTION)
-					&& name.equals(
-							ConfigConstants.CONFIG_KEY_READ_CHANGED_PATHS)) {
-				return true;
-			}
-			return defaultValue;
-		}
-	}
 }

org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevWalkSortTopoWithCommitGraphTest.java
@@ -1,0 +1,163 @@
+/*
+ * Copyright (C) 2026, Vector Informatik GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package org.eclipse.jgit.revwalk;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.assertj.core.api.ThrowingConsumer;
+import org.eclipse.jgit.internal.storage.commitgraph.CommitGraph;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.treewalk.filter.ChangedPathTreeFilter;
+import org.junit.Test;
+
+public class RevWalkSortTopoWithCommitGraphTest
+		extends AbstractRevWalkWithCommitGraphTest {
+
+	@Test
+	public void testSort_TOPO_WithTreeFilter() throws Exception {
+		RevCommit a = commit(tree(file("a", blob("1"))));
+		RevCommit b = commit(tree(file("a", blob("2"))), a);
+		RevCommit c1 = commit(-5, tree(file("a", blob("3"))), b);
+		RevCommit c2 = commit(10,
+				tree(file("a", blob("2")), file("b", blob("1"))), b);
+		RevCommit d = commit(tree(file("a", blob("3")), file("b", blob("1"))),
+				c1, c2);
+		RevCommit e = commit(tree(file("a", blob("4")), file("b", blob("2"))),
+				d);
+		branch(e, "main");
+
+		testWalkBehavior( //
+				walk -> {
+					walk.sort(RevSort.TOPO);
+					walk.setTreeFilter(ChangedPathTreeFilter.create("a"));
+					walk.markStart(walk.lookupCommit(e));
+				}, //
+				actual -> {
+					assertEquals(4, actual.size());
+
+					assertRewrittenCommit(actual.get(0), e, c1);
+					assertRewrittenCommit(actual.get(1), c1, b);
+					assertRewrittenCommit(actual.get(2), b, a);
+					assertRewrittenCommit(actual.get(3), a);
+				} //
+		);
+	}
+
+	@Test
+	public void testSort_TOPO_WithTreeFilter_Reachable() throws Exception {
+		RevCommit a = commit(tree(file("a", blob("1"))));
+		RevCommit b = commit(tree(file("a", blob("2"))), a);
+		RevCommit c1 = commit(-5, tree(file("a", blob("3"))), b);
+		RevCommit c2 = commit(10,
+				tree(file("a", blob("2")), file("b", blob("1"))), b);
+		RevCommit d = commit(tree(file("a", blob("3")), file("b", blob("1"))),
+				c1, c2);
+		RevCommit e = commit(tree(file("a", blob("4")), file("b", blob("2"))),
+				d);
+		branch(e, "main");
+
+		testWalkBehavior( //
+				walk -> {
+					walk.sort(RevSort.TOPO);
+					walk.setTreeFilter(ChangedPathTreeFilter.create("a"));
+					walk.markStart(walk.lookupCommit(e));
+					walk.markUninteresting(walk.lookupCommit(b));
+				}, //
+				actual -> {
+					assertEquals(2, actual.size());
+
+					assertRewrittenCommit(actual.get(0), e, c1);
+					assertRewrittenCommit(actual.get(1), c1, b);
+				} //
+		);
+	}
+
+	@Test
+	public void testSort_TOPO_WithTreeFilter_Reachable2() throws Exception {
+		RevCommit a = commit(tree(file("a", blob("1"))));
+		RevCommit b = commit(tree(file("a", blob("2"))), a);
+		RevCommit c1 = commit(-5, tree(file("a", blob("3"))), b);
+		RevCommit c2 = commit(10,
+				tree(file("a", blob("2")), file("b", blob("1"))), b);
+		RevCommit d = commit(tree(file("a", blob("3")), file("b", blob("1"))),
+				c1, c2);
+		RevCommit e = commit(tree(file("a", blob("4")), file("b", blob("2"))),
+				d);
+		branch(e, "main");
+
+		testWalkBehavior( //
+				walk -> {
+					walk.sort(RevSort.TOPO);
+					walk.setTreeFilter(ChangedPathTreeFilter.create("a"));
+					walk.markStart(walk.lookupCommit(e));
+					walk.markUninteresting(walk.lookupCommit(e));
+				}, //
+				actual -> {
+					assertEquals(0, actual.size());
+				} //
+		);
+	}
+
+	// Test the RevWalk behavior with and without enabled commit graph
+	private void testWalkBehavior(ThrowingConsumer<RevWalk> configureWalker,
+			Consumer<List<RevCommit>> assertResult) throws Exception {
+		configureWalker.accept(rw);
+		List<RevCommit> actual = new ArrayList<>();
+		rw.forEach(actual::add);
+
+		assertEquals(CommitGraph.EMPTY, rw.commitGraph());
+		for (RevCommit c : actual) {
+			assertEquals(Constants.COMMIT_GENERATION_UNKNOWN,
+					c.getGeneration());
+		}
+
+		assertResult.accept(actual);
+
+		enableAndWriteCommitGraph();
+		reinitializeRevWalk();
+
+		configureWalker.accept(rw);
+		actual.clear();
+		rw.forEach(actual::add);
+
+		assertNotEquals(0, rw.commitGraph().getCommitCnt());
+		for (RevCommit c : actual) {
+			assertNotEquals(Constants.COMMIT_GENERATION_UNKNOWN,
+					c.getGeneration());
+		}
+
+		assertResult.accept(actual);
+	}
+
+	private void assertRewrittenCommit(RevCommit actual, AnyObjectId expectId,
+			RevCommit... expectParents) {
+		assertObjectId(actual, expectId);
+		assertEquals(expectParents.length, actual.getParentCount());
+
+		for (int i = 0; i < expectParents.length; i++) {
+			assertObjectId(actual.getParent(i), expectParents[i]);
+		}
+	}
+
+	private void assertObjectId(AnyObjectId actual, AnyObjectId expectId) {
+		if (!AnyObjectId.isEqual(expectId, actual)) {
+			fail("Expected object id <%s>, got <%s>".formatted(expectId.name(),
+					actual.name()));
+		}
+	}
+
+}

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/PendingGenerator.java
@@ -72,16 +72,16 @@ class PendingGenerator extends Generator {
 	 */
 	private int overScan = OVER_SCAN;
 
-	boolean canDispose;
+	private final boolean canDispose;
 
-	PendingGenerator(final RevWalk w, final DateRevQueue p,
-			final RevFilter f, final int out) {
+	PendingGenerator(RevWalk w, DateRevQueue p, RevFilter f, int out,
+			boolean canDispose) {
 		super(w.isFirstParent());
-		walker = w;
-		pending = p;
-		filter = f;
-		output = out;
-		canDispose = true;
+		this.walker = w;
+		this.pending = p;
+		this.filter = f;
+		this.output = out;
+		this.canDispose = canDispose;
 	}
 
 	@Override

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevCommit.java
@@ -723,13 +723,37 @@ public class RevCommit extends RevObject {
 	}
 
 	/**
+	 * Get the distance of the commit from the root, as defined in
+	 * {@link CommitGraph}
+	 * <p>
+	 * This method differs from {@link #getGeneration()} in that it will never
+	 * return {@link Constants#COMMIT_GENERATION_NOT_COMPUTED}. If the
+	 * Generation number was not calculated for this commit, then
+	 * {@link Constants#COMMIT_GENERATION_UNKNOWN} will be returned instead.
+	 *
+	 * @return the generation number, never
+	 *         {@link Constants#COMMIT_GENERATION_NOT_COMPUTED}
+	 * @since 7.8
+	 */
+	final int getEffectiveGeneration() {
+		int gen = getGeneration();
+
+		if (gen == Constants.COMMIT_GENERATION_NOT_COMPUTED) {
+			return Constants.COMMIT_GENERATION_UNKNOWN;
+		}
+
+		return gen;
+	}
+
+	/**
 	 * Get the changed path filter of the commit.
 	 * <p>
 	 * This is null when there is no commit graph file, the commit is not in the
 	 * commit graph file, or the commit graph file was generated without changed
 	 * path filters.
 	 *
-	 * @param rw A revwalk to load the commit graph (if available)
+	 * @param rw
+	 *            A revwalk to load the commit graph (if available)
 	 * @return the changed path filter
 	 * @since 6.7
 	 */

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/RevWalkUtils.java
@@ -195,4 +195,27 @@ public final class RevWalkUtils {
 		return result;
 	}
 
+	/**
+	 * If not already parsed, parses the headers of the given commit using the
+	 * given Rev Walker
+	 *
+	 * @param commit
+	 *            The commit to parse
+	 * @param walker
+	 *            The RevWalk to be used
+	 * @throws MissingObjectException
+	 *             if the object is missing
+	 * @throws IncorrectObjectTypeException
+	 *             if the object has an unexpected type
+	 * @throws IOException
+	 *             if an IO error occurred
+	 */
+	static void ensureHeadersParsed(RevCommit commit, RevWalk walker)
+			throws MissingObjectException, IncorrectObjectTypeException,
+			IOException {
+		if ((commit.flags & RevWalk.PARSED) == 0) {
+			commit.parseHeaders(walker);
+		}
+	}
+
 }

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/StartGenerator.java
@@ -110,18 +110,33 @@ class StartGenerator extends Generator {
 
 		walker.queue = q;
 
+		if (walker.hasRevSort(RevSort.TOPO)
+				&& walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)) {
+			throw new IllegalStateException(JGitText
+					.get().cannotCombineTopoSortWithTopoKeepBranchTogetherSort);
+		}
+
 		if (walker instanceof DepthWalk) {
 			DepthWalk dw = (DepthWalk) walker;
 			g = new DepthGenerator(dw, pending);
-		} else {
-			g = new PendingGenerator(w, pending, rf, pendingOutputType);
 
-			if (walker.hasRevSort(RevSort.BOUNDARY)) {
-				// Because the boundary generator may produce uninteresting
-				// commits we cannot allow the pending generator to dispose
-				// of them early.
-				//
-				((PendingGenerator) g).canDispose = false;
+			if (walker.hasRevSort(RevSort.TOPO)) {
+				g = new TopoSortGenerator(g);
+			}
+		} else {
+			// Because the boundary generator may produce uninteresting
+			// commits we cannot allow the pending generator to dispose
+			// of them early.
+			//
+			boolean canDispose = !walker.hasRevSort(RevSort.BOUNDARY);
+
+			if (walker.hasRevSort(RevSort.TOPO)) {
+				// Doesn't need rewrite
+				g = new TopoSortPendingGenerator(w, pending, rf,
+						pendingOutputType, canDispose);
+			} else {
+				g = new PendingGenerator(w, pending, rf, pendingOutputType,
+						canDispose);
 			}
 		}
 
@@ -129,16 +144,7 @@ class StartGenerator extends Generator {
 			g = new RewriteGenerator(g);
 		}
 
-		if (walker.hasRevSort(RevSort.TOPO)
-				&& walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)) {
-			throw new IllegalStateException(JGitText
-					.get().cannotCombineTopoSortWithTopoKeepBranchTogetherSort);
-		}
-
-		if (walker.hasRevSort(RevSort.TOPO)
-				&& (g.outputType() & SORT_TOPO) == 0) {
-			g = new TopoSortGenerator(g);
-		} else if (walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)
+		if (walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)
 				&& (g.outputType() & SORT_TOPO) == 0) {
 			g = new TopoNonIntermixSortGenerator(g);
 		}

commit 86d06851da54db5e9c087066cae01c9d2cc462d4

[errorprone] Fix UnnecessaryParentheses error caused by ab51fe2bdb
See https://errorprone.info/bugpattern/UnnecessaryParentheses

Change-Id: I621aad148bef7b51fb00ff23ff75f5af5843af33

org.eclipse.jgit/src/org/eclipse/jgit/revwalk/TopoSortPendingGenerator.java
@@ -30,7 +30,7 @@ class TopoSortPendingGenerator extends Generator {
 		super(walker.isFirstParent());
 
 		this.output = (output | SORT_COMMIT_TIME_DESC | SORT_TOPO)
-				& ~(NEEDS_REWRITE);
+				& ~NEEDS_REWRITE;
 		this.explorePhase = new TopoExplorePhase(walker, filter, canDispose);
 		this.inDegreePhase = new TopoInDegreePhase(walker, explorePhase,
 				(output & NEEDS_REWRITE) != 0);