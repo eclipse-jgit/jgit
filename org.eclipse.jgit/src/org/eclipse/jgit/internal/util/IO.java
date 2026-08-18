417f914edec6641bd9d1a5bb21fc1d4a753dd626
Martin Fick <mfick@nvidia.com>
2026-08-18 15:30:58 -0700

    Handle interrupt in fundamental methods
    Add simple interrupt checks to the main IO paths in ObjectDirectory and
    to the main reading path in WindowCursor. Most of these methods are
    fairly fundamental and called from within long outter loops making them
    good check points for interrupts without having to litter the code base
    with hundreds of interrupt checks. They also are not too low level that
    the checks would become expensive.
    
    We have seen Gerrit upload-packs which have been killed continue to
    iterate in the PackWrite.writeObjects() which calls
    WindowCursor.copyObjectAsIs(), hopefully this change will lead to such
    tasks being terminatted after this change.
    
    Change-Id: Ifa7768a97824d8eec8f18addb2efc4edf6171032
    Signed-off-by: Martin Fick <mfick@nvidia.com>

---
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/util/IO.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025, NVIDIA Corporation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.eclipse.jgit.internal.util;
+
+import java.io.InterruptedIOException;
+
+/**
+ * Input/Output utilities
+ */
+public class IO {
+	/**
+	 * Check wether the current thread is interrupted and throw an
+	 * InterruptedIOException if so.
+	 *
+	 * @throws java.io.InterruptedIOException
+	 *             if the current thread is interrupted
+	 */
+	public static void throwIfInterrupted() throws InterruptedIOException {
+		if (Thread.currentThread().isInterrupted()) {
+			throw new InterruptedIOException();
+		}
+	}
+
+	private IO() {
+		// Don't create instances of a static only utility.
+	}
+}
