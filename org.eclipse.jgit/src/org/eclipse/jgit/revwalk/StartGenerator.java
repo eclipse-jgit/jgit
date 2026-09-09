7f471a9669b7f2b4bed92b2dd4293decfbe93d52
Florian Schwabe <florian.schwabe@vector.com>
2026-09-09 11:28:15 +0200

    StartGenerator: Revert back to TopoSortGenerator
    This partially reverts ab51fe2bdb9d7f05287b686e75be7afc46d0c8f5.
    
    With the new TopoSortPendingGenerator, reachability checks were still
    correct but experienced a performance regression.
    
    The previous chain of PendingGenerator -> TopoSortGenerator could exit
    early, if all pending commits were UNINTERESTING. The new
    TopoSortPendingGenerator does not have this mechanism and can produce
    longer walks in some conditions.
    
    For example, when the target is a recent commit in a short branch,
    other branches are older but much longer. Old walk marks the target as
    uninteresting quickly and stops; new walk needs to go through all
    commits in the other branches with bigger generation number before
    stopping.
    
    Fixes proposed in https://gerrithub.io/c/eclipse-jgit/jgit/+/1248173
    and https://gerrithub.io/c/eclipse-jgit/jgit/+/1247844 help to
    evaluate less commits but don't solve completely the problem. They are
    still valid if TopoSortPendingGenerator is re-enabled with other fixes.
    
    Change-Id: I573f980abb0c97414e8e7dfcc3ac6dab2544f6c1

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/StartGenerator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/StartGenerator.java
@@ -119,10 +119,6 @@ class StartGenerator extends Generator {
 		if (walker instanceof DepthWalk) {
 			DepthWalk dw = (DepthWalk) walker;
 			g = new DepthGenerator(dw, pending);
-
-			if (walker.hasRevSort(RevSort.TOPO)) {
-				g = new TopoSortGenerator(g);
-			}
 		} else {
 			// Because the boundary generator may produce uninteresting
 			// commits we cannot allow the pending generator to dispose
@@ -130,21 +126,18 @@ class StartGenerator extends Generator {
 			//
 			boolean canDispose = !walker.hasRevSort(RevSort.BOUNDARY);
 
-			if (walker.hasRevSort(RevSort.TOPO)) {
-				// Doesn't need rewrite
-				g = new TopoSortPendingGenerator(w, pending, rf,
-						pendingOutputType, canDispose);
-			} else {
-				g = new PendingGenerator(w, pending, rf, pendingOutputType,
-						canDispose);
-			}
+			g = new PendingGenerator(w, pending, rf, pendingOutputType,
+					canDispose);
 		}
 
 		if ((g.outputType() & NEEDS_REWRITE) != 0) {
 			g = new RewriteGenerator(g);
 		}
 
-		if (walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)
+		if (walker.hasRevSort(RevSort.TOPO)
+				&& (g.outputType() & SORT_TOPO) == 0) {
+			g = new TopoSortGenerator(g);
+		} else if (walker.hasRevSort(RevSort.TOPO_KEEP_BRANCH_TOGETHER)
 				&& (g.outputType() & SORT_TOPO) == 0) {
 			g = new TopoNonIntermixSortGenerator(g);
 		}
